24e6ad18f0edecfac25df9aa7d8dbe385b655510
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-01 11:23:30 +0300

    Add babel/ES2015 compilation capability and html templating

--- webapp/index.js
+++ webapp/index.js
@@ -5,7 +5,6 @@ function component () {
 
     /* lodash is required for the next line to work */
     element.innerHTML = R.concat('Hello','webpack');
-
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './webapp/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: [require('babel-plugin-transform-object-rest-spread')]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './web-resources/index.html'})
+    ]
 };
