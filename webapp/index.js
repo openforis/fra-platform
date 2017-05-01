5d7ca30139530eb1608f50e72aae83aa1875fad3
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-01 18:16:55 +0300

    Add less processing and hashes to referenced filenames

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,12 @@
 import R from 'ramda';
 
+require('../web-resources/testStyle.less');
+
 function component () {
     var element = document.createElement('div');
 
     /* lodash is required for the next line to work */
-    element.innerHTML = R.concat('Hello','webpack');
+    element.innerHTML = R.concat('Hello','frap');
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
     entry: './webapp/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle-[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -20,11 +21,19 @@ module.exports = {
                         plugins: [require('babel-plugin-transform-object-rest-spread')]
                     }
                 }
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader']
+                })
             }
         ]
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin({filename: 'styles-[hash].css'}),
         new HtmlWebpackPlugin({template: './web-resources/index.html'})
     ]
 };
