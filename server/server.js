1f23be1a5d286f80eb07bfadea958355022fa2b2
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-02 14:55:19 +0300

    Heroku-specific tuning

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const compression = require('compression')
 
 const app = express()
-const serverHttpPort = 9001
+const serverHttpPort = process.env.PORT || 9001
 
 app.use(compression({threshold: 512}))
 app.use('/', express.static(`${__dirname}/../dist`))
