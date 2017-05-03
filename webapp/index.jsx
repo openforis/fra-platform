5a5af610132d71ba575eac3e5dadaea4943e4cc7
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-03 15:32:21 +0300

    first try with redux

--- webapp/index.jsx
+++ webapp/index.jsx
@@ -1,12 +1,25 @@
 // import R from 'ramda';
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 
 import Routes from './routes'
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const initial = {
+  msg: "hello"
+}
+
+let reducer = (state=initial, action) => {
+  return state
+}
+
 function renderApp() {
     ReactDOM.render(
-        <Routes/>,
+        <Provider store={createStore(reducer)}>
+          <Routes />
+        </Provider>,
         document.getElementById( 'main' )
     );
 }
--- webapp/national-data-entry.js
+++ webapp/national-data-entry.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
+import * as R from 'ramda'
+
 // {/*<Route exact path={match.url} render={() => (*/}
 // {/*<h3>No country selected</h3>*/}
 // {/*)}/>*/}
 
-const NationalDataEntry = ( { match } ) => (
+const NationalDataEntry = ({match, msg} ) => (
     <div>
         <Link to="/">Back home</Link>
-        
+        <h1>{msg}</h1>
         <h3>{match.params.countryIso}</h3>
-    
+
+        <input></input>
     </div>
 )
 
-export  default NationalDataEntry
+const mapstateToProps = R.identity
+
+export default connect(mapstateToProps)(NationalDataEntry)
