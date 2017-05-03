acd0a128e9da399031f93d66c43dafe2d53e307c
Mino Togna <Cosimo.Togna@Fao.org>
2017-05-03 16:57:26 +0300

    added test post function

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const compression = require('compression')
+const express     = require( 'express' )
+const compression = require( 'compression' )
 
-const app = express()
+const app            = express()
 const serverHttpPort = process.env.PORT || 9001
 
-app.use(compression({threshold: 512}))
-app.use('/', express.static(`${__dirname}/../dist`))
+app.use( compression( { threshold: 512 } ) )
+app.use( '/', express.static( `${__dirname}/../dist` ) )
 
-app.listen(serverHttpPort, (req, resp) => {
-    console.log('FRA Platform server listening on port ', serverHttpPort)
-})
+app.post( '/api/data', ( req, res ) => {
+    console.log( req )
+    res.send( 'ok' )
+} )
+
+
+app.listen( serverHttpPort, ( req, resp ) => {
+    console.log( 'FRA Platform server listening on port ', serverHttpPort )
+} )
+
