fc3211e1359391d1754e2d48d0f8a0a026145f82
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-04 11:37:52 +0300

    Simple table and change actions

--- webapp/index.jsx
+++ webapp/index.jsx
@@ -1,40 +1,47 @@
-// import R from 'ramda';
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Routes from './routes'
-
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import * as R from "ramda"
+import React from "react"
+import ReactDOM from "react-dom"
+import Routes from "./routes"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import thunkMiddleware from "redux-thunk"
 
 const initial = {
-  msg: "hello"
+  msg: "hello",
+  data: {
+    "FRA2005": {
+      name: "FRA2005",
+      value: 0
+    },
+    "FRA2010": {
+      name: "FRA2010",
+      value: 10
+    }
+  }
 }
 
 const actions = {
-  'CHANGE_MSG': (state, action) => ({...state, msg: action.newMsg, _debug: "msg"}),
-  'CHANGE_START': (state, action) => ({...state, _debug: "start"})
+  'CHANGED_VALUE': (state, action) =>
+    R.assocPath(['data', action.name, 'value'], action.value, state),
+  'CHANGE_START': (state, action) => ({...state})
 }
 
-let reducer = (state=initial, action) => {
-    console.log("action", action)
-    const actionHandler = actions[action.type]
-    if (actionHandler) return actionHandler(state, action)
-    return state
+let reducer = (state = initial, action) => {
+  console.log("action", action)
+  const actionHandler = actions[action.type]
+  if (actionHandler) return actionHandler(state, action)
+  return state
 }
 
 const store = createStore(reducer, applyMiddleware(thunkMiddleware))
 
-// store.subscribe(() => console.log(store.getState()))
-
 function renderApp() {
-    ReactDOM.render(
-        <Provider store={store}>
-          <Routes />
-        </Provider>,
-        document.getElementById( 'main' )
-    );
+  ReactDOM.render(
+    <Provider store={store}>
+      <Routes />
+    </Provider>,
+    document.getElementById('main')
+  );
 }
 
 renderApp()
--- webapp/national-data-entry.js
+++ webapp/national-data-entry.js
@@ -1,34 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {connect} from 'react-redux'
-import * as R from 'ramda'
+import React from "react"
+import { Link } from "react-router-dom"
+import { connect } from "react-redux"
+import * as R from "ramda"
+import axios from "axios"
 
-import axios from 'axios'
+const DataTable = ({columns, valueChange}) =>
+  <table>
+    <thead>
+    <tr>
+      {
+        R.keys(columns).map(v =>
+          <th key={v}>{v}</th>
+        )
+      }
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      {
+        R.values(columns).map(v =>
+          <td key={v.name}>
+            <input
+              value={v.value}
+              onChange={ e => {valueChange(v.name, e.currentTarget.value)}}/>
+          </td>
+        )
+      }
+    </tr>
+    </tbody>
+  </table>
 
-// {/*<Route exact path={match.url} render={() => (*/}
-// {/*<h3>No country selected</h3>*/}
-// {/*)}/>*/}
+const DataInput = ({name, ...props}) =>
+  <div>
+    <h2>{name}</h2>
+    <DataTable {...props} />
+  </div>
 
-const NationalDataEntry = ({match, msg, changeMsg}) => (
-    <div>
-        <Link to="/">Back home</Link>
-        <h1>{msg}</h1>
-        <h3>{match.params.countryIso}</h3>
-
-        <input onChange={(evt) => changeMsg(evt.target.value)}></input>
-    </div>
-)
+const NationalDataEntry = ({match, data, valueChange}) => {
+  console.log("data", data)
+  return <div>
+    <Link to="/">Back home</Link>
+    <h3>{match.params.countryIso}</h3>
+    <DataInput name="Forest area" columns={data} valueChange={valueChange}/>
+  </div>
+}
 
 const mapstateToProps = R.identity
 
-const changedMsg = newMsg => ({type: 'CHANGE_MSG', newMsg})
+const changedValue = ({name, value}) => ({type: 'CHANGED_VALUE', name, value})
 const changeStart = () => ({type: 'CHANGE_START'})
 
-const changeMsg = newMsg => dispatch => {
-    dispatch(changeStart())
-    return axios.post('/api/data', {newMsg}).then(() => {
-      dispatch(changedMsg(newMsg))
-    })
+const valueChange = (name, value) => dispatch => {
+  dispatch(changeStart())
+  return axios.post('/api/data', {name, value}).then(() => {
+    dispatch(changedValue({name, value}))
+  })
 }
 
-export default connect(mapstateToProps, {changeMsg})(NationalDataEntry)
+export default connect(mapstateToProps, {valueChange})(NationalDataEntry)
