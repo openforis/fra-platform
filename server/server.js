1ed8b24cba7f520cb171d4d5b40940ae2e359755
Joni Rajanen <joni.rajanen@reaktor.com>
2017-05-05 12:36:21 +0300

    Saving data to file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express     = require( 'express' )
+const bodyParser  = require('body-parser')
 const compression = require( 'compression' )
+
+const os = require('os')
+const fs = require('fs')
 
 const app            = express()
 const serverHttpPort = process.env.PORT || 9001
 
 app.use( compression( { threshold: 512 } ) )
 app.use( '/', express.static( `${__dirname}/../dist` ) )
+app.use(bodyParser.json({limit: '5000kb'}))
 
 app.post( '/api/data', ( req, res ) => {
-    console.log( req )
-    res.send( 'ok' )
+    console.log(os.tmpdir())
+    fs.writeFile(os.tmpdir() + '/italy.json', JSON.stringify(req.body), () => {
+        res.send( 'ok' )
+    })
 } )
 
 
