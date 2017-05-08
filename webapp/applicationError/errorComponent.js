ae141a34e4069af855490a3153ddc62c90f739f5
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-08 12:40:19 +0300

    Some styling for error box

--- webapp/applicationError/errorComponent.js
+++ webapp/applicationError/errorComponent.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from "react-redux"
+import "./style.less"
 
-const ErrorComponent = ({msg}) => msg ? <div>{msg}</div> : null
+const ErrorComponent = ({msg}) => msg ? <div className="ae__container">{msg}</div> : null
 
 export default connect(state => state['applicationError'])(ErrorComponent)
