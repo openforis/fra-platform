70424d0278c3df3e5236c27609b3b2c47dbed551
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-08 09:56:17 +0300

    Promisified filesystem operations

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express     = require( 'express' )
 const bodyParser  = require('body-parser')
 const compression = require( 'compression' )
+const os          = require('os')
+const Promise     = require('bluebird')
+const fs          = Promise.promisifyAll(require('fs'))
 
-const initialData  = require('./initialState')
-
-const os = require('os')
-const fs = require('fs')
-
+const initialData    = require('./initialState')
 const app            = express()
 const serverHttpPort = process.env.PORT || 9001
 
@@ -17,21 +16,23 @@ app.use(bodyParser.json({limit: '5000kb'}))
 
 app.post( '/api/country/:countryIso', ( req, res ) => {
     console.log('tmp dir:', os.tmpdir())
-    fs.writeFile(os.tmpdir() + `/${req.params['countryIso']}.json`, JSON.stringify(req.body), () => {
-        res.send( 'ok' )
-    })
+    fs.writeFileAsync(`${os.tmpdir()}/${req.params['countryIso']}.json`, JSON.stringify(req.body))
+        .then(() => {
+            res.send({})
+        }).catch((err) => {
+            console.log('Could not write file', err)
+            res.status(500).send({error: 'Could not write country data'})
+        })
 })
 app.get('/api/country/:countryIso', (req, res) => {
-   fs.readFile(os.tmpdir() + `/${req.params['countryIso']}.json`, (err, data) => {
-     if(err) {
-       return res.send(JSON.stringify(initialData))
-     }
-       return res.send(data)
-   })
+   fs.readFileAsync(`${os.tmpdir()}/${req.params['countryIso']}.json`)
+       .then((data) => {
+         return res.send(data)
+        }).catch((_) => {
+           return res.send(JSON.stringify(initialData))
+       })
 })
-
 
 app.listen( serverHttpPort, ( req, resp ) => {
     console.log( 'FRA Platform server listening on port ', serverHttpPort )
-} )
-
+})
