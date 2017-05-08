9e173c9bbea581e999629b6e8a008e625125c0ea
Mino Togna <Cosimo.Togna@Fao.org>
2017-05-08 14:06:18 +0300

    added FRA default reporting years to initial state

--- server/initialState.js
+++ server/initialState.js
@@ -1,19 +1,40 @@
 module.exports = {
     reportingYears: {
-        "2005": {
-            name  : "2005",
-            value : null,
-            status: undefined
+        "1990": {
+            name  : "1900",
+            value : null
+        },
+        "2000": {
+            name  : "2000",
+            value : null
         },
         "2010": {
             name  : "2010",
-            value : null,
-            status: undefined
+            value : null
         },
         "2015": {
             name  : "2015",
-            value : null,
-            status: undefined
+            value : null
+        },
+        "2016": {
+            name  : "2016",
+            value : null
+        },
+        "2017": {
+            name  : "2017",
+            value : null
+        },
+        "2018": {
+            name  : "2018",
+            value : null
+        },
+        "2019": {
+            name  : "2019",
+            value : null
+        },
+        "2020": {
+            name  : "2020",
+            value : null
         }
     }
 }
