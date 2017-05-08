a11e895aa540b52521b830e89901aa20bd98d2fc
Toni Strandell <toni.strandell@reaktor.fi>
2017-05-08 10:21:59 +0300

    Remove FRA from table headers, as the questionnaire is FRA2020

--- server/initialState.js
+++ server/initialState.js
@@ -1,17 +1,17 @@
 module.exports = {
   columns: {
     "FRA2005": {
-      name: "FRA2005",
+      name: "2005",
       value: 0,
       status: undefined
     },
     "FRA2010": {
-      name: "FRA2010",
+      name: "2010",
       value: 10,
       status: undefined
     },
     "FRA2015": {
-      name: "FRA2015",
+      name: "2015",
       value: 0,
       status: undefined
     }
