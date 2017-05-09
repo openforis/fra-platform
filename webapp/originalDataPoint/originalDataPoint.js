580b726744afb11cfa2c4ec15a09849792e6b1eb
Mino Togna <Cosimo.Togna@Fao.org>
2017-05-09 10:53:21 +0300

    added years to select odp component

--- webapp/originalDataPoint/originalDataPoint.js
+++ webapp/originalDataPoint/originalDataPoint.js
@@ -3,15 +3,16 @@ import "./style.less"
 import React from "react"
 import { Link } from "react-router-dom"
 
+const years = [ 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 ]
+
 const DataInput = ( props ) => {
     return <div className="odp__data-input-component">
         <div className="odp_data-input-row">
             <div><h3>Year</h3></div>
             <div>
                 <select>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
+                    <option></option>
+                    {years.map( ( year ) => <option key={year} value={year}>{year}</option> )}
                 </select>
             </div>
         </div>
@@ -27,11 +28,10 @@ const DataInput = ( props ) => {
     </div>
 }
 
-const OriginalDataPoint = ( { match } ) => {
-    return <div className="odp__container">
+const OriginalDataPoint = ( { match } ) =>
+    <div className="odp__container">
         <h2>Add original data point</h2>
         <DataInput countryIso={match.params.countryIso}/>
     </div>
-}
 
 export default OriginalDataPoint
