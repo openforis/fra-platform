77f2ac4590bcc494718e2bfe0a4f733068227a01
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-15 11:27:43 +0300

    Re-indent

--- server/eof/api.js
+++ server/eof/api.js
@@ -20,56 +20,58 @@ module.exports.init = app => {
         const odp = eofRepository.readOriginalDataPoints(req.params.countryIso)
 
         Promise.all([fra, odp]).then(result => {
-                const forestAreas = R.pipe(
-                  R.merge(forestAreaTableResponse.fra),
-                  R.merge(result[1]),
-                  R.values,
-                  R.sort((a,b) => a.year - b.year)
-                )(result[0])
-                return res.json({fra: forestAreas})
-            })
+            const forestAreas = R.pipe(
+                R.merge(forestAreaTableResponse.fra),
+                R.merge(result[1]),
+                R.values,
+                R.sort((a, b) => a.year - b.year)
+            )(result[0])
+            return res.json({fra: forestAreas})
+        })
             .catch(err => sendErr(res, err))
     })
 
     app.get('/api/country/originalDataPoint/:odpId', (req, res) => {
-        eofRepository.getOdp(req.params.odpId).then(resp =>
-            res.json({odpId: resp.odp_id, forestArea: resp.forest_area, year: resp.year})
-        )
-        .catch(err => sendErr(res, err))
+        eofRepository.getOdp(req.params.odpId)
+            .then(resp =>
+                res.json({odpId: resp.odp_id, forestArea: resp.forest_area, year: resp.year})
+            )
+            .catch(err => sendErr(res, err))
     })
 
-  app.post('/api/country/originalDataPoint/draft/:countryIso', (req, res) => {
-    const odpId = req.params.odpId
-    const countryIso = req.params.countryIso
-    if (!odpId)
-      eofRepository.createOdp(countryIso).then(newOdpId => {
-        updateOdp(newOdpId, countryIso, req.body, res)
-      })
-    else {
-      updateOdp(odpId, countryIso, req.body, res)
+    app.post('/api/country/originalDataPoint/draft/:countryIso', (req, res) => {
+        const odpId = req.params.odpId
+        const countryIso = req.params.countryIso
+        if (!odpId)
+            eofRepository.createOdp(countryIso)
+                .then(newOdpId => {
+                    updateOdp(newOdpId, countryIso, req.body, res)
+                })
+        else {
+            updateOdp(odpId, countryIso, req.body, res)
+        }
+    })
+
+    const updateOdp = (odpId, countryIso, body, res) => {
+        console.log("updating odp id", odpId)
+        eofRepository.getDraftId(odpId)
+            .then(draftId => {
+                if (!draftId) {
+                    eofRepository.insertDraft(countryIso, body)
+                        .then(id => res.json({odpId}))
+                        .catch(err => sendErr(res, err))
+                }
+                else {
+                    eofRepository.updateDraft(body)
+                        .then(() => res.json({odpId}))
+                        .catch(err => sendErr(res, err))
+                }
+            })
     }
-  })
 
-  const updateOdp = (odpId, countryIso, body, res) => {
-    console.log("updating odp id", odpId)
-    eofRepository.getDraftId(odpId).then(draftId => {
-      if (!draftId) {
-        eofRepository.insertDraft(countryIso, body)
-          .then(id => res.json({odpId}))
-          .catch(err => sendErr(res, err))
-      }
-      else {
-        eofRepository.updateDraft(body)
-          .then(() => res.json({odpId}))
-          .catch(err => sendErr(res, err))
-      }
-    })
-  }
-
-  app.post('/api/country/originalDataPoint/draft/markAsActual/:opdId', (req, res) =>
-      eofRepository.markAsActual(req.params.opdId)
-          .then(
-              () => res.json({}))
-          .catch(err => sendErr(res, err))
-  )
+    app.post('/api/country/originalDataPoint/draft/markAsActual/:opdId', (req, res) =>
+        eofRepository.markAsActual(req.params.opdId)
+            .then(() => res.json({}))
+            .catch(err => sendErr(res, err))
+    )
 }
