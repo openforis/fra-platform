c2c81bf213b2326da99b287ccf036b1a507a51f6
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-17 15:35:11 +0300

    Add some styles for bottom-bar

--- webapp/footer/footer.js
+++ webapp/footer/footer.js
@@ -1,8 +1,10 @@
+import "./style.less"
+
 import React from "react"
 import { connect } from "react-redux"
 
-const Footer = ({status}) => console.log(status) || <div>
-        {status}
+const Footer = ({status}) => <div className="footer__container">
+        <span className="footer__autosave-status">{status}</span>
     </div>
 
 const mapStateToProps = state => state.autoSave
