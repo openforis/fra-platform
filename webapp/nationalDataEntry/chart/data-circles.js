0bb9b38f8a444ddb12ec2c7b1428e06fe565d203
Mino Togna <Cosimo.Togna@Fao.org>
2017-05-24 16:35:03 +0200

    added two connected lines for odp and fra values

--- webapp/nationalDataEntry/chart/data-circles.js
+++ webapp/nationalDataEntry/chart/data-circles.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as d3 from 'd3'
+import R from 'ramda'
 
 const renderCircles = ({xScale, yScale}) => {
   return (d, index) => {
@@ -7,7 +8,7 @@ const renderCircles = ({xScale, yScale}) => {
     const circleProps = {
       cx: xScale(d.year),
       cy: yScale(d.forestArea),
-      r: d.type === 'odp' ? 4 : 7,
+      r: d.type === 'odp' ? 4 : 6,
       key: index
     }
 
@@ -22,6 +23,7 @@ const renderCircles = ({xScale, yScale}) => {
 const renderLines = ({xScale, yScale, data}) => d3.line()
   .x((d) => xScale(d.year))
   .y((d) => yScale(d.forestArea))
+  .curve(d3.curveLinear)
   (data)
 
 const renderImage = ({data, width, padding}) => {
@@ -37,11 +39,18 @@ const DataCircles = (props) => {
 
   return <g>
     { renderImage(props) }
-    <path d={renderLines(props)} style={{
+    <path d={renderLines({...props, data: R.filter(v => v.type === 'odp', props.data)})} style={{
       fill: 'none',
       stroke: '#acacb3',
       strokeWidth: 1.5,
       shapeRendering: 'geometricPrecision'
+    }}></path>
+    <path d={renderLines({...props, data: R.filter(v => v.type === 'fra', props.data)})} style={{
+      fill: 'none',
+      stroke: 'rgba(73,144,226,.6)',
+      strokeWidth: 3,
+      shapeRendering: 'geometricPrecision',
+      strokeDasharray: '6,6'
     }}></path>
     { props.data.map(renderCircles(props)) }
   </g>
