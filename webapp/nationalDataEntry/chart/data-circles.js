46b3bca84bac4b54f27fca8f71fe7531a5a8e460
Mino Togna <Cosimo.Togna@Fao.org>
2017-05-25 11:30:50 +0200

    updated chart stle

--- webapp/nationalDataEntry/chart/data-circles.js
+++ webapp/nationalDataEntry/chart/data-circles.js
@@ -13,7 +13,7 @@ const renderCircles = ({xScale, yScale}) => {
     }
 
     return (circleProps.cx && circleProps.cy) ? <circle {...circleProps}
-                                                        fill={d.type === 'odp' ? '#189aa7' : 'none' }
+                                                        fill={d.type === 'odp' ? '#189aa7' : '#FFF' }
                                                         stroke={d.type === 'odp' ? 'none' : '#333333' }
                                                         strokeWidth={d.type === 'odp' ? 0 : 1.5 }
     /> : null
@@ -32,16 +32,16 @@ const DataCircles = (props) => {
   return <g>
     <path d={renderLines({...props, data: R.filter(v => v.type === 'odp', props.data)})} style={{
       fill: 'none',
-      stroke: '#acacb3',
+      stroke: '#a1a1a8',
       strokeWidth: 1.5,
       shapeRendering: 'geometricPrecision'
     }}></path>
     <path d={renderLines({...props, data: R.filter(v => v.type === 'fra', props.data)})} style={{
       fill: 'none',
-      stroke: 'rgba(73,144,226,.6)',
-      strokeWidth: 3,
+      stroke: 'rgba(73,144,226,.35)',
+      strokeWidth: 2.5,
       shapeRendering: 'geometricPrecision',
-      strokeDasharray: '6,6'
+      strokeDasharray: '6,2'
     }}></path>
     { props.data.map(renderCircles(props)) }
   </g>
