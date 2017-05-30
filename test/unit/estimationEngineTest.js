339a99936c97506346912c5c103337023bfb4121
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-30 09:57:10 +0300

    Simplify test-data: make otherWoodenLand increase by 1000 each year

--- test/unit/estimationEngineTest.js
+++ test/unit/estimationEngineTest.js
@@ -16,14 +16,14 @@ const fraYears = [
 const testOdpSet1 = [
   {
     forestArea: 40000,
-    otherWoodenLand: 80000,
+    otherWoodenLand: 2000,
     otherLand: 80000,
     type: 'odp',
     year: 1991,
   },
   {
     forestArea: 80000,
-    otherWoodenLand: 40000,
+    otherWoodenLand: 29000,
     otherLand: 80000,
     type: 'odp',
     year: 2018,
@@ -32,55 +32,55 @@ const testOdpSet1 = [
 const expectedEstimations1 = [
   {
     forestArea: 38518.519,
-    otherWoodenLand: 81481.481,
+    otherWoodenLand: 1000,
     otherLand: 80000,
     year: 1990,
   },
   {
     forestArea: 53333.333,
-    otherWoodenLand: 66666.667,
+    otherWoodenLand: 11000,
     otherLand: 80000,
     year: 2000,
   },
   {
     forestArea: 68148.148,
-    otherWoodenLand: 51851.852,
+    otherWoodenLand: 21000,
     otherLand: 80000,
     year: 2010,
   },
   {
     forestArea: 75555.556,
-    otherWoodenLand: 44444.444,
+    otherWoodenLand: 26000,
     otherLand: 80000,
     year: 2015,
   },
   {
     forestArea: 77037.037,
-    otherWoodenLand: 42962.963,
+    otherWoodenLand: 27000,
     otherLand: 80000,
     year: 2016,
   },
   {
     forestArea: 78518.519,
-    otherWoodenLand: 41481.482,
+    otherWoodenLand: 28000,
     otherLand: 80000,
     year: 2017,
   },
   {
     forestArea: 80000,
-    otherWoodenLand: 40000,
+    otherWoodenLand: 29000,
     otherLand: 80000,
     year: 2018,
   },
   {
     forestArea: 81481.481,
-    otherWoodenLand: 38518.518,
+    otherWoodenLand: 30000,
     otherLand: 80000,
     year: 2019,
   },
   {
     forestArea: 82962.962,
-    otherWoodenLand: 37037.036,
+    otherWoodenLand: 31000,
     otherLand: 80000,
     year: 2020,
   }]
