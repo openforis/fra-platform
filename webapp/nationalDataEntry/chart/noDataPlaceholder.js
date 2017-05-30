1ee5ff31f4437ed8a43e34d0c9d8fa3d3892ab29
Mino Togna <Cosimo.Togna@Fao.org>
2017-05-30 18:10:46 +0200

    added text when no data is available in the chart

--- webapp/nationalDataEntry/chart/noDataPlaceholder.js
+++ webapp/nationalDataEntry/chart/noDataPlaceholder.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
+const Text = ({text, width, y}) =>
+  <foreignObject width={width} y={y} style={{textAlign: 'center'}}>
+    <text style={{fontSize: '14px', color: '#666666'}} x="0" y="0">{text}</text>
+  </foreignObject>
+
 const NoDataPlaceholder = ({data, width, padding}) =>
   data.forestArea.length <= 0 && data.otherWoodedLand.length <= 0
     ? <g className="noDataPlaceholder">
     <image href="/img/tucan.svg"
            width="62" height="87"
            x={(width / 2) + padding - 40} y="16.5"></image>
+    <Text text="To get started, add new national data points and use" width={width} y="130"/>
+    <Text text="them to generate FRA values automatically." width={width} y="150"/>
   </g>
     : null
 
