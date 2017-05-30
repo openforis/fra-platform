9ecd7f66e1b8154067fb89874d3faf3fa8e244c8
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-05-30 19:55:12 +0300

    Update login styles

--- webapp/login/login.js
+++ webapp/login/login.js
@@ -8,21 +8,22 @@ const Login = () =>
       <img src="img/fao_logo.svg" className="fao-logo"/>
       <div className="login__box">
         <img src="img/tucan.svg" className="tucan"/>
-        <div className="input">
         <h2 className="login__header headline">Login to FRA Platform</h2>
+        <div className="input-group">
           <label htmlFor="un">Email</label>
           <input id="un" type="text"/>
         </div>
-        <div className="input">
+        <div className="input-group">
           <label htmlFor="un">Password</label>
           <input id="un" type="password"/>
         </div>
-        <div className="checkbox">
+        <div className="checkbox-group">
           <input id="un" type="checkbox" disabled="disabled"/>
           <label htmlFor="un" type="text">Remember me on this browser</label>
         </div>
         <Link to="/country/ITA" className="btn btn-primary login__btn">Sign in</Link>
       </div>
+      <p className="legal">&copy; FAO, 2017</p>
     </div>
   </div>
 
