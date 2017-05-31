4dbf8122202dd512ce8388c6074a7c6da0b45506
Perttu Auramo <perttu.auramo@reaktor.com>
2017-05-31 10:48:59 +0300

    Fix font-size issue (not a recognized attributre)

--- webapp/nationalDataEntry/chart/noDataPlaceholder.js
+++ webapp/nationalDataEntry/chart/noDataPlaceholder.js
@@ -4,8 +4,8 @@ const NoDataPlaceholder = ({data, width, padding}) =>
   data.length <= 0 ? <g className="noDataPlaceholder">
     <image href="/img/tucan.svg"
            width="62" height="87"
-           x={(width / 2) + padding - 40} y="15.5"></image>
-    <text x="100" y="154" width="360" font-size="20" fill="#999">To get started, add new national data points and use them to generate FRA values automatically.</text>
+           x={(width / 2) + padding - 40} y="15.5"/>
+    <text x="100" y="154" width="360" fontSize="14" fill="#999">To get started, add new national data points and use them to generate FRA values automatically.</text>
   </g>
     : null
 
