ce58437017ddb5e878b9e33228f13d1ac44995ed
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-05 15:58:14 +0300

    Remove cruft

--- server/user/userRepository.js
+++ server/user/userRepository.js
@@ -1,11 +1,5 @@
 const db = require('../db/db')
 
-module.exports.getUserInfo = (email) => {
-  return db.query('SELECT id, name FROM fra_user WHERE email = $1', [email])
-    .then(res => {
-      if (res.rows.length > 0)
-        return res.rows[0]
-      else
-        return null
-    })
-}
+module.exports.getUserInfo = (email) =>
+  db.query('SELECT id, name FROM fra_user WHERE email = $1', [email])
+    .then(res => res.rows.length > 0 ?  res.rows[0] : null)
