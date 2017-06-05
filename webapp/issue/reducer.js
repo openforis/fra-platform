d7083f6cb26c49ea589e851faf3e6bb40248485c
Joni Rajanen <joni.rajanen@reaktor.com>
2017-06-05 13:00:04 +0300

    review fix: removing seagulls

--- webapp/issue/reducer.js
+++ webapp/issue/reducer.js
@@ -6,15 +6,10 @@ import {
 } from './actions'
 
 const actionHandlers = {
-  [issuePostCommentCompleted]: (state, action) => {
-    return ({...state, 'status': action.status})
-  },
-  [issueRetrieveCommentsStarted]: (state, action) => {
-    return ({...state, 'status': action.status})
-  },
-  [issueRetrieveCommentsCompleted]: (state, action) => {
-    return ({...state, 'comments': action.comments})
-  }
+  [issuePostCommentCompleted]: (state, action) => ({...state, 'status': action.status}),
+  [issueRetrieveCommentsStarted]: (state, action) => ({...state, 'status': action.status}),
+  [issueRetrieveCommentsCompleted]: (state, action) => ({...state, 'comments': action.comments})
+}
 }
 
 export default (state = {}, action) => applyReducerFunction(actionHandlers, state, action)
