c7f4dc0ac8208de69a2a26c8b266106a230bc86d
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-08 14:11:50 +0300

    Add missing default template fields

--- server/eof/forestAreaTableResponse.js
+++ server/eof/forestAreaTableResponse.js
@@ -11,55 +11,73 @@ module.exports = {
       name: '1990',
       year: 1990,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2000': {
       name: '2000',
       year: 2000,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2010': {
       name: '2010',
       year: 2010,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2015': {
       name: '2015',
       year: 2015,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2016': {
       name: '2016',
       year: 2016,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2017': {
       name: '2017',
       year: 2017,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2018': {
       name: '2018',
       year: 2018,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2019': {
       name: '2019',
       year: 2019,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     },
     'fra_2020': {
       name: '2020',
       year: 2020,
       type: 'fra',
-      forestArea: null
+      forestArea: null,
+      otherWoodedLand: null,
+      otherLand: null
     }
   }
 }
