2cc5bd4267b47f1bb388fbd5c5266be26130bee0
Mino Togna <Cosimo.Togna@Fao.org>
2017-06-08 10:50:06 +0200

    added estimated flags to nationalDataEntry action/reducer

--- webapp/nationalDataEntry/reducers.js
+++ webapp/nationalDataEntry/reducers.js
@@ -5,8 +5,7 @@ import { applyReducerFunction } from '../utils/reduxUtils'
 
 const updateValue = (state, action) => {
   const idx = R.findIndex(R.propEq('name', action.name), state.fra)
-  const updatedValue = {...action.value, estimated: false}
-  return {...state, fra: R.update(idx, updatedValue, state.fra)}
+  return {...state, fra: R.update(idx, {...action.value}, state.fra)}
 }
 
 const actionHandlers = {
