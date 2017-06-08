2cc7baa8b78f9f16bac804bba6872980a8c9c729
Joni Rajanen <joni.rajanen@reaktor.com>
2017-06-08 13:11:33 +0300

    fix handling of results in comment reading

--- webapp/issue/actions.js
+++ webapp/issue/actions.js
@@ -6,29 +6,29 @@ export const issueRetrieveCommentsCompleted = 'issue/comment/retrieve/completed'
 export const issueOpenCommentThread = 'issue/comment/thread/open'
 export const issueCloseCommentThread = 'issue/comment/thread/close'
 
+const readingCompletion = (section, target, dispatch) => resp => {
+  const targetkey = target.join('_')
+  dispatch({
+    type: issueRetrieveCommentsCompleted,
+    section: section,
+    target: targetkey,
+    issue: resp.data
+  })
+}
+
 export const postComment = (countryIso, section, target, userId, issueId, msg) => dispatch => {
   axios.post(`api/country/issue/${countryIso}/${section}?target=${target}`, {msg}).then(() => {
       dispatch({target: target, type: issuePostCommentCompleted, status: 'completed'})
-      axios.get(`api/country/issue/${countryIso}/${section}?target=${target}`).then(resp =>
-        dispatch({target: target, type: issueRetrieveCommentsCompleted, comments: resp.data})
-      )
+      axios.get(`api/country/issue/${countryIso}/${section}?target=${target}`)
+        .then(readingCompletion(section, target, dispatch))
     }
   )
 }
 
 export const retrieveComments = (countryIso, section, target) => dispatch => {
   dispatch({section: section, type: issueRetrieveCommentsStarted, status: 'started'})
-  axios.get(`api/country/issue/${countryIso}/${section}?target=${target}`).then(resp => {
-      const targetkey = target.join('_')
-      console.log('target', targetkey)
-      dispatch({
-        type: issueRetrieveCommentsCompleted,
-        section: section,
-        target: targetkey,
-        issue: resp.data
-      })
-    }
-  )
+  axios.get(`api/country/issue/${countryIso}/${section}?target=${target}`)
+    .then(readingCompletion(section, target, dispatch))
 }
 
 export const openCommentThread = target => ({type: issueOpenCommentThread, target})
