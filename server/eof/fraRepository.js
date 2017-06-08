477ddef02552083fbd7e42482b7e9256c54e27d9
Mino Togna <Cosimo.Togna@Fao.org>
2017-06-08 10:59:11 +0200

    added estimated flags to persistFraValues fraRepository

--- server/eof/fraRepository.js
+++ server/eof/fraRepository.js
@@ -6,40 +6,46 @@ const emptyFraForestArea = (countryIso, year) =>
   db.query('SELECT id FROM eof_fra_values WHERE country_iso = $1 and year = $2', [countryIso, year])
     .then(result => result.rows.length === 0)
 
-module.exports.persistFraValues = (countryIso, year, fraValues, estimated = false) =>
+module.exports.persistFraValues = (countryIso, year, fraValues) =>
   emptyFraForestArea(countryIso, year).then(isEmpty =>
     isEmpty
-      ? insertFraForestArea(countryIso, year, fraValues, estimated)
-      : updateFraForestArea(countryIso, year, fraValues, estimated))
+      ? insertFraForestArea(countryIso, year, fraValues)
+      : updateFraForestArea(countryIso, year, fraValues))
 
-const insertFraForestArea = (countryIso, year, fraValues, estimated) =>
+const insertFraForestArea = (countryIso, year, fraValues) =>
   db.query(`INSERT INTO 
              eof_fra_values 
-             (country_iso, year, forest_area, other_wooded_land, other_land, estimated) 
+             (country_iso, year, forest_area, other_wooded_land, other_land, forest_area_estimated, other_wooded_land_estimated, other_land_estimated) 
              VALUES 
-             ($1, $2, $3, $4, $5, $6)`,
+             ($1, $2, $3, $4, $5, $6, $7, $8)`,
     [countryIso,
-     year,
-     fraValues.forestArea,
-     fraValues.otherWoodedLand,
-     fraValues.otherLand,
-     estimated])
+      year,
+      fraValues.forestArea,
+      fraValues.otherWoodedLand,
+      fraValues.otherLand,
+      fraValues.forestAreaEstimated,
+      fraValues.otherWoodedLandEstimated,
+      fraValues.otherLandEstimated])
 
-const updateFraForestArea = (countryIso, year, fraValues, estimated) =>
+const updateFraForestArea = (countryIso, year, fraValues) =>
   db.query(`UPDATE 
             eof_fra_values 
             SET 
              forest_area = $3,
              other_wooded_land = $4,
              other_land = $5,
-             estimated = $6 
+             forest_area_estimated = $6, 
+             other_wooded_land_estimated = $7, 
+             other_land_estimated = $8
             WHERE country_iso = $1 AND year = $2`,
     [countryIso,
-     year,
-     fraValues.forestArea,
-     fraValues.otherWoodedLand,
-     fraValues.otherLand,
-     estimated])
+      year,
+      fraValues.forestArea,
+      fraValues.otherWoodedLand,
+      fraValues.otherLand,
+      fraValues.forestAreaEstimated,
+      fraValues.otherWoodedLandEstimated,
+      fraValues.otherLandEstimated])
 
 const toNumberOrNull = (numericFromDb) => numericFromDb === null
   ? null
