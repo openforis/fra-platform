cf160dad8fffa71fead9fdea2c8bc3331a3ff631
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-09 14:22:50 +0300

    Add user to bottom bar and fix styles a bit

--- webapp/footer/footer.js
+++ webapp/footer/footer.js
@@ -1,12 +1,15 @@
 import './style.less'
+import * as R from 'ramda'
 
 import React from 'react'
 import { connect } from 'react-redux'
 
-const Footer = ({status}) => <div className="footer__container">
-  <span className="footer__autosave-status">{status}</span>
+const Footer = ({status, userInfo}) => <div className="footer__container">
+  <div/>
+  <div className="footer__item footer__autosave-status">{status}</div>
+  <div className="footer__item">{userInfo ? userInfo.name : ''}</div>
 </div>
 
-const mapStateToProps = state => state.autoSave
+const mapStateToProps = state => R.merge(state.autoSave, state.user)
 
 export default connect(mapStateToProps)(Footer)
