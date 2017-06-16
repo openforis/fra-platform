14d9cf19622ad91c7b2083af7ca20b27143c8164
Joni Rajanen <joni.rajanen@reaktor.com>
2017-06-16 13:12:13 +0300

    Styling for odp listing

--- webapp/originalDataPoint/originalDataListView.js
+++ webapp/originalDataPoint/originalDataListView.js
@@ -7,21 +7,22 @@ import { fetchOdps } from './actions'
 import { Link } from './../link'
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 
-const ODPListing = ({countryIso, odps = []}) => <div>
-  <table>
+const ODPListing = ({countryIso, odps = []}) => <div className="odp-list__container">
+  <h2>National data</h2>
+  <table className="odp-list__list-table">
     <thead>
-    <tr>
+    <tr className='odp-list__list-row'>
       <th>Year</th>
       <th>Methods</th>
       <th></th>
     </tr>
     </thead>
     <tbody>
-    { odps.map(odp => <tr>
-      <td>{odp.year}</td>
+    { odps.map(odp => <tr className='odp-list__list-row' key={odp.odpId}>
+      <td>{odp.year == 0 ? '-' : odp.year}</td>
       <td>-</td>
       <td><Link to={`/country/${countryIso}/odp/${odp.odpId}`}>Edit</Link></td>
-    </tr> )}
+    </tr>)}
     </tbody>
   </table>
   <Link className="btn btn-primary" to={`/country/${countryIso}/odp`}>
@@ -47,7 +48,6 @@ class DataFetchingComponent extends React.Component {
   }
 
   render () {
-    console.log('list props', this.props)
     return <LoggedInPageTemplate>
       <ODPListing countryIso={this.props.match.params.countryIso} {...this.props} />
     </LoggedInPageTemplate>
@@ -55,8 +55,7 @@ class DataFetchingComponent extends React.Component {
 }
 
 const mapStateToProps = state => {
-  console.log('state', state)
-  return R.merge({}, state.originalDataPoint)
+  return state.originalDataPoint
 }
 
 export default connect(mapStateToProps, {fetchOdps})(DataFetchingComponent)
