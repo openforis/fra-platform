dac84d56af4634f99c69040887cb4e42df27db80
Perttu Auramo <perttu.auramo@reaktor.fi>
2017-06-18 12:19:04 +0300

    Some initial sketching of generic table generation with spec

--- webapp/reusableUiComponents/genericTable.js
+++ webapp/reusableUiComponents/genericTable.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import R from 'ramda'
 
 import './tableStyles.less'
+import { ThousandSeparatedIntegerInput } from '../reusableUiComponents/thousandSeparatedIntegerInput'
 
-export default () => <table className="fra-table">
+
+const exampleTable = () => <table className="fra-table">
   <thead>
-    <tr><td className="fra-table__header-cell">heading</td></tr>
+  <tr><td className="fra-table__header-cell">heading</td></tr>
   </thead>
   <tbody>
   <tr className="">
@@ -14,3 +17,45 @@ export default () => <table className="fra-table">
   </tr>
   </tbody>
 </table>
+
+const mapIndexed = R.addIndex(R.map)
+
+const integerInput = (rowIdx, colIdx) => {
+  return <td key={`${rowIdx}-${colIdx}`} className="fra-table__cell-input">
+    <ThousandSeparatedIntegerInput integerValue={ null }
+                                   onChange={ console.log('integer updated', rowIdx, colIdx) }
+                                   onPaste={ console.log('pasted', rowIdx, colIdx) }/>
+
+  </td>
+}
+
+const cell = (rowIdx, colIdx, cellSpec) => {
+  if (cellSpec.type === 'integerInput') {
+    return integerInput(rowIdx, colIdx)
+  } else if (cellSpec.type === 'readOnly') {
+    return cellSpec.jsx
+  } else {
+    throw `Unknown cell type ${cellSpec.type}`
+  }
+}
+
+const tableRows = (tableSpec) => {
+  return mapIndexed(
+    (rowSpec, rowIdx) =>
+      <tr key={rowIdx}>
+        {mapIndexed((cellSpec, colIdx) => cell(rowIdx, colIdx, cellSpec), rowSpec)}
+      </tr>
+    ,tableSpec.rows)
+}
+
+const tableBody = (tableSpec) =>
+  <tbody>
+  {tableRows(tableSpec)}
+  </tbody>
+
+const createTable = (tableSpec) => <table>
+    {tableSpec.header}
+   {tableBody(tableSpec)}
+  </table>
+
+export default ({tableSpec}) => createTable(tableSpec)//exampleTable()
--- webapp/specificForestCategories/specificForestCategoriesView.js
+++ webapp/specificForestCategories/specificForestCategoriesView.js
@@ -5,17 +5,40 @@ import { connect } from 'react-redux'
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import GenericTable from '../reusableUiComponents/genericTable'
 
-const mapStateToProps = state => state
+
+const tableSpec = {
+  name: 'specificForestCategories', // used to uniquely identify table
+  header:   <thead>
+              <tr>
+                <td className="fra-table__header-cell">FRA 2020 Categories</td>
+                <td className="fra-table__header-cell">1990</td>
+                <td className="fra-table__header-cell">2000</td>
+              </tr>
+            </thead>,
+  rows: [
+    [{type: 'readOnly', jsx: <td className="fra-table__header-cell">Bamboo</td>},
+      {type: 'integerInput'},
+      {type: 'integerInput'}
+    ],
+    [
+      {type: 'readOnly', jsx: <td className="fra-table__header-cell">Mangroves</td>},
+      {type: 'integerInput'},
+      {type: 'integerInput'}
+    ]
+  ]
+}
 
 class SpecificForestCategoriesView extends React.Component {
   render() {
     return <LoggedInPageTemplate>
       <div className="sfc__container">
         <h2 className="headline">Specific forest categories</h2>
-        <GenericTable/>
+        <GenericTable tableSpec={tableSpec}/>
       </div>
     </LoggedInPageTemplate>
   }
 }
+
+const mapStateToProps = state => state
 
 export default connect(mapStateToProps, {})(SpecificForestCategoriesView)
