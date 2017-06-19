84e1050d24cd8723b8eee7a96588974caa8db02b
Joni Rajanen <joni.rajanen@reaktor.com>
2017-06-19 10:08:06 +0300

    Refactored odp api

--- server/odp/api.js
+++ server/odp/api.js
@@ -1,15 +1,42 @@
 const R = require('ramda')
 
+const db = require('../db/db')
 const odpRepository = require('../eof/odpRepository')
 
 module.exports.init = app => {
 
-  app.get('/api/odp/:countryIso', (req, res) => {
-    odpRepository.readOriginalDataPoints(req.params.countryIso, true)
-      .then(resp => res.json(R.values(resp)))
-      .catch(err => {
-        console.error(err)
-        res.status(500).json({error: 'Could not retrieve data'})
-      })
+  app.get('/api/odp', (req, res) => {
+    if(R.not(R.isNil(req.query.odpId))) {
+      odpRepository.getOdp(req.query.odpId)
+        .then(resp => res.json(resp))
+        .catch(err => sendErr(res, err))
+    }
+    if(R.not(R.isNil(req.query.countryIso))) {
+      odpRepository.readOriginalDataPoints(req.query.countryIso, true)
+        .then(resp => res.json(R.values(resp)))
+        .catch(err => {
+          console.error(err)
+          res.status(500).json({error: 'Could not retrieve data'})
+        })
+    }
   })
+
+  app.delete('/api/odp', (req, res) => {
+    db.transaction(odpRepository.deleteOdp, [req.query.odpId])
+      .then(() => res.json({}))
+      .catch(err => sendErr(res, err))
+  })
+
+  app.post('/api/odp/draft', (req, res) => {
+    const countryIso = req.query.countryIso
+    db.transaction(odpRepository.saveDraft, [countryIso, req.body])
+      .then(result => res.json(result))
+      .catch(err => sendErr(res, err))
+  })
+
+  app.post('/api/odp/markAsActual', (req, res) =>
+    db.transaction(odpRepository.markAsActual, [req.query.opdId])
+      .then(() => res.json({}))
+      .catch(err => sendErr(res, err))
+  )
 }
