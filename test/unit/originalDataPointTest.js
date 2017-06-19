d9de4e5c771f65e305e3c479eb5eab54d0d5bf49
Joni Rajanen <joni.rajanen@reaktor.com>
2017-06-19 14:14:50 +0300

    NDP: Round calculated total forest to nearest integer

--- test/unit/originalDataPointTest.js
+++ test/unit/originalDataPointTest.js
@@ -11,4 +11,24 @@ describe('originalDataPoint', () => {
     }
     assert.equal(200.00, originalDataPoint.totalForest(odpWithNationalClasses, 'forestPercent'))
   })
+
+  it('rounds decimals to nearest integer', () => {
+    const odpWithNationalClasses = {
+      nationalClasses: [{area: 200, forestPercent: 50},
+        {area: 1002, forestPercent: 10},
+        {area: null, forestPercent: 5},
+        {area: 400, forestPercent: null}]
+    }
+    assert.equal(200, originalDataPoint.totalForest(odpWithNationalClasses, 'forestPercent'))
+
+    const odpWithNationalClasses2 = {
+      nationalClasses: [{area: 200, forestPercent: 50},
+        {area: 1008, forestPercent: 10},
+        {area: null, forestPercent: 5},
+        {area: 400, forestPercent: null}]
+    }
+    assert.equal(201, originalDataPoint.totalForest(odpWithNationalClasses2, 'forestPercent'))
+
+    assert.equal(-1, String(originalDataPoint.totalForest(odpWithNationalClasses2, 'forestPercent')).indexOf('.'))
+  })
 })
