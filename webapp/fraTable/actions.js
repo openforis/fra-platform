8258248274886b5ad86f05ead84e524ab0ad2c92
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-20 11:10:06 +0300

    Handle the new-state-creation in actions

--- webapp/fraTable/actions.js
+++ webapp/fraTable/actions.js
@@ -1,4 +1,19 @@
+import * as table from './table'
+import assert from 'assert'
+
 export const tableValueChangedAction = 'fraTable/tableValueChanged'
 
-export const tableValueChanged = (tableSpec, rowIdx, colIdx, newValue) =>
-  ({type: tableValueChangedAction, tableSpec, rowIdx, colIdx, newValue})
+const createNewTableState = (tableSpec, rowIdx, colIdx, newValue, getState) => {
+  const fraTableState = getState().fraTable
+  assert(tableSpec.name, 'tableSpec is missing name')
+  const tableValues = fraTableState[tableSpec.name] || table.createTableData(tableSpec)
+  return table.update(tableValues, rowIdx, colIdx, !isNaN(newValue) ? Number(newValue) : newValue) // TODO use tableSpec for this
+}
+
+const saveChanges = () => {}
+
+export const tableValueChanged = (tableSpec, rowIdx, colIdx, newValue) => (dispatch, getState) => {
+  const newTableState = createNewTableState(tableSpec, rowIdx, colIdx, newValue, getState)
+  dispatch({type: tableValueChangedAction, tableSpec, newTableState})
+  dispatch(saveChanges)
+}
--- webapp/fraTable/reducer.js
+++ webapp/fraTable/reducer.js
@@ -1,15 +1,9 @@
 import * as actions from './actions'
-import assert from 'assert'
 import { applyReducerFunction } from '../utils/reduxUtils'
-import * as table from './table'
 
 const actionHandlers = {
   [actions.tableValueChangedAction]: (state, action) => {
-    const tableSpec = action.tableSpec
-    assert(tableSpec.name, 'tableSpec is missing name')
-    const tableValues = state[tableSpec.name] || table.createTableData(tableSpec)
-    const updatedValues = table.update(tableValues, action.rowIdx, action.colIdx, Number(action.newValue))
-    return {...state, [tableSpec.name]: updatedValues}
+    return {...state, [action.tableSpec.name]: action.newTableState}
   }
 }
 
