7363778ad56e19332c31301121ebcbabb3c530c2
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-21 13:42:59 +0300

    Implement mapping

---
+++ server/fraTable/mappings/specificForestCategories.js
@@ -0,0 +1,12 @@
+const mapping = {
+  rows: {
+    indexOffset: 0,
+    names: ['bamboo', 'mangroves', 'rubber_plantations']
+  },
+  columns: {
+    indexOffset: 1,
+    names: ['1990', '2000']
+  }
+}
+
+export default mapping
---
+++ server/fraTable/tableMappings.js
@@ -0,0 +1,25 @@
+import R from 'ramda'
+
+import specificForestCategories from './mappings/specificForestCategories'
+
+const mappings = {specificForestCategories}
+
+const getIndex = (name, names, indexOffset) => {
+  const idx = R.findIndex((x) => x === name, names)
+  return idx === -1 ? -1 : idx +  indexOffset
+}
+
+const getName = (idx, names, indexOffset) => names[idx - indexOffset]
+
+export const Mapping = (mapping) => ({
+  getRowName: (idx) => getName(idx, mapping.rows.names, mapping.rows.indexOffset) ,//mapping.rows.names[idx - mapping.rows.indexOffset],
+  getRowIndex: (name) => getIndex(name, mapping.rows.names, mapping.rows.indexOffset),
+  getColumnName: (idx) => getName(idx, mapping.columns.names, mapping.columns.indexOffset),//mapping.columns.names[idx - mapping.columns.indexOffset],
+  getColumnIndex: (name) => getIndex(name, mapping.columns.names, mapping.columns.indexOffset)
+})
+
+export const getMapping = (tableSpecName) => {
+  const mapping = mappings[tableSpecName]
+  if (!mapping) throw new Error(`Could not find mapping for tableSpecName ${tableSpecName}`)
+  return mapping
+}
