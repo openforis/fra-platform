293cf5305f430b6e78d302ef048b4d363ccb6608
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-27 16:05:04 +0300

    Add columns and add the "final" table for specificForestCategories

--- server/fraTable/mappings/specificForestCategories.js
+++ server/fraTable/mappings/specificForestCategories.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   columns: {
     indexOffset: 1,
-    names: ['1990', '2000']
+    names: ['1990', '2000', '2010', '2015', '2020']
   }
 }
--- webapp/specificForestCategories/specificForestCategoriesView.js
+++ webapp/specificForestCategories/specificForestCategoriesView.js
@@ -10,26 +10,35 @@ const tableSpec = {
   name: 'specificForestCategories', // used to uniquely identify table
   header:   <thead>
               <tr>
-                <td rowSpan="2" className="fra-table__header-cell">FRA 2020 Categories</td>
-                <td colSpan="2"className="fra-table__header-cell">Area (1000 ha)</td>
-              </tr>
-              <tr>
+                <td className="fra-table__header-cell"/>
                 <td className="fra-table__header-cell">1990</td>
                 <td className="fra-table__header-cell">2000</td>
+                <td className="fra-table__header-cell">2010</td>
+                <td className="fra-table__header-cell">2015</td>
+                <td className="fra-table__header-cell">2020</td>
               </tr>
             </thead>,
   rows: [
     [{type: 'readOnly', jsx: <td key="bamboo" className="fra-table__header-cell">Bamboo</td>},
+      {type: 'integerInput'},
+      {type: 'integerInput'},
+      {type: 'integerInput'},
       {type: 'integerInput'},
       {type: 'integerInput'}
     ],
     [
       {type: 'readOnly', jsx: <td key="mangroves" className="fra-table__header-cell">Mangroves</td>},
       {type: 'integerInput'},
+      {type: 'integerInput'},
+      {type: 'integerInput'},
+      {type: 'integerInput'},
       {type: 'integerInput'}
     ],
     [
       {type: 'readOnly', jsx: <td key="mangroves" className="fra-table__header-cell">Rubber plantations</td>},
+      {type: 'integerInput'},
+      {type: 'integerInput'},
+      {type: 'integerInput'},
       {type: 'integerInput'},
       {type: 'integerInput'}
     ]
