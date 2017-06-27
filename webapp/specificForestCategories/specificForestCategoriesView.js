cc7d01ca26416bcda62086929dc180b9c912da45
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-27 16:21:03 +0300

    Extract tableSpec

--- webapp/specificForestCategories/specificForestCategoriesView.js
+++ webapp/specificForestCategories/specificForestCategoriesView.js
@@ -4,46 +4,7 @@ import { connect } from 'react-redux'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import GenericTable from '../fraTable/genericTable'
-
-
-const tableSpec = {
-  name: 'specificForestCategories', // used to uniquely identify table
-  header:   <thead>
-              <tr>
-                <td className="fra-table__header-cell"/>
-                <td className="fra-table__header-cell">1990</td>
-                <td className="fra-table__header-cell">2000</td>
-                <td className="fra-table__header-cell">2010</td>
-                <td className="fra-table__header-cell">2015</td>
-                <td className="fra-table__header-cell">2020</td>
-              </tr>
-            </thead>,
-  rows: [
-    [{type: 'readOnly', jsx: <td key="bamboo" className="fra-table__header-cell">Bamboo</td>},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'}
-    ],
-    [
-      {type: 'readOnly', jsx: <td key="mangroves" className="fra-table__header-cell">Mangroves</td>},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'}
-    ],
-    [
-      {type: 'readOnly', jsx: <td key="mangroves" className="fra-table__header-cell">Rubber plantations</td>},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'},
-      {type: 'integerInput'}
-    ]
-  ]
-}
+import tableSpec from './tableSpec'
 
 class SpecificForestCategoriesView extends React.Component {
   render() {
