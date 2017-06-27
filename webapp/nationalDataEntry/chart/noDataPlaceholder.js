c92e1e771d9564410b0528391213cb92271d7d8a
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-06-27 13:10:06 +0300

    Unify empty/placeholder styles

--- webapp/nationalDataEntry/chart/noDataPlaceholder.js
+++ webapp/nationalDataEntry/chart/noDataPlaceholder.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Text = ({text, width, y}) =>
   <foreignObject width={width} y={y} style={{textAlign: 'center'}}>
-    <text style={{fontSize: '14px', color: '#666666'}} x="0" y="0">{text}</text>
+    <text style={{fontSize: '14px', color: '#999999'}} x="0" y="0">{text}</text>
   </foreignObject>
 
 const NoDataPlaceholder = ({data, wrapperWidth}) =>
