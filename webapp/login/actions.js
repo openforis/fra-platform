18129ccc0537a08cb26e758fdce8de47cb568734
Mino Togna <Cosimo.Togna@Fao.org>
2017-06-28 12:58:52 +0200

    removed old login actions

--- webapp/login/actions.js
+++ webapp/login/actions.js
@@ -1,23 +1,7 @@
 import axios from 'axios'
 import { applicationError } from '../applicationError/actions'
 
-export const loginSuccess = 'login/success'
-export const loginFailure = 'login/failure'
 export const userInfo = 'login/userInfo'
-
-export const requestLogin = (email) => dispatch => {
-  axios.post(`/api/login/${email}`)
-    .then(resp => {
-      if (resp.data.loginStatus === 'ok') {
-        dispatch({type: loginSuccess})
-        window.location = `#/country/ITA`
-      } else {
-        dispatch({type: loginFailure})
-      }
-    }).catch((err) => {
-      dispatch(applicationError(err))
-    })
-}
 
 export const getLoggedinUserInfo = () => dispatch => {
   axios.get(`/api/loggedInUser/`)
