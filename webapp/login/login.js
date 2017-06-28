4867123610b80a5de2242825f6ce2393dfc75ea3
Mino Togna <Cosimo.Togna@Fao.org>
2017-06-28 12:52:21 +0200

    showing User not authorized message to login page

--- webapp/login/login.js
+++ webapp/login/login.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { requestLogin } from './actions'
 import './style.less'
+import { getUrlParameter } from '../utils/urlUtils'
 
-const LoginView = ({requestLogin, loginFailed}) =>
+const LoginView = ({requestLogin}) =>
   <div className="login__root">
     <div className="login__wrapper">
       <img src="img/fao_logo.svg" className="login__fao-logo"/>
       <div className="login__box">
         <img src="img/tucan.svg" className="login__tucan"/>
         <h2 className="login__header headline">Login to FRA Platform</h2>
-        {loginFailed ? <div className="login__fail-info">Login failed</div> : ''}
+        {getUrlParameter('u') ? <div className="login__fail-info">User not authorized</div> : ''}
         <div className="login__input-group">
           <label htmlFor="email">Email</label>
           <input id="email" type="text" defaultValue="jan.egeland@mailinator.com"/>
         </div>
         <button className="btn btn-primary login__btn"
-                onClick={() => requestLogin(document.getElementById("email").value)}>
+                onClick={() => requestLogin(document.getElementById('email').value)}>
           Sign in
         </button>
       </div>
