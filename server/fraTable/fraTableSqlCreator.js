801a0993e5c3fa000233b4e06e65ce9b149dfe1b
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-28 08:10:11 +0300

    Remove mapping.mapping madness

--- server/fraTable/fraTableSqlCreator.js
+++ server/fraTable/fraTableSqlCreator.js
@@ -8,10 +8,10 @@ const fixedFraTableColumnDataTypes = ['VARCHAR', 'VARCHAR']
 const createInsert = (tableName, columnNamesStr, valuePlaceholdersStr, row) =>
   [`INSERT INTO ${tableName} (${columnNamesStr}) VALUES (${valuePlaceholdersStr})`, row]
 
-const createColumnNames = (mapping) => R.map((columnName) => `"${columnName}"`,[...fixedFraTableColumns, ...mapping.mapping.columns.names])
+const createColumnNames = (mapping) => R.map((columnName) => `"${columnName}"`,[...fixedFraTableColumns, ...mapping.columns.names])
 
 const createRowData = (countryIso, mapping, rowIndex, rawRow) => {
-  const trimmed = R.drop(mapping.mapping.columns.indexOffset, rawRow)
+  const trimmed = R.drop(mapping.columns.indexOffset, rawRow)
   //These values are there for all fra tables
   const fixedValues = [countryIso, mapping.getRowName(rowIndex)]
   return [...fixedValues, ...trimmed]
@@ -21,23 +21,23 @@ const getMapping = (tableSpecName) => tableMappings.getMapping(tableSpecName)
 
 const createSelect = (countryIso, tableSpecName) => {
   const mapping = getMapping(tableSpecName)
-  return [`SELECT ${createColumnNames(mapping)} FROM ${mapping.mapping.tableName} WHERE country_iso = $1`, [countryIso]]
+  return [`SELECT ${createColumnNames(mapping)} FROM ${mapping.tableName} WHERE country_iso = $1`, [countryIso]]
 }
 
 const createDelete = (countryIso, tableSpecName) => {
   const mapping = getMapping(tableSpecName)
-  return [`DELETE FROM ${mapping.mapping.tableName} WHERE country_iso = $1;`, [countryIso]]
+  return [`DELETE FROM ${mapping.tableName} WHERE country_iso = $1;`, [countryIso]]
 }
 
 const createInserts = (countryIso, tableSpecName, tableData) => {
   const mapping = getMapping(tableSpecName)
   assert(mapping, `Could not find mapping for ${tableSpecName}`)
-  const tableSpecificColumnCount = tableData[0].length - mapping.mapping.columns.indexOffset
+  const tableSpecificColumnCount = tableData[0].length - mapping.columns.indexOffset
   const columnNames = createColumnNames(mapping)
-  const tableName = mapping.mapping.tableName
+  const tableName = mapping.tableName
   const columnNamesStr = R.join(',', columnNames)
   const valuePlaceholdersStr = R.join(',', R.map((idx) => `$${idx+1}`, R.range(0, tableSpecificColumnCount + fixedFraTableColumns.length)))
-  const trimmedTableRows = R.drop(mapping.mapping.rows.indexOffset, tableData)
+  const trimmedTableRows = R.drop(mapping.rows.indexOffset, tableData)
   return R.addIndex(R.map)((row, rowIndex) => createInsert(
     tableName,
     columnNamesStr,
@@ -50,12 +50,12 @@ const createInserts = (countryIso, tableSpecName, tableData) => {
 const createTableDefinition = (tableSpecName, columnDataType) => {
   const mapping = getMapping(tableSpecName)
   const columnNames = createColumnNames(mapping)
-  const dynamicDataDataTypeArray = R.map(()=> columnDataType, mapping.mapping.columns.names)
+  const dynamicDataDataTypeArray = R.map(()=> columnDataType, mapping.columns.names)
   const dataTypes = [...fixedFraTableColumnDataTypes, ...dynamicDataDataTypeArray]
   assert(dataTypes.length === columnNames.length, 'Data types and column names arrays should be of the same length! Check your mapping')
   const columns = R.zip(columnNames, dataTypes)
   const columnsStr = R.join(', ', R.map(([name, dataType]) => `${name} ${dataType}`, columns))
-  return `CREATE TABLE ${mapping.mapping.tableName} (${columnsStr}, PRIMARY KEY (country_iso, row_name));`
+  return `CREATE TABLE ${mapping.tableName} (${columnsStr}, PRIMARY KEY (country_iso, row_name));`
 }
 
 module.exports.createInserts = createInserts
--- server/fraTable/tableMappings.js
+++ server/fraTable/tableMappings.js
@@ -6,28 +6,29 @@ const mappings = {specificForestCategories}
 
 const getIndex = (name, names, indexOffset) => {
   const idx = R.findIndex((x) => x === name, names)
-  return idx === -1 ? -1 : idx +  indexOffset
+  return idx === -1 ? -1 : idx + indexOffset
 }
 
 const getName = (idx, names, indexOffset) => names[idx - indexOffset]
 
-const Mapping = (mapping) => ({
-  mapping: mapping,
-  getRowName: (idx) => getName(idx, mapping.rows.names, mapping.rows.indexOffset),
-  getRowIndex: (name) => getIndex(name, mapping.rows.names, mapping.rows.indexOffset),
-  getFullRowCount: () => mapping.rows.names.length + mapping.rows.indexOffset,
-  getColumnName: (idx) => getName(idx, mapping.columns.names, mapping.columns.indexOffset),
-  getColumnIndex: (name) => getIndex(name, mapping.columns.names, mapping.columns.indexOffset),
-  getFullColumnCount: () => mapping.columns.names.length + mapping.columns.indexOffset
-})
+const Mapping = (mapping) =>
+  R.merge(mapping,
+    {
+      getRowName: (idx) => getName(idx, mapping.rows.names, mapping.rows.indexOffset),
+      getRowIndex: (name) => getIndex(name, mapping.rows.names, mapping.rows.indexOffset),
+      getFullRowCount: () => mapping.rows.names.length + mapping.rows.indexOffset,
+      getColumnName: (idx) => getName(idx, mapping.columns.names, mapping.columns.indexOffset),
+      getColumnIndex: (name) => getIndex(name, mapping.columns.names, mapping.columns.indexOffset),
+      getFullColumnCount: () => mapping.columns.names.length + mapping.columns.indexOffset
+    })
 
 const assertSanity = (mappingObj) => {
   const errMsg = 'Malformed FRA table mapping'
   assert(mappingObj.getFullRowCount() > 0, errMsg)
   assert(mappingObj.getFullColumnCount() > 0, errMsg)
-  assert(mappingObj.mapping, errMsg)
-  assert(mappingObj.mapping.rows.names.length > 0, errMsg)
-  assert(mappingObj.mapping.columns.names.length > 0, errMsg)
+  assert(mappingObj, errMsg)
+  assert(mappingObj.rows.names.length > 0, errMsg)
+  assert(mappingObj.columns.names.length > 0, errMsg)
 }
 
 const getMapping = (tableSpecName) => {
