dc44e2f6d3d422949f14e63c74a5fed8b8485bf3
Mino Togna <Cosimo.Togna@Fao.org>
2017-06-29 11:20:40 +0200

    code clean up

--- server/auth/authConfig.js
+++ server/auth/authConfig.js
@@ -1,23 +1,10 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-const express = require('express')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const session = require('express-session')
 
 module.exports.init = (app, verifyCallback) => {
 
-  // app.use(express.static('public'))
   app.use(cookieParser())
-  // app.use(bodyParser.urlencoded({extended: true}))
-  // app.use(bodyParser.json())
-  // app.use(session({
-  //   secret: 'loggedInUser',
-  //   name: 'loggedInUser',
-  //   resave: true,
-  //   saveUninitialized: true,
-  //   cookie: {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: false}
-  // }))
   app.use(passport.initialize())
   app.use(passport.session())
 
