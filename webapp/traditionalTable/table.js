9cca6f1d51f591b11b46d5620759965b15a25277
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-29 15:38:13 +0300

    Refactor

--- webapp/traditionalTable/table.js
+++ webapp/traditionalTable/table.js
@@ -6,10 +6,16 @@ export const createTableData = (tableSpec) =>
     (rowIdx) => new Array(tableSpec.rows[0].length),
     R.range(0, tableSpec.rows.length))
 
-export const fillTableDatafromValueSlice = (tableSpec, fullTableData, valueSliceData) => {
+const getSliceLenses = (tableSpec) => {
   const valueSlice = tableSpec.valueSlice || {} //Lenses work with undefined as default values nicely
-  const rowSliceLens = L.slice(valueSlice.rowStart, valueSlice.rowEnd)
-  const colSliceLens = L.slice(valueSlice.columnStart, valueSlice.columnEnd)
+  return [
+    L.slice(valueSlice.rowStart, valueSlice.rowEnd),
+    L.slice(valueSlice.columnStart, valueSlice.columnEnd)
+  ]
+}
+
+export const fillTableDatafromValueSlice = (tableSpec, fullTableData, valueSliceData) => {
+  const [rowSliceLens, colSliceLens] = getSliceLenses(tableSpec)
   const handleRow = (index, row, data) => {
     const rowLens = [rowSliceLens, L.index(index)]
     return L.set(rowLens, L.set(colSliceLens, row, L.get(rowLens, data)), data)
@@ -26,8 +32,6 @@ export const update = (tableValues, rowIdx, colIdx, newValue) =>
   R.update(rowIdx, R.update(colIdx, newValue, tableValues[rowIdx]), tableValues)
 
 export const getValueSliceFromTableValues = (tableSpec, tableValues) => {
-  const valueSlice = tableSpec.valueSlice || {} //Lenses work with undefined as default values nicely
-  const rowSliceLens = L.slice(valueSlice.rowStart, valueSlice.rowEnd)
-  const colSliceLens = L.slice(valueSlice.columnStart, valueSlice.columnEnd)
+  const [rowSliceLens, colSliceLens] = getSliceLenses(tableSpec)
   return L.collect([rowSliceLens, L.elems, colSliceLens], tableValues)
 }
