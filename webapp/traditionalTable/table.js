1dffad41e2a9abd976cf78da59da26cbe361b130
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-29 15:32:51 +0300

    Refactor

--- webapp/traditionalTable/table.js
+++ webapp/traditionalTable/table.js
@@ -10,8 +10,15 @@ export const fillTableDatafromValueSlice = (tableSpec, fullTableData, valueSlice
   const valueSlice = tableSpec.valueSlice || {} //Lenses work with undefined as default values nicely
   const rowSliceLens = L.slice(valueSlice.rowStart, valueSlice.rowEnd)
   const colSliceLens = L.slice(valueSlice.columnStart, valueSlice.columnEnd)
-  const handleRow = (index, row, data) => L.set([rowSliceLens, L.index(index)], L.set(colSliceLens, row, L.get([rowSliceLens, L.index(index)], data)), data)
-  const reduceResult = R.reduce((accu, row) => ({data: handleRow(accu.index, row, accu.data), index: accu.index + 1}), {index: 0, data: fullTableData}, valueSliceData)
+  const handleRow = (index, row, data) => {
+    const rowLens = [rowSliceLens, L.index(index)]
+    return L.set(rowLens, L.set(colSliceLens, row, L.get(rowLens, data)), data)
+  }
+  const reduceResult = R.reduce(
+    (accu, row) => ({data: handleRow(accu.index, row, accu.data), index: accu.index + 1}),
+    {index: 0, data: fullTableData},
+    valueSliceData
+  )
   return reduceResult.data
 }
 
