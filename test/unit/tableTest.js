6b4b982d697a487aea34a962d42749230956defd
Perttu Auramo <perttu.auramo@reaktor.com>
2017-06-29 15:28:54 +0300

    Merge subview back to main data with lenses

--- test/unit/tableTest.js
+++ test/unit/tableTest.js
@@ -2,25 +2,30 @@ const assert = require('chai').assert
 const R = require('ramda')
 const table = require('../../webapp/traditionalTable/table')
 
+//Not a realistic dataset in that these rhs etc. will actually be null...
 const dataToSlice = [['', 'h1', 'h2'], ['rh1', 1, 2], ['rh2', 3, 4], ['total', 'tbd', 'tbd']]
-//Not a full tableSpec
-const specForSlicing = {}
+const specWithSlice = {
+  valueSlice: {
+    rowStart: 1,
+    rowEnd: -1,
+    columnStart: 1,
+    columnEnd: undefined
+  }
+}
 
 describe('table', () => {
   it('Slices last row and first column away', () => {
     const expected = [[1, 2], [3, 4]]
-    const actual = table.getValueSliceFromTableValues({
-      valueSlice: {
-        rowStart: 1,
-        rowEnd: -1,
-        columnStart: 1,
-        columnEnd: undefined
-      }
-    }, dataToSlice)
+    const actual = table.getValueSliceFromTableValues(specWithSlice, dataToSlice)
     assert(R.equals(expected, actual), `Expected ${JSON.stringify(expected)} actual ${JSON.stringify(actual)}`)
   })
   it('Returns all data if there is no slicing', () => {
     const actual = table.getValueSliceFromTableValues({}, dataToSlice)
     assert(R.equals(dataToSlice, actual), `Expected ${JSON.stringify(dataToSlice)} actual ${JSON.stringify(actual)}`)
+  })
+  it('Creates the full, larger data matrix from a slice and tableSpec', () => {
+    const expected = [['', 'h1', 'h2'], ['rh1', 4, 5], ['rh2', 6, 7], ['total', 'tbd', 'tbd']]
+    const actual = table.fillTableDatafromValueSlice(specWithSlice, dataToSlice, [[4, 5], [6, 7]])
+    assert(R.equals(expected, actual), `Expected ${JSON.stringify(dataToSlice)} actual ${JSON.stringify(actual)}`)
   })
 })
--- webapp/traditionalTable/table.js
+++ webapp/traditionalTable/table.js
@@ -6,6 +6,15 @@ export const createTableData = (tableSpec) =>
     (rowIdx) => new Array(tableSpec.rows[0].length),
     R.range(0, tableSpec.rows.length))
 
+export const fillTableDatafromValueSlice = (tableSpec, fullTableData, valueSliceData) => {
+  const valueSlice = tableSpec.valueSlice || {} //Lenses work with undefined as default values nicely
+  const rowSliceLens = L.slice(valueSlice.rowStart, valueSlice.rowEnd)
+  const colSliceLens = L.slice(valueSlice.columnStart, valueSlice.columnEnd)
+  const handleRow = (index, row, data) => L.set([rowSliceLens, L.index(index)], L.set(colSliceLens, row, L.get([rowSliceLens, L.index(index)], data)), data)
+  const reduceResult = R.reduce((accu, row) => ({data: handleRow(accu.index, row, accu.data), index: accu.index + 1}), {index: 0, data: fullTableData}, valueSliceData)
+  return reduceResult.data
+}
+
 export const update = (tableValues, rowIdx, colIdx, newValue) =>
   R.update(rowIdx, R.update(colIdx, newValue, tableValues[rowIdx]), tableValues)
 
