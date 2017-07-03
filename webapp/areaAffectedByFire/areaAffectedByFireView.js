30d6660f428a592ce4f3b28bd8916a792819370c
Joni Rajanen <joni.rajanen@reaktor.com>
2017-07-03 13:58:56 +0300

    Styling for area affect by fire view

--- webapp/areaAffectedByFire/areaAffectedByFireView.js
+++ webapp/areaAffectedByFire/areaAffectedByFireView.js
@@ -1,3 +1,4 @@
+import './style.less'
 import React from 'react'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
@@ -5,12 +6,13 @@ import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 
 const areaAffectedByFire = ({match}) =>
-    <LoggedInPageTemplate>
-      <div className="fac__container">
-        <h2 className="headline fac__page-header">Area affected by fire</h2>
+  <LoggedInPageTemplate>
+    <div className="aabf__container">
+      <h2 className="headline fac__page-header">Area affected by fire</h2>
+      <div className="aabf__traditional-table-wrapper">
         <TraditionalTable tableSpec={tableSpec} countryIso={match.params.countryIso}/>
       </div>
-    </LoggedInPageTemplate>
-
+    </div>
+  </LoggedInPageTemplate>
 
 export default areaAffectedByFire
