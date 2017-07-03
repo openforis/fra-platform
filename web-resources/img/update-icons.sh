7ef32ccaa6aa1d74d97b93e96ac9fbf0f6679a0c
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-07-03 15:31:36 +0300

    Add new icons and update icon script

--- web-resources/img/update-icons.sh
+++ web-resources/img/update-icons.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sed -i '' -e 's/nc-//g' icon.svg
+sed -i '' -e 's/data-color="color-2" fill="currentColor"//g' icon.svg
 sed -i '' -e 's/nc-//g' demo.html
 sed -i '' -e 's/icon glyph/icon/g' demo.html
 sed -i '' -e 's/icon grid-16 glyph/icon/g' demo.html
 sed -i '' -e 's/class="icon"\&/className="icon"\&/g' demo.html
+sed -i '' -e 's/icon grid-24 glyph/icon-24/g' demo.html
+sed -i '' -e 's/class="icon-24"\&/className="icon-24"\&/g' demo.html
 sed -i '' -e 's/xlink:href/xlinkHref/g' demo.html
