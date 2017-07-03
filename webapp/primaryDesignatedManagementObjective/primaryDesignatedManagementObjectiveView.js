690e1d39076b1985ee6bed29f1b0d363275a48dd
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-03 14:38:08 +0300

    Remove unnecessary redux cruft

--- webapp/primaryDesignatedManagementObjective/primaryDesignatedManagementObjectiveView.js
+++ webapp/primaryDesignatedManagementObjective/primaryDesignatedManagementObjectiveView.js
@@ -1,22 +1,16 @@
 import './style.less'
 import React from 'react'
-import { connect } from 'react-redux'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 
-class PrimaryDesignatedManagementObjectiveView extends React.Component {
-  render() {
-    return <LoggedInPageTemplate>
-      <div className="pdmo__container">
-        <h2 className="headline pdmo__page-header">Primary designated management objective</h2>
-        <TraditionalTable tableSpec={tableSpec} countryIso={this.props.match.params.countryIso}/>
-      </div>
-    </LoggedInPageTemplate>
-  }
-}
+const PrimaryDesignatedManagementObjectiveView = props =>
+  <LoggedInPageTemplate>
+    <div className="pdmo__container">
+      <h2 className="headline pdmo__page-header">Primary designated management objective</h2>
+      <TraditionalTable tableSpec={tableSpec} countryIso={props.match.params.countryIso}/>
+    </div>
+  </LoggedInPageTemplate>
 
-const mapStateToProps = state => state
-
-export default connect(mapStateToProps, {})(PrimaryDesignatedManagementObjectiveView)
+export default PrimaryDesignatedManagementObjectiveView
--- webapp/specificForestCategories/specificForestCategoriesView.js
+++ webapp/specificForestCategories/specificForestCategoriesView.js
@@ -1,22 +1,16 @@
 import './style.less'
 import React from 'react'
-import { connect } from 'react-redux'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 
-class SpecificForestCategoriesView extends React.Component {
-  render() {
-    return <LoggedInPageTemplate>
-      <div className="sfc__container">
-        <h2 className="headline sfc__page-header">Specific forest categories</h2>
-        <TraditionalTable tableSpec={tableSpec} countryIso={this.props.match.params.countryIso}/>
-      </div>
-    </LoggedInPageTemplate>
-  }
-}
+const SpecificForestCategoriesView = props =>
+  <LoggedInPageTemplate>
+    <div className="sfc__container">
+      <h2 className="headline sfc__page-header">Specific forest categories</h2>
+      <TraditionalTable tableSpec={tableSpec} countryIso={props.match.params.countryIso}/>
+    </div>
+  </LoggedInPageTemplate>
 
-const mapStateToProps = state => state
-
-export default connect(mapStateToProps, {})(SpecificForestCategoriesView)
+export default SpecificForestCategoriesView
