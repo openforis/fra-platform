878a85af52a324d912f8acba570d20bb2b8cddc4
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-03 10:59:00 +0300

    Make stateless

--- webapp/forestAreaChange/forestAreaChangeView.js
+++ webapp/forestAreaChange/forestAreaChangeView.js
@@ -1,22 +1,16 @@
 import './style.less'
 import React from 'react'
-import { connect } from 'react-redux'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 
-class ForestAreaChangeView extends React.Component {
-  render() {
-    return <LoggedInPageTemplate>
+const ForestAreaChangeView = (props) =>
+    <LoggedInPageTemplate>
       <div className="fac__container">
         <h2 className="headline fac__page-header">Forest area loss, gain and net change</h2>
-        <TraditionalTable tableSpec={tableSpec} countryIso={this.props.match.params.countryIso}/>
+        <TraditionalTable tableSpec={tableSpec} countryIso={props.match.params.countryIso}/>
       </div>
     </LoggedInPageTemplate>
-  }
-}
 
-const mapStateToProps = state => state
-
-export default connect(mapStateToProps, {})(ForestAreaChangeView)
+export default ForestAreaChangeView
