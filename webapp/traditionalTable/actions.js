69de5df63e512b6c4b621ccc69963633a5e2d891
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-05 14:26:08 +0300

    First working paste
    Very ugly still, let's see how we can refactor this

--- webapp/traditionalTable/actions.js
+++ webapp/traditionalTable/actions.js
@@ -11,10 +11,7 @@ const createNewTableState = (tableSpec, rowIdx, colIdx, newValue, getState) => {
   const traditionalTableState = getState().traditionalTable
   assert(tableSpec.name, 'tableSpec is missing name')
   const tableValues = traditionalTableState[tableSpec.name] || table.createTableData(tableSpec)
-  //When we accept more than integers as input, we should use tableSpec to determine
-  //the type here and use the proper transformation
-  const sanitizedNewValue = acceptNextInteger(newValue, tableValues[rowIdx][colIdx])
-  return table.update(tableValues, rowIdx, colIdx, sanitizedNewValue)
+  return table.update(tableValues, rowIdx, colIdx, newValue)
 }
 
 const saveChanges = (countryIso, tableSpec, tableData) => {
@@ -36,6 +33,12 @@ const saveChanges = (countryIso, tableSpec, tableData) => {
     }
   }
   return debounced
+}
+
+export const tableChanged = (countryIso, tableSpec, newTableState) => dispatch => {
+  dispatch({type: tableValueChangedAction, tableSpec, newTableState})
+  dispatch(autosave.start)
+  dispatch(saveChanges(countryIso, tableSpec, newTableState))
 }
 
 export const tableValueChanged = (countryIso, tableSpec, rowIdx, colIdx, newValue) => (dispatch, getState) => {
--- webapp/traditionalTable/traditionalTable.js
+++ webapp/traditionalTable/traditionalTable.js
@@ -5,28 +5,40 @@ import assert from 'assert'
 
 import { ThousandSeparatedIntegerInput } from '../reusableUiComponents/thousandSeparatedIntegerInput'
 import * as table from './table'
-import { tableValueChanged, fetchTableData } from './actions'
-import { acceptableAsInteger } from '../utils/numberInput'
+import { tableValueChanged, tableChanged, fetchTableData } from './actions'
+import { acceptNextInteger, acceptableAsInteger } from '../utils/numberInput'
 
 const mapIndexed = R.addIndex(R.map)
 
-const handlePaste = (evt) => {
+const handlePaste = (countryIso, cellRowIdx, cellColIdx, tableSpec, tableData, tableChanged) =>(evt) => {
   evt.stopPropagation()
   evt.preventDefault()
-  console.log(evt)
   const el = document.createElement('html')
   el.innerHTML = evt.clipboardData.getData('text/html')
   const rows = el.getElementsByTagName('tr')
-  console.log('pasted html')
-  console.log(evt.clipboardData.getData('text/html'))
-  console.log('pasted rows')
-  console.log(rows)
-  console.log('xpasted text')
+  if (rows.length > 0) {
+    const pastedData =
+      R.flatten(mapIndexed(
+        (row, rowIdx) =>
+          mapIndexed((column, colIdx) => ({rowIdx, colIdx, cellData: column.innerText}), row.getElementsByTagName('td')),
+        rows))
+    const updatedTable = R.reduce((tableData, {rowIdx, colIdx, cellData}) => {
+      const rowIdxToUpdate = cellRowIdx+rowIdx
+      const colIdxToUpdate = cellColIdx+colIdx
+      if (rowIdxToUpdate > tableSpec.rows.length-1 ||
+        colIdxToUpdate > tableSpec.rows[0].length-1) return tableData
+      const [_, cellType] = getCellSpecAndType(tableSpec, rowIdxToUpdate, colIdxToUpdate)
+      if (!cellType.acceptValue) return tableData
+      return table.update(tableData, cellRowIdx+rowIdx, cellColIdx+colIdx, cellType.acceptValue(cellData, tableData[rowIdxToUpdate][colIdxToUpdate]))
+    }, tableData, pastedData)
+    console.log('updated table')
+    console.log(updatedTable)
+    tableChanged(countryIso, tableSpec, updatedTable)
+  }
   const txt = evt.clipboardData.getData('text/plain')
-  console.log('the text', txt)
 }
 
-const IntegerInput = ({countryIso, tableSpec, tableData, rowIdx, colIdx, tableValueChanged}) => {
+const IntegerInput = ({countryIso, tableSpec, tableData, rowIdx, colIdx, tableValueChanged, tableChanged}) => {
   const currentValue = tableData[rowIdx][colIdx]
   return <td className="fra-table__cell">
     <ThousandSeparatedIntegerInput integerValue={ currentValue }
@@ -36,31 +48,33 @@ const IntegerInput = ({countryIso, tableSpec, tableData, rowIdx, colIdx, tableVa
                                        console.log('EVT')
                                       const newValue = evt.target.value
                                       if (acceptableAsInteger(newValue)) {
-                                        console.log('changing tableValue based on', newValue)
-                                        tableValueChanged(countryIso, tableSpec, rowIdx, colIdx, evt.target.value)
+                                        const sanitizedNewValue = acceptNextInteger(newValue, tableData[rowIdx][colIdx])
+                                        tableValueChanged(countryIso, tableSpec, rowIdx, colIdx, sanitizedNewValue)
                                       }
                                     }
                                    }
-                                   onPaste={ handlePaste }/>
+                                   onPaste={ handlePaste(countryIso, rowIdx, colIdx, tableSpec, tableData, tableChanged) }/>
   </td>
 }
 
 const cellTypes = {
-  'integerInput': {render: (cellSpec, props) => <IntegerInput {...props}/>, acceptsValue: acceptableAsInteger},
+  'integerInput': {render: (cellSpec, props) => <IntegerInput {...props}/>, acceptValue: acceptNextInteger},
   'readOnly': {render: (cellSpec, props) => cellSpec.jsx},
   'custom': {render: (cellSpec, props) => cellSpec.render(props)}
 }
 
-const Cell = (props) => {
-  const {tableSpec, rowIdx, colIdx} = props
+const getCellSpecAndType = (tableSpec, rowIdx, colIdx) => {
   const cellSpec = tableSpec.rows[rowIdx][colIdx]
   assert(cellSpec, `No cellspec for ${rowIdx} ${colIdx}`)
-  const handler = cellTypes[cellSpec.type]
-  if (handler) {
-    return handler.render(cellSpec, props)
-  } else {
-    throw `Unknown cell type ${cellSpec.type}`
-  }
+  const cellType = cellTypes[cellSpec.type]
+  assert(cellType, `Unknown cell type ${cellSpec.type}`)
+  return [cellSpec, cellType]
+}
+
+const Cell = (props) => {
+  const {tableSpec, rowIdx, colIdx} = props
+  const [cellSpec, cellType] = getCellSpecAndType(tableSpec, rowIdx, colIdx)
+  return cellType.render(cellSpec, props)
 }
 
 const tableRows = (props) => {
@@ -97,4 +111,4 @@ const mapStateToProps = (state, props) => {
   return {...props, tableData: state.traditionalTable[props.tableSpec.name] || table.createTableData(props.tableSpec)}
 }
 
-export default connect(mapStateToProps, {tableValueChanged, fetchTableData})(FraTable)
+export default connect(mapStateToProps, {tableValueChanged, tableChanged, fetchTableData})(FraTable)
