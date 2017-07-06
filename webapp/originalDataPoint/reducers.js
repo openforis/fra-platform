52dc4970f9c27d4dc5dd7249ecdabc3da75b9706
Mino Togna <Cosimo.Togna@Fao.org>
2017-07-06 15:00:17 +0200

    removed validationStatus after draft is saved

--- webapp/originalDataPoint/reducers.js
+++ webapp/originalDataPoint/reducers.js
@@ -6,14 +6,15 @@ import { emptyDataPoint } from './originalDataPoint'
 const actionHandlers = {
   [types.dataPointSaveDraftStart]: (state, action) => R.assoc('active', action.active, state),
   [types.dataPointSaveDraftCompleted ]: (state, action) =>
-    R.assocPath(['active', 'odpId'], Number(action.odpId), state),
+    R.pipe(
+      R.assocPath(['active', 'odpId'], Number(action.odpId)),
+      R.dissocPath(['active', 'validationStatus'])
+    )(state),
   [types.odpFetchCompleted]: (state, action) => R.assoc('active', action.active)(state),
   [types.clearActiveAction ]: (state, action) => R.assoc('active', emptyDataPoint(), state),
   [types.odpListFetchCompleted]: (state, action) => R.assoc('odps', action.data)(state),
-  [types.ndpValidationStatusFetchCompleted]: (state, action) => ({
-    ...state,
-    active: R.assoc('validationStatus', action.data)(state.active)
-  })
+  [types.ndpValidationStatusFetchCompleted]: (state, action) =>
+    R.assocPath(['active', 'validationStatus'], action.data)(state)
 }
 
 export default (state = {}, action) => applyReducerFunction(actionHandlers, state, action)
