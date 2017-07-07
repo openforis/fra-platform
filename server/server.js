bd573c49989012aa7746855ed812bb78b3415cc0
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-07 09:58:58 +0300

    Move (most of) api initialization stuff into a separate module

--- server/server.js
+++ server/server.js
@@ -5,23 +5,10 @@ const bodyParser = require('body-parser')
 const compression = require('compression')
 const migrations = require('./db/migration/execMigrations')
 const sessionInit = require('./sessionInit')
-
+const apiRouter = require('./apiRouter')
 const authApi = require('./auth/authApi')
-const countryRepository = require('./countryRepository')
-const eofApi = require('./eof/api')
-const odpApi = require('./odp/api')
-const userApi = require('./user/userApi')
-const traditionalTableApi = require('./traditionalTable/api')
-const descriptionsApi = require('./descriptions/api')
-const reviewApi = require('./review/api')
 
 const app = express()
-const apiRouter = express.Router()
-//Nothing should be cached by default with the APIs
-apiRouter.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store')
-  next()
-})
 
 const bundleMatch = /^\/bundle-.*\.js(\.map)?$|^\/styles-.*\.css(\.map)?$/
 const oneYearInSeconds = 60 * 60 * 24 * 365
@@ -38,7 +25,6 @@ app.use((req, res, next) => {
 migrations()
 
 sessionInit.init(app)
-userApi.init(apiRouter)
 
 app.use(compression({threshold: 512}))
 app.use('/', express.static(`${__dirname}/../dist`))
@@ -47,24 +33,10 @@ app.use('/css/', express.static(`${__dirname}/../web-resources/css`))
 app.use('/ckeditor/', express.static(`${__dirname}/../web-resources/ckeditor`))
 app.use(bodyParser.json({limit: '5000kb'}))
 
+//Not part of apiRouter because of special urls (starting from root)
 authApi.init(app)
 
-apiRouter.get('/country/all', (req, res) => {
-  countryRepository.getAllCountries().then(result => {
-    res.json(result.rows)
-  }).catch(err => {
-    console.error(err)
-    res.status(500).json({error: 'Could not retrieve country data'})
-  })
-})
-
-odpApi.init(apiRouter)
-eofApi.init(apiRouter)
-traditionalTableApi.init(apiRouter)
-descriptionsApi.init(apiRouter)
-reviewApi.init(apiRouter)
-
-app.use('/api', apiRouter)
+app.use('/api', apiRouter.router)
 
 app.listen(process.env.PORT, () => {
   console.log('FRA Platform server listening on port ', process.env.PORT)
