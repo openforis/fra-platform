21b5cd8726ce8256670572a02900e6dd7de40c5a
Joni Rajanen <joni.rajanen@reaktor.com>
2017-07-10 08:35:55 +0300

    Refactor fix, use generic follow action in review reducer

--- webapp/router/actions.js
+++ webapp/router/actions.js
@@ -1,7 +1,5 @@
-import {issueCloseCommentThread} from '../review/actions'
 export const routerFollowLink = 'router/link/follow'
 
 export const follow = to => dispatch => {
-  dispatch({type: issueCloseCommentThread})
   dispatch({type: routerFollowLink, to})
 }
