dd2fddc5e0bfbb4a034cfd32162a2aa72c7afe42
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-12 10:04:16 +0300

    Refactor trad views to use CommentableDescription to reduce duplication

--- webapp/forestAreaChange/forestAreaChangeView.js
+++ webapp/forestAreaChange/forestAreaChangeView.js
@@ -3,27 +3,19 @@ import React from 'react'
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
-import Description from '../description/description'
-import ReviewIndicator from '../review/reviewIndicator'
+import CommentableDescription from '../description/commentableDescription'
 
 const ForestAreaChangeView = (props) =>
     <LoggedInPageTemplate>
       <div className="tv__container">
         <h2 className="headline tv__page-header">Forest area loss, gain and net change</h2>
         <TraditionalTable tableSpec={tableSpec} countryIso={props.match.params.countryIso}/>
-        <div className="tv__description-with-review-indicator">
-          <div className="tv__description-wrapper">
-            <Description title="Description"
-                         name="forestAreaChange"
-                         countryIso={props.match.params.countryIso}/>
-          </div>
-          <div className="tv__review-indicator-wrapper">
-            <ReviewIndicator section='forestAreaChange'
-                             name="Forest area change"
-                             target={['description']}
-                             countryIso={props.match.params.countryIso}/>
-          </div>
-        </div>
+        <CommentableDescription
+          section="forestAreaChange"
+          descriptionName="forestAreaChange"
+          descriptionTitle="Description"
+          countryIso={props.match.params.countryIso}
+        />
       </div>
     </LoggedInPageTemplate>
 
