cd18fa98687a495cb125bed9e0b6ccf4af597d12
Mino Togna <Cosimo.Togna@Fao.org>
2017-07-12 16:32:34 +0200

    updating Nav component error status when odp is deleted or draft made actual

--- webapp/navigation/reducers.js
+++ webapp/navigation/reducers.js
@@ -1,17 +1,13 @@
 import { applyReducerFunction } from '../utils/reduxUtils'
 import { listCountries, fetchNavStatusCompleted } from './actions'
-import { issuePostCommentCompleted} from '../review/actions'
+import { issuePostCommentCompleted } from '../review/actions'
+import { odpClearActiveAction } from '../originalDataPoint/actions'
 
 const actionHandlers = {
-  [listCountries]: (state, action) => {
-    return {...state, countries: action.countries}
-  },
-  [issuePostCommentCompleted]: state => {
-    return {...state, updateNeeded: true}
-  },
-  [fetchNavStatusCompleted]: (state, action) => {
-    return {...state, status: action.status, updateNeeded: false}
-  }
+  [listCountries]: (state, action) => ({...state, countries: action.countries}),
+  [issuePostCommentCompleted]: state => ({...state, updateNeeded: true}),
+  [fetchNavStatusCompleted]: (state, action) => ({...state, status: action.status, updateNeeded: false}),
+  [odpClearActiveAction]: state => ({...state, updateNeeded: true})
 }
 
 export default (state = {}, action) => applyReducerFunction(actionHandlers, state, action)
