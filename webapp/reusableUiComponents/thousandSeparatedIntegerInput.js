f5334fd1389d8bf047db5ea611a2b83dd7122119
Perttu Auramo <perttu.auramo@reaktor.fi>
2017-07-17 11:27:19 +0300

    Use focus to determine whether to show a div or input
    input is hidden with opacity so that it's still included in the tab order

--- webapp/reusableUiComponents/thousandSeparatedIntegerInput.js
+++ webapp/reusableUiComponents/thousandSeparatedIntegerInput.js
@@ -8,33 +8,41 @@ const renderUnfocusedIntegerValue = integerValue =>
   typeof integerValue === 'number' ? separateThousandsWithSpaces(integerValue) : ''
 
 export class ThousandSeparatedIntegerInput extends React.Component {
+  constructor () {
+    super()
+    this.state = {hasFocus: false}
+  }
   render () {
+    console.log('hasFocus', this.state.hasFocus)
     const {integerValue, onChange, onPaste, className} = this.props
-    return <div>
-      <div style={{position: 'absolute', left: -3000}}> {integerValue} </div>
-      <input type="text"
-                  maxLength="100"
-                  ref="inputField"
-                  className={className}
-                  value={ this.hasFocus
-                    ? renderFocusedIntegerValue(integerValue)
-                    : renderUnfocusedIntegerValue(integerValue)
-                  }
-                  onChange={ onChange }
-                  onPaste={ onPaste }
-                  onFocus={
-                    () => {
-                      this.hasFocus = true
-                      this.refs.inputField.value = integerValue || null //prevent text "undefined" from rendering
-                    }
-                  }
-                  onBlur={
-                    () => {
-                      this.hasFocus = false
-                      this.refs.inputField.value = separateThousandsWithSpaces(integerValue)
-                    }
-                  }
-    />
-  </div>
+    return <div style={{position: 'relative'}}>
+      <div style={{position: 'absolute', right: '5px', top: '9px', display: this.state.hasFocus ? 'none' : 'inline-block'}}> {integerValue} </div>
+      <div style={{opacity: this.state.hasFocus ? '1' : '0'}}>
+        <input
+          type="text"
+          maxLength="100"
+          ref="inputField"
+          className={className}
+          value={ this.state.hasFocus
+            ? renderFocusedIntegerValue(integerValue)
+            : renderUnfocusedIntegerValue(integerValue)
+          }
+          onChange={ onChange }
+          onPaste={ onPaste }
+          onFocus={
+            () => {
+              this.setState({hasFocus: true})
+              this.refs.inputField.value = integerValue || null //prevent text "undefined" from rendering
+            }
+          }
+          onBlur={
+            () => {
+              this.setState({hasFocus: false})
+              this.refs.inputField.value = separateThousandsWithSpaces(integerValue)
+            }
+          }
+        />
+      </div>
+    </div>
   }
 }
