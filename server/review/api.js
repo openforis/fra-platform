307103e220015ce99d2408da9829213cb0658c48
Mino Togna <Cosimo.Togna@Fao.org>
2017-07-19 10:07:56 +0200

    added delete comment api method

--- server/review/api.js
+++ server/review/api.js
@@ -3,19 +3,19 @@ const R = require('ramda')
 
 const db = require('../db/db')
 const {sendErr} = require('../requestUtils')
-const issueRepository = require('./reviewRepository')
+const reviewRepository = require('./reviewRepository')
 
 module.exports.init = app => {
 
   app.post('/review/:issueId', (req, res) => {
     const userId = req.session.passport.user.id
-    db.transaction(issueRepository.createComment, [req.params.issueId, userId, req.body.msg, ''])
+    db.transaction(reviewRepository.createComment, [req.params.issueId, userId, req.body.msg, ''])
       .then(result => res.json({}))
       .catch(err => sendErr(res, err))
   })
 
   app.get('/review/:countryIso/:section/count', (req, res) => {
-    issueRepository.getIssues(req.params.countryIso, req.params.section)
+    reviewRepository.getIssues(req.params.countryIso, req.params.section)
       .then(result => {
         const target = req.query.target && req.query.target.split(',')
         const issues = R.map(issue => {
@@ -28,17 +28,17 @@ module.exports.init = app => {
   })
 
   app.post('/review/:countryIso/:section', (req, res) => {
-    const userId =  req.session.passport.user.id
+    const userId = req.session.passport.user.id
     const target = req.query.target ? req.query.target.split(',') : []
     db.transaction(
-      issueRepository.createIssueWithComment,
+      reviewRepository.createIssueWithComment,
       [req.params.countryIso, req.params.section, {params: target}, userId, req.body.msg])
       .then(result => res.json({}))
       .catch(err => sendErr(res, err))
   })
 
   app.get('/review/:countryIso/:section', (req, res) => {
-    issueRepository.getIssues(req.params.countryIso, req.params.section)
+    reviewRepository.getIssues(req.params.countryIso, req.params.section)
       .then(result => {
         const target = req.query.target && req.query.target.split(',')
         const issues = R.map(issue => {
@@ -50,17 +50,24 @@ module.exports.init = app => {
             R.reject(R.isEmpty),
             R.map(
               comment =>
-              R.merge(R.omit('email', comment), // leave out email
-              R.pipe( // calculate email hash for gravatar
-                R.prop('email'),
-                v => crypto.createHash('md5').update(v).digest('hex'),
-                h => ({hash: h})
-              )(comment)
-              )
+                R.merge(R.omit('email', comment), // leave out email
+                  R.pipe( // calculate email hash for gravatar
+                    R.prop('email'),
+                    v => crypto.createHash('md5').update(v).digest('hex'),
+                    h => ({hash: h})
+                  )(comment)
+                )
             )
           )(issues)
         )
       })
       .catch(err => sendErr(res, err))
   })
+
+  app.delete('/review/:countryIso/comments/:commentId', (req, res) =>
+    db
+      .transaction(reviewRepository.deleteComment, [req.params.commentId])
+      .then(() => res.json({}))
+      .catch(err => sendErr(res, err))
+  )
 }
