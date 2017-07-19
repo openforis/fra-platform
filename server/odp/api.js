53cd69e8f74025aebfd6c1b8115a1a29fd8e1971
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-19 16:10:46 +0300

    Add the easy access-control checks

--- server/odp/api.js
+++ server/odp/api.js
@@ -3,10 +3,12 @@ const db = require('../db/db')
 const odpRepository = require('./odpRepository')
 const reviewRepository = require('../review/reviewRepository')
 const {sendErr} = require('../utils/requestUtils')
+const {checkCountryAccessFromReqParams} = require('../utils/accessControl')
 
 module.exports.init = app => {
 
   app.get('/odp', (req, res) => {
+    checkCountryAccessFromReqParams(req)
     const odp = R.isNil(req.query.odpId) ? Promise.resolve({}) : odpRepository.getOdp(req.query.odpId)
     const odps = odpRepository.listOriginalDataPoints(req.query.countryIso)
     Promise.all([odp, odps])
@@ -27,52 +29,55 @@ module.exports.init = app => {
       .catch(err => sendErr(res, err))
   })
 
-  app.get('/odps/:countryIso', (req, res) =>
-    odpRepository
-      .listAndValidateOriginalDataPoints(req.params.countryIso)
-      .then(odps => {
-        const issues = odps.map(odp =>
-          reviewRepository
-            .getIssuesByParam(req.params.countryIso, 'NDP', 0, odp.odpId)
-            .then(issues => R.assoc('issues', issues, odp))
-        )
-        Promise
-          .all(issues)
-          .then(odpsWithIssues => res.json(odpsWithIssues))
-      })
-      .catch(err => {
-        console.error(err)
-        res.status(500).json({error: 'Could not retrieve data'})
-      })
+  app.get('/odps/:countryIso', (req, res) => {
+      checkCountryAccessFromReqParams(req)
+      odpRepository
+        .listAndValidateOriginalDataPoints(req.params.countryIso)
+        .then(odps => {
+          const issues = odps.map(odp =>
+            reviewRepository
+              .getIssuesByParam(req.params.countryIso, 'NDP', 0, odp.odpId)
+              .then(issues => R.assoc('issues', issues, odp))
+          )
+          Promise
+            .all(issues)
+            .then(odpsWithIssues => res.json(odpsWithIssues))
+        })
+        .catch(err => {
+          console.error(err)
+          res.status(500).json({error: 'Could not retrieve data'})
+        })
+    }
   )
 
-  app.delete('/odp', (req, res) =>
-    db.transaction(reviewRepository.deleteIssues, [req.query.countryIso, 'NDP', 0, req.query.odpId])
-      .then(db.transaction(odpRepository.deleteOdp, [req.query.odpId])
-        .then(() => res.json({})))
-      .catch(err => sendErr(res, err))
+  app.delete('/odp', (req, res) => {
+      checkCountryAccessFromReqParams(req)
+      //TODO access control! we have to check that odpId is attached to the correct country!
+      db.transaction(reviewRepository.deleteIssues, [req.query.countryIso, 'NDP', 0, req.query.odpId])
+        .then(db.transaction(odpRepository.deleteOdp, [req.query.odpId])
+          .then(() => res.json({})))
+        .catch(err => sendErr(res, err))
+    }
   )
 
   app.post('/odp/draft', (req, res) => {
+    checkCountryAccessFromReqParams(req)
     const countryIso = req.query.countryIso
     return db.transaction(odpRepository.saveDraft, [countryIso, req.body])
       .then(result => res.json(result))
       .catch(err => sendErr(res, err))
   })
 
-  app.delete('/odp/draft', (req, res) =>
-    db.transaction(odpRepository.deleteDraft, [req.query.odpId])
-      .then(() => res.json({}))
-      .catch(err => sendErr(res, err))
-  )
-
-  app.post('/odp/markAsActual', (req, res) =>
-    db.transaction(odpRepository.markAsActual, [req.query.odpId])
-      .then(() => res.json({})
-      ).catch(err => sendErr(res, err))
+  app.delete('/odp/draft', (req, res) => {
+      //TODO access control! We have to check that odpId is attached to the correct country!
+      db.transaction(odpRepository.deleteDraft, [req.query.odpId])
+        .then(() => res.json({}))
+        .catch(err => sendErr(res, err))
+    }
   )
 
   app.get('/prevOdp/:countryIso/:year', (req, res) => {
+    checkCountryAccessFromReqParams(req)
     odpRepository.listOriginalDataPoints(req.params.countryIso)
       .then(resp => {
         const prevOdp = R.pipe(
@@ -87,5 +92,12 @@ module.exports.init = app => {
       })
       .catch(err => sendErr(res, err))
   })
+
+  app.post('/odp/markAsActual', (req, res) =>
+    //TODO access control! request doesn't contain countryIso
+    db.transaction(odpRepository.markAsActual, [req.query.odpId])
+      .then(() => res.json({})
+      ).catch(err => sendErr(res, err))
+  )
 
 }
