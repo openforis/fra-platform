d602a9c00e373de671bc1c1a217c4ebd28932b22
Mino Togna <Cosimo.Togna@Fao.org>
2017-07-19 14:50:54 +0200

    updating navstatus after creating or deleting a comment

--- webapp/navigation/reducers.js
+++ webapp/navigation/reducers.js
@@ -1,10 +1,8 @@
 import { applyReducerFunction } from '../utils/reduxUtils'
 import { listCountries, fetchNavStatusCompleted } from './actions'
-import { issuePostCommentCompleted } from '../review/actions'
 
 const actionHandlers = {
   [listCountries]: (state, action) => ({...state, countries: action.countries}),
-  [issuePostCommentCompleted]: state => ({...state, updateNeeded: true}),
   [fetchNavStatusCompleted]: (state, action) => ({...state, status: action.status, updateNeeded: false})
 }
 
--- webapp/review/actions.js
+++ webapp/review/actions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { fetchNavStatus } from '../navigation/actions'
 
 export const issuePostCommentCompleted = 'issue/comment/post/completed'
 export const issueRetrieveCommentsStarted = 'issue/comment/retrieve/started'
@@ -23,6 +24,7 @@ export const postComment = (issueId, countryIso, section, target, userId, msg) =
   axios.post(api, {msg}).then(() => {
       dispatch({target: target, type: issuePostCommentCompleted, status: 'completed'})
       getCommentCount(countryIso, section, target)(dispatch)
+    fetchNavStatus(countryIso)(dispatch)
       axios.get(`api/review/${countryIso}/${section}?target=${target}`)
         .then(sectionCommentsReceived(section, target, dispatch))
     }
@@ -54,4 +56,5 @@ export const deleteComment = (countryIso, section, target, commentId) => dispatc
     .then(() => {
       retrieveComments(countryIso, section, target)(dispatch)
       getCommentCount(countryIso, section, target)(dispatch)
+      fetchNavStatus(countryIso)(dispatch)
     })
