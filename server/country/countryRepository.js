aed0bd5a9a16c00a29d44f57946a324901eda21d
Perttu Auramo <perttu.auramo@reaktor.fi>
2017-07-25 13:25:11 +0300

    Handle left outer join and it's reduction in all countries list

--- server/country/countryRepository.js
+++ server/country/countryRepository.js
@@ -2,7 +2,48 @@ const R = require('ramda')
 const db = require('../db/db')
 const camelize = require('camelize')
 
-const getAllCountries = () => db.query('SELECT country_iso, name FROM country ORDER BY name ASC').then(res => camelize(res))
+/*
+ * Determine the "overall status" from multiple statuses.
+ * For example, one review is enough to determine that overall
+ * the whlole country is in review
+ * If all statuses are in accepted, we determine that country is in
+ * accepted status
+ */
+const determineCountryAssessmentStatus = (statuses) => {
+  if (R.isEmpty(statuses)) return 'editing' //Initially, there are no rows for country's assessment,
+                                            //this is also considered to be 'editing' status
+  else if (R.all(R.equals('accepted'), statuses)) return 'accepted'
+  else if (R.any(R.equals('review'), statuses)) return 'review'
+  else return 'editing'
+}
+
+const getStatuses = groupedRows =>
+  R.pipe(
+    R.map(R.prop('status')),
+    R.filter(R.identity)
+  )(groupedRows)
+
+const handleCountryResult = result => {
+  const grouped = R.groupBy(row => row.countryIso, camelize(result.rows))
+  const withOverallAssessments = R.map(
+    ([countryIso, vals]) => {
+      return {
+        countryIso,
+        name: vals[0].name,
+        assessmentStatus: determineCountryAssessmentStatus(getStatuses(vals))
+      }
+    },
+    R.toPairs(grouped))
+  console.log(withOverallAssessments)
+  return withOverallAssessments
+}
+
+const getAllCountries = () => console.log('getAllCountries') ||
+  db.query(`SELECT c.country_iso, c.name, a.status
+            FROM country c
+            LEFT OUTER JOIN assessment a ON c.country_iso = a.country_iso 
+            ORDER BY name ASC`)
+    .then(handleCountryResult)
 
 module.exports.getAllowedCountries = (roles) => {
   const hasRole = (role) => R.find(R.propEq('role', role), roles)
@@ -17,6 +58,6 @@ module.exports.getAllowedCountries = (roles) => {
                      WHERE country_iso in (${allowedIsoQueryPlaceholders})
                      ORDER BY name ASC`,
                     allowedCountryIsos)
-      .then(res => camelize(res))
+      .then(res => camelize(res.rows))
   }
 }
