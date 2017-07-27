2bdeff15d0c9d5a181f416a931b8f2148ded6918
Mino Togna <Cosimo.Togna@Fao.org>
2017-07-27 15:17:43 +0200

    changing i18n language when user is loaded or has changed language

--- webapp/user/reducer.js
+++ webapp/user/reducer.js
@@ -1,8 +1,18 @@
 import { userInfo } from './actions'
 import { applyReducerFunction } from '../utils/reduxUtils'
+import R from 'ramda'
+import i18n from '../i18n/i18n'
 
 const actionHandlers = {
-  [userInfo]: (state, action) => ({...state, userInfo: action.userInfo}),
+
+  [userInfo]: (state, action) => {
+    const userInfo = action.userInfo
+
+    if (R.isNil(state.userInfo) || !userInfo.lang !== i18n.language)
+      i18n.changeLanguage(userInfo.lang)
+
+    return {...state, userInfo}
+  }
 }
 
 export default (state = {}, action) => applyReducerFunction(actionHandlers, state, action)
