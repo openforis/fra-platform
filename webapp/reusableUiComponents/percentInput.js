21dc5cc475c2ef814ab11952b2bbb9210aad9ac5
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-27 14:48:57 +0300

    Add stuff mostly copied from tsii

--- webapp/reusableUiComponents/percentInput.js
+++ webapp/reusableUiComponents/percentInput.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import './percentInput.less'
+
+const renderValue = value =>
+  typeof value === 'number' ? value : ''
 
 export class PercentInput extends React.Component {
   constructor () {
     super()
+    this.state = {hasFocus: false}
+  }
+
+  componentDidMount() {
+    /*
+     * For adjusting size
+     */
+    if (this.props.value) {
+      this.forceUpdate()
+    }
+  }
+
+  getWidth() {
+    if (this.refs.wrapper) {
+      return this.refs.wrapper.getBoundingClientRect().width -8
+    }
+    return null
   }
 
   render () {
-    return <div>x</div>
+    const width = this.getWidth()
+    return <div className="percent__field" ref="wrapper">
+      <div className="percent__readonly-view"
+           style={{
+             width: width ? `${width}px` : null,
+             display: this.state.hasFocus ? 'none' : 'inline-block',
+           }}
+      >
+        {renderValue(this.props.value)}
+      </div>
+      <div style={{opacity: this.state.hasFocus ? '1' : '0'}}>
+        <input
+          type="text"
+          maxLength="3"
+          value={renderValue(this.props.value)}
+          onChange={ this.props.onChange }
+          onPaste={ this.props.onPaste }
+        />
+      </div>
+    </div>
   }
 }
