e1377af5b2269e38568f29439667108de1352a2b
Perttu Auramo <perttu.auramo@reaktor.com>
2017-07-28 14:47:22 +0300

    Add a dummy language-selector

--- webapp/footer/footer.js
+++ webapp/footer/footer.js
@@ -5,30 +5,47 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../user/actions'
 
-class UserControl extends React.Component {
+class FooterSelectionControl extends React.Component {
   componentWillMount () {
     this.setState({opened: false})
   }
 
   render () {
     const iconRefSuffix = this.state.opened ? 'down' : 'up'
+    const children = this.props.children
     return <span
       className="footer__user-control"
       onClick={ evt => this.setState({opened: !this.state.opened}) }>
-      {this.props.userName + ' '}
+      {this.props.label + ' '}
       <svg className="icon footer__user-control-caret">
         <use xlinkHref={`img/icon.svg#icon-small-${iconRefSuffix}`}/>
       </svg>
       {
-        this.state.opened
-          ? <div onClick={() => this.props.logout()} className="footer__user-control-opened">
-          Logout
-        </div>
-          : null
+       this.state.opened ? children : null
       }
     </span>
   }
 }
+
+const UserInfo = props =>
+  <FooterSelectionControl label={props.userName} {...props}>
+    <div onClick={() => props.logout()} className="footer__user-control-opened">
+      Logout
+    </div>
+  </FooterSelectionControl>
+
+const LanguageSelection = props =>
+  <FooterSelectionControl label={props.currentLanguage} {...props}>
+    <div className="footer__language-control-opened">
+      <div className="footer__selection-control-item" onClick={() => console.log('a')}>
+        English
+      </div>
+      <div className="footer__selection-control-item" onClick={() => console.log('b')}>
+        French
+      </div>
+    </div>
+  </FooterSelectionControl>
+
 
 const Footer = ({status, userInfo, path, width, ...props}) => {
   const style = {width: `calc(100vw - ${width}px)`}
@@ -36,7 +53,10 @@ const Footer = ({status, userInfo, path, width, ...props}) => {
     {/* Placeholder for space-between flexbox alignment */}
     <div/>
     <div className="footer__item footer__autosave-status">{status}</div>
-    <div className="footer__item">{userInfo ? <UserControl userName={userInfo.name} {...props}/> : ''}</div>
+    <div>
+      <div className="footer__item">{userInfo ? <LanguageSelection currentLanguage={'English'} {...props}/> : ''}</div>
+      <div className="footer__item">{userInfo ? <UserInfo userName={userInfo.name} {...props}/> : ''}</div>
+    </div>
   </div>
 }
 const mapStateToProps = state => R.pipe(R.merge(state.autoSave), R.merge(state.user))(state.router)
