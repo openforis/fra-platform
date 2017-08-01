5c45835e892221664b65cde9d67bd69bc6785df0
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-01 12:07:43 +0200

    added i18n support to Primary designated management objective

--- webapp/primaryDesignatedManagementObjective/primaryDesignatedManagementObjectiveView.js
+++ webapp/primaryDesignatedManagementObjective/primaryDesignatedManagementObjectiveView.js
@@ -1,22 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 import CommentableDescription from '../description/commentableDescription'
 
-const PrimaryDesignatedManagementObjectiveView = props =>
-  <LoggedInPageTemplate>
+const PrimaryDesignatedManagementObjectiveView = ({match, i18n}) => {
+  const tableProps = tableSpec(i18n)
+
+  return <LoggedInPageTemplate>
     <div className="tv__container">
-      <h2 className="headline tv__page-header">Primary designated management objective</h2>
-      <TraditionalTable tableSpec={tableSpec} countryIso={props.match.params.countryIso}/>
+      <h2 className="headline tv__page-header">
+        {i18n.t('primaryDesignatedManagementObjective.primaryDesignatedManagementObjective')}
+      </h2>
+      <TraditionalTable tableSpec={tableProps} countryIso={match.params.countryIso}/>
       <CommentableDescription
-        section={tableSpec.name}
+        section={tableProps.name}
         descriptionName="primaryDesignatedManagementObjective"
-        descriptionTitle="Description"
-        countryIso={props.match.params.countryIso}
+        descriptionTitle={i18n.t('description.description')}
+        countryIso={match.params.countryIso}
       />
     </div>
   </LoggedInPageTemplate>
+}
 
-export default PrimaryDesignatedManagementObjectiveView
+const mapStateToProps = state => ({i18n: state.user.i18n})
+
+export default connect(mapStateToProps)(PrimaryDesignatedManagementObjectiveView)
