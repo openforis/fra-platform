1f17b52b376c88b7b465c721c4e85be3487027c7
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-01 11:57:02 +0200

    added i18n support to Specific forest categories

--- webapp/specificForestCategories/specificForestCategoriesView.js
+++ webapp/specificForestCategories/specificForestCategoriesView.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 import CommentableDescription from '../description/commentableDescription'
 
-const SpecificForestCategoriesView = props =>
-  <LoggedInPageTemplate>
+const SpecificForestCategoriesView = ({match, i18n}) => {
+  const tableProps = tableSpec(i18n)
+
+  return <LoggedInPageTemplate>
     <div className="tv__container">
-      <h2 className="headline tv__page-header">Specific forest categories</h2>
-      <TraditionalTable tableSpec={tableSpec} countryIso={props.match.params.countryIso}/>
+      <h2 className="headline tv__page-header">{i18n.t('specificForestCategories.specificForestCategories')}</h2>
+      <TraditionalTable tableSpec={tableProps} countryIso={match.params.countryIso}/>
       <CommentableDescription
-        section={tableSpec.name}
+        section={tableProps.name}
         descriptionName="specificForestCategories"
-        descriptionTitle="Description"
-        countryIso={props.match.params.countryIso}
+        descriptionTitle={i18n.t('description.description')}
+        countryIso={match.params.countryIso}
       />
     </div>
   </LoggedInPageTemplate>
+}
 
-export default SpecificForestCategoriesView
+const mapStateToProps = state => ({i18n: state.user.i18n})
+
+export default connect(mapStateToProps)(SpecificForestCategoriesView)
