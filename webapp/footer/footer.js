c103aa4b82aec7eae77688be1616b1d80785efc8
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-01 15:06:11 +0200

    added i18n support to footer

--- webapp/footer/footer.js
+++ webapp/footer/footer.js
@@ -31,7 +31,7 @@ const UserInfo = props =>
   <FooterSelectionControl label={props.userName} {...props}>
     <div className="footer__selection-control-opened">
       <div onClick={() => props.logout()} className="footer__selection-control-item">
-        Logout
+        {props.i18n.t('footer.logout')}
       </div>
     </div>
   </FooterSelectionControl>
@@ -67,7 +67,7 @@ const Footer = ({status, userInfo, path, width, i18n, ...props}) => {
         <LanguageSelection currentLanguage={i18n.t(`language.${i18n.language}`)} i18n={i18n} {...props}/>
       </div>
       <div className="footer__item">
-        <UserInfo userName={userInfo.name} {...props}/>
+        <UserInfo userName={userInfo.name} i18n={i18n} {...props}/>
       </div>
     </div>
   </div>
