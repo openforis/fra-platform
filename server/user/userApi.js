746dd40c16c1bc5d0451075cf9979902ede97c88
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-02 10:39:28 +0200

    persisting changed user lang

--- server/user/userApi.js
+++ server/user/userApi.js
@@ -1,6 +1,18 @@
+const db = require('../db/db')
+const userRepository = require('./userRepository')
+const {sendErr} = require('../utils/requestUtils')
+
 module.exports.init = app => {
+
   app.get('/loggedInUser/', (req, res) => {
-    const userInfo = req.session.passport.user
+    const userInfo = req.user
     res.json({userInfo})
   })
+
+  app.post('/user/lang', (req, res) => {
+    db.transaction(userRepository.updateLanguage, [req.query.lang, req.session.passport.user])
+      .then(() => res.json({}))
+      .catch(err => sendErr(res, err))
+  })
+
 }
