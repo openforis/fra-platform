2d99f926bc2739ff7d38029b99fea8a50096d886
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-02 11:21:18 +0200

    changed getUserInfo to findUserById

--- server/user/userRepository.js
+++ server/user/userRepository.js
@@ -2,8 +2,8 @@ const db = require('../db/db')
 const camelize = require('camelize')
 const R = require('ramda')
 
-const getUserInfo = email =>
-  db.query('SELECT id, name ,lang FROM fra_user WHERE email = $1', [email])
+const findUserById = userId =>
+  db.query('SELECT id, name ,lang FROM fra_user WHERE id = $1', [userId])
     .then(res => {
         if (res.rows.length > 0) {
           const resultUser = res.rows[0]
@@ -17,11 +17,11 @@ const getUserInfo = email =>
       }
     ).then(res => res ? R.assoc('roles', camelize(res[1].rows), res[0]) : null)
 
-module.exports.getUserInfoByEmail = getUserInfo
+module.exports.findUserById = findUserById
 
 module.exports.findUserByLoginEmails = emails =>
-  db.query('SELECT email from fra_user WHERE login_email in ($1)', [emails.join(',')])
-    .then(res => res.rows.length > 0 ? getUserInfo(res.rows[0].email) : null)
+  db.query('SELECT id from fra_user WHERE login_email in ($1)', [emails.join(',')])
+    .then(res => res.rows.length > 0 ? findUserById(res.rows[0].id) : null)
 
 module.exports.updateLanguage = (client, lang, userInfo) =>
   client
