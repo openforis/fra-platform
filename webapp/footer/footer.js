6c294ae6748963942bcf334254c54ed7ec7aee79
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-03 11:16:03 +0200

    enabled spanish language

--- webapp/footer/footer.js
+++ webapp/footer/footer.js
@@ -47,7 +47,7 @@ const LanguageSelection = ({i18n, switchLanguage, ...props}) =>
     </div>
   </FooterSelectionControl>
 
-const supportedLangs = ['en', 'fr']
+const supportedLangs = ['en', 'fr', 'es']
 
 const Footer = ({status, userInfo, path, width, i18n, ...props}) => {
   const style = {width: `calc(100vw - ${width}px)`}
