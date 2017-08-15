f2948bf62dd6f5aad920fa9b19135097e99c212b
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-08-15 17:36:43 +0300

    Use new alert style for application errors and login

--- webapp/applicationError/errorComponent.js
+++ webapp/applicationError/errorComponent.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { clearApplicationError } from './actions'
-import './style.less'
 
 const ErrorBox = ({msg, clearApplicationError}) => {
   console.error(msg)
-  return <div className="ae__container">
-    {msg}
-    <button className="ae__clear-button" onClick={() => clearApplicationError()}>X</button>
+  return <div className="alert-container">
+    <div className="alert-error">
+      <div className="alert-icon">
+        <svg className="icon"><use xlinkHref="img/icon.svg#icon-alert"/></svg>
+      </div>
+      <div className="alert-message">{msg}</div>
+      <div className="alert-dismiss" onClick={() => clearApplicationError()}>
+        <svg className="icon"><use xlinkHref="img/icon.svg#icon-small-remove"/></svg>
+      </div>
+    </div>
   </div>
 }
 
