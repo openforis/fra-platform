5fc5eaac50b848633312782cbf713c10b2e6508d
Joni Rajanen <joni.rajanen@reaktor.com>
2017-08-21 13:46:14 +0300

    Update parameters in commentable descriptions component call sites

--- webapp/areaAffectedByFire/areaAffectedByFireView.js
+++ webapp/areaAffectedByFire/areaAffectedByFireView.js
@@ -2,7 +2,7 @@ import './style.less'
 import React from 'react'
 import { connect } from 'react-redux'
 
-import CommentableDescription from '../description/commentableDescription'
+import CommentableDescriptions from '../description/commentableDescription'
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
@@ -14,7 +14,7 @@ const AreaAffectedByFire = ({match, i18n}) => {
     <div className="tv__container aabf__container">
       <h2 className="headline tv__page-header">{i18n.t('areaAffectedByFire.areaAffectedByFire')}</h2>
       <TraditionalTable tableSpec={tableProps} countryIso={match.params.countryIso}/>
-      <CommentableDescription
+      <CommentableDescriptions
         section={tableProps.name}
         name="areaAffectedByFire"
         countryIso={match.params.countryIso}
--- webapp/forestAreaChange/forestAreaChangeView.js
+++ webapp/forestAreaChange/forestAreaChangeView.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
-import CommentableDescription from '../description/commentableDescription'
+import CommentableDescriptions from '../description/commentableDescription'
 
 const ForestAreaChangeView = ({match, i18n}) => {
   const tableProps = tableSpec(i18n)
@@ -13,11 +13,11 @@ const ForestAreaChangeView = ({match, i18n}) => {
     <div className="tv__container">
       <h2 className="headline tv__page-header">{i18n.t('forestAreaChange.forestAreaLossGainChange')}</h2>
       <TraditionalTable tableSpec={tableProps} countryIso={match.params.countryIso}/>
-      <CommentableDescription
+      <CommentableDescriptions
         section={tableProps.name}
-        descriptionName="forestAreaChange"
-        descriptionTitle={i18n.t('description.description')}
+        name="forestAreaChange"
         countryIso={match.params.countryIso}
+        i18n={i18n}
       />
     </div>
   </LoggedInPageTemplate>
