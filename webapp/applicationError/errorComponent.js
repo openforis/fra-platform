8e9b8d0b131257b29b4f206eb5b6993a02308609
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-22 15:10:33 +0200

    added i18n state object to error component props

--- webapp/applicationError/errorComponent.js
+++ webapp/applicationError/errorComponent.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { clearApplicationError } from './actions'
+import * as R from 'ramda'
 
-const ErrorBox = ({msg, clearApplicationError}) => {
-  console.error(msg)
+const ErrorBox = ({applicationError, i18n, clearApplicationError}) => {
+  console.error(applicationError.msg)
   return <div className="alert-container">
     <div className="alert-error">
       <div className="alert-icon">
-        <svg className="icon"><use href="img/icons.svg#alert"/></svg>
+        <svg className="icon">
+          <use href="img/icons.svg#alert"/>
+        </svg>
       </div>
-      <div className="alert-message">{msg}</div>
+      <div className="alert-message">{applicationError.msg}</div>
       <div className="alert-dismiss" onClick={() => clearApplicationError()}>
-        <svg className="icon"><use href="img/icons.svg#remove"/></svg>
+        <svg className="icon">
+          <use href="img/icons.svg#remove"/>
+        </svg>
       </div>
     </div>
   </div>
 }
 
-const ErrorComponent = (props) => props.msg ? <ErrorBox {...props}/> : null
+const ErrorComponent = (props) => R.path(['applicationError', 'msg'], props) ? <ErrorBox {...props}/> : null
 
-export default connect(state => state['applicationError'], {clearApplicationError})(ErrorComponent)
+const mapStateToProps = state => ({applicationError: state.applicationError, i18n: state.user.i18n})
+
+export default connect(mapStateToProps, {clearApplicationError})(ErrorComponent)
