cb4c2558485dac11854515f9c7067b40aaee7978
Mino Togna <Cosimo.Togna@Fao.org>
2017-08-24 14:35:50 +0200

    made chart component using dynamic properties

--- webapp/nationalDataEntry/nationalDataEntryView.js
+++ webapp/nationalDataEntry/nationalDataEntryView.js
@@ -16,7 +16,7 @@ class ChartWrapper extends UpdateOnResizeReactComponent {
     const defaultWidth = 913 //TODO what's a good default before we have bounding rect?
     const width = this.refs.chartWrapper ? this.refs.chartWrapper.getBoundingClientRect().width : defaultWidth
     return <div ref="chartWrapper" className="nde__data-chart">
-      <Chart wrapperWidth={width}/>
+      <Chart wrapperWidth={width} stateName="nationalDataEntry" trends={['forestArea','otherWoodedLand']}/>
     </div>
   }
 }
