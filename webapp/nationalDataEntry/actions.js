082dbf1903eaf94dbdfabbf1f0037be95e415600
Joni Rajanen <joni.rajanen@reaktor.com>
2017-08-24 13:49:31 +0300

    Fix eof estimation of fra values

--- webapp/nationalDataEntry/actions.js
+++ webapp/nationalDataEntry/actions.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+import {fetchItem} from '../originalDataPoint/actions'
+
 export const generateFraValuesStart = 'nationalDataEntry/generateFraValues/start'
 
 export const generateFraValues = (countryIso) => dispatch => {
   dispatch({type: generateFraValuesStart})
 
   axios.post(`/api/country/estimation/generateFraValues/${countryIso}`).then(resp => {
-    dispatch(fetch(countryIso))
+    dispatch(fetchItem('eof', countryIso))
   })
 }
