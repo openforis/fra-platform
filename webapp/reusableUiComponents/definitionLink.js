d74f6d796a70ba291d921b81b402efda35c41e72
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-08-30 17:38:23 +0300

    Add styling to definition content

--- webapp/reusableUiComponents/definitionLink.js
+++ webapp/reusableUiComponents/definitionLink.js
@@ -2,10 +2,6 @@ import React from 'react'
 import './definitionLink.less'
 
 export default ({name, i18n}) => <div className="definition-link"
-                                      onClick={() =>
-                                        window.open(
-                                          `definitions/en/${name}`,
-                                          name,
-                                          'height=600,width=400')}>
-  {i18n.t('definition.linkLabel')}
-  </div>
+                                      onClick={() => window.open(`definitions/en/${name}`, name, 'height=640,width=360')}>
+                                        {i18n.t('definition.linkLabel')}
+                                  </div>
