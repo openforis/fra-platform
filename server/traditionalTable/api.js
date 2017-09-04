1c8fc8117bcd59652d64b8df6e982d18f48d5452
Joni Rajanen <joni.rajanen@reaktor.com>
2017-09-04 15:28:12 +0300

    audit for tradionalData insertions

--- server/traditionalTable/api.js
+++ server/traditionalTable/api.js
@@ -2,10 +2,13 @@ const db = require('../db/db')
 const repository = require('./traditionalTableRepository')
 const {sendErr} = require('../utils/requestUtils')
 const {checkCountryAccessFromReqParams} = require('../utils/accessControl')
+const auditRepository = require('./../audit/auditRepository')
 
 module.exports.init = app => {
   app.post('/traditionalTable/:countryIso/:tableSpecName', (req, res) => {
     checkCountryAccessFromReqParams(req)
+    db.transaction(auditRepository.insertAudit,
+      [req.user.id, 'save_tradionalTable', req.params.countryIso, req.params.tableSpecName])
     db.transaction(repository.save, [req.params.countryIso, req.params.tableSpecName, req.body])
       .then(result => res.json({}))
       .catch(err => sendErr(res, err))
