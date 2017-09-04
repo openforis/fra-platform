be370bda8cdccb2ae1ca4cb0ed1462685d740f71
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-09-04 17:51:05 +0300

    Make aligning elements in nde headers based on classes rather than wildcards

--- webapp/reusableUiComponents/definitionLink.js
+++ webapp/reusableUiComponents/definitionLink.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './definitionLink.less'
 
-export default ({name, i18n}) => <div className="definition-link"
+export default ({className, name, i18n}) => <div className={`definition-link ${className ? className : ''}`}
                                       onClick={() => window.open(`definitions/en/${name}`, name, 'height=640,width=360')}>
                                         {i18n.t('definition.linkLabel')}
                                   </div>
