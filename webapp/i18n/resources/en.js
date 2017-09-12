d92f63de0af4975808d1191716c8126796c53746
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-12 16:43:03 +0200

    added table skeleton

--- webapp/i18n/resources/en.js
+++ webapp/i18n/resources/en.js
@@ -36,7 +36,10 @@ export const translation = {
     naturallyGenerated: 'Naturally regenerating forest',
     plantationForest: 'Plantation forest',
     otherPlantedForest: 'Other planted forest',
-    introduced: '…of which introduces species'
+    introduced: '…of which introduces species',
+    naturallyRegeneratingForest:'Naturally regenerating forest',
+    plantedForest:'Planted forest',
+    totalForest:'Total forest'
   },
 
   fraOtherLandClass: {
@@ -189,7 +192,8 @@ export const translation = {
   },
 
   growingStock: {
-    growingStock: 'Growing stock'
+    growingStock: 'Growing stock',
+    fra2020Categories:'FRA 2020 categories'
   },
 
   biomassStock: {
