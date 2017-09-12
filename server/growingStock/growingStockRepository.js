08319a968eb6b2c1e185482296de06aca9ec9c20
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-12 18:42:05 +0200

    fetching growing stock data together with foc data

--- server/growingStock/growingStockRepository.js
+++ server/growingStock/growingStockRepository.js
@@ -1,3 +1,22 @@
-/**
- * @author Mino Togna
- */
+const db = require('../db/db')
+const R = require('ramda')
+const camelize = require('camelize')
+
+module.exports.readGrowingStock = countryIso =>
+  db
+    .query(`
+      SELECT
+          year,
+          naturally_regenerating_forest,
+          naturally_regenerating_forest_avg,
+          plantation_forest,
+          plantation_forest_avg,
+          other_planted_forest,
+          other_planted_forest_avg,
+          other_wooded_land,
+          other_wooded_land_avg
+      FROM
+          growing_stock
+      WHERE
+              country_iso = $1` , [countryIso])
+    .then(result => camelize(result.rows))
