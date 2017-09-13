267098095019197a669148067e6afdf62be825bc
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-13 18:05:22 +0200

    persisting calculated fields

--- server/growingStock/growingStockRepository.js
+++ server/growingStock/growingStockRepository.js
@@ -17,11 +17,16 @@ module.exports.readGrowingStock = countryIso =>
           other_planted_forest,
           other_planted_forest_avg,
           other_wooded_land,
-          other_wooded_land_avg
+          other_wooded_land_avg,
+          planted_forest,
+          planted_forest_avg,
+          total_forest,
+          total_forest_avg
       FROM
           growing_stock
       WHERE
-              country_iso = $1`, [countryIso])
+              country_iso = $1
+      ORDER BY year`, [countryIso])
     .then(result => result.rows.map(
       row => ({
         year: Number(row.year),
@@ -33,6 +38,10 @@ module.exports.readGrowingStock = countryIso =>
         otherPlantedForestAvg: toNumberOrNull(row.other_planted_forest_avg),
         otherWoodedLand: toNumberOrNull(row.other_wooded_land),
         otherWoodedLandAvg: toNumberOrNull(row.other_wooded_land_avg),
+        plantedForest: toNumberOrNull(row.planted_forest),
+        plantedForestAvg: toNumberOrNull(row.planted_forest_avg),
+        totalForest: toNumberOrNull(row.total_forest),
+        totalForestAvg: toNumberOrNull(row.total_forest_avg)
       })
     ))
 
@@ -53,12 +62,19 @@ module.exports.persistGrowingStock = (client, user, countryIso, values) =>
               other_planted_forest,
               other_planted_forest_avg,
               other_wooded_land,
-              other_wooded_land_avg
+              other_wooded_land_avg,
+              planted_forest,
+              planted_forest_avg,
+              total_forest,
+              total_forest_avg
+              
           )
-          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`,
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);`,
         [countryIso, value.year,
           value.naturallyRegeneratingForest, value.naturallyRegeneratingForestAvg,
           value.plantationForest, value.plantationForestAvg,
           value.otherPlantedForest, value.otherPlantedForestAvg,
-          value.otherWoodedLand, value.otherWoodedLandAvg])
+          value.otherWoodedLand, value.otherWoodedLandAvg,
+          value.plantedForest, value.plantedForestAvg,
+          value.totalForest, value.totalForestAvg])
     )))
