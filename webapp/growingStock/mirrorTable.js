18e36f5648e8e920e122d8491d65bc676dfb021d
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-13 11:19:49 +0200

    populated table with values

--- webapp/growingStock/mirrorTable.js
+++ webapp/growingStock/mirrorTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as R from 'ramda'
+import { ThousandSeparatedIntegerInput } from '../reusableUiComponents/thousandSeparatedIntegerInput'
 
 class MirrorTable extends Component {
 
@@ -12,19 +13,21 @@ class MirrorTable extends Component {
              colsHeader={this.props.avgTableHeader}
              rows={rows}
              cols={cols}
+             values={this.props.values}
              type='avg'/>
 
       <Table categoriesHeader={this.props.header}
              colsHeader={this.props.totalTableHeader}
              rows={rows}
              cols={cols}
+             values={this.props.values}
              type='total'/>
     </div>
   }
 
 }
 
-const Table = ({categoriesHeader, colsHeader, cols, rows, type}) =>
+const Table = ({categoriesHeader, colsHeader, cols, rows, type, values}) =>
   <div className="nde__data-table-container">
     <div className="nde__data-table-scroll-content">
       <table className="fra-table">
@@ -43,8 +46,55 @@ const Table = ({categoriesHeader, colsHeader, cols, rows, type}) =>
           }
         </tr>
         </thead>
+        <tbody>
+
+        {
+          rows.map((row, i) =>
+            <Row
+              key={i}
+              row={row}
+              cols={cols}
+              type={type}
+              values={values}
+            />)
+        }
+        </tbody>
       </table>
     </div>
   </div>
+
+const Row = ({row, cols, type, values}) =>
+  <tr>
+    <td className="fra-table__header-cell">{row.localizedName}</td>
+    {
+      cols.map((col, i) =>
+        <Cell
+          key={i}
+          col={col}
+          type={type}
+          field={row.field}
+          calculated={row.calculated}
+          values={values}
+        />)
+    }
+  </tr>
+
+const Cell = ({col, type, field, values, calculated}) => {
+  const value = R.pipe(
+    R.find(R.propEq('year', col.year)),
+    R.defaultTo({}),
+    R.prop(`${field}${type === 'avg' ? 'Avg' : ''}`),
+    R.defaultTo(null),
+  )(values)
+
+  return <td className="fra-table__cell">
+    <ThousandSeparatedIntegerInput
+      className="fra-table__integer-input"
+      integerValue={value}
+      disabled={calculated}
+      onChange={ e => console.log('-- e ' , e ) }
+    />
+  </td>
+}
 
 export default MirrorTable
