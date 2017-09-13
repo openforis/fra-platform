4949af2073b808f375603db272f4adbe0046484f
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-13 11:19:49 +0200

    populated table with values

--- server/growingStock/growingStockRepository.js
+++ server/growingStock/growingStockRepository.js
@@ -1,6 +1,7 @@
 const db = require('../db/db')
 const R = require('ramda')
 const camelize = require('camelize')
+const {toNumberOrNull} = require('../utils/databaseConversions')
 
 module.exports.readGrowingStock = countryIso =>
   db
@@ -18,5 +19,17 @@ module.exports.readGrowingStock = countryIso =>
       FROM
           growing_stock
       WHERE
-              country_iso = $1` , [countryIso])
-    .then(result => camelize(result.rows))
+              country_iso = $1`, [countryIso])
+    .then(result => result.rows.map(
+      row => ({
+        year: Number(row.year),
+        naturallyRegeneratingForest: toNumberOrNull(row.naturally_regenerating_forest),
+        naturallyRegeneratingForestAvg: toNumberOrNull(row.naturally_regenerating_forest_avg),
+        plantationForest: toNumberOrNull(row.plantation_forest),
+        plantationForestAvg: toNumberOrNull(row.plantation_forest_avg),
+        otherPlantedForest: toNumberOrNull(row.other_planted_forest),
+        otherPlantedForestAvg: toNumberOrNull(row.other_planted_forest_avg),
+        otherWoodedLand: toNumberOrNull(row.other_wooded_land),
+        otherWoodedLandAvg: toNumberOrNull(row.other_wooded_land_avg),
+      })
+    ))
