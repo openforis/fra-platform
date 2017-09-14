360d6eb229e2988f3e7291f1c58ce74a4c6dcf4d
Perttu Auramo <perttu.auramo@reaktor.com>
2017-09-14 16:31:51 +0300

    Use correct import and fix issue with import

--- webapp/traditionalTable/cellTypes.js
+++ webapp/traditionalTable/cellTypes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import assert from 'assert'
 import IntegerCellType from './integerCellType'
-import StringCellType from './integerCellType'
+import StringCellType from './stringCellType'
 
 const cellTypeCreators = {
   'integerInput': IntegerCellType,
--- webapp/traditionalTable/stringCellType.js
+++ webapp/traditionalTable/stringCellType.js
@@ -1,3 +1,4 @@
+import React from 'react'
 
 const StringInput = ({countryIso, tableSpec, tableData, rowIdx, colIdx, tableValueChanged, tableChanged}) => {
   const currentValue = tableData[rowIdx][colIdx]
