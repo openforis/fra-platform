6772c8ba17a971d6bbaa8b050f769e68b73b5a3e
Perttu Auramo <perttu.auramo@reaktor.com>
2017-09-20 12:18:25 +0300

    Fix import
    For some reason, in master this is no longer default export :(

--- webapp/growingStockComposition/growingStockCompositionView.js
+++ webapp/growingStockComposition/growingStockCompositionView.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
-import CommentableDescriptions from '../description/commentableDescription'
+import { CommentableDescriptions } from '../description/commentableDescription'
 
 const GrowingStockCompositionView = ({match, i18n}) => {
   const tableProps = tableSpec(i18n)
