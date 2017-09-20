52657c9d8b48ce2319d007b2e8fee53889e73d72
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-09-20 17:30:23 +0300

    Render anchor tags in markdown for h1 & h2

--- server/definitions/api.js
+++ server/definitions/api.js
@@ -14,9 +14,21 @@ module.exports.init = app => {
     try {
       const lang = readParameterWithAllowedValues(req, 'lang', ['en', 'es', 'fr', 'ru'])
       const name = readAllowedParameter(req, 'name', R.match(/^[a-z0-9]+$/i))
-
       const mdRes = getDefinition(name, lang)
       mdRes.then(markdown => {
+        var renderer = new marked.Renderer()
+        renderer.heading = function(text, level) {
+          if (level < 3) {
+            var escapedText = text.toLowerCase().substr(0, text.indexOf(' '))
+            return '<h' + level + ' id="' + escapedText + '" class="anchor-link">' + text + '</h' + level + '>'
+          } else {
+            return '<h' + level + '>' + text + '</h' + level + '>'
+          }
+        }
+        marked.setOptions({
+          renderer: renderer,
+          smartypants: true
+        })
         const content = markdown ? marked(markdown) : ''
         res.send(`<html lang="fi">
           <head>
