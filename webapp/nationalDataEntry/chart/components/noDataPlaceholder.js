4611c4ac7066e4f613c19bfd65527314a55acd51
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-09-22 14:24:07 +0300

    Refactor placeholder animation
    The tucan should only animate when adding data to table, otherwise a simple show/hide will do. Also made constant out of some methods and renamed others to be more descriptive.

--- webapp/nationalDataEntry/chart/components/noDataPlaceholder.js
+++ webapp/nationalDataEntry/chart/components/noDataPlaceholder.js
@@ -5,159 +5,103 @@ import {hasData} from '../chart'
 
 const Text = ({text, width, y}) =>
   <foreignObject data-y={y} width={width} y={y} style={{textAlign: 'center'}}>
-    <text style={{fontSize: '14px', color: '#999999', opacity: 0}} x="0" y="0">{text}</text>
+    <text style={{fontSize: '14px', color: '#999999'}} x="0" y="0">{text}</text>
   </foreignObject>
+
+const tucanY = 20
+const tucanWidth = 62
+const tucanHeight = 87
 
 class NoDataPlaceholder extends React.Component {
 
   getTucanX() {
-    const {wrapperWidth} = this.props
-    return (wrapperWidth - 62) / 2
-  }
-
-  getTucanY() {
-    return 27
-  }
-
-  getTucanWidth() {
-    return 62
-  }
-
-  getTucanHeight() {
-    return 87
+    return (this.props.wrapperWidth - tucanWidth) / 2
   }
 
   tucan() {
     return d3.select(this.refs.tucan)
   }
 
-  textWrappers() {
-    return d3.select(this.refs.container).selectAll('foreignObject')
-  }
-
   texts() {
-    return this.textWrappers().selectAll('text')
+    let textWrappers = d3.select(this.refs.container).selectAll('foreignObject')
+    return textWrappers.selectAll('text')
   }
 
-  tucanShake(from, to, duration = 100, delay = 0) {
-    const x = R.add(this.getTucanX(), R.divide(this.getTucanHeight(), 2))
-    const y = R.add(this.getTucanY(), R.divide(this.getTucanHeight(), 2))
-    return this.tucan()
-      .transition()
-      .duration(duration)
-      .delay(delay)
-      .tween("attr:transform", (d, i, nodes) => {
-        const interpolateString = d3.interpolateString(`rotate(${from},${x},${y})`, `rotate(${to},${x},${y})`)
-        return (t) => this.tucan().attr("transform", interpolateString(t))
-      })
-  }
-
-  tucanEnter() {
+  hidePlaceholderAnimated() {
     this.tucan()
-      .attr('y', '-100')
-      .style('opacity', '0')
       .transition()
-      .duration(250)
-      .ease(d3.easeBackOut)
-      .attr('y', this.getTucanY())
-      .style('opacity', '1')
-
-    let delay = 250
-    this.tucanShake(0, 3, 100, delay)
-    this.tucanShake(3, -1, 150, delay += 100)
-    this.tucanShake(-1, 0, 100, delay += 150)
+        .duration(400)
+        .delay(100)
+        .ease(d3.easeBackInOut)
+        .attr('y', -tucanHeight)
+        .style('opacity', '0')
+      .transition()
+        .style('visibility', 'hidden')
+    this.texts()
+      .transition()
+        .duration(400)
+        .delay(200)
+        .ease(d3.easeExpOut)
+        .style('opacity', '0')
+      .transition()
+        .style('visibility', 'hidden')
   }
 
-  tucanExit() {
-    this.tucanShake(0, 5, 100)
-    this.tucanShake(1, -3, 50, 100)
-
+  hidePlaceholder() {
     this.tucan()
-      .style('opacity', '1')
       .transition()
-      .duration(500)
-      .delay(300)
-      .ease(d3.easeExpOut)
-      .attr('y', '-100')
-      .style('opacity', '0')
-  }
-
-  enter() {
-    this.tucanEnter()
-
-    this.texts()
-      .style('opacity', '0')
-      .transition()
-      .duration(300)
-      .delay(300)
-      .ease(d3.easeCircleOut)
-      .style('opacity', '1')
-
-    this.textWrappers()
-      .attr('y', (d, i, nodes) => nodes[i].getAttribute('data-y') - 100)
-      .transition()
-      .duration(500)
-      .ease(d3.easePolyOut)
-      .attr('y', (d, i, nodes) => nodes[i].getAttribute('data-y'))
-  }
-
-  exitDidMount() {
-    this.tucanExit()
-
+        .duration(100)
+        .style('visibility', 'hidden')
+        .style('opacity', '0')
     this.texts()
       .transition()
-      .duration(0)
-      .style('opacity', '0')
+        .duration(100)
+        .style('visibility', 'hidden')
+        .style('opacity', '0')
   }
 
-  exit() {
-    this.tucanExit()
-
+  showPlaceholder() {
+    this.tucan()
+      .attr('y', tucanY)
+      .transition()
+        .duration(100)
+        .style('visibility', 'visible')
+        .style('opacity', '1')
     this.texts()
       .transition()
-      .duration(0)
-      .attr('y', (d, i, nodes) => nodes[i].getAttribute('data-y') - 100)
-      .style('opacity', '0')
+        .duration(100)
+        .style('visibility', 'visible')
+        .style('opacity', '1')
   }
 
   componentDidMount() {
-    const {data} = this.props
-
-    hasData(data) ? this.exitDidMount() : this.enter()
+    hasData(this.props.data) ? this.hidePlaceholder() : this.showPlaceholder()
   }
 
   componentWillReceiveProps(nextProps) {
-    const {data} = this.props
-    const propsHasData = hasData(data)
+    const propsHasData = hasData(this.props.data)
     const nextPropsHasData = nextProps ? hasData(nextProps.data) : false
 
     if (propsHasData && !nextPropsHasData) {
-      this.enter()
+      this.showPlaceholder()
     } else if (!propsHasData && nextPropsHasData) {
-      this.exit()
+      this.hidePlaceholderAnimated()
     }
   }
 
   componentWillUnmount() {
-    this.tucanExit()
+    this.hidePlaceholder()
   }
 
   render() {
     const {wrapperWidth, i18n} = this.props
 
     return <g className="chart__no-data-placeholder" ref="container">
-      <image ref="tucan" href="/img/tucan.svg"
-             width={this.getTucanWidth()} height={this.getTucanHeight()}
-             x={this.getTucanX()} y={-100}
-             style={{
-               opacity: 0
-             }}
-      />
-      <Text text={i18n.t('extentOfForest.chart.placeholderLine1')} width={wrapperWidth} y="150"/>
-      <Text text={i18n.t('extentOfForest.chart.placeholderLine2')} width={wrapperWidth} y="170"/>
+      <image ref="tucan" href="/img/tucan.svg" width={tucanWidth} height={tucanHeight} x={this.getTucanX()} y={tucanY} />
+      <Text text={i18n.t('extentOfForest.chart.placeholderLine1')} width={wrapperWidth} y="144"/>
+      <Text text={i18n.t('extentOfForest.chart.placeholderLine2')} width={wrapperWidth} y="164"/>
     </g>
   }
-
 }
 
 export default NoDataPlaceholder
