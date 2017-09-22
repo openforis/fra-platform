7209a18a30049eecfc5e8518bb42ac7c7e300dba
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-09-22 10:27:51 +0300

    Add definitions links to growing stock composition

--- webapp/growingStockComposition/growingStockCompositionView.js
+++ webapp/growingStockComposition/growingStockCompositionView.js
@@ -6,13 +6,18 @@ import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
 import { CommentableDescriptions } from '../description/commentableDescription'
+import DefinitionLink from './../reusableUiComponents/definitionLink'
 
 const GrowingStockCompositionView = ({match, i18n}) => {
   const tableProps = tableSpec(i18n)
 
   return <LoggedInPageTemplate>
     <div className="tv__container growing-stock__container">
-      <h1 className="title tv__page-header">{i18n.t('growingStockComposition.growingStockComposition')}</h1>
+      <div className="tv__header-row">
+        <h1 className="title">{i18n.t('growingStockComposition.growingStockComposition')}</h1>
+        <DefinitionLink document="tad" section="3b" title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
+        <DefinitionLink document="faq" section="3b" title={i18n.t('definition.faqLabel')} lang={i18n.language} className="align-left"/>
+      </div>
       <TraditionalTable tableSpec={tableProps} countryIso={match.params.countryIso}/>
       <CommentableDescriptions
         section={tableProps.name}
