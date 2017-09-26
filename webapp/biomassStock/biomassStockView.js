3af0fb2c07b70114aeeecef23792f345520fc160
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-26 17:41:21 +0200

    added definition links

--- webapp/biomassStock/biomassStockView.js
+++ webapp/biomassStock/biomassStockView.js
@@ -6,6 +6,7 @@ import LoggedInPageTemplate from '../loggedInPageTemplate'
 import TraditionalTable from '../traditionalTable/traditionalTable'
 import { avgTableSpec, totalTableSpec } from './tablesSpec'
 import { CommentableDescriptions } from '../description/commentableDescription'
+import DefinitionLink from './../reusableUiComponents/definitionLink'
 
 const BiomassStockView = ({match, i18n}) => {
   const avgTableProps = avgTableSpec(i18n)
@@ -13,7 +14,11 @@ const BiomassStockView = ({match, i18n}) => {
 
   return <LoggedInPageTemplate>
     <div className="tv__container">
-      <h1 className="title tv__page-header">{i18n.t('biomassStock.biomassStock')}</h1>
+      <div className="tv__page-header">
+        <h1 className="title">{i18n.t('biomassStock.biomassStock')}</h1>
+        <DefinitionLink document="tad" section="3c" title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
+        <DefinitionLink document="faq" section="3c" title={i18n.t('definition.faqLabel')} lang={i18n.language} className="align-left"/>
+      </div>
       <div className="biomass-stock__data-table-container">
         <TraditionalTable tableSpec={avgTableProps} countryIso={match.params.countryIso}/>
       </div>
