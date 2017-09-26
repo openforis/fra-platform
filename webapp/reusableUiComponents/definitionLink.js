64c645cb197aaf5622d77c3c15cac6b5aae6f790
Perttu Auramo <perttu.auramo@reaktor.com>
2017-09-26 17:08:18 +0300

    Extract common stuff from degraded forest view and use it

--- webapp/reusableUiComponents/definitionLink.js
+++ webapp/reusableUiComponents/definitionLink.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './definitionLink.less'
 
-export default ({className, document, section, title, lang}) =>
+export default ({className, document, anchor, title, lang}) =>
 <div className={`definition-link ${className ? className : ''}`}
-     onClick={() => window.open(`definitions/${lang}/${document}${section ? '#'+section : ''}`, document, 'height=640,width=360')}>
+     onClick={() => window.open(`definitions/${lang}/${document}${anchor ? '#'+anchor : ''}`, document, 'height=640,width=360')}>
      {title}
 </div>
