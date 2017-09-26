827a16c3818e4f6cd8c43b7cc1d619f1a690fff9
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-26 11:58:38 +0200

    created table biomass_stock

---
+++ server/db/migration/migrations/sqls/20170926095450-create-biomass-stock-table-up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE
+  biomass_stock
+(
+  country_iso VARCHAR(3) NOT NULL,
+  row_name VARCHAR NOT NULL,
+  "1990" NUMERIC,
+  "2000" NUMERIC,
+  "2010" NUMERIC,
+  "2015" NUMERIC,
+  "2016" NUMERIC,
+  "2017" NUMERIC,
+  "2018" NUMERIC,
+  "2019" NUMERIC,
+  "2020" NUMERIC,
+  PRIMARY KEY (country_iso, row_name),
+  CONSTRAINT biomassstock_country_fk FOREIGN KEY (country_iso) REFERENCES
+    country ("country_iso")
+)
