bffeebc52459bcb3d6afdbc5345b925031f786fe
Mino Togna <Cosimo.Togna@Fao.org>
2017-09-28 15:44:47 +0200

    added carbonStockView

--- webapp/carbonStock/carbonStockView.js
+++ webapp/carbonStock/carbonStockView.js
@@ -7,10 +7,9 @@ import { avgTableSpec, totalTableSpec } from './tablesSpec'
 import { CommentableDescriptions } from '../description/commentableDescription'
 import DefinitionLink from './../reusableUiComponents/definitionLink'
 
-const BiomassStockView = ({match, i18n}) => {
+const CarbonStockView = ({match, i18n}) => {
   const countryIso = match.params.countryIso
   const lang = i18n.language
-  const calculatorFilePath = `/api/biomassStock/${countryIso}/${lang}/download`
 
   const avgTableProps = avgTableSpec(i18n)
   const totalTableProps = totalTableSpec(i18n)
@@ -18,13 +17,9 @@ const BiomassStockView = ({match, i18n}) => {
   return <LoggedInPageTemplate>
     <div className="tv__container">
       <div className="tv__page-header">
-        <h1 className="title">{i18n.t('biomassStock.biomassStock')}</h1>
-        <DefinitionLink document="tad" section="3c" title={i18n.t('definition.definitionLabel')} lang={lang}/>
-        <DefinitionLink document="faq" section="3c" title={i18n.t('definition.faqLabel')} lang={lang} className="align-left"/>
-        <a className="btn btn-primary" href={calculatorFilePath}>
-          <svg className="icon icon-sub icon-white"><use xlinkHref="img/icons.svg#hit-down"/></svg>
-          {i18n.t('biomassStock.downloadExcel')}
-        </a>
+        <h1 className="title">{i18n.t('carbonStock.carbonStock')}</h1>
+        <DefinitionLink document="tad" section="3d" title={i18n.t('definition.definitionLabel')} lang={lang}/>
+        <DefinitionLink document="faq" section="3d" title={i18n.t('definition.faqLabel')} lang={lang} className="align-left"/>
       </div>
 
       <div className="traditional-table-outer-container">
@@ -35,8 +30,8 @@ const BiomassStockView = ({match, i18n}) => {
       </div>
 
       <CommentableDescriptions
-        section="biomassStock"
-        name="biomassStock"
+        section="carbonStock"
+        name="carbonStock"
         countryIso={countryIso}
         i18n={i18n}
       />
@@ -46,4 +41,4 @@ const BiomassStockView = ({match, i18n}) => {
 
 const mapStateToProps = state => ({i18n: state.user.i18n})
 
-export default connect(mapStateToProps)(BiomassStockView)
+export default connect(mapStateToProps)(CarbonStockView)
