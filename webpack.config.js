3b8324b3ef99eb58ba8e5be059be412e6cbf1074
Mino Togna <Cosimo.Togna@Fao.org>
2017-10-05 15:44:05 +0200

    added __LAST_COMMIT__ global variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,72 @@
-var path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
+var path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
 
 const prodBuild = process.env.NODE_ENV === 'production'
 
-const jsBundleName = 'bundle-[hash].js';
-const cssBundleName = 'styles-[hash].css';
+const jsBundleName = 'bundle-[hash].js'
+const cssBundleName = 'styles-[hash].css'
+
+const childProcess = require('child_process')
+const lastCommit = childProcess.execSync('git log -1 --pretty=format:"%h"')
 
 const alwaysInUseplugins = [
-    new ExtractTextPlugin({filename: cssBundleName}),
-    new HtmlWebpackPlugin({template: './web-resources/index.html'})
-];
+  new ExtractTextPlugin({filename: cssBundleName}),
+  new HtmlWebpackPlugin({template: './web-resources/index.html'}),
+  new webpack.DefinePlugin({__LAST_COMMIT__: `"${lastCommit}"`})
+]
 
 const uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
-    compress: {warnings: false},
-    output: {comments: false},
-    sourceMap: true
-});
+  compress: {warnings: false},
+  output: {comments: false},
+  sourceMap: true
+})
 
-const plugins = prodBuild ? [...alwaysInUseplugins, uglifyPlugin] : alwaysInUseplugins;
+const plugins = prodBuild ? [...alwaysInUseplugins, uglifyPlugin] : alwaysInUseplugins
 
-const webPackConfig = {
-    entry: './webapp/main.js',
-    output: {
-        filename: jsBundleName,
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            /*
-              Minifying (production build) of partial lenses breaks without the below rule
-              with current webpack version. According to developer of partial.lenses, webpack 3.5.6
-              should have fixed this. Quick test with the new webpack version broke the svgo module though,
-              so webpack has not yet been upgraded and we need this rule.
-             */
-            {
-              test: /partial\.lenses\.es\.js$/,
-              loader: 'babel-loader',
-              query: {plugins: ["transform-es2015-modules-commonjs"]}
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react'],
-                        plugins: [require('babel-plugin-transform-object-rest-spread')]
-                    }
-                }
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'less-loader']
-                })
-            }
-        ]
-    },
-    plugins: plugins
-};
+const webPackConfig = {
+  entry: './webapp/main.js',
+  output: {
+    filename: jsBundleName,
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      /*
+        Minifying (production build) of partial lenses breaks without the below rule
+        with current webpack version. According to developer of partial.lenses, webpack 3.5.6
+        should have fixed this. Quick test with the new webpack version broke the svgo module though,
+        so webpack has not yet been upgraded and we need this rule.
+       */
+      {
+        test: /partial\.lenses\.es\.js$/,
+        loader: 'babel-loader',
+        query: {plugins: ['transform-es2015-modules-commonjs']}
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')]
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      }
+    ]
+  },
+  plugins: plugins
+}
 
-webPackConfig.devtool = 'source-map';
+webPackConfig.devtool = 'source-map'
 
-module.exports = webPackConfig;
+module.exports = webPackConfig
