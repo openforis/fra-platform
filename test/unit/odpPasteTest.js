3151a2343008c59719ecccef9df1c2a0e0adb566
Perttu Auramo <perttu.auramo@reaktor.com>
2017-10-10 13:59:43 +0300

    First test

--- test/unit/odpPasteTest.js
+++ test/unit/odpPasteTest.js
@@ -1,17 +1,24 @@
+const R = require('ramda')
 const assert = require('chai').assert
-const handlePaste = require('../../webapp/originalDataPoint/paste')
+const handlePaste = require('../../webapp/originalDataPoint/paste').default
 
-const emptyOdp = {nationalClasses: []}
+const emptyOdp = {nationalClasses: [{placeHolder: true}]}
 const ncColumns = [{name: 'className', type: 'text'}, {name: 'definition', type: 'text'}]
 
 describe('odp paste', () => {
   it('Pastes national classes and definitions', () => {
-    const expected = {
-      nationalClasses: [
-        {className: 'Closed Forest', definition: 'Closed def'},
+    const expected =
+      [
+        {className: 'Closed forest', definition: 'Closed def'},
         {className: 'Open forest', definition: 'Open def'}
       ]
-    }
-    //assert.deepEqual(expected, handlePaste(ncColumns, ))
+
+    const pasteData = [['Closed forest', 'Closed def'], ['Open forest', 'Open def']]
+    const pasteResult = handlePaste(ncColumns, () => true, emptyOdp, true, pasteData, 0, 0)
+    const nationalClassInfo = R.map(
+      nc => R.pick(['className', 'definition'], nc),
+      R.take(2, pasteResult.updatedOdp.nationalClasses)
+    )
+    assert.deepEqual(expected, nationalClassInfo)
   })
 })
