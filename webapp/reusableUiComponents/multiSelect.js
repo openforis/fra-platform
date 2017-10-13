df5997318b90e9853ec06e618359baec00abaaa6
Perttu Auramo <perttu.auramo@reaktor.com>
2017-10-13 16:15:25 +0300

    Fix positioning of opened list
    narrowing window will not bring it higher now at all

--- webapp/reusableUiComponents/multiSelect.js
+++ webapp/reusableUiComponents/multiSelect.js
@@ -5,7 +5,7 @@ import R from 'ramda'
 const optionClick = (currentValues, onChange, option) => (evt) => {
   evt.stopPropagation()
   if (R.contains(option, currentValues)) {
-    onChange(R.reject(R.equals(option),  currentValues))
+    onChange(R.reject(R.equals(option), currentValues))
   } else {
     onChange([...currentValues, option])
   }
@@ -29,11 +29,11 @@ export default class MultiSelect extends React.Component {
     window.removeEventListener('click', this.outsideClick)
   }
 
-  toggleOpen() {
+  toggleOpen () {
     this.setState({open: !this.state.open})
   }
 
-  localizeOption(option) {
+  localizeOption (option) {
     return this.props.i18n.t(this.props.localizationPrefix + '.' + option)
   }
 
@@ -46,37 +46,39 @@ export default class MultiSelect extends React.Component {
       <div className="multi-select__closed-content">
         {
           R.isEmpty(values)
-          ? <span className="multi-select__placeholder">{this.props.i18n.t('multiSelect.placeHolderWhenEmpty')}</span>
-          : R.pipe(
-              R.map(option => this.localizeOption(option)),
-              R.join(', ')
-            )(values)
+            ? <span className="multi-select__placeholder">{this.props.i18n.t('multiSelect.placeHolderWhenEmpty')}</span>
+            : R.pipe(
+            R.map(option => this.localizeOption(option)),
+            R.join(', ')
+          )(values)
         }
       </div>
-      <div className="multi-select__arrow">
+      <div className="multi-select__right-column">
+        <div className=".multi-select__right-column-filler"/>
         <svg className="icon icon-sub">
           <use xlinkHref="img/icons.svg#small-down"/>
         </svg>
-        {
-          this.state.open
-            ? <div className="multi-select__opened-content-anchor">
-            <div className="multi-select__opened" style={{width: this.props.openedListWidth}}>
-              {
-                R.map(
-                  option =>
-                    <div className="multi-select__opened-item"
-                         key={option}
-                         onClick={optionClick(values, this.props.onChange, option)}>
-                      <input type="checkbox" readOnly="true" name={option} value={option} checked={R.contains(option, values)}/>
-                      <span className="multi-select__opened-option-text">{this.localizeOption(option)}</span>
-                    </div>,
-                  this.props.options
-                )
-              }
-            </div>
-          </div>
-            : null
-        }
+        <div className="multi-select__opened-content-anchor">
+          {
+            this.state.open
+              ? <div className="multi-select__opened" style={{width: this.props.openedListWidth}}>
+                  {
+                    R.map(
+                      option =>
+                        <div className="multi-select__opened-item"
+                             key={option}
+                             onClick={optionClick(values, this.props.onChange, option)}>
+                          <input type="checkbox" readOnly="true" name={option} value={option}
+                                 checked={R.contains(option, values)}/>
+                          <span className="multi-select__opened-option-text">{this.localizeOption(option)}</span>
+                        </div>,
+                      this.props.options
+                    )
+                  }
+                </div>
+              : null
+          }
+        </div>
       </div>
     </div>
   }
