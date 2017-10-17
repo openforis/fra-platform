e1516c2affb69aba3a4b106a5385dcaf08d6ae72
Perttu Auramo <perttu.auramo@reaktor.com>
2017-10-17 13:46:58 +0300

    Add audit API path which returns the latest audit log

--- server/audit/auditRepository.js
+++ server/audit/auditRepository.js
@@ -1,4 +1,3 @@
-const camelize = require('camelize')
 const db = require('../db/db')
 const R = require('ramda')
 
@@ -7,4 +6,19 @@ module.exports.insertAudit = (client, userId, message, countryIso, section, targ
     'INSERT INTO fra_audit (user_id, message, country_iso, section, target) VALUES ($1, $2, $3, $4, $5);',
     [userId, message, countryIso, section, target]
   )
+}
+
+module.exports.getLastAuditTimeStampForSection = (countryIso, section) => {
+  const excludedMsgs = ['createIssue', 'createComment', 'deleteComment']
+  return db.query(
+    ` SELECT
+        split_part(section, '_', 1) as section_name,
+        to_char(max(time), 'YYYY-MM-DD"T"HH24:MI:ssZ') as latest_edit
+      FROM fra_audit
+      WHERE country_iso = $1
+            AND section = $2
+            AND NOT (message in ($3))
+      GROUP BY section_name
+    `, [countryIso, section, excludedMsgs]
+  ).then(res => R.path(['rows', 0, 'latest_edit'], res))
 }
