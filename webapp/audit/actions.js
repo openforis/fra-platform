0fda294f56514fb30f8d31d87a3afe86b356e05f
Perttu Auramo <perttu.auramo@reaktor.com>
2017-10-18 14:39:57 +0300

    Fix "section not yet edited" -case

--- webapp/audit/actions.js
+++ webapp/audit/actions.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
+import R from 'ramda'
 import { applicationError } from '../applicationError/actions'
 
 export const lastSectionUpdateTimestampReceived = 'audit/lastSectionUpdateTimestampReceived'
 
 export const fetchLastSectionUpdateTimestamp = (countryIso, section) => dispatch => {
   axios.get(`/api/audit/getLatestAuditLogTimestamp/${countryIso}?section=${section}`)
-    .then(resp => dispatch({type: lastSectionUpdateTimestampReceived, timeStamp: resp.data.timeStamp}))
+    .then(resp => {
+      if (!R.isNil(resp.data.timeStamp)) { //There might not be any changes yet for the section
+        dispatch({type: lastSectionUpdateTimestampReceived, timeStamp: resp.data.timeStamp})
+      }
+    })
     .catch(err => dispatch(applicationError(err)))
 }
