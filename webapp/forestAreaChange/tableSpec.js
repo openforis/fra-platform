8245f4f9bf5aed345cc8d305f35a6fdb520d9634
Perttu Auramo <perttu.auramo@gmail.com>
2017-10-24 16:52:56 +0300

    Remove the console.log

--- webapp/forestAreaChange/tableSpec.js
+++ webapp/forestAreaChange/tableSpec.js
@@ -16,48 +16,50 @@ const netChangeCell = (column) => (props) =>
     {netChange(props.tableData[0][column], props.tableData[3][column])}
   </td>
 
-export default (i18n, extentOfForest) => console.log('fac change eof', extentOfForest) || ({
-  name: 'forestAreaChange', // used to uniquely identify table
-  header: <thead>
-  <tr>
-    <th className="fra-table__header-cell" rowSpan="2">{i18n.t('forestAreaChange.categoryHeader')}</th>
-    <th className="fra-table__header-cell-middle" colSpan="4">{i18n.t('forestAreaChange.areaUnitLabel')}</th>
-  </tr>
-  <tr>
-    <td className="fra-table__header-cell-right">1990-2000</td>
-    <td className="fra-table__header-cell-right">2000-2010</td>
-    <td className="fra-table__header-cell-right">2010-2015</td>
-    <td className="fra-table__header-cell-right">2015-2020</td>
-  </tr>
-  </thead>,
-  rows: [
-    [{type: 'readOnly', jsx: <td key="expansion" className="fra-table__header-cell">{i18n.t('forestAreaChange.forestExpansion')}</td>},
-      ...integerInputColumns
+export default (i18n, extentOfForest) => {
+  return {
+    name: 'forestAreaChange', // used to uniquely identify table
+    header: <thead>
+    <tr>
+      <th className="fra-table__header-cell" rowSpan="2">{i18n.t('forestAreaChange.categoryHeader')}</th>
+      <th className="fra-table__header-cell-middle" colSpan="4">{i18n.t('forestAreaChange.areaUnitLabel')}</th>
+    </tr>
+    <tr>
+      <td className="fra-table__header-cell-right">1990-2000</td>
+      <td className="fra-table__header-cell-right">2000-2010</td>
+      <td className="fra-table__header-cell-right">2010-2015</td>
+      <td className="fra-table__header-cell-right">2015-2020</td>
+    </tr>
+    </thead>,
+    rows: [
+      [{type: 'readOnly', jsx: <td key="expansion" className="fra-table__header-cell">{i18n.t('forestAreaChange.forestExpansion')}</td>},
+        ...integerInputColumns
+      ],
+      [
+        {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell-sub">{i18n.t('forestAreaChange.ofWhichAfforestation')}</td>},
+        ...ofWhichColumns
+      ],
+      [
+        {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell-sub">{i18n.t('forestAreaChange.ofWhichNaturalExpansion')}</td>},
+        ...ofWhichColumns
+      ],
+      [
+        {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell">{i18n.t('forestAreaChange.deforestation')}</td>},
+        ...integerInputColumns
+      ],
+      [
+        {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell">{i18n.t('forestAreaChange.forestAreaNetChange')}</td>},
+        {type: 'custom', render: netChangeCell(1)},
+        {type: 'custom', render: netChangeCell(2)},
+        {type: 'custom', render: netChangeCell(3)},
+        {type: 'custom', render: netChangeCell(4)}
+      ]
     ],
-    [
-      {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell-sub">{i18n.t('forestAreaChange.ofWhichAfforestation')}</td>},
-      ...ofWhichColumns
-    ],
-    [
-      {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell-sub">{i18n.t('forestAreaChange.ofWhichNaturalExpansion')}</td>},
-      ...ofWhichColumns
-    ],
-    [
-      {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell">{i18n.t('forestAreaChange.deforestation')}</td>},
-      ...integerInputColumns
-    ],
-    [
-      {type: 'readOnly', jsx: <td key="" className="fra-table__header-cell">{i18n.t('forestAreaChange.forestAreaNetChange')}</td>},
-      {type: 'custom', render: netChangeCell(1)},
-      {type: 'custom', render: netChangeCell(2)},
-      {type: 'custom', render: netChangeCell(3)},
-      {type: 'custom', render: netChangeCell(4)}
-    ]
-  ],
-  valueSlice: {
-    rowStart: 0,
-    rowEnd: -1,
-    columnStart: 1,
-    columnEnd: undefined
+    valueSlice: {
+      rowStart: 0,
+      rowEnd: -1,
+      columnStart: 1,
+      columnEnd: undefined
+    }
   }
-})
+}
