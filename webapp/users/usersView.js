49fae2d50d09f572e2a5412bd0797d6dc809854a
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-10-30 14:41:34 +0200

    Style and markup changes

--- webapp/users/usersView.js
+++ webapp/users/usersView.js
@@ -13,10 +13,10 @@ import { fetchUsers, updateUser, removeUser, updateNewUser, addNewUser } from '.
 import { validationField } from './users'
 
 const AddUserForm = ({countryIso, i18n, user, updateNewUser, addNewUser}) =>
-  <div className="users__add-user-container">
-    <table className="users__add-user-table">
+  <div className="add-user__container">
+    <table className="add-user__table">
       <tbody>
-      <tr className="users__add-user-table-row">
+      <tr>
         <UserTextFieldCol
           countryIso={countryIso} i18n={i18n} user={user} field="name" updateUser={updateNewUser}/>
         <UserRoleSelectCol
@@ -37,26 +37,27 @@ const AddUserForm = ({countryIso, i18n, user, updateNewUser, addNewUser}) =>
   </div>
 
 const UsersTable = ({countryIso, i18n, users, updateUser, removeUser}) =>
-  <table className="users__list-table">
+  <table className="users-list__table">
     <thead>
-    <tr className='users__list-table-header-row'>
-      <th>{i18n.t('users.name')}</th>
-      <th>{i18n.t('users.role')}</th>
-      <th>{i18n.t('users.email')}</th>
-      <th>{i18n.t('users.loginEmail')}</th>
-      <th></th>
-    </tr>
+      <tr>
+        <th className="users-list__header-cell">{i18n.t('users.name')}</th>
+        <th className="users-list__header-cell">{i18n.t('users.role')}</th>
+        <th className="users-list__header-cell">{i18n.t('users.email')}</th>
+        <th className="users-list__header-cell">{i18n.t('users.loginEmail')}</th>
+        <th className="users-list__header-cell"></th>
+      </tr>
     </thead>
     <tbody>
     {
       users.length > 0
         ? users.map(user =>
-          <UserRow key={user.id} countryIso={countryIso} i18n={i18n} user={user}
-                   updateUser={updateUser} removeUser={removeUser}/>
-        )
-        : <tr className="users__list-table-row">
-          <td className="users__list-table-empty" colSpan="5">{i18n.t('users.noUsers')}</td>
-        </tr>
+            <UserRow key={user.id} countryIso={countryIso} i18n={i18n} user={user} updateUser={updateUser} removeUser={removeUser}/>
+          )
+        : <tr>
+            <td className="users-list__read-only-cell" colSpan="5">
+              {i18n.t('users.noUsers')}
+            </td>
+          </tr>
     }
     </tbody>
   </table>
@@ -64,54 +65,57 @@ const UsersTable = ({countryIso, i18n, users, updateUser, removeUser}) =>
 const UserRow = ({countryIso, i18n, user, updateUser, removeUser}) => {
   const readOnly = R.endsWith('ALL', user.role)
 
-  return <tr className={`users__list-table-row${readOnly ? ' read-only' : ''}`}>
-    <UserTextFieldCol
-      countryIso={countryIso} i18n={i18n} user={user} field="name" readOnly={readOnly} updateUser={updateUser}/>
-    <UserRoleSelectCol
-      countryIso={countryIso} i18n={i18n} user={user} readOnly={readOnly} updateUser={updateUser}/>
-    <UserTextFieldCol
-      countryIso={countryIso} i18n={i18n} user={user} field="email" readOnly={readOnly} updateUser={updateUser}/>
-    <UserTextFieldCol
-      countryIso={countryIso} i18n={i18n} user={user} field="loginEmail" readOnly={true} updateUser={updateUser}/>
-    <td>{
+  return <tr>
+    <UserTextFieldCol countryIso={countryIso} i18n={i18n} user={user} field="name" readOnly={readOnly} updateUser={updateUser}/>
+    <UserRoleSelectCol countryIso={countryIso} i18n={i18n} user={user} readOnly={readOnly} updateUser={updateUser}/>
+    <UserTextFieldCol countryIso={countryIso} i18n={i18n} user={user} field="email" readOnly={readOnly} updateUser={updateUser}/>
+    <UserTextFieldCol countryIso={countryIso} i18n={i18n} user={user} field="loginEmail" readOnly={true} updateUser={updateUser}/>
+    <td className="users-list__cell">
+    {
       readOnly
         ? null
-        : <span className="btn btn-destructive"
-                onClick={() => window.confirm(i18n.t('users.confirmDelete', {user: user.name, country:getCountryName(countryIso, i18n.language)})) ? removeUser(countryIso, user.id) : null}>
-              {i18n.t('users.remove')}
-            </span>
-    }</td>
+        : <button className="btn btn-s btn-destructive" onClick={() =>
+            window.confirm(i18n.t('users.confirmDelete', {user: user.name, country:getCountryName(countryIso, i18n.language)}))
+              ? removeUser(countryIso, user.id)
+              : null
+          }>
+            {i18n.t('users.remove')}
+          </button>
+    }
+    </td>
   </tr>
 }
 
 const UserTextFieldCol = ({countryIso, i18n, user, field, readOnly, updateUser}) =>
-  <td className={user[validationField(field)] === false ? 'error' : ''}>{
+  <td className={`users-list__cell ${user[validationField(field)] === false ? 'error' : ''}`}>
+  {
     readOnly
-      ? <div className="users__list-table-read-only-cell">{user[field]}</div>
-      : <div className="text-input__container validation-error-sensitive-field">
-        <TextInput placeholder={i18n.t(`users.${field}`)} value={user[field]}
+      ? <div className="users-list__read-only-cell">{user[field]}</div>
+      : <TextInput placeholder={i18n.t(`users.${field}`)} value={user[field]}
                    onChange={e => updateUser(countryIso, user.id, field, e.target.value)}
                    disabled={user.saving}/>
-      </div>
-  }</td>
+  }
+  </td>
 
 const UserRoleSelectCol = ({countryIso, i18n, user, readOnly, updateUser}) =>
-  <td className={user[validationField('role')] === false ? 'error' : ''}>{
+  <td className={`users-list__cell ${user[validationField('role')] === false ? 'error' : ''}`}>
+  {
     readOnly
-      ? <div className="users__list-table-read-only-cell">{i18n.t(`user.roles.${R.toLower(user.role)}`)}</div>
-      : <div className="text-input__container validation-error-sensitive-field">
-        <select required
-                className="fra-table__select"
-                value={user.role}
-                onChange={e => updateUser(countryIso, user.id, 'role', e.target.value)}
-                disabled={user.saving}>
-          {user.role === '' ? <option value="">{i18n.t('users.role')}</option> : null}
-          <option value={reviewer.role}>{i18n.t(reviewer.labelKey)}</option>
-          <option value={nationalCorrespondent.role}>{i18n.t(nationalCorrespondent.labelKey)}</option>
-          <option value={collaborator.role}>{i18n.t(collaborator.labelKey)}</option>
-        </select>
+      ? <div className="users-list__read-only-cell">{i18n.t(`user.roles.${R.toLower(user.role)}`)}</div>
+      : <div className="users-list__input-container validation-error-sensitive-field">
+          <select required
+                  className="fra-table__select"
+                  value={user.role}
+                  onChange={e => updateUser(countryIso, user.id, 'role', e.target.value)}
+                  disabled={user.saving}>
+            {user.role === '' ? <option value="">{i18n.t('users.role')}</option> : null}
+            <option value={reviewer.role}>{i18n.t(reviewer.labelKey)}</option>
+            <option value={nationalCorrespondent.role}>{i18n.t(nationalCorrespondent.labelKey)}</option>
+            <option value={collaborator.role}>{i18n.t(collaborator.labelKey)}</option>
+          </select>
       </div>
-  }</td>
+  }
+  </td>
 
 class UsersView extends React.Component {
 
@@ -133,12 +137,14 @@ class UsersView extends React.Component {
 
     return users
       ? <LoggedInPageTemplate>
-        <div className="fra-view__content">
-          <h1 className="title">{i18n.t('users.manageUsers')}</h1>
-          <AddUserForm {...this.props} user={newUser} countryIso={match.params.countryIso}/>
-          <UsersTable {...this.props} countryIso={match.params.countryIso}/>
-        </div>
-      </LoggedInPageTemplate>
+          <div className="fra-view__content">
+            <div className="fra-view__page-header">
+              <h1 className="title">{i18n.t('users.manageUsers')}</h1>
+            </div>
+            <AddUserForm {...this.props} user={newUser} countryIso={match.params.countryIso}/>
+            <UsersTable {...this.props} countryIso={match.params.countryIso}/>
+          </div>
+        </LoggedInPageTemplate>
       : null
   }
 }
