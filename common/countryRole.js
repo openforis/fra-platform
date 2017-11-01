aba197eb2d59e38c9e267e553a62b926f87b8ae3
Perttu Auramo <perttu.auramo@reaktor.com>
2017-11-01 14:51:45 +0200

    Fix countryRole bug returning the wrong object

--- common/countryRole.js
+++ common/countryRole.js
@@ -9,6 +9,13 @@ const nationalCorrespondent = {role: 'NATIONAL_CORRESPONDENT', labelKey: 'user.r
 const collaborator = {role: 'COLLABORATOR', labelKey: 'user.roles.collaborator'}
 const noRole = {role: 'NONE', labelKey: 'user.roles.noRole'}
 
+const roles = {
+  [administrator.role]: administrator,
+  [reviewer.role]: reviewer,
+  [nationalCorrespondent.role]: nationalCorrespondent,
+  [collaborator.role]: collaborator
+}
+
 const hasRole = (role, roles) => R.find(R.propEq('role', role))(roles)
 
 const getCountryRoles = (countryIso, userInfo) => R.filter(R.propEq('countryIso', countryIso))(userInfo.roles)
@@ -19,9 +26,10 @@ const roleForCountry= (countryIso, userInfo) => {
   if (hasRole('ADMINISTRATOR', userInfo.roles)) return administrator
   const rolesForCountry = getCountryRoles(countryIso, userInfo)
   assert(rolesForCountry.length < 2, `Ambiguous roles found for user ${userInfo} and country ${countryIso}`)
-  //Return null-object for undefined/null-safe access. Shouldn't happen in practice
   if (rolesForCountry.length === 0) return noRole
-  return rolesForCountry[0]
+  const roleObj = roles[rolesForCountry[0].role]
+  if (!roleObj) return noRole
+  return roleObj
 }
 
 const hasUserRole = (countryIso, userInfo, roleObj) => roleForCountry(countryIso, userInfo).role === roleObj.role
