75d71ea187e2c877fe1cbe2e5b52b8522f1f14b7
Perttu Auramo <perttu.auramo@reaktor.com>
2017-11-07 16:44:59 +0200

    Fetch a full object from DB

--- server/assessment/assessmentRepository.js
+++ server/assessment/assessmentRepository.js
@@ -6,17 +6,6 @@ const {roleForCountry} = require('../../common/countryRole')
 const {getAllowedStatusTransitions} = require('../../common/assessment')
 const {AccessControlException} = require('../utils/accessControl')
 
-const getAssessment = (client, countryIso, assessmentType) =>
-  client.query(
-    `SELECT 
-           status
-         FROM 
-           assessment 
-         WHERE country_iso = $1
-         AND type = $2`,
-    [countryIso, assessmentType]
-  )
-
 const checkStatusTransitionAllowance = (currentStatus, newStatus, role) => {
   const allowed = getAllowedStatusTransitions(role, currentStatus)
   if (!R.contains(newStatus, R.values(allowed))) {
@@ -26,6 +15,21 @@ const checkStatusTransitionAllowance = (currentStatus, newStatus, role) => {
       role: role.role
     })
   }
+}
+
+const getAssessment = async (client, countryIso, assessmentType) => {
+  const result = await client.query(
+    `SELECT 
+           status,
+           desk_study
+         FROM 
+           assessment 
+         WHERE country_iso = $1
+         AND type = $2`,
+    [countryIso, assessmentType]
+  )
+  if (result.rows.length === 0) return null
+  return camelize(result.rows[0])
 }
 
 const addAssessment = (client, countryIso, assessmentType, status) =>
@@ -49,15 +53,14 @@ const updateAssessment = (client, countryIso, assessmentType, status) =>
 
 module.exports.changeAssessmentStatus =
   async (client, countryIso, user, assessmentType, status) => {
-    const assessmentStatusResult = await getAssessment(client, countryIso, assessmentType)
-    const noStatusYet = assessmentStatusResult.rows.length === 0
-    const currentStatus = noStatusYet ? 'editing' : assessmentStatusResult.rows[0].status
+    const currentAssessment = await getAssessment(client, countryIso, assessmentType)
+    const currentStatus = !currentAssessment ? 'editing' : currentAssessment.status
     const role = roleForCountry(countryIso, user)
     checkStatusTransitionAllowance(currentStatus, status, role)
-    if (noStatusYet) {
-      await addAssessment(client, countryIso, assessmentType, status)
-    } else {
+    if (currentAssessment) {
       await updateAssessment(client, countryIso, assessmentType, status)
+    } else {
+      await addAssessment(client, countryIso, assessmentType, status)
     }
   }
 
