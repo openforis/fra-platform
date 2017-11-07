6a83e906ba2cc214478c2e3253c01e511597804f
Perttu Auramo <perttu.auramo@reaktor.com>
2017-11-07 16:23:15 +0200

    Split assessment into smaller parts

--- server/assessment/assessmentRepository.js
+++ server/assessment/assessmentRepository.js
@@ -6,44 +6,60 @@ const {roleForCountry} = require('../../common/countryRole')
 const {getAllowedStatusTransitions} = require('../../common/assessment')
 const {AccessControlException} = require('../utils/accessControl')
 
-module.exports.changeAssessmentStatus =
-  (client, countryIso, user, assessmentType, status) =>
-    client.query(
-      `SELECT 
+const getAssessment = (client, countryIso, assessmentType) =>
+  client.query(
+    `SELECT 
            status
          FROM 
            assessment 
          WHERE country_iso = $1
          AND type = $2`,
-      [countryIso, assessmentType]
-    ).then(result => {
-        const noStatusYet = result.rows.length === 0
-        const currentStatusForAccessControlCheck = noStatusYet ? 'editing' : result.rows[0].status
-        const role = roleForCountry(countryIso, user)
-        const allowed = getAllowedStatusTransitions(role, currentStatusForAccessControlCheck)
+    [countryIso, assessmentType]
+  )
 
-        if (!R.contains(status, R.values(allowed))) {
-          throw new AccessControlException('error.assessment.transitionNotAllowed', {
-            currentStatus: currentStatusForAccessControlCheck,
-            status,
-            role: role.role
-          })
-        }
+const checkStatusTransitionAllowance = (currentStatus, newStatus, role) => {
+  const allowed = getAllowedStatusTransitions(role, currentStatus)
+  if (!R.contains(newStatus, R.values(allowed))) {
+    throw new AccessControlException('error.assessment.transitionNotAllowed', {
+      currentStatus: currentStatus,
+      status: newStatus,
+      role: role.role
+    })
+  }
+}
 
-        return noStatusYet
-          ? client.query(
-            `INSERT INTO
-              assessment (country_iso, type, status)  
-              VALUES ($1, $2, $3)`,
-            [countryIso, assessmentType, status])
-          : client.query(
-            `UPDATE assessment
-              SET status = $1
-              WHERE country_iso = $2
-              AND type = $3`,
-            [status, countryIso, assessmentType])
-      }
-    )
+const addAssessment = (client, countryIso, assessmentType, status) =>
+  client.query(
+    `INSERT INTO
+      assessment 
+      (country_iso, type, status)  
+     VALUES 
+      ($1, $2, $3)`,
+    [countryIso, assessmentType, status]
+  )
+
+const updateAssessment = (client, countryIso, assessmentType, status) =>
+  client.query(
+    `UPDATE assessment
+     SET status = $1
+     WHERE country_iso = $2
+     AND type = $3`,
+    [status, countryIso, assessmentType]
+  )
+
+module.exports.changeAssessmentStatus =
+  async (client, countryIso, user, assessmentType, status) => {
+    const assessmentStatusResult = await getAssessment(client, countryIso, assessmentType)
+    const noStatusYet = assessmentStatusResult.rows.length === 0
+    const currentStatus = noStatusYet ? 'editing' : assessmentStatusResult.rows[0].status
+    const role = roleForCountry(countryIso, user)
+    checkStatusTransitionAllowance(currentStatus, status, role)
+    if (noStatusYet) {
+      await addAssessment(client, countryIso, assessmentType, status)
+    } else {
+      await updateAssessment(client, countryIso, assessmentType, status)
+    }
+  }
 
 module.exports.getAssessmentStatuses = (countryIso) =>
   db.query(
