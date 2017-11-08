fadef756527b53f4b6ab1ebb78628d70ae4a6123
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-11-08 10:43:20 +0200

    Improve chart width functionality

--- webapp/assessmentFra/extentOfForest/chart/chartWrapper.js
+++ webapp/assessmentFra/extentOfForest/chart/chartWrapper.js
@@ -3,13 +3,20 @@ import UpdateOnResizeReactComponent from '../../../reusableUiComponents/updateOn
 import ChartContainer from './chartContainer'
 
 class ChartWrapper extends UpdateOnResizeReactComponent {
-  render() {
-    const defaultWidth = 913 //TODO what's a good default before we have bounding rect?
-    const width = this.refs.chartWrapper ? this.refs.chartWrapper.getBoundingClientRect().width : defaultWidth
+  componentDidUpdate() {
+    if (!this.width || !this.getWidth()) return
+    if (this.width !== this.getWidth()) this.forceUpdate()
+  }
 
+  getWidth() {
+    return this.refs.chartWrapper ? this.refs.chartWrapper.getBoundingClientRect().width : 960
+  }
+
+  render() {
+    this.width = this.getWidth()
     return <div ref="chartWrapper" className="chart__container">
       <ChartContainer
-        wrapperWidth={width}
+        wrapperWidth={this.width}
         stateName={this.props.stateName}
         trends={this.props.trends}/>
     </div>
