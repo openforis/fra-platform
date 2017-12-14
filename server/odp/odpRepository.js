ad6dce50268c5c57c3dece8523542c28fb78d6cc
Perttu Auramo <perttu.auramo@reaktor.com>
2017-12-14 12:30:25 +0200

    fix query

--- server/odp/odpRepository.js
+++ server/odp/odpRepository.js
@@ -144,7 +144,7 @@ const addClassData = (client, odpVersionId, odp) => {
         other_planted_forest_percent,
         uuid)
         VALUES
-        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16);`,
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`,
       [
         odpVersionId,
         nationalClass.className,
