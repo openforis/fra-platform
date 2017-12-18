1ca8e63100820d37571f69ce935835b06b8fef2a
Perttu Auramo <perttu.auramo@reaktor.com>
2017-12-18 13:57:19 +0200

    Use new descriptions in growing non-wood .... view

--- webapp/assessmentFra/nonWoodForestProductsRemovals/nonWoodForestProductsRemovalsView.js
+++ webapp/assessmentFra/nonWoodForestProductsRemovals/nonWoodForestProductsRemovalsView.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux'
 import LoggedInPageTemplate from '../../app/loggedInPageTemplate'
 import TraditionalTable from '../../traditionalTable/traditionalTable'
 import mainTableSpec from './mainTableSpec'
-import { DataSourceDescriptionAndComments } from '../../descriptionBundles/dataSourceDescriptionAndComments'
 import DefinitionLink from '../../reusableUiComponents/definitionLink'
 import { fetchLastSectionUpdateTimestamp } from '../../audit/actions'
+import NationalDataDescriptions from '../../descriptionBundles/nationalDataDescriptions'
+import AnalysisDescriptions from '../../descriptionBundles/analysisDescriptions'
+import GeneralComments from '../../descriptionBundles/generalComments'
 
 const currencyNameTableSpec = i18n => ({
   name: 'nonWoodForestProductsRemovalsCurrency',
@@ -42,8 +44,10 @@ class NonWoodForestProductsRemovalsView extends React.Component {
 
     return <LoggedInPageTemplate>
       <div className="fra-view__content">
+        <NationalDataDescriptions section={mainTableSpecInstance.name} countryIso={match.params.countryIso}/>
+        <AnalysisDescriptions section={mainTableSpecInstance.name} countryIso={match.params.countryIso}/>
         <div className="fra-view__page-header">
-          <h1 className="title">{i18n.t('nonWoodForestProductsRemovals.nonWoodForestProductsRemovals')}</h1>
+          <h3 className="subhead">{i18n.t('nonWoodForestProductsRemovals.nonWoodForestProductsRemovals')}</h3>
           <div className="fra-view__header-secondary-content">
             <DefinitionLink document="tad" anchor="7c" title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
             <DefinitionLink document="faq" anchor="7c" title={i18n.t('definition.faqLabel')} lang={i18n.language}/>
@@ -53,11 +57,9 @@ class NonWoodForestProductsRemovalsView extends React.Component {
         <div className="fra-secondary-table__wrapper">
           <TraditionalTable tableSpec={currencyNameTableSpec(i18n)} countryIso={match.params.countryIso}/>
         </div>
-        <DataSourceDescriptionAndComments
+        <GeneralComments
           section={mainTableSpecInstance.name}
-          name="nonWoodForestProductsRemovals"
           countryIso={match.params.countryIso}
-          i18n={i18n}
         />
       </div>
     </LoggedInPageTemplate>
