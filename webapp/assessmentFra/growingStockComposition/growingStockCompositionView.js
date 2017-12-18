9506ff06f073c075113a88ac26a526f99eaeb026
Perttu Auramo <perttu.auramo@reaktor.com>
2017-12-18 13:51:55 +0200

    Use new descriptions in growing stock composition view

--- webapp/assessmentFra/growingStockComposition/growingStockCompositionView.js
+++ webapp/assessmentFra/growingStockComposition/growingStockCompositionView.js
@@ -5,9 +5,11 @@ import { connect } from 'react-redux'
 import LoggedInPageTemplate from '../../app/loggedInPageTemplate'
 import TraditionalTable from '../../traditionalTable/traditionalTable'
 import tableSpec from './tableSpec'
-import { DataSourceDescriptionAndComments } from '../../descriptionBundles/dataSourceDescriptionAndComments'
 import DefinitionLink from '../../reusableUiComponents/definitionLink'
 import { fetchLastSectionUpdateTimestamp } from '../../audit/actions'
+import NationalDataDescriptions from '../../descriptionBundles/nationalDataDescriptions'
+import AnalysisDescriptions from '../../descriptionBundles/analysisDescriptions'
+import GeneralComments from '../../descriptionBundles/generalComments'
 
 class GrowingStockCompositionView extends React.Component {
 
@@ -25,8 +27,10 @@ class GrowingStockCompositionView extends React.Component {
 
     return <LoggedInPageTemplate>
       <div className="fra-view__content growing-stock-composition-view">
+        <NationalDataDescriptions section={this.tableSpecInstance.name} countryIso={match.params.countryIso}/>
+        <AnalysisDescriptions section={this.tableSpecInstance.name} countryIso={match.params.countryIso}/>
         <div className="fra-view__page-header">
-          <h1 className="title">{i18n.t('growingStockComposition.growingStockComposition')}</h1>
+          <h3 className="subhead">{i18n.t('growingStockComposition.growingStockComposition')}</h3>
           <div className="fra-view__header-secondary-content">
             <DefinitionLink document="tad" anchor="2b" title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
             <DefinitionLink document="faq" anchor="2b" title={i18n.t('definition.faqLabel')} lang={i18n.language}/>
@@ -34,11 +38,9 @@ class GrowingStockCompositionView extends React.Component {
           </div>
         </div>
         <TraditionalTable tableSpec={this.tableSpecInstance} countryIso={match.params.countryIso}/>
-        <DataSourceDescriptionAndComments
+        <GeneralComments
           section={this.tableSpecInstance.name}
-          name="growingStockComposition"
           countryIso={match.params.countryIso}
-          i18n={i18n}
         />
       </div>
     </LoggedInPageTemplate>
