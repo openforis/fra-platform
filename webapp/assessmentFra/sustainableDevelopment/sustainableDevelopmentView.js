e3cb9379f3b8fe3f74e4ae3729345ca4aa94164b
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-12-20 10:59:06 +0200

    Minor style changes to table 8

--- webapp/assessmentFra/sustainableDevelopment/sustainableDevelopmentView.js
+++ webapp/assessmentFra/sustainableDevelopment/sustainableDevelopmentView.js
@@ -42,12 +42,10 @@ class SustainableDevelopmentView extends React.Component {
       ? null
       : <LoggedInPageTemplate>
         <div className="fra-view__content fra-sustainable-dev__content">
-          <div className="fra-view__page-header">
-            <h1 className="title">{i18n.t('sustainableDevelopment.sustainableDevelopment')}</h1>
-            <div className="fra-view__header-secondary-content">
-              <DefinitionLink document="tad" anchor="8" title={i18n.t('definition.definitionLabel')} lang={lang}/>
-              <DefinitionLink document="faq" anchor="8" title={i18n.t('definition.faqLabel')} lang={lang}/>
-            </div>
+          <h2 className="headline">{i18n.t('sustainableDevelopment.sustainableDevelopment')}</h2>
+          <div className="fra-view__section-toolbar">
+            <DefinitionLink className="margin-right-big" document="tad" anchor="8" title={i18n.t('definition.definitionLabel')} lang={lang}/>
+            <DefinitionLink className="align-left" document="faq" anchor="8" title={i18n.t('definition.faqLabel')} lang={lang}/>
           </div>
           <Indicator15_1_1
             i18n={i18n}
