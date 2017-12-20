0b57a930c88f90078650ed24dbb0bad099b708d5
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-12-20 17:30:09 +0200

    Add review indicators and sum rows

--- webapp/assessmentFra/growingStock/actions.js
+++ webapp/assessmentFra/growingStock/actions.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import R from 'ramda'
 import { applicationError } from '../../applicationError/actions'
 import * as autosave from '../../autosave/actions'
-
 export const growingStockFetchCompleted = 'growingStock/fetch/completed'
 export const growingStockChanged = 'growingStock/changed'
+import { acceptNextDecimal} from '../../utils/numberInput'
 
 export const fetch = (countryIso) => dispatch =>
   axios
@@ -12,18 +12,22 @@ export const fetch = (countryIso) => dispatch =>
     .then(resp => dispatch({type: growingStockFetchCompleted, data: resp.data}))
     .catch(err => dispatch(applicationError(err)))
 
-export const changeAvgValue = (countryIso, year, row, value) => (dispatch, getState) => {
-  dispatch(autosave.start)
+export const changeAvgValue = (countryIso, year, row, newValue) => (dispatch, getState) => {
   const growingStock = getState().growingStock
-  const insertValue = R.assocPath(['avgTable', year, row], value, growingStock)
+  const currentValue = R.path(['avgTable', year, row], growingStock)
+  const sanitizedValue = acceptNextDecimal(newValue, currentValue)
+  const insertValue = R.assocPath(['avgTable', year, row], sanitizedValue, growingStock)
+  dispatch(autosave.start)
   dispatch({type: growingStockChanged, data: insertValue})
   dispatch(persistValues(countryIso, insertValue))
 }
 
-export const changeTotalValue = (countryIso, year, row, value) => (dispatch, getState) => {
-  dispatch(autosave.start)
+export const changeTotalValue = (countryIso, year, row, newValue) => (dispatch, getState) => {
   const growingStock = getState().growingStock
-  const insertValue = R.assocPath(['totalTable', year, row], value, growingStock)
+  const currentValue = R.path(['avgTable', year, row], growingStock)
+  const sanitizedValue = acceptNextDecimal(newValue, currentValue)
+  const insertValue = R.assocPath(['totalTable', year, row], sanitizedValue, growingStock)
+  dispatch(autosave.start)
   dispatch({type: growingStockChanged, data: insertValue})
   dispatch(persistValues(countryIso, insertValue))
 }
