65920b63b5ae0bcc4708e1480fa7a7393907e799
Perttu Auramo <perttu.auramo@reaktor.com>
2017-12-21 10:59:29 +0200

    Add a coder-friendly readObject for cases when we need to programmatically handle the trad. table data outside of Traditional Table's own view component code

--- server/traditionalTable/traditionalTableRepository.js
+++ server/traditionalTable/traditionalTableRepository.js
@@ -3,6 +3,7 @@ const R = require('ramda')
 const sqlCreator = require('./traditionalTableSqlCreator')
 const tableMappings = require('./tableMappings')
 const auditRepository = require('./../audit/auditRepository')
+const camelize = require('camelize')
 
 module.exports.save = (client, userId, countryIso, tableSpecName, tableData) => {
   const mapping = tableMappings.getMapping(tableSpecName)
@@ -39,15 +40,34 @@ const handleRow = mapping => (tableData, row) => {
   )
 }
 
-module.exports.read = (countryIso, tableSpecName) => {
-  const mapping = tableMappings.getMapping(tableSpecName)
+const rawRead = async (countryIso, tableSpecName) => {
   const [selectQuery, selectParams] = sqlCreator.createSelect(countryIso, tableSpecName)
-  return db.query(selectQuery, selectParams).then((result) => {
-    if (result.rowCount === 0) return null
-    const emptyTableData = createTableData(
-      mapping.getFullColumnCount(),
-      mapping.getFullRowCount()
-    )
-    return R.reduce(handleRow(mapping), emptyTableData, result.rows)
-  })
+  const result = await db.query(selectQuery, selectParams)
+  if (result.rowCount === 0) return null
+  return result.rows
+}
+
+module.exports.read = async (countryIso, tableSpecName) => {
+  const rows = await rawRead(countryIso, tableSpecName)
+  if (rows === null) return null
+  const mapping = tableMappings.getMapping(tableSpecName)
+  const emptyTableData = createTableData(
+    mapping.getFullColumnCount(),
+    mapping.getFullRowCount()
+  )
+  return R.reduce(handleRow(mapping), emptyTableData, rows)
+}
+
+// Read an object instead of the matrix that plain read returns.
+// This can be used when you need programmatical access to the data
+// outside of the automated traditionalTable FW (in other views or calculations)
+module.exports.readObject = async (countryIso, tableSpecName) => {
+  const rows = await rawRead(countryIso, tableSpecName)
+  if (rows === null) return null
+  return R.pipe(
+    R.values,
+    R.map(row => [row.row_name, R.dissoc('row_name', row)]),
+    R.fromPairs,
+    camelize
+  )(rows)
 }
