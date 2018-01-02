ddd1404c6cb3806857bc03b50cca5245f2a84bce
Mino Togna <Cosimo.Togna@Fao.org>
2018-01-02 17:25:08 +0100

    added custom onchange event function to decimal input to update forest expansion and deforestation values in table 1c

--- webapp/assessmentFra/forestAreaChange/forestAreaChangeView.js
+++ webapp/assessmentFra/forestAreaChange/forestAreaChangeView.js
@@ -2,14 +2,15 @@ import React from 'react'
 import tableSpec from './tableSpec'
 import { connect } from 'react-redux'
 import SingleTraditionalTableView from '../../traditionalTable/singleTraditionalTableView'
+import {tableChanged} from '../../traditionalTable/actions'
 
 const ForestAreaChangeView = props =>
   <SingleTraditionalTableView
     {...props}
     headingLocalizationKey="forestAreaChange.forestAreaChange"
     sectionAnchor="1c"
-    tableSpecInstance={tableSpec(props.i18n, props.extentOfForest, props.match.params.countryIso)}/>
+    tableSpecInstance={tableSpec(props.i18n, props.extentOfForest, props.match.params.countryIso, props.tableChanged)}/>
 
 const mapStateToProps = state => ({i18n: state.user.i18n, extentOfForest: state.extentOfForest})
 
-export default connect(mapStateToProps)(ForestAreaChangeView)
+export default connect(mapStateToProps, {tableChanged})(ForestAreaChangeView)
