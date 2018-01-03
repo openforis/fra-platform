9fb56211466ed594702d23806b3174ecb8324f78
Mino Togna <Cosimo.Togna@Fao.org>
2018-01-03 16:33:15 +0100

    frontend: using country names and iso2 from countries loaded from server

---
+++ webapp/country/country.js
@@ -0,0 +1,30 @@
+import * as R from 'ramda'
+
+const demoCountry = {
+  countryIso: 'ATL',
+  countryIso2: 'atlantis',
+  listName: {
+    en: 'Atlantis',
+    es: 'Atlantis',
+    fr: 'Atlantis',
+    ru: 'Atlantis'
+  },
+  fullName: {
+    en: 'Atlantis',
+    es: 'Atlantis',
+    fr: 'Atlantis',
+    ru: 'Atlantis'
+  },
+  lastEdit: null
+}
+
+const isDemoCountryISO = countryIso => countryIso === demoCountry.countryIso
+
+export const getCountry = countryIso => isDemoCountryISO(countryIso)
+  ? () => demoCountry
+  : R.pipe(
+    R.path(['country', 'countries']),
+    R.values,
+    R.flatten,
+    R.find(R.propEq('countryIso', countryIso)),
+  )
