88f8db11e782b6faf5300212b9ccba83cb7851e7
Perttu Auramo <perttu.auramo@reaktor.com>
2018-01-04 15:10:30 +0200

    Remove tableChanged which isn't in use any more

--- webapp/assessmentFra/forestAreaChange/forestAreaChangeView.js
+++ webapp/assessmentFra/forestAreaChange/forestAreaChangeView.js
@@ -2,15 +2,14 @@ import React from 'react'
 import tableSpec from './tableSpec'
 import { connect } from 'react-redux'
 import SingleTraditionalTableView from '../../traditionalTable/singleTraditionalTableView'
-import {tableChanged} from '../../traditionalTable/actions'
 
 const ForestAreaChangeView = props =>
   <SingleTraditionalTableView
     {...props}
     headingLocalizationKey="forestAreaChange.forestAreaChange"
     sectionAnchor="1c"
-    tableSpecInstance={tableSpec(props.i18n, props.extentOfForest, props.match.params.countryIso, props.tableChanged)}/>
+    tableSpecInstance={tableSpec(props.i18n, props.extentOfForest, props.match.params.countryIso)}/>
 
 const mapStateToProps = state => ({i18n: state.user.i18n, extentOfForest: state.extentOfForest})
 
-export default connect(mapStateToProps, {tableChanged})(ForestAreaChangeView)
+export default connect(mapStateToProps)(ForestAreaChangeView)
