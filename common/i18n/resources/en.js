78a3bde1e561aa2f8c054e433de9a548e6232ff6
Mino Togna <Cosimo.Togna@Fao.org>
2018-01-04 11:42:10 +0100

    made deforestation calculated row

--- common/i18n/resources/en.js
+++ common/i18n/resources/en.js
@@ -385,9 +385,8 @@ The FRA team
     ofWhichAfforestation: '…of which afforestation',
     ofWhichNaturalExpansion: '…of which natural expansion',
     deforestation: 'Deforestation',
-    forestAreaNetChange: 'Forest area net change (a-b)',
-    netChangeDoesNotMatch: 'Doesn’t match Forest area net change',
-    total: 'Total'
+    forestAreaNetChange: 'Forest area net change',
+    netChangeDoesNotMatch: 'Doesn’t match Forest area net change'
   },
 
   annualReforestation: {
