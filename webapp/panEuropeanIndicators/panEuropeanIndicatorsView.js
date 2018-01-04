921634535c033bf270a63d832dc19bbe3266a9df
Mino Togna <Cosimo.Togna@Fao.org>
2018-01-04 18:09:52 +0100

    disabling upload button when upload is in progress

--- webapp/panEuropeanIndicators/panEuropeanIndicatorsView.js
+++ webapp/panEuropeanIndicators/panEuropeanIndicatorsView.js
@@ -17,12 +17,20 @@ class PanEuropeanIndicatorsView extends React.Component {
     this.props.fetchLastSectionUpdateTimestamp(this.props.countryIso, 'panEuropean')
   }
 
+  componentWillReceiveProps (nextProps) {
+    const status = this.props.status
+    const nextStatus = nextProps.status
+    if (!R.equals(status, nextStatus) && R.equals('complete', nextStatus)) {
+      this.refs.inputFile.value = ''
+    }
+  }
+
   onFileSelected () {
     this.props.uploadQuestionnaire(this.props.countryIso, this.refs.inputFile.files[0])
   }
 
   render () {
-    const {i18n, countryIso} = this.props
+    const {i18n, countryIso, status} = this.props
     return <LoggedInPageTemplate>
       <div className="fra-view__content">
 
@@ -37,24 +45,26 @@ class PanEuropeanIndicatorsView extends React.Component {
               type="file"
               style={{display: 'none'}}
               onChange={() => this.onFileSelected()}
-              accept=".xls,xlsx"/>
-            <a className="btn btn-primary"
-               onClick={() => this.refs.inputFile.dispatchEvent(new MouseEvent('click'))}>
+              accept=".xls,.xlsx"
+            />
+            <button className="btn btn-primary"
+                    onClick={() => this.refs.inputFile.dispatchEvent(new MouseEvent('click'))}
+                    disabled={R.equals('saving', status)}>
               <Icon className="icon-sub icon-white" name="hit-down"/>
               {i18n.t('panEuropeanIndicators.uploadQuestionnaire')}
-            </a>
+            </button>
             <a className="btn btn-primary" href={`/api/panEuropean/${countryIso}/download`} target="_blank">
               <Icon className="icon-sub icon-white" name="hit-down"/>
               {i18n.t('panEuropeanIndicators.downloadQuestionnaire')}
             </a>
           </div>
-          <div>
-            <ReviewIndicator
-              section={'panEuropeanIndicators'}
-              title={i18n.t('panEuropeanIndicators.panEuropeanIndicators')}
-              target={['uploadQuestionnaire']}
-              countryIso={countryIso}/>
-          </div>
+
+          <ReviewIndicator
+            section={'panEuropeanIndicators'}
+            title={i18n.t('panEuropeanIndicators.panEuropeanIndicators')}
+            target={['uploadQuestionnaire']}
+            countryIso={countryIso}
+          />
         </div>
 
       </div>
@@ -65,7 +75,8 @@ class PanEuropeanIndicatorsView extends React.Component {
 
 const mapStateToProps = (state, props) => ({
   i18n: state.user.i18n,
-  countryIso: props.match.params.countryIso
+  countryIso: props.match.params.countryIso,
+  status: state.autoSave.status
 })
 
 export default connect(mapStateToProps, {
