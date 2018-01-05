15b92f718999a3b8957b4d9e51ff8f60f04b45b3
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2018-01-05 15:36:06 +0200

    Simplify 7a and 7b and make colspans counted

--- webapp/assessmentFra/graduationOfStudents/tableSpec.js
+++ webapp/assessmentFra/graduationOfStudents/tableSpec.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import R from 'ramda'
 
+const years = [1990, 2000, 2010, 2015]
+
 const yearFields = (rowIdx, validator) => yearIdx => [
   {type: 'integerInput', validator: validator},
   {type: 'integerInput', validator: validator},
@@ -9,37 +11,36 @@ const yearFields = (rowIdx, validator) => yearIdx => [
 
 const inputRow = (rowIdx, rowHeaderCell, validator) => [
   rowHeaderCell,
-  ...R.flatten(R.map(yearFields(rowIdx, validator), R.range(0, 4)))
+  ...R.flatten(R.map(yearFields(rowIdx, validator), years))
 ]
 
 const inputRowHeading = (i18n, localizationKey) =>
   ({type: 'readOnly', jsx: <th className="fra-table__category-cell">{i18n.t(localizationKey)}</th>})
-
-const yearSubHeadings = i18n =>
-  [
-    <th key="total" className="fra-table__header-cell">{i18n.t('graduationOfStudents.total')}</th>,
-    <th key="female" className="fra-table__category-cell-right">{i18n.t('graduationOfStudents.female')}</th>,
-    <th key="male" className="fra-table__category-cell-right">{i18n.t('graduationOfStudents.male')}</th>
-  ]
 
 export default i18n => ({
   name: 'graduationOfStudents',
   header: <thead>
   <tr>
     <th className="fra-table__header-cell-left" rowSpan="3">{i18n.t('graduationOfStudents.fra2020Categories')}</th>
-    <th className="fra-table__header-cell" colSpan="12">{i18n.t('graduationOfStudents.numberOfStudents')}</th>
+    <th className="fra-table__header-cell" colSpan={years.length * 3}>{i18n.t('graduationOfStudents.numberOfStudents')}</th>
   </tr>
   <tr>
-    <th colSpan="3" className="fra-table__header-cell">1990</th>
-    <th colSpan="3" className="fra-table__header-cell">2000</th>
-    <th colSpan="3" className="fra-table__header-cell">2010</th>
-    <th colSpan="3" className="fra-table__header-cell">2015</th>
+    {
+      R.map(year=>
+        <th key={year} colSpan="3" className="fra-table__header-cell">{year}</th>
+      , years)
+    }
   </tr>
   <tr>
-    {yearSubHeadings(i18n)}
-    {yearSubHeadings(i18n)}
-    {yearSubHeadings(i18n)}
-    {yearSubHeadings(i18n)}
+    {
+      R.map(year=>
+        [
+          <th key="total" className="fra-table__header-cell">{i18n.t('graduationOfStudents.total')}</th>,
+          <th key="female" className="fra-table__category-cell-right">{i18n.t('graduationOfStudents.female')}</th>,
+          <th key="male" className="fra-table__category-cell-right">{i18n.t('graduationOfStudents.male')}</th>
+        ]
+      , years)
+    }
   </tr>
   </thead>,
   rows: [
