d6542662d3d2bd575ec4ed2e18cb6ef138a1b0ec
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2018-01-19 13:15:20 +0200

    Use getRoleLabelKey on dashboard and invitaitons
    Also, don't lowercase user roles into audit target column

--- server/user/sendInvitation.js
+++ server/user/sendInvitation.js
@@ -1,17 +1,18 @@
 const { getCountry } = require('../country/countryRepository')
 const { createI18nPromise } = require('../../common/i18n/i18nFactory')
 const { sendMail } = require('../email/sendMail')
+const { getRoleLabelKey } = require('../../common/countryRole')
 
 const createMail = async (countryIso, invitedUser, loggedInUser, url, i18n) => {
   const link = `${url}/login${invitedUser.invitationUuid ? `?i=${invitedUser.invitationUuid}` : ''}`
   const dbCountry = await getCountry(countryIso)
   const country = dbCountry.listName.en
-  const role = invitedUser.role.toLowerCase()
+  const role = getRoleLabelKey(invitedUser.role)
   return {
     to: invitedUser.email,
     subject: i18n.t('userManagement.invitationEmail.subject', {country}),
-    text: i18n.t('userManagement.invitationEmail.textMessage', {country, invitedUser: invitedUser.name, role: `$t(user.roles.${role})`, loggedInUser: loggedInUser.name, link, url}),
-    html: i18n.t('userManagement.invitationEmail.htmlMessage', {country, invitedUser: invitedUser.name, role: `$t(user.roles.${role})`, loggedInUser: loggedInUser.name, link, url})
+    text: i18n.t('userManagement.invitationEmail.textMessage', {country, invitedUser: invitedUser.name, role: `$t(${role})`, loggedInUser: loggedInUser.name, link, url}),
+    html: i18n.t('userManagement.invitationEmail.htmlMessage', {country, invitedUser: invitedUser.name, role: `$t(${role})`, loggedInUser: loggedInUser.name, link, url})
   }
 }
 
