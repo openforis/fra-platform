07d701a288e144cc69bd32645e772eec2ed0eb62
Perttu Auramo <perttu.auramo@reaktor.com>
2018-01-19 11:26:53 +0200

    Fix the following scenario:
    User invites herself to the platform with another google login email address.
    User clicks the login link from her email while still logged in to the FRA platform with another google login email.
    This caused the email used for the inviting process to get the new role for country and
    invite was considered to be used.
    
    Now the current user is always logged out when invitation UUID is part of the URL.

--- server/auth/loginHandler.js
+++ server/auth/loginHandler.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const {sendErr} = require('../utils/requestUtils')
 const countryRepository = require('../country/countryRepository')
-const db = require('../db/db')
-const userRepository = require('../user/userRepository')
 
 module.exports.init = app => {
+  const loginPage = (req, res, next) => {
+    express.static(`${__dirname}/../../web-resources/login.html`)(req, res, next)
+  }
+
   app.use('/login', async (req, res, next) => {
     try {
       if (req.user) {
         if (req.query.i) {
-          await db.transaction(userRepository.addCountryRoleAndUpdateUserBasedOnInvitation, [req.user, req.query.i])
+          req.logout()
+          loginPage(req, res, next)
+          return
         }
         const defaultCountry = await countryRepository.getFirstAllowedCountry(req.user.roles)
         res.redirect(`/#/country/${defaultCountry.countryIso}`)
       } else {
-        express.static(`${__dirname}/../../web-resources/login.html`)(req, res, next)
+        loginPage(req, res, next)
       }
     } catch (err) {
       sendErr(res, err)
