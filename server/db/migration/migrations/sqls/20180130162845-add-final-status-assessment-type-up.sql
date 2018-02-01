514570adc81bce94167a03ab6b36a66d88bd1f71
Mino Togna <Cosimo.Togna@Fao.org>
2018-02-01 14:39:02 +0100

    introduced approval status

--- server/db/migration/migrations/sqls/20180130162845-add-final-status-assessment-type-up.sql
+++ server/db/migration/migrations/sqls/20180130162845-add-final-status-assessment-type-up.sql
@@ -1,7 +1,6 @@
 ALTER TYPE assessment_status RENAME TO _old_assessment_status;
 
-
-CREATE TYPE assessment_status as ENUM ('editing', 'review', 'accepted', 'final');
+CREATE TYPE assessment_status as ENUM ('editing', 'review', 'approval', 'accepted');
 
 ALTER TABLE assessment ALTER COLUMN status TYPE assessment_status USING status::text::assessment_status;
 
