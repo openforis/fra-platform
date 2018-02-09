9bca980e3bbd32d794ddb95f492113acad88e335
Mino Togna <Cosimo.Togna@Fao.org>
2018-02-09 15:43:57 +0100

    removed demo country

--- webapp/country/country.js
+++ webapp/country/country.js
@@ -1,30 +1,8 @@
 import * as R from 'ramda'
 
-const demoCountry = {
-  countryIso: 'ATL',
-  listName: {
-    en: 'Atlantis',
-    es: 'Atlantis',
-    fr: 'Atlantis',
-    ru: 'Atlantis'
-  },
-  fullName: {
-    en: 'Atlantis',
-    es: 'Atlantis',
-    fr: 'Atlantis',
-    ru: 'Atlantis'
-  },
-  panEuropean: true,
-  lastEdit: null
-}
-
-const isDemoCountryISO = countryIso => countryIso === demoCountry.countryIso
-
-export const getCountry = countryIso => isDemoCountryISO(countryIso)
-  ? () => demoCountry
-  : R.pipe(
-    R.path(['country', 'countries']),
-    R.values,
-    R.flatten,
-    R.find(R.propEq('countryIso', countryIso)),
-  )
+export const getCountry = countryIso => R.pipe(
+  R.path(['country', 'countries']),
+  R.values,
+  R.flatten,
+  R.find(R.propEq('countryIso', countryIso)),
+)
