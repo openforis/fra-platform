ff14b305d0f74c73c50b733eed49f274bb56244e
Mino Togna <Cosimo.Togna@Fao.org>
2018-02-12 14:19:01 +0100

    added authorization check to descriptions api

--- server/descriptions/api.js
+++ server/descriptions/api.js
@@ -1,31 +1,43 @@
 const db = require('../db/db')
-const {sendErr} = require('../utils/requestUtils')
+const {sendErr, sendOk} = require('../utils/requestUtils')
 const repository = require('./descriptionsRepository')
 const {checkCountryAccessFromReqParams} = require('../utils/accessControl')
 const auditRepository = require('./../audit/auditRepository')
+const {allowedToEditDataCheck} = require('../assessment/assessmentEditAccessControl')
 
 module.exports.init = app => {
 
-  app.get('/country/descriptions/:countryIso/:section/:name', (req, res) => {
-      checkCountryAccessFromReqParams(req)
-      db.transaction(repository.readDescriptions, [req.params.countryIso, req.params.section, req.params.name])
-        .then(result => res.json(result))
-        .catch(err => sendErr(res, err))
+  app.get('/country/descriptions/:countryIso/:section/:name', async (req, res) => {
+      try {
+        checkCountryAccessFromReqParams(req)
+
+        const result = await db.transaction(repository.readDescriptions, [req.params.countryIso, req.params.section, req.params.name])
+
+        res.json(result)
+      } catch (err) {
+        sendErr(res, err)
+      }
     }
   )
 
-  app.post('/country/descriptions/:countryIso/:section/:name', (req, res) => {
-      checkCountryAccessFromReqParams(req)
-      db.transaction(auditRepository.insertAudit,
-        [req.user.id, 'saveDescriptions', req.params.countryIso, req.params.section]
-      ).then(
-        () => db.transaction(
+  app.post('/country/descriptions/:countryIso/:section/:name', async (req, res) => {
+      try {
+        checkCountryAccessFromReqParams(req)
+        await allowedToEditDataCheck(req.params.countryIso, req.user, req.params.section)
+
+        await db.transaction(
+          auditRepository.insertAudit,
+          [req.user.id, 'saveDescriptions', req.params.countryIso, req.params.section]
+        )
+        await db.transaction(
           repository.persistDescriptions,
           [req.params.countryIso, req.params.section, req.params.name, req.body.content]
         )
-      )
-      .then(result => res.json({}))
-      .catch(err => sendErr(res, err))
+
+        sendOk(res)
+      } catch (err) {
+        sendErr(res, err)
+      }
     }
   )
 
