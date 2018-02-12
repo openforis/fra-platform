2ccdfa30f3168ce6c488be20179ad0afc211f919
Mino Togna <Cosimo.Togna@Fao.org>
2018-02-12 10:53:34 +0100

    added forest area validation to bamboo and mangroves rows

--- webapp/assessmentFra/specificForestCategories/specificForestCategoriesView.js
+++ webapp/assessmentFra/specificForestCategories/specificForestCategoriesView.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import SingleTraditionalTableView from '../../traditionalTable/singleTraditionalTableView'
 import tableSpec from './tableSpec'
 
-export default props =>
+const SpecificForestCategories = props =>
   <SingleTraditionalTableView
     {...props}
     headingLocalizationKey="specificForestCategories.specificForestCategories"
     sectionAnchor="1e"
-    tableSpec={tableSpec}/>
+    tableSpecInstance={tableSpec(props.i18n, props.extentOfForest)}/>
+
+const mapStateToProps = state => ({
+  i18n: state.user.i18n,
+  extentOfForest: state.extentOfForest
+})
+
+export default connect(mapStateToProps)(SpecificForestCategories)
--- webapp/assessmentFra/specificForestCategories/tableSpec.js
+++ webapp/assessmentFra/specificForestCategories/tableSpec.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { forestAreaLessThanOrEqualToExtentOfForestValidator } from '../../traditionalTable/validators'
 
-export default i18n => ({
+const years = [1990, 2000, 2010, 2015, 2020]
+
+const forestAreaRow = (i18n, extentOfForest, key) => [
+  {
+    type: 'readOnly',
+    jsx: <th key={key} className="fra-table__category-cell">{i18n.t(`specificForestCategories.${key}`)}</th>
+  },
+  ...years.map(year => ({
+      type: 'decimalInput',
+      validator: forestAreaLessThanOrEqualToExtentOfForestValidator(year, extentOfForest)
+    })
+  )
+]
+
+export default (i18n, extentOfForest) => ({
   name: 'specificForestCategories', // used to uniquely identify table
   header: <thead>
   <tr>
@@ -8,18 +23,19 @@ export default i18n => ({
     <th className="fra-table__header-cell" colSpan="5">{i18n.t('specificForestCategories.areaUnitLabel')}</th>
   </tr>
   <tr>
-    <th className="fra-table__header-cell">1990</th>
-    <th className="fra-table__header-cell">2000</th>
-    <th className="fra-table__header-cell">2010</th>
-    <th className="fra-table__header-cell">2015</th>
-    <th className="fra-table__header-cell">2020</th>
+    {
+      years.map(year => <th key={year} className="fra-table__header-cell">{year}</th>)
+    }
   </tr>
   </thead>,
   rows: [
+    forestAreaRow(i18n, extentOfForest, 'bamboo'),
+    forestAreaRow(i18n, extentOfForest, 'mangroves'),
     [
       {
         type: 'readOnly',
-        jsx: <th key="bamboo" className="fra-table__category-cell">{i18n.t('specificForestCategories.bamboo')}</th>
+        jsx: <th key="temporarilyUnstocked"
+                 className="fra-table__category-cell">{i18n.t('specificForestCategories.temporarilyUnstocked')}</th>
       },
       {type: 'decimalInput'},
       {type: 'decimalInput'},
@@ -30,29 +46,8 @@ export default i18n => ({
     [
       {
         type: 'readOnly',
-        jsx: <th key="mangroves" className="fra-table__category-cell">{i18n.t('specificForestCategories.mangroves')}</th>
-      },
-      {type: 'decimalInput'},
-      {type: 'decimalInput'},
-      {type: 'decimalInput'},
-      {type: 'decimalInput'},
-      {type: 'decimalInput'}
-    ],
-    [
-      {
-        type: 'readOnly',
-        jsx: <th key="temporarilyUnstocked" className="fra-table__category-cell">{i18n.t('specificForestCategories.temporarilyUnstocked')}</th>
-      },
-      {type: 'decimalInput'},
-      {type: 'decimalInput'},
-      {type: 'decimalInput'},
-      {type: 'decimalInput'},
-      {type: 'decimalInput'}
-    ],
-    [
-      {
-        type: 'readOnly',
-        jsx: <th key="primaryForest" className="fra-table__category-cell">{i18n.t('specificForestCategories.primaryForest')}</th>
+        jsx: <th key="primaryForest"
+                 className="fra-table__category-cell">{i18n.t('specificForestCategories.primaryForest')}</th>
       },
       {type: 'decimalInput'},
       {type: 'decimalInput'},
