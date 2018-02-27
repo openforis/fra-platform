7bb086617d70a70ea9a4393d1430917d733cbc40
minot <cosimo.togna@fao.org>
2018-02-27 09:30:25 +0100

    added institution and position user columns

--- server/user/userRepository.js
+++ server/user/userRepository.js
@@ -6,7 +6,7 @@ const auditRepository = require('./../audit/auditRepository')
 const { AccessControlException } = require('../utils/accessControl')
 
 const findUserById = async (userId, client = db) => {
-  const res = await client.query('SELECT id, name, email, login_email, lang FROM fra_user WHERE id = $1', [userId])
+  const res = await client.query('SELECT id, name, email, login_email, institution, position, lang FROM fra_user WHERE id = $1', [userId])
   if (res.rows.length < 1) return null
   const resultUser = camelize(res.rows[0])
   const resultRoles = await client.query('SELECT country_iso, role FROM user_country_role WHERE user_id = $1', [resultUser.id])
