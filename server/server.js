d6019d70f0fe50981030299641880aee61508511
minot <cosimo.togna@fao.org>
2018-03-08 12:59:21 +0100

    removed server start function

--- server/server.js
+++ server/server.js
@@ -6,33 +6,29 @@ const serverInit = require('./serverInit')
 
 const cluster = require('cluster')
 
-const start = async () => {
-  if (cluster.isMaster) {
+if (cluster.isMaster) {
 
-    // check db migrations in master process
-    migrations()
+  // check db migrations in master process
+  migrations()
 
-    const numWorkers = process.env.WEB_CONCURRENCY || require('os').cpus().length
+  const numWorkers = process.env.WEB_CONCURRENCY || require('os').cpus().length
 
-    console.log('Master cluster setting up ' + numWorkers + ' workers...')
+  console.log('Master cluster setting up ' + numWorkers + ' workers...')
 
-    for (let i = 0; i < numWorkers; i++) {
-      cluster.fork()
-    }
-
-    cluster.on('online', function (worker) {
-      console.log('Worker ' + worker.process.pid + ' is online')
-    })
-
-    cluster.on('exit', function (worker, code, signal) {
-      console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal)
-      console.log('Starting a new worker')
-      cluster.fork()
-    })
-  } else {
-    serverInit()
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork()
   }
 
+  cluster.on('online', function (worker) {
+    console.log('Worker ' + worker.process.pid + ' is online')
+  })
+
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal)
+    console.log('Starting a new worker')
+    cluster.fork()
+  })
+} else {
+  serverInit()
 }
 
-start()
