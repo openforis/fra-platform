8133c71d990c310d4da38a3eae5a7cbdf7e7de92
Mino Togna <Cosimo.Togna@Fao.org>
2018-05-08 14:05:31 +0200

    persisting collaborator table access

--- server/collaborators/collaboratorsApi.js
+++ server/collaborators/collaboratorsApi.js
@@ -3,19 +3,35 @@ const Promise = require('bluebird')
 
 const db = require('../db/db')
 const {checkCountryAccessFromReqParams} = require('../utils/accessControl')
-const {fetchCollaborators} = require('./collaboratorsRepository')
+const {fetchCollaboratorsCountryAccess, persistCollaboratorCountryAccess} = require('./collaboratorsRepository')
 const {sendErr, sendOk, serverUrl} = require('../utils/requestUtils')
 
 module.exports.init = app => {
 
-  // get collaborators
-  app.get('/collaborators/:countryIso', async (req, res) => {
+  // get collaborators country access
+  app.get('/collaboratorCountryAccess/:countryIso/all', async (req, res) => {
     try {
       checkCountryAccessFromReqParams(req)
 
       const countryIso = req.params.countryIso
 
-      const collaborators = await fetchCollaborators(countryIso)
+      const collaborators = await fetchCollaboratorsCountryAccess(countryIso)
+
+      res.json({collaborators})
+    } catch (err) {
+      sendErr(res, err)
+    }
+  })
+
+  // save collaborator country access access
+  app.post('/collaboratorCountryAccess/:countryIso', async (req, res) => {
+    try {
+      checkCountryAccessFromReqParams(req)
+
+      const countryIso = req.params.countryIso
+      const collaboratorTableAccess = req.body
+
+      const collaborators = await db.transaction(persistCollaboratorCountryAccess, [req.user, countryIso, collaboratorTableAccess])
 
       res.json({collaborators})
     } catch (err) {
