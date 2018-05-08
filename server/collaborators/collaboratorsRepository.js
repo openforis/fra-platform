dc2cedafb916bf096313ecdf5511802a53a2a31d
Mino Togna <Cosimo.Togna@Fao.org>
2018-05-08 16:39:42 +0200

    preventing editing of non authorized tables

--- server/collaborators/collaboratorsRepository.js
+++ server/collaborators/collaboratorsRepository.js
@@ -42,6 +42,49 @@ const fetchCollaboratorsCountryAccess = async countryIso => {
 
 }
 
+const fetchCollaboratorCountryAccessTables = async (countryIso, collaboratorId) => {
+
+  const selectRes = await db.query(`
+    SELECT
+      u.id as user_id,
+      u.email,
+      u.name,
+      u.login_email,
+      u.lang,
+      cr.role,
+      ca.tables
+    FROM
+      fra_user u
+    JOIN
+      user_country_role cr
+      ON
+        u.id = cr.user_id
+      AND
+        cr.country_iso = $1
+    LEFT OUTER JOIN
+        collaborators_country_access ca
+        ON ca.user_id = u.id
+        AND ca.country_iso = cr.country_iso
+    WHERE
+      cr.role = $2
+    AND
+      u.id = $3    
+  `, [countryIso, collaborator.role, collaboratorId])
+
+  return R.pipe(
+    camelize,
+    R.map(ca => ({
+        ...ca,
+        tables: R.path(['tables', 'tables'], ca)
+      })
+    ),
+    R.head,
+    R.prop('tables'),
+    R.defaultTo([{tableNo: 'all', section: 'all', label: 'contactPersons.all'}])
+  )(selectRes.rows)
+
+}
+
 const persistCollaboratorCountryAccess = async (client, user, countryIso, collaboratorTableAccess) => {
   const selectRes = await client.query(`
     SELECT id 
@@ -73,5 +116,6 @@ const persistCollaboratorCountryAccess = async (client, user, countryIso, collab
 
 module.exports = {
   fetchCollaboratorsCountryAccess,
+  fetchCollaboratorCountryAccessTables,
   persistCollaboratorCountryAccess
 }
