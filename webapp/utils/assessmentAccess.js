02c51d84cf3655b3cef36406bfe56a48a4b7b60c
Mino Togna <Cosimo.Togna@Fao.org>
2018-06-06 13:39:18 +0200

    methods refactor

--- webapp/utils/assessmentAccess.js
+++ webapp/utils/assessmentAccess.js
@@ -3,50 +3,57 @@ import * as R from 'ramda'
 import { isCollaborator, isReviewer, isAdministrator } from '../../common/countryRole'
 import { isCollaboratorAllowedToEditSectionData } from '../../common/assessmentRoleAllowance'
 
-const getAssessment = (state, name) => R.pipe(
+const getUserInfo = R.path(['user', 'userInfo'])
+const getCountryIso = R.path(['router', 'country'])
+
+const getAssessment = name => R.pipe(
   R.path(['country', 'status', 'assessments', name]),
   R.defaultTo({})
-)(state)
+)
 
-const getFra2020 = state => getAssessment(state, 'fra2020')
-
-const getFra2020Prop = (state, prop, defaultValue = null) => R.pipe(
-  getFra2020,
+const getAssessmentProp = (assessmentName, prop, defaultValue = null) => R.pipe(
+  getAssessment(assessmentName),
   R.prop(prop),
   R.defaultTo(defaultValue)
-)(state)
+)
 
-export const isAssessmentEditable = (state, name) => R.pipe(
-  R.partialRight(getAssessment, [name]),
-  R.prop('canEditData'),
-  R.defaultTo(false)
-)(state)
+export const isAssessmentLocked = (state, assessmentName) => {
+  const canEdit = getAssessmentProp(assessmentName, 'canEditData', false)(state)
 
-const isFRA2020Editable = state => getFra2020Prop(state, 'canEditData', false)
+  const userInfo = getUserInfo(state)
+  const countryIso = getCountryIso(state)
+
+  if (isReviewer(countryIso, userInfo) && !isAdministrator(userInfo)) {
+    const locked = getAssessmentProp(assessmentName, 'locked', true)(state)
+    return locked
+  }
+
+  return !canEdit
+}
+
+const fra2020 = 'fra2020'
 
 const canEditFRA2020Section = (state, section = 'all') => {
-  const userInfo = R.path(['user', 'userInfo'], state)
+  const isLocked = isAssessmentLocked(state, fra2020)
 
-  const canEdit = isFRA2020Editable(state)
-  if (canEdit) {
-    const countryIso = R.path(['router', 'country'], state)
+  if (isLocked) {
+
+    return false
+
+  } else {
+
+    const userInfo = getUserInfo(state)
+    const countryIso = getCountryIso(state)
 
     // if user is collaborator, he could have restricted access to specific tables
     if (isCollaborator(countryIso, userInfo)) {
-      const allowedTables = getFra2020Prop(state, 'tablesAccess')
+      const allowedTables = getAssessmentProp(fra2020, 'tablesAccess')(state)
       return isCollaboratorAllowedToEditSectionData(section, allowedTables)
     }
-
-    //if user is reviewer he must manually unlock the assessment
-    if (isReviewer(countryIso, userInfo) && !isAdministrator(userInfo)) {
-      const locked = getFra2020Prop(state, 'locked', true)
-
-      return !locked
-    }
     return true
-  } else {
-    return false
+
   }
+
 }
 
 export const isFRA2020SectionEditDisabled = (state, section) => {
