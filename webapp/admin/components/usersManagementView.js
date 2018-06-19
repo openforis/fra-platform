5d2a3544cd18356472a194853ef77e945e965f18
minotogna <mino.togna@gmail.com>
2018-06-19 09:36:01 +0200

    added wrapper component for users table+filter

--- webapp/admin/components/usersManagementView.js
+++ webapp/admin/components/usersManagementView.js
@@ -2,27 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as R from 'ramda'
 
-import UsersTable from '../../userManagement/list/usersTable'
+import UsersTableFilterWrapper from '../../userManagement/list/usersTableFilterWrapper'
 import UsersCount from './usersCount'
-import UsersTableFilter from './usersTableFilter'
+
 import EditUserForm from '../../userManagement/edit/editUserForm'
 
 import { fetchAllUsers, removeUser, sendInvitationEmail } from '../../userManagement/actions'
 import { getCountryName } from '../../country/actions'
 
 class UsersManagementView extends React.Component {
-
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      filter: {
-        countries: [],
-        langs: [],
-        roles: [],
-      }
-    }
-  }
 
   componentWillReceiveProps (next) {
     // edit user is completed, reloading users and resetting state
@@ -40,39 +28,8 @@ class UsersManagementView extends React.Component {
     this.props.fetchAllUsers()
   }
 
-  filterUsers () {
-    const {allUsers = []} = this.props
-    const {langs, roles, countries} = R.path(['state', 'filter'], this)
-
-    const filterFn = user => {
-      let include = true
-
-      if (!R.isEmpty(langs)) {
-        // TODO
-      }
-
-      if (!R.isEmpty(roles)) {
-
-        //invitation
-        if (user.invitationUuid)
-          include = R.contains(user.role, roles)
-        else
-          include = R.any(
-            countryRole => R.contains(countryRole.role, roles),
-            user.roles
-          )
-
-      }
-
-      return include
-    }
-
-    return R.filter(filterFn, allUsers)
-  }
-
   render () {
-    const {i18n, userCounts, countryIso} = this.props
-    const users = this.filterUsers()
+    const {i18n, allUsers, userCounts, countryIso} = this.props
 
     const onEditClick = userId => this.setState({editingUserId: userId})
     const editingUserId = R.path(['state', 'editingUserId'], this)
@@ -83,8 +40,10 @@ class UsersManagementView extends React.Component {
                       onCancel={() => this.setState({editingUserId: null})}
       />
       : <div>
-        <UsersTableFilter i18n={i18n} filter={this.state.filter} onChange={filter => this.setState({filter})}/>
-        <UsersTable {...this.props} isAdminTable={true} users={users} onEditClick={onEditClick}/>
+        <UsersTableFilterWrapper {...this.props}
+                                 isAdminTable={true}
+                                 users={allUsers}
+                                 onEditClick={onEditClick}/>
         <UsersCount i18n={i18n} userCounts={userCounts}/>
       </div>
   }
