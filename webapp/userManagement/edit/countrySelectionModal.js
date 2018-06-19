33c6e2ac20b57158897a1d4b0389007e3a097d85
minotogna <mino.togna@gmail.com>
2018-06-19 11:27:42 +0200

    wrapped country selection modal within country-selection div

--- webapp/userManagement/edit/countrySelectionModal.js
+++ webapp/userManagement/edit/countrySelectionModal.js
@@ -22,45 +22,47 @@ class CountrySelectionModal extends React.Component {
     const isSelected = countryIso => R.contains(countryIso, selection)
     const isUnselactable = countryIso => R.contains(countryIso, unselectableCountries)
 
-    return <Modal isOpen={true}>
-      <ModalHeader>
-        {headerLabel}
-        <ModalClose onClose={onClose}/>
-      </ModalHeader>
+    return <div className="country-selection">
+      <Modal isOpen={true}>
+        <ModalHeader>
+          {headerLabel}
+          <ModalClose onClose={onClose}/>
+        </ModalHeader>
 
-      <ModalBody>
-        <div className="edit-user__form-field-country-selection">
-          {
-            R.keys(countryRegions)
-              .sort((a, b) => i18n.t(`country.region.${a}`) < i18n.t(`country.region.${b}`) ? -1 : 1)
-              .map(region =>
-                <div key={region} className="edit-user__form-field-region-container">
-                  <div className="edit-user__form-field-region-label">{i18n.t(`country.region.${region}`)}</div>
-                  {
-                    R.prop(region, countryRegions).map(country => {
-                      const selected = isSelected(country.countryIso)
-                      const isCountryUnselectable = isUnselactable(country.countryIso)
-                      return <div key={country.countryIso}
-                                  className={`edit-user__form-field-country-selector${isCountryUnselectable ? ' disabled' : ''}`}
-                                  onClick={() => isCountryUnselectable ? null : toggleCountry(country.countryIso)}>
-                        <div className={`fra-checkbox${selected ? ' checked' : ''}`}></div>
-                        <div className="edit-user__form-field-country-label">
-                          {getCountryName(country.countryIso, userInfo.lang)}
+        <ModalBody>
+          <div className="edit-user__form-field-country-selection">
+            {
+              R.keys(countryRegions)
+                .sort((a, b) => i18n.t(`country.region.${a}`) < i18n.t(`country.region.${b}`) ? -1 : 1)
+                .map(region =>
+                  <div key={region} className="edit-user__form-field-region-container">
+                    <div className="edit-user__form-field-region-label">{i18n.t(`country.region.${region}`)}</div>
+                    {
+                      R.prop(region, countryRegions).map(country => {
+                        const selected = isSelected(country.countryIso)
+                        const isCountryUnselectable = isUnselactable(country.countryIso)
+                        return <div key={country.countryIso}
+                                    className={`edit-user__form-field-country-selector${isCountryUnselectable ? ' disabled' : ''}`}
+                                    onClick={() => isCountryUnselectable ? null : toggleCountry(country.countryIso)}>
+                          <div className={`fra-checkbox${selected ? ' checked' : ''}`}></div>
+                          <div className="edit-user__form-field-country-label">
+                            {getCountryName(country.countryIso, userInfo.lang)}
+                          </div>
                         </div>
-                      </div>
-                    })
-                  }
-                </div>
-              )
-          }
-        </div>
-      </ModalBody>
+                      })
+                    }
+                  </div>
+                )
+            }
+          </div>
+        </ModalBody>
 
-      <ModalFooter>
-        <button className="btn btn-primary" onClick={onClose}>{i18n.t('editUser.done')}</button>
-      </ModalFooter>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={onClose}>{i18n.t('editUser.done')}</button>
+        </ModalFooter>
 
-    </Modal>
+      </Modal>
+    </div>
   }
 }
 
