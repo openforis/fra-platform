7f6fbd2490087580aa45be34c89a9debe4592291
minotogna <mino.togna@gmail.com>
2018-07-04 15:30:41 +0200

    added check if status is empty before rendering navigation component

--- webapp/navigation/navigation.js
+++ webapp/navigation/navigation.js
@@ -23,7 +23,6 @@ import { assessments } from '../../common/assessmentSectionItems'
 import { roleForCountry } from '../../common/countryRole'
 // import { hasOdps } from '../assessmentFra/extentOfForest/extentOfForestHelper'
 
-
 const roleLabel = (countryIso, userInfo, i18n) => i18n.t(roleForCountry(countryIso, userInfo).labelKey)
 
 class Nav extends React.Component {
@@ -50,75 +49,78 @@ class Nav extends React.Component {
 
     const {userInfo, i18n, path, countries, country, changeAssessment, isPanEuropeanCountry} = this.props
 
-    return <div className="fra-nav__container no-print">
-      {R.isNil(countries)
-        ? null
-        : <div className="fra-nav">
-          <CountrySelection
-            {...this.props}
-            name={country}
-            countries={countries}
-            role={roleLabel(country, userInfo, i18n)}
-          />
-          <div className="nav__scroll-content" ref="scroll_content" onScroll={() => {
-            const content = ReactDOM.findDOMNode(this.refs.scroll_content)
-            this.props.navigationScroll(content.scrollTop)
-          }}>
-            <SectionLink
-              countryIso={country}
-              i18n={i18n}
-              path={path}
-              pathTemplate="/country/:countryIso"
-              label={i18n.t('landing.home')}
-            />
-            {
-              // this.props.showOriginalDataPoints
-              //   ? <NationalData
-              //     label={i18n.t('nationalDataPoint.nationalData')}
-              //     countryIso={country}
-              //     status={R.merge(getReviewStatus('odp'), status.odpStatus)}
-              //     path={path}
-              //     pathTemplate="/country/:countryIso/odps"
-              //     secondaryPathTemplate="/country/:countryIso/odp"
-              //     userInfo={userInfo}/>
-              //   : null
-            }
-            <div className="nav__divider"></div>
-            {
-              R.map(([assessment, sections]) =>
-                  <Assessment
-                    {...this.props}
-                    key={assessment}
-                    assessment={status.assessments[assessment]}
-                    countryIso={country}
-                    changeAssessment={changeAssessment}
-                    userInfo={userInfo}
-                    sections={sections}
-                    getReviewStatus={getReviewStatus}
-                    i18n={i18n}/>
-                , R.toPairs(assessments))
-            }
-            {
-              isPanEuropeanCountry(country)
-                ? <div>
-                  <div className="nav__divider"/>
-                  <SectionLink
-                    countryIso={country}
-                    i18n={i18n}
-                    path={path}
-                    pathTemplate="/country/:countryIso/panEuropeanIndicators"
-                    label={i18n.t('navigation.sectionHeaders.panEuropeanIndicators')}
-                  />
-                </div>
-                : null
-            }
-            <div className="nav__divider"/>
+    console.log(R.isEmpty(status))
 
-            <Footer
-              countryIso={country}
-              {...this.props}/>
+    return <div className="fra-nav__container no-print">
+      {
+        R.isNil(countries) || R.isEmpty(status)
+          ? null
+          : <div className="fra-nav">
+            <CountrySelection
+              {...this.props}
+              name={country}
+              countries={countries}
+              role={roleLabel(country, userInfo, i18n)}
+            />
+            <div className="nav__scroll-content" ref="scroll_content" onScroll={() => {
+              const content = ReactDOM.findDOMNode(this.refs.scroll_content)
+              this.props.navigationScroll(content.scrollTop)
+            }}>
+              <SectionLink
+                countryIso={country}
+                i18n={i18n}
+                path={path}
+                pathTemplate="/country/:countryIso"
+                label={i18n.t('landing.home')}
+              />
+              {
+                // this.props.showOriginalDataPoints
+                //   ? <NationalData
+                //     label={i18n.t('nationalDataPoint.nationalData')}
+                //     countryIso={country}
+                //     status={R.merge(getReviewStatus('odp'), status.odpStatus)}
+                //     path={path}
+                //     pathTemplate="/country/:countryIso/odps"
+                //     secondaryPathTemplate="/country/:countryIso/odp"
+                //     userInfo={userInfo}/>
+                //   : null
+              }
+              <div className="nav__divider"></div>
+              {
+                R.map(([assessment, sections]) =>
+                    <Assessment
+                      {...this.props}
+                      key={assessment}
+                      assessment={status.assessments[assessment]}
+                      countryIso={country}
+                      changeAssessment={changeAssessment}
+                      userInfo={userInfo}
+                      sections={sections}
+                      getReviewStatus={getReviewStatus}
+                      i18n={i18n}/>
+                  , R.toPairs(assessments))
+              }
+              {
+                isPanEuropeanCountry(country)
+                  ? <div>
+                    <div className="nav__divider"/>
+                    <SectionLink
+                      countryIso={country}
+                      i18n={i18n}
+                      path={path}
+                      pathTemplate="/country/:countryIso/panEuropeanIndicators"
+                      label={i18n.t('navigation.sectionHeaders.panEuropeanIndicators')}
+                    />
+                  </div>
+                  : null
+              }
+              <div className="nav__divider"/>
+
+              <Footer
+                countryIso={country}
+                {...this.props}/>
+            </div>
           </div>
-        </div>
       }
     </div>
   }
