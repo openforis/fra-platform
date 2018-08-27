d1d6f2144ce7e35b9bff7903ebc39d5e69014358
minotogna <mino.togna@gmail.com>
2018-08-27 15:15:32 +0200

    added activate/deactivate user button

--- webapp/userManagement/edit/editUserForm.js
+++ webapp/userManagement/edit/editUserForm.js
@@ -54,6 +54,8 @@ class EditUserForm extends React.Component {
     const {i18n, userInfo, countryIso, countries, getCountryName, persistUser, onCancel = defaultOnCancel} = this.props
     const {user, validation} = this.state
 
+    console.log(userInfo)
+
     const hasValidProp = prop => R.pipe(
       R.path([prop, 'valid']),
       R.defaultTo(true)
@@ -233,6 +235,20 @@ class EditUserForm extends React.Component {
 
           </div>
         </div>
+
+        {
+          canEditRoles ?
+            <div className="edit-user__form-item-buttons">
+              <div className="edit-user__form-label"></div>
+              <div className="edit-user__form-field-buttons">
+                <button className="btn btn-secondary"
+                        onClick={() => {}}>
+                  {i18n.t('editUser.deactivate')}
+                </button>
+              </div>
+            </div>
+            : null
+        }
 
         <div className="edit-user__form-item-buttons">
           <div className="edit-user__form-label"></div>
