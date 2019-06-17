badeee8366ca5462106cbc4d23afa6b83e409956
minotogna <mino.togna@gmail.com>
2019-06-17 19:13:41 +0200

    code refactor

--- server/country/countryService.js
+++ server/country/countryService.js
@@ -1,10 +1,12 @@
 const R = require('ramda')
+const Promise = require('bluebird')
 
 const countryConfig = require('./countryConfig')
 
 const countryRepository = require('./countryRepository')
+const traditionalTableRepository = require('../traditionalTable/traditionalTableRepository')
 
-const getCountryConfigFull = async countryIso => {
+const getCountryConfig = async countryIso => {
   const dynamicConfig = await countryRepository.getDynamicCountryConfiguration(countryIso)
 
   const staticConfig = countryConfig[countryIso]
@@ -12,6 +14,25 @@ const getCountryConfigFull = async countryIso => {
   const fullConfig = R.mergeDeepRight(staticConfig, dynamicConfig)
 
   return fullConfig
+}
+
+const getCountryConfigFull = async countryIso => {
+  const [config, result] = await Promise.all([
+    getCountryConfig(countryIso),
+    traditionalTableRepository.read(countryIso, 'climaticDomain')
+  ])
+
+  const climaticDomainPercents = {
+    boreal: (result && result[0][0]) || config.climaticDomainPercents2015.tropical,
+    temperate: (result && result[1][0]) || config.climaticDomainPercents2015.temperate,
+    subtropical: (result && result[2][0]) || config.climaticDomainPercents2015.subtropical,
+    tropical: (result && result[3][0]) || config.climaticDomainPercents2015.tropical,
+  }
+
+  config.climaticDomainPercents = climaticDomainPercents
+
+  return config
+
 }
 
 module.exports = {
