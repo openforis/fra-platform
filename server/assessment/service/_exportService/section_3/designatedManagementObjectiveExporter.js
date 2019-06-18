bb35c7a2102345a21dfa91f03c9d7cb5d3037782
minotogna <mino.togna@gmail.com>
2019-06-18 13:57:16 +0200

    added Fra-years data export table 3a

---
+++ server/assessment/service/_exportService/section_3/designatedManagementObjectiveExporter.js
@@ -0,0 +1,54 @@
+const R = require('ramda')
+const Promise = require('bluebird')
+
+const TraditionalTableService = require('../../../../traditionalTable/traditionalTableRepository')
+
+const TraditionalTableExporter = require('../traditionalTableExporter')
+
+const fieldsPrimary = [
+  'prim_prod', 'prim_prot', 'prim_biodiv', 'prim_socserv', 'prim_multi', 'prim_other'
+]
+const fieldsTotalArea = [
+  'tot_prod', 'tot_prot', 'tot_biodiv', 'tot_socserv', 'tot_other'
+]
+
+class DesignatedManagementObjectiveExporter extends TraditionalTableExporter {
+
+  constructor () {
+
+    super(
+      'primaryDesignatedManagementObjective',
+      [
+        ...fieldsPrimary,
+        ...fieldsTotalArea
+      ],
+      '3a'
+    )
+  }
+
+  fetchData (countryIso) {
+    return Promise.all([
+      TraditionalTableService.read(countryIso, this.tableName),
+      TraditionalTableService.read(countryIso, 'totalAreaWithDesignatedManagementObjective'),
+    ])
+  }
+
+  parseResultRow ([primary, totalArea], yearIdx, year) {
+    const resultRow = {}
+
+    fieldsPrimary.forEach((field, fieldIdx) => {
+      resultRow[field] = R.path([fieldIdx, yearIdx], primary)
+    })
+
+    fieldsTotalArea.forEach((field, fieldIdx) => {
+      resultRow[field] = R.path([fieldIdx, yearIdx], totalArea)
+    })
+
+    return resultRow
+  }
+
+}
+
+const instance = new DesignatedManagementObjectiveExporter()
+
+module.exports = instance
