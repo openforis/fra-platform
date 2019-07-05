294fa5cda440a156b0f1fa2f592b6ce5b2871bfc
minotogna <mino.togna@gmail.com>
2019-07-05 15:29:53 +0200

    fixed climatic domain / updated filename with date

--- server/country/countryService.js
+++ server/country/countryService.js
@@ -23,7 +23,7 @@ const getCountryConfigFull = async countryIso => {
   ])
 
   const climaticDomainPercents = {
-    boreal: (result && result[0][0]) || config.climaticDomainPercents2015.tropical,
+    boreal: (result && result[0][0]) || config.climaticDomainPercents2015.boreal,
     temperate: (result && result[1][0]) || config.climaticDomainPercents2015.temperate,
     subtropical: (result && result[2][0]) || config.climaticDomainPercents2015.subtropical,
     tropical: (result && result[3][0]) || config.climaticDomainPercents2015.tropical,
