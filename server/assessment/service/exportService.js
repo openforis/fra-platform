f588296bb0ac1d03488342a82a011a809cb06e9d
minotogna <mino.togna@gmail.com>
2019-07-08 13:18:20 +0200

    added ndp exporter

--- server/assessment/service/exportService.js
+++ server/assessment/service/exportService.js
@@ -8,6 +8,7 @@ const CountryService = require('../../country/countryService')
 const FRAYearsExporter = require('./fraYears/fraYearsExporter')
 const IntervalYearsExporter = require('./intervals/intervalYearsExporter')
 const AnnualYearsExporter = require('./annual/annualYearsExporter')
+const NdpExporter = require('./ndp/ndpExporter')
 
 const JSONOutput = require('./jsonOutput')
 
@@ -27,6 +28,7 @@ const exportData = async (user, exportType = EXPORT_TYPE.JSON) => {
   const fraYearsOutput = isExportTypeJson ? new JSONOutput('fraYears') : FRAYearsExporter.getCsvOutput()
   const intervalsOutput = isExportTypeJson ? new JSONOutput('intervals') : IntervalYearsExporter.getCsvOutput()
   const annualOutput = isExportTypeJson ? new JSONOutput('annual') : AnnualYearsExporter.getCsvOutput()
+  const ndpOutput = isExportTypeJson ? new JSONOutput('ndp') : NdpExporter.getCsvOutput()
 
   await Promise.each(
     countries.map(async country =>
@@ -34,24 +36,28 @@ const exportData = async (user, exportType = EXPORT_TYPE.JSON) => {
         FRAYearsExporter.getCountryData(country),
         IntervalYearsExporter.getCountryData(country),
         AnnualYearsExporter.getCountryData(country),
+        NdpExporter.getCountryData(country),
       ])
     ),
 
-    ([fraYearsRes, intervalsRes, annualRes]) => {
+    ([fraYearsRes, intervalsRes, annualRes, ndps]) => {
       fraYearsOutput.pushContent(fraYearsRes)
       intervalsOutput.pushContent(intervalsRes)
       annualOutput.pushContent(annualRes)
+      ndpOutput.pushContent(ndps)
     }
   )
 
   fraYearsOutput.pushContentDone()
   intervalsOutput.pushContentDone()
   annualOutput.pushContentDone()
+  ndpOutput.pushContentDone()
 
   return {
     ...fraYearsOutput.output,
     ...intervalsOutput.output,
     ...annualOutput.output,
+    ...ndpOutput.output,
   }
 }
 
