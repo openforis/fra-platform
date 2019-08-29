bafdc04c43a25b1fbcb5074d52468e8e091082a7
minotogna <mino.togna@gmail.com>
2019-08-29 17:00:01 +0200

    excluding Non wood forest products removals when empty in fraTables print

--- webapp/assessmentFra/nonWoodForestProductsRemovals/nonWoodForestProductsRemovalsView.js
+++ webapp/assessmentFra/nonWoodForestProductsRemovals/nonWoodForestProductsRemovalsView.js
@@ -2,15 +2,20 @@ import './style.less'
 
 import React from 'react'
 import { connect } from 'react-redux'
+import * as R from 'ramda'
 
 import LoggedInPageTemplate from '../../app/loggedInPageTemplate'
 import TraditionalTable from '../../traditionalTable/traditionalTable'
-import mainTableSpec , {sectionName} from './mainTableSpec'
+import mainTableSpec, { sectionName } from './mainTableSpec'
 import DefinitionLink from '../../reusableUiComponents/definitionLink'
 import { fetchLastSectionUpdateTimestamp } from '../../audit/actions'
 import NationalDataDescriptions from '../../descriptionBundles/nationalDataDescriptions'
 import GeneralComments from '../../descriptionBundles/generalComments'
 import { isFRA2020SectionEditDisabled } from '../../utils/assessmentAccess'
+import { fetchTableData } from '../../traditionalTable/actions'
+import * as table from '../../traditionalTable/table'
+import { isPrintingOnlyTables } from '../../printAssessment/printAssessment'
+import FraUtils from '../../../common/fraUtils'
 
 const currencyNameTableSpec = i18n => ({
   name: 'nonWoodForestProductsRemovalsCurrency',
@@ -24,7 +29,7 @@ const currencyNameTableSpec = i18n => ({
           {i18n.t('nonWoodForestProductsRemovals.currency')}
         </td>
       },
-      {type: 'textInput'}
+      { type: 'textInput' }
     ]
   ],
   valueSlice: {
@@ -33,50 +38,69 @@ const currencyNameTableSpec = i18n => ({
 })
 
 class NonWoodForestProductsRemovalsView extends React.Component {
-  componentWillMount() {
-    this.props.fetchLastSectionUpdateTimestamp(
-      this.props.match.params.countryIso,
-      'nonWoodForestProductsRemovals'
-    )
+
+  componentWillMount () {
+    const countryIso = this.props.match.params.countryIso
+    const tableSpec = this.props.tableSpecInstance
+
+    this.props.fetchTableData(countryIso, tableSpec)
+    this.props.fetchLastSectionUpdateTimestamp(countryIso, tableSpec.name)
   }
 
-  render() {
-    const {match, i18n, isEditDataDisabled} = this.props
-    const mainTableSpecInstance = mainTableSpec(i18n)
+  render () {
+    const { match, i18n, isEditDataDisabled, tableSpecInstance, tableData } = this.props
 
-    return <LoggedInPageTemplate>
+    const render = isPrintingOnlyTables() ? FraUtils.hasData(tableData) : true
 
-      <h2 className="title only-print">
-        7c {i18n.t('nonWoodForestProductsRemovals.nonWoodForestProductsRemovals')}
-      </h2>
+    return render &&
+      <LoggedInPageTemplate>
 
-      <div className="fra-view__content">
-        <NationalDataDescriptions section={mainTableSpecInstance.name} countryIso={match.params.countryIso} disabled={isEditDataDisabled} />
-        <h2 className="headline no-print">
-          {i18n.t('nonWoodForestProductsRemovals.nonWoodForestProductsRemovals')}
+        <h2 className="title only-print">
+          7c {i18n.t('nonWoodForestProductsRemovals.nonWoodForestProductsRemovals')}
         </h2>
-        <div className="fra-view__section-toolbar">
-          <DefinitionLink className="margin-right-big" document="tad" anchor="7c" title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
-          <DefinitionLink className="align-left" document="faq" anchor="7c" title={i18n.t('definition.faqLabel')} lang={i18n.language}/>
+
+        <div className="fra-view__content">
+          <NationalDataDescriptions section={tableSpecInstance.name} countryIso={match.params.countryIso}
+                                    disabled={isEditDataDisabled}/>
+          <h2 className="headline no-print">
+            {i18n.t('nonWoodForestProductsRemovals.nonWoodForestProductsRemovals')}
+          </h2>
+          <div className="fra-view__section-toolbar">
+            <DefinitionLink className="margin-right-big" document="tad" anchor="7c"
+                            title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
+            <DefinitionLink className="align-left" document="faq" anchor="7c" title={i18n.t('definition.faqLabel')}
+                            lang={i18n.language}/>
+          </div>
+          <TraditionalTable tableSpec={tableSpecInstance} countryIso={match.params.countryIso}
+                            disabled={isEditDataDisabled}/>
+          <div className="page-break"/>
+          <div className="fra-secondary-table__wrapper">
+            <TraditionalTable tableSpec={currencyNameTableSpec(i18n)} countryIso={match.params.countryIso}
+                              disabled={isEditDataDisabled}/>
+          </div>
+          <GeneralComments
+            section={tableSpecInstance.name}
+            countryIso={match.params.countryIso}
+            disabled={isEditDataDisabled}
+          />
         </div>
-        <TraditionalTable tableSpec={mainTableSpecInstance} countryIso={match.params.countryIso} disabled={isEditDataDisabled}/>
-        <div className="page-break"/>
-        <div className="fra-secondary-table__wrapper">
-          <TraditionalTable tableSpec={currencyNameTableSpec(i18n)} countryIso={match.params.countryIso} disabled={isEditDataDisabled}/>
-        </div>
-        <GeneralComments
-          section={mainTableSpecInstance.name}
-          countryIso={match.params.countryIso}
-          disabled={isEditDataDisabled}
-        />
-      </div>
-    </LoggedInPageTemplate>
+      </LoggedInPageTemplate>
   }
 }
 
-const mapStateToProps = state => ({
-  i18n: state.user.i18n,
-  isEditDataDisabled: isFRA2020SectionEditDisabled(state, sectionName)
-})
+const mapStateToProps = state => {
+  const i18n = state.user.i18n
+  const tableSpecInstance = mainTableSpec(i18n)
 
-export default connect(mapStateToProps, {fetchLastSectionUpdateTimestamp})(NonWoodForestProductsRemovalsView)
+  return {
+    i18n,
+    isEditDataDisabled: isFRA2020SectionEditDisabled(state, sectionName),
+    tableSpecInstance,
+    tableData: R.path(['traditionalTable', tableSpecInstance.name, 'tableData'], state) || table.createTableData(tableSpecInstance),
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetchLastSectionUpdateTimestamp,
+  fetchTableData
+})(NonWoodForestProductsRemovalsView)
