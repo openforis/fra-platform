10649672b9a506f126e9c52b864093dd15d4fe50
minotogna <mino.togna@gmail.com>
2019-09-06 14:06:50 +0200

    added desk study to csv download countries

--- webapp/navigation/components/countrySelection.js
+++ webapp/navigation/components/countrySelection.js
@@ -81,6 +81,7 @@ const CountryList = ({ isOpen, countries, userInfo, ...props }) => {
                       name: getCountryName(country.countryIso, i18n.language),
                       status: i18n.t(`assessment.status.${country.fra2020Assessment}.label`),
                       edited: getRelativeDate(country.lastEdit, i18n) || i18n.t('audit.notStarted'),
+                      deskStudy: i18n.t(`yesNoTextSelect.${R.propEq('fra2020DeskStudy', true, country) ? 'yes' : 'no'}`)
                     }), roleCountries)
                 ),
                 R.flatten
@@ -89,7 +90,8 @@ const CountryList = ({ isOpen, countries, userInfo, ...props }) => {
             headers={[
               { label: i18n.t('admin.country'), key: 'name' },
               { label: i18n.t('countryListing.fra2020'), key: 'status' },
-              { label: i18n.t('audit.edited'), key: 'edited' }
+              { label: i18n.t('audit.edited'), key: 'edited' },
+              { label: i18n.t('assessment.deskStudy'), key: 'deskStudy' },
             ]}>
             <Icon className="icon-sub icon-white" name="hit-down"/>CSV
           </CSVLink>
