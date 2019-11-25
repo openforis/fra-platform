67c9b9c83926b93ad25a450d8819b91a740034d3
Miró Sorja <ping@mirosorja.com>
2019-11-25 12:12:55 +0100

    Added redux devtools

--- webapp/app/main.js
+++ webapp/app/main.js
@@ -5,13 +5,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Page from './routes'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createDebounce from 'redux-debounced'
 import reducer from './rootReducer'
 import * as loginStatusChecker from '../user/loginStatusChecker'
 
-const store = createStore(reducer, applyMiddleware(createDebounce(), thunkMiddleware))
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
+
+const middleware = [createDebounce(), thunkMiddleware]
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+);
+
+const store = createStore(reducer, enhancer);
 
 function renderApp () {
 
