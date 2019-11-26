408025f7a2670fb8f7471b141276e2a94b5e03ed
Miró Sorja <ping@mirosorja.com>
2019-11-26 12:18:44 +0100

    initial commit for webpack upgrade

--- webpack.config.babel-test.js
+++ webpack.config.babel-test.js
@@ -1,8 +1,10 @@
 const nodeExternals = require('webpack-node-externals')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  plugins: [new ExtractTextPlugin({filename: 'test-style.css'})],
+  mode: 'test',
+  plugins: [new MiniCssExtractPlugin({filename: 'test-style.css'})],
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
@@ -19,12 +21,16 @@ module.exports = {
         }
       },
       {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader']
-        })
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
+      // {
+      //   test: /\.less$/,
+      //   use: MiniCssExtractPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: ['css-loader', 'less-loader']
+      //   })
+      // }
     ]
   }
 }
