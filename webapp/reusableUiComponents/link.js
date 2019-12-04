327af7b88d5476ee2bd1647dc28e4602b52bf40c
Miró Sorja <ping@mirosorja.com>
2019-12-04 17:14:05 +0100

    Remove reusableUiComponents/link

--- webapp/reusableUiComponents/link.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import * as R from 'ramda'
-
-import { connect } from 'react-redux'
-
-const link = ({to, ...props}) => <a href={`#${to}`} {...props} />
-
-export const Link = connect(R.empty, {})(link)
