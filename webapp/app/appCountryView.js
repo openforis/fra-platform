171a22be4f32ea5cfb073fe554bb17d5e331cf34
Miró Sorja <ping@mirosorja.com>
2019-12-04 11:07:48 +0100

    Added appCountryView

---
+++ webapp/app/appCountryView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class appCountryView extends Component {
+    render() {
+        console.log('?_?')
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
