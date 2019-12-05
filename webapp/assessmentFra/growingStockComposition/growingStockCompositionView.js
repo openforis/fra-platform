d0eaff09664e86813b93ddebdba02c787701eec3
Miró Sorja <ping@mirosorja.com>
2019-12-05 10:33:55 +0100

    growingStockCompositionView.js: Refactor to functional

--- webapp/assessmentFra/growingStockComposition/growingStockCompositionView.js
+++ webapp/assessmentFra/growingStockComposition/growingStockCompositionView.js
@@ -1,6 +1,7 @@
 import './style.less'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import * as R from 'ramda'
 
 import TraditionalTable from '../../traditionalTable/traditionalTable'
@@ -16,52 +17,48 @@ import { isPrintingOnlyTables } from '../../printAssessment/printAssessment'
 import FraUtils from '../../../common/fraUtils'
 import { fetchTableData } from '../../traditionalTable/actions'
 
-class GrowingStockCompositionView extends React.Component {
+const GrowingStockCompositionView = props => {
+  const { i18n, isEditDataDisabled, tableData, tableSpecInstance } = props
+  const { countryIso } = useParams()
 
-  componentDidMount () {
-    const countryIso = this.props.match.params.countryIso
-    const tableSpec = this.props.tableSpecInstance
+  useEffect(() => {
+    props.fetchTableData(countryIso, tableSpecInstance)
+    props.fetchLastSectionUpdateTimestamp(countryIso, tableSpecInstance.name)
+  }, [])
 
-    this.props.fetchTableData(countryIso, tableSpec)
-    this.props.fetchLastSectionUpdateTimestamp(countryIso, tableSpec.name)
-  }
+  const render = isPrintingOnlyTables() ? FraUtils.hasData(tableData) : true
 
-  render () {
-    const { match, i18n, isEditDataDisabled, tableData, tableSpecInstance } = this.props
+  if (!render) return null
+  return <>
+      <h2 className="title only-print">
+        {`${isPrintingOnlyTables() ? '' : '2b '}${i18n.t('growingStockComposition.growingStockComposition')}`}
+      </h2>
 
-    const render = isPrintingOnlyTables() ? FraUtils.hasData(tableData) : true
-
-    return render &&
-      <>
-        <h2 className="title only-print">
-          {`${isPrintingOnlyTables() ? '' : '2b '}${i18n.t('growingStockComposition.growingStockComposition')}`}
+      <div className="fra-view__content growing-stock-composition-view">
+        <NationalDataDescriptions section={tableSpecInstance.name} countryIso={countryIso}
+          disabled={isEditDataDisabled} />
+        <AnalysisDescriptions section={tableSpecInstance.name} countryIso={countryIso}
+          disabled={isEditDataDisabled} />
+        <h2 className="headline no-print">
+          {i18n.t('growingStockComposition.growingStockComposition')}
         </h2>
-
-        <div className="fra-view__content growing-stock-composition-view">
-          <NationalDataDescriptions section={tableSpecInstance.name} countryIso={match.params.countryIso}
-                                    disabled={isEditDataDisabled}/>
-          <AnalysisDescriptions section={tableSpecInstance.name} countryIso={match.params.countryIso}
-                                disabled={isEditDataDisabled}/>
-          <h2 className="headline no-print">
-            {i18n.t('growingStockComposition.growingStockComposition')}
-          </h2>
-          <div className="fra-view__section-toolbar">
-            <DefinitionLink className="margin-right-big" document="tad" anchor="2b"
-                            title={i18n.t('definition.definitionLabel')} lang={i18n.language}/>
-            <DefinitionLink className="align-left" document="faq" anchor="2b" title={i18n.t('definition.faqLabel')}
-                            lang={i18n.language}/>
-            <div className="support-text">{i18n.t('growingStockComposition.rankingYear')}</div>
-          </div>
-          <TraditionalTable tableSpec={tableSpecInstance} countryIso={match.params.countryIso}
-                            disabled={isEditDataDisabled}/>
-          <GeneralComments
-            section={tableSpecInstance.name}
-            countryIso={match.params.countryIso}
-            disabled={isEditDataDisabled}
-          />
+        <div className="fra-view__section-toolbar">
+          <DefinitionLink className="margin-right-big" document="tad" anchor="2b"
+            title={i18n.t('definition.definitionLabel')} lang={i18n.language} />
+          <DefinitionLink className="align-left" document="faq" anchor="2b" title={i18n.t('definition.faqLabel')}
+            lang={i18n.language} />
+          <div className="support-text">{i18n.t('growingStockComposition.rankingYear')}</div>
         </div>
-      </>
-  }
+        <TraditionalTable tableSpec={tableSpecInstance} countryIso={countryIso}
+          disabled={isEditDataDisabled} />
+        <GeneralComments
+          section={tableSpecInstance.name}
+          countryIso={countryIso}
+          disabled={isEditDataDisabled}
+        />
+      </div>
+    </>
+
 
 }
 
