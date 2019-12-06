ef5a32703d3bbf589cd6c6a186a2067924256a00
Miró Sorja <ping@mirosorja.com>
2019-12-06 09:26:41 +0100

    usersManagementView.js: Refactor to functional

--- webapp/admin/components/usersManagementView.js
+++ webapp/admin/components/usersManagementView.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import * as R from 'ramda'
 
 import UsersTableFilterWrapper from '../../userManagement/list/usersTableFilterWrapper'
@@ -11,57 +12,55 @@ import { fetchAllUsers, removeUser, sendInvitationEmail } from '../../userManage
 import { getCountryName } from '../../country/actions'
 import { administrator } from '../../../common/countryRole'
 
-class UsersManagementView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      editUserStatus: null,
-      editingUserId: null
+const UsersManagementView = props => {
+  const {
+    fetchAllUsers,
+    editUserStatus,
+    i18n,
+    allUsers,
+    userCounts,
+  } = props
+  const { countryIso } = useParams()
+  const [editingUserId, setEditingUserId] = useState(null)
+
+  useEffect(() => {
+    fetchAllUsers()
+  }, [])
+
+  useEffect(() => {
+    if (editingUserId && editUserStatus == 'completed') {
+      setEditingUserId(null)
+      fetchAllUsers()
     }
+  }, [editUserStatus])
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     editUserStatus: null,
+  //     editingUserId: null
+  //   }
+  // }
+  if (editingUserId) {
+    return <EditUserForm userId={editingUserId}
+      countryIso={countryIso}
+      onCancel={userId => setEditingUserId(null)}
+    />
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (R.prop('editUserStatus', this.props) === 'completed' && R.prop('editUserStatus', prevProps) === 'loaded') {
-      this.setState({editingUserId: null})
-      this.fetchUsers()
-    }
-  }
-
-  componentDidMount () {
-    this.fetchUsers()
-  }
-
-  fetchUsers () {
-    this.props.fetchAllUsers()
-  }
-
-  render () {
-    const {i18n, allUsers, userCounts, countryIso} = this.props
-
-    const onEditClick = userId => this.setState({editingUserId: userId})
-    const editingUserId = R.path(['state', 'editingUserId'], this)
-
-    return editingUserId
-      ? <EditUserForm userId={editingUserId}
-                      countryIso={countryIso}
-                      onCancel={() => this.setState({editingUserId: null})}
-      />
-      : <>
-        <UsersTableFilterWrapper {...this.props}
-                                 isAdminTable={true}
-                                 users={allUsers}
-                                 onEditClick={onEditClick}/>
-        <UsersCount i18n={i18n} userCounts={userCounts}/>
-      </>
-  }
-
+  return <>
+    <UsersTableFilterWrapper {...props}
+      isAdminTable={true}
+      users={allUsers}
+      onEditClick={userId => setEditingUserId(userId)} />
+    <UsersCount i18n={i18n} userCounts={userCounts} />
+  </>
 }
 
 const mapStateToProps = (state, props) =>
   ({
     i18n: state.user.i18n,
     userInfo: state.user.userInfo,
-    countryIso: R.path(['match', 'params', 'countryIso'], props),
     allUsers: state.userManagement.allUsers,
     userCounts: state.userManagement.userCounts,
     editUserStatus: R.path(['userManagement', 'editUser', 'status'], state),
@@ -70,5 +69,5 @@ const mapStateToProps = (state, props) =>
 
 export default connect(
   mapStateToProps,
-  {fetchAllUsers, removeUser, sendInvitationEmail, getCountryName}
+  { fetchAllUsers, removeUser, sendInvitationEmail, getCountryName }
 )(UsersManagementView)
