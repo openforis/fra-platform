2abc8f820bc9a32e323b80ae514a9b418f6f43a1
Miró Sorja <ping@mirosorja.com>
2019-12-09 10:40:56 +0100

    Added NotFound as catch for missing urls under country/:countryIso/foo

--- webapp/app/notfound.js
+++ webapp/app/notfound.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
-const Notfound = () => (
-  <div className="notfound">
-    <img src="/img/tucan.svg" alt="tucan"/>
-    <p className="subhead"><strong>Page not found</strong></p>
-    <a href="/" className="link">Back to main page</a>
-  </div>
-)
+const Notfound = () => {
+  const { countryIso } = useParams()
 
+  return (
+    <div className="notfound">
+      <img src="/img/tucan.svg" alt="tucan" />
+      <p className="subhead"><strong>Page not found</strong></p>
+      <Link to={`/country/${countryIso}`}>
+        Back to main page
+      </Link>
+    </div>
+  )
+}
 export default Notfound
