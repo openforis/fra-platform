4e1f063e10f40a7bef2ebff768b45cedb6c7b1f7
Miró Sorja <ping@mirosorja.com>
2019-12-16 11:04:25 +0100

    Added appView and appViewExport

---
+++ webapp/loggedin/appView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoggedInView from './loggedInView'
+
+const AppView = props => {
+  return (
+    <>
+      <LoggedInView />
+    </>
+  )
+}
+
+export default AppView
