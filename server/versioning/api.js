d80f35af34e8cd88d6921b917278dfd9db6a46b1
Miró Sorja <ping@mirosorja.com>
2020-01-02 16:47:58 +0200

    versioning api: create endpoint

--- server/versioning/api.js
+++ server/versioning/api.js
@@ -1,24 +1,36 @@
 const db = require('../db/db')
+const { sendErr, sendOk } = require('../utils/requestUtils')
+const versions = [
+  {
+    version: '1.0.0',
+    timestamp: '1576677210872',
+    createdBy: 'Admin User',
+    status: 'pending'
+  },
+  {
+    version: '2.0.0',
+    timestamp: '1576677210872',
+    createdBy: 'Admin User2',
+    status: 'pending'
+  }
+]
 
 module.exports.init = app => {
 
+  app.post('/versioning/', async (req, res) => {
+    try {
+      const version = '';
+      // Create new schema
+      console.log(req.body)
+      res.json(versions)
+    } catch (err) {
+      console.log(err)
+      sendErr(res, err)
+    }
+  })
+
   app.get('/versioning/', async (req, res) => {
-    res.json(
-      [
-        {
-          version: '1.0.0',
-          timestamp: '1576677210872',
-          createdBy: 'Admin User',
-          status: 'pending'
-        },
-        {
-          version: '2.0.0',
-          timestamp: '1576677210872',
-          createdBy: 'Admin User2',
-          status: 'pending'
-        }
-      ]
-    )
+    res.json(versions)
   })
 
 }
