fbfa30a036356026315201f093e1bec3a6e001a2
Miró Sorja <ping@mirosorja.com>
2020-01-09 12:15:37 +0200

    versioningRep: newSchemaVersion, updateVersionStatus, getRunningVersions, getAllPendingVersions, getPendingVersions

--- server/versioning/versioningRepository.js
+++ server/versioning/versioningRepository.js
@@ -1,6 +1,14 @@
 const db = require('../db/db')
 const camelize = require('camelize')
 
+/*
+  Possible status for version
+  'pending', DEFAULT
+  'running',
+  'completed',
+  'failed'
+*/
+
 const getAllVersions = async () => {
   const result = await db.query(`
       SELECT v.id,
@@ -16,6 +24,53 @@ const getAllVersions = async () => {
   return camelize(result.rows)
 }
 
+// Returns all pending versions from fra_version
+// that have publish_time (timestamp) in next 5 minutes
+const getPendingVersions = async () => {
+  const result = await db.query(`
+    SELECT *
+    FROM fra_version v
+    WHERE v.publish_time > NOW() - interval '1 minute'
+      AND v.publish_time <= NOW() + interval '5 minute'
+      AND v.status = 'pending'
+    ORDER BY v.publish_time;
+  `)
+  return camelize(result.rows)
+}
+
+// Returns all pending versions from fra_version
+const getAllPendingVersions = async () => {
+  const result = await db.query(`
+    SELECT *
+    FROM fra_version v
+    WHERE v.status = 'pending'
+    ORDER BY v.publish_time;
+  `)
+  return camelize(result.rows)
+}
+
+// Returns all running versions from fra_version
+const getRunningVersions = async () => {
+  const result = await db.query(`
+    SELECT *
+    FROM fra_version v
+    WHERE v.status = 'running'
+    ORDER BY v.publish_time;
+  `)
+  return camelize(result.rows)
+}
+
+const updateVersionStatus = async (id, status) => {
+  await db.query(`
+      UPDATE fra_version
+      SET
+        status = $2,
+        updated_at = NOW()
+      WHERE
+        id = $1;
+  `, [id, status])
+}
+
 const addVersion = async (userId, version, timestamp) => {
   const query = `
     INSERT INTO fra_version (created_by, version_number, status, publish_time)
@@ -27,7 +82,29 @@ const addVersion = async (userId, version, timestamp) => {
   await db.query(query, [userId, version, timestamp])
 }
 
+const newSchemaVersion = async (to, from = 'public') => {
+  if (!to) {
+    console.error(`
+      versioningRepository/newSchemaVersion: param @to not defined or null:
+    `, to)
+    return
+  }
+
+  console.log(`Creating new schema version from ${from} to ${to}`);
+
+  const query = `
+    SELECT clone_schema($1, $2);
+  `
+  // Replace all dots to underscores to avoid problems
+  await db.query(query, [from.replace(/\./g, '_'), to.replace(/\./g, '_')])
+}
+
 module.exports = {
   addVersion,
-  getAllVersions
+  getAllVersions,
+  getPendingVersions,
+  newSchemaVersion,
+  updateVersionStatus,
+  getAllPendingVersions,
+  getRunningVersions
 }
