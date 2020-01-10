165f74d438c57f6d5bd54843ad7adcaa8c9d3930
Miró Sorja <ping@mirosorja.com>
2020-01-10 12:43:15 +0200

    VersioningViewTable: Minor fixes to UI

--- webapp/loggedin/admin/components/versioningView/VersioningViewTable.js
+++ webapp/loggedin/admin/components/versioningView/VersioningViewTable.js
@@ -1,32 +1,49 @@
 import React from 'react';
-import { classNames, sortVersions } from "./versioningViewUtils";
+import { Link, useParams } from 'react-router-dom'
+import Icon from '@webapp/components/icon'
+import { classNames, sortVersions, formatDate } from './versioningViewUtils'
 
-const VersioningViewTableRow = ({ version, timestamp, name, email, status }) => {
-  return <tr>
+const VersioningViewTableRow = ({ deleteVersion, id, uid, version, timestamp, name, email, status }) => {
+  const { countryIso } = useParams()
+  return <tr className={`tr-${status}`}>
     <td className={classNames.td}>{version}</td>
-    <td className={classNames.td}>{timestamp}</td>
     <td className={classNames.td}>
-      <a href={`mailto:${email}`}>{name}</a>
+      {status === 'pending' ? 'Scheduled at:' : ''}
+      {formatDate(timestamp)}
+    </td>
+    <td className={classNames.td}>
+      <Link to={`/country/${countryIso}/user/${uid}`}>{name}</Link>
     </td>
     <td className={classNames.td}>{status}</td>
-    <td className={classNames.td}>remove []</td>
+    <td className={classNames.td}>
+      <button onClick={() => deleteVersion(id)} className={classNames.button}>
+        <Icon className={classNames.icon} name="remove" /> Delete
+      </button>
+    </td>
   </tr>
 }
 
 export const VersioningViewTable = (props) => {
-  const { versions } = props;
+  const { versions, deleteVersion } = props;
   const thead = ['Version Number', 'Timestamp', 'Created By', 'Status', ''];
-  return <table style={{ maxWidth: 700 }} className={classNames.table}>
-    <thead>
-      <tr>
-        <th className={classNames.th} colSpan="5">Database Versions</th>
-      </tr>
-      <tr>
-        {thead.map((title, i) => <th className={classNames.th} key={i}>{title}</th>)}
-      </tr>
-    </thead>
-    <tbody>
-      {sortVersions(versions).map((version, i) => <VersioningViewTableRow key={i} {...version} />)}
-    </tbody>
-  </table>;
+  return (
+    <div className="fra-table__container">
+      <div className="fra-table__scroll-wrapper">
+        <table style={{ maxWidth: 700 }} className={classNames.table}>
+          <thead>
+            <tr>
+              <th className={classNames.th} colSpan="5">Database Versions</th>
+            </tr>
+            <tr>
+              {thead.map((title, i) => <th className={classNames.th} key={i}>{title}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            {sortVersions(versions).map((version, i) =>
+              <VersioningViewTableRow deleteVersion={deleteVersion} key={i} {...version} />)}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 };
