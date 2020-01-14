89c9807c74a3f8dad02144b05f6c38ecd83d224c
Miró Sorja <ping@mirosorja.com>
2020-01-14 12:25:18 +0200

    versioningScheduler: 5 minute timer

--- server/system/schedulers/versioningScheduler.js
+++ server/system/schedulers/versioningScheduler.js
@@ -5,12 +5,10 @@ const handleNewVersion = async (version) => {
   const {
     id,
     versionNumber,
-    // publishTime: timestamp,
   } = version
 
   try {
 
-    const startTime = +new Date() / 1000
     await versioningRepository.updateVersionStatus(id, 'running')
     
     try {
@@ -23,9 +21,6 @@ const handleNewVersion = async (version) => {
 
     await versioningRepository.updateVersionStatus(id, 'completed')
 
-    console.log((+new Date() / 1000) - startTime)
-    console.log(`Created new schema version: ${versionNumber} (${id})`)
-
   } catch (error) {
     await versioningRepository.updateVersionStatus(id, 'failed')
     console.error(`Error creating new schema: ${error.toString()}`)
@@ -34,14 +29,12 @@ const handleNewVersion = async (version) => {
 
 const init = async () => {
   // Check every 5 minutes for new entries/if we need to do something
-  // schedule.scheduleJob('*/5 * * * *', async () => {
-  schedule.scheduleJob('*/5 * * * * *', async () => {
+  schedule.scheduleJob('*/5 * * * *', async () => {
     /* 
       3. lock system
     */
 
     const versions = await versioningRepository.getPendingVersions();
-    console.log({ versions, ts: +new Date() })
     // If no versions, do nothing
     if (!versions.length) {
       return
