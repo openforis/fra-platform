2d241554cd9c536f24b70b6c046169810c944447
Miró Sorja <ping@mirosorja.com>
2020-02-07 10:20:15 +0100

    Remove unused import

--- webapp/loggedin/applicationError/errorComponent.js
+++ webapp/loggedin/applicationError/errorComponent.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { clearApplicationError } from './actions'
 import Icon from '@webapp/components/icon'
-import * as R from 'ramda'
 import './style.less'
 
 import * as ApplicationErrorState from '@webapp/loggedin/applicationError/applicationErrorState'
