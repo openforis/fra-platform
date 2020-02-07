5fb192c3373a700e94ba4b579f1e56f4ef471db1
Miró Sorja <ping@mirosorja.com>
2020-02-07 09:54:42 +0100

    assocStatus for autosave-state

--- webapp/autosave/reducer.js
+++ webapp/autosave/reducer.js
@@ -1,14 +1,19 @@
+import * as R from 'ramda'
 import { autoSaveStart, autoSaveComplete } from './actions'
 import { applyReducerFunction } from '@webapp/utils/reduxUtils'
 import { lastSectionUpdateTimestampReceived } from '@webapp/audit/actions'
 
 import { status } from './autosave'
 
+import * as AutosaveState from '@webapp/autosave/autosaveState'
+
 const actionHandlers = {
-  [autoSaveStart]: (state) => ({ ...state, status: status.saving }),
-  [autoSaveComplete]: (state) => ({ ...state, status: status.complete }),
-  [lastSectionUpdateTimestampReceived]: (state, action) =>
-    ({ ...state, status: 'lastSaveTimestampReceived', lastSaveTimeStamp: action.timeStamp })
+  [autoSaveStart]: (state) => AutosaveState.assocStatus(status.saving)(state),
+  [autoSaveComplete]: (state) => AutosaveState.assocStatus(status.complete)(state),
+  [lastSectionUpdateTimestampReceived]: (state, { timeStamp }) => R.pipe(
+    AutosaveState.assocStatus(status.lastSaveTimestampReceived),
+    AutosaveState.assocLastSaveTimeStamp(timeStamp)
+  )(state)
 }
 
 export default (state = {}, action) => applyReducerFunction(actionHandlers, state, action)
