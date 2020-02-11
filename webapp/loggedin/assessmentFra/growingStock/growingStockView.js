9736fe944cdf89cc151622fb089dc65f53d33b46
minotogna <mino.togna@gmail.com>
2020-02-11 10:53:42 +0100

    Table 2a. added new line in print view before avg table

--- webapp/loggedin/assessmentFra/growingStock/growingStockView.js
+++ webapp/loggedin/assessmentFra/growingStock/growingStockView.js
@@ -238,6 +238,8 @@ const GrowingStock = (props) => {
       <div className="support-text full-width no-print">{i18n.t('growingStock.supportText')}</div>
     </div>
 
+    <div className="page-break" />
+
     {/*AVG Table*/}
     <div className="fra-table__container">
       <div className="fra-table__scroll-wrapper">
