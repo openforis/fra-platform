f3b29785b305a6d4de74695d54ce9aa78ff4e2d0
Miró Sorja <ping@mirosorja.com>
2020-02-21 13:28:09 +0100

    authApiMiddleware: use next(error)

--- server/auth/authApiMiddleware.js
+++ server/auth/authApiMiddleware.js
@@ -1,19 +1,18 @@
 const { checkCountryAccessFromReqParams } = require('../utils/accessControl')
-const { sendErr, sendOk, getUser, getUserName } = require('../utils/requestUtils')
+const { getUser } = require('../utils/requestUtils')
 const { allowedToEditDataCheck } = require('../assessment/assessmentEditAccessControl')
 
 const requireCountryEditPermission = async (req, res, next) => {
   const section = req.params.section || req.query.section
   const countryIso = req.params.countryIso || req.query.countryIso
   const user = getUser(req)
-  const userName = getUserName(req)
 
   try {
     checkCountryAccessFromReqParams(req)
     await allowedToEditDataCheck(countryIso, user, section)
     next()
   } catch (error) {
-    sendErr(res, error)
+    next(error)
   }
 }
 
