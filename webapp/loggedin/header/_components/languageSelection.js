b896bdd3a6c4772f595c8a098999a0daacd6de07
minotogna <mino.togna@gmail.com>
2020-02-21 19:59:00 +0100

    Removed unused imports

--- webapp/loggedin/header/_components/languageSelection.js
+++ webapp/loggedin/header/_components/languageSelection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import * as R from 'ramda'
 
 import { PopoverControl } from '@webapp/components/popoverControl'
