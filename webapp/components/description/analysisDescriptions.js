ec1d1db29170112c82a91fc98996e563561dd951
minotogna <mino.togna@gmail.com>
2020-02-22 16:44:32 +0100

    using i18n hook in Comment components

--- webapp/components/description/analysisDescriptions.js
+++ webapp/components/description/analysisDescriptions.js
@@ -1,37 +1,32 @@
 import React from 'react'
-import { connect, useSelector } from 'react-redux'
+
+import { isPrintingMode, isPrintingOnlyTables } from '@webapp/loggedin/printAssessment/printAssessment'
 
 import CommentableDescription from '@webapp/components/description/commentableDescription'
-import { isPrintingMode, isPrintingOnlyTables } from '@webapp/loggedin/printAssessment/printAssessment'
-import * as AppState from '@webapp/app/appState'
-import * as UserState from '@webapp/user/userState'
+import useI18n from '@webapp/components/hooks/useI18n'
 
 const AnalysisDescriptions = props => {
-  const countryIso = useSelector(AppState.getCountryIso)
+  const i18n = useI18n()
 
   return !isPrintingOnlyTables() &&
     <div className="fra-description__container">
-      <h2 className="headline fra-description__group-header">{props.i18n.t('description.analysisAndProcessing')}</h2>
+      <h2 className="headline fra-description__group-header">{i18n.t('description.analysisAndProcessing')}</h2>
       <CommentableDescription
-        title={props.i18n.t('description.estimationAndForecasting')}
+        title={i18n.t('description.estimationAndForecasting')}
         name="estimationAndForecasting"
         showAlertEmptyContent={!isPrintingMode()}
         showDashEmptyContent={isPrintingMode()}
-        countryIso={countryIso}
         {...props}
       />
       <CommentableDescription
-        title={props.i18n.t('description.reclassification')}
+        title={i18n.t('description.reclassification')}
         name="reclassification"
         showAlertEmptyContent={!isPrintingMode()}
-        countryIso={countryIso}
         showDashEmptyContent={isPrintingMode()}
         {...props}
       />
     </div>
 }
 
-const mapStateToProps = (state) => ({ i18n: UserState.getI18n(state) })
-
-export default connect(mapStateToProps, {})(AnalysisDescriptions)
+export default AnalysisDescriptions
 
