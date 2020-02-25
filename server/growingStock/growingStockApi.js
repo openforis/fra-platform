6c771202a335ea9078eaf53693604c23070cb821
Miró Sorja <ping@mirosorja.com>
2020-02-25 11:37:04 +0100

    growingStockApi: Schema name

--- server/growingStock/growingStockApi.js
+++ server/growingStock/growingStockApi.js
@@ -1,8 +1,9 @@
 const db = require('../db/db')
 const { sendErr } = require('../utils/requestUtils')
-const { checkCountryAccessFromReqParams } = require('../utils/accessControl')
 const repository = require('./growingStockRepository')
 const Auth = require('../auth/authApiMiddleware')
+
+const VersionService = require('../versioning/service')
 
 const GrowingStockService = require('./growingStockService')
 
@@ -10,10 +11,8 @@ module.exports.init = app => {
 
   app.get('/growingStock/:countryIso', async (req, res) => {
     try {
-      checkCountryAccessFromReqParams(req)
-
-      const growingStock = await GrowingStockService.getGrowingStock(req.params.countryIso)
-
+      const schemaName = await VersionService.getDatabaseSchema(req)
+      const growingStock = await GrowingStockService.getGrowingStock(req.params.countryIso, schemaName)
       res.json(growingStock)
     } catch (err) {
       sendErr(res, err)
