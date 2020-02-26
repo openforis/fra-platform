15caf661c289d7779bf4eaf156b716b4fc26bbf5
minotogna <mino.togna@gmail.com>
2020-02-26 15:00:46 +0100

    CountryListDownload: extracted to a separate component

--- webapp/loggedin/countrySelection/components/countryList.js
+++ webapp/loggedin/countrySelection/components/countryList.js
@@ -1,71 +1,27 @@
 import './countryList.less'
 
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import * as R from 'ramda'
+import { useSelector } from 'react-redux'
 
-import { isAdministrator } from '@common/countryRole'
-import { getRelativeDate } from '@webapp/utils/relativeDate'
-
-import { CSVLink } from 'react-csv'
-import Icon from '@webapp/components/icon'
 import CountryListRoleSection from '@webapp/loggedin/countrySelection/components/countryListRoleSection'
-import useUserInfo from '@webapp/components/hooks/useUserInfo'
-import useI18n from '@webapp/components/hooks/useI18n'
+import CountryListDownload from '@webapp/loggedin/countrySelection/components/countryListDownload'
 
 import * as CountryState from '@webapp/country/countryState'
 
-import { getCountryName } from '@webapp/country/actions'
-
 const CountryList = () => {
-  const dispatch = useDispatch()
-  const userInfo = useUserInfo()
-  const i18n = useI18n()
   const countries = useSelector(CountryState.getCountries)
-  const roleCountriesPair = R.toPairs(countries)
 
   return (
     <div className="country-selection-list">
-      {
-        isAdministrator(userInfo) &&
-        <div className="country-selection-list-download">
-          <CSVLink
-            className="btn-s btn-primary"
-            target="_blank"
-            filename="FRA-Countries.csv"
-            data={
-              R.pipe(
-                R.map(
-                  ([_, roleCountries]) =>
-                    R.map(country => ({
-                      name: dispatch(getCountryName(country.countryIso, i18n.language)),
-                      status: i18n.t(`assessment.status.${country.fra2020Assessment}.label`),
-                      edited: getRelativeDate(country.lastEdit, i18n) || i18n.t('audit.notStarted'),
-                      deskStudy: i18n.t(`yesNoTextSelect.${R.propEq('fra2020DeskStudy', true, country) ? 'yes' : 'no'}`)
-                    }), roleCountries)
-                ),
-                R.flatten
-              )(roleCountriesPair)
-            }
-            headers={[
-              { label: i18n.t('admin.country'), key: 'name' },
-              { label: i18n.t('countryListing.fra2020'), key: 'status' },
-              { label: i18n.t('audit.edited'), key: 'edited' },
-              { label: i18n.t('assessment.deskStudy'), key: 'deskStudy' },
-            ]}>
-            <Icon className="icon-sub icon-white" name="hit-down"/>CSV
-          </CSVLink>
-        </div>
-      }
+      <CountryListDownload/>
 
       <div className="country-selection-list__content">
         {
-          roleCountriesPair.map(([role, roleCountries]) =>
+          Object.keys(countries).map(role =>
             <CountryListRoleSection
               key={role}
-              i18n={i18n}
               role={role}
-              roleCountries={roleCountries}
+              roleCountries={countries[role]}
             />
           )
         }
