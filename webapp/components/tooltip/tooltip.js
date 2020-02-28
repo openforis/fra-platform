37f0e18e1aee579eff4fe6107f0c0ef7d12dc1f6
Miró Sorja <ping@mirosorja.com>
2020-02-28 13:35:05 +0100

    tooltipStyle: use vars for colors

--- webapp/components/tooltip/tooltip.js
+++ webapp/components/tooltip/tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './style.css'
+import './tooltipStyle.less'
 
 const Tooltip = ({ text, children, error }) => {
   return <div className={ error ? 'error' : '' } data-tooltip={text}>
