7e8b002ea56805de351f627bc80a5cebd17bc24c
minotogna <mino.togna@gmail.com>
2020-02-28 14:46:41 +0100

    Navigation assessment: refactored section

---
+++ webapp/loggedin/navigation/components/assessment/section/reviewStatus.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import * as ReviewStatusState from '@webapp/country/reviewStatusState'
+
+const ReviewStatus = props => {
+
+  const { status } = props
+
+  if (status[ReviewStatusState.keys.issueStatus] !== ReviewStatusState.keysIssueStatus.opened) {
+    return null
+  }
+
+  const className = `nav__review-status--${status[ReviewStatusState.keys.hasUnreadIssues] ? 'unread' : 'open'}`
+
+  return (
+    <div className={className}/>
+  )
+
+}
+
+ReviewStatus.propTypes = {
+  status: PropTypes.object.isRequired
+}
+
+export default ReviewStatus
