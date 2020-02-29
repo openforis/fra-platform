de4a59dacbe7844a9016c530533762104675e5df
minotogna <mino.togna@gmail.com>
2020-02-29 22:44:23 +0100

    Tables 2c/2d: extracted common excelCalculatorDownload component and hiding it in public view

--- webapp/loggedin/assessmentFra/carbonStock/carbonStockView.js
+++ webapp/loggedin/assessmentFra/carbonStock/carbonStockView.js
@@ -8,6 +8,7 @@ import NationalDataDescriptions from '@webapp/components/description/nationalDat
 import AnalysisDescriptions from '@webapp/components/description/analysisDescriptions'
 import GeneralComments from '@webapp/components/description/generalComments'
 import DefinitionLink from '@webapp/components/definitionLink'
+import ExcelCalculatorDownload from '@webapp/loggedin/assessmentFra/biomassStock/excelCalculatorDownload'
 import { fetchLastSectionUpdateTimestamp } from '@webapp/audit/actions'
 import { isFRA2020SectionEditDisabled } from '@webapp/utils/assessmentAccess'
 import { isPrintingOnlyTables } from '@webapp/loggedin/printAssessment/printAssessment'
@@ -16,28 +17,8 @@ import * as table from '@webapp/traditionalTable/table'
 import { fetchTableData } from '@webapp/traditionalTable/actions'
 
 import * as AppState from '@webapp/app/appState'
-import * as CountryState from '@webapp/country/countryState'
-
 
 const sectionName = 'carbonStock'
-const domains = ['boreal', 'temperate', 'subtropical', 'tropical']
-const downloadPath = (countryIso, selectedDomain, language) =>
-  `/api/biomassStock/${countryIso}/${selectedDomain}/${language}/download`
-
-const Select = ({ i18n, selectedDomain, setSelectedDomain, ...props }) =>
-  <select
-    className="select-s margin-right"
-    value={selectedDomain}
-    onChange={({ target: { value } }) => setSelectedDomain(value)}>
-    {
-      domains.map(domain =>
-        <option value={domain} key={domain}>
-          {i18n.t(`climaticDomain.${domain}`)}
-          {domain === props.domain && ` (${i18n.t('climaticDomain.selectDefault')})`}
-        </option>
-      )
-    }
-  </select>
 
 const soilDepthTableSpec = i18n => ({
   name: 'carbonStockSoilDepth',
@@ -63,7 +44,6 @@ const CarbonStockView = props => {
   const {
     i18n,
     disabled,
-    domain,
     tableSpecInstance,
     tableData,
     fetchTableData,
@@ -72,8 +52,6 @@ const CarbonStockView = props => {
 
   const { language } = i18n
   const countryIso = useSelector(AppState.getCountryIso)
-  const [selectedDomain, setSelectedDomain] = useState(domain)
-  const calculatorFilePath = downloadPath(countryIso, selectedDomain, language)
 
   useEffect(() => {
     fetchTableData(countryIso, tableSpecInstance)
@@ -91,29 +69,23 @@ const CarbonStockView = props => {
     <div className="app-view__content">
       <NationalDataDescriptions section={sectionName} countryIso={countryIso} disabled={disabled}/>
       <AnalysisDescriptions section={sectionName} countryIso={countryIso} disabled={disabled}/>
+
       <h2 className="headline no-print">
         {i18n.t('carbonStock.carbonStock')}
       </h2>
+
       <div className="app-view__section-toolbar" style={{ marginTop: '4px' }}>
         <DefinitionLink className="margin-right-big" document="tad" anchor="2d"
                         title={i18n.t('definition.definitionLabel')} lang={language}/>
         <DefinitionLink className="align-left" document="faq" anchor="2c" title={i18n.t('definition.faqLabel')}
                         lang={language}/>
-        <div className="no-print">
-          {
-            !R.isNil(domain) &&
-            <Select i18n={i18n} domain={domain} selectedDomain={selectedDomain} setSelectedDomain={setSelectedDomain}/>
-          }
-          <a className="btn-s btn-primary" href={calculatorFilePath}>
-            {i18n.t('biomassStock.downloadExcel')}
-          </a>
-        </div>
 
+        <ExcelCalculatorDownload/>
       </div>
 
       {
         !isPrintingOnlyTables() &&
-        <div className="page-break" />
+        <div className="page-break"/>
       }
 
       <TraditionalTable tableSpec={tableSpecInstance} countryIso={countryIso} disabled={disabled}/>
@@ -135,7 +107,6 @@ const mapStateToProps = state => {
   const tableSpecInstance = tableSpec(i18n)
 
   return {
-    domain: CountryState.getConfigDomain(state),
     i18n,
     disabled: isFRA2020SectionEditDisabled(state, sectionName),
     tableSpecInstance,
