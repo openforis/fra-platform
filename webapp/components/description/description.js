9f34c9ac7a986f410d9fd2d657f3510c73594c7f
Miró Sorja <ping@mirosorja.com>
2020-03-03 10:51:57 +0100

    descrption: funcitonal component

--- webapp/components/description/description.js
+++ webapp/components/description/description.js
@@ -1,7 +1,6 @@
 import './description.less'
 
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import * as R from 'ramda'
@@ -10,133 +9,130 @@ import Icon from '@webapp/components/icon'
 import Tooltip from '@webapp/components/tooltip'
 import ckEditorConfig from '@webapp/components/ckEditor/ckEditorConfig'
 
+import useI18n from '@webapp/components/hooks/useI18n'
+import useCountryIso from '@webapp/components/hooks/useCountryIso'
 import * as AppState from '@webapp/app/appState'
 
 import { saveDescriptions, fetchDescriptions, openEditor, closeEditor } from './actions'
 
-class Description extends Component {
+const Description = props => {
+  const {
+    title,
+    content,
+    editing,
+    name,
+    closeEditor,
+    openEditor,
+    disabled,
+    showAlertEmptyContent = false,
+    showDashEmptyContent = false,
+    fetchDescriptions,
+    section,
+  } = props
+  const i18n = useI18n()
+  const countryIso = useCountryIso()
 
-  fetchData(countryIso) {
-    this.props.fetchDescriptions(countryIso, this.props.section, this.props.name)
-  }
+  useEffect(() => {
+    fetchDescriptions(countryIso, section, name)
+  }, [])
 
-  componentDidMount() {
-    this.fetchData(this.props.countryIso)
-  }
+  useEffect(() => {
+    fetchDescriptions(countryIso, section, name)
+  }, [countryIso])
 
-  componentDidUpdate(prevProps, prevState) {
-    const currentCountryIso = this.props.countryIso
-    const previousCountryIso = prevProps.countryIso
-    if (currentCountryIso !== previousCountryIso)
-      this.props.fetchData(nextCountryIso)
-  }
-
-  showEditorContent(isActive, showDash) {
+  const showEditorContent = (isActive, showDash) => {
     if (showDash)
       return <div>-</div>
-    if (R.isNil(this.props.content))
+    if (R.isNil(props.content))
       return null
     if (isActive)
-      return <DescriptionEditor {...this.props} />
-    if (this.props.content)
-      return <div className="fra-description__preview" dangerouslySetInnerHTML={{ __html: this.props.content }} />
-    if (this.props.template)
-      return <div className="fra-description__preview" dangerouslySetInnerHTML={{ __html: this.props.template }} />
+      return <DescriptionEditor {...props} />
+    if (props.content)
+      return <div className="fra-description__preview" dangerouslySetInnerHTML={{ __html: props.content }} />
+    if (props.template)
+      return <div className="fra-description__preview" dangerouslySetInnerHTML={{ __html: props.template }} />
     return null
   }
 
-  render() {
-    const {
-      i18n,
-      title,
-      content,
-      editing,
-      name,
-      closeEditor,
-      openEditor,
-      disabled,
-      showAlertEmptyContent = false,
-      showDashEmptyContent = false,
-    } = this.props
+  const isActive = editing === name
+  const showError = showAlertEmptyContent && !content
+  const showDash = showDashEmptyContent && !content
 
-    const isActive = editing === name
-    const showError = showAlertEmptyContent && !content
-    const showDash = showDashEmptyContent && !content
-
-    return <div>
-      <div className="fra-description__header-row">
-          <h3 ref="descriptionHeader"
-            className={`subhead fra-description__header${showError ? ' icon-red' : ''}`}
-          >
-          {
-            showError ?
-              <Tooltip error text={i18n.t('generalValidation.emptyField')}>
-                {title} <Icon key="icon-error" className="icon-margin-left icon-red" name="alert" />
-              </Tooltip>
-              :
-              <>{title}</>
-          }
-
-          </h3>
-        {
-          !disabled
-          && <div className="fra-description__link no-print"
-            onClick={e => {
-              isActive ? closeEditor() : openEditor(name)
-              e.stopPropagation()
-            }}>
-            {isActive ? i18n.t('description.done') : i18n.t('description.edit')}
-          </div>
-        }
-
-      </div>
-      <div ref="editorContent">
-        {this.showEditorContent(isActive, showDash)}
-      </div>
-    </div>
-  }
-}
-
-class DescriptionEditor extends Component {
-
-  initCkeditorChangeListener () {
-    this.editor.on('change', (evt) =>
-      this.props.saveDescriptions(this.props.countryIso, this.props.section, this.props.name, evt.editor.getData())
-    )
-  }
-
-  setEditorContent (content) {
-    this.editor.setData(content, {
-      callback: () => {
-        if (!this.editor.hasListeners('change'))
-          this.initCkeditorChangeListener()
+  return <div className="fra-description__header-row">
+    <h3 className={`subhead fra-description__header${showError ? ' icon-red' : ''}`}>
+      {
+        showError ?
+          <Tooltip error text={i18n.t('generalValidation.emptyField')}>
+            {title} <Icon key="icon-error" className="icon-margin-left icon-red" name="alert" />
+          </Tooltip>
+          :
+          <>{title}</>
       }
-    })
-  }
+    </h3>
+    {
+      !disabled
+      && <div className="fra-description__link no-print"
+        onClick={e => {
+          isActive ? closeEditor() : openEditor(name)
+          e.stopPropagation()
+        }}>
+        {isActive ? i18n.t('description.done') : i18n.t('description.edit')}
+      </div>
+    }
 
-  componentDidMount () {
-    const domNode = ReactDOM.findDOMNode(this.refs[this.props.name])
-    this.editor = CKEDITOR.replace(domNode, ckEditorConfig)
-    // Data fetching is necessary when CKEDITOR instances are ready
-    this.editor.on('instanceReady', () => {
-      this.setEditorContent(this.props.content || this.props.template)
-    })
-  }
-
-  componentWillUnmount () {
-    this.editor.destroy(false)
-    this.editor = null
-  }
-
-  render () {
-    return <div className="cke_wrapper">
-      <textarea id={this.props.name} ref={this.props.name}/>
-    </div>
-  }
+    {showEditorContent(isActive, showDash)}
+  </div>
 }
 
 Description.propTypes = {
   section: PropTypes.string.isRequired
+}
+
+const DescriptionEditor = props => {
+  const {
+    section,
+    name,
+    template,
+    saveDescriptions,
+    content
+  } = props
+
+  const textareaRef = useRef(null)
+  let editor = useRef(null)
+
+  const countryIso = useCountryIso()
+
+  const initCkeditorChangeListener = () => {
+    editor.current.on('change', (evt) =>
+      saveDescriptions(countryIso, section, name, evt.editor.getData())
+    )
+  }
+
+  const setEditorContent = (content) => {
+    editor.current.setData(content, {
+      callback: () => {
+        if (!editor.current.hasListeners('change'))
+          initCkeditorChangeListener()
+      }
+    })
+  }
+
+  useEffect(() => {
+    editor.current = CKEDITOR.replace(textareaRef.current, ckEditorConfig)
+    // Data fetching is necessary when CKEDITOR instances are ready
+    editor.current.on('instanceReady', () => {
+      setEditorContent(content || template)
+    })
+    return () => {
+      editor.current.destroy(false)
+      editor = null
+    };
+  }, [])
+
+  return <div className="cke_wrapper">
+    <textarea id={name} ref={textareaRef} />
+  </div>
+
 }
 
 const mapStateToProps = (state, props) => ({
