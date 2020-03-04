4163ba9e72e8bc28a708eb74c434b3ca40d0ca7a
Miró Sorja <ping@mirosorja.com>
2020-03-04 11:53:44 +0100

    description/actions: async/await axios

--- webapp/components/description/actions.js
+++ webapp/components/description/actions.js
@@ -18,7 +18,7 @@ export const saveDescriptions = (countryIso, section, name, content) => dispatch
 }
 
 const startSaveDescriptions = (section, name, content) =>
-({ type: descriptionsChangeStart, section, name, content })
+  ({ type: descriptionsChangeStart, section, name, content })
 
 export const openEditorStart = 'descriptions/editor/open'
 export const closeEditorStart = 'descriptions/editor/close'
@@ -26,10 +26,9 @@ export const openEditor = (name) => ({ type: openEditorStart, name })
 export const closeEditor = () => ({ type: closeEditorStart })
 
 const changeDescriptions = (countryIso, section, name, content) => {
-  const dispatched = dispatch => {
-    return axios.post(`/api/country/descriptions/${countryIso}/${section}/${name}`, { content }).then(() => {
-      dispatch(autosave.complete)
-    })
+  const dispatched = async dispatch => {
+    await axios.post(`/api/country/descriptions/${countryIso}/${section}/${name}`, { content })
+    dispatch(autosave.complete)
   }
   dispatched.meta = {
     debounce: {
