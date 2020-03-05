01d8c274644bbf4b031f5c30e78227f5b40b3f4c
Miró Sorja <ping@mirosorja.com>
2020-03-05 14:41:22 +0100

    remove duplicate attribute

--- webapp/app/statisticalFactsheets/statisticalFactsheets.js
+++ webapp/app/statisticalFactsheets/statisticalFactsheets.js
@@ -7,7 +7,6 @@ const StatisticalFactsheets = () => {
   const src = 'https://app.powerbi.com/reportEmbed?reportId=b4d9e5d2-1926-4a3a-a575-3dc5023dfa0b&autoAuth=true&ctid=163ac468-abb8-44d0-81fd-d9db15e3af96&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWV1cm9wZS1ub3J0aC1iLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9'
   const style = { 
     overflow:'hidden',
-    height:"100%",
     width:"100%",
     height:"100vh",
   }
