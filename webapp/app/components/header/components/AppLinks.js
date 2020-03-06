39213707809418ced7338db96896894af4cfac28
Miró Sorja <ping@mirosorja.com>
2020-03-06 15:50:01 +0100

    Added prettier and eslint

--- webapp/app/components/header/components/AppLinks.js
+++ webapp/app/components/header/components/AppLinks.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
-
-const isStatisticalFactsheets = pathname => pathname.includes('statisticalFactsheets')
+const isStatisticalFactsheets = pathname =>
+  pathname.includes('statisticalFactsheets')
 
 const AppLinks = ({ i18n }) => {
   const { pathname } = useLocation()
   const hideDivider = isStatisticalFactsheets(pathname)
 
-  const isActive = (match, { pathname }) => !(isStatisticalFactsheets(pathname)) || (match && match.isExact)
+  const isActive = (match, { pathname }) =>
+    !isStatisticalFactsheets(pathname) || (match && match.isExact)
 
-  return <>
-    <NavLink
-      activeClassName="hidden"
-      to={`/statisticalFactsheets`} className="app-header__menu-item">
-      {i18n.t('common.statisticalFactsheets')}
-    </NavLink>
+  return (
+    <>
+      <NavLink
+        activeClassName="hidden"
+        to="/statisticalFactsheets"
+        className="app-header__menu-item"
+      >
+        {i18n.t('common.statisticalFactsheets')}
+      </NavLink>
 
-    <NavLink
-      activeClassName="hidden"
-      isActive={isActive}
-      to={`/`}
-      className="app-header__menu-item">
-      {i18n.t('common.fraPlatform')}
-    </NavLink>
-    {!hideDivider && <div className="app-header__menu-item-separator" />}
-  </>
+      <NavLink
+        activeClassName="hidden"
+        isActive={isActive}
+        to="/"
+        className="app-header__menu-item"
+      >
+        {i18n.t('common.fraPlatform')}
+      </NavLink>
+      {!hideDivider && <div className="app-header__menu-item-separator" />}
+    </>
+  )
 }
 
 export default AppLinks
