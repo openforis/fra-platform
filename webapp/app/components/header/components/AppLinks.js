ec424a49232d27ecf4b5e705e85dcf4539b601bf
Miró Sorja <ping@mirosorja.com>
2020-03-06 16:09:41 +0100

    some eslint/prettier conf

--- webapp/app/components/header/components/AppLinks.js
+++ webapp/app/components/header/components/AppLinks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+
 const isStatisticalFactsheets = pathname =>
   pathname.includes('statisticalFactsheets')
 
