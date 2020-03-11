79318e353aeb6e52aebe8c96b6ca54da6f7d1c52
minotogna <mino.togna@gmail.com>
2020-03-11 13:27:23 +0100

    using webpack.config.babel in eslintrc

--- babel.config.js
+++ babel.config.js
@@ -11,14 +11,5 @@ module.exports = function (api) {
   return {
     presets: ['@babel/preset-env', '@babel/react'],
     plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-syntax-dynamic-import'],
-    resolve: {
-      extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
-      alias: {
-        '@common': path.resolve(__dirname, 'common/'),
-        '@server': path.resolve(__dirname, 'server/'),
-        '@webapp': path.resolve(__dirname, 'webapp/'),
-        '@test': path.resolve(__dirname, 'test/'),
-      },
-    }
   }
 }
