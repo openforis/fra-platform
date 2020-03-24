8d18e4dffe7bbebe6ec9cf153185dc179e1ddf13
minotogna <mino.togna@gmail.com>
2020-03-24 17:53:53 +0100

    Table 3b: added lessThanOrEqualToForestValidator

--- webapp/app/assessment/fra/sections/forestAreaWithinProtectedAreas/forestAreaWithinProtectedAreasValidatorState.js
+++ webapp/app/assessment/fra/sections/forestAreaWithinProtectedAreas/forestAreaWithinProtectedAreasValidatorState.js
@@ -1,6 +1,8 @@
 import * as FRA from '@common/assessment/fra'
 
+import * as AssessmentState from '@webapp/app/assessment/assessmentState'
 import * as AssessmentStateValidator from '@webapp/app/assessment/assessmentStateValidator'
+import * as ExtentOfForestValidatorState from '@webapp/app/assessment/fra/sections/extentOfForest/extentOfForestValidatorState'
 
 const section = FRA.sections['3'].children.b
 
@@ -12,6 +14,18 @@ export const protectedAreaValidator = AssessmentStateValidator.subCategoryValida
   [2]
 )
 
+export const forestAreaValidator = (colIdx, rowIdx) => state =>
+  ExtentOfForestValidatorState.lessThanOrEqualToForestValidator(
+    FRA.years[colIdx],
+    AssessmentState.getTableDataCell({
+      assessmentType: FRA.type,
+      sectionName: section.name,
+      tableName: section.tables.forestAreaWithinProtectedAreas,
+      rowIdx,
+      colIdx,
+    })(state)
+  )(state)
+
 export const getValidationMessages = data => state => {
   const colNo = data[0].length
   const messages = []
@@ -22,7 +36,10 @@ export const getValidationMessages = data => state => {
 
     if (!protectedAreaValidator(colIdx, 2)(state)) {
       colMessages.push({ key: 'generalValidation.subCategoryExceedsParent' })
-      break
+    }
+
+    if (!forestAreaValidator(colIdx, 0)(state) || !forestAreaValidator(colIdx, 1)(state)) {
+      colMessages.push({ key: 'generalValidation.forestAreaExceedsExtentOfForest' })
     }
   }
 
