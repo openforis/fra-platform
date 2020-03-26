70e354c8a9a6ccb4b89609a7dc1241923e35e56e
Miró Sorja <ping@mirosorja.com>
2020-03-26 11:19:36 +0200

    added missing prop

--- webapp/app/components/description/analysisDescriptions.js
+++ webapp/app/components/description/analysisDescriptions.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { isPrintingMode, isPrintingOnlyTables } from '@webapp/app/assessment/components/print/printAssessment'
 
 import CommentableDescription from '@webapp/app/components/description/commentableDescription'
 import useI18n from '@webapp/components/hooks/useI18n'
 
-const AnalysisDescriptions = () => {
+const AnalysisDescriptions = ({ section }) => {
   const i18n = useI18n()
 
   return (
@@ -14,12 +15,14 @@ const AnalysisDescriptions = () => {
         <h2 className="headline fra-description__group-header">{i18n.t('description.analysisAndProcessing')}</h2>
         <CommentableDescription
           title={i18n.t('description.estimationAndForecasting')}
+          section={section}
           name="estimationAndForecasting"
           showAlertEmptyContent={!isPrintingMode()}
           showDashEmptyContent={isPrintingMode()}
         />
         <CommentableDescription
           title={i18n.t('description.reclassification')}
+          section={section}
           name="reclassification"
           showAlertEmptyContent={!isPrintingMode()}
           showDashEmptyContent={isPrintingMode()}
@@ -27,6 +30,10 @@ const AnalysisDescriptions = () => {
       </div>
     )
   )
+}
+
+AnalysisDescriptions.propTypes = {
+  section: PropTypes.string.isRequired,
 }
 
 export default AnalysisDescriptions
