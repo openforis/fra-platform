e93fbd78a99a91b644faaa496f8231ea2fc1341b
minotogna <mino.togna@gmail.com>
2020-03-28 20:13:32 +0100

    dataTable: reset last update timestamp on unmount

--- webapp/app/components/audit/actions.js
+++ webapp/app/components/audit/actions.js
@@ -8,12 +8,18 @@ export const lastAuditFeedReceived = 'audit/lastAuditFeedReceived'
 export const fetchLastSectionUpdateTimestamp = (countryIso, section) => async (dispatch, getState) => {
   const userInfo = UserState.getUserInfo(getState())
   if (userInfo) {
-    const { data: { timeStamp = null } } = await axios.get(`/api/audit/getLatestAuditLogTimestamp/${countryIso}?section=${section}`)
+    const {
+      data: { timeStamp = null },
+    } = await axios.get(`/api/audit/getLatestAuditLogTimestamp/${countryIso}?section=${section}`)
     dispatch({ type: lastSectionUpdateTimestampReceived, timeStamp })
   }
 }
 
-export const fetchAuditFeed = (countryIso) => async dispatch => {
-  const { data: { feed } } = await axios.get(`/api/audit/getAuditFeed/${countryIso}`)
+export const resetSectionUpdateTimestamp = () => ({ type: lastSectionUpdateTimestampReceived, timeStamp: null })
+
+export const fetchAuditFeed = countryIso => async dispatch => {
+  const {
+    data: { feed },
+  } = await axios.get(`/api/audit/getAuditFeed/${countryIso}`)
   dispatch({ type: lastAuditFeedReceived, feed })
 }
