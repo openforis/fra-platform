cbe07da24479637d7d1e51941403fa81e4eee8ee
Miró Sorja <ping@mirosorja.com>
2020-04-10 17:16:44 +0300

    description/title: FIX: error as defaultProp

--- webapp/app/assessment/components/section/components/descriptions/components/components/title.js
+++ webapp/app/assessment/components/section/components/descriptions/components/components/title.js
@@ -4,10 +4,14 @@ import PropTypes from 'prop-types'
 import Icon from '@webapp/components/icon'
 import Tooltip from '@webapp/components/tooltip'
 
-const Title = props => {
+const Title = (props) => {
   const { error, title, i18n } = props
+
+  let titleClassName = 'subhead fra-description__header'
+  if (error) titleClassName += ' icon-red'
+
   return (
-    <h3 className={`subhead fra-description__header${error ? ' icon-red' : ''}`}>
+    <h3 className={titleClassName}>
       {error ? (
         <Tooltip error text={i18n.t('generalValidation.emptyField')}>
           {title} <Icon key="icon-error" className="icon-margin-left icon-red" name="alert" />
@@ -19,8 +23,12 @@ const Title = props => {
   )
 }
 
+Title.defaultProps = {
+  error: false,
+}
+
 Title.propTypes = {
-  error: PropTypes.bool.isRequired,
+  error: PropTypes.bool,
   title: PropTypes.string.isRequired,
   i18n: PropTypes.object.isRequired,
 }
