f12184a30f1398f94dd03e5d205d2d86feec68d6
Miró Sorja <ping@mirosorja.com>
2020-04-22 13:54:58 +0300

    NewVersionForm: Minor refactoring and use getAdminVersioningLink

--- webapp/app/admin/components/versioning/NewVersionForm.js
+++ webapp/app/admin/components/versioning/NewVersionForm.js
@@ -1,94 +1,120 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { format } from 'date-fns'
 import useI18n from '@webapp/components/hooks/useI18n'
 
-import * as AppState from '@webapp/app/appState'
 import * as AdminState from '@webapp/app/admin/adminState'
+import * as BasePaths from '@webapp/main/basePaths'
 
-import { validField, versionIsGreater } from './versioningViewUtils'
 import * as FRAVersion from '@common/versioning/fraVersion'
+import { validField, versionIsGreater } from './versioningViewUtils'
 
 const DateInput = (props) => {
   const { onChange } = props
 
-  const minDate = format(new Date(),
-    'yyyy-MM-ddThh:mm', { awareOfUnicodeTokens: true });
-  const maxDate = format(new Date(new Date().getUTCFullYear() + 2 + ''),
-    'yyyy-MM-ddThh:mm', { awareOfUnicodeTokens: true });
+  const minDate = format(new Date(), 'yyyy-MM-ddThh:mm', { awareOfUnicodeTokens: true })
+  const maxDate = format(new Date(`${new Date().getUTCFullYear() + 2}`), 'yyyy-MM-ddThh:mm', {
+    awareOfUnicodeTokens: true,
+  })
 
-  return <input
-    className="new-version__input"
-    min={minDate}
-    max={maxDate}
-    onChange={onChange}
-    type="datetime-local"
-    name={FRAVersion.keys.publishedAt} />
+  return (
+    <input
+      className="new-version__input"
+      min={minDate}
+      max={maxDate}
+      onChange={onChange}
+      type="datetime-local"
+      name={FRAVersion.keys.publishedAt}
+    />
+  )
+}
+
+DateInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
 }
 
 const VersionInput = (props) => {
   const { onChange } = props
-  return <input
-    className="new-version__input"
-    onChange={onChange}
-    placeholder="Ex. 1.0.0"
-    type="text"
-    name={FRAVersion.keys.versionNumber} />
+  return (
+    <input
+      className="new-version__input"
+      onChange={onChange}
+      placeholder="Ex. 1.0.0"
+      type="text"
+      name={FRAVersion.keys.versionNumber}
+    />
+  )
+}
+
+VersionInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
 }
 
 const NewVersionForm = (props) => {
-  const { onSubmit, onChange } = props;
+  const { onSubmit, onChange } = props
   const [error, setError] = useState(null)
-  const countryIso = useSelector(AppState.getCountryIso)
   const versions = useSelector(AdminState.getVersions) || {}
   const newVersionForm = useSelector(AdminState.getNewVersionForm) || {}
-  const history = useHistory();
+  const history = useHistory()
   const i18n = useI18n()
 
-  const goBack = (e) => {
-    e.preventDefault();
-    history.goBack();
-  };
+  const goBack = (event) => {
+    event.preventDefault()
+    history.goBack()
+  }
 
-  const onFormSubmit = (e) => {
+  const onFormSubmit = (event) => {
     // Prevent <form> element doing page refresh on submit
-    e.preventDefault();
-    if (!(
-      validField(newVersionForm, 'versionNumber') &&
-      validField(newVersionForm, 'date') &&
-      versionIsGreater(versions, FRAVersion.getVersionNumber(newVersionForm))
-    )) {
+    event.preventDefault()
+    if (
+      !(
+        validField(newVersionForm, 'versionNumber') &&
+        validField(newVersionForm, 'date') &&
+        versionIsGreater(versions, FRAVersion.getVersionNumber(newVersionForm))
+      )
+    ) {
       setError(true)
       return
     }
 
     onSubmit()
-    const url = `/country/${countryIso}/admin/versioning/`;
+    const url = BasePaths.getAdminVersioningLink()
     history.push(url)
   }
 
-  return <div className="new-version__container">
-    {
-      error &&
-      <div className="new-version__error-container">
-        {i18n.t('landing.versioning.form.error')}
-      </div>
-    }
+  return (
+    <div className="new-version__container">
+      {error && <div className="new-version__error-container">{i18n.t('landing.versioning.form.error')}</div>}
 
-    <form onSubmit={onFormSubmit}>
-      <h3 className="new-version__title">{i18n.t('landing.versioning.form.newVersion')}</h3>
-      <label className="new-version__label">{i18n.t('landing.versioning.form.versionNumber')}</label><br />
-      <VersionInput value={FRAVersion.getVersionNumber(newVersionForm)} onChange={onChange} /><br />
-      <label className="new-version__label">{i18n.t('landing.versioning.form.date')}</label><br />
-      <DateInput value={FRAVersion.getPublishedAt(newVersionForm)} onChange={onChange} /> <br />
-      <div className="new-version__button-container">
-        <button className="btn btn-secondary" onClick={goBack}>{i18n.t('landing.versioning.form.cancel')}</button>
-        <input className="btn btn-primary" type="submit" />
-      </div>
+      <form onSubmit={onFormSubmit}>
+        <h3 className="new-version__title">{i18n.t('landing.versioning.form.newVersion')}</h3>
+        <label htmlFor="versioninput" className="new-version__label">
+          {i18n.t('landing.versioning.form.versionNumber')}
+        </label>
+        <br />
+        <VersionInput id="versioninput" value={FRAVersion.getVersionNumber(newVersionForm)} onChange={onChange} />
+        <br />
+        <label htmlFor="dateinput" className="new-version__label">
+          {i18n.t('landing.versioning.form.date')}
+        </label>
+        <br />
+        <DateInput id="dateinput" value={FRAVersion.getPublishedAt(newVersionForm)} onChange={onChange} /> <br />
+        <div className="new-version__button-container">
+          <button type="button" className="btn btn-secondary" onClick={goBack}>
+            {i18n.t('landing.versioning.form.cancel')}
+          </button>
+          <input className="btn btn-primary" type="submit" />
+        </div>
+      </form>
+    </div>
+  )
+}
 
-    </form>
-  </div>
-};
+NewVersionForm.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+}
 
 export default NewVersionForm
