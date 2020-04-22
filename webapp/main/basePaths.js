0a68b96c9c6977c97436730c63293ceb42281a16
Miró Sorja <ping@mirosorja.com>
2020-04-22 13:57:31 +0300

    basePaths: use pathGenerator

--- webapp/main/basePaths.js
+++ webapp/main/basePaths.js
@@ -8,20 +8,58 @@ export const assessmentPrint = `${country}print/:assessmentType/`
 export const assessmentPrintOnlyTables = `${country}print/:assessmentType/onlyTables/`
 export const odp = `${country}odp/:tab/`
 export const user = '/users/:userId/'
+export const admin = '/admin/'
+
+const pathFragments = {
+  assessment: 'assessment',
+  print: 'print',
+  odp: 'odp',
+  api: 'api',
+  users: 'users',
+  user: 'user',
+  profilePicture: 'profilePicture',
+  versioning: 'versioning',
+  onlyTables: 'onlyTables',
+  admin: 'admin',
+}
+
 // ==== getter utilities
 
-export const getCountryHomeLink = (countryIso) => `/${countryIso}/`
+/**
+ * Returns consistent URL strings starting and ending with /
+ *
+ * @param {string[]} parts - url parts from which the url is generated
+ */
+const _pathGenerator = (...parts) => `/${parts.filter((p) => !!p).join('/')}/`
 
+// /:countryIso
+export const getCountryHomeLink = (countryIso) => _pathGenerator(countryIso)
+
+// /:countryIso/assessment/:assessmentType/:sectionName/
 export const getAssessmentSectionLink = (countryIso, assessmentType, sectionName) =>
-  `/${countryIso}/assessment/${assessmentType}/${sectionName}/`
+  _pathGenerator(countryIso, pathFragments.assessment, assessmentType, sectionName)
 
+// /:countryIso/print/:assessmentType/
 export const getAssessmentPrintLink = (countryIso, assessmentType, onlyTables = false) =>
-  `/${countryIso}/print/${assessmentType}/${onlyTables ? 'onlyTables/' : ''}`
+  _pathGenerator(countryIso, pathFragments.print, assessmentType, onlyTables && pathFragments.onlyTables)
 
-export const getOdpLink = (countryIso, sectionName, odpId = null) =>
-  `/${countryIso}/odp/${sectionName}/${odpId ? `${odpId}/` : ''}`
+// /:countryIso/print/:assessmentType/onlyTables/
+export const getOdpLink = (countryIso, sectionName, odpId) =>
+  _pathGenerator(countryIso, pathFragments.odp, sectionName, odpId)
 
+// /api/users/:countryIso/user/:userId/profilePicture
 export const getUserProfilePictureLink = (countryIso, userId) =>
-  `/api/users/${countryIso}/user/${userId}/profilePicture`
+  _pathGenerator(
+    pathFragments.api,
+    pathFragments.users,
+    countryIso,
+    pathFragments.user,
+    userId,
+    pathFragments.profilePicture
+  )
 
-export const getUserLink = (userId) => `/users/${userId}/`
+// /users/:userId/
+export const getUserProfileLink = (userId) => _pathGenerator(pathFragments.users, userId)
+
+// /admin/versioning/
+export const getAdminVersioningLink = () => _pathGenerator(pathFragments.admin, pathFragments.versioning)
