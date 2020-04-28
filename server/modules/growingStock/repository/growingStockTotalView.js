c8b2589c233dea4ef137a43e52fb8347204e21b9
Miró Sorja <ping@mirosorja.com>
2020-04-28 16:44:52 +0300

    repository/growingStockTotalView: select * and remove id

--- server/modules/growingStock/repository/growingStockTotalView.js
+++ server/modules/growingStock/repository/growingStockTotalView.js
@@ -4,20 +4,11 @@ const db = require('../../../db/db')
 const getGrowingStockTotal = async (schemaName = 'public') => {
   const result = await db.query(`
   SELECT
-    DISTINCT ON
-    (country_iso) country_iso,
-    id,
-    row_name,
-    "1990",
-    "2000",
-    "2010",
-    "2015",
-    "2020"
+    *
   FROM
     ${schemaName}.GROWING_STOCK_TOTAL_VIEW GSTV
   ORDER BY
-    country_iso,
-    id ASC;
+    country_iso ASC
   `)
   return camelize(result.rows)
 }
