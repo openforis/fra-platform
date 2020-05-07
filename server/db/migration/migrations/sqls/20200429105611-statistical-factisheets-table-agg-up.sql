c3d8e23d472737d1f4a8f3e5e6ae133ef30ae85c
Miró Sorja <ping@mirosorja.com>
2020-05-07 12:54:34 +0300

    statistical-factsheets-table-agg: variable to row_name

--- server/db/migration/migrations/sqls/20200429105611-statistical-factisheets-table-agg-up.sql
+++ server/db/migration/migrations/sqls/20200429105611-statistical-factisheets-table-agg-up.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS statistical_factisheets_table_agg;
 CREATE TABLE statistical_factisheets_table_agg(
     id serial PRIMARY KEY,
     level text NOT NULL,
-    variable text NOT NULL,
+    row_name text NOT NULL,
     "1990" decimal,
     "2000" decimal,
     "2010" decimal,
     "2015" decimal,
     "2020" decimal,
     data_availability decimal,
-    UNIQUE(level, variable)
+    UNIQUE(level, row_name)
 );
