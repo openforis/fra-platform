bca6087513fc2fcf3aa2d95060f9cc774a0a2706
Mino Togna <mino.togna@gmail.com>
2020-05-11 19:04:28 +0200

    forest_characteristics_view: refactored planted_forest column

--- server/db/migration/migrations/sqls/20200129121445-forest-characteristics-view-up.sql
+++ server/db/migration/migrations/sqls/20200129121445-forest-characteristics-view-up.sql
@@ -1,4 +1,4 @@
-DROP VIEW IF EXISTS forest_characteristics_view;
+DROP VIEW IF EXISTS forest_characteristics_view CASCADE;
 CREATE VIEW
     forest_characteristics_view AS
 (
@@ -22,70 +22,88 @@ WITH forest_characteristics AS (
                f.plantation_forest_introduced_area,
                f.other_planted_forest_area
         FROM foc_fra_values f
-        ORDER BY 1, 2, 3) AS q),
-     forest_characteristics2 AS (
-         SELECT fc.country_iso,
-                'natural_forest_area'                                     AS row_name,
-                SUM(fc.natural_forest_area) FILTER (WHERE fc.year = 1990) AS "1990",
-                SUM(fc.natural_forest_area) FILTER (WHERE fc.year = 2000) AS "2000",
-                SUM(fc.natural_forest_area) FILTER (WHERE fc.year = 2010) AS "2010",
-                SUM(fc.natural_forest_area) FILTER (WHERE fc.year = 2015) AS "2015",
-                SUM(fc.natural_forest_area) FILTER (WHERE fc.year = 2020) AS "2020"
-         FROM forest_characteristics fc
-         WHERE fc.row_number = 1
-         GROUP BY fc.country_iso
-         UNION
-         SELECT fc.country_iso,
-                'plantation_forest_area'                                     AS row_name,
-                SUM(fc.plantation_forest_area) FILTER (WHERE fc.year = 1990) AS "1990",
-                SUM(fc.plantation_forest_area) FILTER (WHERE fc.year = 2000) AS "2000",
-                SUM(fc.plantation_forest_area) FILTER (WHERE fc.year = 2010) AS "2010",
-                SUM(fc.plantation_forest_area) FILTER (WHERE fc.year = 2015) AS "2015",
-                SUM(fc.plantation_forest_area) FILTER (WHERE fc.year = 2020) AS "2020"
-         FROM forest_characteristics fc
-         WHERE fc.row_number = 1
-         GROUP BY fc.country_iso
-         UNION
-         SELECT fc.country_iso,
-                'plantation_forest_introduced_area'                                     AS row_name,
-                SUM(fc.plantation_forest_introduced_area) FILTER (WHERE fc.year = 1990) AS "1990",
-                SUM(fc.plantation_forest_introduced_area) FILTER (WHERE fc.year = 2000) AS "2000",
-                SUM(fc.plantation_forest_introduced_area) FILTER (WHERE fc.year = 2010) AS "2010",
-                SUM(fc.plantation_forest_introduced_area) FILTER (WHERE fc.year = 2015) AS "2015",
-                SUM(fc.plantation_forest_introduced_area) FILTER (WHERE fc.year = 2020) AS "2020"
-         FROM forest_characteristics fc
-         WHERE fc.row_number = 1
-         GROUP BY fc.country_iso
-         UNION
-         SELECT fc.country_iso,
-                'other_planted_forest_area'                                     AS row_name,
-                SUM(fc.other_planted_forest_area) FILTER (WHERE fc.year = 1990) AS "1990",
-                SUM(fc.other_planted_forest_area) FILTER (WHERE fc.year = 2000) AS "2000",
-                SUM(fc.other_planted_forest_area) FILTER (WHERE fc.year = 2010) AS "2010",
-                SUM(fc.other_planted_forest_area) FILTER (WHERE fc.year = 2015) AS "2015",
-                SUM(fc.other_planted_forest_area) FILTER (WHERE fc.year = 2020) AS "2020"
-         FROM forest_characteristics fc
-         WHERE fc.row_number = 1
-         GROUP BY fc.country_iso
-         ORDER BY 1, 2)
+        ORDER BY 1, 2, 3) AS q
+),
+     natural_forest_area AS (
+         SELECT f.country_iso,
+                'natural_forest_area'::TEXT                             AS row_name,
+                SUM(f.natural_forest_area) FILTER (WHERE f.year = 1990) AS "1990",
+                SUM(f.natural_forest_area) FILTER (WHERE f.year = 2000) AS "2000",
+                SUM(f.natural_forest_area) FILTER (WHERE f.year = 2010) AS "2010",
+                SUM(f.natural_forest_area) FILTER (WHERE f.year = 2015) AS "2015",
+                SUM(f.natural_forest_area) FILTER (WHERE f.year = 2020) AS "2020"
+         FROM forest_characteristics f
+         WHERE f.row_number = 1
+         GROUP BY 1
+     ),
+     plantation_forest_area AS (
+         SELECT f.country_iso,
+                'plantation_forest_area'::TEXT                             AS row_name,
+                SUM(f.plantation_forest_area) FILTER (WHERE f.year = 1990) AS "1990",
+                SUM(f.plantation_forest_area) FILTER (WHERE f.year = 2000) AS "2000",
+                SUM(f.plantation_forest_area) FILTER (WHERE f.year = 2010) AS "2010",
+                SUM(f.plantation_forest_area) FILTER (WHERE f.year = 2015) AS "2015",
+                SUM(f.plantation_forest_area) FILTER (WHERE f.year = 2020) AS "2020"
+         FROM forest_characteristics f
+         WHERE f.row_number = 1
+         GROUP BY 1
+     ),
+     plantation_forest_introduced_area AS (
+         SELECT f.country_iso,
+                'plantation_forest_introduced_area'::TEXT                             AS row_name,
+                SUM(f.plantation_forest_introduced_area) FILTER (WHERE f.year = 1990) AS "1990",
+                SUM(f.plantation_forest_introduced_area) FILTER (WHERE f.year = 2000) AS "2000",
+                SUM(f.plantation_forest_introduced_area) FILTER (WHERE f.year = 2010) AS "2010",
+                SUM(f.plantation_forest_introduced_area) FILTER (WHERE f.year = 2015) AS "2015",
+                SUM(f.plantation_forest_introduced_area) FILTER (WHERE f.year = 2020) AS "2020"
+         FROM forest_characteristics f
+         WHERE f.row_number = 1
+         GROUP BY 1
+     ),
+     other_planted_forest_area AS (
+         SELECT f.country_iso,
+                'other_planted_forest_area'::TEXT                             AS row_name,
+                SUM(f.other_planted_forest_area) FILTER (WHERE f.year = 1990) AS "1990",
+                SUM(f.other_planted_forest_area) FILTER (WHERE f.year = 2000) AS "2000",
+                SUM(f.other_planted_forest_area) FILTER (WHERE f.year = 2010) AS "2010",
+                SUM(f.other_planted_forest_area) FILTER (WHERE f.year = 2015) AS "2015",
+                SUM(f.other_planted_forest_area) FILTER (WHERE f.year = 2020) AS "2020"
+         FROM forest_characteristics f
+         WHERE f.row_number = 1
+         GROUP BY 1
+     )
 SELECT *
-FROM forest_characteristics2
+FROM natural_forest_area
 UNION
-SELECT country_iso,
-       'planted_forest'                                               AS row_name,
-       SUM("1990") FILTER (WHERE row_name = 'other_planted_forest_area' OR
-                                 row_name = 'plantation_forest_area') AS "1990",
-       SUM("2000") FILTER (WHERE row_name = 'other_planted_forest_area' OR
-                                 row_name = 'plantation_forest_area') AS "2000",
-       SUM("2010") FILTER (WHERE row_name = 'other_planted_forest_area' OR
-                                 row_name = 'plantation_forest_area') AS "2010",
-       SUM("2015") FILTER (WHERE row_name = 'other_planted_forest_area' OR
-                                 row_name = 'plantation_forest_area') AS "2015",
-       SUM("2020") FILTER (WHERE row_name = 'other_planted_forest_area' OR
-                                 row_name = 'plantation_forest_area') AS "2020"
-FROM forest_characteristics2
-GROUP BY 1
+SELECT *
+FROM plantation_forest_area
+UNION
+SELECT *
+FROM plantation_forest_introduced_area
+UNION
+SELECT *
+FROM other_planted_forest_area
+UNION
+SELECT o.country_iso,
+       'planted_forest'::TEXT                                     AS row_name,
+       CASE
+           WHEN o."1990" IS NULL AND p."1990" IS NULL THEN NULL
+           ELSE coalesce(o."1990", 0) + coalesce(p."1990", 0) END AS "1990",
+       CASE
+           WHEN o."2000" IS NULL AND p."2000" IS NULL THEN NULL
+           ELSE coalesce(o."2000", 0) + coalesce(p."2000", 0) END AS "2000",
+       CASE
+           WHEN o."2010" IS NULL AND p."2010" IS NULL THEN NULL
+           ELSE coalesce(o."2010", 0) + coalesce(p."2010", 0) END AS "2010",
+       CASE
+           WHEN o."2015" IS NULL AND p."2015" IS NULL THEN NULL
+           ELSE coalesce(o."2015", 0) + coalesce(p."2015", 0) END AS "2015",
+       CASE
+           WHEN o."2020" IS NULL AND p."2020" IS NULL THEN NULL
+           ELSE coalesce(o."2020", 0) + coalesce(p."2020", 0) END AS "2020"
+FROM other_planted_forest_area o
+LEFT JOIN plantation_forest_area p
+ON o.country_iso = p.country_iso
 ORDER BY 1
-
-    );
-
+    )
+;
