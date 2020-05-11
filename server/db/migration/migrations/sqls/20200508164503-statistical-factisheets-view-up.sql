c6810dfe3301a0157761d1f627547b09da20bf19
Miró Sorja <ping@mirosorja.com>
2020-05-11 10:26:20 +0300

    statisticalFactsheetsRepository: move where clause to view

--- server/db/migration/migrations/sqls/20200508164503-statistical-factisheets-view-up.sql
+++ server/db/migration/migrations/sqls/20200508164503-statistical-factisheets-view-up.sql
@@ -113,7 +113,7 @@ WITH s AS (
        OR row_name = 'public_ownership'
        OR row_name = 'other_or_unknown'
     GROUP BY country_iso, row_name
-    ORDER BY country_iso)
+    ORDER BY country_iso), s2 as (
 SELECT c.list_name_en AS level,
        s.row_name,
        s."1990",
@@ -159,5 +159,24 @@ SELECT level,
 FROM statistical_factisheets_table_agg
 WHERE row_name NOT LIKE '% ha)'
   AND row_name NOT LIKE '%billion m3%'
-ORDER BY level ASC, row_name;
+ORDER BY level ASC, row_name)   SELECT * FROM s2
+  WHERE row_name = 'Carbon stock in biomass (Gt)'
+    OR row_name = 'Conservation (1000 ha)'
+    OR row_name = 'Forest area (1000 ha)'
+    OR row_name = 'Forest area (% of land area)'
+    OR row_name = 'Forest in protected areas (1000 ha)'
+    OR row_name = 'Growing stock (million m3)'
+    OR row_name = 'Multiple use (1000 ha)'
+    OR row_name = 'Naturally regenerating forest (1000 ha)'
+    OR row_name = 'Other (1000 ha)'
+    OR row_name = 'Other/unknown ownership (1000 ha)'
+    OR row_name = 'Planted forest (1000 ha)'
+    OR row_name = 'Planted forest of which plantation forest (1000 ha)'
+    OR row_name = 'Primary forest (1000 ha)'
+    OR row_name = 'Private ownership (1000 ha)'
+    OR row_name = 'Production (1000 ha)'
+    OR row_name = 'Protection of soil and water (1000 ha)'
+    OR row_name = 'Public ownership (1000 ha)'
+    OR row_name = 'Social services (1000 ha)'
+    OR row_name = 'Total carbon stock (Gt)';
 
--- server/statisticalFactsheets/statisticalFactsheetsRepository.js
+++ server/statisticalFactsheets/statisticalFactsheetsRepository.js
@@ -5,26 +5,6 @@ const getStatisticalFactsheetTableAgg = async (schemaName) => {
   const result = await db.query(`
   SELECT *
   FROM ${schemaName}.statistical_factsheets_view
-  -- All the rows/variables used in statistical factsheet 
-  WHERE row_name = 'Carbon stock in biomass (Gt)'
-    OR row_name = 'Conservation (1000 ha)'
-    OR row_name = 'Forest area (1000 ha)'
-    OR row_name = 'Forest area (% of land area)'
-    OR row_name = 'Forest in protected areas (1000 ha)'
-    OR row_name = 'Growing stock (million m3)'
-    OR row_name = 'Multiple use (1000 ha)'
-    OR row_name = 'Naturally regenerating forest (1000 ha)'
-    OR row_name = 'Other (1000 ha)'
-    OR row_name = 'Other/unknown ownership (1000 ha)'
-    OR row_name = 'Planted forest (1000 ha)'
-    OR row_name = 'Planted forest of which plantation forest (1000 ha)'
-    OR row_name = 'Primary forest (1000 ha)'
-    OR row_name = 'Private ownership (1000 ha)'
-    OR row_name = 'Production (1000 ha)'
-    OR row_name = 'Protection of soil and water (1000 ha)'
-    OR row_name = 'Public ownership (1000 ha)'
-    OR row_name = 'Social services (1000 ha)'
-    OR row_name = 'Total carbon stock (Gt)';
   `)
 
   return camelize(result.rows)
