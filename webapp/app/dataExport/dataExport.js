cf477a20fea8122afcb2873f5bcf02834dad8811
Miró Sorja <ping@mirosorja.com>
2020-05-12 15:09:44 +0300

    initial commit for data export view

--- webapp/app/dataExport/dataExport.js
+++ webapp/app/dataExport/dataExport.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Header from '@webapp/app/components/header/header'
+import Navigation from '@webapp/app/components/navigation/navigation'
+import * as NavigationState from '@webapp/app/components/navigation/navigationState'
+import { useSelector } from 'react-redux'
+import { useCountryIso } from '@webapp/components/hooks'
 
 const DataExport = () => {
+  const countryIso = useCountryIso()
+  const navigationVisible = useSelector(NavigationState.isVisible)
+
+  let classNameAppView = 'app-view'
+  classNameAppView += navigationVisible ? ' navigation-on' : ''
+  classNameAppView += !navigationVisible && countryIso ? ' navigation-off' : ''
+
   return (
-    <>
+    <div className={classNameAppView}>
       <Header />
-      <h1>DataExport</h1>
-    </>
+      <Navigation />
+      <div className="app-view__content">
+        <h1>DataExport</h1>
+      </div>
+    </div>
   )
 }
 
