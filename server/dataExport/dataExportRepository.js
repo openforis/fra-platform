cce0380c5aaff868fe9048dc9fe3c4fbde3240aa
Miró Sorja <ping@mirosorja.com>
2020-05-19 15:14:58 +0300

    dataExport api: removed unnecessary code

--- server/dataExport/dataExportRepository.js
+++ server/dataExport/dataExportRepository.js
@@ -1,38 +1,28 @@
 const camelize = require('camelize')
 const db = require('../db/db')
 
-const KEYS_TABLE = {
-  extentOfForest: 'extent_of_forest_view',
-  forestCharacteristics: 'forest_characteristics_view',
-  forestAreaChange: '',
-  annualReforestation: '',
-  specificForestCategories: '',
-  otherLandWithTreeCover: '',
-  growingStock: '',
-  growingStockComposition: 'growing_stock_total_view',
-  biomassStock: '',
-  carbonStock: 'carbon_stock_view',
-  designatedManagementObjective: '',
-  forestAreaWithinProtectedAreas: '',
-  forestOwnership: '',
-  holderOfManagementRights: '',
-  disturbances: '',
-  areaAffectedByFire: '',
-  degradedForest: '',
-  forestPolicy: '',
-  areaOfPermanentForestEstate: '',
-  employment: '',
-  graduationOfStudents: '',
-}
+// Some data is fetched from views
+const views = [
+  'extent_of_forest',
+  'forest_characteristics',
+  'forest_area_change',
+  'growing_stock',
+  'carbon_stock',
+  'forest_ownership',
+  'holder_of_management_rights',
+  'disturbances',
+]
 
 const getExportData = async (table, variable, countries, columns) => {
   // Add "" around year columns
   const columnsJoined = columns.map((x) => `"${x}"`).join(',')
   const countriesJoined = countries.map((x) => `'${x}'`).join(',')
+  // check if table exists in views array
+  const tableName = views.includes(table) ? `${table}_view` : table
 
   const query = `
     SELECT country_iso, row_name, ${columnsJoined}
-    FROM ${KEYS_TABLE[table]} t
+    FROM ${tableName} t
     WHERE t.country_iso IN (${countriesJoined})
     AND t.row_name = $1
   `
