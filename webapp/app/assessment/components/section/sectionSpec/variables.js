df75ce3feb0c0befe25bdf8826aa0e63012e5cda
Miró Sorja <ping@mirosorja.com>
2020-05-19 16:33:56 +0300

    variables: initialize spec variables

---
+++ webapp/app/assessment/components/section/sectionSpec/variables.js
@@ -0,0 +1,35 @@
+const variables = {
+  // 1a
+  forest_area: 'forest_area',
+  total_land_area: 'total_land_area',
+  forest_area_percent: 'forest_area_percent',
+  other_wooded_land: 'other_wooded_land',
+  // 1b
+  plantation_forest_introduced_area: 'plantation_forest_introduced_area',
+  planted_forest: 'planted_forest',
+  other_planted_forest_area: 'other_planted_forest_area',
+  natural_forest_area: 'natural_forest_area',
+  plantation_forest_area: 'plantation_forest_area',
+  // 1c
+  afforestation: 'afforestation',
+  deforestation: 'deforestation',
+  forest_area_net_change: 'forest_area_net_change',
+  forest_expansion: 'forest_expansion',
+  natural_expansion: 'natural_expansion',
+  // 1d
+  reforestation: 'reforestation',
+  // 1e
+  bamboo: 'bamboo',
+  mangroves: 'mangroves',
+  temporarily_unstocked: 'temporarily_unstocked',
+  primary_forest: 'primary_forest',
+  rubber_wood: 'rubber_wood',
+  // 1f
+  palms: 'palms',
+  tree_orchards: 'tree_orchards',
+  agroforestry: 'agroforestry',
+  trees_in_urban_settings: 'trees_in_urban_settings',
+  other: 'other',
+}
+
+export default variables
