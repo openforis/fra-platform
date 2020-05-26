00528f09816ca69bbf26534a7d21258b5a2fb1be
Miró Sorja <ping@mirosorja.com>
2020-05-26 12:43:15 +0300

    useDataExportState: use params and labels for selection

--- webapp/app/dataExport/useDataExportState.js
+++ webapp/app/dataExport/useDataExportState.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import axios from 'axios'
+
 import useGetRequest from '@webapp/components/hooks/useGetRequest'
 import * as SectionSpecs from '@webapp/app/assessment/components/section/sectionSpecs'
 import { TableSpec } from '@webapp/app/assessment/components/section/sectionSpec'
@@ -8,11 +10,13 @@ export default () => {
   const { section, assessmentType } = useParams()
   const [variables, setVariables] = useState([])
   const [columns, setColumns] = useState([])
+  const [results, setResults] = useState({})
 
   const [selection, setSelection] = useState({
     countries: [],
     columns: [],
-    variable: '',
+    // { param, label }
+    variable: {},
   })
 
   const { data: countries = [], dispatch: fetchCountries } = useGetRequest(`/api/countries`)
@@ -20,11 +24,10 @@ export default () => {
   useEffect(() => {
     fetchCountries()
   }, [])
-
   useEffect(() => {
     setSelection({
       ...selection,
-      variable: '',
+      variable: {},
       columns: [],
     })
 
@@ -36,10 +39,40 @@ export default () => {
       setColumns(colsExport)
     }
   }, [section])
+  useEffect(() => {
+    if (!section || !selection.countries.length || !selection.columns.length || !selection.variable.param) {
+      return
+    }
+    // format section camelCase to snake_case
+    const fetchResultsUrl = `/api/export/${section
+      .split(/(?=[A-Z])/)
+      .join('_')
+      .toLowerCase()}`
+    axios
+      .get(fetchResultsUrl, {
+        params: {
+          columns: selection.columns.map(({ param }) => param),
+          countries: selection.countries.map(({ param }) => param),
+          variable: selection.variable.param,
+        },
+      })
+      .then(({ data }) => {
+        setResults(data)
+      })
+  }, [section, selection.countries, selection.columns, selection.variable])
 
   const setSelectionCountries = (value) => setSelection({ ...selection, countries: value })
   const setSelectionColumns = (value) => setSelection({ ...selection, columns: value })
   const setSelectionVariable = (value) => setSelection({ ...selection, variable: value })
 
-  return { countries, columns, selection, variables, setSelectionCountries, setSelectionColumns, setSelectionVariable }
+  return {
+    results,
+    countries,
+    columns,
+    selection,
+    variables,
+    setSelectionCountries,
+    setSelectionColumns,
+    setSelectionVariable,
+  }
 }
