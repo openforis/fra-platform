ce94fd23f8155c2f85532bd868005ba28205debe
Miró Sorja <ping@mirosorja.com>
2020-06-03 13:41:52 +0300

    dataExport: catch errors

--- server/dataExport/api.js
+++ server/dataExport/api.js
@@ -1,11 +1,16 @@
 const DataExportRepository = require('./dataExportRepository')
+const Request = require('../utils/requestUtils')
 
 module.exports.init = (app) => {
   app.get('/export/:table', async (req, res) => {
-    const { countries, columns, variable } = req.query
-    const { table } = req.params
+    try {
+      const { countries, columns, variable } = req.query
+      const { table } = req.params
 
-    const result = await DataExportRepository.getExportData(table, variable, countries, columns)
-    res.json(result)
+      const result = await DataExportRepository.getExportData(table, variable, countries, columns)
+      res.json(result)
+    } catch (err) {
+      Request.sendErr(res, err)
+    }
   })
 }
