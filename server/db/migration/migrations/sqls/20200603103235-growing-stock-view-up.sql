ac4c9f0c2af712e066d73ae4315ca5456be15e32
Miró Sorja <ping@mirosorja.com>
2020-06-03 14:15:41 +0300

    growing-stock-view: append growing_stock_total

--- server/db/migration/migrations/sqls/20200603103235-growing-stock-view-up.sql
+++ server/db/migration/migrations/sqls/20200603103235-growing-stock-view-up.sql
@@ -1,6 +1,7 @@
-CREATE VIEW growing_stock_view AS (
+CREATE VIEW growing_stock_view AS
+(
 SELECT gst.country_iso,
-       'naturally_regenerating_forest'::TEXT                                      AS row_name,
+       'naturally_regenerating_forest'::TEXT                                          AS row_name,
        sum(gst.naturally_regenerating_forest) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
        sum(gst.naturally_regenerating_forest) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
        sum(gst.naturally_regenerating_forest) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
@@ -14,7 +15,7 @@ FROM growing_stock_total gst
 GROUP BY country_iso
 UNION
 SELECT gst.country_iso,
-       'plantation_forest'::TEXT                                      AS row_name,
+       'plantation_forest'::TEXT                                          AS row_name,
        sum(gst.plantation_forest) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
        sum(gst.plantation_forest) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
        sum(gst.plantation_forest) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
@@ -28,7 +29,7 @@ FROM growing_stock_total gst
 GROUP BY country_iso
 UNION
 SELECT gst.country_iso,
-       'other_planted_forest'::TEXT                                      AS row_name,
+       'other_planted_forest'::TEXT                                          AS row_name,
        sum(gst.other_planted_forest) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
        sum(gst.other_planted_forest) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
        sum(gst.other_planted_forest) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
@@ -42,7 +43,7 @@ FROM growing_stock_total gst
 GROUP BY country_iso
 UNION
 SELECT gst.country_iso,
-       'other_wooded_land'::TEXT                                      AS row_name,
+       'other_wooded_land'::TEXT                                          AS row_name,
        sum(gst.other_wooded_land) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
        sum(gst.other_wooded_land) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
        sum(gst.other_wooded_land) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
@@ -56,7 +57,7 @@ FROM growing_stock_total gst
 GROUP BY country_iso
 UNION
 SELECT gst.country_iso,
-       'planted_forest'::TEXT                                      AS row_name,
+       'planted_forest'::TEXT                                          AS row_name,
        sum(gst.planted_forest) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
        sum(gst.planted_forest) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
        sum(gst.planted_forest) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
@@ -70,7 +71,7 @@ FROM growing_stock_total gst
 GROUP BY country_iso
 UNION
 SELECT gst.country_iso,
-       'forest'::TEXT                                      AS row_name,
+       'forest'::TEXT                                          AS row_name,
        sum(gst.forest) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
        sum(gst.forest) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
        sum(gst.forest) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
@@ -82,6 +83,27 @@ SELECT gst.country_iso,
        sum(gst.forest) FILTER (WHERE gst.year = 2020::NUMERIC) AS "2020"
 FROM growing_stock_total gst
 GROUP BY country_iso
+UNION
+SELECT q.country_iso,
+       'growing_stock_total'::TEXT                                      AS row_name,
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 1990::NUMERIC) AS "1990",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2000::NUMERIC) AS "2000",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2010::NUMERIC) AS "2010",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2015::NUMERIC) AS "2015",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2016::NUMERIC) AS "2016",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2017::NUMERIC) AS "2017",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2018::NUMERIC) AS "2018",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2019::NUMERIC) AS "2019",
+       sum(q.growing_stock_total) FILTER (WHERE q.year = 2020::NUMERIC) AS "2020"
+FROM (SELECT g_1.country_iso,
+             g_1.year,
+             sum(COALESCE(g_1.naturally_regenerating_forest, 0::NUMERIC) +
+                 COALESCE(g_1.plantation_forest, 0::NUMERIC) + COALESCE(g_1.forest, 0::NUMERIC) +
+                 COALESCE(g_1.other_wooded_land, 0::NUMERIC)) AS growing_stock_total
+      FROM growing_stock_total g_1
+      GROUP BY g_1.country_iso, g_1.year
+      ORDER BY g_1.country_iso, g_1.year) q
+GROUP BY q.country_iso
 ORDER BY country_iso, row_name
-);
+    );
 
