65eb82cfa86fbd1b4b2d40781ffb25b6c8a22ef1
Alexander <sasha_durham@hotmail.com>
2020-06-17 08:52:34 +0300

    Pan European table 1.1a metadata: years90_20 substituted years

--- server/traditionalTable/mappings/panEuropean/index.js
+++ server/traditionalTable/mappings/panEuropean/index.js
@@ -1,5 +1,12 @@
 const table11a = require('./table1_1a')
+const table14b = require('./table1_4b')
+const table64b = require('./table6_4b')
+const table64c = require('./table6_4c')
 
 module.exports = {
   panEuropean11a: table11a,
+  panEuropean14b: table14b,
+  panEuropean64b: table64b,
+  panEuropean64c: table64c,  
 }
+
