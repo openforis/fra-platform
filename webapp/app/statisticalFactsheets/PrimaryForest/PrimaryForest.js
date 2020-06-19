ba8082bcd1f4d1d8b6a488be4873ca8d7d9f3cca
Mino Togna <mino.togna@gmail.com>
2020-06-19 12:41:36 +0200

    statisticalFactsheets: added PrimaryForest

---
+++ webapp/app/statisticalFactsheets/PrimaryForest/PrimaryForest.js
@@ -0,0 +1,12 @@
+import './primaryForest.less'
+import React from 'react'
+
+const PrimaryForest = () => {
+  return (
+    <div className="statistical-factsheets-primary-forest">
+      <h3 className="header">Primary forest % of forest area (2020)</h3>
+    </div>
+  )
+}
+
+export default PrimaryForest
