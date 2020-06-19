ede5510d7f7da30ab002284b11b556ced1485d40
Mino Togna <mino.togna@gmail.com>
2020-06-19 12:43:53 +0200

    statisticalFactsheets: added ForestOwnership

---
+++ webapp/app/statisticalFactsheets/ForestOwnership/ForestOwnership.js
@@ -0,0 +1,12 @@
+import './forestOwnership.less'
+import React from 'react'
+
+const ForestOwnership = () => {
+  return (
+    <div className="statistical-factsheets-forest-ownership">
+      <h3 className="header">Forest Ownership 2015</h3>
+    </div>
+  )
+}
+
+export default ForestOwnership
