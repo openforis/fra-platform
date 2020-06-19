d19c049428e5cc57bb5312e133fa330f8a24a307
Mino Togna <mino.togna@gmail.com>
2020-06-19 12:25:58 +0200

    statisticalFactsheets: added template view with ForestArea

---
+++ webapp/app/statisticalFactsheets/ForestArea/ForestArea.js
@@ -0,0 +1,12 @@
+import './forestArea.less'
+import React from 'react'
+
+const ForestArea = () => {
+  return (
+    <div className="statistical-factsheets-forest-area">
+      <h3 className="header">Forest Area (1900 - 2000)</h3>
+    </div>
+  )
+}
+
+export default ForestArea
