2836110ea733ea6d6df418eafacc0eea0db55fa2
Mino Togna <mino.togna@gmail.com>
2020-06-19 12:28:33 +0200

    statisticalFactsheets: added CarbonGrowingStock

---
+++ webapp/app/statisticalFactsheets/CarbonGrowingStock/CarbonGrowingStock.js
@@ -0,0 +1,12 @@
+import './carbonGrowingStock.less'
+import React from 'react'
+
+const CarbonGrowingStock = () => {
+  return (
+    <div className="statistical-factsheets-carbon-growing-stock">
+      <h3 className="header">Carbon and Growing Stock (1900 - 2000)</h3>
+    </div>
+  )
+}
+
+export default CarbonGrowingStock
