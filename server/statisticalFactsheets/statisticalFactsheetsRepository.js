9c445ebc7df03fa134586cbad290dd81db726dcc
Miró Sorja <ping@mirosorja.com>
2020-06-22 13:24:20 +0300

    statisticalFactsheet/api: carbonAndGrowingStock/primaryDesignatedManagementObjective separate endpoints

--- server/statisticalFactsheets/statisticalFactsheetsRepository.js
+++ server/statisticalFactsheets/statisticalFactsheetsRepository.js
@@ -10,6 +10,39 @@ const getStatisticalFactsheetTableAgg = async (schemaName) => {
   return camelize(result.rows)
 }
 
+const getCarbonAndGrowingStock = async (schemaName, level) => {
+  const result = await db.query(`
+    SELECT *
+    FROM ${schemaName}.statistical_factsheets_view
+    WHERE level   = '${level}'
+    AND( row_name = 'carbon_stock_biomass_total'
+       OR row_name = 'carbon_stock_total'
+       OR row_name = 'growing_stock_total'
+       ) 
+  `)
+
+  return camelize(result.rows)
+}
+
+const getPrimaryDesignatedManagementObjective = async (schemaName, level) => {
+  const result = await db.query(`
+    SELECT *
+    FROM ${schemaName}.statistical_factsheets_view
+    WHERE level   = '${level}'
+    AND( row_name = 'production'
+       OR row_name = 'multiple_use'
+       OR row_name = 'conservation'
+       OR row_name = 'other'
+       OR row_name = 'protection_of_soil_and_water'
+       OR row_name = 'social_services'
+       ) 
+  `)
+
+  return camelize(result.rows)
+}
+
 module.exports = {
   getStatisticalFactsheetTableAgg,
+  getCarbonAndGrowingStock,
+  getPrimaryDesignatedManagementObjective,
 }
