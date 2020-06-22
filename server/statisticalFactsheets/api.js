6539f431a75c8a13adf343e287004023e1e79434
Miró Sorja <ping@mirosorja.com>
2020-06-22 13:24:20 +0300

    statisticalFactsheet/api: carbonAndGrowingStock/primaryDesignatedManagementObjective separate endpoints

--- server/statisticalFactsheets/api.js
+++ server/statisticalFactsheets/api.js
@@ -1,4 +1,8 @@
-const { getStatisticalFactsheetTableAgg } = require('./statisticalFactsheetsRepository')
+const {
+  getStatisticalFactsheetTableAgg,
+  getCarbonAndGrowingStock,
+  getPrimaryDesignatedManagementObjective,
+} = require('./statisticalFactsheetsRepository')
 
 const VersionService = require('../versioning/service')
 
@@ -8,5 +12,22 @@ module.exports.init = (app) => {
     const statisticalFactsheetData = await getStatisticalFactsheetTableAgg(schemaName)
 
     res.json(statisticalFactsheetData)
+  })
+
+  app.get('/statisticalFactsheets/carbonAndGrowingStock/:level', async (req, res) => {
+    const schemaName = await VersionService.getDatabaseSchema(req)
+    const carbonAndGrowingStock = await getCarbonAndGrowingStock(schemaName, req.params.level)
+
+    res.json(carbonAndGrowingStock)
+  })
+
+  app.get('/statisticalFactsheets/primaryDesignatedManagementObjective/:level', async (req, res) => {
+    const schemaName = await VersionService.getDatabaseSchema(req)
+    const primaryDesignatedManagementObjective = await getPrimaryDesignatedManagementObjective(
+      schemaName,
+      req.params.level
+    )
+
+    res.json(primaryDesignatedManagementObjective)
   })
 }
