ff4b2afca806538eafedb3bebb16b2a3b267bc34
Miró Sorja <ping@mirosorja.com>
2020-06-22 13:25:32 +0300

    statisticalFactsheets: Updated CarbonGrowingStock.js and PrimaryDesignatedManagementObjective.js

--- webapp/app/statisticalFactsheets/CarbonGrowingStock/CarbonGrowingStock.js
+++ webapp/app/statisticalFactsheets/CarbonGrowingStock/CarbonGrowingStock.js
@@ -1,12 +1,24 @@
 import './carbonGrowingStock.less'
 import React from 'react'
+import PropTypes from 'prop-types'
+import Table from '../components/table'
 
-const CarbonGrowingStock = () => {
+const CarbonGrowingStock = (props) => {
+  const { levelIso } = props
+
+  const tableHeads = ['rowName', '1990', '2000', '2010', '2020']
+
   return (
     <div className="statistical-factsheets-carbon-growing-stock">
       <h3 className="header">Carbon and Growing Stock (1900 - 2000)</h3>
+
+      <Table tableHeads={tableHeads} url={`/api/statisticalFactsheets/carbonAndGrowingStock/${levelIso}`} />
     </div>
   )
+}
+
+CarbonGrowingStock.propTypes = {
+  levelIso: PropTypes.string.isRequired,
 }
 
 export default CarbonGrowingStock
--- webapp/app/statisticalFactsheets/PrimaryDesignatedManagementObjective/PrimaryDesignatedManagementObjective.js
+++ webapp/app/statisticalFactsheets/PrimaryDesignatedManagementObjective/PrimaryDesignatedManagementObjective.js
@@ -1,12 +1,26 @@
 import './primaryDesignatedManagementObjective.less'
 import React from 'react'
+import PropTypes from 'prop-types'
+import Table from '../components/table'
 
-const PrimaryDesignatedManagementObjective = () => {
+const PrimaryDesignatedManagementObjective = (props) => {
+  const { levelIso } = props
+
+  const tableHeads = ['rowName', '1990', '2000', '2010', '2020']
+
   return (
     <div className="statistical-factsheets-naturally-primary-designated-management-objective">
       <h3 className="header">Primary Designated Management Objective (1990 - 2000)</h3>
+      <Table
+        tableHeads={tableHeads}
+        url={`/api/statisticalFactsheets/primaryDesignatedManagementObjective/${levelIso}`}
+      />
     </div>
   )
+}
+
+PrimaryDesignatedManagementObjective.propTypes = {
+  levelIso: PropTypes.string.isRequired,
 }
 
 export default PrimaryDesignatedManagementObjective
