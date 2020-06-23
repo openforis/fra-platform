d2dedac2160f3fc4e9ec6cbcf480764483d5073b
Miró Sorja <ping@mirosorja.com>
2020-06-23 16:45:22 +0300

    statisticalFactsheets: added bar chart for NaturallyRegeneratingForest

--- webapp/app/statisticalFactsheets/NaturallyRegeneratingForest/NaturallyRegeneratingForest.js
+++ webapp/app/statisticalFactsheets/NaturallyRegeneratingForest/NaturallyRegeneratingForest.js
@@ -1,12 +1,39 @@
 import './naturallyRegeneratingForest.less'
-import React from 'react'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Bar } from 'react-chartjs-2'
+import { useGetRequest, useI18n } from '@webapp/components/hooks'
+import * as APIUtils from '../utils/apiUtils'
+import * as ChartUtils from '../utils/chartUtils'
+import { getParams } from '../utils/apiUtils'
 
-const NaturallyRegeneratingForest = () => {
+const NaturallyRegeneratingForest = (props) => {
+  const { levelIso } = props
+  const i18n = useI18n()
+  const section = 'naturallyRegeneratingForest'
+  const chartHeads = ['1990', '2000', '2010', '2020']
+
+  const url = APIUtils.getUrl(levelIso)
+  const params = getParams(section)
+
+  const { data, dispatch: fetchData, loaded } = useGetRequest(url, {
+    params,
+  })
+
+  useEffect(fetchData, [url])
   return (
     <div className="statistical-factsheets-naturally-regenerating-forest">
-      <h3 className="header">Naturally regenerating forest and planted forest (1990 - 2000)</h3>
+      <h3 className="header">{i18n.t(`statisticalFactsheets.${section}.title`)}</h3>
+      <Bar
+        data={ChartUtils.getData(data, chartHeads, section, loaded, i18n)}
+        options={ChartUtils.getOptions('stackedBar', loaded)}
+      />
     </div>
   )
+}
+
+NaturallyRegeneratingForest.propTypes = {
+  levelIso: PropTypes.string.isRequired,
 }
 
 export default NaturallyRegeneratingForest
