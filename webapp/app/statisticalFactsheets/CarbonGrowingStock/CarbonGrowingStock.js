3ced04f2850fa0990a083c9b60f43ad8f950fe00
Miró Sorja <ping@mirosorja.com>
2020-06-23 11:21:46 +0300

    statisticalFactsheets: added en translations for tables and titles

--- webapp/app/statisticalFactsheets/CarbonGrowingStock/CarbonGrowingStock.js
+++ webapp/app/statisticalFactsheets/CarbonGrowingStock/CarbonGrowingStock.js
@@ -1,18 +1,20 @@
 import './carbonGrowingStock.less'
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useI18n } from '@webapp/components/hooks'
 import Table from '../components/table'
 
 const CarbonGrowingStock = (props) => {
   const { levelIso } = props
+  const i18n = useI18n()
 
   const tableHeads = ['rowName', '1990', '2000', '2010', '2020']
-
+  const section = 'carbonAndGrowingStock'
   return (
     <div className="statistical-factsheets-carbon-growing-stock">
-      <h3 className="header">Carbon and Growing Stock (1900 - 2000)</h3>
+      <h3 className="header">{i18n.t(`statisticalFactsheets.${section}.title`)}</h3>
 
-      <Table tableHeads={tableHeads} url={`/api/statisticalFactsheets/carbonAndGrowingStock/${levelIso}`} />
+      <Table tableHeads={tableHeads} section={section} url={`/api/statisticalFactsheets/${section}/${levelIso}`} />
     </div>
   )
 }
--- webapp/app/statisticalFactsheets/components/table/table.js
+++ webapp/app/statisticalFactsheets/components/table/table.js
@@ -5,7 +5,7 @@ import useGetRequest from '@webapp/components/hooks/useGetRequest'
 
 const Table = (props) => {
   const i18n = useI18n()
-  const { url, tableHeads } = props
+  const { url, tableHeads, section } = props
 
   const { data, dispatch: fetchData } = useGetRequest(url)
 
@@ -15,13 +15,15 @@ const Table = (props) => {
     return null
   }
 
+  const t = (value) => (Number.isNaN(+value) ? i18n.t(`statisticalFactsheets.${section}.${value}`) : value)
+
   return (
     <table className="fra-table">
       <thead>
         <tr>
           {tableHeads.map((key) => (
             <th key={key} className="fra-table__header-cell">
-              {i18n.t(key)}
+              {i18n.t(t(key))}
             </th>
           ))}
         </tr>
@@ -31,7 +33,7 @@ const Table = (props) => {
           <tr key={row.rowName}>
             {tableHeads.map((key) => (
               <td key={`${row.rowName}-${row[key]}-${key}`} className="fra-table__calculated-cell">
-                {i18n.t(row[key])}
+                {t(row[key])}
               </td>
             ))}
           </tr>
@@ -44,6 +46,7 @@ const Table = (props) => {
 Table.propTypes = {
   tableHeads: PropTypes.array.isRequired,
   url: PropTypes.string.isRequired,
+  section: PropTypes.string.isRequired,
 }
 
 export default Table
