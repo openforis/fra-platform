c2397048537cbf68a643bc8967e8b69a2195f391
Miró Sorja <ping@mirosorja.com>
2020-06-29 13:12:41 +0300

    dataExport/state: check if panEuropean for countries, reset countries on assessmentType change

--- webapp/app/dataExport/useDataExportState.js
+++ webapp/app/dataExport/useDataExportState.js
@@ -8,20 +8,23 @@ import { TableSpec } from '@webapp/app/assessment/components/section/sectionSpec
 import { throttle } from '@webapp/utils/functionUtils'
 
 import { formatColumn, formatSection } from '@webapp/app/dataExport/utils/format'
+import { isPanEuropean } from '@webapp/app/dataExport/utils/panEuropean'
+
+const initialSelection = {
+  countries: [],
+  columns: [],
+  // { param, label }
+  variable: {},
+}
 
 export default () => {
   const { section = '', assessmentType } = useParams()
   const [variables, setVariables] = useState([])
   const [columns, setColumns] = useState([])
 
-  const [selection, setSelection] = useState({
-    countries: [],
-    columns: [],
-    // { param, label }
-    variable: {},
-  })
+  const [selection, setSelection] = useState({ ...initialSelection })
 
-  const { data: countries = [], dispatch: fetchCountries } = useGetRequest(`/api/countries`)
+  const { data: allCountries = [], dispatch: fetchCountries } = useGetRequest(`/api/countries`)
 
   const hasSelection = !!(selection.countries.length && selection.columns.length && selection.variable.param)
 
@@ -30,7 +33,7 @@ export default () => {
     dispatch: fetchResults,
     setState: setResultState,
     loading: resultsLoading,
-  } = useGetRequest(`/api/export/${snake(formatSection(section))}`, {
+  } = useGetRequest(`/api/export/${assessmentType}/${snake(formatSection(section, assessmentType))}`, {
     params: {
       columns: selection.columns.map(({ param }) => param).map((column) => formatColumn(column, section)),
       countries: selection.countries.map(({ param }) => param),
@@ -59,6 +62,12 @@ export default () => {
     }
   }, [section])
 
+  // If assessmentType (panEuropean -> FRA2020 -> panEuropean) changes,
+  // reset countries
+  useEffect(() => {
+    setSelection({ ...initialSelection })
+  }, [assessmentType])
+
   useEffect(() => {
     if (section && hasSelection) {
       throttle(fetchResults, `fetchDataExportResults`, 800)()
@@ -68,6 +77,12 @@ export default () => {
   const setSelectionCountries = (value) => setSelection({ ...selection, countries: value })
   const setSelectionColumns = (value) => setSelection({ ...selection, columns: value })
   const setSelectionVariable = (value) => setSelection({ ...selection, variable: value })
+
+  // Handle pan european countries
+  // const isPaneuropean = (country) => country.panEuropean
+  const isPanEuropeanAssessment = assessmentType === 'panEuropean'
+  const panEuropeanCountries = allCountries.filter(isPanEuropean)
+  const countries = isPanEuropeanAssessment ? panEuropeanCountries : allCountries
 
   return {
     results,
