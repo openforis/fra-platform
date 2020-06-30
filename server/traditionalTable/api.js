cdc95280581db9884935ae7ecc938bab428608db
Miró Sorja <ping@mirosorja.com>
2020-06-30 14:01:29 +0300

    traditionalTable: add :assessmentType

--- server/traditionalTable/api.js
+++ server/traditionalTable/api.js
@@ -1,21 +1,20 @@
 const db = require('../db/db')
 const repository = require('./traditionalTableRepository')
-const {sendErr, sendOk} = require('../utils/requestUtils')
+const { sendErr, sendOk } = require('../utils/requestUtils')
 
 const Auth = require('../auth/authApiMiddleware')
 const VersionService = require('../versioning/service')
 
-module.exports.init = app => {
-
+module.exports.init = (app) => {
   app.post('/traditionalTable/:countryIso/:tableSpecName', Auth.requireCountryEditPermission, async (req, res) => {
     try {
-      const tableSpecName = req.params.tableSpecName
-      const countryIso = req.params.countryIso
+      const {
+        user,
+        body,
+        params: { countryIso, tableSpecName },
+      } = req
 
-      await db.transaction(
-        repository.save,
-        [req.user, countryIso, tableSpecName, req.body]
-      )
+      await db.transaction(repository.save, [user, countryIso, tableSpecName, body])
 
       sendOk(res)
     } catch (err) {
@@ -23,14 +22,16 @@ module.exports.init = app => {
     }
   })
 
-  app.get('/traditionalTable/:countryIso/:tableSpecName', async (req, res) => {
+  app.get('/traditionalTable/:assessmentType/:countryIso/:tableSpecName', async (req, res) => {
     try {
-      const schemaName = await VersionService.getDatabaseSchema(req)
-      const result = await repository.read(req.params.countryIso, req.params.tableSpecName, schemaName)
+      const {
+        params: { assessmentType, countryIso, tableSpecName },
+      } = req
+      const schemaName = assessmentType === 'panEuropean' ? 'pan_european' : await VersionService.getDatabaseSchema(req)
+      const result = await repository.read(countryIso, tableSpecName, schemaName)
       res.json(result)
     } catch (err) {
       sendErr(res, err)
     }
   })
-
 }
