97c39a5440b528873caee918745e47d8591e3a94
Miró Sorja <ping@mirosorja.com>
2020-07-07 12:43:41 +0300

    landingView: hide tabs if region

--- webapp/app/landing/landingView.js
+++ webapp/app/landing/landingView.js
@@ -11,6 +11,8 @@ import useI18n from '@webapp/components/hooks/useI18n'
 import useUserInfo from '@webapp/components/hooks/useUserInfo'
 import useLandingViewSections from '@webapp/app/landing/useLandingViewSections'
 
+import * as Area from '@common/country/area'
+
 const LandingView = () => {
   const countryIso = useCountryIso()
   const userInfo = useUserInfo()
@@ -19,6 +21,7 @@ const LandingView = () => {
   const url = `/${countryIso}/`
   const sections = useLandingViewSections()
   const userAndCountry = userInfo && countryIso
+  const displayTabs = userAndCountry && Area.isISOCountry(countryIso)
 
   return (
     <div className="app-view__content">
@@ -26,7 +29,7 @@ const LandingView = () => {
         <h1 className="landing__page-title">
           {countryIso ? i18n.t(`area.${countryIso}.listName`) : i18n.t('common.fraPlatform')}
         </h1>
-        {userAndCountry && (
+        {displayTabs && (
           <div className="landing__page-menu">
             {sections.map(({ name: section }) => (
               <NavLink
