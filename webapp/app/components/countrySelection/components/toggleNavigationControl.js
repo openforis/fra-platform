25e5e2543bee364eac38f572dc2b2d5b9506d62f
Mino Togna <mino.togna@gmail.com>
2020-07-08 06:37:52 +0200

    Disable toggle navigation button in home page

--- webapp/app/components/countrySelection/components/toggleNavigationControl.js
+++ webapp/app/components/countrySelection/components/toggleNavigationControl.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { matchPath, useLocation } from 'react-router'
 
-import { useCountryIso } from '@webapp/components/hooks'
 import Icon from '@webapp/components/icon'
 
 import { toggleNavigation } from '@webapp/app/components/navigation/actions'
 
 const ToggleNavigationControl = () => {
   const dispatch = useDispatch()
-  const countryIso = useCountryIso()
+  const { pathname } = useLocation()
+  const matchHome = Boolean(matchPath(pathname, { path: '/', exact: true }))
 
   return (
     <button
       type="button"
       className="btn app-header__toggle-navigation-visible"
-      disabled={!countryIso}
+      disabled={matchHome}
       onClick={() => {
         dispatch(toggleNavigation())
       }}
