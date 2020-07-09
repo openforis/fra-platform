4668b3fe2cf0a644cc451ff71a7ecdabb9265259
Mino Togna <mino.togna@gmail.com>
2020-07-09 21:26:39 +0200

    Fixed footer links when user is not logged in

--- webapp/components/footer/components/sendFeedback.js
+++ webapp/components/footer/components/sendFeedback.js
@@ -13,19 +13,15 @@ const SendFeedback = () => {
 ${newLine}${newLine}
 *** ${i18n.t('navigation.support.dontDelete')} ***
 ${newLine}${newLine}
-${
-  userInfo
-    ? `${i18n.t('navigation.support.user')}: ${userInfo.name} (${userInfo.id})
-        ${newLine}`
-    : ''
-}
+${i18n.t('navigation.support.user')}: ${userInfo.name} (${userInfo.id})
+${newLine}
 ${i18n.t('navigation.support.platformVersion')}: ${__APPLICATION_VERSION__}
 ${newLine}
 ${i18n.t('navigation.support.userAgent')}: ${navigator.userAgent}
 `
 
   return (
-    <a target="_top" href={`mailto:fra@fao.org?subject=${subject}&body=${body}`}>
+    <a target="_top" href={`mailto:fra@fao.org?subject=${subject}&body=${body.trim()}`}>
       {i18n.t('footer.sendFeedback')}
     </a>
   )
