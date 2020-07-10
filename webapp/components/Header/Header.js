376e5affd946d9b66bcccba1a2eff056673098e2
Miró Sorja <ping@mirosorja.com>
2020-07-10 14:58:50 +0300

    header: hide autosave if in country root

--- webapp/components/Header/Header.js
+++ webapp/components/Header/Header.js
@@ -16,6 +16,7 @@ const Header = () => {
   const i18n = useI18n()
   const { pathname } = useLocation()
   const isLogin = Boolean(matchPath(pathname, { path: BasePaths.login }))
+  const isCountry = Boolean(matchPath(pathname, { path: BasePaths.country, exact: true }))
 
   return (
     <div className="app-header no-print">
@@ -25,7 +26,7 @@ const Header = () => {
 
       <div className="app-header__global-fra">
         {i18n.t('common.globalFRA')}
-        <AutoSaveStatusText />
+        {!isCountry && <AutoSaveStatusText />}
       </div>
 
       <div className="app-header__menu">
