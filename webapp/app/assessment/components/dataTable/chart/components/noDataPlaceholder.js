6cf4ff4d8d4ed1c45f41defbd4ae29ae76128a83
Mino Togna <mino.togna@gmail.com>
2020-07-15 09:29:13 +0200

    Table 1a/1b: removed no data placeholder text

--- webapp/app/assessment/components/dataTable/chart/components/noDataPlaceholder.js
+++ webapp/app/assessment/components/dataTable/chart/components/noDataPlaceholder.js
@@ -2,11 +2,6 @@ import React from 'react'
 import * as d3 from 'd3'
 import { hasData, defaultTransitionDuration } from '../chart'
 
-const Text = ({ text, width, y }) =>
-  <foreignObject data-y={y} width={width} height="25" y={y} style={{ textAlign: 'center' }}>
-    <span style={{ fontSize: '14px', color: '#999999' }} x="0" y="0">{text}</span>
-  </foreignObject>
-
 const tucanY = 20
 const tucanWidth = 62
 const tucanHeight = 87
@@ -21,11 +16,6 @@ class NoDataPlaceholder extends React.Component {
     return d3.select(this.refs.tucan)
   }
 
-  texts () {
-    let textWrappers = d3.select(this.refs.container).selectAll('foreignObject')
-    return textWrappers.selectAll('span')
-  }
-
   hidePlaceholderAnimated () {
     this.tucan()
       .transition()
@@ -33,14 +23,6 @@ class NoDataPlaceholder extends React.Component {
       .delay(100)
       .ease(d3.easeBackInOut)
       .attr('y', -tucanHeight)
-      .style('opacity', '0')
-      .transition()
-      .style('visibility', 'hidden')
-    this.texts()
-      .transition()
-      .duration(defaultTransitionDuration)
-      .delay(100)
-      .ease(d3.easePolyOut)
       .style('opacity', '0')
       .transition()
       .style('visibility', 'hidden')
@@ -52,21 +34,11 @@ class NoDataPlaceholder extends React.Component {
       .duration(100)
       .style('visibility', 'hidden')
       .style('opacity', '0')
-    this.texts()
-      .transition()
-      .duration(100)
-      .style('visibility', 'hidden')
-      .style('opacity', '0')
   }
 
   showPlaceholder () {
     this.tucan()
       .attr('y', tucanY)
-      .transition()
-      .duration(100)
-      .style('visibility', 'visible')
-      .style('opacity', '1')
-    this.texts()
       .transition()
       .duration(100)
       .style('visibility', 'visible')
@@ -97,8 +69,6 @@ class NoDataPlaceholder extends React.Component {
 
     return <g className="chart__no-data-placeholder" ref="container">
       <image ref="tucan" href="/img/tucan.svg" width={tucanWidth} height={tucanHeight} x={this.getTucanX()} y={tucanY} style={{opacity: 0}}/>
-      <Text text={i18n.t('extentOfForest.chart.placeholderLine1')} width={wrapperWidth} y="144" style={{opacity: 0}}/>
-      <Text text={i18n.t('extentOfForest.chart.placeholderLine2')} width={wrapperWidth} y="164" style={{opacity: 0}}/>
     </g>
   }
 }
