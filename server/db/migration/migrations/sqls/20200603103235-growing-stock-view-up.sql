273183e1b64ce6890e65046372cf07610c0e1efb
Mino Togna <mino.togna@gmail.com>
2020-07-21 15:48:42 +0200

    Fixed growing stock view

--- server/db/migration/migrations/sqls/20200603103235-growing-stock-view-up.sql
+++ server/db/migration/migrations/sqls/20200603103235-growing-stock-view-up.sql
@@ -86,27 +86,19 @@ SELECT gst.country_iso,
 FROM growing_stock_total gst
 GROUP BY country_iso
 UNION
-SELECT q.country_iso,
+SELECT gst.country_iso,
        'growing_stock_total'::TEXT                                      AS row_name,
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 1990::NUMERIC) AS "1990",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2000::NUMERIC) AS "2000",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2010::NUMERIC) AS "2010",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2015::NUMERIC) AS "2015",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2016::NUMERIC) AS "2016",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2017::NUMERIC) AS "2017",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2018::NUMERIC) AS "2018",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2019::NUMERIC) AS "2019",
-       sum(q.growing_stock_total) FILTER (WHERE q.year = 2020::NUMERIC) AS "2020"
-FROM (SELECT g_1.country_iso,
-             g_1.year,
-             sum(
-                         COALESCE(g_1.naturally_regenerating_forest, 0::NUMERIC) +
-                         COALESCE(g_1.planted_forest, 0::NUMERIC) + COALESCE(g_1.forest, 0::NUMERIC)
-                 ) AS growing_stock_total
-      FROM growing_stock_total g_1
-      GROUP BY g_1.country_iso, g_1.year
-      ORDER BY g_1.country_iso, g_1.year) q
-GROUP BY q.country_iso
+       sum(gst.forest) FILTER (WHERE gst.year = 1990::NUMERIC) AS "1990",
+       sum(gst.forest) FILTER (WHERE gst.year = 2000::NUMERIC) AS "2000",
+       sum(gst.forest) FILTER (WHERE gst.year = 2010::NUMERIC) AS "2010",
+       sum(gst.forest) FILTER (WHERE gst.year = 2015::NUMERIC) AS "2015",
+       sum(gst.forest) FILTER (WHERE gst.year = 2016::NUMERIC) AS "2016",
+       sum(gst.forest) FILTER (WHERE gst.year = 2017::NUMERIC) AS "2017",
+       sum(gst.forest) FILTER (WHERE gst.year = 2018::NUMERIC) AS "2018",
+       sum(gst.forest) FILTER (WHERE gst.year = 2019::NUMERIC) AS "2019",
+       sum(gst.forest) FILTER (WHERE gst.year = 2020::NUMERIC) AS "2020"
+FROM growing_stock_total gst
+GROUP BY country_iso
 ORDER BY country_iso, row_name
     );
 
