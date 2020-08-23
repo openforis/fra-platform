0b192134e104acdfb172774155eb80169ec46583
Alexander <sasha_durham@hotmail.com>
2020-08-23 16:26:05 +0300

    Pan European table 6.5a metadata

--- server/traditionalTable/mappings/panEuropean/index.js
+++ server/traditionalTable/mappings/panEuropean/index.js
@@ -1,9 +1,9 @@
 const table11a = require('./table1_1a')
-const table12a = require('./table1_2a')
 const table14b = require('./table1_4b')
+const table65a = require('./table6_5a')
 
 module.exports = {
   table_1_1a: table11a,
-  table_1_2a: table12a,
   table_1_4b: table14b,
+  table_6_5a: table65a,
 }
