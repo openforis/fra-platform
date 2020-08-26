da46f31f4b40e79b6baa9780b86596ddcfb513c1
Miró Sorja <ping@mirosorja.com>
2020-08-26 12:14:28 +0300

    disturbances: remove unused code/files

--- webapp/app/assessment/fra/sections/disturbances/disturbancesValidatorState.js
+++ webapp/app/assessment/fra/sections/disturbances/disturbancesValidatorState.js
@@ -3,13 +3,13 @@ import * as FRA from '@common/assessment/fra'
 import * as DisturbancesState from '@webapp/app/assessment/fra/sections/disturbances/disturbancesState'
 import * as ExtentOfForestValidatorState from '@webapp/app/assessment/fra/sections/extentOfForest/extentOfForestValidatorState'
 
-export const disturbancesTotalValidator = colIdx => state =>
+export const disturbancesTotalValidator = (colIdx) => (state) =>
   ExtentOfForestValidatorState.lessThanOrEqualToForestValidator(
     FRA.yearsAnnual[colIdx],
     DisturbancesState.getDisturbancesTotal(colIdx)(state)
   )(state)
 
-export const getValidationMessages = data => state => {
+export const getValidationMessages = (data) => (state) => {
   const colNo = data[0].length
   const messages = []
 
