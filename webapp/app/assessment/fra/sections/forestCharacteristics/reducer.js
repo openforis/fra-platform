ddff422f07b233c68e98f2f181de4b7e40747850
Miró Sorja <ping@mirosorja.com>
2020-08-26 14:22:25 +0300

    forestCharacteristics: remove unused code/files

--- webapp/app/assessment/fra/sections/forestCharacteristics/reducer.js
+++ webapp/app/assessment/fra/sections/forestCharacteristics/reducer.js
@@ -1,14 +1,22 @@
+import { applyReducerFunction } from '@webapp/utils/reduxUtils'
 import * as tableWithOdpActions from '../../components/tableWithOdp/actions'
 import { updateValueReducer, updateValuesReducer } from '../../components/tableWithOdp/reducerFunctions'
-import { applyReducerFunction } from '@webapp/utils/reduxUtils'
 
 const actionHandlers = {
   [tableWithOdpActions.valueChangeStart('forestCharacteristics')]: (state, action) => updateValueReducer(state, action),
-  [tableWithOdpActions.valuesFetched('forestCharacteristics')]: (state, action) => ({...state, ...action.data, generatingFraValues: false}),
-  [tableWithOdpActions.generateFraValuesStart('forestCharacteristics')]: (state, action) => ({...state, generatingFraValues: true}),
-  [tableWithOdpActions.pasteChangeStart('forestCharacteristics')]: (state, action) => updateValuesReducer(state, action),
-  [tableWithOdpActions.odpCleanAction('forestCharacteristics')]: (state, action) => ({...state, odpDirty: false}),
-  [tableWithOdpActions.odpDirtyAction]: (state, action) => ({...state, odpDirty: true})
+  [tableWithOdpActions.valuesFetched('forestCharacteristics')]: (state, action) => ({
+    ...state,
+    ...action.data,
+    generatingFraValues: false,
+  }),
+  [tableWithOdpActions.generateFraValuesStart('forestCharacteristics')]: (state) => ({
+    ...state,
+    generatingFraValues: true,
+  }),
+  [tableWithOdpActions.pasteChangeStart('forestCharacteristics')]: (state, action) =>
+    updateValuesReducer(state, action),
+  [tableWithOdpActions.odpCleanAction('forestCharacteristics')]: (state) => ({ ...state, odpDirty: false }),
+  [tableWithOdpActions.odpDirtyAction]: (state) => ({ ...state, odpDirty: true }),
 }
 
 export default (state = {}, action) => applyReducerFunction(actionHandlers, state, action)
