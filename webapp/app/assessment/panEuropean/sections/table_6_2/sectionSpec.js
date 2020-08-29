398a26d1bd113400a08ddc58d1247ca8384fa926
Alexander <sasha_durham@hotmail.com>
2020-08-29 19:29:06 +0300

    Pan European table 6.2 metadata

---
+++ webapp/app/assessment/panEuropean/sections/table_6_2/sectionSpec.js
@@ -0,0 +1,74 @@
+import * as PanEuropean from '@common/assessment/panEuropean'
+
+import * as SectionSpec from '@webapp/app/assessment/components/section/sectionSpec'
+
+const section = PanEuropean.sections['6'].children['62']
+
+const variables = [
+  'forestry',
+  'manufacture_of_wood_and_articles_in_wood',
+  'manufacture_of_paper_and_paper_products',
+]
+
+const years = [...PanEuropean.years90_15].reverse();
+
+const tableSpec = SectionSpec.newTableSpec({
+  [SectionSpec.KEYS_TABLE.name]: section.tables.table_6_2,
+  [SectionSpec.KEYS_TABLE.columnsExport]: ['million_national_currency'],
+  [SectionSpec.KEYS_TABLE.columnsExport]: ['percent_of_total_gva'],
+  
+  [SectionSpec.KEYS_TABLE.rows]: [
+    // row header
+    SectionSpec.newRowHeader({
+      [SectionSpec.KEYS_ROW.cols]: [
+        SectionSpec.newColHeader({
+          [SectionSpec.KEYS_COL.labelKey]: 'panEuropean.grossValueAdded.categoryYear',
+          [SectionSpec.KEYS_COL.rowSpan]: 2,
+          [SectionSpec.KEYS_COL.left]: true,
+        }),
+        SectionSpec.newColHeader({
+          [SectionSpec.KEYS_COL.labelKey]: 'panEuropean.grossValueAdded.grossValueAddedCol',
+          [SectionSpec.KEYS_COL.colSpan]: 2,
+        }),
+      ],
+    }),
+
+    SectionSpec.newRowHeader({
+      [SectionSpec.KEYS_ROW.cols]: [
+        SectionSpec.newColHeader({
+          [SectionSpec.KEYS_COL.labelKey]: 'panEuropean.grossValueAdded.million_national_currency',
+        }),
+        SectionSpec.newColHeader({
+          [SectionSpec.KEYS_COL.labelKey]: 'panEuropean.grossValueAdded.percent_of_total_gva',
+        }),
+      ],
+    }),
+
+    // rows data
+    ...variables.flatMap((variable) =>
+      years.map((year) =>
+        SectionSpec.newRowData({
+          [SectionSpec.KEYS_ROW.labelKey]: `panEuropean.grossValueAdded.${variable}`,
+          [SectionSpec.KEYS_ROW.labelParams]: { year },
+          [SectionSpec.KEYS_ROW.variableExport]: `${variable}_${year}`,
+          [SectionSpec.KEYS_ROW.cols]: [
+            SectionSpec.newColDecimal(),
+            SectionSpec.newColDecimal(),
+          ],
+        })
+      )
+    ),
+  ],
+})
+
+const tableSection = SectionSpec.newTableSection({
+  [SectionSpec.KEYS_TABLE_SECTION.tableSpecs]: [tableSpec],
+})
+
+const grossValueAdded = SectionSpec.newSectionSpec({
+  [SectionSpec.KEYS_SECTION.sectionName]: section.name,
+  [SectionSpec.KEYS_SECTION.sectionAnchor]: section.anchor,
+  [SectionSpec.KEYS_SECTION.tableSections]: [tableSection],
+})
+
+export default grossValueAdded
