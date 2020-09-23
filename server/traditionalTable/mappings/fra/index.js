b014c1a9421fea6e312cbdb5c42cdf734b8651bc
Miró Sorja <ping@mirosorja.com>
2020-09-23 15:15:29 +0300

    server/traditionalTable: use spread for FRA table mappings

---
+++ server/traditionalTable/mappings/fra/index.js
@@ -0,0 +1,75 @@
+const specificForestCategories = require('./specificForestCategories')
+const forestAreaChange = require('./forestAreaChange')
+const holderOfManagementRights = require('./holderOfManagementRights')
+const primaryDesignatedManagementObjective = require('./primaryDesignatedManagementObjective')
+const areaAffectedByFire = require('./areaAffectedByFire')
+const areaAffectedByFirePrint1 = require('./areaAffectedByFirePrint1')
+const areaAffectedByFirePrint2 = require('./areaAffectedByFirePrint2')
+const growingStockComposition = require('./growingStockComposition')
+const nonWoodForestProductsRemovals = require('./nonWoodForestProductsRemovals')
+const nonWoodForestProductsRemovalsCurrency = require('./nonWoodForestProductsRemovalsCurrency')
+const biomassStock = require('./biomassStock')
+const carbonStock = require('./carbonStock')
+const carbonStockSoilDepth = require('./carbonStockSoilDepth')
+const degradedForest = require('./degradedForest')
+const employment = require('./employment')
+const employmentPrint1 = require('./employmentPrint1')
+const employmentPrint2 = require('./employmentPrint2')
+const graduationOfStudents = require('./graduationOfStudents')
+const graduationOfStudentsPrint1 = require('./graduationOfStudentsPrint1')
+const graduationOfStudentsPrint2 = require('./graduationOfStudentsPrint2')
+const forestAreaWithinProtectedAreas = require('./forestAreaWithinProtectedAreas')
+const totalAreaWithDesignatedManagementObjective = require('./totalAreaWithDesignatedManagementObjective')
+const annualReforestation = require('./annualReforestation')
+const forestOwnership = require('./forestOwnership')
+const disturbances = require('./disturbances')
+const disturbancesPrint1 = require('./disturbancesPrint1')
+const disturbancesPrint2 = require('./disturbancesPrint2')
+const areaOfPermanentForestEstate = require('./areaOfPermanentForestEstate')
+const forestPolicy = require('./forestPolicy')
+const otherLandWithTreeCover = require('./otherLandWithTreeCover')
+const climaticDomain = require('./climaticDomain')
+const sustainableDevelopmentAgencyIndicator = require('./sustainableDevelopmentAgencyIndicator')
+const sustainableDevelopmentAgencySubIndicator1 = require('./sustainableDevelopmentAgencySubIndicator1')
+const sustainableDevelopmentAgencySubIndicator2 = require('./sustainableDevelopmentAgencySubIndicator2')
+const sustainableDevelopmentAgencySubIndicator3 = require('./sustainableDevelopmentAgencySubIndicator3')
+const sustainableDevelopmentAgencySubIndicator4 = require('./sustainableDevelopmentAgencySubIndicator4')
+
+module.exports = {
+  specificForestCategories,
+  forestAreaChange,
+  primaryDesignatedManagementObjective,
+  areaAffectedByFire,
+  areaAffectedByFirePrint1,
+  areaAffectedByFirePrint2,
+  growingStockComposition,
+  holderOfManagementRights,
+  nonWoodForestProductsRemovals,
+  nonWoodForestProductsRemovalsCurrency,
+  degradedForest,
+  employment,
+  employmentPrint1,
+  employmentPrint2,
+  graduationOfStudents,
+  graduationOfStudentsPrint1,
+  graduationOfStudentsPrint2,
+  forestOwnership,
+  forestAreaWithinProtectedAreas,
+  totalAreaWithDesignatedManagementObjective,
+  annualReforestation,
+  disturbances,
+  disturbancesPrint1,
+  disturbancesPrint2,
+  biomassStock,
+  carbonStock,
+  carbonStockSoilDepth,
+  areaOfPermanentForestEstate,
+  forestPolicy,
+  otherLandWithTreeCover,
+  climaticDomain,
+  sustainableDevelopmentAgencyIndicator,
+  sustainableDevelopmentAgencySubIndicator1,
+  sustainableDevelopmentAgencySubIndicator2,
+  sustainableDevelopmentAgencySubIndicator3,
+  sustainableDevelopmentAgencySubIndicator4,
+}
