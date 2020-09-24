033a586a80033ecc5f3f569d1fd4c1a83a2a3092
Miró Sorja <ping@mirosorja.com>
2020-09-24 14:31:34 +0300

    contentCheck: sort imports

--- webapp/app/countryLanding/views/contentCheck/contentCheck.js
+++ webapp/app/countryLanding/views/contentCheck/contentCheck.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
-import AssessmentSection from '@webapp/app/assessment/components/section/assessmentSectionView/assessmentSection'
-import * as FRA from '@common/assessment/fra'
-import { useCountryIso } from '@webapp/components/hooks'
 import { useDispatch } from 'react-redux'
-import useSectionTables from '@webapp/app/assessment/components/section/assessmentSectionView/useSectionTables'
+
 import { batchActions } from '@webapp/main/reduxBatch'
 import { fetchTableData } from '@webapp/app/assessment/components/dataTable/actions'
+import { useCountryIso } from '@webapp/components/hooks'
+
+import useSectionTables from '@webapp/app/assessment/components/section/assessmentSectionView/useSectionTables'
+import AssessmentSection from '@webapp/app/assessment/components/section/assessmentSectionView/assessmentSection'
 import * as SectionSpec from '@webapp/app/assessment/components/section/sectionSpec'
+
+import * as FRA from '@common/assessment/fra'
 
 const ContentCheck = () => {
   const assessmentType = FRA.type
