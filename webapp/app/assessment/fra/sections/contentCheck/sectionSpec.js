e085de46b0861176d0624d7a2832f6e8009ca99a
Miró Sorja <ping@mirosorja.com>
2020-09-24 11:48:54 +0300

    contentCheck: add contentCheck sectionSpec

---
+++ webapp/app/assessment/fra/sections/contentCheck/sectionSpec.js
@@ -0,0 +1,73 @@
+import * as FRA from '@common/assessment/fra'
+
+import * as SectionSpec from '@webapp/app/assessment/components/section/sectionSpec'
+
+const section = {
+  name: 'contentCheck',
+  tables: {
+    extent: 'extent',
+  },
+}
+
+const variables = [
+  'forest_area',
+  'other_wooded_land',
+
+  'primary_forest_percent',
+  'protected_forest_percent',
+  'management_plan_percent',
+
+  'certified_area',
+  'mangroves',
+  'bamboo',
+]
+
+const tableSpecExtent = SectionSpec.newTableSpec({
+  [SectionSpec.KEYS_TABLE.name]: section.tables.extent,
+  [SectionSpec.KEYS_TABLE.rows]: [
+    SectionSpec.newRowHeader({
+      [SectionSpec.KEYS_ROW.cols]: [
+        SectionSpec.newColHeader({
+          [SectionSpec.KEYS_COL.labelKey]: 'contentCheck.extent',
+          [SectionSpec.KEYS_COL.rowSpan]: 2,
+          [SectionSpec.KEYS_COL.left]: true,
+        }),
+      ],
+    }),
+
+    SectionSpec.newRowHeader({
+      [SectionSpec.KEYS_ROW.cols]: FRA.years.map((year) =>
+        SectionSpec.newColHeader({
+          [SectionSpec.KEYS_COL.label]: year,
+        })
+      ),
+    }),
+
+    ...variables.map((variable) =>
+      SectionSpec.newRowData({
+        [SectionSpec.KEYS_ROW.labelKey]: `contentCheck.${variable}`,
+        [SectionSpec.KEYS_ROW.variableExport]: `${variable}`,
+        [SectionSpec.KEYS_ROW.cols]: FRA.years.map(() => SectionSpec.newColDecimal()),
+      })
+    ),
+  ],
+})
+
+const tableSection = SectionSpec.newTableSection({
+  [SectionSpec.KEYS_TABLE_SECTION.tableSpecs]: [tableSpecExtent],
+})
+
+const contentCheckView = SectionSpec.newSectionSpec({
+  [SectionSpec.KEYS_SECTION.sectionName]: section.name,
+  [SectionSpec.KEYS_SECTION.sectionAnchor]: '',
+  [SectionSpec.KEYS_SECTION.tableSections]: [tableSection],
+  [SectionSpec.KEYS_SECTION.showTitle]: false,
+  [SectionSpec.KEYS_SECTION.dataExport]: { [SectionSpec.KEYS_DATA_EXPORT.included]: false },
+  [SectionSpec.KEYS_SECTION.descriptions]: {
+    [SectionSpec.KEYS_SECTION_DESCRIPTIONS.nationalData]: false,
+    [SectionSpec.KEYS_SECTION_DESCRIPTIONS.analysisAndProcessing]: false,
+    [SectionSpec.KEYS_SECTION_DESCRIPTIONS.comments]: false,
+  },
+})
+
+export default contentCheckView
--- webapp/app/countryLanding/views/contentCheck/contentCheck.js
+++ webapp/app/countryLanding/views/contentCheck/contentCheck.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import ResultsTable from './components/resultsTable'
+import React, { useEffect } from 'react'
+import AssessmentSection from '@webapp/app/assessment/components/section/assessmentSectionView/assessmentSection'
+import * as FRA from '@common/assessment/fra'
+import { useCountryIso } from '@webapp/components/hooks'
+import { useDispatch } from 'react-redux'
+import useSectionTables from '@webapp/app/assessment/components/section/assessmentSectionView/useSectionTables'
+import { batchActions } from '@webapp/main/reduxBatch'
+import { fetchTableData } from '@webapp/app/assessment/components/dataTable/actions'
+import * as SectionSpec from '@webapp/app/assessment/components/section/sectionSpec'
 
 const ContentCheck = () => {
-  const sections = ['extent']
-  return (
-    <>
-      {sections.map((section) => (
-        <ResultsTable key={section} section={section} />
-      ))}
-    </>
-  )
+  const assessmentType = FRA.type
+  const sectionName = 'contentCheck'
+  const countryIso = useCountryIso()
+
+  const dispatch = useDispatch()
+  const tables = useSectionTables(assessmentType, sectionName)
+
+  useEffect(() => {
+    dispatch(
+      batchActions([
+        ...tables.map((table) =>
+          fetchTableData(
+            table[SectionSpec.KEYS_TABLE_DATA_REQUIRED.assessmentType],
+            table[SectionSpec.KEYS_TABLE_DATA_REQUIRED.sectionName],
+            table[SectionSpec.KEYS_TABLE_DATA_REQUIRED.tableName]
+          )
+        ),
+      ])
+    )
+  }, [countryIso])
+
+  return <AssessmentSection assessmentType={assessmentType} sectionName={sectionName} />
 }
 
 export default ContentCheck
