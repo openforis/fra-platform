19af02446ba2baee16dea11bc76116c585bc9453
Alexander <sasha_durham@hotmail.com>
2020-10-03 20:33:02 +0300

    Pan European table 4.5 metadata: 4.5a & 4.5b are merged

--- webapp/app/assessment/panEuropean/sections/table_4_5/sectionSpec.js
+++ webapp/app/assessment/panEuropean/sections/table_4_5/sectionSpec.js
@@ -2,7 +2,7 @@ import * as PanEuropean from '@common/assessment/panEuropean'
 
 import * as SectionSpec from '@webapp/app/assessment/components/section/sectionSpec'
 
-const section = PanEuropean.sections['4'].children['45a']
+const section = PanEuropean.sections['4'].children['45']
 
 const variables1 = [
   'forest',
@@ -20,7 +20,7 @@ const years1 = [...PanEuropean.years90_15].reverse();
 const years2 = [...PanEuropean.years15]
 
 const tableSpec = SectionSpec.newTableSpec({
-  [SectionSpec.KEYS_TABLE.name]: section.tables.table_4_5a,
+  [SectionSpec.KEYS_TABLE.name]: section.tables.table_4_5,
   [SectionSpec.KEYS_TABLE.columnsExport]: ['total'],
   [SectionSpec.KEYS_TABLE.columnsExport]: ['standing'],
   [SectionSpec.KEYS_TABLE.columnsExport]: ['lying'],
@@ -69,11 +69,14 @@ const tableSpec = SectionSpec.newTableSpec({
         })
       )
     ),
+    /*
+    // The original version
     SectionSpec.newRowData({
       [SectionSpec.KEYS_ROW.labelKey]: 'panEuropean.deadwood.volumeOfDeadwoodInFOWLBySpeciesGroups',
       [SectionSpec.KEYS_ROW.colSpan]: 4,
       [SectionSpec.KEYS_ROW.mainCategory]: true,
     }),
+    */
     ...variables2.flatMap((variable) =>
       years2.map((year) =>
         SectionSpec.newRowData({
