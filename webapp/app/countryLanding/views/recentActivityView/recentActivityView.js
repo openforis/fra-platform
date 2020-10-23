56b7b3fe7fe34762a08d778a1210607c8f0b450c
Miró Sorja <ping@mirosorja.com>
2020-10-23 10:25:42 +0300

    recentActivityView: use getAssessmentHomeLink

--- webapp/app/countryLanding/views/recentActivityView/recentActivityView.js
+++ webapp/app/countryLanding/views/recentActivityView/recentActivityView.js
@@ -6,6 +6,7 @@ import * as BasePaths from '@webapp/main/basePaths'
 
 import { useCountryIso, useI18n } from '@webapp/components/hooks'
 
+import * as FRA from '@common/assessment/fra'
 import * as LandingState from '@webapp/app/countryLanding/landingState'
 import { fetchAuditFeed } from '@webapp/app/components/audit/actions'
 
@@ -36,7 +37,7 @@ const RecentActivityView = () => {
           <img src="/img/tucan.svg" height="72" alt="tucan" />
           <p className="landing__activity-empty-title">{i18n.t('landing.recentActivity.noRecentActivityTitle')}</p>
           <p>{i18n.t('landing.recentActivity.noRecentActivityBody')}</p>
-          <Link className="btn-s btn-primary" to={BasePaths.getCountryHomeLink(countryIso)}>
+          <Link className="btn-s btn-primary" to={BasePaths.getAssessmentHomeLink(countryIso, FRA.type)}>
             {i18n.t('landing.recentActivity.getStarted')}
           </Link>
         </div>
