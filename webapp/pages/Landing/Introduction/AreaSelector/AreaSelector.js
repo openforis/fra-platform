ad56bec32f034a69815d1dfcd0547e76229abe4a
Miró Sorja <ping@mirosorja.com>
2020-10-26 13:02:54 +0200

    AreaSelector: remove support for paneuropean

--- webapp/pages/Landing/Introduction/AreaSelector/AreaSelector.js
+++ webapp/pages/Landing/Introduction/AreaSelector/AreaSelector.js
@@ -1,11 +1,9 @@
 import './areaSelector.less'
 import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 import { Area, Country } from '@common/country'
 import * as Fra from '@common/assessment/fra'
-import * as PanEuropean from '@common/assessment/panEuropean'
 import * as BasePaths from '@webapp/main/basePaths'
 
 import { useCountries, useRegions } from '@webapp/store/app'
@@ -19,8 +17,7 @@ const areas = {
   regions: 'regions',
 }
 
-const AreaSelector = (props) => {
-  const { assessmentType } = props
+const AreaSelector = () => {
   const i18n = useI18n()
   const regions = useRegions()
   const countries = useCountries()
@@ -28,61 +25,40 @@ const AreaSelector = (props) => {
   const [dropdownOpened, setDropdownOpened] = useState('')
   const [countryISOs, setCountryISOs] = useState([])
 
-  const fra = assessmentType === Fra.type
-  const globalCode = fra ? Area.levels.global : Area.levels.europe
-
-  // on mount fetch countries
   useEffect(() => {
-    const countriesFiltered = fra
-      ? countries
-      : countries.filter((country) => Country.getRegionCodes(country).includes(Area.levels.europe))
-    setCountryISOs(countriesFiltered.map(Country.getCountryIso))
+    setCountryISOs(countries.map(Country.getCountryIso))
   }, [])
 
   return (
     <div className="home-area-selector">
       <img alt="" src="/img/iconGlobal.svg" />
-      <Link
-        className="home-link m-r"
-        to={BasePaths.getAssessmentHomeLink(globalCode, assessmentType)}
-        target={fra ? '_self' : '_blank'}
-      >
-        {i18n.t(`area.${globalCode}.listName`)}
+      <Link className="home-link m-r" to={BasePaths.getAssessmentHomeLink(Area.levels.global, Fra.type)}>
+        {i18n.t(`area.${Area.levels.global}.listName`)}
       </Link>
 
-      {fra && (
-        <>
-          <img alt="" src="/img/iconRegions.svg" />
-          <div>{i18n.t('common.regions')}</div>
-          <DropdownAreas
-            area={areas.regions}
-            areaISOs={regions}
-            assessmentType={assessmentType}
-            dropdownOpened={dropdownOpened}
-            setDropdownOpened={setDropdownOpened}
-          />
-        </>
-      )}
+      <>
+        <img alt="" src="/img/iconRegions.svg" />
+        <div>{i18n.t('common.regions')}</div>
+        <DropdownAreas
+          area={areas.regions}
+          areaISOs={regions}
+          assessmentType={Fra.type}
+          dropdownOpened={dropdownOpened}
+          setDropdownOpened={setDropdownOpened}
+        />
+      </>
 
       <img alt="" src="/img/iconCountries.svg" />
       <div>{i18n.t('common.countries')}</div>
       <DropdownAreas
         area={areas.countries}
         areaISOs={countryISOs}
-        assessmentType={assessmentType}
+        assessmentType={Fra.type}
         dropdownOpened={dropdownOpened}
         setDropdownOpened={setDropdownOpened}
       />
     </div>
   )
-}
-
-AreaSelector.propTypes = {
-  assessmentType: PropTypes.oneOf([Fra.type, PanEuropean.type]),
-}
-
-AreaSelector.defaultProps = {
-  assessmentType: Fra.type,
 }
 
 export default AreaSelector
