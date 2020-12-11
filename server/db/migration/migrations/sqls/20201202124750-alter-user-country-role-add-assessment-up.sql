f85ca0d53fdc46a8f2c041c1b01919de14681b3f
Miró Sorja <ping@mirosorja.com>
2020-12-11 10:51:37 +0200

    migration/alter-country-role: add default fra2020

--- server/db/migration/migrations/sqls/20201202124750-alter-user-country-role-add-assessment-up.sql
+++ server/db/migration/migrations/sqls/20201202124750-alter-user-country-role-add-assessment-up.sql
@@ -1,5 +1,5 @@
 
-alter table user_country_role add assessment assessment_type;
+alter table user_country_role add assessment assessment_type default 'fra2020';
 
 UPDATE user_country_role
 SET assessment = 'fra2020';
