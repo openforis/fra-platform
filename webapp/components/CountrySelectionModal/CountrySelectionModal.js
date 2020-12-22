14f1583e2dd119a79b667080ee7d37c0effa6aff
Miró Sorja <ping@mirosorja.com>
2020-12-22 07:12:51 +0200

    CountrySelectionModal: initial commit for new version of component

---
+++ webapp/components/CountrySelectionModal/CountrySelectionModal.js
@@ -0,0 +1,109 @@
+import React, { useRef, useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { Modal, ModalClose, ModalFooter, ModalHeader } from '@webapp/components/modal'
+import { useI18n, useOnUpdate } from '@webapp/components/hooks'
+import { Country } from '@common/country'
+
+import CountrySelectionModalBody from './CountrySelectionModalBody'
+
+const CountrySelectionModal = (props) => {
+  const { countries, showCount, canSave, isOpen, onChange, onClose, unselectableCountries, headerLabel } = props
+  const [selection, setSelection] = useState([])
+  const [countriesFiltered, setCountriesFiltered] = useState(countries)
+  const inputRef = useRef(null)
+
+  const i18n = useI18n()
+
+  const resetAll = () => {
+    setSelection([])
+  }
+
+  const normalizeString = (str) => str.trim().toLowerCase().replace(/\s/g, '')
+
+  const checkMatch = (country, value) => {
+    const countryLabel = i18n.t(`area.${Country.getCountryIso(country)}.listName`)
+    const searchString = normalizeString(`${countryLabel}`)
+    return searchString.includes(value)
+  }
+
+  const updateCountries = () => {
+    if (!inputRef.current) return
+    const value = normalizeString(inputRef.current.value)
+    if (value === '') {
+      setCountriesFiltered(countries)
+    } else {
+      setCountriesFiltered(
+        countries.filter((country) => {
+          return checkMatch(country, value)
+        })
+      )
+    }
+  }
+
+  useOnUpdate(updateCountries, [countries])
+
+  const _onChange = (countryIso) => {
+    if (selection.includes(countryIso)) {
+      const filteredSelection = selection.filter((_countryIso) => _countryIso !== countryIso)
+      setSelection(filteredSelection)
+      onChange(countryIso, filteredSelection)
+    } else {
+      setSelection([...selection, countryIso])
+      onChange(countryIso, [...selection, countryIso])
+    }
+  }
+
+  const _onClose = () => {
+    onClose(selection)
+  }
+
+  return (
+    <Modal className="country-selection" isOpen={isOpen}>
+      <ModalHeader>
+        {headerLabel} {showCount && `(${selection.length})`}
+        <input ref={inputRef} type="text" placeholder={i18n.t('emoji.picker.search')} onChange={updateCountries} />
+        <ModalClose onClose={_onClose} />
+      </ModalHeader>
+
+      <CountrySelectionModalBody
+        countries={countriesFiltered}
+        onChange={_onChange}
+        selection={selection}
+        unselectableCountries={unselectableCountries}
+      />
+
+      <ModalFooter>
+        <button className="btn btn-primary" disabled={!canSave(selection)} onClick={_onClose}>
+          {i18n.t('common.done')}
+        </button>
+        <button className="btn btn-secondary" onClick={resetAll}>
+          {i18n.t('common.resetAll')}
+        </button>
+      </ModalFooter>
+    </Modal>
+  )
+}
+
+CountrySelectionModal.defaultProps = {
+  unselectableCountries: [],
+  canSave: () => true,
+  onChange: () => true,
+  showCount: true,
+}
+
+CountrySelectionModal.propTypes = {
+  headerLabel: PropTypes.string.isRequired,
+
+  countries: PropTypes.array.isRequired,
+  unselectableCountries: PropTypes.array,
+
+  isOpen: PropTypes.bool.isRequired,
+  showCount: PropTypes.bool,
+
+  canSave: PropTypes.func,
+  onChange: PropTypes.func,
+  onClose: PropTypes.func.isRequired,
+}
+
+export default CountrySelectionModal
---
+++ webapp/components/CountrySelectionModal/CountrySelectionModalBody.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useI18n } from '@webapp/components/hooks'
+import { ModalBody } from '@webapp/components/modal'
+
+const CountrySelectionModalBody = (props) => {
+  const { countries, onChange, selection = [], unselectableCountries } = props
+
+  const i18n = useI18n()
+
+  // Sort given countries (from props) to hashmap: {regionCode}: [{countryIso},..]
+  const regionCountries = {}
+
+  countries.forEach((country) => {
+    const { countryIso, regionCodes } = country
+    regionCodes.forEach((regionCode) => {
+      if (!Array.isArray(regionCountries[regionCode])) {
+        regionCountries[regionCode] = []
+      }
+      regionCountries[regionCode].push(countryIso)
+    })
+  })
+
+  const _isCountryUnselectable = (countryIso) => unselectableCountries.includes(countryIso)
+
+  const _onClick = (countryIso) => {
+    if (!_isCountryUnselectable(countryIso)) {
+      onChange(countryIso)
+    }
+  }
+
+  return (
+    <ModalBody>
+      <div className="edit-user__form-field-country-selection">
+        {Object.entries(regionCountries).map(([regionCode, countryIsos]) => (
+          <div key={regionCode} className="edit-user__form-field-region-container">
+            <div className="edit-user__form-field-region-label">{i18n.t(`area.${regionCode}.listName`)}</div>
+
+            {countryIsos.map((countryIso) => {
+              let classChecked = 'fra-checkbox'
+              if (selection.includes(countryIso)) classChecked += ' checked'
+
+              let classCountry = 'edit-user__form-field-country-selector'
+              if (_isCountryUnselectable(countryIso)) classCountry += ' disabled'
+
+              return (
+                <div
+                  key={countryIso}
+                  className={classCountry}
+                  onClick={() => _onClick(countryIso)}
+                >
+                  <div className={classChecked} />
+                  <div className="edit-user__form-field-country-label">{i18n.t(`area.${countryIso}.listName`)}</div>
+                </div>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    </ModalBody>
+  )
+}
+
+CountrySelectionModalBody.propTypes = {
+  countries: PropTypes.array.isRequired,
+  selection: PropTypes.array.isRequired,
+  unselectableCountries: PropTypes.array.isRequired,
+
+  onChange: PropTypes.func.isRequired,
+}
+
+export default CountrySelectionModalBody
