8322e1293c6260de575ea687e3bd2c049c579e05
Miró Sorja <ping@mirosorja.com>
2021-01-13 10:49:32 +0200

    migrations: add region ECOMIFAC

---
+++ server/db/migration/migrations/sqls/20210113084041-add-region-ECOMIFAC-up.sql
@@ -0,0 +1,14 @@
+INSERT INTO region
+VALUES ('ECOMIFAC', 'espace comifac');
+
+INSERT INTO country_region
+VALUES ('ECOMIFAC', 'BDI'),
+       ('ECOMIFAC', 'CMR'),
+       ('ECOMIFAC', 'COD'),
+       ('ECOMIFAC', 'COG'),
+       ('ECOMIFAC', 'GAB'),
+       ('ECOMIFAC', 'GNQ'),
+       ('ECOMIFAC', 'TCD'),
+       ('ECOMIFAC', 'RWA'),
+       ('ECOMIFAC', 'CAF'),
+       ('ECOMIFAC', 'STP');
