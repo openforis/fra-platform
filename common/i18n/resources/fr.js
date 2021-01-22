37998b5d731bc0710c74e1377c79fada616bb2cd
Miró Sorja <ping@mirosorja.com>
2021-01-22 12:13:27 +0200

    i18n/{en,es,fr}: common.{apply,done,resetAll} and disclaimer.statisticalFactsheets

--- common/i18n/resources/fr.js
+++ common/i18n/resources/fr.js
@@ -12,6 +12,8 @@ module.exports.translation = {
   },
 
   common: {
+    apply: 'Appliquer',
+    resetAll: 'Effacer tout',
     column: 'Colonne',
     countries: 'Pays',
     dataExport: 'Exportation de données',
@@ -51,6 +53,8 @@ des domaines boréal, tempéré et sous-tropical`,
   statisticalFactsheets,
 
   disclaimer: {
+    statisticalFactsheets:
+      'Avertissement: peut contenir des estimations de la FAO, peut contenir des valeurs manquantes',
     part1: `Ce site a été élaboré avec l’aide de l'`,
     europeanUnion: 'Union européenne',
     part2: ',  du ',
