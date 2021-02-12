2aceb7d50aa22a9ad039478a1bd79a4ee513318b
sorja <ping@mirosorja.com>
2021-02-12 10:30:36 +0200

    counterFilter: convert to ts, add redux types

--- webapp/store/ui/home/actions.js
+++
@@ -1,8 +0,0 @@
-export const uiHomeUpdateSelectedCountries = 'ui/home/update/selectedCountries'
-
-export const updateSelectedCountries = (countries = []) => (dispatch) => {
-  dispatch({
-    type: uiHomeUpdateSelectedCountries,
-    countries,
-  })
-}
