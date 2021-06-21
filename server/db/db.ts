4f29bea3e4597e9ea9d72cf400f14c00487e51be
sorja <ping@mirosorja.com>
2021-06-21 15:40:38 +0300

    Add server/db with pg-promise

---
+++ server/db/db.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config'
+
+import pgPromise from 'pg-promise'
+import { ProcessEnv } from '@server/utils'
+
+const logger = {
+  debug: console.debug,
+}
+
+const debugOptions = {
+  query: (e: pgPromise.IEventContext) => {
+    logger.debug(`QUERY: ${e.query}`)
+    if (e.params) {
+      logger.debug(`PARAMS: ${JSON.stringify(e.params)}`)
+    }
+  },
+}
+
+const initOptions = {
+  ...(ProcessEnv.debug ? debugOptions : {}),
+}
+
+const pgp = pgPromise(initOptions)
+
+// Timestamp will automatically be converted to UTC time-zone - No need to convert in select queries anymore
+// 1114 is OID for timestamp in Postgres
+pgp.pg.types.setTypeParser(1114, (str: string) => new Date(`${str} GMT`))
+
+const configCommon = {
+  // How long a client is allowed to remain idle before being closed
+  idleTimeoutMillis: 30000,
+  // Max number of clients in the pool
+  max: 30,
+  // Whether to use ssl connections
+  ssl: ProcessEnv.pgSsl,
+}
+
+const config = ProcessEnv.dbUrl
+  ? {
+      connectionString: ProcessEnv.dbUrl,
+      ...configCommon,
+    }
+  : {
+      user: ProcessEnv.pgUser,
+      database: ProcessEnv.pgDatabase,
+      password: ProcessEnv.pgPassword,
+      host: ProcessEnv.pgHost,
+      port: ProcessEnv.pgPort,
+      ...configCommon,
+    }
+
+export type BaseProtocol<T = any> = pgPromise.IBaseProtocol<T>
+
+export const DB = pgp(config)
