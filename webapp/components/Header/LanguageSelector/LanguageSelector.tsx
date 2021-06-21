49ec0fd17aa770768bcdfb8fa2afd228a6dd51f5
Mino Togna <minotogna@users.noreply.github.com>
2021-06-21 11:41:46 +0200

    Add header mobile layout (#809)
    * add header mobile style
    
    * fix app title mobile
    
    * move LanguageCodes under core

---
+++ webapp/components/Header/LanguageSelector/LanguageSelector.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+import { LanguageCodes } from '@core/lang'
+import { AppActions } from '@webapp/store/app'
+
+import { useI18n } from '@webapp/components/hooks'
+import PopoverControl from '@webapp/components/PopoverControl'
+import Icon from '@webapp/components/icon'
+
+const LanguageSelector: React.FC = () => {
+  const dispatch = useDispatch()
+  const i18n = useI18n()
+
+  const languageSelectionItems = LanguageCodes.map((lang) => ({
+    content: i18n.t(`language.${lang}`),
+    onClick: () => dispatch(AppActions.switchLanguage(lang)),
+  }))
+
+  return (
+    <PopoverControl items={languageSelectionItems}>
+      <div className="app-header__menu-item">
+        {i18n.t(`language.${i18n.language}`)}
+        <Icon className="icon-middle" name="small-down" />
+      </div>
+    </PopoverControl>
+  )
+}
+export default LanguageSelector
