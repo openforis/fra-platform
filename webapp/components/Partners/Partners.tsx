67aaf494e475f21c2b2fd76375a0ede5b507326f
minotogna <mino.togna@gmail.com>
2021-06-21 18:21:55 +0200

    add home Partners mobile layout

--- webapp/components/Partners/Partners.tsx
+++ webapp/components/Partners/Partners.tsx
@@ -1,14 +1,15 @@
-import './partners.less'
+import './partners.scss'
 import React from 'react'
+
 import { useI18n } from '@webapp/components/hooks'
 
-const Partners = () => {
+const Partners: React.FC = () => {
   const i18n = useI18n()
   return (
     <div className="partners">
       <div className="partners__imgs">
-        <img alt="" src="/img/partners/UNECE.gif" />
-        <img alt="" src={`/img/fao/FAO${i18n.language}_blue.svg`} />
+        <img alt="" className="partners__col1Mobile" src="/img/partners/UNECE.gif" />
+        <img alt="" className="partners__col1Mobile" src={`/img/fao/FAO${i18n.language}_blue.svg`} />
         <img alt="" src="/img/partners/ForestEurope.png" />
         <img alt="" src="/img/partners/COMIFAC.png" />
         <img alt="" src="/img/partners/ITTO.gif" />
@@ -16,36 +17,38 @@ const Partners = () => {
       </div>
 
       <div className="partners__imgs">
-        <div className="partners__support">{(i18n as any).t('landing.overview.withFinancialSupportOf')}</div>
+        <div className="partners__support partners__col1Mobile">
+          {i18n.t('landing.overview.withFinancialSupportOf')}
+        </div>
         <img alt="" src="/img/partners/EU.jpg" />
         <img alt="" src="/img/partners/NICFI.png" />
-        <img alt="" src="/img/partners/mfafi.gif" className="mfafi" />
-        <img alt="" src="/img/partners/mmmfi.png" />
-        <img alt="" src="/img/partners/GEF.png" />
+        <img alt="" src="/img/partners/mfafi.gif" className="mfafi partners__col1Mobile" />
+        <img alt="" className="partners__col1Mobile" src="/img/partners/mmmfi.png" />
+        <img alt="" className="partners__col1Mobile" src="/img/partners/GEF.png" />
       </div>
 
       <div className="partners__disclaimer">
-        {(i18n as any).t('disclaimer.part1')}
+        {i18n.t('disclaimer.part1')}
         <a href="https://ec.europa.eu/info/index_en" target="_blank" rel="noreferrer">
-          {(i18n as any).t('disclaimer.europeanUnion')}
+          {i18n.t('disclaimer.europeanUnion')}
         </a>
-        {(i18n as any).t('disclaimer.part2')}
+        {i18n.t('disclaimer.part2')}
         <a href="https://um.fi/frontpage" target="_blank" rel="noreferrer">
-          {(i18n as any).t('disclaimer.govFinland')}
+          {i18n.t('disclaimer.govFinland')}
         </a>
-        {(i18n as any).t('disclaimer.part3')}
+        {i18n.t('disclaimer.part3')}
         <a
           href="https://norad.no/en/front/thematic-areas/climate-change-and-environment/norways-international-climate-and-forest-initiative-nicfi/"
           target="_blank"
           rel="noreferrer"
         >
-          {(i18n as any).t('disclaimer.govNorway')}
+          {i18n.t('disclaimer.govNorway')}
         </a>
-        {(i18n as any).t('disclaimer.part4')}
+        {i18n.t('disclaimer.part4')}
         <a href="https://www.thegef.org" target="_blank" rel="noreferrer">
-          {(i18n as any).t('disclaimer.gef')}
+          {i18n.t('disclaimer.gef')}
         </a>
-        {(i18n as any).t('disclaimer.part5')}
+        {i18n.t('disclaimer.part5')}
       </div>
     </div>
   )
