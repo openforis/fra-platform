839001bba1f520d9804a7da6f4636200b2992d6b
sorja <ping@mirosorja.com>
2021-06-22 12:45:50 +0300

    service/odp: import AuditRepository

--- server/service/odp/create.ts
+++ server/service/odp/create.ts
@@ -2,13 +2,13 @@ import { CountryIso } from '@core/country'
 import { OdpRepository } from '@server/repository/odp'
 import { User } from '@core/auth'
 import { BaseProtocol, DB } from '@server/db/db'
-import { insertAudit } from '@server/repository/audit/auditRepository'
+import * as AuditRepository from '@server/repository/audit/auditRepository'
 
 export const create = (options: { countryIso: CountryIso; user: User }, client: BaseProtocol = DB) => {
   const { countryIso, user } = options
   return client.tx(async (t) => {
     const odpId = OdpRepository.create({ countryIso }, t)
-    await insertAudit(t, user.id, 'createOdp', countryIso, 'odp', { odpId })
+    await AuditRepository.insertAudit(t, user.id, 'createOdp', countryIso, 'odp', { odpId })
     return odpId
   })
 }
