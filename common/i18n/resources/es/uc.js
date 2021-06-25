e4d619f7ab89e54e24641ae487b2b578e102856e
minotogna <mino.togna@gmail.com>
2021-06-25 08:43:56 +0200

    update UC layout

--- common/i18n/resources/es/uc.js
+++ common/i18n/resources/es/uc.js
@@ -4,8 +4,8 @@ module.exports = {
   message3:
     'We would very much appreciate if you please can help us improve our services by providing your feedback by taking our user survey. The survey should not take more than 5-10 minutes to complete. Your views are very important to us.',
   message4: 'Thank you!',
-  message5: '/The FRA team.',
+  message5: 'The FRA team.',
   noThanks: 'No thanks',
-  yesPlease: 'Yes please',
+  yesPlease: 'OK',
   doNotShow: 'Do not show again',
 }
--- webapp/components/UserConsultationSurvey/UserConsultationSurvey.tsx
+++ webapp/components/UserConsultationSurvey/UserConsultationSurvey.tsx
@@ -5,16 +5,19 @@ import { useI18n } from '@webapp/components/hooks'
 
 import ButtonCheckBox from '@webapp/components/buttonCheckBox'
 
+const localStorageKey = 'uc/doNotShow'
+
 const UserConsultationSurvey: React.FC = () => {
   const i18n = useI18n()
-  // TODO: get default value of  showDialog from local storage
-  const [showDialog, setShowDialog] = useState<boolean>(true)
+
+  const [showDialog, setShowDialog] = useState<boolean>(localStorage.getItem(localStorageKey) !== 'true')
   const [doNotShow, setDoNotShow] = useState<boolean>(false)
 
   const setDoNotShowHandler = () => {
     setDoNotShow((prevState) => {
-      // TODO: set in local storage do not show uc survey again
-      return !prevState
+      const doNotShowUpdate = !prevState
+      localStorage.setItem(localStorageKey, String(doNotShowUpdate))
+      return doNotShowUpdate
     })
   }
 
@@ -23,18 +26,29 @@ const UserConsultationSurvey: React.FC = () => {
   return (
     <div className="uc-survey">
       <div className="uc-survey-content">
-        <div>
-          <div>{i18n.t('uc.message1')}</div>
-          <div>{i18n.t('uc.message2')}</div>
-          <div>{i18n.t('uc.message3')}</div>
-          <div>{i18n.t('uc.message4')}</div>
-          <div>{i18n.t('uc.message5')}</div>
+        <div className="uc-survey__message">
+          <img alt="" src="/img/tucan.svg" />
+          <div>
+            <div>{i18n.t('uc.message1')}</div>
+            <div>{i18n.t('uc.message2')}</div>
+            <div>{i18n.t('uc.message3')}</div>
+            <br />
+            <div>{i18n.t('uc.message4')}</div>
+            <div>{i18n.t('uc.message5')}</div>
+          </div>
         </div>
         <div className="uc-survey__btns">
           <button className="btn btn-secondary" type="button" onClick={() => setShowDialog(false)}>
             {i18n.t('uc.noThanks')}
           </button>
-          <button className="btn btn-primary" type="button">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => {
+              window.open('https://www.w3schools.com', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes')
+              setShowDialog(false)
+            }}
+          >
             {i18n.t('uc.yesPlease')}
           </button>
         </div>
