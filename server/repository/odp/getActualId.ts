bdf5266c7f2f85eb20cbaed3ec2b25d3edaabab4
Miró Sorja <ping@mirosorja.com>
2021-08-06 14:49:24 +0300

    #2: ODP: Delete Draft (#831)
    * initial commit for persist odp draft
    
    * Major refactor: odpDraft -> odpVersion
    
    * OdpRepository: readActualId
    
    * service/odp: update deleteDraft
    
    * fix issue with updateDraftId
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ server/repository/odp/getActualId.ts
@@ -0,0 +1,7 @@
+import { BaseProtocol, DB } from '@server/db'
+
+export const readActualId = async (options: { odpId: any }, client: BaseProtocol = DB) => {
+  const { odpId } = options
+  const [{ actual_id: actualId }] = await client.query('SELECT actual_id FROM odp WHERE id = $1', [odpId])
+  return actualId
+}
--- server/repository/odp/updateDraftId.ts
+++ server/repository/odp/updateDraftId.ts
@@ -1,6 +1,11 @@
 import { BaseProtocol, DB } from '@server/db'
 
-export const updateDraftId = async (options: { odpId: number }, client: BaseProtocol = DB) => {
-  const { odpId } = options
-  return client.query('UPDATE odp SET draft_id = (SELECT last_value FROM odp_version_id_seq) WHERE id = $1', [odpId])
+export const updateDraftId = async (options: { odpId: number; draftId?: number }, client: BaseProtocol = DB) => {
+  const { odpId, draftId } = options
+  return client.query(
+    `UPDATE odp SET draft_id = ${
+      !Number.isNaN(+draftId) ? draftId : '(SELECT last_value FROM odp_version_id_seq)'
+    } WHERE id = $1`,
+    [odpId]
+  )
 }
