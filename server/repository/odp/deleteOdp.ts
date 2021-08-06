fce02b8c92df77f2c426e3b13f49bda25e40b00f
sorja <ping@mirosorja.com>
2021-08-06 15:05:50 +0300

    missing bracket

--- server/repository/odp/deleteOdp.ts
+++ server/repository/odp/deleteOdp.ts
@@ -3,3 +3,4 @@ import { BaseProtocol, DB } from '@server/db'
 export const deleteOdp = async (options: { odpId: number }, client: BaseProtocol = DB) => {
   const { odpId } = options
   return client.query('DELETE FROM odp WHERE id = $1 RETURNING *', [odpId])
+}
