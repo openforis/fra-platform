88b262372dd26766090bb0b6a36c650ea1e934e0
sorja <ping@mirosorja.com>
2021-08-09 08:33:43 +0300

    api/odp: delete: cast user as User)

--- server/api/odp/delete.ts
+++ server/api/odp/delete.ts
@@ -4,6 +4,7 @@ import { allowedToEditDataCheck } from '@server/assessment/assessmentEditAccessC
 import { Requests } from '@server/utils'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { OdpService } from '@server/service'
+import { User } from '@core/auth'
 
 export const OdpDelete = {
   init: (express: Express): void => {
@@ -14,9 +15,7 @@ export const OdpDelete = {
         try {
           const { countryIso } = req.query
           await allowedToEditDataCheck(countryIso, req.user, 'extentOfForest')
-          // @ts-ignore
-          // TODO: Add user type to express
-          await OdpService.deleteOdp({ odpId: Number(req.query.odpId), user: req.user })
+          await OdpService.deleteOdp({ odpId: Number(req.query.odpId), user: req.user as User })
 
           Requests.sendOk(res)
         } catch (err) {
