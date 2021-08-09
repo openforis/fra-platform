f11f78756aaa4ee83d24f9b1e35812bf1e2eedb0
sorja <ping@mirosorja.com>
2021-08-09 11:33:00 +0300

    repository/listOriginalDataPoints: Use pg-promise

--- server/repository/odp/listOriginalDataPoints.ts
+++ server/repository/odp/listOriginalDataPoints.ts
@@ -1,10 +1,16 @@
-import * as db from '@server/db/db_deprecated'
-
+import { BaseProtocol, DB } from '@server/db'
+import { ODP } from '@core/odp'
+import { CountryIso } from '@core/country'
 import { getOdp } from './getOdp'
 
-export const listOriginalDataPoints = async (countryIso: any, schemaName = 'public') => {
+export const listOriginalDataPoints = async (
+  options: { countryIso: CountryIso; schemaName: string },
+  client: BaseProtocol = DB
+) => {
+  const { countryIso, schemaName = 'public' } = options
+
   const tableName = `${schemaName}.odp`
-  const res = await db.pool.query(
+  const res = await client.query(
     `
     SELECT p.id as odp_id
     FROM ${tableName} p
@@ -12,6 +18,7 @@ export const listOriginalDataPoints = async (countryIso: any, schemaName = 'publ
   `,
     [countryIso]
   )
+
   const odps = await Promise.all(res.rows.map((r: any) => getOdp(r.odp_id, schemaName)))
-  return [...odps].sort((odp1, odp2) => odp1.year - odp2.year)
+  return [...odps].sort((odp1: ODP, odp2: ODP) => +odp1.year - +odp2.year)
 }
