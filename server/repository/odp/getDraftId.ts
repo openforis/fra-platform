9bf128dcbf02de92291f45d034d4eb909122045e
sorja <ping@mirosorja.com>
2021-08-12 13:24:17 +0300

    repository/getDraftId: update function signature

--- server/repository/odp/getDraftId.ts
+++ server/repository/odp/getDraftId.ts
@@ -1,4 +1,10 @@
-export const getDraftId = async (client: any, odpId: any): Promise<number> => {
+import { BaseProtocol, DB } from '@server/db'
+
+export const getDraftId = async (
+  options: { odpId: string | number },
+  client: BaseProtocol = DB
+): Promise<null | string> => {
+  const { odpId } = options
   const [{ draft_id: draftId }] = await client.query('SELECT draft_id FROM odp WHERE id = $1', [odpId])
   return draftId
 }
