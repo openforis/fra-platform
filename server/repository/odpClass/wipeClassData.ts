6d105ec89f4a4c6924b7834461b11382683da5b5
sorja <ping@mirosorja.com>
2021-08-12 13:11:16 +0300

    repository/wipeClassData: minor type update for odpVersionId

--- server/repository/odpClass/wipeClassData.ts
+++ server/repository/odpClass/wipeClassData.ts
@@ -1,6 +1,9 @@
 import { BaseProtocol, DB } from '@server/db'
 
-export const wipeClassData = async (options: { odpVersionId: number }, client: BaseProtocol = DB): Promise<void> => {
+export const wipeClassData = async (
+  options: { odpVersionId: number | string },
+  client: BaseProtocol = DB
+): Promise<void> => {
   const { odpVersionId } = options
   return client.query('DELETE FROM odp_class WHERE odp_version_id = $1', [odpVersionId])
 }
