7467a7b2667728df8d9829d1ceb632e8f3f84611
Mino Togna <minotogna@users.noreply.github.com>
2021-08-26 15:32:02 +0200

    always show User consultation survey popup (#886)

--- webapp/components/UserConsultationSurvey/UserConsultationSurvey.tsx
+++ webapp/components/UserConsultationSurvey/UserConsultationSurvey.tsx
@@ -3,12 +3,13 @@ import React, { useState } from 'react'
 
 import { useI18n } from '@webapp/components/hooks'
 
-const localStorageKey = 'uc/doNotShow'
+// const localStorageKey = 'uc/doNotShow'
 
 const UserConsultationSurvey: React.FC = () => {
   const i18n = useI18n()
 
-  const [showDialog, setShowDialog] = useState<boolean>(localStorage.getItem(localStorageKey) !== 'true')
+  // const [showDialog, setShowDialog] = useState<boolean>(localStorage.getItem(localStorageKey) !== 'true')
+  const [showDialog, setShowDialog] = useState<boolean>(true)
 
   if (!showDialog) return null
 
@@ -27,9 +28,6 @@ const UserConsultationSurvey: React.FC = () => {
           </div>
         </div>
         <div className="uc-survey__btns">
-          <button className="btn btn-secondary" type="button" onClick={() => setShowDialog(false)}>
-            {i18n.t('uc.noThanks')}
-          </button>
           <button
             className="btn btn-primary"
             type="button"
@@ -40,10 +38,16 @@ const UserConsultationSurvey: React.FC = () => {
                 'toolbar=yes,scrollbars=yes,resizable=yes'
               )
               setShowDialog(false)
-              localStorage.setItem(localStorageKey, 'true')
+              // localStorage.setItem(localStorageKey, 'true')
             }}
           >
             {i18n.t('uc.yesPlease')}
+          </button>
+          <button className="btn btn-secondary" type="button" onClick={() => setShowDialog(false)}>
+            {i18n.t('uc.noThanks')}
+          </button>
+          <button className="btn btn-secondary" type="button" onClick={() => setShowDialog(false)}>
+            {i18n.t('uc.alreadyAnswered')}
           </button>
         </div>
       </div>
