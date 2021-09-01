46f467aa50e86b049d3d4589900cb496ab9a5a74
Mino Togna <minotogna@users.noreply.github.com>
2021-09-01 08:39:08 +0200

    Data Export mobile (#897)
    * update DataExport form mobile layout
    
    * improve DataExport header form mobile layout
    
    * fix ResultsTable scroll
    
    * fix deepscan

--- webapp/components/Assessment/DataExport/VariableSelect/VariableSelect.tsx
+++ webapp/components/Assessment/DataExport/VariableSelect/VariableSelect.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import MediaQuery from 'react-responsive'
 
 import { FRA, PanEuropean } from '@core/assessment'
 import { SectionSpecs } from '@webapp/sectionSpec'
@@ -7,6 +8,7 @@ import { useAssessmentType } from '@webapp/store/app'
 import { DataExportAction, DataExportSelection, useDataExportSelection } from '@webapp/store/page/dataExport'
 import { useI18n, useParamSection } from '@webapp/components/hooks'
 import { getVariableLabelKey } from '@webapp/components/Assessment/DataExport/utils'
+import { Breakpoints } from '@webapp/utils/breakpoints'
 
 import ButtonCheckBox from '@webapp/components/buttonCheckBox'
 
@@ -25,35 +27,61 @@ const VariableSelect: React.FC = () => {
   const tableSpec = SectionSpecs.getTableSpecExport(assessmentType, assessmentSection)
   const variables = tableSpec.rows.filter((row) => !!row.variableExport)
 
+  const toggleVariable = (variableExport: string): void => {
+    const selected = variableExport === selection.variable
+    const selectionUpdate: DataExportSelection = {
+      ...selection,
+      variable: selected ? '' : variableExport,
+    }
+    dispatch(DataExportAction.updateSelection({ assessmentSection, selection: selectionUpdate }))
+  }
+
   return (
     <div className="export__form-section">
       <div className="export__form-section-header">
         <h4>{i18n.t(Heading[assessmentType])}</h4>
       </div>
 
-      <div className="divider" />
+      <MediaQuery maxWidth={Breakpoints.laptop - 1}>
+        <select onChange={(event) => toggleVariable(event.target.value)} value={selection.variable}>
+          <option value="">-</option>
+          {variables.map((variable) => {
+            const { cols, variableExport } = variable
+            const { labelKey, labelParams, labelPrefixKey } = cols[0]
+            const label = getVariableLabelKey(labelKey)
 
-      <div className="export__form-section-variables">
-        {variables.map((variable) => {
-          const { cols, variableExport } = variable
-          const { labelKey, labelParams, labelPrefixKey } = cols[0]
-          const label = getVariableLabelKey(labelKey)
-          const selected = variableExport === selection.variable
+            return (
+              <option key={variableExport} value={variableExport}>
+                {`${labelPrefixKey ? i18n.t(labelPrefixKey) : ''}${i18n.t(label, labelParams)}`}
+              </option>
+            )
+          })}
+        </select>
+      </MediaQuery>
 
-          return (
-            <ButtonCheckBox
-              key={variableExport}
-              checked={selected}
-              label={[labelPrefixKey, label]}
-              labelParam={labelParams}
-              onClick={() => {
-                const selectionUpdate: DataExportSelection = { ...selection, variable: selected ? '' : variableExport }
-                dispatch(DataExportAction.updateSelection({ assessmentSection, selection: selectionUpdate }))
-              }}
-            />
-          )
-        })}
-      </div>
+      <MediaQuery minWidth={Breakpoints.laptop}>
+        <>
+          <div className="divider" />
+          <div className="export__form-section-variables">
+            {variables.map((variable) => {
+              const { cols, variableExport } = variable
+              const { labelKey, labelParams, labelPrefixKey } = cols[0]
+              const label = getVariableLabelKey(labelKey)
+              const selected = variableExport === selection.variable
+
+              return (
+                <ButtonCheckBox
+                  key={variableExport}
+                  checked={selected}
+                  label={[labelPrefixKey, label]}
+                  labelParam={labelParams}
+                  onClick={() => toggleVariable(variableExport)}
+                />
+              )
+            })}
+          </div>
+        </>
+      </MediaQuery>
     </div>
   )
 }
