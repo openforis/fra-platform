ef9722171790aaa4af4afcfb684a632f6fcf6066
sorja <ping@mirosorja.com>
2021-09-15 13:45:59 +0300

    rootReducer: update App

--- webapp/store/rootReducer.ts
+++ webapp/store/rootReducer.ts
@@ -1,17 +1,16 @@
 import applicationError from '@webapp/components/error/reducer' // Reducers available before login
 import { UserReducer, UserState } from '@webapp/store/user'
-import app from '@webapp/store/app/reducer'
 import login from '@webapp/pages/Login/reducer'
 import country from '@webapp/app/country/reducer'
 import { pageReducer } from '@webapp/store/page'
 
-import * as AppState from '@webapp/store/app/state'
 import * as ApplicationErrorState from '@webapp/components/error/applicationErrorState'
 import * as CountryState from '@webapp/app/country/countryState'
+import { AppReducer } from './app'
 
 export default {
   [ApplicationErrorState.stateKey]: applicationError,
-  [AppState.stateKey]: app,
+  app: AppReducer,
   login,
   [UserState.stateKey]: UserReducer,
   [CountryState.stateKey]: country,
