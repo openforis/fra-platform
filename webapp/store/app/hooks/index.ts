f0e6a203c5126dc09294707b73bfccfcfbe9c5ce
sorja <ping@mirosorja.com>
2021-09-22 13:45:50 +0300

    app/hooks: update on i18n.language

--- webapp/store/app/hooks/index.ts
+++ webapp/store/app/hooks/index.ts
@@ -12,12 +12,13 @@ export const useAssessmentType = (): AssessmentType | null =>
 
 export const useCountries = (): Array<Country> => {
   const i18n = useI18n()
+
   const dispatch = useAppDispatch()
   const { countries = [] } = useAppSelector((state) => state.app)
 
   useEffect(() => {
     dispatch(AppActions.updateCountries(Areas.sortCountries(countries, i18n)))
-  }, [i18n])
+  }, [i18n.language])
 
   return countries
 }
@@ -32,7 +33,7 @@ export const useRegions = (): Array<Region> => {
 
   useEffect(() => {
     dispatch(AppActions.updateRegions(Areas.sortRegions(regions, i18n)))
-  }, [i18n])
+  }, [i18n.language])
 
   return regions
 }
