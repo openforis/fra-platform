ed4d4ecf352a7368521e9fd297a5354ede922538
sorja <ping@mirosorja.com>
2021-10-04 15:17:51 +0300

    RootState: add dataexport

--- webapp/store/RootState.ts
+++ webapp/store/RootState.ts
@@ -1,8 +1,12 @@
+import { DataExportState } from '@webapp/store/page/dataExport'
 import { AppDispatch } from './store'
 import { AppState } from './app/AppStateType'
 import { CountryState } from './country/CountryStateType'
 
 export type RootState = {
+  page: {
+    dataExport: DataExportState
+  }
   dispatch: AppDispatch
 
   app: AppState
