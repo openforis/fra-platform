a471d41cc58ddcada2266aca2dcc7b3b7c987693
minotogna <mino.togna@gmail.com>
2021-10-18 09:56:50 +0200

    update read odps for table 1a/1b

--- server/api/country/getOverviewStatus.ts
+++ server/api/country/getOverviewStatus.ts
@@ -1,6 +1,5 @@
 import { Express, Response, Request } from 'express'
 import { Requests } from '@server/utils'
-import { OdpService } from '@server/service'
 import * as reviewRepository from '@server/repository/review/reviewRepository'
 import { isCollaborator, roleForCountry } from '@common/countryRole'
 import * as R from 'ramda'
@@ -11,7 +10,7 @@ import {
 import * as assessmentRepository from '@server/repository/assessment/assessmentRepository'
 import { fetchCollaboratorCountryAccessTables } from '@server/repository/collaborators/collaboratorsRepository'
 import { ApiEndPoint } from '@common/api/endpoint'
-import { CountryIso } from '@core/country'
+import { OriginalDataPointService } from '@server/service/originalDataPoint'
 
 // TODO - REFACTOR
 
@@ -25,7 +24,7 @@ export const CountryGetOverviewStatus = {
         const userInfo = req.user
         const assessmentsPromise = assessmentRepository.getAssessments(countryIso)
         if (userInfo) {
-          const odpDataPromise = OdpService.listAndValidateOriginalDataPoints({ countryIso: countryIso as CountryIso })
+          const odpDataPromise = OriginalDataPointService.getMany({ countryIso, validate: true })
           const reviewStatusPromise = reviewRepository.getCountryIssuesSummary(countryIso, userInfo)
           const [odps, reviewStatus, assessmentsDB] = await Promise.all([
             odpDataPromise,
