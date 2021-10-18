c6391f5a69c60911bbd3135d59e86c2eeb2b93a2
sorja <ping@mirosorja.com>
2021-10-18 14:11:39 +0300

    repository/originalDataPoint: extract id

--- server/repository/originalDataPoint/create.ts
+++ server/repository/originalDataPoint/create.ts
@@ -3,7 +3,7 @@ import { BaseProtocol, DB } from '@server/db'
 export const create = async (props: { countryIso: string }, client: BaseProtocol = DB): Promise<string> => {
   const { countryIso } = props
 
-  const id = await client.one(
+  const { id } = await client.one(
     `
         insert into original_data_point (country_iso) values ($1) returning id;
     `,
