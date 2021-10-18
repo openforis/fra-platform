63e5b8f60a1e9389c9b1e7776733de138ea890ed
Mino Togna <minotogna@users.noreply.github.com>
2021-10-18 09:59:47 +0200

    update read odps for table 1a/1b (#956)

---
+++ server/repository/originalDataPoint/index.ts
@@ -0,0 +1,6 @@
+import { getMany, getManyNormalized } from '@server/repository/originalDataPoint/getMany'
+
+export const OriginalDataPointRepository = {
+  getMany,
+  getManyNormalized,
+}
---
+++ server/service/originalDataPoint/index.ts
@@ -0,0 +1,5 @@
+import { getMany } from '@server/service/originalDataPoint/getMany'
+
+export const OriginalDataPointService = {
+  getMany,
+}
