6829167f3bfbc7435d707f164b260160f1cf1574
Miró Sorja <ping@mirosorja.com>
2021-10-19 16:05:15 +0300

    initial commit for odp/remove (#958)
    * initial commit for odp/remove
    
    * repository/originalDataPoint/remove: fix return type, add camelize

--- server/api/originalDataPoint/index.ts
+++ server/api/originalDataPoint/index.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { OdpCreate } from '@server/api/originalDataPoint/create'
+import { OdpRemove } from '@server/api/originalDataPoint/remove'
 
 export const OriginalDataPointApi = {
   init: (express: Express): void => {
     OdpCreate.init(express)
+    OdpRemove.init(express)
   },
 }
--- server/service/originalDataPoint/index.ts
+++ server/service/originalDataPoint/index.ts
@@ -1,7 +1,9 @@
 import { create } from '@server/service/originalDataPoint/create'
 import { getMany } from '@server/service/originalDataPoint/getMany'
+import { remove } from '@server/service/originalDataPoint/remove'
 
 export const OriginalDataPointService = {
   create,
   getMany,
+  remove,
 }
