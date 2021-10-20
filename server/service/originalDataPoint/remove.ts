1938e51d6aa92f40e8966c3ed06c286338930260
Miró Sorja <ping@mirosorja.com>
2021-10-20 10:27:08 +0300

    server/odp: minor fixes (#961)

--- server/service/originalDataPoint/remove.ts
+++ server/service/originalDataPoint/remove.ts
@@ -1,6 +1,7 @@
 import { OriginalDataPointRepository } from '@server/repository/originalDataPoint'
+import { ODP } from '@core/odp'
 
-export const remove = async (props: { id: string }): Promise<string> => {
+export const remove = async (props: { id: string }): Promise<ODP> => {
   const { id } = props
   const odp = await OriginalDataPointRepository.remove({ id })
   return odp
