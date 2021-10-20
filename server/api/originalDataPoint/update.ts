ff1eb8687db21517c4cca75ceb663ba3f1bceed7
sorja <ping@mirosorja.com>
2021-10-20 10:59:22 +0300

    Add auth

--- server/api/originalDataPoint/update.ts
+++ server/api/originalDataPoint/update.ts
@@ -2,13 +2,13 @@ import { Express, Response, Request } from 'express'
 import { Requests } from '@server/utils'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { OriginalDataPointService } from '@server/service/originalDataPoint'
-// import { ApiAuthMiddleware } from '@server/api/middleware'
+import { ApiAuthMiddleware } from '@server/api/middleware'
 
 export const OdpUpdate = {
   init: (express: Express): void => {
     express.patch(
       ApiEndPoint.OriginalDataPoint.one(),
-      // ApiAuthMiddleware.requireCountryEditPermission,
+      ApiAuthMiddleware.requireCountryEditPermission,
       async (req: Request, res: Response) => {
         try {
           const { id } = req.params
