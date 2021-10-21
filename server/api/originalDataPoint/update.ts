985fa122833de242b0dcf5f3513c176ea61eda0e
Miró Sorja <ping@mirosorja.com>
2021-10-21 11:27:40 +0300

    Add audit log for dp (#964)
    * Add audit log for create odp
    
    * Add audit log for update odp
    
    * Add audit log for remove odp
    
    * odp/auditLog: rename target to odp

--- server/api/originalDataPoint/update.ts
+++ server/api/originalDataPoint/update.ts
@@ -3,6 +3,7 @@ import { Requests } from '@server/utils'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { OriginalDataPointService } from '@server/service/originalDataPoint'
 import { ApiAuthMiddleware } from '@server/api/middleware'
+import { User } from '@core/auth'
 
 export const OdpUpdate = {
   init: (express: Express): void => {
@@ -13,7 +14,7 @@ export const OdpUpdate = {
         try {
           const { id } = req.params
           const { odp } = req.body
-          const updatedOdp = await OriginalDataPointService.update({ id, odp })
+          const updatedOdp = await OriginalDataPointService.update({ id, odp, user: req.user as User })
           res.json({ odp: updatedOdp })
         } catch (err) {
           Requests.sendErr(res, err)
