ee7eeb9546922617017d6e107ba70abff48beea1
Miró Sorja <ping@mirosorja.com>
2021-10-26 12:00:10 +0300

    Chartjs/fix chart issues (#974)
    * chartjs: fix tooltip issue
    
    * fix issue with thousand spacing, use space

--- webapp/components/Chart/utils/getChartOptions.ts
+++ webapp/components/Chart/utils/getChartOptions.ts
@@ -1,10 +1,11 @@
 import * as NumberUtils from '@common/bignumberUtils'
-import { ChartData, ChartOptions /* ChartTooltipItem */ } from 'chart.js'
+import { ChartOptions /* ChartTooltipItem */ } from 'chart.js'
 import { ChartTypeRegistry } from 'chart.js/auto'
 import { ChartType } from './ChartType'
 
 // chart.js has no export for ChartTooltipItem
 interface ChartTooltipItem {
+  dataIndex: number
   label?: string | undefined
   value?: string | undefined
   xLabel?: string | number | undefined
@@ -13,43 +14,48 @@ interface ChartTooltipItem {
   index?: number | undefined
   x?: number | undefined
   y?: number | undefined
+  dataset: {
+    label?: string
+    unit?: string
+    data: Array<{ datasets: { data: number[] | string[]; unit: string }[] }>
+  }
 }
 
 const commonOptions = {
   maintainAspectRatio: false,
   responsive: true,
   responsiveAnimationDuration: 200,
-  tooltips: {
-    backgroundColor: 'rgba(45, 45, 45, 0.95)',
-    caretSize: 4,
-    cornerRadius: 3,
-    position: 'average',
-    xPadding: 12,
-    yPadding: 18,
-    titleAlign: 'center',
-    titleFontFamily: `'Open Sans', sans-serif`,
-    titleFontSize: 14,
-    titleFontStyle: '600',
-    titleMarginBottom: 12,
-    bodyFontFamily: `'Open Sans', sans-serif`,
-    bodyFontSize: 13,
-    bodySpacing: 6,
-    callbacks: {
-      label: (tooltipItem: ChartTooltipItem, data: ChartData) => {
-        const { datasetIndex, index } = tooltipItem
-        const { datasets, labels } = data
-        const dataset = datasets[datasetIndex]
-        return dataset.label || labels[index]
-      },
-      afterLabel: (
-        tooltipItem: ChartTooltipItem,
-        data: { datasets: { data: number[] | string[]; unit: string }[] }
-      ) => {
-        const { datasetIndex, index } = tooltipItem
-        const { datasets } = data
-        const dataset = datasets[datasetIndex]
-        const value = dataset.data[index]
-        return `${NumberUtils.formatNumber(value)} (${dataset.unit})`
+  plugins: {
+    tooltip: {
+      backgroundColor: 'rgba(45, 45, 45, 0.95)',
+      caretSize: 4,
+      cornerRadius: 3,
+      position: 'average',
+      xPadding: 12,
+      yPadding: 18,
+      titleAlign: 'center',
+      titleFontFamily: `'Open Sans', sans-serif`,
+      titleFontSize: 14,
+      titleFontStyle: '600',
+      titleMarginBottom: 12,
+      bodyFontFamily: `'Open Sans', sans-serif`,
+      bodyFontSize: 13,
+      bodySpacing: 6,
+      callbacks: {
+        label: (tooltipItem: ChartTooltipItem) => {
+          const { dataset } = tooltipItem
+          return dataset.label || tooltipItem.label
+        },
+        afterLabel: (tooltipItem: ChartTooltipItem) => {
+          const {
+            dataset,
+            dataset: { data },
+          } = tooltipItem
+          const { dataIndex } = tooltipItem
+          const value = data[dataIndex]
+
+          return `${NumberUtils.formatNumber(value)} (${dataset.unit})`
+        },
       },
     },
   },
