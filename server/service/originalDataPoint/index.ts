47f346b89cb9af2aaff9a1aae7cdec43d5842a3b
Miró Sorja <ping@mirosorja.com>
2021-10-28 16:20:01 +0300

    odp reserved years (#983)
    * initial commit for odp reserved years api
    
    * server/odp/reservedYears: update return type
    
    * ui/odp/YearSelection: use api
    
    * remove todo
    
    * Update apiEndPoint for odp years

--- server/service/originalDataPoint/index.ts
+++ server/service/originalDataPoint/index.ts
@@ -3,10 +3,12 @@ import { get } from '@server/service/originalDataPoint/get'
 import { getMany, getManyNormalized } from '@server/service/originalDataPoint/getMany'
 import { remove } from '@server/service/originalDataPoint/remove'
 import { update } from '@server/service/originalDataPoint/update'
+import { getReservedYears } from '@server/service/originalDataPoint/getReservedYears'
 
 export const OriginalDataPointService = {
   create,
   get,
+  getReservedYears,
   getMany,
   getManyNormalized,
   remove,
