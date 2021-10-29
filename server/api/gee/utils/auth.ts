fbb8994ada21ead5afd27875869f239511518c56
xbexbex <petja.valkama@gmail.com>
2021-10-29 17:57:56 +0300

    Add auth function

---
+++ server/api/gee/utils/auth.ts
@@ -0,0 +1,26 @@
+import { Response, Request } from 'express'
+import { Requests } from '@server/utils'
+// @ts-ignore
+import ee from '@google/earthengine'
+
+const privateKey = JSON.parse(process.env.FRA_GOOGLE_SERVICE_ACCOUNT_KEY)
+
+// The authenticating structure to run all GEE computations through
+export const run = async (req: Request, res: Response, cloudFn: (req: Request) => any) => {
+  try {
+    ee.data.authenticateViaPrivateKey(
+      privateKey,
+      async () => {
+        ee.initialize(null, null, async () => {
+          const data = await cloudFn(req)
+          res.json(data)
+        })
+      },
+      (error: string) => {
+        throw error
+      }
+    )
+  } catch (err) {
+    Requests.sendErr(res, err)
+  }
+}
