063c70dda613c2a907a2fc1b2b26906c9668674f
Miró Sorja <ping@mirosorja.com>
2021-11-02 09:47:16 +0200

    initial commit for previous odp api (#989)

--- server/api/originalDataPoint/index.ts
+++ server/api/originalDataPoint/index.ts
@@ -4,12 +4,14 @@ import { OdpRemove } from '@server/api/originalDataPoint/remove'
 import { OdpGet } from '@server/api/originalDataPoint/get'
 import { OdpUpdate } from '@server/api/originalDataPoint/update'
 import { OdpGetReservedYears } from '@server/api/originalDataPoint/getReservedYears'
+import { OdpGetPrevious } from '@server/api/originalDataPoint/getPrevious'
 
 export const OriginalDataPointApi = {
   init: (express: Express): void => {
     OdpCreate.init(express)
     OdpGet.init(express)
     OdpGetReservedYears.init(express)
+    OdpGetPrevious.init(express)
     OdpRemove.init(express)
     OdpUpdate.init(express)
   },
