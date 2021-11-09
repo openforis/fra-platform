bc3963fbbcfc5815e326198a6dd8ef5a39e8b85d
sorja <ping@mirosorja.com>
2021-11-09 08:36:24 +0200

    update migration: add unique

--- server/db/migration/migrations/sqls/20211108111343-assessment-schema-update-up.sql
+++ server/db/migration/migrations/sqls/20211108111343-assessment-schema-update-up.sql
@@ -6,5 +6,6 @@ create table assessment
     id    bigserial NOT NULL,
     uuid  uuid  default uuid_generate_v4(),
     props jsonb default '{}'::jsonb,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    unique(uuid)
 );
