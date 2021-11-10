0ea6e13f0a59dd4ddaa3f53540288de3e12f8812
sorja <ping@mirosorja.com>
2021-11-10 13:39:49 +0200

    repository/assessment: add createAssessment

---
+++ server/repository/assessment/createAssessment.ts
@@ -0,0 +1,19 @@
+import { Assessment } from '@core/meta/assessment'
+import { BaseProtocol, DB } from '@server/db'
+import { Objects } from '@core/utils'
+
+export const createAssessment = async (
+  params: {
+    assessment: Assessment
+  },
+  client: BaseProtocol = DB
+): Promise<Assessment> => {
+  const { assessment } = params
+
+  const query = `
+  insert into assessment (props) values ('${JSON.stringify(assessment.props)}'::jsonb) returning  *;
+
+`
+  const ret = await client.query<Assessment>(query)
+  return Objects.camelize(ret)
+}
--- server/repository/assessment/index.ts
+++ server/repository/assessment/index.ts
@@ -1,5 +1,7 @@
 import { createAssessmentSchema } from './createAssessmentSchema'
+import { createAssessment } from './createAssessment'
 
 export const AssessmentRepository = {
+  createAssessment,
   createAssessmentSchema,
 }
