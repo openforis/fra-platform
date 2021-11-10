5a38788aec1bba45d88c3aecb4f8561ab51d2d83
sorja <ping@mirosorja.com>
2021-11-10 13:38:18 +0200

    schemas: useassessment.props.name

--- server/db/schemas.ts
+++ server/db/schemas.ts
@@ -1,7 +1,7 @@
 import { Assessment } from '@core/meta/assessment'
 
 const getName = (assessment: Assessment): string => {
-  return `assessment_${assessment.id}`
+  return `assessment_${assessment.props.name}`
 }
 
 export const Schemas = {
