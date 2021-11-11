d2c210becd4d49fb5546b30992b71e073387a4a8
sorja <ping@mirosorja.com>
2021-11-11 12:17:54 +0200

    service/createAssessment: return assessment only

--- server/service/assessment/createAssessment.ts
+++ server/service/assessment/createAssessment.ts
@@ -2,22 +2,19 @@ import { BaseProtocol, DB } from '@server/db'
 import { AssessmentRepository } from '@server/repository/assessment'
 import { Assessment } from '@core/meta/assessment'
 import { ActivityLogRepository } from '@server/repository/activityLog'
-import { ActivityLog, ActivityLogMessage } from '@core/meta/activityLog'
+import { ActivityLogMessage } from '@core/meta/activityLog'
 import { User } from '@core/auth'
 
 export const createAssessment = async (
   props: { user: User; assessment: Assessment },
   client: BaseProtocol = DB
-): Promise<{
-  schemaName: string
-  activityLogEntry: ActivityLog<any>
-}> => {
+): Promise<Assessment> => {
   const { assessment, user } = props
   const schemaName = await AssessmentRepository.createAssessmentSchema({ assessment }, client)
 
   return client.tx(async (t) => {
     const createdAssessment = await AssessmentRepository.createAssessment({ assessment }, t)
-    const activityLogEntry = await ActivityLogRepository.insertActivityLog(
+    await ActivityLogRepository.insertActivityLog(
       {
         activityLog: {
           target: assessment,
@@ -29,6 +26,6 @@ export const createAssessment = async (
       },
       t
     )
-    return { schemaName, activityLogEntry, assessment: createdAssessment }
+    return createdAssessment
   })
 }
