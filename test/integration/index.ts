258a262a644e24c096a2ad9ab16d929a163cfc4a
Mino Togna <minotogna@users.noreply.github.com>
2021-11-18 12:31:07 +0100

    Area tabnles (#1018)
    * add assessment area tables
    
    * add assessment area tables

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,3 +1,5 @@
+import { DB } from '@server/db'
+
 import userCreate from './user/userCreate.test'
 import userInactive from './user/userSetInactive.test'
 import userActive from './user/userSetActive.test'
@@ -5,9 +7,15 @@ import assessmentCreate from './assessment/createAssessment'
 import assessmentRemove from './assessment/removeAssessment'
 import userRemove from './user/userRemove.test'
 
-userCreate()
-userInactive()
-userActive()
-assessmentCreate()
-assessmentRemove()
-userRemove()
+afterAll(async () => {
+  await DB.$pool.end()
+})
+
+describe('Metadata integration test', () => {
+  userCreate()
+  userInactive()
+  userActive()
+  assessmentCreate()
+  assessmentRemove()
+  userRemove()
+})
