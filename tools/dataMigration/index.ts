e20fdf2767427375daa555a062f80380a8786c68
Mino Togna <minotogna@users.noreply.github.com>
2021-11-26 12:54:38 +0100

    add users migration (#1032)

--- tools/dataMigration/index.ts
+++ tools/dataMigration/index.ts
@@ -2,47 +2,32 @@ import * as path from 'path'
 import { config } from 'dotenv'
 
 import { Assessment } from '../../core/meta/assessment'
-import { Section } from '../../core/meta/section'
-import { TableSection } from '../../core/meta/tableSection'
-import { Table } from '../../core/meta/table'
-import { Row } from '../../core/meta/row'
-import { Col } from '../../core/meta/col'
-import { Cycle } from '../../core/meta/cycle'
 import { SectionSpec } from '../../webapp/sectionSpec'
 
 import { FraSpecs } from './fraSpecs'
-import { getSection } from './getSection'
-import { getTableSection } from './getTableSection'
-import { getTable } from './getTable'
-import { getRow } from './getRow'
-import { getCol } from './getCol'
 import { DB } from '../../server/db'
 import { getCreateSchemaDDL } from '../../server/repository/assessment/getCreateSchemaDDL'
-import { Objects } from '../../core/utils/objects'
+import { migrateUsers } from './migrateUsers'
+import { migrateMetadata } from './migrateMetadata'
+import { migrateAreas } from './migrateAreas'
 
 config({ path: path.resolve(__dirname, '..', '..', '.env') })
 
 export const migrate = async (spec: Record<string, SectionSpec>): Promise<void> => {
-  // const assessment = await DB.one<Assessment>(
-  //   `
-  //   insert into assessment (props)
-  //   values ($1::jsonb)
-  //   returning *;
-  //   `,
-  //   [JSON.stringify({ name: 'fra' })]
-  // )
-  // console.log(assessment)
-  // console.log(FraSpecs)
-
   // delete old fra
   await DB.query(`drop schema if exists assessment_fra cascade`)
-  await DB.query(`delete from assessment where props->>'name' = $1`, ['fra'])
+  await DB.query(
+    `delete
+                  from assessment
+                  where props ->> 'name' = $1`,
+    ['fra']
+  )
 
   // insert assessment
   const assessment = await DB.one<Assessment>(
     `insert into assessment (props)
-            values ($1::jsonb)
-            returning *;`,
+     values ($1::jsonb)
+     returning *;`,
     [JSON.stringify({ name: 'fra' })]
   )
 
@@ -50,103 +35,10 @@ export const migrate = async (spec: Record<string, SectionSpec>): Promise<void> 
   const schema = `assessment_${assessment.props.name}`
   await DB.query(getCreateSchemaDDL(schema))
 
-  // insert cycle
-  const cycle: Cycle = await DB.one<Cycle>(`insert into ${schema}.cycle (name) values($1) returning *`, ['2020'])
-  const cycles: Array<string> = [cycle.uuid]
-
-  const sections: Array<Section> = []
-  const tableSections: Array<TableSection> = []
-  const tables: Array<Table> = []
-  const rows: Array<Row> = []
-  const cols: Array<Col> = []
-
-  await DB.tx(async (t) => {
-    await Promise.all(
-      Object.values(spec).map(async (spec, index) => {
-        let section = getSection({ assessmentId: assessment.id, spec, cycles, index })
-        section = await t.one<Section>(
-          `insert into assessment_fra.section (assessment_id,props)
-                    values ($1, $2::jsonb)
-                    returning *`,
-          [section.assessmentId, JSON.stringify(section.props)],
-          Objects.camelize
-        )
-        sections.push(section)
-
-        await Promise.all(
-          spec.tableSections.map(async (tableSectionSpec) => {
-            let tableSection = getTableSection({ cycles, tableSectionSpec, section })
-            tableSection = await t.one<TableSection>(
-              `insert into assessment_fra.table_section (section_id,props) values ($1, $2::jsonb) returning *;`,
-              [tableSection.sectionId, JSON.stringify(tableSection.props)],
-              Objects.camelize
-            )
-            tableSections.push(tableSection)
-
-            await Promise.all(
-              tableSectionSpec.tableSpecs.map(async (tableSpec) => {
-                let table = getTable({ cycles, tableSpec, tableSection })
-                table = await t.one<Table>(
-                  `insert into ${schema}."table" (table_section_id, props) values ($1, $2::jsonb) returning *;`,
-                  [table.tableSectionId, JSON.stringify(table.props)],
-                  Objects.camelize
-                )
-                tables.push(table)
-
-                await Promise.all(
-                  tableSpec.rows.map(async (rowSpec) => {
-                    let row = getRow({ cycles, rowSpec, table })
-                    row = await t.one<Row>(
-                      `insert into ${schema}.row (table_id, props) values ($1, $2::jsonb) returning *;`,
-                      [row.tableId, JSON.stringify(row.props)],
-                      Objects.camelize
-                    )
-                    rows.push(row)
-
-                    await Promise.all(
-                      rowSpec.cols.map(async (colSpec) => {
-                        let col = getCol({ cycles, colSpec, row })
-                        col = await t.one<Col>(
-                          `insert into ${schema}.col (row_id, props) values ($1, $2::jsonb) returning *;`,
-                          [col.rowId, JSON.stringify(col.props)],
-                          Objects.camelize
-                        )
-                        cols.push(col)
-                      })
-                    )
-                  })
-                )
-              })
-            )
-          })
-        )
-      })
-    )
-
-    await t.query(`
-        insert into ${schema}.country (country_iso)
-        select country_iso
-        from country
-        order by country_iso;
-    `)
-    await t.query(`
-        insert into ${schema}.region_group (name, "order")
-        select name, "order"
-        from _legacy.region_group
-        order by "order";
-    `)
-    await t.query(`
-        insert into ${schema}.region (region_code, region_group_id)
-        select r.region_code,
-               rg.id as region_group_id
-        from region r
-                 left join _legacy.region r_l
-                           on r.region_code = r_l.region_code
-                 left join _legacy.region_group rg_l on r_l.region_group = rg_l.id
-                 left join ${schema}.region_group rg on rg_l.name = rg.name
-        where r.region_code != 'FE'
-        order by r.region_code;
-    `)
+  await DB.tx(async (client) => {
+    await migrateMetadata({ assessment, schema, spec, client })
+    await migrateAreas({ client, schema })
+    await migrateUsers({ client })
   })
 }
 
