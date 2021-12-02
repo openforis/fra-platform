bc6456e091566783289cf575c68c93a02331d6de
sorja <ping@mirosorja.com>
2021-12-02 14:06:23 +0200

    fix deepscan issue

--- client/pages/PageRoutes/useTheme.ts
+++ client/pages/PageRoutes/useTheme.ts
@@ -1,24 +1,24 @@
 // import { useEffect } from 'react'
 
-import { FRA, PanEuropean } from '@core/assessment'
+// import { FRA, PanEuropean } from '@core/assessment'
 // import { useIsAssessment } from '../../../webapp/hooks'
 // import { useAssessmentType } from '../../../webapp/store/app'
 
-const defaultTheme = {
-  '--ui-accent-light': '#c4e7eb',
-  '--ui-bg': '#f5f5f5',
-  '--ui-bg-hover': '#ebebeb',
-}
-
-const themes = {
-  [FRA.type]: defaultTheme,
-  [PanEuropean.type]: {
-    '--ui-accent-light': '#F9E6D6',
-    '--ui-bg': '#FFF7F3',
-    '--ui-bg-hover': '#FFE9DF',
-  },
-  null: defaultTheme,
-}
+// const defaultTheme = {
+//   '--ui-accent-light': '#c4e7eb',
+//   '--ui-bg': '#f5f5f5',
+//   '--ui-bg-hover': '#ebebeb',
+// }
+//
+// const themes = {
+//   [FRA.type]: defaultTheme,
+//   [PanEuropean.type]: {
+//     '--ui-accent-light': '#F9E6D6',
+//     '--ui-bg': '#FFF7F3',
+//     '--ui-bg-hover': '#FFE9DF',
+//   },
+//   null: defaultTheme,
+// }
 
 export const useTheme = () => {
   // const assessmentType = useAssessmentType()
