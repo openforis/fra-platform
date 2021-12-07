6db16106e6bfdf2cf03480e5dbbafcc1cd20d30c
sorja <ping@mirosorja.com>
2021-12-07 09:20:33 +0200

    update i18n imports

--- client/i18n.ts
+++ client/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import { createParams } from '../i18n/i18nFactory'
+import { createParams } from '@i18n/i18nFactory'
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
