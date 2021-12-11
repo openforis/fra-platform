9775585c47ba7538940a4bb6cc06496a841e0bd0
sorja <ping@mirosorja.com>
2021-12-11 14:20:20 +0200

    support language change

--- client/components/Header/LanguageSelector/LanguageSelector.tsx
+++ client/components/Header/LanguageSelector/LanguageSelector.tsx
@@ -7,12 +7,11 @@ import Icon from '@client/components/Icon'
 import { useTranslation } from 'react-i18next'
 
 const LanguageSelector: React.FC = () => {
-  // const dispatch = useAppDispatch()
   const { i18n } = useTranslation()
 
   const languageSelectionItems = LanguageCodes.map((lang: Lang) => ({
     content: i18n.t(`language.${lang}`),
-    // onClick: () => dispatch(/* AppActions.switchLanguage(lang) */),
+    onClick: () => i18n.changeLanguage(lang),
   }))
 
   return (
