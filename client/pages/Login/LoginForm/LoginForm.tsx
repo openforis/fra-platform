65c4c812135579384df5eca8ee72804bf70b6e93
sorja <ping@mirosorja.com>
2022-01-04 13:08:44 +0200

    add local login validation

--- client/pages/Login/LoginForm/LoginForm.tsx
+++ client/pages/Login/LoginForm/LoginForm.tsx
@@ -8,6 +8,7 @@ import { BasePaths } from '@client/basePaths'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { useAppDispatch } from '@client/store'
 import { LoginActions } from '@client/store/login'
+import { LoginValidator } from '@client/pages/Login/utils/LoginValidator'
 
 const LoginForm: React.FC = () => {
   const { i18n } = useTranslation()
@@ -17,47 +18,60 @@ const LoginForm: React.FC = () => {
 
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [errors, setErrors] = useState<Record<string, string>>({})
 
   // TODO:
-  // Handle login
   // Handle invitation (=> get invited user details (email))
   const [password2, setPassword2] = useState<string>('')
 
   const onCancel = window.history.back
   const onLogin = () => {
-    dispatch(
-      LoginActions.localLogin({
-        email,
-        password,
-      })
-    )
+    const fieldErrors = LoginValidator.localValidate(email, password)
+    setErrors(fieldErrors)
+
+    if (!fieldErrors.password && !fieldErrors.password) {
+      dispatch(
+        LoginActions.localLogin({
+          email,
+          password,
+        })
+      )
+    }
   }
 
   return (
     <div className="login__form">
       <h3>{i18n.t('login.signInFRA')}</h3>
       <input
+        onFocus={() => setErrors({ ...errors, email: null })}
         value={email || ''}
         disabled={isInvitation}
         type="text"
         placeholder={i18n.t('login.email')}
         onChange={(event) => setEmail(event.target.value)}
       />
+      {errors.email && <span className="login__field-error">{i18n.t(errors.email)}</span>}
 
       <input
+        onFocus={() => setErrors({ ...errors, password: null })}
         value={password}
         type="password"
         placeholder={i18n.t('login.password')}
         onChange={(event) => setPassword(event.target.value)}
       />
+      {errors.password && <span className="login__field-error">{i18n.t(errors.password)}</span>}
 
       {invitation && (
-        <input
-          value={password2}
-          type="password"
-          placeholder={i18n.t('login.repeatPassword')}
-          onChange={(event) => setPassword2(event.target.value)}
-        />
+        <>
+          <input
+            onFocus={() => setErrors({ ...errors, password2: null })}
+            value={password2}
+            type="password"
+            placeholder={i18n.t('login.repeatPassword')}
+            onChange={(event) => setPassword2(event.target.value)}
+          />
+          {errors.password2 && <span className="login__field-error">{errors.password2}</span>}
+        </>
       )}
 
       <div>
