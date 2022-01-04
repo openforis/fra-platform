fdf638ac88ac8ce796474aca0f06f32972776ea5
Mino Togna <minotogna@users.noreply.github.com>
2022-01-04 11:49:58 +0100

    Migrate user role (#1090)
    * node table test insert 1
    
    * update user role/invitation migration

--- tools/dataMigration/dataTable/tableMappings.ts
+++ tools/dataMigration/dataTable/tableMappings.ts
@@ -20,7 +20,7 @@ export const getColumnName = (idx: any, columns: any) => R.path([idx, 'name'], c
 
 export const getColumnIndex = (name: any, columns: any) => R.findIndex((x: any) => x.name === name, columns)
 
-export const Mapping = (mapping: any) =>
+export const Mapping = (mapping: any): TableMapping =>
   R.merge(mapping, {
     getRowName: (idx: any) => getRowName(idx, mapping.rows.names),
     getRowIndex: (name: any) => getRowIndex(name, mapping.rows.names),
@@ -29,7 +29,7 @@ export const Mapping = (mapping: any) =>
     getColumnName: (idx: any) => getColumnName(idx, mapping.columns),
     getColumnIndex: (name: any) => getColumnIndex(name, mapping.columns),
     getFullColumnCount: () => mapping.columns.length,
-  })
+  }) as TableMapping
 
 export const assertSanity = (mappingObj: any) => {
   const errMsg = 'Malformed FRA table mapping'
@@ -40,7 +40,23 @@ export const assertSanity = (mappingObj: any) => {
   assert(mappingObj.columns.length > 0, errMsg)
 }
 
-export const getMapping = (tableSpecName: string) => {
+export type ColumnMapping = { name: string; type: string }
+export type TableMapping = {
+  tableName: string
+  rows: {
+    names: Array<string>
+  }
+  columns: Array<ColumnMapping>
+  getRowName: (idx: number) => string
+  getRowIndex: (name: string) => number
+  getFullRowCount: () => number
+  getColumn: (idx: number) => ColumnMapping
+  getColumnName: (idx: number) => string
+  getColumnIndex: (name: string) => number
+  getFullColumnCount: () => number
+}
+
+export const getMapping = (tableSpecName: string): TableMapping => {
   const mappingData = mappings[tableSpecName]
   if (!mappingData) throw new Error(`Could not find mapping for tableSpecName ${tableSpecName}`)
   const mappingObj = Mapping(mappingData)
