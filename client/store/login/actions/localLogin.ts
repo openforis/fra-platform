ae9dd29a1c676777eee2470636e023254f062c45
sorja <ping@mirosorja.com>
2022-01-04 10:34:58 +0200

    add types to action

--- client/store/login/actions/localLogin.ts
+++ client/store/login/actions/localLogin.ts
@@ -3,6 +3,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { ApiEndPoint } from '@common/api/endpoint'
 
-export const localLogin = createAsyncThunk('login/local', async (user: { email: string; password: string }) => {
+export const localLogin = createAsyncThunk<
+  void,
+  {
+    email: string
+    password: string
+  }
+>('login/local', async (user: { email: string; password: string }) => {
   await axios.post(ApiEndPoint.Auth.Login.local(), user)
 })
