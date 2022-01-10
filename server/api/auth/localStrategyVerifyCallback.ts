5268d43f3860d433d32b25dd2b5a3ce45c7d3c28
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-01-10 23:15:17 +0100

    invitation management for registered user, invitationUuid is now passed using querystring

--- server/api/auth/localStrategyVerifyCallback.ts
+++ server/api/auth/localStrategyVerifyCallback.ts
@@ -9,68 +9,34 @@ export const localStrategyVerifyCallback = async (req: Request, email: string, p
   const sendErr = (message: string) => done(null, false, { message })
 
   try {
-    // accepting invitation
-    // if (invitationUUID) {
-    //   const invitation = await userRepository.fetchInvitation(invitationUUID);
-    //   const password2 = req.body.password2 || "";
-    //   // validating invitation
-    //   if (!invitation) {
-    //     sendResp(null, "login.noInvitation");
-    //   } else {
-    //     const user = await userRepository.findUserByEmail(email);
-    //     if (user) {
-    //       // existing user
-    //       if (Objects.isEmpty(password.trim())) {
-    //         sendResp(null, "login.noEmptyPassword");
-    //       } else {
-    //         const validatedUser = await userRepository.findUserByEmailAndPassword(email, password);
-    //         if (validatedUser) {
-    //           const hash = await passwordHash(password);
-    //           const acceptedUser = await db.transaction(userRepository.acceptInvitationLocalUser, [
-    //             invitationUUID,
-    //             hash
-    //           ]);
-    //           sendResp(acceptedUser);
-    //         } else {
-    //           sendResp(null, "login.noMatchingUser");
-    //         }
-    //       }
-    //     } else {
-    //       // new user
-    //       if (Objects.isEmpty(password.trim()) || Objects.isEmpty(password2.trim())) {
-    //         sendResp(null, "Passwords cannot be empty");
-    //       } else if (password.trim() !== password2.trim()) {
-    //         sendResp(null, "Passwords don't match");
-    //       } else if (!validPassword(password)) {
-    //         sendResp(null, "login.passwordError");
-    //       } else {
-    //         const hash = await passwordHash(password);
-    //         const _user = await db.transaction(userRepository.acceptInvitationLocalUser, [invitationUUID, hash]);
-    //         sendResp(_user);
-    //       }
-    //     }
-    //   }
-    // login
     if (!validEmail({ email })) {
       sendErr('login.invalidEmail')
     } else if (Objects.isEmpty(password.trim())) {
       sendErr('login.noEmptyPassword')
     } else {
-      // const user = await userRepository.findUserByEmailAndPassword(email, password)
+      const invitationUuid = String(req.query.invitationUuid)
+      if (invitationUuid) {
+        const { user: invitedUser } = await UserController.readByInvitation({ invitationUuid })
+        if (invitedUser?.status !== 'active') {
+          // TODO set password
+        }
+      }
 
       let user = await UserRepository.read({ user: { email } })
       const userProvider = await UserProviderRepository.read({ user, provider: 'local' })
+      if (!userProvider) {
+        sendErr('login.noMatchingUser');
+      }
 
       const passwordMatch = await bcrypt.compare(password, userProvider.props.password)
 
       if (passwordMatch) {
-        const invitationUuid = String(req.query.state)
-
         if (invitationUuid) {
           const { user: invitedUser, userRole } = await UserController.readByInvitation({ invitationUuid })
+          if (!userRole) {
+            sendErr('login.noInvitation');
+          }
           user = await UserController.acceptInvitation({ user: invitedUser, userRole })
-        } else {
-          user = await UserController.read({ user: { email } })
         }
 
         done(null, user)
