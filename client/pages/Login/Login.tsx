c68aeda7f34b091fb1322c1ebd03b4dff9dc314d
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-01-11 14:35:15 +0100

    fixed deepscan issue

--- client/pages/Login/Login.tsx
+++ client/pages/Login/Login.tsx
@@ -1,17 +1,28 @@
 import './login.scss'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 
 import Partners from '@client/components/Partners'
 
 import { BasePaths } from '@client/basePaths'
 import { useUser } from '@client/store/user'
+import { Urls } from '@client/utils'
+
 import Invitation from './Invitation'
 import LoginForm from './LoginForm'
 import ResetPassword from './ResetPassword'
 
 const Login: React.FC = () => {
+  const history = useHistory()
   const user = useUser()
+  const invitationUuid = Urls.getRequestParam('invitationUuid')
+
+  useEffect(() => {
+    if (user && !invitationUuid) {
+      history.push(BasePaths.Root())
+    }
+  }, [user])
 
   return (
     <>
@@ -20,17 +31,13 @@ const Login: React.FC = () => {
           <Invitation />
         </Route>
 
-        {!user &&
-          <>
-            <Route exact path={BasePaths.Login.root()}>
-              <LoginForm />
-            </Route>
+        <Route exact path={BasePaths.Login.root()}>
+          <LoginForm />
+        </Route>
 
-            <Route exact path={BasePaths.Login.resetPassword()}>
-              <ResetPassword />
-            </Route>
-          </>
-        }
+        <Route exact path={BasePaths.Login.resetPassword()}>
+          <ResetPassword />
+        </Route>
 
         <img alt="" src="/img/tucan.svg" className="login__tucan" />
       </div>
