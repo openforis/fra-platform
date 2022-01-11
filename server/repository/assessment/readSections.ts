137f4cff56b9781a9343ccc6a99f16305cbcc9c0
sorja <ping@mirosorja.com>
2022-01-11 13:12:57 +0200

    readSections: update query

--- server/repository/assessment/readSections.ts
+++ server/repository/assessment/readSections.ts
@@ -1,7 +1,10 @@
 import { BaseProtocol, DB } from '@server/db'
 import { Objects } from '@core/utils'
 
-export const readSections = async (props: { assessmentName: string }, client: BaseProtocol = DB): Promise<any> =>
+export const readSections = async (
+  props: { assessmentName: string; assessmentCycleUuid: string },
+  client: BaseProtocol = DB
+): Promise<any> =>
   client
     .manyOrNone<any>(
       `
@@ -9,7 +12,7 @@ export const readSections = async (props: { assessmentName: string }, client: Ba
             select s.parent_id,
                    jsonb_agg(s.* order by (props ->> 'index')::numeric) as sub_sections
             from assessment_${props.assessmentName}.section s
-            where s.parent_id is not null
+            where s.parent_id is not null and props->'cycles' ? $1
             group by s.parent_id
             order by s.parent_id
         )
@@ -17,10 +20,10 @@ export const readSections = async (props: { assessmentName: string }, client: Ba
                ss.sub_sections
         from assessment_${props.assessmentName}.section s
                  left join ss on ss.parent_id = s.id
-        where s.parent_id is null
+        where s.parent_id is null and props->'cycles' ? $1 ? $1
         order by (s.props ->> 'index')::numeric
         ;
     `,
-      []
+      [props.assessmentCycleUuid]
     )
     .then(Objects.camelize)
