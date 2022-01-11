08de5b2e4c42e695faba559e7a489a87350cd7af
sorja <ping@mirosorja.com>
2022-01-11 13:01:48 +0200

    readSections: update query

--- server/repository/assessment/readSections.ts
+++ server/repository/assessment/readSections.ts
@@ -1,15 +1,26 @@
 import { BaseProtocol, DB } from '@server/db'
 import { Objects } from '@core/utils'
 
-export const readSections = async (
-  props: { assessmentName: string; assessmentCycleUuid: string },
-  client: BaseProtocol = DB
-): Promise<any> =>
+export const readSections = async (props: { assessmentName: string }, client: BaseProtocol = DB): Promise<any> =>
   client
     .manyOrNone<any>(
       `
-        select * from assessment_${props.assessmentName}.section where (props->'cycles')::jsonb ? $1;
+        with ss as (
+            select s.parent_id,
+                   jsonb_agg(s.* order by (props ->> 'index')::numeric) as sub_sections
+            from assessment_${props.assessmentName}.section s
+            where s.parent_id is not null
+            group by s.parent_id
+            order by s.parent_id
+        )
+        select s.*,
+               ss.sub_sections
+        from assessment_${props.assessmentName}.section s
+                 left join ss on ss.parent_id = s.id
+        where s.parent_id is null
+        order by (s.props ->> 'index')::numeric
+        ;
     `,
-      [props.assessmentCycleUuid]
+      []
     )
     .then(Objects.camelize)
