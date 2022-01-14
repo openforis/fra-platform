c5f270a0d8edcf6b9cad48848490a1937f0da264
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-01-14 10:06:49 +0100

    another small fix

--- client/pages/Login/LoginForm/LoginForm.tsx
+++ client/pages/Login/LoginForm/LoginForm.tsx
@@ -11,7 +11,7 @@ type Props = {
   invitationUuid?: string
 }
 
-const LoginForm: React.FC<Props> = (props) => {
+const LoginForm: React.FC<Props> = (props: Props) => {
   const { invitationUuid } = props
 
   const dispatch = useAppDispatch()
