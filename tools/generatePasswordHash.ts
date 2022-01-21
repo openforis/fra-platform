b5c7fef98cb069eb82341c3e97e5b027e775c809
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-01-21 10:25:29 +0100

    generate password hash works again

--- tools/generatePasswordHash.ts
+++ tools/generatePasswordHash.ts
@@ -1,4 +1,4 @@
-import { passwordHash } from '../server/api/auth/utils/passwordHash'
+import { passwordHash } from '../_legacy_server/api/auth/utils/passwordHash'
 
 if (process.argv[2]) passwordHash(process.argv[2]).then(console.log)
 else console.log('usage: ts-node generatePasswordHash.ts <password>\nEx: ts-node generatePasswordHash.ts 123')
