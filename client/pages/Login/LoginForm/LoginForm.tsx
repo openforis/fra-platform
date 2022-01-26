3284434331f016b25f73db1a00baf662de781719
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-01-26 13:39:56 +0100

    restored google login and other fixes (#1121)
    * restored google login
    
    * fixed google login when invitationUuid is not defined
    
    * fixed local login when invitationUuid is not defined
    
    * fixed email invitationUuid parameter
    
    * formatting issues, other small changes
    
    * added redirect if login successful
    
    * fixed wrong condition
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- client/pages/Login/LoginForm/LoginForm.tsx
+++ client/pages/Login/LoginForm/LoginForm.tsx
@@ -20,6 +20,7 @@ const LoginForm: React.FC<Props> = (props: Props) => {
 
   const { invitedUser } = useInvitation()
 
+  const [loginLocal, setLoginLocal] = useState(false)
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [password2, setPassword2] = useState<string>(undefined)
@@ -33,15 +34,11 @@ const LoginForm: React.FC<Props> = (props: Props) => {
     }
   }, [])
 
-  const onCancel = () => {
-    history.push(BasePaths.Root())
-  }
-
   const onLogin = () => {
     const fieldErrors = LoginValidator.localValidate(email, password, password2)
     setErrors(fieldErrors)
 
-    if (fieldErrors.isError) {
+    if (!fieldErrors.isError) {
       dispatch(
         LoginActions.localLogin({
           email,
@@ -49,68 +46,93 @@ const LoginForm: React.FC<Props> = (props: Props) => {
           invitationUuid,
         })
       )
+      history.push(BasePaths.Root())
     }
   }
 
+  if (loginLocal)
+    return (
+      <div className="login__form">
+        <input
+          onFocus={() => setErrors({ ...errors, email: null })}
+          value={email}
+          disabled={!!invitedUser}
+          type="text"
+          placeholder={i18n.t('login.email')}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        {errors.email && <span className="login__field-error">{i18n.t(errors.email)}</span>}
+
+        <input
+          onFocus={() => setErrors({ ...errors, password: null })}
+          value={password}
+          type="password"
+          placeholder={i18n.t('login.password')}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        {errors.password && <span className="login__field-error">{i18n.t(errors.password)}</span>}
+
+        {invitedUser && invitedUser.status !== 'active' && (
+          <>
+            <input
+              onFocus={() => setErrors({ ...errors, password2: null })}
+              value={password2}
+              type="password"
+              placeholder={i18n.t('login.repeatPassword')}
+              onChange={(event) => setPassword2(event.target.value)}
+            />
+            {errors.password2 && <span className="login__field-error">{i18n.t(errors.password2)}</span>}
+          </>
+        )}
+
+        {invitedUser && (
+          <button type="button" className="btn" onClick={onLogin}>
+            {i18n.t('login.acceptInvitation')}
+          </button>
+        )}
+
+        {!invitedUser && (
+          <>
+            <div>
+              <button type="button" className="btn" onClick={() => setLoginLocal(false)}>
+                {i18n.t('login.cancel')}
+              </button>
+
+              <button type="button" className="btn" onClick={onLogin}>
+                {i18n.t('login.login')}
+              </button>
+            </div>
+
+            <Link to={BasePaths.Login.resetPassword()} type="button" className="btn-forgot-pwd">
+              {i18n.t('login.forgotPassword')}
+            </Link>
+          </>
+        )}
+      </div>
+    )
   return (
-    <div className="login__form">
-      <input
-        onFocus={() => setErrors({ ...errors, email: null })}
-        value={email}
-        disabled={!!invitedUser}
-        type="text"
-        placeholder={i18n.t('login.email')}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      {errors.email && <span className="login__field-error">{i18n.t(errors.email)}</span>}
+    <div className="login__formWrapper">
+      <div>
+        <a className="btn" href={`/auth/google${invitationUuid ? `?invitationUuid=${invitationUuid}` : ''}`}>
+          {i18n.t('login.signInGoogle')}
+        </a>
 
-      <input
-        onFocus={() => setErrors({ ...errors, password: null })}
-        value={password}
-        type="password"
-        placeholder={i18n.t('login.password')}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      {errors.password && <span className="login__field-error">{i18n.t(errors.password)}</span>}
-
-      {invitedUser && invitedUser.status !== 'active' && (
-        <>
-          <input
-            onFocus={() => setErrors({ ...errors, password2: null })}
-            value={password2}
-            type="password"
-            placeholder={i18n.t('login.repeatPassword')}
-            onChange={(event) => setPassword2(event.target.value)}
-          />
-          {errors.password2 && <span className="login__field-error">{i18n.t(errors.password2)}</span>}
-        </>
-      )}
-
-      {invitedUser && (
-        <button type="button" className="btn" onClick={onLogin}>
-          {i18n.t('login.acceptInvitation')}
+        <button className="btn" type="button" onClick={() => setLoginLocal(true)}>
+          {i18n.t('login.signInFRA')}
         </button>
-      )}
-
-      {!invitedUser && (
-        <>
-          <div>
-            <button type="button" className="btn" onClick={onCancel}>
-              {i18n.t('login.cancel')}
-            </button>
-
-            <button type="button" className="btn" onClick={onLogin}>
-              {i18n.t('login.login')}
-            </button>
-          </div>
-
-          <Link to={BasePaths.Login.resetPassword()} type="button" className="btn-forgot-pwd">
-            {i18n.t('login.forgotPassword')}
-          </Link>
-        </>
-      )}
+      </div>
+      <div>
+        <div>{i18n.t('login.accessLimited')}</div>
+        <div>
+          {i18n.t('login.returnHome')} <a href="/">{i18n.t('login.returnHomeClick')}</a>
+        </div>
+      </div>
     </div>
   )
+}
+
+LoginForm.defaultProps = {
+  invitationUuid: null,
 }
 
 export default LoginForm
