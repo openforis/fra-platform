600b8f3fc787ebfa3d8973d037334dbad7e09836
sorja <ping@mirosorja.com>
2022-01-26 12:31:53 +0200

    fix query

--- tools/dataMigration/migrateData/migrateCountryStatus.ts
+++ tools/dataMigration/migrateData/migrateCountryStatus.ts
@@ -2,23 +2,24 @@ import { ITask } from 'pg-promise'
 
 import * as pgPromise from 'pg-promise'
 import { Assessment } from '../../../meta/assessment'
+import { DBNames } from '../_DBNames'
 
-export const migrateCountryStatus = async (
-  /* props: { assessment: Assessment } */ _: { assessment: Assessment },
-  client: ITask<any>
-): Promise<void> => {
+export const migrateCountryStatus = async (props: { assessment: Assessment }, client: ITask<any>): Promise<void> => {
   const pgp = pgPromise()
 
-  const queries = [
-    `
-      insert into assessment_fra_2020.country_status (country_iso, status, desk_study)
-      select country_iso, status, desk_study from _legacy.assessment;
-    `,
-    `
-    insert into assessment_fra_2025.country_status (country_iso, status, desk_study)
-    select country_iso, 'editing', false from _legacy.assessment;
-    `,
-  ]
+  const { assessment } = props
+
+  const queries = assessment.cycles.map((cycle, i) => {
+    const cycleName = DBNames.getCycleSchema(assessment.props.name, cycle.name)
+
+    if (i === 0) {
+      return `insert into ${cycleName}.country_status (country_iso, status, desk_study)
+      select country_iso, status, desk_study from _legacy.assessment;`
+    }
+
+    return `insert into ${cycleName}.country_status (country_iso, status, desk_study)
+    select country_iso, 'editing', false from _legacy.assessment;`
+  })
 
   await client.query(pgp.helpers.concat(queries))
 }
