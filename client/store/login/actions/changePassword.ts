65b2df6097c35fec9d2f7fce41657203b0be248c
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-02-03 14:37:15 +0100

    moved type definition into type assertion

--- client/store/login/actions/changePassword.ts
+++ client/store/login/actions/changePassword.ts
@@ -5,25 +5,20 @@ import axios from 'axios'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { BasePaths } from '@client/basePaths'
 
-export const changePassword = createAsyncThunk(
-  'login/post/changePassword',
-  async ({
-    email,
-    password,
-    resetPasswordUuid,
-    history,
-  }: {
+export const changePassword = createAsyncThunk<
+  { message?: string; error?: string },
+  {
     email: string
     password: string
     resetPasswordUuid: string
     history: RouteComponentProps['history']
-  }) => {
-    const { data } = await axios.post(ApiEndPoint.Auth.changePassword(), {
-      email,
-      password,
-      uuid: resetPasswordUuid,
-    })
-    if (data?.message) history.push(BasePaths.Root())
-    return data
   }
-)
+>('login/post/changePassword', async ({ email, password, resetPasswordUuid, history }) => {
+  const { data } = await axios.post(ApiEndPoint.Auth.changePassword(), {
+    email,
+    password,
+    uuid: resetPasswordUuid,
+  })
+  if (data?.message) history.push(BasePaths.Root())
+  return data
+})
--- client/store/login/actions/createResetPassword.ts
+++ client/store/login/actions/createResetPassword.ts
@@ -5,11 +5,11 @@ import axios from 'axios'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { BasePaths } from '@client/basePaths'
 
-export const createResetPassword = createAsyncThunk(
-  'login/post/createResetPassword',
-  async ({ email, history }: { email: string; history: RouteComponentProps['history'] }) => {
-    const { data } = await axios.post(ApiEndPoint.Auth.ResetPassword.one(), { email })
-    if (data?.message) history.push(BasePaths.Root())
-    return data
-  }
-)
+export const createResetPassword = createAsyncThunk<
+  { message?: string; error?: string },
+  { email: string; history: RouteComponentProps['history'] }
+>('login/post/createResetPassword', async ({ email, history }) => {
+  const { data } = await axios.post(ApiEndPoint.Auth.ResetPassword.one(), { email })
+  if (data?.message) history.push(BasePaths.Root())
+  return data
+})
