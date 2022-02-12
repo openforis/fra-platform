23d9668ee8b9a077aae198ed45fc1499408e6e89
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-02-12 16:30:53 +0100

    removed useless script

--- server/repository/assessmentCycle/index.ts
+++ server/repository/assessmentCycle/index.ts
@@ -1,9 +1,7 @@
 import { create } from './create'
-import { createSchema } from './createSchema'
 import { removeSchema } from './removeSchema'
 
 export const AssessmentCycleRepository = {
   create,
-  createSchema,
   removeSchema,
 }
