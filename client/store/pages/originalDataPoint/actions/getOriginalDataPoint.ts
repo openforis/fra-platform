45660580d4714a73ba74845003e440f57eecf022
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-02-24 14:44:50 +0100

    import of ODPs

--- client/store/pages/originalDataPoint/actions/getOriginalDataPoint.ts
+++ client/store/pages/originalDataPoint/actions/getOriginalDataPoint.ts
@@ -3,12 +3,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { OriginalDataPoint } from '@meta/assessment'
-import { addNationalClassPlaceHolder } from '@core/odp/odps/addNationalClassPlaceHolder'
+import { ODPs } from '@meta/assessment/originalDataPoint'
 
 export const getOriginalDataPoint = createAsyncThunk<
   OriginalDataPoint,
   { assessmentName: string; cycleName: string; odpId: string }
 >('originalDataPoint/get/byId', async ({ assessmentName, cycleName, odpId }) => {
   const { data } = await axios.get(ApiEndPoint.Assessment.OriginalDataPoint.one(assessmentName, cycleName, odpId))
-  return addNationalClassPlaceHolder(data)
+  return ODPs.addNationalClassPlaceHolder(data)
 })
--- client/store/pages/originalDataPoint/actions/updateOriginalDataPoint.ts
+++ client/store/pages/originalDataPoint/actions/updateOriginalDataPoint.ts
@@ -3,15 +3,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { OriginalDataPoint } from '@meta/assessment'
-import { addNationalClassPlaceHolder } from '@core/odp/odps/addNationalClassPlaceHolder'
-import { removeNationalClassPlaceHolder } from '@core/odp/odps/removeNationalClassPlaceHolder'
+import { ODPs } from '@meta/assessment/originalDataPoint'
 
 export const updateOriginalDataPoint = createAsyncThunk<
   OriginalDataPoint,
   { assessmentName: string; cycleName: string; odpId: string; originalDataPoint: OriginalDataPoint }
 >('originalDataPoint/put/byId', async ({ assessmentName, cycleName, odpId, originalDataPoint }) => {
   const { data } = await axios.put(ApiEndPoint.Assessment.OriginalDataPoint.one(assessmentName, cycleName, odpId), {
-    originalDataPoint: removeNationalClassPlaceHolder(originalDataPoint),
+    originalDataPoint: ODPs.removeNationalClassPlaceHolder(originalDataPoint),
   })
-  return addNationalClassPlaceHolder(data)
+  return ODPs.addNationalClassPlaceHolder(data)
 })
