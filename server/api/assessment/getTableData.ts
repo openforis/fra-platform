29d3ec6faa0483bb1d4bc64ba938ee5c1ac1f6b0
sorja <ping@mirosorja.com>
2022-02-25 15:08:15 +0200

    use new convnetion for table data

--- server/api/assessment/getTableData.ts
+++ server/api/assessment/getTableData.ts
@@ -1,29 +1,24 @@
-import { Express, Request, Response } from 'express'
-import { ApiEndPoint } from '@common/api/endpoint'
+import { Request, Response } from 'express'
 import Requests from '@server/utils/requests'
 import { CountryIso } from '@meta/area'
 import { CycleDataController } from '@server/controller/cycleData'
 import { AssessmentController } from '@server/controller'
 
-export const AssessmentGetTableData = {
-  init: (express: Express): void => {
-    express.get(ApiEndPoint.Assessment.TableData.one(), async (req: Request, res: Response) => {
-      try {
-        const { countryIso, assessmentName, cycleName } = req.params
-        const { tableNames } = req.query as { tableNames: Array<string> }
+export const getTableData = async (req: Request, res: Response) => {
+  try {
+    const { countryIso, assessmentName, cycleName } = req.params
+    const { tableNames } = req.query as { tableNames: Array<string> }
 
-        const { assessment, cycle } = await AssessmentController.getOneWithCycle({ name: assessmentName, cycleName })
+    const { assessment, cycle } = await AssessmentController.getOneWithCycle({ name: assessmentName, cycleName })
 
-        const table = await CycleDataController.getTableData({
-          countryIso: countryIso as CountryIso,
-          cycle,
-          assessment,
-          tableNames,
-        })
-        Requests.send(res, table)
-      } catch (e) {
-        Requests.sendErr(res, e)
-      }
+    const table = await CycleDataController.getTableData({
+      countryIso: countryIso as CountryIso,
+      cycle,
+      assessment,
+      tableNames,
     })
-  },
+    Requests.send(res, table)
+  } catch (e) {
+    Requests.sendErr(res, e)
+  }
 }
--- server/api/assessment/index.ts
+++ server/api/assessment/index.ts
@@ -4,7 +4,7 @@ import { AssessmentPostCountryStatus } from './postCountryStatus'
 import { AssessmentGetSections } from './getSections'
 import { AssessmentGetOdp } from './getOdp'
 import { AssessmentGetCountryStatus } from './getCountryStatus'
-import { AssessmentGetTableData } from './getTableData'
+import { getTableData } from './getTableData'
 import { AssessmentGetSectionMetadata } from './getSectionMetadata'
 import { getReservedYears } from './getReservedYears'
 import { updateOriginalDataPoint } from './updateOriginalDataPoint'
@@ -12,13 +12,17 @@ import { updateOriginalDataPoint } from './updateOriginalDataPoint'
 export const AssessmentApi = {
   init: (express: Express): void => {
     AssessmentGetSections.init(express)
-    AssessmentGetTableData.init(express)
     AssessmentGetSectionMetadata.init(express)
     AssessmentGetOdp.init(express)
     AssessmentGetCountryStatus.init(express)
 
     AssessmentPostCountryStatus.init(express)
+    // OriginalDataPoint
     express.get(ApiEndPoint.Assessment.OriginalDataPoint.ReservedYears.many(), getReservedYears)
     express.put(ApiEndPoint.Assessment.OriginalDataPoint.one(), updateOriginalDataPoint)
+
+    // TableData
+    express.get(ApiEndPoint.Assessment.TableData.one(), getTableData)
+    // TODO: express.put(ApiEndPoint.Assessment.TableData.one(), updateTableData)
   },
 }
