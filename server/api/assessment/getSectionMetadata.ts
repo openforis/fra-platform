b4587bea65be3c6ff15f3cd213144c5166089438
Miró Sorja <ping@mirosorja.com>
2022-03-01 17:55:29 +0200

    fix typo

--- server/api/assessment/getSectionMetadata.ts
+++ server/api/assessment/getSectionMetadata.ts
@@ -3,27 +3,22 @@ import { ApiEndPoint } from '@common/api/endpoint'
 import Requests from '@server/utils/requests'
 import { AssessmentController } from '@server/controller'
 import { AssessmentName } from '@meta/assessment'
-import { AuthorizationMiddleware } from '@server/middleware/Authorization'
 
 export const AssessmentGetSectionMetadata = {
   init: (express: Express): void => {
-    express.get(
-      ApiEndPoint.Assessment.Sections.Metadata.many(),
-      AuthorizationMiddleware.canView,
-      async (req: Request, res: Response) => {
-        try {
-          const { assessmentName, section, cycleName } = req.params
+    express.get(ApiEndPoint.Assessment.Sections.Metadata.many(), async (req: Request, res: Response) => {
+      try {
+        const { assessmentName, section, cycleName } = req.params
 
-          const tablesMetadata = await AssessmentController.getSectionMetadata({
-            cycleName,
-            assessmentName: assessmentName as AssessmentName,
-            section,
-          })
-          Requests.send(res, tablesMetadata)
-        } catch (e) {
-          Requests.sendErr(res, e)
-        }
+        const tablesMetadata = await AssessmentController.getSectionMetadata({
+          cycleName,
+          assessmentName: assessmentName as AssessmentName,
+          section,
+        })
+        Requests.send(res, tablesMetadata)
+      } catch (e) {
+        Requests.sendErr(res, e)
       }
-    )
+    })
   },
 }
