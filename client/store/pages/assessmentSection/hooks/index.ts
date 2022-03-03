275da12063728ca1bbb3a4f2324166535d988448
minotogna <mino.togna@gmail.com>
2022-03-03 09:50:19 +0100

    add useTableData

--- client/store/pages/assessmentSection/hooks/index.ts
+++ client/store/pages/assessmentSection/hooks/index.ts
@@ -1,5 +1,8 @@
 import { useAppSelector } from '@client/store'
 import { TableSection } from '@meta/assessment'
+import { TableData } from '@meta/data'
 
 export const useTableSections = (): Array<TableSection> =>
   useAppSelector((state) => state.pages.assessmentSection.tableSections)
+
+export const useTableData = (): TableData => useAppSelector((state) => state.pages.assessmentSection.data)
