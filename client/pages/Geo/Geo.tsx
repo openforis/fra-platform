5a87000b0afd9c2bdce3d4cc1f0506faebb4c6be
xbexbex <petja.valkama@gmail.com>
2022-03-06 16:04:48 +0200

    Update geo map to fill remaining screen space

--- client/pages/Geo/Geo.tsx
+++ client/pages/Geo/Geo.tsx
@@ -1,8 +1,26 @@
 import './geo.scss'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import GeoMap from './GeoMap'
 
 const Geo: React.FC = () => {
-  return <div />
+  const [isRenderAllowed, setIsRenderAllowed] = useState(false)
+
+  // makes the map fill remaining screen space nicely
+  useEffect(() => {
+    const main = document.getElementById('main')
+    const mainDefaultVal = main.style.minHeight
+    main.style.minHeight = '100vh'
+
+    // don't render map until height is set or it will be rendered with a height of 0
+    setIsRenderAllowed(true)
+
+    return () => {
+      main.style.minHeight = mainDefaultVal
+    }
+  }, [])
+
+  return <div className="geo-container">{isRenderAllowed && <GeoMap />}</div>
 }
 
 export default Geo
