9cce9f997bb3afbe414a08634e7befefa9ac09a2
xbexbex <petja.valkama@gmail.com>
2022-03-07 20:18:43 +0200

    Remove dynamic css styling from Geo

--- client/pages/Geo/Geo.tsx
+++ client/pages/Geo/Geo.tsx
@@ -1,34 +1,16 @@
 import './geo.scss'
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense } from 'react'
 
 import Loading from '@client/components/Loading'
 
 const GeoMap = React.lazy(() => import('./GeoMap'))
 
 const Geo: React.FC = () => {
-  const [isRenderAllowed, setIsRenderAllowed] = useState(false)
-
-  // makes the map fill remaining screen space nicely
-  useEffect(() => {
-    const main = document.getElementById('main')
-    const mainDefaultVal = main.style.minHeight
-    main.style.minHeight = '100vh'
-
-    // don't render map until height is set or it will be rendered with a height of 0
-    setIsRenderAllowed(true)
-
-    return () => {
-      main.style.minHeight = mainDefaultVal
-    }
-  }, [])
-
   return (
     <div className="geo-container">
-      {isRenderAllowed && (
-        <Suspense fallback={<Loading />}>
-          <GeoMap />
-        </Suspense>
-      )}
+      <Suspense fallback={<Loading />}>
+        <GeoMap />
+      </Suspense>
     </div>
   )
 }
