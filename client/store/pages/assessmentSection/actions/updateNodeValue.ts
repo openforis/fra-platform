acf487670d8e53f5b98396fdb5d4726e04036a91
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-03-08 12:31:45 +0100

    cosmetic changes

--- client/store/pages/assessmentSection/actions/updateNodeValue.ts
+++ client/store/pages/assessmentSection/actions/updateNodeValue.ts
@@ -4,7 +4,6 @@ import axios from 'axios'
 import { ApiEndPoint } from '@common/api/endpoint'
 import { CountryIso } from '@meta/area'
 import { AssessmentName, NodeValue, TableData } from '@meta/assessment'
-import { RootState } from '@client/store'
 // import { TableDatas } from '@meta/data'
 
 export const updateNodeValue = createAsyncThunk<
@@ -20,47 +19,42 @@ export const updateNodeValue = createAsyncThunk<
     value: NodeValue
     // TODO support multiple values
     // values: Array<NodeValue>
-  },
-  {
-    state: RootState
   }
 >(
   'section/patch/nodeValue',
-  async ({ assessmentName, countryIso, colName, cycleName, sectionName, tableName, variableName, value }) =>
-    // { getState }
-    {
-      const { data } = await axios.patch(
-        ApiEndPoint.CycleData.PersistNode.one(),
-        {
+  async ({ assessmentName, countryIso, colName, cycleName, sectionName, tableName, variableName, value }) => {
+    const { data } = await axios.patch(
+      ApiEndPoint.CycleData.PersistNode.one(),
+      {
+        variableName,
+        value,
+      },
+      {
+        params: {
+          assessmentName,
+          countryIso,
+          colName,
+          cycleName,
+          sectionName,
+          tableName,
           variableName,
           value,
         },
-        {
-          params: {
-            assessmentName,
-            countryIso,
-            colName,
-            cycleName,
-            sectionName,
-            tableName,
-            variableName,
-            value,
-          },
-        }
-      )
+      }
+    )
 
-      return data?.data
+    return data?.data
 
-      // // TODO: Remove after websocket implemented
-      // const { data } = getState().pages.assessmentSection
-      // return data
-      // return TableDatas.updateDatum({
-      //   data,
-      //   countryIso,
-      //   tableName,
-      //   variableName,
-      //   colName,
-      //   value,
-      // })
-    }
+    // // TODO: Remove after websocket implemented
+    // const { data } = getState().pages.assessmentSection
+    // return data
+    // return TableDatas.updateDatum({
+    //   data,
+    //   countryIso,
+    //   tableName,
+    //   variableName,
+    //   colName,
+    //   value,
+    // })
+  }
 )
