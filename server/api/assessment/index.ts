aa9679bce6637013ec0dfdc02f4ae1a8fe903fcd
sorja <ping@mirosorja.com>
2022-03-10 08:51:31 +0200

    requireView permission

--- server/api/assessment/index.ts
+++ server/api/assessment/index.ts
@@ -1,5 +1,6 @@
 import { Express } from 'express'
 import { ApiEndPoint } from '@common/api/endpoint'
+import { AuthMiddleware } from '@server/middleware/auth'
 import { getSections } from './getSections'
 import { getOriginalDataPoint } from './getOdp'
 import { getCountryStatus } from './getCountryStatus'
@@ -15,21 +16,31 @@ import { updateOriginalDataPoint } from './updateOriginalDataPoint'
 
 export const AssessmentApi = {
   init: (express: Express): void => {
-    express.get(ApiEndPoint.Assessment.sections(), getSections)
-    express.get(ApiEndPoint.Assessment.TableData.one(), getTableData)
-    express.get(ApiEndPoint.Assessment.Sections.Metadata.many(), getSectionMetadata)
-    express.get(ApiEndPoint.Assessment.OriginalDataPoint.one(), getOriginalDataPoint)
-    express.get(ApiEndPoint.Assessment.countryStatus(), getCountryStatus)
+    // CountryStatus
+    express.get(ApiEndPoint.Assessment.countryStatus(), AuthMiddleware.requireView, getCountryStatus)
     express.post(ApiEndPoint.Assessment.countryStatus(), postCountryStatus)
 
     // OriginalDataPoint
-    express.get(ApiEndPoint.Assessment.OriginalDataPoint.ReservedYears.many(), getReservedYears)
+    express.get(
+      ApiEndPoint.Assessment.OriginalDataPoint.ReservedYears.many(),
+      AuthMiddleware.requireView,
+      getReservedYears
+    )
+
     express.post(ApiEndPoint.Assessment.OriginalDataPoint.one(), createOriginalDataPoint)
     express.delete(ApiEndPoint.Assessment.OriginalDataPoint.one(), deleteOriginalDataPoint)
+
+    express.get(ApiEndPoint.Assessment.OriginalDataPoint.one(), AuthMiddleware.requireView, getOriginalDataPoint)
     express.put(ApiEndPoint.Assessment.OriginalDataPoint.one(), updateOriginalDataPoint)
 
     // TableData
-    express.get(ApiEndPoint.Assessment.TableData.one(), getTableData)
+    express.get(ApiEndPoint.Assessment.TableData.one(), AuthMiddleware.requireView, getTableData)
+
+    express.get(ApiEndPoint.Assessment.TableData.one(), AuthMiddleware.requireView, getTableData)
     express.patch(ApiEndPoint.CycleData.PersistNode.one(), persistNodeValue)
+
+    // Sections
+    express.get(ApiEndPoint.Assessment.sections(), AuthMiddleware.requireView, getSections)
+    express.get(ApiEndPoint.Assessment.Sections.Metadata.many(), AuthMiddleware.requireView, getSectionMetadata)
   },
 }
