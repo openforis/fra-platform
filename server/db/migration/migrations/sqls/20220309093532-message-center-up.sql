f53f7ab6f6f754844d89edb51912e3728ab72665
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-03-16 16:00:41 +0100

    queries have been cleaned up

--- server/db/migration/migrations/sqls/20220309093532-message-center-up.sql
+++ server/db/migration/migrations/sqls/20220309093532-message-center-up.sql
@@ -1,15 +1,14 @@
-CREATE TYPE public.message_topic_status AS ENUM
-    ('opened', 'resolved');
+CREATE TYPE public.message_topic_status AS ENUM ('opened', 'resolved');
 
 CREATE SEQUENCE message_topic_id_seq INCREMENT 1 START 1;
 
 CREATE TABLE IF NOT EXISTS public.message_topic
 (
     id bigint NOT NULL DEFAULT nextval('message_topic_id_seq'::regclass),
-    country_iso character varying(3) COLLATE pg_catalog."default" NOT NULL,
+    country_iso character varying(3) NOT NULL,
     assessment_id bigint NOT NULL,
     cycle_uuid uuid NOT NULL,
-    key character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    key character varying(256) NOT NULL,
     status message_topic_status DEFAULT 'opened'::message_topic_status,
     CONSTRAINT message_topic_pkey PRIMARY KEY (id),
     CONSTRAINT message_topic_assessment_id_fkey FOREIGN KEY (assessment_id)
@@ -29,24 +28,26 @@ CREATE TABLE IF NOT EXISTS public.message_topic
         NOT VALID
 );
 
+CREATE SEQUENCE message_id_seq INCREMENT 1 START 1;
+
 CREATE TABLE IF NOT EXISTS public.message
 (
-    id bigint NOT NULL,
+    id bigint NOT NULL DEFAULT nextval('message_id_seq'::regclass),
     topic_id bigint NOT NULL,
     user_id bigint NOT NULL,
-    message text COLLATE pg_catalog."default",
+    message text,
     deleted boolean DEFAULT false,
     created_time timestamp with time zone DEFAULT now(),
     CONSTRAINT message_pkey PRIMARY KEY (id),
     CONSTRAINT message_topic_id_fkey FOREIGN KEY (topic_id)
         REFERENCES public.message_topic (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         NOT VALID,
     CONSTRAINT message_user_id_fkey FOREIGN KEY (user_id)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         NOT VALID
 );
 
@@ -58,11 +59,11 @@ CREATE TABLE IF NOT EXISTS public.message_topic_user
     CONSTRAINT message_topic_user_topic_id_fkey FOREIGN KEY (topic_id)
         REFERENCES public.message_topic (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         NOT VALID,
     CONSTRAINT message_topic_user_user_id_fkey FOREIGN KEY (user_id)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+        ON DELETE CASCADE
         NOT VALID
 );
