a850717ed2ce3db92b639b4c6cba6630e2249e90
sorja <ping@mirosorja.com>
2022-03-17 18:57:05 +0200

    originalDataPoint data API, View, etc

--- server/controller/cycleData/index.ts
+++ server/controller/cycleData/index.ts
@@ -1,7 +1,9 @@
 import { persistNodeValue } from './persistNodeValue'
 import { getTableData } from './getTableData'
+import { getOriginalDataPointData } from './getOriginalDataPointData'
 
 export const CycleDataController = {
   persistNodeValue,
   getTableData,
+  getOriginalDataPointData,
 }
--- tools/dataMigration/migrateData/migrateOdps.ts
+++ tools/dataMigration/migrateData/migrateOdps.ts
@@ -3,6 +3,7 @@ import { ITask } from 'pg-promise'
 import * as pgPromise from 'pg-promise'
 import { Assessment } from '../../../meta/assessment/assessment'
 import { DBNames } from '../_DBNames'
+import { getCreateODPViewDDL } from './_createOdpView'
 
 export const migrateOdps = async (props: { assessment: Assessment }, client: ITask<any>): Promise<void> => {
   const { assessment } = props
@@ -11,28 +12,29 @@ export const migrateOdps = async (props: { assessment: Assessment }, client: ITa
   const queries = assessment.cycles.map((cycle) => {
     const cycleName = DBNames.getCycleSchema(assessment.props.name, cycle.name)
     return `
-insert into ${cycleName}.original_data_point (id,
-    country_iso,
-    year,
-    data_source_additional_comments,
-    data_source_methods,
-    data_source_references,
-    description,
-    national_classes,
-    id_legacy
-)
-select id,
-       country_iso,
-       year,
-       data_source_additional_comments,
-       data_source_methods,
-       data_source_references,
-       description,
-       national_classes,
-       id_legacy
-from _legacy.original_data_point;
-select setval('${cycleName}.original_data_point_id_seq', (select max(id) from _legacy.original_data_point), true);
-`
+      insert into ${cycleName}.original_data_point (id,
+          country_iso,
+          year,
+          data_source_additional_comments,
+          data_source_methods,
+          data_source_references,
+          description,
+          national_classes,
+          id_legacy
+      )
+      select id,
+             country_iso,
+             year,
+             data_source_additional_comments,
+             data_source_methods,
+             data_source_references,
+             description,
+             national_classes,
+             id_legacy
+      from _legacy.original_data_point;
+      select setval('${cycleName}.original_data_point_id_seq', (select max(id) from _legacy.original_data_point), true);
+    `
   })
   await client.query(pgp.helpers.concat(queries))
+  await client.query(getCreateODPViewDDL({ assessment }))
 }
