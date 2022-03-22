449646919a5a79a2c94ee1409a71ef96bbc2c2fb
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-03-22 11:14:58 +0100

    fixed missing parameter

--- server/api/messageCenter/getTopic.ts
+++ server/api/messageCenter/getTopic.ts
@@ -5,12 +5,13 @@ import { CountryIso } from '@core/country'
 
 export const getTopic = async (req: Request, res: Response) => {
   try {
-    const { countryIso, assessmentName, cycleName } = req.query
+    const { countryIso, assessmentName, cycleName, key } = req.query
 
     const topic = await MessageCenterController.getTopic({
       countryIso: countryIso as CountryIso,
       assessmentName: String(assessmentName),
       cycleName: String(cycleName),
+      key: String(key),
     })
 
     Requests.sendOk(res, topic)
