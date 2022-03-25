06373811bcd5beadf8cd60bf88d536d824d41243
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-03-25 10:08:17 +0100

    topics file has been separated for readability

--- meta/messageCenter/index.ts
+++ meta/messageCenter/index.ts
@@ -1,12 +1,5 @@
-import { Row } from '@meta/assessment'
-
 export type { Message } from './message'
 export type { MessageTopic } from './messageTopic'
 
 export { MessageTopicStatus } from './messageTopic'
-
-const getDataReviewTopicKey = (row: Row): string => row.uuid
-
-export const Topics = {
-  getDataReviewTopicKey,
-}
+export { Topics } from './topics'
