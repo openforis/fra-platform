3a073aea78fadaddf5564ecdff78bbaa8ca6b381
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-03-31 11:07:30 +0200

    added message time

--- client/components/MessageCenter/Topic/Message/Message.tsx
+++ client/components/MessageCenter/Topic/Message/Message.tsx
@@ -1,17 +1,21 @@
 import './Message.scss'
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'
 import { Message as MessageType } from '@meta/messageCenter/message'
+import { getRelativeDate } from '@client/utils/relativeDate'
 
 type MessageProps = {
   message: MessageType
 }
 
 const TopicMessage: React.FC<MessageProps> = ({ message }) => {
+  const { i18n } = useTranslation()
+
   return (
     <div className="message">
       <div className="message-header">
         <div className="message-author">{message.user.name}</div>
+        <div className="message-time">{getRelativeDate(message.createdTime, i18n) || i18n.t('time.aMomentAgo')}</div>
       </div>
       <div className="message-body">{message.message}</div>
     </div>
