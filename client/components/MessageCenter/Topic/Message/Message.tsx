9d538928376e2be580e922a64e601b61c010dd29
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-03-31 16:42:16 +0200

    added avatar into messages

--- client/components/MessageCenter/Topic/Message/Message.tsx
+++ client/components/MessageCenter/Topic/Message/Message.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Message as MessageType } from '@meta/messageCenter/message'
 import { getRelativeDate } from '@client/utils/relativeDate'
+import { ApiEndPoint } from '@common/api/endpoint'
 
 type MessageProps = {
   message: MessageType
@@ -14,8 +15,11 @@ const TopicMessage: React.FC<MessageProps> = ({ message }) => {
   return (
     <div className="message">
       <div className="message-header">
-        <div className="message-author">{message.user.name}</div>
-        <div className="message-time">{getRelativeDate(message.createdTime, i18n) || i18n.t('time.aMomentAgo')}</div>
+        <img className="message-avatar" src={ApiEndPoint.User.getProfilePicture(String(message.user.id))} alt="" />
+        <div className="message-info">
+          <div className="message-author">{message.user.name}</div>
+          <div className="message-time">{getRelativeDate(message.createdTime, i18n) || i18n.t('time.aMomentAgo')}</div>
+        </div>
       </div>
       <div className="message-body">{message.message}</div>
     </div>
