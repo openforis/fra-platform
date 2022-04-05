309a887cb11d5686fcd5dbbe25ac6b121d127058
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-04-05 12:53:48 +0200

    added missing return type

--- server/api/auth/postLogout.ts
+++ server/api/auth/postLogout.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express'
 
-export const postLogout = (_: Request, res: Response) => {
+export const postLogout = (_: Request, res: Response): void => {
   res.clearCookie('token')
   res.json({})
 }
