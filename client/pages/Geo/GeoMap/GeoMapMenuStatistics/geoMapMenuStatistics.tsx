3659de8ebb75fd2dd4e507be0fed19dd40971fef
xbexbex <petja.valkama@gmail.com>
2022-04-12 11:12:05 +0300

    Add geo map menu statistics placeholder

---
+++ client/pages/Geo/GeoMap/GeoMapMenuStatistics/geoMapMenuStatistics.tsx
@@ -0,0 +1,16 @@
+import './geoMapMenuStatistics.scss'
+import React from 'react'
+
+import GeoMapMenuButton from '../GeoMapMenuButton'
+
+// Placeholder for Data Layers menu
+
+const GeoMapMenuStatistics: React.FC = () => {
+  return (
+    <div className="geo-map-menu-item">
+      <GeoMapMenuButton panel="statistics" text="Statistics" />
+    </div>
+  )
+}
+
+export default GeoMapMenuStatistics
