bbc16030e2bde776507e1532f77c99f6c8aa6be6
Miró Sorja <ping@mirosorja.com>
2022-04-13 10:16:14 +0300

    simple import sort (#1255)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,90 @@
+module.exports = {
+  globals: {
+    __DEV__: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['prettier', 'airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  plugins: ['prettier', 'simple-import-sort'],
+  rules: {
+    'no-underscore-dangle': 0,
+    'prettier/prettier': 1,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/forbid-prop-types': 0,
+    'react/prop-types': 'off',
+    'import/prefer-default-export': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [2, { argsIgnorePattern: '^_' }],
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-restricted-exports': 'off',
+    'react/function-component-definition': 'off',
+
+    // simple-import-sort rules
+
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            '^\\u0000', // Side effect imports.
+          ],
+          [
+            '^react', // `react` related packages come first.
+          ],
+          [
+            // Node.js builtins.
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+            '^@?\\w',
+          ],
+          [
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./?$', // Parent imports. Put `..` last.
+            '^\\./(?=.*/)(?!/?$)',
+            '^\\.(?!/?$)',
+            '^\\./?$', // Other relative imports. Put same-folder imports and `.` last.
+            '^.+\\.s?css$', // Style imports.
+          ],
+        ],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {},
+      webpack: {
+        config: 'webpack.config.babel.js',
+      },
+    },
+  },
+}
