e13f75d8b44c19d49c3cabac091dcca8fe5f735b
Mino Togna <minotogna@users.noreply.github.com>
2022-04-13 16:19:36 +0200

    migrate row chart props (#1260)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- tools/dataMigration/migrateMetadata/getRow.ts
+++ tools/dataMigration/migrateMetadata/getRow.ts
@@ -1,6 +1,6 @@
-import { RowSpec } from '../../../webapp/sectionSpec'
-import { Table } from '../../../meta/assessment/table'
 import { Row, RowType } from '../../../meta/assessment/row'
+import { Table } from '../../../meta/assessment/table'
+import { RowSpec } from '../../../webapp/sectionSpec'
 
 export const getRow = (props: { cycles: Array<string>; rowSpec: RowSpec; table: Table }): Row => {
   const { cycles, rowSpec, table } = props
@@ -16,6 +16,9 @@ export const getRow = (props: { cycles: Array<string>; rowSpec: RowSpec; table: 
     },
     cols: [],
     tableId: table.id,
+  }
+  if (rowSpec.chartProps) {
+    row.props.chart = rowSpec.chartProps
   }
   return row
 }
