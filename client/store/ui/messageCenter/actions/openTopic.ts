d66ade3b712745c9bab88ea5d5ff7b15287811dd
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-05-04 17:31:24 +0200

    fixed problem with message status open when the topic is not stored into the db

--- client/store/ui/messageCenter/actions/openTopic.ts
+++ client/store/ui/messageCenter/actions/openTopic.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 import { CountryIso } from '@meta/area'
-import { MessageTopic, MessageTopicType } from '@meta/messageCenter'
+import { MessageTopic, MessageTopicStatus, MessageTopicType } from '@meta/messageCenter'
 
 type Params = {
   countryIso: CountryIso
@@ -21,6 +21,15 @@ export const openTopic = createAsyncThunk<MessageTopic, Params>(
   async ({ countryIso, assessmentName, cycleName, key, title, subtitle, type, section }) => {
     const params = { countryIso, assessmentName, cycleName, key, type, section }
     const { data } = await axios.get(ApiEndPoint.MessageCenter.Topic.get(), { params })
-    return { ...data, countryIso, type, title, subtitle, key, messages: data?.messages || [] }
+    return {
+      ...data,
+      countryIso,
+      key,
+      status: MessageTopicStatus.opened,
+      type,
+      title,
+      subtitle,
+      messages: data?.messages || [],
+    }
   }
 )
