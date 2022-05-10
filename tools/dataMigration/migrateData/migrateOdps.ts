f70e99341de8bea7400878d9c511d575367868cc
Mino Togna <minotogna@users.noreply.github.com>
2022-05-10 18:39:32 +0200

    fix ODP calculated node values (#1307)

--- tools/dataMigration/migrateData/migrateOdps.ts
+++ tools/dataMigration/migrateData/migrateOdps.ts
@@ -1,7 +1,8 @@
 import { ITask } from 'pg-promise'
-
 import * as pgPromise from 'pg-promise'
+
 import { Assessment } from '../../../meta/assessment/assessment'
+import { getCreateSchemaCycleOriginalDataPointViewDDL } from '../../../server/repository/assessment/assessment/getCreateSchemaDDL'
 import { DBNames } from '../_DBNames'
 
 export const migrateOdps = async (props: { assessment: Assessment }, client: ITask<any>): Promise<void> => {
@@ -9,9 +10,9 @@ export const migrateOdps = async (props: { assessment: Assessment }, client: ITa
   const pgp = pgPromise()
 
   const queries = assessment.cycles.map((cycle) => {
-    const cycleName = DBNames.getCycleSchema(assessment.props.name, cycle.name)
+    const schemaCycle = DBNames.getCycleSchema(assessment.props.name, cycle.name)
     return `
-      insert into ${cycleName}.original_data_point (id,
+      insert into ${schemaCycle}.original_data_point (id,
           country_iso,
           year,
           data_source_additional_comments,
@@ -31,7 +32,9 @@ export const migrateOdps = async (props: { assessment: Assessment }, client: ITa
              national_classes,
              id_legacy
       from _legacy.original_data_point;
-      select setval('${cycleName}.original_data_point_id_seq', (select max(id) from _legacy.original_data_point), true);
+      select setval('${schemaCycle}.original_data_point_id_seq', (select max(id) from _legacy.original_data_point), true);
+
+      ${getCreateSchemaCycleOriginalDataPointViewDDL(schemaCycle)}
     `
   })
   await client.query(pgp.helpers.concat(queries))
