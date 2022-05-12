57f12faac7c14f5e7462b7038c68971662a5c233
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-05-12 13:22:22 +0200

    topic user CRU operations

---
+++ server/repository/assessmentCycle/messageTopicUser/getOneOrNone.ts
@@ -0,0 +1,25 @@
+import { Objects } from '@core/utils'
+
+import { Assessment, Cycle } from '@meta/assessment'
+import { MessageTopic } from '@meta/messageCenter'
+import { User } from '@meta/user'
+
+import { BaseProtocol, DB, Schemas } from '@server/db'
+
+export const getOneOrNone = async (
+  props: { assessment: Assessment; cycle: Cycle; topic: MessageTopic; user: User },
+  client: BaseProtocol = DB
+): Promise<{ lastOpenTime: string } | null> => {
+  const { assessment, cycle, topic, user } = props
+
+  const cycleSchema = Schemas.getNameCycle(assessment, cycle)
+
+  return client.oneOrNone<{ lastOpenTime: string } | null>(
+    `
+      select last_open_time from ${cycleSchema}.message_topic_user
+      where topic_id = $1 and user_id = $2
+    `,
+    [topic.id, user.id],
+    Objects.camelize
+  )
+}
