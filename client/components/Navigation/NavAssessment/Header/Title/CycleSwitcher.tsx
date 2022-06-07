f894ea0201feeb60c4057819fc728c01a77dc167
Miró Sorja <ping@mirosorja.com>
2022-06-07 16:02:41 +0300

    CycleSwitcher/canSwitchCycle: check user cycles (#1361)

--- client/components/Navigation/NavAssessment/Header/Title/CycleSwitcher.tsx
+++ client/components/Navigation/NavAssessment/Header/Title/CycleSwitcher.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 
+import { Authorizer } from '@meta/user'
+
 import { useAssessment, useCycle } from '@client/store/assessment'
 import { useIsDataLocked } from '@client/store/ui/dataLock'
 import { useUser } from '@client/store/user'
@@ -12,8 +14,19 @@ const CycleSwitcher = () => {
   const history = useHistory()
   const cycle = useCycle()
   const assessment = useAssessment()
+  const user = useUser()
+  const isDataLocked = useIsDataLocked()
 
-  const canSwitchCycle = useUser() && useIsDataLocked() // TODO
+  const userCycles = assessment.cycles.filter((cycle) =>
+    Authorizer.canView({
+      countryIso,
+      user,
+      cycle,
+      assessment,
+    })
+  )
+
+  const canSwitchCycle = user && isDataLocked && userCycles.length > 1
 
   if (!canSwitchCycle) return <span>{cycle.name}</span>
 
