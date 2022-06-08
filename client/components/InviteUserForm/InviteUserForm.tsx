ce44d62db9d13eb7ae66fd85e5ca2e605e2355c1
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-06-08 13:18:42 +0200

    form style refactor

--- client/components/InviteUserForm/InviteUserForm.tsx
+++ client/components/InviteUserForm/InviteUserForm.tsx
@@ -2,8 +2,6 @@ import './InviteUserForm.scss'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import classNames from 'classnames'
-
 import { RoleName, Users } from '@meta/user'
 
 import { useAppDispatch } from '@client/store'
@@ -12,8 +10,6 @@ import { useUser } from '@client/store/user'
 import { UserManagementActions } from '@client/store/userManagement'
 import { useCountryIso } from '@client/hooks'
 import { useToaster } from '@client/hooks/useToaster'
-
-import TextInput from '../TextInput'
 
 const validateName = (name: string) => !!name.trim()
 const validateRole = (role: string) => !!role
@@ -27,58 +23,6 @@ interface UserToInvite {
   name: string
   role?: RoleName
   email: string
-}
-
-interface UserTextFieldColProps {
-  user: UserToInvite
-  field: keyof UserToInvite
-  updateUser: React.Dispatch<React.SetStateAction<UserToInvite>>
-  isValid: boolean
-}
-
-const UserTextFieldCol = ({ user, field, updateUser, isValid }: UserTextFieldColProps) => {
-  const { i18n } = useTranslation()
-
-  return (
-    <td className={classNames('user-list__cell editing', { error: isValid })}>
-      <TextInput
-        placeholder={i18n.t(`userManagement.${field}`)}
-        value={user[field]}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateUser({ ...user, [field]: e.target.value })}
-        disabled={false}
-      />
-    </td>
-  )
-}
-
-interface UserRoleSelectColProps {
-  user: UserToInvite
-  updateUser: React.Dispatch<React.SetStateAction<UserToInvite>>
-  isValid: boolean
-  allowedRoles: Array<RoleName>
-}
-
-const UserRoleSelectCol = ({ user, updateUser, isValid, allowedRoles }: UserRoleSelectColProps) => {
-  const { i18n } = useTranslation()
-
-  return (
-    <td className={classNames('user-list__cell editing error', { error: isValid })}>
-      <div className="user-list__input-container validation-error-sensitive-field">
-        <select
-          className="fra-table__select"
-          value={user.role}
-          onChange={(e) => updateUser({ ...user, role: e.target.value as RoleName })}
-        >
-          {!user.role ? <option value="">{i18n.t('userManagement.placeholder')}</option> : null}
-          {allowedRoles.map((role: RoleName) => (
-            <option key={role} value={role}>
-              {i18n.t(Users.getI18nRoleLabelKey(role))}
-            </option>
-          ))}
-        </select>
-      </div>
-    </td>
-  )
 }
 
 const InviteUserForm: React.FC = () => {
@@ -96,64 +40,82 @@ const InviteUserForm: React.FC = () => {
   const cycle = useCycle()
   const user = useUser()
 
+  const onUserInvite = () => {
+    const fieldErrors = {
+      name: !validateName(userToInvite.name),
+      role: !validateRole(userToInvite.role),
+      email: !validateEmail(userToInvite.email),
+    }
+    setErrors(fieldErrors)
+
+    if (!Object.values(fieldErrors).find((value) => !!value))
+      dispatch(
+        UserManagementActions.inviteUser({
+          countryIso,
+          assessmentName: assessment.props.name,
+          cycleName: cycle.name,
+          name: userToInvite.name,
+          role: userToInvite.role,
+          email: userToInvite.email,
+        })
+      ).then(() => {
+        setUserToInvite({ name: '', email: '' })
+        toaster.info(i18n.t('userManagement.userAdded', { email: userToInvite.email }))
+      })
+  }
+
   return (
-    <div className="add-user__container">
+    <div className="invite-user-container">
       {Object.values(errors).find((value) => !!value) && (
         <div className="add-user__error-container">{i18n.t('userManagement.formErrors')}</div>
       )}
-
-      <table className="add-user__table">
-        <thead>
-          <tr>
-            <th className="user-list__header-cell">{i18n.t('userManagement.name')}</th>
-            <th className="user-list__header-cell">{i18n.t('userManagement.role')}</th>
-            <th className="user-list__header-cell">{i18n.t('userManagement.email')}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <UserTextFieldCol user={userToInvite} field="name" updateUser={setUserToInvite} isValid={errors.name} />
-            <UserRoleSelectCol
-              user={userToInvite}
-              updateUser={setUserToInvite}
-              isValid={errors.role}
-              allowedRoles={Users.getRolesAllowedToEdit({ user, countryIso })}
-            />
-            <UserTextFieldCol user={userToInvite} field="email" updateUser={setUserToInvite} isValid={errors.email} />
-            <td style={{ padding: 0 }}>
-              <button
-                className="btn btn-primary"
-                onClick={() => {
-                  const fieldErrors = {
-                    name: !validateName(userToInvite.name),
-                    role: !validateRole(userToInvite.role),
-                    email: !validateEmail(userToInvite.email),
-                  }
-                  setErrors(fieldErrors)
-
-                  if (!Object.values(fieldErrors).find((value) => !!value))
-                    dispatch(
-                      UserManagementActions.inviteUser({
-                        countryIso,
-                        assessmentName: assessment.props.name,
-                        cycleName: cycle.name,
-                        name: userToInvite.name,
-                        role: userToInvite.role,
-                        email: userToInvite.email,
-                      })
-                    ).then(() => {
-                      setUserToInvite({ name: '', email: '' })
-                      toaster.info(i18n.t('userManagement.userAdded', { email: userToInvite.email }))
-                    })
-                }}
-                type="submit"
-              >
-                {i18n.t('userManagement.addUser')}
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="invite-user-form">
+        <div>
+          <div className="label">{i18n.t('userManagement.name')}</div>
+          <input
+            onFocus={() => setErrors({ ...errors, name: null })}
+            name="name"
+            value={userToInvite.name}
+            type="text"
+            placeholder={i18n.t('userManagement.name')}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserToInvite({ ...userToInvite, name: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <div className="label">{i18n.t('userManagement.role')}</div>
+          <select
+            value={userToInvite.role}
+            onChange={(e) => setUserToInvite({ ...userToInvite, role: e.target.value as RoleName })}
+          >
+            {!userToInvite.role ? <option value="">{i18n.t('userManagement.placeholder')}</option> : null}
+            {Users.getRolesAllowedToEdit({ user, countryIso }).map((role: RoleName) => (
+              <option key={role} value={role}>
+                {i18n.t(Users.getI18nRoleLabelKey(role))}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <div className="label">{i18n.t('userManagement.email')}</div>
+          <input
+            onFocus={() => setErrors({ ...errors, email: null })}
+            name="email"
+            value={userToInvite.email}
+            type="text"
+            placeholder={i18n.t('userManagement.email')}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserToInvite({ ...userToInvite, email: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary" onClick={onUserInvite} type="submit">
+            {i18n.t('userManagement.addUser')}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
