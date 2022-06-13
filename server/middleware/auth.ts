f8511dd85f5ecda3acba6c8ec0e6c064a266d899
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-06-13 17:33:07 +0200

    removed useless export and used AssessmentName cast inside the parameter list

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -14,33 +14,36 @@ const _next = (allowed: boolean, next: NextFunction): void => {
   return next(new Error(`userNotAuthorized`))
 }
 
-export const requireEdit = async (req: Request, _res: Response, next: NextFunction) => {
+const requireEdit = async (req: Request, _res: Response, next: NextFunction) => {
   const {
     countryIso,
     assessmentName,
     cycleName,
     section: sectionName,
   } = <Record<string, string>>{ ...req.params, ...req.query, ...req.body }
-  const name = <AssessmentName>assessmentName
   const user = Requests.getRequestUser(req)
 
-  const { cycle, assessment } = await AssessmentController.getOneWithCycle({ name, cycleName })
+  const { cycle, assessment } = await AssessmentController.getOneWithCycle({
+    name: assessmentName as AssessmentName,
+    cycleName,
+  })
   const section = await AssessmentController.getSection({ assessment, cycle, sectionName })
   const country = await AssessmentController.getCountry({ countryIso: countryIso as CountryIso, assessment, cycle })
 
   _next(Authorizer.canEdit({ user, section, countryIso: countryIso as CountryIso, country }), next)
 }
 
-export const requireView = async (req: Request, _res: Response, next: NextFunction) => {
+const requireView = async (req: Request, _res: Response, next: NextFunction) => {
   const { countryIso, assessmentName, cycleName } = <Record<string, string>>{ ...req.params, ...req.query }
   if (!countryIso || !assessmentName || !cycleName) {
     next(new Error(`missingParam ${JSON.stringify({ countryIso, assessmentName, cycleName })}`))
   }
-
-  const name = <AssessmentName>assessmentName
   const user = Requests.getRequestUser(req)
 
-  const { cycle, assessment } = await AssessmentController.getOneWithCycle({ name, cycleName })
+  const { cycle, assessment } = await AssessmentController.getOneWithCycle({
+    name: assessmentName as AssessmentName,
+    cycleName,
+  })
 
   _next(Authorizer.canView({ user, countryIso: countryIso as CountryIso, cycle, assessment }), next)
 }
