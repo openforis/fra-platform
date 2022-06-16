5fc1dab2eb69b6944172f80bd5586f6885188ac0
Miró Sorja <ping@mirosorja.com>
2022-06-16 18:09:16 +0300

    DataDownload: Initial commit (#1393)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ server/api/file/getDataDownloadFile.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+
+import { FileRepository, fileTypes } from '@server/service/file'
+
+type Query = {
+  fileName: string
+  fileType: string
+  language: string
+}
+
+export const getDataDownloadFile = async (req: Request, res: Response) => {
+  try {
+    const { fileName, fileType, language } = <Query>req.query
+    FileRepository.download(res, fileTypes.dataDownload(fileName, fileType), language)
+  } catch (err) {
+    res.status(500).send('An error occurred fetching the file.')
+  }
+}
