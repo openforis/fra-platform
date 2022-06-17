ffe28b793e2e2913faaf0e30f31043b2efa843a3
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-06-17 15:05:40 +0200

    removed useless isActive logic

--- client/components/CountrySelect/LinkLanding/LinkLanding.tsx
+++ client/components/CountrySelect/LinkLanding/LinkLanding.tsx
@@ -1,34 +1,27 @@
 import './linkLanding.scss'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import { useAssessment, useCycle } from '@client/store/assessment'
-import { useCountryIso, useCountryLandingSections } from '@client/hooks'
+import { useCountryIso } from '@client/hooks'
 import { BasePaths } from '@client/basePaths'
 import Icon from '@client/components/Icon'
 
 const LinkLanding: React.FC = () => {
   const { i18n } = useTranslation()
   const countryIso = useCountryIso()
-  const location = useLocation()
-  const sections = useCountryLandingSections()
   const assessment = useAssessment()
   const cycle = useCycle()
   const assessmentType = assessment?.props?.name
-
-  const isActive = (match: any) =>
-    match && (match.isExact || sections.find((section: any) => location.pathname.indexOf(section?.name) > 0))
 
   return (
     <NavLink
       to={BasePaths.Assessment.root(countryIso, assessmentType, cycle?.name)}
       className="country-selection-link-landing"
-      activeClassName="selected"
-      isActive={isActive}
     >
       <Icon name="icon-bar-chart" className="icon-sub icon-margin-right" />
-      <div className="nav__link-label">{i18n.t(`area.${countryIso}.listName`)}</div>
+      <div className="nav__link-label">{i18n.t<string>(`area.${countryIso}.listName`)}</div>
     </NavLink>
   )
 }
