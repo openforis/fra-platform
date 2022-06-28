c4c28e323e05a148354dac9f3cc6556243fa4812
minotogna <mino.togna@gmail.com>
2022-06-28 17:38:19 +0200

    add validations display

--- client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/Cell.tsx
+++ client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/Cell.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
-import { AssessmentName, Col, ColType, Row, Table } from '@meta/assessment'
+import { AssessmentName, Col, ColType, NodeValueValidations, Row, Table } from '@meta/assessment'
 import { TableData, TableDatas } from '@meta/data'
 
+import { useUser } from '@client/store/user'
 import { useCountryIso } from '@client/hooks'
+import Tooltip from '@client/components/Tooltip'
 
 import Calculated from './Calculated'
 import Number from './Number'
@@ -14,7 +17,7 @@ import Text from './Text'
 import useClassName from './useClassName'
 import useOnChange from './useOnChange'
 
-const ComponentsByName: Record<string, React.FC<PropsCell>> = {
+const Components: Record<string, React.FC<PropsCell>> = {
   [ColType.calculated]: Calculated,
   [ColType.text]: Text,
   [ColType.textarea]: Text,
@@ -37,38 +40,48 @@ type Props = {
 
 const Cell: React.FC<Props> = (props) => {
   const { data, assessmentName, sectionName, table, disabled, rowIndex, col, row } = props
+
   const countryIso = useCountryIso()
-  const params = {
-    data,
-    countryIso,
-    tableName: table.props.name,
-    variableName: row.props.variableName,
-    colName: col.props.colName,
-  }
+  const user = useUser()
+  const { t } = useTranslation()
+  const tableName = table.props.name
+  const { variableName } = row.props
+  const { colName } = col.props
+  const params = { data, countryIso, tableName, variableName, colName }
   const datum = TableDatas.getDatum(params)
   const nodeValue = TableDatas.getNodeValue(params)
-  const className = useClassName(col, row)
+  const valid = !user || NodeValueValidations.isValid(nodeValue)
 
-  const propsOnChange = { table, col, row, nodeValue, data }
+  const className = useClassName({ col, row, valid })
+  const { onChange, onPaste } = useOnChange({ table, col, row, nodeValue, data })
+  const Component = Components[col.props.colType]
 
-  const { onChange, onPaste } = useOnChange(propsOnChange)
-  const Component = ComponentsByName[col.props.colType]
+  if (!Component) return null
+
+  const ComponentInstance = (
+    <Component
+      assessmentName={assessmentName}
+      sectionName={sectionName}
+      table={table}
+      disabled={disabled || nodeValue?.odp}
+      rowIndex={rowIndex}
+      col={col}
+      row={row}
+      datum={datum}
+      onChange={onChange}
+      onPaste={onPaste}
+    />
+  )
 
   return (
     <td className={className}>
-      {Component &&
-        React.createElement(Component, {
-          datum,
-          assessmentName,
-          sectionName,
-          table,
-          disabled: disabled || nodeValue?.odp,
-          col,
-          rowIndex,
-          onChange,
-          row,
-          onPaste: disabled ? () => ({}) : onPaste,
-        })}
+      {valid ? (
+        ComponentInstance
+      ) : (
+        <Tooltip text={nodeValue.validation.messages.map(({ key, params }) => t(key, params)).join(`\n\r`)} error>
+          {ComponentInstance}
+        </Tooltip>
+      )}
     </td>
   )
 }
--- client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/useClassName.ts
+++ client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/useClassName.ts
@@ -1,21 +1,20 @@
 import { Col, Cols, ColType, Row } from '@meta/assessment'
 
-export default (col: Col, row: Row): string => {
-  const { colType /* validator */ } = col.props
-  // const user = useUser()
+type Props = {
+  col: Col
+  row: Row
+  valid: boolean
+}
 
-  const valid = true // TODO useSelector((state) => {
-  // if (!user /* || !validator */) {
-  //   return true
-  // }
-  // return false // validator(col.props.index as number, rowIdx)(state)
-  // })
+export default (props: Props): string => {
+  const { col, row, valid } = props
+  const { colType } = col.props
 
   let className = 'fra-table__cell'
   if (Cols.isReadOnly({ col, row })) className = 'fra-table__calculated-cell'
   if ([ColType.text, ColType.textarea, ColType.select].includes(colType)) className = 'fra-table__cell-left'
   if (colType === ColType.placeholder) className = 'fra-table__category-cell fra-table__filler-last'
+  if (!valid) className += ' validation-error'
 
-  className += valid ? '' : ' validation-error'
   return className
 }
