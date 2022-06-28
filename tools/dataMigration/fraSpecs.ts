d33e23072e0cae57818385af049d7ef03ad2aba7
minotogna <mino.togna@gmail.com>
2022-06-28 15:21:50 +0200

    fix forest ownership other_or_unknown calc formula

--- tools/dataMigration/fraSpecs.ts
+++ tools/dataMigration/fraSpecs.ts
@@ -5262,7 +5262,7 @@ export const FraSpecs: Record<string, SectionSpec> = {
                 variableName: 'other_or_unknown',
                 migration: {
                   colNames: ['1990', '2000', '2010', '2015'],
-                  calcFormula: `forestOwnership.private_ownership && forestOwnership.public_ownership 
+                  calcFormula: `forestOwnership.private_ownership || forestOwnership.public_ownership 
                     ? extentOfForest.forestArea - (forestOwnership.private_ownership || 0) - (forestOwnership.public_ownership || 0)
                     : null`,
                   validateFns: [`validatorGreaterThenOrZero(forestOwnership.other_or_unknown)`],
