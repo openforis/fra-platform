a88ef22fd753e81695ce0d57cc0f15b90a7176c7
minotogna <mino.togna@gmail.com>
2022-06-29 09:27:06 +0200

    fix cycle switcher value and style update

--- client/components/Navigation/NavAssessment/Header/Title/CycleSwitcher.tsx
+++ client/components/Navigation/NavAssessment/Header/Title/CycleSwitcher.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import './CycleSwitcher.scss'
+import React, { useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import { Authorizer } from '@meta/user'
@@ -12,32 +13,29 @@ import { BasePaths } from '@client/basePaths'
 const CycleSwitcher = () => {
   const countryIso = useCountryIso()
   const history = useHistory()
-  const cycle = useCycle()
+  const cycleCurrent = useCycle()
   const assessment = useAssessment()
   const user = useUser()
   const isDataLocked = useIsDataLocked()
 
-  const userCycles = assessment.cycles.filter((cycle) =>
-    Authorizer.canView({
-      countryIso,
-      user,
-      cycle,
-      assessment,
-    })
-  )
-
+  const userCycles = assessment.cycles.filter((cycle) => Authorizer.canView({ countryIso, user, cycle, assessment }))
   const canSwitchCycle = user && isDataLocked && userCycles.length > 1
 
-  if (!canSwitchCycle) return <span>{cycle.name}</span>
+  const onSelectChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      history.push(BasePaths.Assessment.home(countryIso, assessment.props.name, event.target.value))
+    },
+    [assessment.props.name, countryIso, history]
+  )
 
-  const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    history.push(BasePaths.Assessment.home(countryIso, assessment.props.name, event.target.value))
-  }
+  if (!canSwitchCycle) return <span>{cycleCurrent.name}</span>
 
   return (
-    <select onChange={onSelectChange}>
+    <select className="cycle-switcher" value={cycleCurrent.name} onChange={onSelectChange}>
       {assessment.cycles.map((cycle) => (
-        <option key={cycle.uuid}>{cycle.name}</option>
+        <option key={cycle.uuid} value={cycle.name}>
+          {cycle.name}
+        </option>
       ))}
     </select>
   )
