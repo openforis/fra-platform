c81140256693e8ba71c70786e274ccce22319430
minotogna <mino.togna@gmail.com>
2022-08-02 17:10:25 +0200

    fix sections fetch

--- client/components/Navigation/Navigation.tsx
+++ client/components/Navigation/Navigation.tsx
@@ -1,24 +1,15 @@
 import './Navigation.scss'
-import React, { useEffect } from 'react'
+import React from 'react'
 import MediaQuery from 'react-responsive'
 
 import { useCountryIso } from '@client/hooks'
 import { Breakpoints } from '@client/utils'
-import { AssessmentActions, useAssessment, useCycle } from '@client/store/assessment'
-import { useAppDispatch } from '@client/store'
 
 import NavigationDesktop from './NavigationDesktop'
 import NavigationMobile from './NavigationMobile'
 
 const Navigation: React.FC = () => {
   const countryIso = useCountryIso()
-  const cycle = useCycle()
-  const assessment = useAssessment()
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(AssessmentActions.getSections({ countryIso, name: assessment.props.name, cycleName: cycle.name }))
-  }, [])
 
   // admin view - navigation is not rendered
   if (!countryIso) return null
