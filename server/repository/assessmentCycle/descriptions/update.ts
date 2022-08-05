b531d346f567c7594eab15dcdfff4d9d1bb462ec
sorja <ping@mirosorja.com>
2022-08-05 09:37:27 +0300

    descriptions: Implement description edit

--- server/repository/assessmentCycle/descriptions/update.ts
+++ server/repository/assessmentCycle/descriptions/update.ts
@@ -21,12 +21,13 @@ export const update = async (
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
 
   const query = `
-          update ${schemaCycle}.descriptions set
-            content = $4
-          where country_iso = $1
-            and section_name = $2
-            and name = $3
-          returning content`
+      insert into
+          ${schemaCycle}.descriptions (country_iso, section_name, name, content)
+          values ($1, $2, $3, $4)
+      on conflict (country_iso, section_name, name) do update 
+            set content = $4
+      returning content;
+    `
 
   return client.oneOrNone<string>(query, [countryIso, sectionName, name, content], Objects.camelize)
 }
