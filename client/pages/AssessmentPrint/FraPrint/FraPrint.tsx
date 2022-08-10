f9b16777765e29024890d8d1c922ae0fb478cbad
sorja <ping@mirosorja.com>
2022-08-10 13:26:09 +0300

    FraPrint with content

--- client/pages/AssessmentPrint/FraPrint/FraPrint.tsx
+++ client/pages/AssessmentPrint/FraPrint/FraPrint.tsx
@@ -2,49 +2,36 @@ import './FraPrint.scss'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { AssessmentName } from '@meta/assessment'
-
 import { useAssessmentSections } from '@client/store/assessment'
 import { useCountryIso } from '@client/hooks'
-// import { Assessment, FRA } from '@core/assessment'
-// import * as CountryState from '@webapp/app/country/countryState'
-// import SectionView from '@webapp/components/Assessment/SectionView'
+import { useIsPrint } from '@client/hooks/useIsPath'
 import Loading from '@client/components/Loading'
-import AssessmentSection from '@client/pages/AssessmentSection'
+import AssessmentPrintSection from '@client/pages/AssessmentPrint/FraPrint/AssessmentPrintSection'
 
-// import ContactPersonsPrintView from '@webapp/sectionSpec/fra/contactPersons/contactPersonsPrintView'
+import ContactPersons from './ContactPersons'
 import TableOfContent from './TableOfContent'
-// import useFraPrintDataFetch from './useFraPrintDataFetch'
 
 const FraPrint: React.FC = () => {
   const countryIso = useCountryIso()
   const i18n = useTranslation()
 
   const sections = useAssessmentSections()
-  console.log(sections)
 
-  // eslint-disable-next-line no-sparse-arrays
-  const [, printOnlyTablesView] = [, false] // usePrintView()
+  const { onlyTables } = useIsPrint()
   // const { dataLoaded } = useFraPrintDataFetch(countryIso)
   // const assessment: Assessment = useSelector(CountryState.getAssessmentFra2020) as Assessment
   // const { deskStudy } = assessment
 
-  const deskStudy = true
-  const dataLoaded = true
+  const deskStudy = true // todo
 
-  if (!dataLoaded || !sections) {
+  if (!sections) {
     return <Loading />
   }
 
-  // @ts-ignore
-  window.isReadyForPDF = true
-
   let title = ''
-  if (printOnlyTablesView) title = i18n.t('fraReportPrint.titleTables')
-  if (!printOnlyTablesView && deskStudy) title = `${i18n.t('assessment.fra2020')} ${i18n.t('assessment.deskStudy')}`
-  if (!printOnlyTablesView && !deskStudy) title = i18n.t('fraReportPrint.title')
-
-  console.log(sections)
+  if (onlyTables) title = i18n.t('fraReportPrint.titleTables')
+  if (!onlyTables && deskStudy) title = `${i18n.t('assessment.fra')} ${i18n.t('assessment.deskStudy')}`
+  if (!onlyTables && !deskStudy) title = i18n.t('fraReportPrint.title')
 
   return (
     <div>
@@ -55,24 +42,25 @@ const FraPrint: React.FC = () => {
 
       <hr />
 
-      {!printOnlyTablesView && <TableOfContent deskStudy={deskStudy} />}
+      {!onlyTables && <TableOfContent deskStudy={deskStudy} />}
 
-      {Object.entries(sections).map(([key, section]) => (
-        <div key={section.uuid} id={`section${key}`}>
-          {!printOnlyTablesView && (
-            <h1 className="title only-print">
-              {Number(key) === 0 ? '' : key} {i18n.t<string>(section.props.labelKey)}
-            </h1>
-          )}
+      {Object.entries(sections).map(([key, section], i) => {
+        return (
+          <div key={section.uuid} id={`section${key}`}>
+            {!onlyTables && (
+              <h1 className="title only-print">
+                {i === 0 ? '' : key} {i18n.t<string>(section.props.labelKey)}
+              </h1>
+            )}
 
-          {key === '0' && !deskStudy && <pre>{`<ContactPersonsPrintView />`}</pre>}
+            {i === 0 && !deskStudy && <ContactPersons />}
 
-          {Object.values(section.subSections).map((sectionItem) => {
-            // return <p>{JSON.stringify(key)}</p>
-            return <AssessmentSection key={sectionItem.uuid} sectionNameProp={sectionItem.props.name} />
-          })}
-        </div>
-      ))}
+            {Object.values(section.subSections).map((sectionItem) => {
+              return <AssessmentPrintSection key={sectionItem.uuid} section={sectionItem} />
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }
