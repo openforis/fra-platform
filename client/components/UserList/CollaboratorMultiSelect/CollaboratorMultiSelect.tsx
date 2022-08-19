acbd3b786e040ee8ef2570f0ec41b8b9e1f7a5b8
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-08-19 11:26:28 +0200

    removed useless scss file

--- client/components/UserList/CollaboratorMultiSelect/CollaboratorMultiSelect.tsx
+++ client/components/UserList/CollaboratorMultiSelect/CollaboratorMultiSelect.tsx
@@ -1,4 +1,4 @@
-import './CollaboratorMultiSelect.scss'
+import '../../MultiSelect/MultiSelect.scss'
 import React, { useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
