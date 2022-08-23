7855619652f5ccd14d6e9ed8b5a3e9c796c7298f
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-08-23 08:56:40 +0200

    recent activity date

--- client/pages/AssessmentHome/FraHome/RecentActivity/RecentActivityItem/RecentActivityItem.tsx
+++ client/pages/AssessmentHome/FraHome/RecentActivity/RecentActivityItem/RecentActivityItem.tsx
@@ -1,9 +1,12 @@
 import './RecentActivityItem.scss'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { ApiEndPoint } from '@common/api/endpoint'
 
 import { ActivityLog } from '@meta/assessment'
+
+import { Dates } from '@client/utils'
 
 type Props = {
   activityLog: ActivityLog<any>
@@ -11,6 +14,7 @@ type Props = {
 
 const RecentActivityItem: React.FC<Props> = ({ activityLog }) => {
   const { user } = activityLog
+  const { i18n } = useTranslation()
 
   return (
     <div className="landing__activity-item">
@@ -22,6 +26,7 @@ const RecentActivityItem: React.FC<Props> = ({ activityLog }) => {
       <div className="landing__activity-name">
         <strong>{user.name}</strong>
       </div>
+      <div className="landing__activity-time">{Dates.getRelativeDate(activityLog.time, i18n)}</div>
     </div>
   )
 }
