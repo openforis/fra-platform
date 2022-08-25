dcf3f5fdbf0aa614088a546791ac56da986c9bfb
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-08-25 10:16:49 +0200

    renamed return variable, user CycleDataRequest instead of generic request, moved all the code under try/catch block

--- server/api/assessment/getActivities.ts
+++ server/api/assessment/getActivities.ts
@@ -1,25 +1,20 @@
-import { Request, Response } from 'express'
+import { Response } from 'express'
 
-import { CountryIso } from '@meta/area'
-import { AssessmentName } from '@meta/assessment'
+import { CycleDataRequest } from '@meta/api/request'
 
 import { AssessmentController } from '@server/controller/assessment'
 import { CycleDataController } from '@server/controller/cycleData'
 import Requests from '@server/utils/requests'
 
-export const getActivities = async (req: Request, res: Response) => {
-  const { countryIso, assessmentName, cycleName } = req.query as {
-    countryIso: CountryIso
-    assessmentName: AssessmentName
-    cycleName: string
-  }
-
+export const getActivities = async (req: CycleDataRequest, res: Response) => {
   try {
+    const { countryIso, assessmentName, cycleName } = req.query
+
     const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
 
-    const activityLog = await CycleDataController.getActivities({ countryIso, assessment, cycleUuid: cycle.uuid })
+    const activities = await CycleDataController.getActivities({ countryIso, assessment, cycleUuid: cycle.uuid })
 
-    Requests.send(res, activityLog)
+    Requests.send(res, activities)
   } catch (e) {
     Requests.sendErr(res, e)
   }
