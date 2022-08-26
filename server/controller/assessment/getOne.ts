57d304b88cfb6c548316bf5891e4eba4e43c3a42
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-08-26 11:17:07 +0200

    added way to get Assessment with Cycle by CycleUuid

--- server/controller/assessment/getOne.ts
+++ server/controller/assessment/getOne.ts
@@ -9,20 +9,27 @@ export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<A
   return AssessmentRepository.read(props, client)
 }
 
-const _getCycle = (props: { cycleName?: string; assessment: Assessment }): Cycle | undefined => {
+const _getCycleByName = (props: { cycleName?: string; assessment: Assessment }): Cycle | undefined => {
   const { cycleName, assessment } = props
   if (cycleName) return assessment.cycles.find((cycle) => cycle.name === cycleName)
   // Return default cycle if cycleName not defined
   return assessment.cycles.find((cycle) => cycle.uuid === assessment.props.defaultCycle)
 }
 
+const _getCycleByUuid = (props: { cycleUuid?: string; assessment: Assessment }): Cycle | undefined => {
+  const { cycleUuid, assessment } = props
+  if (cycleUuid) return assessment.cycles.find((cycle) => cycle.uuid === cycleUuid)
+  // Return default cycle if cycleName not defined
+  return assessment.cycles.find((cycle) => cycle.uuid === assessment.props.defaultCycle)
+}
+
 export const getOneWithCycle = async (
-  props: Props & { cycleName?: string },
+  props: Props & { cycleName?: string; cycleUuid?: string },
   client: BaseProtocol = DB
 ): Promise<{ assessment: Assessment; cycle: Cycle }> => {
-  const { cycleName } = props
+  const { cycleName, cycleUuid } = props
   const assessment = await AssessmentRepository.read(props, client)
-  const cycle = _getCycle({ cycleName, assessment })
+  const cycle = cycleName ? _getCycleByName({ cycleName, assessment }) : _getCycleByUuid({ cycleUuid, assessment })
 
   if (!cycle) {
     throw new Error(`Cycle ${cycleName} not found in assessment ${assessment.props.name}`)
