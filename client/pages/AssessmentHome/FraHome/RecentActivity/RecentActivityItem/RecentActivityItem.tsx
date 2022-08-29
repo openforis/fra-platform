8ddf192b2ee2037d95992a585091a98d3c1f6cb3
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-08-29 08:37:18 +0200

    bugfix

--- client/pages/AssessmentHome/FraHome/RecentActivity/RecentActivityItem/RecentActivityItem.tsx
+++ client/pages/AssessmentHome/FraHome/RecentActivity/RecentActivityItem/RecentActivityItem.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
-import { ApiEndPoint } from '@meta/api/endpoint'
+import classNames from 'classnames'
 
+import { ApiEndPoint } from '@meta/api/endpoint'
 import { ActivityLog, ActivityLogs } from '@meta/assessment'
 
 import { useAssessment, useCycle } from '@client/store/assessment'
 import { useCountryIso } from '@client/hooks'
 import { ClientRoutes } from '@client/clientRoutes'
 import { Dates } from '@client/utils'
-import classNames from 'classnames'
 
 type Props = {
   activity: ActivityLog<any>
 }
 
 const RecentActivityItem: React.FC<Props> = ({ activity }) => {
-  const { user, section } = activity
+  const { user, section: sectionName } = activity
 
   const countryIso = useCountryIso()
   const assessment = useAssessment()
@@ -29,7 +29,7 @@ const RecentActivityItem: React.FC<Props> = ({ activity }) => {
     <div className="landing__activity-item">
       <img
         className="landing__activity-avatar"
-        src={ApiEndPoint.User.getProfilePicture(String(user.id))}
+        src={ApiEndPoint.User.profilePicture(String(user.id))}
         alt={user.name}
       />
       <div className="landing__activity-name">
@@ -42,7 +42,7 @@ const RecentActivityItem: React.FC<Props> = ({ activity }) => {
               countryIso,
               assessmentName: assessment.props.name,
               cycleName: cycle.name,
-              section,
+              sectionName,
             })}
           >
             {ActivityLogs.getLabelSection(activity, i18n)}
