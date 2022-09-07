03f44f35f82cbbe26fa054e86e0a8d35e7e9d245
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-09-07 14:39:49 +0200

    added hardcoded links, scss style, global files

--- src/client/pages/AssessmentHome/FraHome/Links/Links.tsx
+++ src/client/pages/AssessmentHome/FraHome/Links/Links.tsx
@@ -1,13 +1,22 @@
+import './Links.scss'
 import React, { useEffect, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
-
-import { Objects } from '@utils/objects'
 
 import { ApiEndPoint } from '@meta/api/endpoint'
 import { AssessmentFile } from '@meta/assessment'
 
 import { useAssessment } from '@client/store/assessment'
 import { useCountryIso, useGetRequest } from '@client/hooks'
+
+const links = [
+  {
+    href: 'http://unfccc.int/parties_observers/parties/national_focal_points/items/9336.php',
+    key: 'unfcccFocalPoints',
+  },
+  { href: '/api/landing/sdgFocalPoints', key: 'sdgFocalPoints' },
+  { href: 'http://www.slms4redd.org', key: 'reddPortal' },
+  { href: 'https://goo.gl/aYJmzd', key: 'fraGeoSpatialTools' },
+]
 
 const Links: React.FC = () => {
   const countryIso = useCountryIso()
@@ -24,19 +33,36 @@ const Links: React.FC = () => {
 
   if (!data) return null
 
+  const countryFiles = data.filter((f) => f.countryIso === countryIso)
+  const globalFiles = data.filter((f) => !f.countryIso)
+
   return (
     <div className="landing__page-container">
-      <div className="landing__repository-header">
+      <div className="landing__page-container-header landing__repository-header">
         <h3>{i18n.t('landing.links.links')}</h3>
       </div>
-      {!Objects.isEmpty(data) ? (
-        data.map((assessmentFile, index) => {
+
+      {links.map((link) => (
+        <div key={link.key} className="landing__link-container">
+          <a href={link.href} target="_blank" rel="noreferrer">
+            {i18n.t(`landing.links.${link.key}`)}
+          </a>
+        </div>
+      ))}
+
+      {globalFiles.map((assessmentFile, index) => {
+        // eslint-disable-next-line react/no-array-index-key
+        return <div key={String(index)}>{assessmentFile.fileName}</div>
+      })}
+
+      <div className="landing__page-container-header landing__repository-header">
+        <h3>{i18n.t('landing.links.repository')}</h3>
+
+        {countryFiles.map((assessmentFile, index) => {
           // eslint-disable-next-line react/no-array-index-key
           return <div key={String(index)}>{assessmentFile.fileName}</div>
-        })
-      ) : (
-        <div>Empty!</div>
-      )}
+        })}
+      </div>
     </div>
   )
 }
