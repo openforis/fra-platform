013a73bd0f30ac51c187e3e6bc9753df643dc594
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-09-12 11:08:34 +0200

    used uuid instead of id to remove an assessment file

--- src/server/api/file/removeAssessmentFile.ts
+++ src/server/api/file/removeAssessmentFile.ts
@@ -8,13 +8,13 @@ import Requests from '@server/utils/requests'
 
 export const removeAssessmentFile = async (req: Request, res: Response) => {
   try {
-    const { id } = req.params
+    const { uuid } = req.params
 
     const { assessmentName } = req.query as { assessmentName: AssessmentName }
 
     const assessment = await AssessmentController.getOne({ assessmentName })
 
-    await FileController.removeAssessmentFile({ assessment, id: Number(id) })
+    await FileController.removeAssessmentFile({ assessment, uuid })
 
     Requests.sendOk(res)
   } catch (e) {
--- src/server/repository/assessment/file/remove.ts
+++ src/server/repository/assessment/file/remove.ts
@@ -3,12 +3,12 @@ import { Assessment } from '@meta/assessment'
 import { BaseProtocol, DB, Schemas } from '@server/db'
 
 export const remove = async (
-  props: { assessment: Assessment; id: number },
+  props: { assessment: Assessment; uuid: string },
   client: BaseProtocol = DB
 ): Promise<void> => {
-  const { assessment, id } = props
+  const { assessment, uuid } = props
 
   const schemaName = Schemas.getName(assessment)
 
-  await client.query(`delete from ${schemaName}.file where id = $1 returning *;`, [id])
+  await client.query(`delete from ${schemaName}.file where uuid = $1 returning *;`, [uuid])
 }
