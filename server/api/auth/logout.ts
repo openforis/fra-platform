84100b5bf8a47300132196476de80c4c6101f588
minotogna <mino.togna@gmail.com>
2022-09-16 17:51:52 +0200

    fix owl

--- server/api/auth/logout.ts
+++ server/api/auth/logout.ts
@@ -4,8 +4,7 @@ import { ApiEndPoint } from '@common/api/endpoint'
 export const AuthLogout = {
   init: (express: Express): void => {
     express.post(ApiEndPoint.Auth.logout(), (req: Request, res: Response): void => {
-      req.logout()
-      res.json({})
+      req.logout(() => res.json({}))
     })
   },
 }
