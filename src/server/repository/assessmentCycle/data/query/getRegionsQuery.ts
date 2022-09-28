174817da1a437672cfc25047e8734cbf9b138e05
Miró Sorja <ping@mirosorja.com>
2022-09-28 13:48:47 +0200

    Bulkdownload: Remove unused query

--- src/server/repository/assessmentCycle/data/query/getRegionsQuery.ts
+++
@@ -1,7 +0,0 @@
-export const getRegionsQuery = (schemaCycle: string) => {
-  return `
-     select cr.country_iso, array_to_string(ARRAY_AGG(distinct cr.region_code), ';') as regions
-     from ${schemaCycle}.country_region cr
-     group by cr.country_iso
-     `
-}
