3c30232e9ae19a20be0119eb44e1b8e335548010
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-09-29 14:25:50 +0200

    fixed tests

--- src/server/repository/public/user/getOne.ts
+++ src/server/repository/public/user/getOne.ts
@@ -43,10 +43,13 @@ export const getOne = async (
       data
         ? {
             ...Objects.camelize(data),
-            roles: data.roles.map(({ props, ...role }) => ({
-              ...Objects.camelize(role),
-              props: { ...Objects.camelize(props), sections: (props as CollaboratorProps).sections },
-            })),
+            roles:
+              data.roles && data.roles[0] !== null
+                ? data.roles.map(({ props, ...role }) => ({
+                    ...Objects.camelize(role),
+                    props: { ...Objects.camelize(props), sections: (props as CollaboratorProps).sections },
+                  }))
+                : data.roles,
           }
         : data
     )
