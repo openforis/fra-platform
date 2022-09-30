0da8a9bb8a2da08f4faa91bfa935651fa5b5f575
Miró Sorja <ping@mirosorja.com>
2022-09-30 10:11:11 +0200

    Taxon search api (#1566)
    * ext_data.taxon migration
    
    * Taxon: search api
    
    * order by scientific name
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/server/repository/extData/search.ts
@@ -0,0 +1,24 @@
+import { Objects } from '@utils/objects'
+
+import { Taxon } from '@meta/extData'
+
+import { BaseProtocol, DB } from '@server/db'
+
+export const search = async (
+  props: { query: string; limit: string },
+  client: BaseProtocol = DB
+): Promise<Array<Taxon>> => {
+  const { limit, query } = props
+
+  return client.map<Taxon>(
+    `
+        select *
+        from ext_data.taxon t
+        where t.scientific_name ilike '%$1:value%'
+        order by scientific_name
+        limit $2
+    `,
+    [query, limit],
+    (row) => Objects.camelize(row)
+  )
+}
