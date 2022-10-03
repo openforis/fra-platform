4a58086755b39720ce981a582dc6aa6e8fbd6e65
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-03 12:46:18 +0200

    activity_log moved into public schema, activity log migration

--- tools/dataMigration/migrateActivityLog.ts
+++ tools/dataMigration/migrateActivityLog.ts
@@ -1,16 +1,15 @@
 import { Cycle } from '../../src/meta/assessment'
 import { Assessment } from '../../src/meta/assessment/assessment'
 import { BaseProtocol } from '../../src/server/db'
-import { DBNames } from './_DBNames'
 
 export const migrateActivityLog = async (props: { assessment: Assessment }, client: BaseProtocol): Promise<void> => {
   const { assessment } = props
+
   const cycle = assessment.cycles.find((cycle: Cycle) => cycle.name === '2020')
 
-  const schemaName = DBNames.getAssessmentSchema(assessment.props.name)
   const query = `
-      insert into ${schemaName}.activity_log (
-          time, message, country_iso, section, target, user_id, cycle_uuid
+      insert into activity_log (
+          time, message, country_iso, section, target, user_id, assessment_uuid, cycle_uuid
       )
       select time,
           case
@@ -40,6 +39,7 @@ export const migrateActivityLog = async (props: { assessment: Assessment }, clie
        section,
        target,
        u.id,
+       '${assessment.uuid}' as assessment_uuid,
        '${cycle.uuid}' as cycle_uuid
         from _legacy.fra_audit a
                  join public.users u on (user_login_email = email)
