c790c842d174166a6c7ec73ef421c9944d89b6ec
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-04 19:10:34 +0200

    wrong branch

--- src/server/repository/public/user/getMany.ts
+++ src/server/repository/public/user/getMany.ts
@@ -22,15 +22,9 @@ export const getMany = async (
         select ${selectFields}, jsonb_agg(to_jsonb(ur.*)) as roles
         from public.users u
           join public.users_role ur on (u.id = ur.user_id)
-        where ur.assessment_id = $2
+        where ur.country_iso = $1
+          and ur.assessment_id = $2
           and ur.cycle_uuid = $3
-          and u.id in (
-            select user_id
-            from public.users_role ur
-            where ur.country_iso = $1
-              and ur.assessment_id = $2
-              and ur.cycle_uuid = $3
-            )
         group by ${selectFields}
     `,
       [countryIso, assessment.id, cycle.uuid]
