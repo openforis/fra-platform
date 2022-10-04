75776150678b7edcb4f90eab47f8841df7efd750
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-04 10:35:36 +0200

    turned json to jsonb

--- src/server/db/migration/migrations/sqls/20221003094432-table-activity-log-up.sql
+++ src/server/db/migration/migrations/sqls/20221003094432-table-activity-log-up.sql
@@ -6,7 +6,7 @@ create table activity_log
     message          text,
     country_iso      varchar(3) references public.country,
     section          varchar(250)                                   not null,
-    target           json,
+    target           jsonb,
     id               bigserial                                      not null,
     user_id          bigint not null references public.users (id)   on update cascade on delete cascade,
     assessment_uuid  uuid references public.assessment (uuid) on update cascade on delete cascade,
