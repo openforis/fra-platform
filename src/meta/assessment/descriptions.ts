96d58c7ffce252d24897405b8cec6d631c8e3a79
Miró Sorja <ping@mirosorja.com>
2022-10-07 13:06:44 +0200

    WIP: Initial commit for data sources

---
+++ src/meta/assessment/descriptions.ts
@@ -0,0 +1,9 @@
+// TODO export translation keys
+export const dataSourceTypes = [
+  'National Forest Inventory (NFI)',
+  'Remote sensing based assessment with ground truthing',
+  'Statistical remote sensing survey with accuracy assessment',
+  'Remote sensing based assessment without ground truthing',
+  'Vegetation mapping',
+  'Registers and statistics',
+]
