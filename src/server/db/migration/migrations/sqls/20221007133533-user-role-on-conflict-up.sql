2dc3a2a6fd8724a6e27b3b1dcf42544dae3df410
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-10 11:24:44 +0200

    role is not part of unique consrtaint

--- src/server/db/migration/migrations/sqls/20221007133533-user-role-on-conflict-up.sql
+++ src/server/db/migration/migrations/sqls/20221007133533-user-role-on-conflict-up.sql
@@ -1,3 +1,3 @@
 alter table public.users_role
-    add constraint user_assessment_cycle_country_role_key
-    unique (user_id, assessment_id, cycle_uuid, country_iso, role);
+    add constraint user_assessment_cycle_country_key
+    unique (user_id, assessment_id, cycle_uuid, country_iso);
