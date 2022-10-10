184f52741f05e8531d4aece5721697f66379f4f9
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-10 14:24:17 +0200

    moved migrateDescriptions under iteration

--- src/test/dataMigration/steps/migrateDescriptions.ts
+++ src/test/dataMigration/steps/migrateDescriptions.ts
@@ -1,14 +1,12 @@
-import { Assessment } from '@meta/assessment'
+import { Assessment, Cycle } from '@meta/assessment'
 
 import { BaseProtocol, Schemas } from '@server/db'
 
-type Props = {
-  assessment: Assessment
-}
-
-export const migrateDescriptions = async (props: Props, client: BaseProtocol): Promise<void> => {
-  const { assessment } = props
-  const cycle = assessment.cycles.find((c) => c.name === '2020')
+export const migrateDescriptions = async (
+  props: { assessment: Assessment; cycle: Cycle },
+  client: BaseProtocol
+): Promise<void> => {
+  const { assessment, cycle } = props
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
 
   await client.query(`
