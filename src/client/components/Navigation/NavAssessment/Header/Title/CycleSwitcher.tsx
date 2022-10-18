cd73ad85c89f2057900317d0969c7e9e42882c49
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-18 11:34:13 +0200

    used useIsDataExportView

--- src/client/components/Navigation/NavAssessment/Header/Title/CycleSwitcher.tsx
+++ src/client/components/Navigation/NavAssessment/Header/Title/CycleSwitcher.tsx
@@ -3,13 +3,12 @@ import React, { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { ClientRoutes } from '@meta/app'
-import { Areas } from '@meta/area'
 import { Authorizer } from '@meta/user'
 
 import { useAssessment, useCycle } from '@client/store/assessment'
 import { useIsDataLocked } from '@client/store/ui/dataLock'
 import { useUser } from '@client/store/user'
-import { useCountryIso } from '@client/hooks'
+import { useCountryIso, useIsDataExportView } from '@client/hooks'
 
 const CycleSwitcher = () => {
   const countryIso = useCountryIso()
@@ -18,11 +17,11 @@ const CycleSwitcher = () => {
   const assessment = useAssessment()
   const user = useUser()
   const isDataLocked = useIsDataLocked()
-  const isDataExport = countryIso && !Areas.isISOCountry(countryIso)
+  const isDataExportView = useIsDataExportView()
 
   const assessmentName = assessment.props.name
   const userCycles = assessment.cycles.filter((cycle) => Authorizer.canView({ countryIso, user, cycle, assessment }))
-  const canSwitchCycle = user && (isDataLocked || isDataExport) && userCycles.length > 1
+  const canSwitchCycle = user && (isDataLocked || isDataExportView) && userCycles.length > 1
 
   const onSelectChange = useCallback(
     (event: React.ChangeEvent<HTMLSelectElement>) => {
