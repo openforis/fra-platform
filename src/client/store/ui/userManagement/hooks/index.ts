d06ac2a330dc1b91b01aa6817daea877bd6f32c2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-10-21 10:51:33 +0200

    fixed userManagement hooks

--- src/client/store/ui/userManagement/hooks/index.ts
+++ src/client/store/ui/userManagement/hooks/index.ts
@@ -2,6 +2,6 @@ import { User } from '@meta/user'
 
 import { useAppSelector } from '@client/store/store'
 
-export const useUserToEdit = (): User => useAppSelector((state) => state.userManagement.userToEdit)
+export const useUserToEdit = (): User => useAppSelector((state) => state.ui.userManagement.user)
 
-export const useUsers = (): Array<User> => useAppSelector((state) => state.userManagement.users)
+export const useUsers = (): Array<User> => useAppSelector((state) => state.ui.userManagement.users)
