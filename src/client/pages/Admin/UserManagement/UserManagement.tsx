4aecda4f81f0d3dcf29dec276b3f433d1614dcfa
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-11-02 15:01:05 +0100

    moved UserListFilters out of UserList component

--- src/client/pages/Admin/UserManagement/UserManagement.tsx
+++ src/client/pages/Admin/UserManagement/UserManagement.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 import { useAppDispatch } from '@client/store'
 import { UserManagementActions, useUsers } from '@client/store/ui/userManagement'
 import UserList from '@client/components/UserList'
-import UserListFilters from '@client/components/UserList/UserListFilters'
+import UserListFilters from '@client/components/UserListFilters'
 
 const UserManagement: React.FC = () => {
   const dispatch = useAppDispatch()
