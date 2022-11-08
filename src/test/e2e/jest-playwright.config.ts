73b21ff28bb6bfc4b04901d90b36f729713d1b07
Miró Sorja <ping@mirosorja.com>
2022-11-08 16:06:32 +0100

    remove placeholders

--- src/test/e2e/jest-playwright.config.ts
+++ src/test/e2e/jest-playwright.config.ts
@@ -101,34 +101,3 @@ export default {
   //   port: 9000,
   // },
 } as PlaywrightTestConfig
-
-// Config imported from @openforis/arena
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { downloadsPath } = require('./paths')
-//
-// const headless = process.env.PWDEBUG !== '1'
-// // https://github.com/playwright-community/jest-playwright/#configuration
-// module.exports = {
-//   // browsers: ['chromium', 'firefox', 'webkit'],
-//   browsers: ['chromium'],
-//   exitOnPageError: true,
-//   launchOptions: {
-//     downloadsPath,
-//     headless,
-//     slowMo: headless ? null : 250,
-//   },
-//   contextOptions: {
-//     acceptDownloads: true,
-//     geolocation: {
-//       latitude: 41.890221,
-//       longitude: 12.492348,
-//     },
-//     locale: 'it-IT',
-//     timezoneId: 'Europe/Rome',
-//     viewport: {
-//       width: 1366,
-//       height: 768,
-//     },
-//   },
-// }
