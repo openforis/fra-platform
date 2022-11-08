d94fbb1a3f72795557789a86b75bc2f3429f0040
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-11-08 12:01:54 +0100

    added administrator role

--- src/client/pages/Admin/UserManagement/utils/roleNames.ts
+++ src/client/pages/Admin/UserManagement/utils/roleNames.ts
@@ -1,6 +1,7 @@
 import { RoleName } from '@meta/user'
 
 export const roleNames = [
+  RoleName.ADMINISTRATOR,
   RoleName.REVIEWER,
   RoleName.NATIONAL_CORRESPONDENT,
   RoleName.ALTERNATE_NATIONAL_CORRESPONDENT,
