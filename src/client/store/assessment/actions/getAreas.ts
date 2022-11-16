d945a5b80c554833d0530c58757fd58640766b7a
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-11-16 14:35:53 +0100

    added initApp params type

--- src/client/store/assessment/actions/getAreas.ts
+++ src/client/store/assessment/actions/getAreas.ts
@@ -5,10 +5,7 @@ import { ApiEndPoint } from '@meta/api/endpoint'
 import { Country, RegionGroup } from '@meta/area'
 
 export const getAreas = createAsyncThunk<
-  {
-    countries: Array<Country>
-    regionGroups: Record<string, RegionGroup>
-  },
+  { countries: Array<Country>; regionGroups: Record<string, RegionGroup> },
   { assessmentName: string; cycleName?: string } | void
 >('assessment/areas/get', async (params) => {
   const { data } = await axios.get(ApiEndPoint.Area.areas(), { params })
--- src/client/store/assessment/actions/initApp.ts
+++ src/client/store/assessment/actions/initApp.ts
@@ -5,14 +5,11 @@ import { ApiEndPoint } from '@meta/api/endpoint'
 import { Assessment } from '@meta/assessment'
 
 export const initApp = createAsyncThunk<
-  {
-    assessment: Assessment
-  },
-  void
+  { assessment: Assessment },
+  { assessmentName?: string; cycleName?: string } | void
 >('assessment/get/init', async (params) => {
   const { data } = await axios.get(ApiEndPoint.init(), { params })
   const lang = (await localStorage.getItem('i18n/lang')) ?? data?.user?.lang
   if (lang === 'ar') document.body.classList.add('rtl')
-
   return data
 })
