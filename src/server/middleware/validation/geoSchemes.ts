6d1f868fd2e8f26c7d62c6c87db3a178e053efa2
Eugenia Raviolo <eugenia.raviolo@gmail.com>
2022-11-16 10:26:01 -0300

    pass message key for required/invalid req params values

--- src/server/middleware/validation/geoSchemes.ts
+++ src/server/middleware/validation/geoSchemes.ts
@@ -2,21 +2,37 @@ import { query } from 'express-validator'
 
 import { ForestSource, precalForestAgreementSources } from '@meta/geo'
 
-const countryIsoQuery = query('countryIso').exists().bail().isLength({ min: 3, max: 3 })
+const countryIsoQuery = query('countryIso')
+  .exists()
+  .withMessage('validation.errors.requiredValue')
+  .bail()
+  .isLength({ min: 3, max: 3 })
+  .withMessage('validation.errors.invalidValue')
 
-const opacityQuery = query('opacity').default(1).isFloat({ min: 0, max: 1 }).bail().toFloat()
+const opacityQuery = query('opacity')
+  .default(1)
+  .isFloat({ min: 0, max: 1 })
+  .withMessage('validation.errors.invalidValue')
+  .toFloat()
 
 export const forestLayerSchema = [
   countryIsoQuery,
   opacityQuery,
 
-  query('forestSource').exists().bail().isIn(precalForestAgreementSources),
+  query('forestSource')
+    .exists()
+    .withMessage('validation.errors.requiredValue')
+    .bail()
+    .isIn(precalForestAgreementSources)
+    .withMessage('validation.errors.invalidValue'),
 
   query('gteHansenTreeCoverPerc')
     .if(query('forestSource').equals(ForestSource.Hansen))
     .exists()
+    .withMessage('validation.errors.requiredValue')
     .bail()
     .isInt({ min: 0, max: 100 })
+    .withMessage('validation.errors.invalidValue')
     .toInt(),
 
   query('onlyProtected').default(false).toBoolean(),
@@ -38,17 +54,19 @@ export const forestAgreementLayerSchema = [
       if (layer.length >= 2 && layer.every((layerId: any) => sourceOptions.includes(layerId))) {
         return true
       }
-      return Promise.reject()
+      return Promise.reject(Error('validation.errors.invalidValue'))
     }),
 
   query('gteAgreementLevel')
     .exists()
     .bail()
+    .withMessage('validation.errors.requiredValue')
     .isInt({ min: 1 })
+    .withMessage('validation.errors.invalidValue')
     .bail()
     .custom((value, { req }) => {
       if (value > req.query.layer.length) {
-        return Promise.reject()
+        return Promise.reject(Error('validation.errors.invalidValue'))
       }
       return true
     })
@@ -61,8 +79,11 @@ export const forestAgreementLayerSchema = [
       }
       return value
     })
-    .isInt({ min: 0, max: 100 })
+    .exists()
     .bail()
+    .withMessage('validation.errors.requiredValue')
+    .isInt({ min: 0, max: 100 })
+    .withMessage('validation.errors.invalidValue')
     .toInt(),
 ]
 
