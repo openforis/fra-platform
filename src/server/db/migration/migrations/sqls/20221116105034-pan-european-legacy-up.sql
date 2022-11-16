1d2a55cf858296063ccdcc495811fed48662a9c5
persas <diego.persas@gmail.com>
2022-11-16 16:11:29 +0100

    add if exists to avoid errors

--- src/server/db/migration/migrations/sqls/20221116105034-pan-european-legacy-up.sql
+++ src/server/db/migration/migrations/sqls/20221116105034-pan-european-legacy-up.sql
@@ -1,3 +1,13 @@
 /* Replace with your SQL commands */
+DO $$
+    BEGIN
 
-alter schema pan_european RENAME TO _legacy_pan_european;
+        IF EXISTS(
+                SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'pan_european'
+            )
+        THEN
+            alter schema pan_european  RENAME TO _legacy_pan_european;
+        END IF;
+
+    END
+$$;
