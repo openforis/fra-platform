772979d6ff5a955bad99c25dff4915d18e63775e
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-11-17 14:32:43 +0100

    post migration html->markdown converter

--- src/test/dataMigration/steps/migrateDescriptions.ts
+++ src/test/dataMigration/steps/migrateDescriptions.ts
@@ -1,6 +1,13 @@
+import * as TurndownService from 'turndown'
+// @ts-ignore
+import { tables } from 'turndown-plugin-gfm'
+
 import { Assessment, Cycle } from '@meta/assessment'
 
 import { BaseProtocol, Schemas } from '@server/db'
+
+const turndownService = new TurndownService()
+turndownService.use(tables)
 
 export const migrateDescriptions = async (
   props: { assessment: Assessment; cycle: Cycle },
@@ -9,11 +16,18 @@ export const migrateDescriptions = async (
   const { assessment, cycle } = props
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
 
-  await client.query(`
-      insert into ${schemaCycle}.descriptions (country_iso, section_name, name, value)
-      select d.country_iso, d.section, d.name,jsonb_build_object(
-              'text', d.content
-          )
-      from _legacy.descriptions d;
+  const descriptions = await client.many<any>(`
+    select d.country_iso, d.section, d.name, d.content
+    from _legacy.descriptions d;
   `)
+
+  await descriptions.forEach(async (description) => {
+    client.query(
+      `
+        insert into ${schemaCycle}.descriptions (country_iso, section_name, name, value)
+        values ($1, $2, $3, jsonb_build_object('text', $4))
+    `,
+      [description.country_iso, description.section, description.name, turndownService.turndown(description.content)]
+    )
+  })
 }
