0d14c8af816f77c461834a58da32f5fa509980e1
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-11-25 11:49:22 +0100

    added AuthProviderGoogleProps and AuthProviderLocalProps

--- src/server/repository/public/userProvider/read.ts
+++ src/server/repository/public/userProvider/read.ts
@@ -1,16 +1,17 @@
 import { Objects } from '@utils/objects'
 
 import { AuthProvider, User, UserAuthProvider } from '@meta/user'
+import { AuthProviderGoogleProps, AuthProviderLocalProps } from '@meta/user/userAuth'
 
 import { BaseProtocol, DB } from '@server/db'
 
 export const read = async (
   props: { user: User; provider: AuthProvider },
   client: BaseProtocol = DB
-): Promise<UserAuthProvider<{ password: string }>> => {
+): Promise<UserAuthProvider<AuthProviderGoogleProps | AuthProviderLocalProps>> => {
   const { user, provider } = props
 
-  return client.one<UserAuthProvider<{ password: string }>>(
+  return client.oneOrNone<UserAuthProvider<AuthProviderGoogleProps | AuthProviderLocalProps>>(
     `
         select * from  public.users_auth_provider uap where uap.user_id = $1 and provider = $2;
     `,
