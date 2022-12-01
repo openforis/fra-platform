eb3696dccbb5abb227794f55b8011e225e85b174
Miró Sorja <ping@mirosorja.com>
2022-12-01 13:24:20 +0100

    Add return type for hook

--- src/client/store/assessment/hooks/useSyncDataPage.ts
+++ src/client/store/assessment/hooks/useSyncDataPage.ts
@@ -6,14 +6,14 @@ import { AssessmentName } from '@meta/assessment'
 import { useAppDispatch, useAppSelector } from '@client/store'
 import { AssessmentActions, useAssessment, useAssessmentSections } from '@client/store/assessment'
 
-export const useSyncDataPage = () => {
+export const useSyncDataPage = (): boolean => {
   const dispatch = useAppDispatch()
   const assessment = useAssessment()
   const { assessmentName, cycleName } = useParams<{ assessmentName: AssessmentName; cycleName: string }>()
   const countries = useAppSelector((state) => state.assessment.countries)
   const assessmentSections = useAssessmentSections()
 
-  const appReady = assessment && countries && assessmentSections
+  const appReady = Boolean(assessment && countries && assessmentSections)
 
   useEffect(() => {
     dispatch(AssessmentActions.initApp({ assessmentName }))
