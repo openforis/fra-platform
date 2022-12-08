81c03db6a2ecc5410eed51cbe1d9040cc8afd541
minotogna <mino.togna@gmail.com>
2022-12-08 21:35:22 +0100

    add env PG_MAX_CONNECTIONS

--- src/server/utils/processEnv.ts
+++ src/server/utils/processEnv.ts
@@ -31,6 +31,7 @@ export const ProcessEnv = {
   pgHost,
   pgPort: Number(pgPort),
   pgDatabase,
+  pgMaxConnections: Number(process.env.PG_MAX_CONNECTIONS ?? 20),
   pgSsl: process.env.PGSSL === 'true',
 
   redisUrl: process.env.REDIS_URL ?? 'redis://127.0.0.1:6379',
