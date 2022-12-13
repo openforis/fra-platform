706a51c579d3ca224f37aa3feba287ffacb6e664
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-12-13 11:33:53 +0100

    full working download calculator endpoint

--- src/server/api/file/getBiomassStockFile.ts
+++ src/server/api/file/getBiomassStockFile.ts
@@ -1,31 +1,50 @@
 import * as fs from 'fs'
+import * as path from 'path'
 import { Objects } from '@utils/objects'
-import { Response } from 'express'
+import { Request, Response } from 'express'
 
-import { CycleRequest } from '@meta/api/request'
+import { CountryIso } from '@meta/area'
 
 import { Requests } from '@server/utils'
 
 const fileName = 'calculator'
 const availableLanguages = ['en', 'fr', 'es', 'ru']
 
-export const getBiomassStockFile = async (req: CycleRequest<{ invitationUuid: string }>, res: Response) => {
+export const getBiomassStockFile = async (
+  req: Request<never, never, never, { countryIso: CountryIso; language: string; selectedDomain: string }>,
+  res: Response
+) => {
   try {
-    const countryDomain = req.params.domain
+    const { countryIso, language, selectedDomain: countryDomain } = req.query
+
     if (Objects.isEmpty(countryDomain)) {
-      res.status(500).json({ error: `Could not find domain for country ${req.params.countryIso}` })
+      res.status(500).json({ error: `Could not find domain for country ${countryIso}` })
       return
     }
-    const lang = availableLanguages.includes(req.params.lang) ? req.params.lang : 'en'
-    const filePath = `${__dirname}/../../static/biomassStock/${fileName}_${countryDomain}_${lang}.xlsx`
-    const fallbackFilePath = `${__dirname}/../../static/biomassStock/${fileName}_${countryDomain}_en.xlsx`
+
+    const lang = availableLanguages.includes(language) ? language : 'en'
+
+    const filePath = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'static',
+      'biomassStock',
+      `${fileName}_${countryDomain}_${lang}.xlsx`
+    )
+    const fallbackFilePath = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'static',
+      'biomassStock',
+      `${fileName}_${countryDomain}_en.xlsx`
+    )
 
     if (fs.existsSync(filePath)) {
       res.download(filePath, 'BiomassCalculator.xlsx')
-    } else if (lang !== 'en' && fs.existsSync(fallbackFilePath)) {
-      res.download(fallbackFilePath, 'BiomassCalculator.xlsx')
     } else {
-      res.status(404).send('404 / File not found')
+      res.download(fallbackFilePath, 'BiomassCalculator.xlsx')
     }
   } catch (err) {
     Requests.sendErr(res, err)
