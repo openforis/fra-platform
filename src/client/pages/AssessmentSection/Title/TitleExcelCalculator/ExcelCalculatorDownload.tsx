9804b0b87cf4a39aaba190d38087dae0987a7788
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-12-13 12:42:10 +0100

    used fileRepository

--- src/client/pages/AssessmentSection/Title/TitleExcelCalculator/ExcelCalculatorDownload.tsx
+++ src/client/pages/AssessmentSection/Title/TitleExcelCalculator/ExcelCalculatorDownload.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Objects } from '@utils/objects'
-
 import { ApiEndPoint } from '@meta/api/endpoint'
 
 import { useCountry } from '@client/store/assessment'
@@ -11,36 +9,35 @@ import { useCountryIso } from '@client/hooks'
 
 const domains: Array<string> = ['boreal', 'temperate', 'tropical', 'subtropical']
 
-const getDownloadPath = (countryIso: string, language: string, selectedDomain: string): string =>
-  `${ApiEndPoint.File.biomassStock()}?countryIso=${countryIso}&language=${language}&selectedDomain=${selectedDomain}`
+const getDownloadPath = (language: string, selectedDomain: string): string =>
+  `${ApiEndPoint.File.biomassStock()}?&language=${language}&selectedDomain=${selectedDomain}`
 
 const ExcelCalculatorDownload: React.FC = () => {
   const countryIso = useCountryIso()
   const { i18n, t } = useTranslation()
   const userInfo = useUser()
   const countryDomain = useCountry(countryIso)?.props?.domain ?? 'boreal'
+
   const [selectedDomain, setSelectedDomain] = useState<string>(countryDomain)
-  const calculatorFilePath = getDownloadPath(countryIso, i18n.language, selectedDomain)
+  const calculatorFilePath = getDownloadPath(i18n.language, selectedDomain)
 
   if (!userInfo) return null
 
   return (
     <div className="no-print">
-      {!Objects.isEmpty(countryDomain) && (
-        <select
-          className="select-s"
-          defaultValue={countryDomain}
-          value={selectedDomain}
-          onChange={(e) => setSelectedDomain(e.target.value)}
-        >
-          {domains.map((domain) => (
-            <option value={domain} key={domain}>
-              {t(`climaticDomain.${domain}`)}
-              {domain === countryDomain && ` (${t('climaticDomain.selectDefault')})`}
-            </option>
-          ))}
-        </select>
-      )}
+      <select
+        className="select-s"
+        defaultValue={countryDomain}
+        value={selectedDomain}
+        onChange={(e) => setSelectedDomain(e.target.value)}
+      >
+        {domains.map((domain) => (
+          <option value={domain} key={domain}>
+            {t(`climaticDomain.${domain}`)}
+            {domain === countryDomain && ` (${t('climaticDomain.selectDefault')})`}
+          </option>
+        ))}
+      </select>
       <a className="btn-s btn-primary" href={calculatorFilePath}>
         {t('biomassStock.downloadExcel')}
       </a>
--- src/server/api/file/getBiomassStockFile.ts
+++ src/server/api/file/getBiomassStockFile.ts
@@ -1,51 +1,16 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { Objects } from '@utils/objects'
 import { Request, Response } from 'express'
 
-import { CountryIso } from '@meta/area'
-
+import { FileRepository, fileTypes } from '@server/service/file'
 import { Requests } from '@server/utils'
 
-const fileName = 'calculator'
-const availableLanguages = ['en', 'fr', 'es', 'ru']
-
 export const getBiomassStockFile = async (
-  req: Request<never, never, never, { countryIso: CountryIso; language: string; selectedDomain: string }>,
+  req: Request<never, never, never, { language: string; selectedDomain: string }>,
   res: Response
 ) => {
   try {
-    const { countryIso, language, selectedDomain: countryDomain } = req.query
+    const { language, selectedDomain: domain } = req.query
 
-    if (Objects.isEmpty(countryDomain)) {
-      res.status(500).json({ error: `Could not find domain for country ${countryIso}` })
-      return
-    }
-
-    const lang = availableLanguages.includes(language) ? language : 'en'
-
-    const filePath = path.resolve(
-      __dirname,
-      '..',
-      '..',
-      'static',
-      'biomassStock',
-      `${fileName}_${countryDomain}_${lang}.xlsx`
-    )
-    const fallbackFilePath = path.resolve(
-      __dirname,
-      '..',
-      '..',
-      'static',
-      'biomassStock',
-      `${fileName}_${countryDomain}_en.xlsx`
-    )
-
-    if (fs.existsSync(filePath)) {
-      res.download(filePath, 'BiomassCalculator.xlsx')
-    } else {
-      res.download(fallbackFilePath, 'BiomassCalculator.xlsx')
-    }
+    FileRepository.download(res, fileTypes.biomassStock(domain), language)
   } catch (err) {
     Requests.sendErr(res, err)
   }
