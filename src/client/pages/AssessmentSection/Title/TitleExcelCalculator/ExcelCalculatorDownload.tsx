9412c81aca487dbe04fc56da783764ef20a73bbb
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-12-13 11:50:33 +0100

    fixed countryDomain

--- src/client/pages/AssessmentSection/Title/TitleExcelCalculator/ExcelCalculatorDownload.tsx
+++ src/client/pages/AssessmentSection/Title/TitleExcelCalculator/ExcelCalculatorDownload.tsx
@@ -3,24 +3,26 @@ import { useTranslation } from 'react-i18next'
 
 import { Objects } from '@utils/objects'
 
+import { ApiEndPoint } from '@meta/api/endpoint'
+
+import { useCountry } from '@client/store/assessment'
 import { useUser } from '@client/store/user'
 import { useCountryIso } from '@client/hooks'
 
 const domains: Array<string> = ['boreal', 'temperate', 'subtropical', 'tropical']
-const getDownloadPath = (countryIso: string, selectedDomain: string, language: string): string =>
-  `/api/biomassStock/${countryIso}/${selectedDomain}/${language}/download`
+
+const getDownloadPath = (countryIso: string, language: string, selectedDomain: string): string =>
+  `${ApiEndPoint.File.biomassStock()}?countryIso=${countryIso}&language=${language}&selectedDomain=${selectedDomain}`
 
 const ExcelCalculatorDownload: React.FC = () => {
   const countryIso = useCountryIso()
   const { i18n, t } = useTranslation()
   const userInfo = useUser()
-  const countryDomain = 'climaticDomain' // TODO useSelector(CountryState.getConfigDomain) as string
+  const countryDomain = useCountry(countryIso)?.props?.domain ?? 'tropical'
   const [selectedDomain, setSelectedDomain] = useState<string>(countryDomain)
-  const calculatorFilePath = getDownloadPath(countryIso, selectedDomain, i18n.language)
+  const calculatorFilePath = getDownloadPath(countryIso, i18n.language, selectedDomain)
 
-  if (!userInfo) {
-    return null
-  }
+  if (!userInfo) return null
 
   return (
     <div className="no-print">
