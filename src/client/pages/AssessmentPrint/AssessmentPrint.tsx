4e8b4b10248e9b1efc55fbe0157d286bf5b56f4f
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2022-12-22 17:16:06 +0100

    remove useless getSections

--- src/client/pages/AssessmentPrint/AssessmentPrint.tsx
+++ src/client/pages/AssessmentPrint/AssessmentPrint.tsx
@@ -1,11 +1,9 @@
 import './style.scss'
-import React, { memo, useEffect } from 'react'
+import React, { memo } from 'react'
 
 import { AssessmentNames } from '@meta/assessment'
 
-import { useAppDispatch } from '@client/store'
-import { AssessmentActions, useAssessment, useCycle } from '@client/store/assessment'
-import { useCountryIso } from '@client/hooks'
+import { useAssessment } from '@client/store/assessment'
 import Loading from '@client/components/Loading'
 
 import FraPrint from './FraPrint'
@@ -15,18 +13,7 @@ const Components: Record<string, React.FC> = {
 }
 
 const AssessmentPrint: React.FC = () => {
-  const dispatch = useAppDispatch()
   const assessment = useAssessment()
-  const cycle = useCycle()
-  const countryIso = useCountryIso()
-
-  useEffect(() => {
-    if (assessment && cycle) {
-      dispatch(
-        AssessmentActions.getSections({ countryIso, assessmentName: assessment.props.name, cycleName: cycle.name })
-      )
-    }
-  }, [assessment, countryIso, cycle, dispatch])
 
   if (!assessment) {
     return <Loading />
