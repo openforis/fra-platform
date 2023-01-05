7cbc2fe00dc70cf217397c2b98c67aa0e3f85582
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-01-05 19:08:45 +0100

    fixed DeskStyudy label visibility

--- src/client/components/Navigation/NavAssessment/Header/Header.tsx
+++ src/client/components/Navigation/NavAssessment/Header/Header.tsx
@@ -2,10 +2,7 @@ import './Header.scss'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Areas } from '@meta/area'
-
 import { useCountry } from '@client/store/assessment'
-import { useUser } from '@client/store/user'
 import { useCountryIso, useIsDataExportView } from '@client/hooks'
 
 import ButtonToggleAll from './ButtonToggleAll'
@@ -20,22 +17,18 @@ const Header: React.FC<Props> = (props) => {
   const countryIso = useCountryIso()
   const country = useCountry(countryIso)
 
-  const user = useUser()
   const { t } = useTranslation()
 
-  const isCountry = Areas.isISOCountry(countryIso)
   const isDataExportView = useIsDataExportView()
 
   const { deskStudy } = country.props
 
   return (
     <div className="nav-assessment-header">
-      {user && isCountry && (
-        <div>
-          {isDataExportView ? ` - ${t('common.dataExport')}` : ''}
-          {deskStudy && <div className="desk-study">({t('assessment.deskStudy')})</div>}
-        </div>
-      )}
+      <div>
+        {isDataExportView ? ` - ${t('common.dataExport')}` : ''}
+        {deskStudy && <div className="desk-study">({t('assessment.deskStudy')})</div>}
+      </div>
 
       <ButtonToggleAll showSections={showSections} setShowSections={setShowSections} />
     </div>
