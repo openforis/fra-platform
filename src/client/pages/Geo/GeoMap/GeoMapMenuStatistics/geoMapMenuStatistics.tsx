703f3ef634e29aa4b45fd1702290b6f8c60646a8
Yhoan Alejandro Guzman Garcia <yhoan.alejandro@Yhoans-MacBook-Pro.local>
2023-01-10 19:55:14 +0100

    Adding a graph panel to visualize forest estimations (#1746)
    Added a panel with a chart that shows the area per source and
    its percentage relative to the land area. It also includes a
    bar with the forest area and its percentage relative to the total
    land area.

--- src/client/pages/Geo/GeoMap/GeoMapMenuStatistics/geoMapMenuStatistics.tsx
+++ src/client/pages/Geo/GeoMap/GeoMapMenuStatistics/geoMapMenuStatistics.tsx
@@ -7,6 +7,7 @@ import { getForestEstimationData } from '@client/pages/Geo/utils/forestEstimatio
 
 import GeoMapMenuButton from '../GeoMapMenuButton'
 import GeoMenuItem from '../GeoMapMenuItem'
+import StatisticalGraphsPanel from './StatisticalGraphsPanel'
 import TreeCoverAreaPanel from './TreeCoverAreaPanel'
 
 const GeoMapMenuStatistics: React.FC = () => {
@@ -45,6 +46,15 @@ const GeoMapMenuStatistics: React.FC = () => {
           <GeoMenuItem title="Tree Cover Area" checked={null} tabIndex={-1}>
             {!isLoading && statisticsData.length > 0 && (
               <TreeCoverAreaPanel data={statisticsData} countryIso={countryIso} year={year} />
+            )}
+            {!isLoading && statisticsData.length === 0 && !error && <p>Found no data.</p>}
+            {!isLoading && error && <p>An error has occured while fetching the statistics: {error}</p>}
+            {isLoading && <p>Loading...</p>}
+          </GeoMenuItem>
+          <div className="geo-map-menu-separator" />
+          <GeoMenuItem title="Statistical Graphs" checked={null} tabIndex={-3}>
+            {!isLoading && statisticsData.length > 0 && (
+              <StatisticalGraphsPanel data={statisticsData} countryIso={countryIso} year={year} />
             )}
             {!isLoading && statisticsData.length === 0 && !error && <p>Found no data.</p>}
             {!isLoading && error && <p>An error has occured while fetching the statistics: {error}</p>}
