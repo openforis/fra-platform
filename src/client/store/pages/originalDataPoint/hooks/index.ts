514be8e75609fa0809c88ec75526d06531fb37b7
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-01-11 10:51:23 +0100

    added new hook to dynamically get odp max year

--- src/client/store/pages/originalDataPoint/hooks/index.ts
+++ src/client/store/pages/originalDataPoint/hooks/index.ts
@@ -1,3 +1,7 @@
+import { Arrays } from '@utils/arrays'
+
+import { Cycle } from '@meta/assessment'
+
 import { useAppSelector } from '@client/store'
 
 export const useOriginalDataPoint = () => useAppSelector((state) => state.pages.originalDataPoint?.data)
@@ -6,3 +10,13 @@ export const useIsOriginalDataPointUpdating = () => useAppSelector((state) => st
 
 export const useOriginalDataPointReservedYears = () =>
   useAppSelector((state) => state.pages.originalDataPoint.reservedYears)
+
+export const useODPYears = (cycle: Cycle): { years: Array<number>; reservedYears: Array<number> } => {
+  const years = cycle ? Arrays.reverse(Arrays.range(1950, Number(cycle.name))) : []
+  const reservedYears = useOriginalDataPointReservedYears() ?? []
+
+  return {
+    years,
+    reservedYears,
+  }
+}
