ad15a88cf9dc896a17f19b1d9c17eb5dc29a1f77
Miró Sorja <ping@mirosorja.com>
2023-01-11 17:41:01 +0100

    faq/taq by cycle, fix 7c -> 7a

--- src/server/api/definitions/get.ts
+++ src/server/api/definitions/get.ts
@@ -1,14 +1,17 @@
-import { Response, Request } from 'express'
-import { getHtml } from '@server/api/definitions/utils/getHtml'
-import { readParameterWithAllowedValues, readAllowedParameter } from '../../utils/sanityChecks'
+import { Request, Response } from 'express'
 
-export const getDefition = async (req: Request, res: Response) => {
+import { getHtml } from '@server/api/definitions/utils/getHtml'
+
+import { readAllowedParameter, readParameterWithAllowedValues } from '../../utils/sanityChecks'
+
+export const getDefinition = async (req: Request, res: Response) => {
   try {
     const lang = readParameterWithAllowedValues(req, 'lang', ['en', 'es', 'fr', 'ru', 'ar', 'zh'])
     const name = readAllowedParameter(req, 'name', (x) => /^[a-z0-9]+$/i.test(x))
+    const { assessmentName, cycleName } = req.params
 
     try {
-      const html = await getHtml(lang, name)
+      const html = await getHtml({ assessmentName, cycleName, lang, name })
 
       res.send(html)
     } catch (err) {
