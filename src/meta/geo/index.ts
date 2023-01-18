6604339c2fea4c6daae7a2fbc0186947689000a0
eraviolo <eugenia.raviolo@gmail.com>
2023-01-18 08:27:49 -0300

    Geo: get estimated area property name for selected combination of layers  (#2034)
    * add precalculated forest agreement recipes
    
    * change search from for to find

--- src/meta/geo/index.ts
+++ src/meta/geo/index.ts
@@ -1,6 +1,12 @@
 export type { Layer } from './forest'
 export type { ForestOptions } from './forest'
-export { agreementPalette, ForestSource, precalForestAgreementSources, sourcesMetadata } from './forest'
+export {
+  agreementPalette,
+  ForestSource,
+  getRecipeAgreementAreaProperty,
+  precalForestAgreementSources,
+  sourcesMetadata,
+} from './forest'
 export type { ForestEstimations, ForestEstimationsData } from './forestEstimations'
 export type { MosaicOptions, MosaicSource } from './mosaic'
 export type { MapPanel } from './ui'
