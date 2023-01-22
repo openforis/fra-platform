622d46253e8fbf8e41bcf1e63c69366aaf8615e9
minotogna <mino.togna@gmail.com>
2023-01-22 16:09:26 +0100

    notify client on node updates after commit

--- src/server/controller/cycleData/updateDependencies/updateNodeDependencies.ts
+++ src/server/controller/cycleData/updateDependencies/updateNodeDependencies.ts
@@ -1,4 +1,4 @@
-import { NodeUpdate, NodeUpdates } from '@meta/data'
+import { NodeUpdates } from '@meta/data'
 
 import { BaseProtocol } from '@server/db'
 
@@ -7,13 +7,12 @@ import { updateCalculationDependencies } from './updateCalculationDependencies'
 import { updateValidationDependencies } from './updateValidationDependencies'
 
 export const updateNodeDependencies = async (
-  props: Omit<PersistNodeValueProps, 'value'> & { sourceNode?: NodeUpdate; isODP?: boolean },
+  props: Omit<PersistNodeValueProps, 'value'> & { isODP?: boolean },
   client: BaseProtocol
 ): Promise<{ nodeUpdates: NodeUpdates; validations: NodeUpdates }> => {
-  const { isODP, sourceNode } = props
+  const { isODP } = props
 
   const nodeUpdates = await updateCalculationDependencies(props, client)
-  if (sourceNode) nodeUpdates.nodes.unshift(sourceNode)
 
   const validations = await updateValidationDependencies({ nodeUpdates, isODP }, client)
 
