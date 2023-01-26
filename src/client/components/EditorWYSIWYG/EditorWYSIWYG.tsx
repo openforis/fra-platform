36f3a1b1dc393638c8e33a130af0d5b8348b5c9f
Miró Sorja <ping@mirosorja.com>
2023-01-26 10:39:18 +0100

    Remove buttons from WYSIWYG

--- src/client/components/EditorWYSIWYG/EditorWYSIWYG.tsx
+++ src/client/components/EditorWYSIWYG/EditorWYSIWYG.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useRef } from 'react'
 
+import type { Jodit } from 'jodit/types/jodit'
 import JoditEditor from 'jodit-react'
 
 type Props = {
@@ -7,13 +8,17 @@ type Props = {
   onChange: (value?: string) => void
 }
 
+const removeButtons = ['image', 'video', 'eraser', 'copyformat', 'fullsize', 'print', 'about']
+
 const EditorWYSIWYG: React.FC<Props> = (props: Props) => {
   const { onChange, value } = props
   const editor = useRef(null)
 
-  const config = useMemo(
+  const config = useMemo<Partial<Jodit['options']>>(
     () => ({
-      readonly: false, // all options from https://xdsoft.net/jodit/doc/,
+      readonly: false, // all options from https://xdsoft.net/jodit/doc/
+      removeButtons,
+      uploader: undefined,
     }),
     []
   )
