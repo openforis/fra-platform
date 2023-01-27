378ff3f36859aa39def794b5436bc2d25a375dd1
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-01-27 05:44:45 +0200

    Make recipe selector a separate component

--- src/client/pages/Geo/GeoMap/GeoMapMenuData/MapVisualizerPanel/MapVisualizerPanel.tsx
+++ src/client/pages/Geo/GeoMap/GeoMapMenuData/MapVisualizerPanel/MapVisualizerPanel.tsx
@@ -16,6 +16,30 @@ import AgreementLevelsControl from '../MapVisualizerAgreementLevelsControl'
 import LayerOptionsPanel from './LayerOptionsPanel'
 import { layers } from '.'
 
+const RecipeSelector: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const forestOptions = useForestSourceOptions()
+
+  return (
+    <div>
+      <p>Recipes</p>
+      <select
+        value={forestOptions.recipe}
+        onChange={(e) => {
+          dispatch(GeoActions.setRecipe(e.target.value))
+        }}
+      >
+        <option value="custom">custom</option>
+        {forestAgreementRecipes.map((recipe) => (
+          <option key={recipe.forestAreaDataProperty} value={recipe.forestAreaDataProperty}>
+            {recipe.forestAreaDataProperty}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
 const MapVisualizerPanel: React.FC = () => {
   const dispatch = useAppDispatch()
   const forestOptions = useForestSourceOptions()
@@ -72,20 +96,7 @@ const MapVisualizerPanel: React.FC = () => {
 
   return (
     <div className="geo-map-menu-data-visualizer-panel">
-      <p>Recipes</p>
-      <select
-        value={forestOptions.recipe}
-        onChange={(e) => {
-          dispatch(GeoActions.setRecipe(e.target.value))
-        }}
-      >
-        <option value="custom">custom</option>
-        {forestAgreementRecipes.map((recipe) => (
-          <option key={recipe.forestAreaDataProperty} value={recipe.forestAreaDataProperty}>
-            {recipe.forestAreaDataProperty}
-          </option>
-        ))}
-      </select>
+      <RecipeSelector />
       <p>Forest Layers</p>
       <div className="geo-map-menu-data-visualizer-panel-layers">
         {layers.map((layer, index) => {
