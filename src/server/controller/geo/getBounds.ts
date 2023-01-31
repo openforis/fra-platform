738fe9c996e5d63b7a1df9a42f236a289d58e5ec
eraviolo <eugenia.raviolo@gmail.com>
2023-01-31 06:52:38 -0300

    GEO: country bounds to center map (#2104)
    * add bounds import
    
    * add bounds endpoint
    
    * move bounds api endpoint
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/server/controller/geo/getBounds.ts
@@ -0,0 +1,11 @@
+import { CountryIso } from '@meta/area'
+import { Bounds } from '@meta/geo'
+
+import { BaseProtocol, DB } from '@server/db'
+import { BoundsRepository } from '@server/repository/geo/bounds'
+
+type Props = { countryIso: CountryIso }
+
+export const getBounds = async (props: Props, client: BaseProtocol = DB): Promise<Bounds> => {
+  return BoundsRepository.getOne(props, client)
+}
