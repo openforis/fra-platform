193c76a489848fe8ad5fb8574421d732f58a5bca
Miró Sorja <ping@mirosorja.com>
2023-01-31 15:40:47 +0200

    Generic ODP fixes: labels, calculations, first year empty (#2109)
    * Generic ODP fixes: labels, calculations, first year empty
    
    * Use existing translation and move null check for ODP to Country for country
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/client/pages/Country/Country.tsx
+++ src/client/pages/Country/Country.tsx
@@ -10,7 +10,7 @@ import { Sockets } from '@meta/socket'
 import { Authorizer } from '@meta/user'
 
 import { useAppDispatch } from '@client/store'
-import { AssessmentActions, useAssessment, useCycle } from '@client/store/assessment'
+import { AssessmentActions, useAssessment, useCountry, useCycle } from '@client/store/assessment'
 import { useNavigationVisible } from '@client/store/ui/navigation'
 import { ReviewActions } from '@client/store/ui/review'
 import { useUser } from '@client/store/user'
@@ -36,6 +36,7 @@ const Country: React.FC = () => {
   const countryIso = useCountryIso()
   const assessment = useAssessment()
   const cycle = useCycle()
+  const country = useCountry(countryIso)
   const isDataExportView = useIsDataExportView()
   useGetUsers()
 
@@ -72,6 +73,7 @@ const Country: React.FC = () => {
   }, [countryIso, assessmentName, cycleName, user, dispatch])
 
   if (!countryIso) return null
+  if (!country) return null
 
   if (!Authorizer.canView({ countryIso, assessment, cycle, user })) window.location.href = ClientRoutes.Root.path
 
