69805a6adad5efea2660f3cc6609c874af69594d
Miró Sorja <ping@mirosorja.com>
2023-02-07 12:13:16 +0200

    Fix/typo in login (#2167)
    * Fix typo in login check
    
    * Remove uuid check from jwt

--- src/server/api/auth/strategy/jwt.ts
+++ src/server/api/auth/strategy/jwt.ts
@@ -16,15 +16,11 @@ const jwtFromRequest = (req: Request) => {
 const jwtStrategyVerifyCallback = async (_req: Request, { uuid }: User, done: VerifiedCallback) => {
   const sendErr = (message: string) => done(null, false, { message })
   try {
-    if (!uuid) {
-      throw new Error('login.invalidToken')
-    }
-
     const user = await UserController.getOne({ uuid })
 
     return done(null, user)
   } catch (e) {
-    return sendErr(e.message ?? 'login.noUser')
+    return sendErr('login.noUser')
   }
 }
 
