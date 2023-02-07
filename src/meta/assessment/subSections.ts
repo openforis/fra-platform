2cd804a794645d82e24aa6c18a41c1e0c4b8d186
Miró Sorja <ping@mirosorja.com>
2023-02-07 11:48:36 +0100

    Use indexes to fetch previous subsection

--- src/meta/assessment/subSections.ts
+++ src/meta/assessment/subSections.ts
@@ -22,21 +22,16 @@ const getAnchorsByUuid = (props: { cycle: Cycle; sections: Array<Section> }): Re
 
 const getPrevious = (props: { subSection: SubSection; sections: Array<Section> }): SubSection => {
   const { subSection, sections } = props
-  const subsections = sections.reduce<Array<SubSection>>((acc, section) => [...acc, ...section.subSections], [])
-  const previousSubsection = subsections.findIndex((s) => s.id === subSection.id) - 1
-  return subsections[previousSubsection]
-}
-
-const getNext = (props: { subSection: SubSection; sections: Array<Section> }): SubSection => {
-  const { subSection, sections } = props
-  const subsections = sections.reduce<Array<SubSection>>((acc, section) => [...acc, ...section.subSections], [])
-  const nextSubsection = subsections.findIndex((s) => s.id === subSection.id) + 1
-  return subsections[nextSubsection]
+  const sectionIsFirstOfParent = subSection.props.index === 0
+  const parentIndex = sections.findIndex((s) => s.id === subSection.parentId)
+  if (sectionIsFirstOfParent) {
+    return sections[parentIndex - 1].subSections[sections[parentIndex - 1].subSections.length - 1]
+  }
+  return sections[parentIndex].subSections[subSection.props.index - 1]
 }
 
 export const SubSections = {
   getAnchor,
   getAnchorsByUuid,
   getPrevious,
-  getNext,
 }
