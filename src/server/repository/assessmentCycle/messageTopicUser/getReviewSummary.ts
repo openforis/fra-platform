ffffac343723f0e628ce410cd45bee8fd73bb084
Miró Sorja <ping@mirosorja.com>
2023-02-12 16:07:19 +0200

    DataSources: Review indicator fix (#2199)
    * add uui for dataSources
    
    * Update summary and status queries for review indicator
    
    * Fix copy uuids, hide review in placeholder datasourc

--- src/server/repository/assessmentCycle/messageTopicUser/getReviewSummary.ts
+++ src/server/repository/assessmentCycle/messageTopicUser/getReviewSummary.ts
@@ -17,50 +17,57 @@ export const getReviewSummary = async (
 
   return client.one<Array<ReviewSummary>>(
     `
-        with r as (
-            select r.uuid as row_uuid, s.id as sub_section_id, s.parent_id
-            from ${schemaName}.row r
-                     left join ${schemaName}."table" t
-                               on t.id = r.table_id
-                     left join ${schemaName}.table_section ts
-                               on ts.id = t.table_section_id
-                     left join ${schemaName}.section s
-                               on s.id = ts.section_id
-            where r.props -> 'cycles' ? $1
-        ),
-             m as (
-                 select r.row_uuid,
-                        r.sub_section_id,
-                        r.parent_id,
-                        m.topic_id,
-                        m.created_time                                                as last_message_created_time,
-                        row_number() over (partition by r.row_uuid, r.sub_section_id) as row_number
-                 from r
-                          left join ${cycleSchema}.message_topic mt
-                                    on r.row_uuid::varchar = mt.key
-                          left join ${cycleSchema}.message m
-                                    on m.topic_id = mt.id
-                 where mt.status = 'opened'
-                   and not m.deleted
-                   and mt.country_iso = $3
-             ),
-             summaries as (
-                 select m.sub_section_id,
-                        m.parent_id,
-                        m.row_uuid                                     as key,
-                        mt.status,
-                        m.last_message_created_time,
-                        u.last_open_time,
-                        u.last_open_time is null
-                            or
-                        m.last_message_created_time > u.last_open_time as has_unread_messages
-                 from m
-                          left join ${cycleSchema}.message_topic_user u
-                                    on u.topic_id = m.topic_id and u.user_id = $2
-                          left join ${cycleSchema}.message_topic mt
-                                    on mt.id = m.topic_id
-                 where m.row_number = 1
-             )
+        with data_source_uuids as (select country_iso,
+                                          section_name,
+                                          jsonb_array_elements(value -> 'dataSources') ->> 'uuid' as uuid
+                                   from ${cycleSchema}.descriptions d
+                                   where d.value -> 'dataSources' is not null),
+             data_source_rows as (select dsr.uuid::uuid as row_uuid,
+                                         s.id           as sub_section_id,
+                                         s.parent_id
+                                  from data_source_uuids dsr
+                                           left join ${schemaName}.section s on s.props ->> 'name' = dsr.section_name),
+             r as (select r.uuid as row_uuid, s.id as sub_section_id, s.parent_id
+                   from ${schemaName}.row r
+                            left join ${schemaName}."table" t
+                                      on t.id = r.table_id
+                            left join ${schemaName}.table_section ts
+                                      on ts.id = t.table_section_id
+                            left join ${schemaName}.section s
+                                      on s.id = ts.section_id
+                   where r.props -> 'cycles' ? '5883400e-988d-45e5-9b65-fff6715c25b7'
+                   union all
+                   select *
+                   from data_source_rows),
+             m as (select r.row_uuid,
+                          r.sub_section_id,
+                          r.parent_id,
+                          m.topic_id,
+                          m.created_time                                                as last_message_created_time,
+                          row_number() over (partition by r.row_uuid, r.sub_section_id) as row_number
+                   from r
+                            left join ${cycleSchema}.message_topic mt
+                                      on r.row_uuid::varchar = mt.key
+                            left join ${cycleSchema}.message m
+                                      on m.topic_id = mt.id
+                   where mt.status = 'opened'
+                     and not m.deleted
+                     and mt.country_iso = $3),
+             summaries as (select m.sub_section_id,
+                                  m.parent_id,
+                                  m.row_uuid                                     as key,
+                                  mt.status,
+                                  m.last_message_created_time,
+                                  u.last_open_time,
+                                  u.last_open_time is null
+                                      or
+                                  m.last_message_created_time > u.last_open_time as has_unread_messages
+                           from m
+                                    left join ${cycleSchema}.message_topic_user u
+                                              on u.topic_id = m.topic_id and u.user_id = $2
+                                    left join ${cycleSchema}.message_topic mt
+                                              on mt.id = m.topic_id
+                           where m.row_number = 1)
         select jsonb_agg(s.*) as data
         from summaries s
     `,
