5c8dac3c8f85af20958e59c38b51bc52a6fe9ad1
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-02-15 09:20:37 +0100

    redirect to / instead of redirecting to the current cycle (#2224)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/client/store/login/actions/localLogin.ts
+++ src/client/store/login/actions/localLogin.ts
@@ -1,11 +1,9 @@
 import { NavigateFunction } from 'react-router-dom'
 
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import { Objects } from '@utils/objects'
 import axios from 'axios'
 
 import { ApiEndPoint } from '@meta/api/endpoint'
-import { ClientRoutes } from '@meta/app'
 
 import { initApp } from '@client/store/assessment/actions/initApp'
 
@@ -18,21 +16,11 @@ export const localLogin = createAsyncThunk<
     navigate: NavigateFunction
   }
 >('login/post/local', async ({ email, password, invitationUuid, navigate }, { dispatch }) => {
-  const { data, status } = await axios.post(
-    ApiEndPoint.Auth.login(),
-    { email, password },
-    { params: { invitationUuid } }
-  )
+  const { status } = await axios.post(ApiEndPoint.Auth.login(), { email, password }, { params: { invitationUuid } })
 
   if (status === 200) {
     dispatch(initApp()).then(() => {
-      let redirectUrl = '/'
-
-      if (!Objects.isEmpty(data)) {
-        redirectUrl = ClientRoutes.Assessment.Cycle.Country.Home.Root.getLink(data)
-      }
-
-      navigate(redirectUrl)
+      navigate('/')
     })
   }
 })
