c184807e04b1ed45ddf789a500446b1b51e50953
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-02-15 13:44:26 +0200

    Move mosaic state up
    The issue before was that it required the Background popup to be open.
    Now the popup can be open or closed and the mosaics still load.

--- src/client/pages/Geo/GeoMap/GeoMapMenuMosaic/GeoMapMenuMosaic.tsx
+++ src/client/pages/Geo/GeoMap/GeoMapMenuMosaic/GeoMapMenuMosaic.tsx
@@ -1,13 +1,63 @@
 import './GeoMapMenuMosaic.scss'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-import { useSelectedPanel } from '@client/store/ui/geo'
+import { useAppDispatch } from '@client/store'
+import {
+  GeoActions,
+  useAppliedMosaicOptions,
+  useMosaicSelected,
+  useMosaicUrl,
+  useSelectedPanel,
+} from '@client/store/ui/geo'
+import { useCountryIso, useGeoMap } from '@client/hooks'
+import { MapController } from '@client/utils'
 
 import GeoMapMenuButton from '../GeoMapMenuButton'
 import MosaicControl from './MosaicControl'
 
 const GeoMapMenuMosaic: React.FC = () => {
+  const dispatch = useAppDispatch()
   const selectedPanel = useSelectedPanel()
+  const map = useGeoMap()
+  const mapControllerRef = useRef<MapController>(null)
+  const mosaicSelected = useMosaicSelected()
+  const mosaicLayerKey = 'mosaic'
+  const countryIso = useCountryIso()
+  const mosaicUrl = useMosaicUrl(countryIso)
+  const appliedMosaicOptions = useAppliedMosaicOptions()
+
+  // Reset MapController when map is loaded
+  useEffect(() => {
+    if (map) {
+      mapControllerRef.current = new MapController(map)
+    }
+  }, [map])
+
+  // Mosaic layer toggled, mosaicUrl updated or appliedMosaicOptions changed
+  useEffect(() => {
+    // console.log(mosaicSelected, appliedMosaicOptions, mosaicUrl, countryIso)
+    // If map is still loading, stop
+    if (!map) return
+
+    // In any case, if there's an existing background layer, it should be removed
+    mapControllerRef.current.removeLayer(mosaicLayerKey)
+
+    // Mosaic layer not selected, so do nothing
+    if (!mosaicSelected) {
+      return
+    }
+
+    // Use existing mosaic url if available to avoid unnecessary Sepal calls
+    if (mosaicUrl) {
+      mapControllerRef.current.addSepalLayer(mosaicLayerKey, mosaicUrl)
+      return
+    }
+
+    // Get mosaic url from Sepal
+    if (appliedMosaicOptions.sources.length > 0) {
+      dispatch(GeoActions.postMosaicOptions({ mosaicOptions: appliedMosaicOptions, countryIso }))
+    }
+  }, [mosaicSelected, appliedMosaicOptions, mosaicUrl, countryIso, map, dispatch])
 
   return (
     <div className="geo-map-menu-item">
--- src/client/pages/Geo/GeoMap/GeoMapMenuMosaic/MosaicControl/MosaicControl.tsx
+++ src/client/pages/Geo/GeoMap/GeoMapMenuMosaic/MosaicControl/MosaicControl.tsx
@@ -1,46 +1,15 @@
 import './MosaicControl.scss'
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 
 import { useAppDispatch } from '@client/store'
-import { GeoActions, useAppliedMosaicOptions, useMosaicSelected, useMosaicUrl } from '@client/store/ui/geo'
-import { useCountryIso, useGeoMap } from '@client/hooks'
-import { MapController } from '@client/utils'
+import { GeoActions, useMosaicSelected } from '@client/store/ui/geo'
 
 import GeoMenuItem from '../../GeoMapMenuItem'
 import SatelliteSourcePanel from '../SatelliteSourcePanel'
 
 const MosaicControl: React.FC = () => {
   const dispatch = useAppDispatch()
-  const appliedMosaicOptions = useAppliedMosaicOptions()
   const mosaicSelected = useMosaicSelected()
-  const map = useGeoMap()
-  const mapControllerRef = useRef<MapController>(new MapController(map))
-  const mosaicLayerKey = 'mosaic'
-  const countryIso = useCountryIso()
-  const mosaicUrl = useMosaicUrl(countryIso)
-
-  // Mosaic layer toggled, mosaicUrl updated or appliedMosaicOptions changed
-  useEffect(() => {
-    // console.log(mosaicSelected, appliedMosaicOptions, mosaicUrl, countryIso)
-    // In any case, if there's an existing background layer, it should be removed
-    mapControllerRef.current.removeLayer(mosaicLayerKey)
-
-    // Mosaic layer not selected, so do nothing
-    if (!mosaicSelected) {
-      return
-    }
-
-    // Use existing mosaic url if available to avoid unnecessary Sepal calls
-    if (mosaicUrl) {
-      mapControllerRef.current.addSepalLayer(mosaicLayerKey, mosaicUrl)
-      return
-    }
-
-    // Get mosaic url from Sepal
-    if (appliedMosaicOptions.sources.length > 0) {
-      dispatch(GeoActions.postMosaicOptions({ mosaicOptions: appliedMosaicOptions, countryIso }))
-    }
-  }, [mosaicSelected, appliedMosaicOptions, mosaicUrl, countryIso, dispatch])
 
   return (
     <div>
