094663a482d9136d82d3dca14c060b9e3ebb1138
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-02-15 12:52:09 +0100

    2215 - Mandatory fields (#2216)
    * removed some not mandatory fields from checking
    
    * mandatory fields evidence
    
    * fixed bug when contact method is empty
    
    * added (* are mandatory fields) message
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/client/components/EditUserForm/TextInputField/TextInputField.tsx
+++ src/client/components/EditUserForm/TextInputField/TextInputField.tsx
@@ -14,10 +14,11 @@ type Props = {
   onChange: (name: string, value: string) => void
   validator?: (partial: Partial<User> | Partial<UserProps>) => boolean
   enabled?: boolean
+  mandatory?: boolean
 }
 
 const TextInputField: React.FC<Props> = (props) => {
-  const { name, value, onChange, validator, enabled } = props
+  const { name, value, onChange, validator, enabled, mandatory } = props
 
   const { t } = useTranslation()
 
@@ -25,7 +26,10 @@ const TextInputField: React.FC<Props> = (props) => {
 
   return (
     <div className="edit-user__form-item" key={name}>
-      <div className="edit-user__form-label">{t(`editUser.${name}`)}</div>
+      <div className="edit-user__form-label">
+        {t(`editUser.${name}`)}
+        {mandatory && '*'}
+      </div>
       <div className={classNames(`edit-user__form-field${enabled ? '' : '-disabled'}`, { error: !valid })}>
         <TextInput
           value={value}
@@ -40,6 +44,7 @@ const TextInputField: React.FC<Props> = (props) => {
 TextInputField.defaultProps = {
   validator: undefined,
   enabled: false,
+  mandatory: false,
 }
 
 export default TextInputField
