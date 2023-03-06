0dd0d7427f9d360c879cd5b886c4c5839be17814
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-03-06 15:59:07 +0100

    added link to edit country role in administration page

--- src/client/components/UserList/AdministrationListElement/AdministrationListElement.tsx
+++ src/client/components/UserList/AdministrationListElement/AdministrationListElement.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
-// import { useTranslation } from 'react-i18next'
-// import { Link } from 'react-router-dom'
 import classNames from 'classnames'
 
-// import { ClientRoutes } from '@meta/app'
 import { RoleName, User, Users, UserStatus } from '@meta/user'
 
 import { useFilteredRoleNames } from '@client/store/ui/userManagement'
@@ -14,10 +11,6 @@ import UserRolesField from '../UserRolesField'
 
 const AdministrationListElement: React.FC<{ user: User }> = ({ user }) => {
   const filteredRoleNames = useFilteredRoleNames()
-
-  // const { t } = useTranslation()
-
-  // const { id, status } = user
 
   return (
     <tr
@@ -32,11 +25,7 @@ const AdministrationListElement: React.FC<{ user: User }> = ({ user }) => {
         <UserRolesField key={roleName} roleName={roleName} user={user} />
       ))}
       <UserField user={user} field="email" />
-      <td className="user-list__cell user-list__edit-column">
-        {/* <Link to={ClientRoutes.Admin.User.getLink({ id })} type="button" className="link">
-          {t('userManagement.edit')}
-        </Link> */}
-      </td>
+      <td className="user-list__cell user-list__edit-column" />
     </tr>
   )
 }
--- src/client/components/UserList/UserRolesField/UserRolesField.tsx
+++ src/client/components/UserList/UserRolesField/UserRolesField.tsx
@@ -1,23 +1,42 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
+import { ClientRoutes } from '@meta/app'
 import { Areas } from '@meta/area'
 import { RoleName, User, Users } from '@meta/user'
 
+import { useAssessment, useCycle } from '@client/store/assessment'
+
 const UserRolesField: React.FC<{ roleName: RoleName; user: User }> = ({ roleName, user }) => {
+  const assessment = useAssessment()
+  const cycle = useCycle()
+
   const { t } = useTranslation()
 
   return (
     <td className="user-list__cell">
-      <div className="user-list__cell--read-only">
+      <div className="user-list__cell--roles">
         {user.roles
           .filter((role) => role.role === roleName)
-          .map((role) =>
-            role.role === RoleName.ADMINISTRATOR
-              ? t(Users.getI18nRoleLabelKey(role.role))
-              : t(Areas.getTranslationKey(role.countryIso))
-          )
-          .join(', ')}
+          .map((role) => {
+            if (role.role === RoleName.ADMINISTRATOR) return t(Users.getI18nRoleLabelKey(role.role))
+            if (role.invitedAt && !role.acceptedAt) return t(Areas.getTranslationKey(role.countryIso))
+            return (
+              <Link
+                to={ClientRoutes.Assessment.Cycle.Country.Users.User.getLink({
+                  countryIso: role.countryIso,
+                  assessmentName: assessment.props.name,
+                  cycleName: cycle.name,
+                  id: user.id,
+                })}
+                type="button"
+                className="link"
+              >
+                {t(Areas.getTranslationKey(role.countryIso))}
+              </Link>
+            )
+          })}
       </div>
     </td>
   )
