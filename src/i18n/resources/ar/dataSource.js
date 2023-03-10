568eb36e31405a2d3f37b26a22106d039e35e21b
sorja <ping@mirosorja.com>
2023-03-10 14:33:49 +0200

    Add missing translations: ar

---
+++ src/i18n/resources/ar/dataSource.js
@@ -0,0 +1,14 @@
+module.exports = {
+  referenceToTataSource: 'مرجع لمصدر البيانات',
+  typeOfDataSource: 'نوع مصدر البيانات',
+  fraVariable: 'متغير (فرا)',
+  variable: 'المتغير',
+  yearForDataSource: 'سنة لمصدر البيانات',
+  comments: 'التعليقات',
+  nationalForestInventory: 'الجرد الوطني للغابات (NFI)',
+  remoteSensingBasedAssessmentWithGroundTruthing: 'التقييم القائم على الاستشعار عن بعد مع التحقيق السطحي',
+  statisticalRemoteSensingSurveyWithAccuracyAssessment: 'المسح الإحصائي بالاستشعار عن بعد مع تقييم الدقة',
+  remoteSensingBasedAssessmentWithoutGroundTruthing: 'التقييم القائم على الاستشعار عن بعد دون التحقيق السطحي',
+  vegetationMapping: 'رسم خرائط الغطاء النباتي',
+  registersAndStatistics: 'السجلات والإحصاءات',
+}
