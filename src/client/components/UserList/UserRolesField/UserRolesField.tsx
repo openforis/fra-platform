d22456cd47913d3f7e6b27f2a1075b1672c9c2e6
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-03-16 10:51:10 +0100

    long country list now uses tooltip

--- src/client/components/UserList/UserRolesField/UserRolesField.tsx
+++ src/client/components/UserList/UserRolesField/UserRolesField.tsx
@@ -4,20 +4,25 @@ import { useTranslation } from 'react-i18next'
 import { Areas } from '@meta/area'
 import { RoleName, User, Users } from '@meta/user'
 
+import Tooltip from '@client/components/Tooltip'
+
 const UserRolesField: React.FC<{ roleName: RoleName; user: User }> = ({ roleName, user }) => {
   const { t } = useTranslation()
+
+  const roles = user.roles
+    .filter((role) => role.role === roleName)
+    .map((role) =>
+      role.role === RoleName.ADMINISTRATOR
+        ? t(Users.getI18nRoleLabelKey(role.role))
+        : t(Areas.getTranslationKey(role.countryIso))
+    )
 
   return (
     <td className="user-list__cell">
       <div className="user-list__cell--read-only">
-        {user.roles
-          .filter((role) => role.role === roleName)
-          .map((role) =>
-            role.role === RoleName.ADMINISTRATOR
-              ? t(Users.getI18nRoleLabelKey(role.role))
-              : t(Areas.getTranslationKey(role.countryIso))
-          )
-          .join(', ')}
+        <Tooltip text={roles.join(', ')}>
+          {roles.length > 3 ? `${roles.slice(0, 3).join(', ')}...` : roles.join(', ')}
+        </Tooltip>
       </div>
     </td>
   )
