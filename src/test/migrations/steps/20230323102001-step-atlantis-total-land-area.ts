962ffb3a20903ac2882679f985c7c5a8d24e7cc5
sorja <ping@mirosorja.com>
2023-03-23 11:00:35 +0200

    Constant value atlantis total land area

--- src/test/migrations/steps/20230323102001-step-atlantis-total-land-area.ts
+++ src/test/migrations/steps/20230323102001-step-atlantis-total-land-area.ts
@@ -5,30 +5,8 @@ import { BaseProtocol, Schemas } from '@server/db'
 
 export default async (client: BaseProtocol) => {
   const years = Array.from({ length: 2025 - 1950 + 1 }, (_, i) => i + 1950)
+  const countryIsos = Array.from({ length: 20 }, (_, i) => `X${i < 9 ? '0' : ''}${i + 1}`)
 
-  // Random values picked from the original data
-  const totalLandAreaValues = {
-    X01: '11276',
-    X02: '112859.9766',
-    X03: '1217',
-    X04: '13996',
-    X05: '13996',
-    X06: '1827',
-    X07: '18378',
-    X08: '2805',
-    X09: '30459',
-    X10: '4239',
-    X11: '4831',
-    X12: '51089',
-    X13: '5439',
-    X14: '5439',
-    X15: '5596.43',
-    X16: '7102',
-    X17: '7727',
-    X18: '924',
-    X19: '952.3',
-    X20: '66991',
-  }
   const tableName = 'extentOfForest'
   const variableName = 'totalLandArea'
 
@@ -60,8 +38,10 @@ export default async (client: BaseProtocol) => {
     }
   )
 
+  const value = '4412.0'
+
   const values = years.flatMap((year) => {
-    return Object.entries(totalLandAreaValues).flatMap(([countryIso, value]) => {
+    return countryIsos.flatMap((countryIso) => {
       return {
         country_iso: countryIso,
         table_name: tableName,
