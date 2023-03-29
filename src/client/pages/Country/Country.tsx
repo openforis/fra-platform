c5d03401f784bbc46478fd719fa26234edeb1a7a
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-03-29 20:55:31 +0200

    reverted change

--- src/client/pages/Country/Country.tsx
+++ src/client/pages/Country/Country.tsx
@@ -76,8 +76,6 @@ const Country: React.FC = () => {
 
   if (!countryIso) return null
 
-  if (!country) return null
-
   if ((Areas.isISOCountry(countryIso) && !country) || !Authorizer.canView({ countryIso, assessment, cycle, user }))
     window.location.href = ClientRoutes.Assessment.Cycle.Landing.getLink({ assessmentName, cycleName })
 
