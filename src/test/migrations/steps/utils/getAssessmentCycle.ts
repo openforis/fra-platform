69a42af02900fe1374dd13bf2c6c0c894a703985
sorja <ping@mirosorja.com>
2023-04-04 17:10:57 +0300

    map datasource freetext to constant

---
+++ src/test/migrations/steps/utils/getAssessmentCycle.ts
@@ -0,0 +1,12 @@
+import { Assessment, AssessmentNames, Cycle } from '@meta/assessment'
+
+import { AssessmentController } from '@server/controller/assessment'
+import { BaseProtocol } from '@server/db'
+
+const getFra2025 = async (client: BaseProtocol): Promise<{ assessment: Assessment; cycle: Cycle }> => {
+  return AssessmentController.getOneWithCycle({ assessmentName: AssessmentNames.fra, cycleName: '2025' }, client)
+}
+
+export const AssessmentCycleUtil = {
+  getFra2025,
+}
