266a2491475bd349d1aa5cedfbca5b47e6977904
sorja <ping@mirosorja.com>
2023-04-25 10:42:24 +0300

    Update DataSource type definition

--- src/meta/assessment/description/dataSourceVariables.ts
+++ src/meta/assessment/description/dataSourceVariables.ts
@@ -1,5 +1,8 @@
 import { Label, VariableName } from '@meta/assessment'
 
+/**
+ * @deprecated
+ */
 export interface DataSourceVariables {
   // Custom i18n keys to include
   include: Array<string>
--- src/meta/assessment/description/nationalDataDataSourceDescription.ts
+++ src/meta/assessment/description/nationalDataDataSourceDescription.ts
@@ -1,7 +1,37 @@
+import { Label } from '@meta/assessment'
 import { DataSourceColumn } from '@meta/assessment/description/dataSourceColumn'
 import { DataSourceVariables } from '@meta/assessment/description/dataSourceVariables'
 
+/**
+ * @deprecated
+ */
 export interface NationalDataDataSourceDescription {
   table?: { columns: Array<DataSourceColumn>; dataSourceVariables?: DataSourceVariables }
+  text?: { readOnly?: boolean }
+}
+
+/*
+* "dataSources": {
+    "text": {
+      "readOnly": true
+    },
+    "table": {
+      variables: [ { value: 'natural_expansion'}, { value: 'afforestation'}, { value: 'forest_expansion'}, { value: 'common.otherSpecifyInComments', label: { key: 'common.otherSpecifyInComments' } } ],
+      typeOfDataSource: 'dataSourceType',
+    }
+  }
+* */
+
+export interface DataSourceVariable {
+  value: string
+  label?: Label
+  prefixLabel?: Label
+}
+
+export interface DataSourceDescription {
+  table?: {
+    variables: Array<DataSourceVariable>
+    typeOfDataSourceText?: boolean
+  }
   text?: { readOnly?: boolean }
 }
