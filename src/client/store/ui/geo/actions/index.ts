6de710ee6f3735e956dac43e05e70df8ffcf8fb5
yaguzmang <alejandroguzman.dev@gmail.com>
2023-04-26 18:07:36 +0200

    Remove deprecated redux logic and actions

--- src/client/store/ui/geo/actions/index.ts
+++ src/client/store/ui/geo/actions/index.ts
@@ -1,5 +1,2 @@
-export { getBurnedAreaLayer } from './getBurnedAreaLayer'
 export { getForestEstimationData } from './getForestEstimationData'
-export { getForestLayer } from './getForestLayer'
-export { getProtectedAreaLayer } from './getProtectedAreaLayer'
 export { postMosaicOptions } from './postMosaicOptions'
