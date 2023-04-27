7c283095a3b2f34e5439c6b0a864699aac0d147f
yaguzmang <alejandroguzman.dev@gmail.com>
2023-04-27 18:20:19 +0200

    Add sections array & update export file
    Added the sections array in a new file to avoid circular
    dependency issues. This file can later be used to handle
    additional typing for layer grouping as well.

--- src/meta/geo/index.ts
+++ src/meta/geo/index.ts
@@ -1,11 +1,19 @@
 export type { Bounds } from './bounds'
-export { BurnedAreaKey, burnedAreaLayersMetadata } from './burnedAreaSource'
-export { agreementPalette, ForestKey, forestLayersMetadata, getRecipeAgreementAreaProperty } from './forest'
+export { BurnedAreaKey, burnedAreaLayers, burnedAreaLayersMetadata } from './burnedAreaSource'
+export type { HansenPercentage } from './forest'
+export {
+  agreementPalette,
+  ForestKey,
+  forestLayers,
+  forestLayersMetadata,
+  getRecipeAgreementAreaProperty,
+} from './forest'
 export type { ForestEstimations, ForestEstimationsData } from './forestEstimations'
 export { ExtraEstimation, extraEstimationsMetadata } from './forestEstimations'
 export type { GeoStatisticsState } from './geoStatistics'
-export type { Layer, LayerConfig, LayerMetadata, LayerOptions, LayerSection, LayerSource } from './layer'
+export type { Layer, LayerConfig, LayerKey, LayerMetadata, LayerOptions, LayerSection, LayerSource } from './layer'
 export { LayerSectionKey } from './layer'
 export type { MosaicOptions, MosaicSource } from './mosaic'
-export { ProtectedAreaKey, protectedAreaLayersMetadata } from './protectedAreaSource'
+export { ProtectedAreaKey, protectedAreaLayers, protectedAreaLayersMetadata } from './protectedAreaSource'
+export { sections } from './sections'
 export type { MapPanel } from './ui'
