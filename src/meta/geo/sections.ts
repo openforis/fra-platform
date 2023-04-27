663175436c62f8fd80ea1c72a73cecfa0dadaca2
yaguzmang <alejandroguzman.dev@gmail.com>
2023-04-27 18:20:19 +0200

    Add sections array & update export file
    Added the sections array in a new file to avoid circular
    dependency issues. This file can later be used to handle
    additional typing for layer grouping as well.

---
+++ src/meta/geo/sections.ts
@@ -0,0 +1,3 @@
+import { burnedAreaLayers, forestLayers, LayerSection, protectedAreaLayers } from '.'
+
+export const sections: Array<LayerSection> = [forestLayers, burnedAreaLayers, protectedAreaLayers]
