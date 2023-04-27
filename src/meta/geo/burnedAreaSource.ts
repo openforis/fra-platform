2e010f3a9ece54646fbc161f4fbd223a475f118c
yaguzmang <alejandroguzman.dev@gmail.com>
2023-04-27 18:14:50 +0200

    Rename BurnedAreaKey MODIS to MODIS_FIRE

--- src/meta/geo/burnedAreaSource.ts
+++ src/meta/geo/burnedAreaSource.ts
@@ -3,11 +3,11 @@ import { Arrays } from '@utils/arrays'
 import { LayerMetadata, LayerSection, LayerSectionKey } from './layer'
 
 export enum BurnedAreaKey {
-  MODIS = 'MODIS',
+  MODIS_FIRE = 'MODIS_FIRE',
 }
 
 export const burnedAreaLayersMetadata: Record<BurnedAreaKey, LayerMetadata> = {
-  [BurnedAreaKey.MODIS]: {
+  [BurnedAreaKey.MODIS_FIRE]: {
     palette: ['#FF0000'],
     scale: 500,
   },
@@ -17,7 +17,7 @@ export const burnedAreaLayers: LayerSection = {
   key: LayerSectionKey.BurnedArea,
   layers: [
     {
-      key: BurnedAreaKey.MODIS,
+      key: BurnedAreaKey.MODIS_FIRE,
       options: { years: Arrays.range(2000, new Date().getFullYear() + 1, 1) },
     },
   ],
