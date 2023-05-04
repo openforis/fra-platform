3c2befdc3c868679a030a0864271756a4f9e9a90
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-04 18:01:31 +0200

    Add custom asset input

---
+++ src/client/pages/Geo/GeoMap/GeoMapMenuData/MapVisualizerPanel/LayersSectionPanel/components/CustomAssetControl/CustomAssetControl.tsx
@@ -0,0 +1,75 @@
+import './CustomAssetControl.scss'
+import React, { ChangeEvent, useState } from 'react'
+
+import classNames from 'classnames'
+
+import { LayerKey } from '@meta/geo'
+
+import LayerOpacityControl from '../LayerOpacityControl'
+
+interface Props {
+  checked: boolean
+  onOpacityChange: (value: number, layerKey: LayerKey) => void
+  onToggle: (layerKey: LayerKey) => void
+  layerKey: LayerKey
+  backgroundColor?: string
+}
+
+const CustomAssetControl: React.FC<Props> = ({ checked, onToggle, onOpacityChange, layerKey, backgroundColor }) => {
+  const [inputValue, setInputValue] = useState<string>('')
+  const [inputError, setInputError] = useState(false)
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(event.target.value)
+    if (inputError && event.target.value.trim() !== '') {
+      setInputError(false)
+    }
+  }
+
+  const handleSubmit = (): void => {
+    if (inputValue.trim() === '') {
+      setInputError(true)
+    } else {
+      setInputError(false)
+    }
+  }
+
+  return (
+    <>
+      <div className="custom-asset-list-element">
+        <div className="custom-asset-item-title">
+          <div
+            className="custom-asset-list-element-checkbox"
+            role="checkbox"
+            aria-checked={checked}
+            tabIndex={0}
+            onClick={() => onToggle(layerKey)}
+            onKeyDown={() => onToggle(layerKey)}
+          >
+            <div style={checked ? { backgroundColor } : {}} className={classNames('fra-checkbox', { checked })} />
+          </div>
+          <div className="custom-input-container">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Asset ID"
+              className={classNames('custom-input', inputError ? 'error' : '')}
+            />
+            <button type="button" className="btn-primary" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+        </div>
+        <LayerOpacityControl layerKey={layerKey} checked={checked} onChange={onOpacityChange} />
+      </div>
+      <div className="custom-asset-list-element-bottom" />
+    </>
+  )
+}
+
+CustomAssetControl.defaultProps = {
+  backgroundColor: null,
+}
+
+export default CustomAssetControl
