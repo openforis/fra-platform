2d18852295776e79c7acd6fc2e307d53a4cdc871
persas <diego.persas@gmail.com>
2023-05-09 12:33:05 +0200

    2353 paneuropean linked nodes from fra 2025 (#2583)
    * add linkedNodes col prop with data migration
    
    * add linkedNodes panEu 1.1a forest 2025/2020 col spec migration
    
    * update data views with linked node
    
    * ui: make linkedNodes cells calculated look alike
    
    * linked nodes table 1.1
    
    * linked nodes table 1.2
    
    * fix growing stock table name
    
    * carbon stock (1.4) linked nodes
    
    * Linked Nodes for table 2.4
    
    * linked nodes table 3.3
    
    * rest of the tables
    
    * fixed email comments
    
    * generate table data views with odp
    
    * linked nodes use views with odp
    
    ---------
    
    Co-authored-by: minotogna <mino.togna@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Mino Togna <minotogna@users.noreply.github.com>

--- src/client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/hooks/useClassName.ts
+++ src/client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/hooks/useClassName.ts
@@ -1,8 +1,9 @@
-import { Col, Cols, ColType, Row } from '@meta/assessment'
+import { Col, Cols, ColType, Cycle, Row } from '@meta/assessment'
 
 import { useNodeValueValidation } from '@client/store/ui/assessmentSection'
 
 type Props = {
+  cycle: Cycle
   col: Col
   row: Row
   tableName: string
@@ -10,7 +11,7 @@ type Props = {
 }
 
 export default (props: Props): string => {
-  const { col, row, tableName, valid } = props
+  const { cycle, col, row, tableName, valid } = props
   const { colType } = col.props
   const nodeUpdate = useNodeValueValidation({ tableName })
 
@@ -21,7 +22,7 @@ export default (props: Props): string => {
     nodeUpdate.colName === col.props.colName
 
   let className = 'fra-table__cell'
-  if (Cols.isReadOnly({ col, row })) className = 'fra-table__calculated-cell'
+  if (Cols.isReadOnly({ cycle, col, row })) className = 'fra-table__calculated-cell'
   if ([ColType.text, ColType.textarea, ColType.select, ColType.taxon].includes(colType))
     className = 'fra-table__cell-left'
   if (colType === ColType.placeholder) className = 'fra-table__category-cell fra-table__filler-last'
