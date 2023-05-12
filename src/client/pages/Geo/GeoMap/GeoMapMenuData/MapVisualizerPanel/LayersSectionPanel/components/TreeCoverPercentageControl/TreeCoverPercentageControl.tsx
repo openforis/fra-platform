fdbe55df2da7879bd58e9140465131503f2cc71d
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-12 20:52:55 +0200

    Add TreeCoverPercentageControl component
    Added a component to handle layers with a percent picker.
    Added logic to handle that component in LayerSectionPanel. The
    new logic includes a 'fetch' flag to 'toggle' layer, so we can prevent
    fetching the layer when toggling. This is because when the user selects
    a layer, they should be able to see its menu before it is fetched.

---
+++ src/client/pages/Geo/GeoMap/GeoMapMenuData/MapVisualizerPanel/LayersSectionPanel/components/TreeCoverPercentageControl/TreeCoverPercentageControl.tsx
@@ -0,0 +1,50 @@
+import './TreeCoverPercentageControl.scss'
+import React from 'react'
+
+import { Layer, LayerKey, LayerSectionKey } from '@meta/geo'
+
+import { useAppDispatch } from '@client/store'
+import { GeoActions } from '@client/store/ui/geo'
+import { LayerState } from '@client/store/ui/geo/stateType'
+
+interface Props {
+  sectionKey: LayerSectionKey
+  layerKey: LayerKey
+  layer: Layer
+  layerState: LayerState
+}
+const TreeCoverPercentageControl: React.FC<Props> = ({ sectionKey, layerKey, layerState, layer }) => {
+  const dispatch = useAppDispatch()
+
+  const handlePercentageChange = (percentage: number) => {
+    dispatch(GeoActions.setLayerGteTreeCoverPercent({ sectionKey, layerKey, gteTreeCoverPercent: percentage }))
+  }
+
+  return (
+    <div className="geo-map-menu-tree-cover-inputs ">
+      <div>
+        <div>
+          <p>Select min. tree cover percentage:</p>
+          <fieldset>
+            {layer.options?.gteTreeCoverPercent.map((percentage) => {
+              const id = `${sectionKey}-${layerKey}-${percentage}`
+              return (
+                <label htmlFor={id} key={id}>
+                  <input
+                    type="radio"
+                    checked={layerState?.options?.gteTreeCoverPercent === percentage}
+                    id={id}
+                    onChange={() => handlePercentageChange(percentage)}
+                  />
+                  <small>{percentage} %</small>
+                </label>
+              )
+            })}
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TreeCoverPercentageControl
