a0cd46c4f98688e70e9dd0963043d00f2cbc51d7
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-15 17:48:03 +0200

    Add logic to build the agreement layer req body
    Adding a builder for the agreement layer calls, using the
    section state with which the postLayer action was called. That
    state is later used to get the selected layers and add them to
    the layers array in the api call.

--- src/client/store/ui/geo/actions/postLayer.ts
+++ src/client/store/ui/geo/actions/postLayer.ts
@@ -5,7 +5,7 @@ import { LayerResponseData } from '@meta/api/request/geo/layer'
 import { CountryIso } from '@meta/area'
 import { LayerKey, LayerSectionKey, sectionsApiEndpoint } from '@meta/geo'
 
-import { LayerState } from '../stateType'
+import { LayersSectionState, LayerState } from '../stateType'
 import { _getLayerRequestBody } from '.'
 
 export interface PostLayerProps {
@@ -13,12 +13,13 @@ export interface PostLayerProps {
   sectionKey: LayerSectionKey
   layerKey: LayerKey
   layerState: LayerState
+  sectionState?: LayersSectionState
 }
 
 export const postLayer = createAsyncThunk<[LayerSectionKey, LayerKey, LayerResponseData], PostLayerProps>(
   'geo/post/layer',
-  async ({ countryIso, sectionKey, layerKey, layerState }) => {
-    const body = _getLayerRequestBody(countryIso, layerKey, layerState)
+  async ({ countryIso, sectionKey, layerKey, layerState, sectionState }) => {
+    const body = _getLayerRequestBody(countryIso, layerKey, layerState, sectionState)
     const url = sectionsApiEndpoint[sectionKey]
     const { data } = await axios({ method: 'POST', url, data: body })
     return [sectionKey, layerKey, data]
