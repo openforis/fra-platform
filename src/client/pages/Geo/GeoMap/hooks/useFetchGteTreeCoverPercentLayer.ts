10185988e37d040c81ca02975acee7656f502e0c
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-16 20:46:48 +0200

    Delete unnecessary usePrevious from useFetchGteTreeCoverPercentLayer
    Since we now get the state from within postLayer, the usePrevious hook here
    becomes unnecessary.

--- src/client/pages/Geo/GeoMap/hooks/useFetchGteTreeCoverPercentLayer.ts
+++ src/client/pages/Geo/GeoMap/hooks/useFetchGteTreeCoverPercentLayer.ts
@@ -4,20 +4,16 @@ import { LayerKey, LayerSectionKey } from '@meta/geo'
 
 import { useAppDispatch } from '@client/store'
 import { GeoActions, useGeoLayer } from '@client/store/ui/geo'
-import { useCountryIso, usePrevious } from '@client/hooks'
+import { useCountryIso } from '@client/hooks'
 
 export const useFetchGteTreeCoverPercentLayer = (sectionKey: LayerSectionKey, layerKey: LayerKey) => {
   const dispatch = useAppDispatch()
   const countryIso = useCountryIso()
   const layerState = useGeoLayer(sectionKey, layerKey)
   const gteTreeCoverPercent = layerState?.options?.gteTreeCoverPercent
-  const prevGteTreeCoverPercent = usePrevious(gteTreeCoverPercent)
 
   useEffect(() => {
-    if (prevGteTreeCoverPercent === gteTreeCoverPercent) return // Skip if no change
     if (gteTreeCoverPercent === undefined) return // Skip when the property is not set
-    if (prevGteTreeCoverPercent === null) return // Skip with initial prev. value
-
     dispatch(GeoActions.postLayer({ countryIso, sectionKey, layerKey }))
-  }, [countryIso, layerKey, layerState, gteTreeCoverPercent, prevGteTreeCoverPercent, sectionKey, dispatch])
+  }, [countryIso, layerKey, gteTreeCoverPercent, sectionKey, dispatch])
 }
