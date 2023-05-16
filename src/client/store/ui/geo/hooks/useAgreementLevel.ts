4da46d59ca5fe65a907ea6cdb120287d01723042
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-16 20:18:50 +0200

    Update useAgreementLevel
    Deleting the unnecessary hooks and updating after delegating the state
    check to the postLayer action.

--- src/client/store/ui/geo/hooks/useAgreementLevel.ts
+++ src/client/store/ui/geo/hooks/useAgreementLevel.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import { LayerKey, LayerSectionKey } from '@meta/geo'
 
 import { useAppDispatch } from '@client/store'
-import { useCountryIso, usePrevious } from '@client/hooks'
+import { useCountryIso } from '@client/hooks'
 
 import { GeoActions } from '../slice'
 import { useGeoLayerSection } from '.'
@@ -14,13 +14,9 @@ export const useAgreementLevel = (sectionKey: LayerSectionKey, layerKey: LayerKe
   const sectionState = useGeoLayerSection(sectionKey)
   const layerState = sectionState?.[layerKey]
   const agreementLevel = layerState?.options?.agreementLayer?.level
-  const prevAgreementLevel = usePrevious(agreementLevel)
 
   useEffect(() => {
-    if (prevAgreementLevel === agreementLevel) return // Skip if no change
     if (agreementLevel === undefined) return // Skip when the property is not set
-    if (prevAgreementLevel === null) return // Skip with initial prev. value
-
-    dispatch(GeoActions.postLayer({ countryIso, sectionKey, layerKey, layerState, sectionState }))
-  }, [countryIso, layerKey, sectionState, layerState, agreementLevel, prevAgreementLevel, sectionKey, dispatch])
+    dispatch(GeoActions.postLayer({ countryIso, sectionKey, layerKey }))
+  }, [countryIso, layerKey, agreementLevel, sectionKey, dispatch])
 }
