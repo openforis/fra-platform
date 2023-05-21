7ce5ace1f52465a25048c6ed7294b72ad4489b01
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-21 17:07:21 +0200

    Re fetch agreement layer when selected layers change

--- src/client/pages/Geo/GeoMap/hooks/useFetchAgreementLevelLayer.ts
+++ src/client/pages/Geo/GeoMap/hooks/useFetchAgreementLevelLayer.ts
@@ -6,15 +6,18 @@ import { useAppDispatch } from '@client/store'
 import { GeoActions, useGeoLayerSection } from '@client/store/ui/geo'
 import { useCountryIso } from '@client/hooks'
 
+import { useCountSectionSelectedLayers } from './useCountSectionSelectedLayers'
+
 export const useFetchAgreementLevelLayer = (sectionKey: LayerSectionKey, layerKey: LayerKey) => {
   const dispatch = useAppDispatch()
   const countryIso = useCountryIso()
   const sectionState = useGeoLayerSection(sectionKey)
   const layerState = sectionState?.[layerKey]
   const agreementLevel = layerState?.options?.agreementLayer?.level
+  const countSelectedLayers = useCountSectionSelectedLayers(sectionKey)
 
   useEffect(() => {
     if (agreementLevel === undefined) return // Skip when the property is not set
     dispatch(GeoActions.postLayer({ countryIso, sectionKey, layerKey }))
-  }, [countryIso, layerKey, agreementLevel, sectionKey, dispatch])
+  }, [countryIso, layerKey, agreementLevel, sectionKey, dispatch, countSelectedLayers])
 }
