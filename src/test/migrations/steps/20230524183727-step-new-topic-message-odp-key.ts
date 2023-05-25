3a7aec0cad82b0eb1f799987d6ef9f242d0070e4
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-05-25 07:59:18 +0200

    split "message topic odp key" step migration in two

---
+++ src/test/migrations/steps/20230524183727-step-new-topic-message-odp-key.ts
@@ -0,0 +1,24 @@
+import { AssessmentController } from '@server/controller/assessment'
+import { BaseProtocol, Schemas } from '@server/db'
+
+export default async (client: BaseProtocol) => {
+  const assessment = await AssessmentController.getOne({ assessmentName: 'fra' }, client)
+
+  await Promise.all(
+    assessment.cycles.map((cycle) => {
+      const schemaCycle = Schemas.getNameCycle(assessment, cycle)
+
+      return client.query(`
+        update ${schemaCycle}.message_topic mt
+        set key=CONCAT('odp-', temprow.key)
+        from (
+          select mt.id, mt.key from ${schemaCycle}.message_topic mt
+          where split_part(mt.key, '-', 2) in ('dataSourceReferences', 'dataSourceMethods', 'dataSourceAdditionalComments', 'class')
+          and mt.key not like 'odp-%'
+          and mt.key not like '-%'
+        ) temprow
+        where mt.id=temprow.id
+      `)
+    })
+  )
+}
