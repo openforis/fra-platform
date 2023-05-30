bcb362fee7b30fe59b5eac34d8c2f747edf86abc
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-05-30 13:31:00 +0200

    fixed missing parameter translation

--- src/client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/hooks/useErrorMessages.tsx
+++ src/client/pages/AssessmentSection/DataTable/Table/Row/RowData/Cell/hooks/useErrorMessages.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-import { useTranslation } from 'react-i18next'
+import { TFunction, useTranslation } from 'react-i18next'
 
-import { NodeValue } from '@meta/assessment'
+import { NodeValue, NodeValueValidationMessageParam } from '@meta/assessment'
 
 type Props = {
   nodeValue: NodeValue
+}
+
+const translateErrorMessageParams = (t: TFunction, text: NodeValueValidationMessageParam): string => {
+  if (Array.isArray(text)) {
+    return `(${text.map((item) => translateErrorMessageParams(t, item)).join(', ')})`
+  }
+  return t(String(text))
 }
 
 export default (props: Props): string => {
@@ -13,7 +20,9 @@ export default (props: Props): string => {
 
   const { t } = useTranslation()
 
-  const dataValidationMessages = nodeValue?.validation?.messages?.map(({ key, params }) => t(key, params)) ?? []
+  const dataValidationMessages = nodeValue.validation.messages.map(({ key, params }) =>
+    t(key, params && Object.fromEntries(Object.entries(params).map(([k, v]) => [k, translateErrorMessageParams(t, v)])))
+  )
 
   return dataValidationMessages.length > 0
     ? ReactDOMServer.renderToStaticMarkup(
