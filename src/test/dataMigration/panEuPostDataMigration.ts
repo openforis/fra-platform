806c930f61dd92056e70df3e4fc5770d25512a80
minotogna <mino.togna@gmail.com>
2023-06-02 12:29:50 +0200

    migrate panEu post data migration script

--- src/test/dataMigration/panEuPostDataMigration.ts
+++ src/test/dataMigration/panEuPostDataMigration.ts
@@ -1,4 +1,9 @@
+import 'tsconfig-paths/register'
+import 'dotenv/config'
+
 import { AssessmentController } from 'server/controller/assessment'
+import { UpdateDependenciesQueueFactory } from 'server/controller/cycleData/updateDependencies'
+import { WorkerFactory } from 'server/controller/cycleData/updateDependencies/workerFactory'
 import { DB } from 'server/db'
 import { Logger } from 'server/utils/logger'
 
@@ -10,37 +15,36 @@ const client = DB
 let index = 0
 const start = new Date().getTime()
 
-afterAll(async () => {
+const close = async () => {
+  // quick and dirty workaround to close redis connection after running integration tests
+  // TODO: find a better strategy to handle Redis connections
+  UpdateDependenciesQueueFactory.connection.quit()
+  WorkerFactory.connection.quit()
   await DB.$pool.end()
-})
+}
 
-const migrateCycle = (cycleName: string) =>
-  test(`Cycle ${cycleName} migration`, async () => {
-    const { assessment, cycle } = await AssessmentController.getOneWithCycle(
-      { assessmentName, cycleName, metaCache: true },
-      client
-    )
+const migrateCycle = async (cycleName: string) => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName, metaCache: true }, client)
 
-    index += 1
-    Logger.debug(`    ========== ${index} updateCalculatedNodes ${cycle.name}`)
-    await updateCalculatedNodes({ assessment, cycle }, client)
+  index += 1
+  Logger.debug(`    ========== ${index} updateCalculatedNodes ${cycle.name}`)
+  await updateCalculatedNodes({ assessment, cycle }, client)
 
-    index += 1
-    Logger.debug(`    ========== ${index} migrateCountryComments ${cycle.name}`)
-    await migrateCountryComments({ assessment, cycle }, client)
-  })
+  index += 1
+  Logger.debug(`    ========== ${index} migrateCountryComments ${cycle.name}`)
+  await migrateCountryComments({ assessment, cycle }, client)
+}
 
-describe(`Post Data migration`, () => {
-  test(`LogStart`, async () => {
-    Logger.debug(`========== START POST DATA MIGRATION `, start)
-  })
+const exec = async () => {
+  Logger.debug(`========== START POST DATA MIGRATION `, start)
 
   // migrateCycle('2020')
-  migrateCycle('2025')
+  await migrateCycle('2025')
 
-  test(`LogEnd`, () => {
-    const end = new Date().getTime()
-    Logger.debug(`========== END `, end, `ELAPSED (s)`, (end - start) / 1000)
-    process.exit(0)
-  })
+  await close()
+}
+
+exec().then(() => {
+  const end = new Date().getTime()
+  Logger.info(`========== panEuropean post data migration executed in ${(end - start) / 1000} (s)`)
 })
