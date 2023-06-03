c797f0d5a16ddcf7a46d40c8512fc00b6787120b
sorja <ping@mirosorja.com>
2023-06-03 17:38:57 +0300

    debug db config

--- src/test/integration/user/userCreate.ts
+++ src/test/integration/user/userCreate.ts
@@ -5,8 +5,13 @@ import { UserController } from 'server/controller/user'
 
 import { userMockTest, userMockTestPassword } from 'test/integration/mock/user'
 
+import { DB } from '../../../server/db'
+
 export default () =>
   test('Expect user to be created', async () => {
+    console.log('-----------------')
+    console.log(DB.$config)
+    console.log('-----------------')
     const user = await UserController.create({
       user: userMockTest,
       provider: {
