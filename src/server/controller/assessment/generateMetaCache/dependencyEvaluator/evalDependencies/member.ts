b0626490b67d4a07bee5f2138baa2acf5eb48b57
sorja <ping@mirosorja.com>
2023-06-05 13:44:45 +0300

    MetaCache v2, commit 1

--- src/server/controller/assessment/generateMetaCache/dependencyEvaluator/evalDependencies/member.ts
+++ src/server/controller/assessment/generateMetaCache/dependencyEvaluator/evalDependencies/member.ts
@@ -131,12 +131,11 @@ const getTableNameAndVariableName = (
     // @ts-ignore
     expressionNode.object.object.object.object.type === 'Identifier'
   ) {
-    // should return, assessmentName = fra, cycleName = 2025
     return {
       // @ts-ignore
       tableName: expressionNode.object.object.property.name,
       // @ts-ignore
-      variableName: expressionNode.property.value,
+      variableName: expressionNode.object.property.name,
       // @ts-ignore
       assessmentName: expressionNode.object.object.object.object.name,
       // @ts-ignore
@@ -165,7 +164,9 @@ const getTableNameAndVariableName = (
   }
 
   // Case when parsing a member expression: extentOfForest.forestArea['2025']
+  // Case when parsing growingStockComposition2025.remainingIntroduced.growingStockMillionCubicMeter
   if (
+    (expressionNode.property.type === 'Literal' || expressionNode.property.type === 'Identifier') &&
     expressionNode.type === 'MemberExpression' &&
     expressionNode.object.type === 'MemberExpression' &&
     // @ts-ignore
@@ -192,12 +193,12 @@ const getTableNameAndVariableName = (
       cycleName: undefined,
     }
   }
+
+  throw new Error(`Could not parse member expression: ${JSON.stringify(expressionNode)}`)
 }
 
 export class MemberEvaluator extends ExpressionNodeEvaluator<Context, MemberExpression> {
   evaluate(expressionNode: MemberExpression): string {
-    const { object, property } = expressionNode
-
     const { assessmentMetaCache, row, tableName, type } = this.context
 
     // Example of: <calcualteFn> = [...result]
@@ -206,20 +207,6 @@ export class MemberEvaluator extends ExpressionNodeEvaluator<Context, MemberExpr
     // Case 3: fra['2025'].extentOfForest.forestArea (4) [fra, '2025', extentOfForest, forestArea]
     // Case 4: fra['2025'].extentOfForest.forestArea['2025'] (4) [fra, '2025', extentOfForest, forestArea]
 
-    // NOTE: For testing purposes row with id 211, is updated manually
-    // "calculateFn": {
-    //     "66817a08-dc93-4151-b5ed-176d8f04e9b7": "fra['2020'].extentOfForest.forestArea",
-    //     "66da2217-da42-492f-9ff4-c99a59e6675c": "fra['2025'].extentOfForest.forestArea['2025']"
-    //   },
-
-    if (!getTableNameAndVariableName(expressionNode) || this.context.row.id === 211) {
-      console.log('MemberEvaluator')
-      console.log(object, property, expressionNode)
-      console.log(this.context.row)
-      console.log(JSON.stringify(expressionNode, null, 2))
-      return ''
-    }
-
     const {
       tableName: _tableName,
       variableName: _variableName,
@@ -227,21 +214,18 @@ export class MemberEvaluator extends ExpressionNodeEvaluator<Context, MemberExpr
       cycleName,
     } = getTableNameAndVariableName(expressionNode)
 
-    const objectName = _tableName
-    const propertyName = _variableName
-
-    if (assessmentMetaCache.variablesByTable[objectName]) {
-      const dependantTable = assessmentMetaCache[type].dependants?.[objectName] ?? {}
-      const dependants = dependantTable[propertyName] ?? []
+    if (assessmentMetaCache.variablesByTable[_tableName]) {
+      const dependantTable = assessmentMetaCache[type].dependants?.[_tableName] ?? {}
+      const dependants = dependantTable[_variableName] ?? []
       const dependant: VariableCache = { variableName: row.props.variableName, tableName }
-      if (!excludeDependant(row, objectName, propertyName) && !includesVariableCache(dependants, dependant)) {
+      if (!excludeDependant(row, _tableName, _variableName) && !includesVariableCache(dependants, dependant)) {
         assessmentMetaCache[type].dependants = {
           ...assessmentMetaCache[type].dependants,
           // @ts-ignore
-          [objectName]: {
+          [_tableName]: {
             ...dependantTable,
             // @ts-ignore
-            [propertyName]: [...dependants, dependant],
+            [_variableName]: [...dependants, dependant],
           },
         }
       }
@@ -249,7 +233,7 @@ export class MemberEvaluator extends ExpressionNodeEvaluator<Context, MemberExpr
       const dependencyTable = assessmentMetaCache[type].dependencies?.[tableName] ?? {}
       const dependencies = dependencyTable[row.props.variableName] ?? []
       // @ts-ignore
-      const dependency: VariableCache = { variableName: propertyName, tableName: objectName }
+      const dependency: VariableCache = { variableName: _variableName, tableName: _tableName }
 
       if (assessmentName && cycleName) {
         dependency.assessmentName = assessmentName
@@ -266,12 +250,8 @@ export class MemberEvaluator extends ExpressionNodeEvaluator<Context, MemberExpr
         }
       }
 
-      const evaluateObject = this.evaluator.evaluateNode(object, this.context)
-      console.log('evaluateObject', evaluateObject)
-      const evaluateProperty = this.evaluator.evaluateNode(property, this.context)
-      console.log('evaluateProperty', evaluateProperty)
-      return `${evaluateObject}.${evaluateProperty}`
+      return `${tableName}.${_variableName}`
     }
-    return `${objectName}.${propertyName}`
+    return `${_tableName}.${_variableName}`
   }
 }
--- src/server/controller/cycleData/getOriginalDataPointVariables.ts
+++ src/server/controller/cycleData/getOriginalDataPointVariables.ts
@@ -61,7 +61,7 @@ export const getOriginalDataPointVariables = (cycle: Cycle) => {
 // Find given variable from ODPVariables
 export const isODPVariable = (cycle: Cycle, variable: VariableCache): boolean => {
   return getOriginalDataPointVariables(cycle).some((odpVariable) => {
-    const keys: Array<keyof VariableCache> = ['tableName', 'variableName']
+    const keys: ['tableName', 'variableName'] = ['tableName', 'variableName']
     return keys.every((key) => variable[key] === odpVariable[key])
   })
 }
