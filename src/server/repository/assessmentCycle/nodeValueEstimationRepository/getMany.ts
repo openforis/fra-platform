f2a2a28fd75fee7b3a4041bbb8def1492b7dc98b
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-06-05 13:59:45 +0300

    bugfix

--- src/server/repository/assessmentCycle/nodeValueEstimationRepository/getMany.ts
+++ src/server/repository/assessmentCycle/nodeValueEstimationRepository/getMany.ts
@@ -22,11 +22,11 @@ export const getMany = (props: Props, client: BaseProtocol = DB): Promise<Array<
     `
       with estimations_ranked as (
         select e.uuid,
-          row_number() over (partition by jsonb_object_keys(e.variables) order by e.uuid desc) as row_number
+          row_number() over (partition by jsonb_object_keys(e.variables) order by e.id desc) as row_number
         from ${schemaCycle}.node_values_estimation e
         where e.table_uuid = $1
           and e.country_iso = $2
-        order by e.uuid desc
+        order by e.id desc
       )
       select e.uuid, e.country_iso, e.table_uuid, e.method, e.variables
       from ${schemaCycle}.node_values_estimation e
