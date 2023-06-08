3ccaadfad7863a5fae862813906c276c0a9f31ae
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-06-08 11:19:28 +0200

    added uuid unique constraint

--- src/test/migrations/steps/20230607170529-step-primary-key-for-table-file.ts
+++ src/test/migrations/steps/20230607170529-step-primary-key-for-table-file.ts
@@ -1,7 +1,11 @@
 import { BaseProtocol } from 'server/db'
 
 export default async (client: BaseProtocol) => {
-  await client.query(`ALTER TABLE assessment_fra.file ADD PRIMARY KEY (id);`)
+  await client.query('ALTER TABLE assessment_fra.file ADD PRIMARY KEY (id);')
 
-  await client.query(`ALTER TABLE assessment_paneuropean.file ADD PRIMARY KEY (id);`)
+  await client.query('ALTER TABLE assessment_paneuropean.file ADD PRIMARY KEY (id);')
+
+  await client.query('ALTER TABLE assessment_fra.file ADD CONSTRAINT file_uuid_unique UNIQUE (uuid);')
+
+  await client.query('ALTER TABLE assessment_paneuropean.file ADD CONSTRAINT file_uuid_unique UNIQUE (uuid);')
 }
