9cfe899ddd99a45d7a07283d9d560acae8cec694
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-06-14 16:36:16 +0200

    default country value

--- src/client/components/EditUserForm/EditUserForm.tsx
+++ src/client/components/EditUserForm/EditUserForm.tsx
@@ -107,7 +107,14 @@ const EditUserForm: React.FC<Props> = ({ user, canEditPermissions, canEditRoles,
       {[RoleName.NATIONAL_CORRESPONDENT, RoleName.ALTERNATE_NATIONAL_CORRESPONDENT, RoleName.COLLABORATOR].includes(
         userRole?.role
       ) &&
-        roleToEdit && <UserRolePropsFields role={roleToEdit} onChange={changeUserRoleProp} enabled={enabled} />}
+        roleToEdit && (
+          <UserRolePropsFields
+            role={roleToEdit}
+            onChange={changeUserRoleProp}
+            defaultCountryIso={countryIso}
+            enabled={enabled}
+          />
+        )}
 
       <div className="edit-user__form-item">
         <div className="edit-user__form-label">{t('editUser.mandatoryFields')}</div>
--- src/client/components/EditUserForm/UserRolePropsFields/UserRolePropsFields.tsx
+++ src/client/components/EditUserForm/UserRolePropsFields/UserRolePropsFields.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { CountryIso } from 'meta/area'
 import { RoleName, UserContactPreference, UserRole } from 'meta/user/userRole'
 
 import ContactMethodField from '../ContactMethodField'
@@ -10,11 +11,12 @@ import TextInputField from '../TextInputField'
 type Props = {
   role: UserRole<any, any>
   onChange: (name: string, value: any) => void
+  defaultCountryIso?: CountryIso
   enabled?: boolean
 }
 
 const UserRolePropsFields: React.FC<Props> = (props) => {
-  const { role, onChange, enabled } = props
+  const { role, onChange, defaultCountryIso, enabled } = props
 
   const { props: roleProps } = role
 
@@ -76,7 +78,7 @@ const UserRolePropsFields: React.FC<Props> = (props) => {
 
           <CountrySelectField
             name="countryIso"
-            value={roleProps.address?.countryIso}
+            value={roleProps.address?.countryIso ?? defaultCountryIso}
             onChange={changeAddress}
             enabled
           />
@@ -118,6 +120,7 @@ const UserRolePropsFields: React.FC<Props> = (props) => {
 }
 
 UserRolePropsFields.defaultProps = {
+  defaultCountryIso: null,
   enabled: false,
 }
 
