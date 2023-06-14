2597baa1f6a901732fffc6c454f5e0eb6f34edfc
Miró Sorja <ping@mirosorja.com>
2023-06-14 16:14:34 +0300

    PanEuropean: use only English (#2769)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/client/pages/Assessment/Assessment.tsx
+++ src/client/pages/Assessment/Assessment.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Route, Routes, useParams } from 'react-router-dom'
 
 import { ClientRoutes } from 'meta/app'
-import { AssessmentName } from 'meta/assessment'
+import { AssessmentName, AssessmentNames } from 'meta/assessment'
+import { Lang } from 'meta/lang'
 
 import { useAppDispatch } from 'client/store'
 import { AssessmentActions, useAssessment } from 'client/store/assessment'
 import { useOnUpdate } from 'client/hooks'
+import { updateLanguage } from 'client/utils/updateLanguage'
 
 import Cycle from '../Cycle'
 
 const Assessment: React.FC = () => {
   const dispatch = useAppDispatch()
   const assessment = useAssessment()
+  const { i18n } = useTranslation()
 
   const { assessmentName } = useParams<{ assessmentName: AssessmentName }>()
 
@@ -32,6 +36,22 @@ const Assessment: React.FC = () => {
     }
     return undefined
   }, [assessment, assessmentName, dispatch])
+
+  // If the assessment is panEuropean, we use the english language,
+  useEffect(() => {
+    if (assessment?.props.name === AssessmentNames.panEuropean) {
+      ;(async () => {
+        await i18n.changeLanguage(Lang.en)
+      })()
+    } else {
+      ;(async () => {
+        const _lang = await localStorage.getItem('i18n/lang')
+        if (_lang) {
+          await updateLanguage(_lang as Lang, i18n)
+        }
+      })()
+    }
+  }, [assessment?.props.name, i18n])
 
   if (!assessment) return null
 
