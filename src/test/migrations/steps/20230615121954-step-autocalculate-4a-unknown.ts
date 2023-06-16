dd0b1005b1b0179c3304cfaf4371b1392c415b44
Miró Sorja <ping@mirosorja.com>
2023-06-16 17:29:31 +0300

    Update 4a unknown (#2780)
    * Update 4a, add helper function
    
    * Fix total 4a

---
+++ src/test/migrations/steps/20230615121954-step-autocalculate-4a-unknown.ts
@@ -0,0 +1,26 @@
+import { BaseProtocol } from 'server/db'
+
+import { AssessmentCycleUtil } from 'test/migrations/steps/utils/getAssessmentCycle'
+import { updateRowCalculateFn } from 'test/migrations/steps/utils/updateRowCalculateFn'
+
+export default async (client: BaseProtocol) => {
+  const { assessment, cycle } = await AssessmentCycleUtil.getFra2025(client)
+
+  const formula =
+    'extentOfForest.forestArea || forestOwnership.private_ownership || forestOwnership.public_ownership ? Math.max(0, (extentOfForest.forestArea - (forestOwnership.private_ownership || 0) - (forestOwnership.public_ownership || 0) - (forestOwnership.other || 0)))  : null'
+  const tableName = 'forestOwnership'
+  const variableName = 'unknown'
+
+  await updateRowCalculateFn({ assessment, cycle, formula, tableName, variableName }, client)
+  await updateRowCalculateFn(
+    {
+      assessment,
+      cycle,
+      formula:
+        '(extentOfForest.forestArea || forestOwnership.private_ownership || forestOwnership.public_ownership || forestOwnership.other || forestOwnership.unknown) ? (forestOwnership.private_ownership || 0) + (forestOwnership.public_ownership || 0) + (forestOwnership.other || 0) + (extentOfForest.forestArea || forestOwnership.private_ownership || forestOwnership.public_ownership ? Math.max(0, (extentOfForest.forestArea - (forestOwnership.private_ownership || 0) - (forestOwnership.public_ownership || 0) - (forestOwnership.other || 0))) : null) : null',
+      tableName,
+      variableName: 'total',
+    },
+    client
+  )
+}
