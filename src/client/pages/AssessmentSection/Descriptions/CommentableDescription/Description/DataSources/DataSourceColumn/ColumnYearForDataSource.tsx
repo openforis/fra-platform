dd04917733fe390987a87e1b6e0000c0549e43c2
Miró Sorja <ping@mirosorja.com>
2023-07-05 09:47:19 +0300

    Datasources: Freetext for year

--- src/client/pages/AssessmentSection/Descriptions/CommentableDescription/Description/DataSources/DataSourceColumn/ColumnYearForDataSource.tsx
+++ src/client/pages/AssessmentSection/Descriptions/CommentableDescription/Description/DataSources/DataSourceColumn/ColumnYearForDataSource.tsx
@@ -1,17 +1,9 @@
-import React, { useMemo } from 'react'
-import { useTranslation } from 'react-i18next'
-
-import classNames from 'classnames'
-import { Arrays } from 'utils/arrays'
+import React from 'react'
 
 import { DataSource } from 'meta/assessment'
-import { TooltipId } from 'meta/tooltip'
 
-import { useCycle } from 'client/store/assessment'
-import Autocomplete from 'client/components/Autocomplete'
 import DataColumn from 'client/components/DataGrid/DataColumn'
-
-import { datasourceValidators } from './datasourceValidators'
+import VerticallyGrowingTextField from 'client/components/VerticallyGrowingTextField'
 
 type Props = {
   disabled: boolean
@@ -21,36 +13,12 @@ type Props = {
 
 const ColumnVariable: React.FC<Props> = (props: Props) => {
   const { dataSourceValue, disabled, onChange } = props
-  const { t } = useTranslation()
-  const cycle = useCycle()
-  const columns = cycle
-    ? Arrays.reverse(Arrays.range(1950, Number(cycle.name))).map((col) => ({
-        label: String(col),
-        value: String(col),
-      }))
-    : []
 
-  const _onChange = ({ value }: { value: string }) => onChange('year', value)
-
-  const validationError = useMemo(() => {
-    return datasourceValidators.year(dataSourceValue.year)
-  }, [dataSourceValue.year])
+  const _onChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => onChange('year', event.target.value)
 
   return (
-    <DataColumn
-      data-tooltip-id={TooltipId.error}
-      data-tooltip-content={validationError ? t('generalValidation.valueMustBeYear') : ''}
-      className={classNames('data-source-column', {
-        'validation-error': validationError,
-      })}
-    >
-      <Autocomplete
-        disabled={disabled}
-        items={columns}
-        onSave={_onChange}
-        readOnlyOptions
-        value={dataSourceValue.year}
-      />
+    <DataColumn className="data-source-column">
+      <VerticallyGrowingTextField disabled={disabled} onChange={_onChange} value={dataSourceValue.year} />
     </DataColumn>
   )
 }
