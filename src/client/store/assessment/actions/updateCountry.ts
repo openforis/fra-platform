86a3bd7441ddfffd33dc17d16c9e469f8e97f76b
Mino Togna <minotogna@users.noreply.github.com>
2023-08-03 12:54:01 +0200

    file repository: allow collaborators to upload files (#2903)

--- src/client/store/assessment/actions/updateCountry.ts
+++ src/client/store/assessment/actions/updateCountry.ts
@@ -5,28 +5,19 @@ import { ApiEndPoint } from 'meta/api/endpoint'
 import { Country, CountryIso } from 'meta/area'
 import { AssessmentName } from 'meta/assessment'
 
-export const updateCountry = createAsyncThunk<
-  Country,
-  {
-    assessmentName: AssessmentName
-    country: Country
-    countryIso: CountryIso
-    cycleName: string
-    message?: string
-    notifyUsers?: boolean
-  }
->('assessment/post/country', async ({ country, countryIso, assessmentName, cycleName, notifyUsers, message }) => {
-  const { data } = await axios.post(
-    ApiEndPoint.Area.country(),
-    { country, message },
-    {
-      params: {
-        assessmentName,
-        countryIso,
-        cycleName,
-        notifyUsers,
-      },
-    }
-  )
+type Props = {
+  assessmentName: AssessmentName
+  country: Country
+  countryIso: CountryIso
+  cycleName: string
+  message?: string
+  notifyUsers?: boolean
+}
+
+export const updateCountry = createAsyncThunk<Country, Props>('assessment/post/country', async (props) => {
+  const { country, countryIso, assessmentName, cycleName, notifyUsers, message } = props
+
+  const params = { assessmentName, countryIso, cycleName, notifyUsers }
+  const { data } = await axios.post(ApiEndPoint.Area.country(), { country, message }, { params })
   return data
 })
