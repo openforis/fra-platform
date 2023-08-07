322252f9406a435d8d5a39c338c3e449c11db844
Miró Sorja <ping@mirosorja.com>
2023-08-07 15:22:07 +0200

    fix crash when lang null (#2914)
    * fix crash when lang null
    
    * Cleanup useInitLanguage

--- src/client/pages/PageRoutes/hooks/useInitLanguage.tsx
+++ src/client/pages/PageRoutes/hooks/useInitLanguage.tsx
@@ -4,26 +4,27 @@ import { AssessmentNames } from 'meta/assessment'
 import { Lang } from 'meta/lang'
 
 import { useAssessment } from 'client/store/assessment'
+import { useUser } from 'client/store/user'
 import { useLanguage, useUpdateLanguage } from 'client/hooks/useLanguage'
-import { useOnMount } from 'client/hooks/useOnMount'
 import { Urls } from 'client/utils'
 
 export const useInitLanguage = (): void => {
+  const user = useUser()
   const language = useLanguage()
   const updateLanguage = useUpdateLanguage()
   const assessment = useAssessment()
 
-  useOnMount(() => {
-    const lang = (Urls.getRequestParam('lang') || localStorage.getItem('i18n/lang') || Lang.en) as Lang
-    if (lang !== language) updateLanguage({ lang }).then(() => ({}))
-  })
-
   useEffect(() => {
+    const langRequestParam = Urls.getRequestParam('lang')
+    const langLocalStorage = localStorage.getItem('i18n/lang')
+    const langUser = user?.props?.lang
+
+    const lang = (langRequestParam || langLocalStorage || langUser || Lang.en) as Lang
+
+    if (lang !== language) updateLanguage({ lang }).then(() => ({}))
     // If assessment is panEuropean, we use the english language,
-    const params =
-      assessment?.props.name === AssessmentNames.panEuropean
-        ? { lang: Lang.en, persist: false }
-        : { lang: localStorage.getItem('i18n/lang') as Lang }
+    const isPanEuropean = assessment?.props.name === AssessmentNames.panEuropean
+    const params = isPanEuropean ? { lang: Lang.en, persist: false } : { lang }
     updateLanguage(params).then(() => ({}))
-  }, [assessment?.props.name, updateLanguage])
+  }, [assessment?.props.name, language, updateLanguage, user])
 }
