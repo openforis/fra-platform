a400837fe0fc7895fa8c1b5227c31e66d1b102c5
sorja <ping@mirosorja.com>
2023-08-10 16:34:13 +0300

    Sort printview odps by year

--- src/client/pages/AssessmentPrint/OriginalDataPointsPrint/useOriginalDataPoints.ts
+++ src/client/pages/AssessmentPrint/OriginalDataPointsPrint/useOriginalDataPoints.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
 import { OriginalDataPoint } from 'meta/assessment'
@@ -12,7 +12,7 @@ export const useOriginalDataPoints = (): { originalDataPoints: Array<OriginalDat
   const countryIso = useCountryIso()
 
   const {
-    data: originalDataPoints = [],
+    data = [],
     dispatch: fetchResults,
     loading,
   } = useGetRequest(ApiEndPoint.CycleData.OriginalDataPoint.many(), {
@@ -27,5 +27,13 @@ export const useOriginalDataPoints = (): { originalDataPoints: Array<OriginalDat
     fetchResults()
   }, [])
 
-  return { originalDataPoints, loading }
+  const originalDataPoints = useMemo(
+    () => data.sort((a: OriginalDataPoint, b: OriginalDataPoint) => a.year - b.year),
+    [data]
+  )
+
+  return {
+    originalDataPoints,
+    loading,
+  }
 }
