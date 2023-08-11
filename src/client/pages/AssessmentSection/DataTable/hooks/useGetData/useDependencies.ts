177b3ed3cda680957bcf3ff6a83606e42b900437
Miró Sorja <ping@mirosorja.com>
2023-08-11 13:28:10 +0200

    Remove hardcoded dependency (#2934)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/client/pages/AssessmentSection/DataTable/hooks/useGetData/useDependencies.ts
+++ src/client/pages/AssessmentSection/DataTable/hooks/useGetData/useDependencies.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 
-import { AssessmentNames, TableNames, VariableCache } from 'meta/assessment'
+import { TableNames, VariableCache } from 'meta/assessment'
 
 import { useAssessment, useCycle } from 'client/store/assessment'
 import { useCanEdit } from 'client/store/user'
@@ -20,17 +20,11 @@ export const useDependencies = (props: Props): TableDependencies => {
   const canEdit = useCanEdit(sectionName)
 
   return useMemo<TableDependencies>(() => {
-    const assessmentName = assessment.props.name
-    const cycleName = cycle.name
     const { name: tableName } = table.props
     const dependencies = new TableDependencies({ assessment, cycle })
 
     dependencies.add({ tableName })
     staticDependencies[tableName]?.forEach((t) => dependencies.add({ tableName: t }))
-    // TODO: fix this in https://github.com/openforis/fra-platform/issues/2879
-    if (assessmentName === AssessmentNames.fra) {
-      dependencies.add({ assessmentName, cycleName, tableName: TableNames.extentOfForest })
-    }
 
     const addVariables = (variables: Array<Array<VariableCache>>): void => {
       variables.flat(1).forEach((variable) => {
