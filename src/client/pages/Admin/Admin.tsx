00b39ff152b19b3df2b9697aca0857db1684bf9c
sorja <ping@mirosorja.com>
2023-08-17 13:05:53 +0300

    Remove client routes reference

--- src/client/pages/Admin/Admin.tsx
+++ src/client/pages/Admin/Admin.tsx
@@ -6,7 +6,8 @@ import { Navigate, NavLink, Outlet } from 'react-router-dom'
 import classNames from 'classnames'
 import { Objects } from 'utils/objects'
 
-import { AdminRouteNames, ClientRoutes } from 'meta/app'
+import { AdminRouteNames } from 'meta/app'
+import { Routes } from 'meta/routes'
 import { Users } from 'meta/user'
 
 import { useCountries } from 'client/store/area'
@@ -24,7 +25,7 @@ const Admin: React.FC = () => {
   const countries = useCountries()
   const user = useUser()
 
-  if (!Users.isAdministrator(user)) return <Navigate to={ClientRoutes.Root.path} replace />
+  if (!Users.isAdministrator(user)) return <Navigate to={Routes.Root.path} replace />
 
   if (Objects.isEmpty(countries)) return null
 
