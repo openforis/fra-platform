eb3f8130cf6a39c0cf114783ae23ac7619020c3d
Miró Sorja <ping@mirosorja.com>
2023-08-21 13:41:41 +0200

    Calculated cell: Fix zero value rendering issue (#2966)
    * Calculated cell: Fix zero value rendering issue
    
    * Use Objects.isEmpty for truthy check

--- src/client/pages/Section/DataTable/Table/Row/RowData/Cell/Calculated/Calculated.tsx
+++ src/client/pages/Section/DataTable/Table/Row/RowData/Cell/Calculated/Calculated.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import { Numbers } from 'utils/numbers'
+import { Objects } from 'utils/objects'
 
 import { PropsCell } from '../props'
 
 const Calculated: React.FC<PropsCell> = (props) => {
   const { nodeValue, row } = props
 
-  const value = nodeValue?.raw ? Numbers.format(Number(nodeValue.raw), row.props?.format?.integer ? 0 : 2) : ''
+  const value = !Objects.isEmpty(nodeValue.raw) ? Numbers.format(nodeValue.raw, row.props?.format?.integer ? 0 : 2) : ''
 
   return <div>{value}</div>
 }
