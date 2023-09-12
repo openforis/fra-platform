b7c4e8e6328abc8bf80b13c5a753e76a4f5d6e0d
minotogna <mino.togna@gmail.com>
2023-09-12 15:32:58 +0200

    add country_summary materialized view and scheduler

--- src/server/controller/area/index.ts
+++ src/server/controller/area/index.ts
@@ -1,4 +1,5 @@
 import { CountryRepository } from 'server/repository/assessmentCycle/country'
+import { CountrySummaryRepository } from 'server/repository/assessmentCycle/countrySummary'
 import { RegionRepository } from 'server/repository/assessmentCycle/region'
 
 import { updateCountry } from './updateCountry'
@@ -6,8 +7,9 @@ import { updateCountry } from './updateCountry'
 export const AreaController = {
   getCountries: CountryRepository.getMany,
   getCountry: CountryRepository.getOne,
-  getCountrySummaries: CountryRepository.getManySummaries,
-  getCountrySummariesCount: CountryRepository.getCountSummaries,
+  getCountrySummaries: CountrySummaryRepository.getMany,
+  getCountrySummariesCount: CountrySummaryRepository.getCount,
   getRegionGroups: RegionRepository.getRegionGroups,
+  refreshSummaries: CountrySummaryRepository.refreshMaterializedView,
   updateCountry,
 }
