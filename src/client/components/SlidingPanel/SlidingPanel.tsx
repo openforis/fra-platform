308b5f4acee29037cee5500fa840e134d8477b5f
sorja <ping@mirosorja.com>
2023-09-13 10:30:40 +0300

    Remove template code

--- src/client/components/SlidingPanel/SlidingPanel.tsx
+++ src/client/components/SlidingPanel/SlidingPanel.tsx
@@ -3,37 +3,28 @@ import 'react-sliding-side-panel/lib/index.css'
 import React from 'react'
 import SlidingPanelComponent from 'react-sliding-side-panel'
 
-import classNames from 'classnames'
-
 type Props = {
-  className?: string
   openPanel: boolean
   setOpenPanel: (isOpen: boolean) => void
 }
 
 const SlidingPanel: React.FC<React.PropsWithChildren<Props>> = (props) => {
-  const { children, className, openPanel, setOpenPanel } = props
+  const { children, openPanel, setOpenPanel } = props
   const panelType = 'right'
   const panelSize = 30
 
   return (
-    <div className="example-container">
-      <SlidingPanelComponent
-        type={panelType}
-        isOpen={openPanel}
-        backdropClicked={() => setOpenPanel(false)}
-        size={panelSize}
-        panelClassName={classNames('sliding-panel', className)}
-        panelContainerClassName=""
-      >
-        <div className="panel-container">{children}</div>
-      </SlidingPanelComponent>
-    </div>
+    <SlidingPanelComponent
+      type={panelType}
+      isOpen={openPanel}
+      backdropClicked={() => setOpenPanel(false)}
+      size={panelSize}
+      panelClassName="sliding-panel"
+      panelContainerClassName=""
+    >
+      <div className="panel-container">{children}</div>
+    </SlidingPanelComponent>
   )
-}
-
-SlidingPanel.defaultProps = {
-  className: '',
 }
 
 export default SlidingPanel
