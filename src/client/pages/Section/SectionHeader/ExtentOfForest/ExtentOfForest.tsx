a69be959df23c550f37562b15aa808d8eb44868f
Miró Sorja <ping@mirosorja.com>
2023-09-14 12:21:02 +0200

    Original Data Point: Navigate and Create on year selection (#3065)
    * addNationalDataPoint: navigate to odp view with year -1
    
    * ODP: Create DB entry on year select
    
    * Allow year selection before odp.id exists
    
    * ODP never null

--- src/client/pages/Section/SectionHeader/ExtentOfForest/ExtentOfForest.tsx
+++ src/client/pages/Section/SectionHeader/ExtentOfForest/ExtentOfForest.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import { useAppDispatch } from 'client/store'
+import classNames from 'classnames'
+
+import { Routes } from 'meta/routes'
+
 import { useAssessment, useCycle } from 'client/store/assessment'
-import { OriginalDataPointActions } from 'client/store/ui/originalDataPoint'
 import { useUser } from 'client/store/user'
 import { useCountryIso } from 'client/hooks'
 import Icon from 'client/components/Icon'
@@ -14,9 +16,7 @@ import { Props } from '../props'
 const ExtentOfForest: React.FC<Props> = (props) => {
   const { disabled } = props
 
-  const dispatch = useAppDispatch()
-  const navigate = useNavigate()
-  const { i18n } = useTranslation()
+  const { t } = useTranslation()
   const assessment = useAssessment()
   const cycle = useCycle()
   const countryIso = useCountryIso()
@@ -24,28 +24,21 @@ const ExtentOfForest: React.FC<Props> = (props) => {
 
   if (!user) return null
 
-  const handleClick = async () => {
-    dispatch(
-      OriginalDataPointActions.createOriginalDataPoint({
-        assessmentName: assessment.props.name,
-        cycleName: cycle.name,
-        countryIso,
-        navigate,
-      })
-    )
-  }
-
   return (
     <>
-      <button
-        type="button"
-        className={`btn btn-primary no-print${disabled ? ' disabled' : ''}`}
-        onClick={handleClick}
-        style={{ marginRight: 16 }}
+      <Link
+        className={classNames('btn btn-primary no-print', { disabled })}
+        to={Routes.OriginalDataPoint.generatePath({
+          countryIso,
+          assessmentName: assessment.props.name,
+          cycleName: cycle.name,
+          year: '-1',
+          sectionName: 'extentOfForest',
+        })}
       >
         <Icon className="icon-sub icon-white" name="small-add" />
-        {i18n.t<string>('nationalDataPoint.addNationalDataPoint')}
-      </button>
+        {t('nationalDataPoint.addNationalDataPoint')}
+      </Link>
       <hr className="no-print" />
     </>
   )
