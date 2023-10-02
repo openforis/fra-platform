78d075f78daa103cea78c3fd2e717b3660e0f291
sorja <ping@mirosorja.com>
2023-10-02 10:16:52 +0300

    Recent Activity UI: Fix no activity display

--- src/client/pages/CountryHome/FraHome/RecentActivity/RecentActivity.tsx
+++ src/client/pages/CountryHome/FraHome/RecentActivity/RecentActivity.tsx
@@ -1,8 +1,14 @@
 import React, { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+import { Objects } from 'utils/objects'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
 import { ActivityLog } from 'meta/assessment'
+import { Routes } from 'meta/routes'
 
+import { useTablePaginatedCount } from 'client/store/ui/tablePaginated'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import TablePaginated, { Column } from 'client/components/TablePaginated'
 
@@ -22,24 +28,31 @@ const useColumns = (): Array<Column<ActivityLog<any>>> => {
 
 const RecentActivity: React.FC = () => {
   const columns = useColumns()
-  const { countryIso } = useCountryRouteParams()
-  const params = { countryIso }
+  const { t } = useTranslation()
+
+  const { assessmentName, cycleName, countryIso } = useCountryRouteParams()
+  const params = useMemo(
+    () => ({
+      countryIso,
+    }),
+    [countryIso]
+  )
   const path = ApiEndPoint.CycleData.activities()
 
-  // TODO: No data show empty state:
+  const count = useTablePaginatedCount(path)
 
-  // return
-  //       <div className="landing__activity-empty">
-  //         <img src="/img/tucan.svg" height="72" alt="tucan" />
-  //         <p className="landing__activity-empty-title">{t('landing.recentActivity.noRecentActivityTitle')}</p>
-  //         <p>{t('landing.recentActivity.noRecentActivityBody')}</p>
-  //         <Link
-  //           className="btn-s btn-primary"
-  //           to={Routes.Country.generatePath({ countryIso, assessmentName, cycleName })}
-  //         >
-  //           {t('landing.recentActivity.getStarted')}
-  //         </Link>
-  //       </div>
+  if (!Objects.isEmpty(count) && count.total === 0) {
+    return (
+      <div className="landing__activity-empty">
+        <img src="/img/tucan.svg" height="72" alt="tucan" />
+        <p className="landing__activity-empty-title">{t('landing.recentActivity.noRecentActivityTitle')}</p>
+        <p>{t('landing.recentActivity.noRecentActivityBody')}</p>
+        <Link className="btn-s btn-primary" to={Routes.Country.generatePath({ countryIso, assessmentName, cycleName })}>
+          {t('landing.recentActivity.getStarted')}
+        </Link>
+      </div>
+    )
+  }
 
   return <TablePaginated header={false} columns={columns} path={path} params={params} />
 }
