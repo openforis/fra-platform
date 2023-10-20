217e0e3b5ae42e0971dc1e5ba1cd052b6368666e
sorja <ping@mirosorja.com>
2023-10-20 16:12:35 +0300

    Refresh all CountryActivityLogs per hour

--- src/server/schedulers/initCountryActivityLog.ts
+++ src/server/schedulers/initCountryActivityLog.ts
@@ -1,21 +1,11 @@
-import { Job, Queue, Worker } from 'bullmq'
+import { Queue, Worker } from 'bullmq'
 import IORedis from 'ioredis'
-import { Objects } from 'utils/objects'
+import { Promises } from 'utils/promises'
 
-import { CountryIso } from 'meta/area'
-
+import { AreaController } from 'server/controller/area'
 import { AssessmentController } from 'server/controller/assessment'
-import { BaseProtocol, DB } from 'server/db'
 import { CountryActivityLogRepository } from 'server/repository/assessmentCycle/countryActivityLog'
 import { Logger } from 'server/utils/logger'
-
-const client: BaseProtocol = DB
-
-type NewActivityLogEntry = {
-  assessmentUuid: string
-  cycleUuid: string
-  countryIso: CountryIso
-}
 
 export const initCountryActivityLog = (connection: IORedis): Worker => {
   const name = 'countryActivityLogQueue'
@@ -23,31 +13,21 @@ export const initCountryActivityLog = (connection: IORedis): Worker => {
 
   const worker = new Worker(
     name,
-    async (job: Job) => {
+    async () => {
       Logger.info(`[System schedulers] ** ${name} started`)
 
-      const jobCreatedAt = new Date(job.timestamp).toISOString()
       const assessments = await AssessmentController.getAll({})
 
-      const newActivityLogEntries = await client.map<NewActivityLogEntry>(
-        `select assessment_uuid, cycle_uuid, country_iso from public.activity_log where time > $1 order by time desc`,
-        [jobCreatedAt],
-        (row) => Objects.camelize(row)
-      )
-
-      await Promise.all(
-        newActivityLogEntries.map(async (entry) => {
-          const { assessmentUuid, cycleUuid, countryIso } = entry
-          const assessment = assessments.find((assessment) => assessment.uuid === assessmentUuid)
-          const cycle = assessment?.cycles.find((cycle) => cycle.uuid === cycleUuid)
-
-          if (assessment && cycle && countryIso) {
+      await Promises.each(assessments, (assessment) =>
+        Promises.each(assessment.cycles, async (cycle) => {
+          const countryISOs = await AreaController.getCountries({ assessment, cycle })
+          return Promises.each(countryISOs, async ({ countryIso }) => {
             const props = { assessment, cycle, countryIso }
             Logger.info(
               `[System schedulers] ** ${name} Refreshing materialized view for ${assessment.props.name} ${cycle.name}} ${countryIso}`
             )
-            await CountryActivityLogRepository.refreshMaterializedView(props)
-          }
+            return CountryActivityLogRepository.refreshMaterializedView(props)
+          })
         })
       )
 
