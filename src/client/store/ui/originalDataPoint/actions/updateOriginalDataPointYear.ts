25536f041d2b683f9337177650e90c86585cb8dc
sorja <ping@mirosorja.com>
2023-10-23 10:12:24 +0300

    Refetch reserved years

--- src/client/store/ui/originalDataPoint/actions/updateOriginalDataPointYear.ts
+++ src/client/store/ui/originalDataPoint/actions/updateOriginalDataPointYear.ts
@@ -6,6 +6,10 @@ import { ApiEndPoint } from 'meta/api/endpoint'
 import { CycleParams } from 'meta/api/request'
 import { OriginalDataPoint } from 'meta/assessment'
 
+import { AppDispatch } from 'client/store/store'
+
+import { getOriginalDataPointReservedYears } from './getOriginalDataPointReservedYears'
+
 type Props = CycleParams & {
   originalDataPoint: OriginalDataPoint
   id: string
@@ -14,13 +18,14 @@ type Props = CycleParams & {
 }
 
 const putOriginalDataPointYear = Functions.debounce(
-  async (props: Props) => {
+  async (props: Props, dispatch: AppDispatch) => {
     const { countryIso, assessmentName, cycleName, year, targetYear, id } = props
 
     const params = { countryIso, assessmentName, cycleName, sectionName: 'extentOfForest' }
     const config = { params }
     const data = { id, year, targetYear }
     await axios.put(ApiEndPoint.CycleData.OriginalDataPoint.year(), data, config)
+    dispatch(getOriginalDataPointReservedYears(params))
   },
   1000,
   'updateOriginalDataPointYear'
@@ -28,8 +33,8 @@ const putOriginalDataPointYear = Functions.debounce(
 
 export const updateOriginalDataPointYear = createAsyncThunk<OriginalDataPoint, Props>(
   'originalDataPoint/year/update',
-  async (props) => {
-    putOriginalDataPointYear(props)
+  async (props, { dispatch }) => {
+    putOriginalDataPointYear(props, dispatch)
     return props.originalDataPoint
   }
 )
