49360425a36f8fab787dbfc9053c8799e9d86356
minotogna <mino.togna@gmail.com>
2023-10-25 12:50:49 +0200

    Print page: fetch descriptions per country

--- src/client/pages/Print/FraPrint/hooks/useGetDescriptionValues.ts
+++ src/client/pages/Print/FraPrint/hooks/useGetDescriptionValues.ts
@@ -4,22 +4,14 @@ import { CountryIso } from 'meta/area'
 
 import { useAppDispatch } from 'client/store'
 import { DataActions } from 'client/store/data'
-import { useIsPrintRoute } from 'client/hooks/useIsRoute'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 
-type Props = { sectionName: string }
-
-export const useGetDescriptionValues = (props: Props): void => {
-  const { sectionName } = props
-
+export const useGetDescriptionValues = (): void => {
   const dispatch = useAppDispatch()
-  const { print } = useIsPrintRoute()
   const { assessmentName, cycleName, countryIso: _countryIso } = useCountryRouteParams()
   const countryIso = _countryIso as CountryIso
 
   useEffect(() => {
-    if (!print) {
-      dispatch(DataActions.getDescription({ countryIso, assessmentName, cycleName, sectionName }))
-    }
-  }, [assessmentName, countryIso, cycleName, dispatch, print, sectionName])
+    dispatch(DataActions.getDescription({ countryIso, assessmentName, cycleName }))
+  }, [assessmentName, countryIso, cycleName, dispatch])
 }
