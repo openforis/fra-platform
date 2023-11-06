3a5cfedc251f5700b72346fdcf2595710d4bf216
minotogna <mino.togna@gmail.com>
2023-11-06 11:01:53 +0100

    3199 - Fix Forest validation with previous cycle (fetch external dependencies)

--- src/client/store/data/actions/getTableData.ts
+++ src/client/store/data/actions/getTableData.ts
@@ -7,12 +7,13 @@ import { RecordAssessmentData } from 'meta/data'
 
 type Props = CycleParams & {
   tableNames: Array<string>
+  mergeOdp?: boolean
 }
 
 export const getTableData = createAsyncThunk<RecordAssessmentData, Props>('data/tableData/get', async (props) => {
-  const { countryIso, assessmentName, cycleName, tableNames } = props
+  const { countryIso, assessmentName, cycleName, tableNames, mergeOdp = false } = props
 
-  const params = { assessmentName, countryIso, cycleName, tableNames, countryISOs: [countryIso], mergeOdp: false }
+  const params = { assessmentName, countryIso, cycleName, tableNames, countryISOs: [countryIso], mergeOdp }
   const { data } = await axios.get(ApiEndPoint.CycleData.Table.tableData(), { params })
 
   return data
