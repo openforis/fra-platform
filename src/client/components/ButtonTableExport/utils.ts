8fa4ecf3c2fdc193b2a181fc3655aeef8bff902c
minotogna <mino.togna@gmail.com>
2023-11-07 10:53:52 +0100

    3154 - fix CSV download auto complete field

--- src/client/components/ButtonTableExport/utils.ts
+++ src/client/components/ButtonTableExport/utils.ts
@@ -1,52 +1,68 @@
 // Inspiration/base from cheerio-tableparser
 //
+
 const normalizeString = (string = '') => string.trim().replace(/\s/g, ' ')
 
-const getElementText = ({ element }: any): any => {
+const _getElementText = (element: HTMLElement): string => {
   const { children, innerText } = element
 
   if (element.classList.contains('no-csv')) return ''
 
   if (element.nodeName === 'SELECT') {
-    return normalizeString(element.options[element.selectedIndex].text)
+    const select = element as HTMLSelectElement
+    return normalizeString(select.options[select.selectedIndex].text)
   }
+
+  if (element.classList.contains('autocomplete-input__wrapper')) {
+    const input = Array.from(children).find((x) => x.nodeName === 'INPUT') as HTMLInputElement
+    return normalizeString(input.value)
+  }
+
   if (children.length > 0) {
     return Array.from(children).reduce(
-      (text, child) => normalizeString(`${text} ${getElementText({ element: child })}`),
+      (text, child) => normalizeString(`${text} ${_getElementText(child as HTMLElement)}`),
       ''
     )
   }
   return normalizeString(innerText)
 }
 
-export const getData = (tableElement: any, dupCols = true, dupRows = true, textMode = true, formatToNumber = true) => {
-  if (!tableElement) {
+export const getData = (
+  table: HTMLTableElement,
+  dupCols = true,
+  dupRows = true,
+  textMode = true,
+  formatToNumber = true
+): Array<object> => {
+  if (!table) {
     return []
   }
   // Initialize variables
-  const columns: any = []
+  const columns: Array<Array<string>> = []
   let currentX = 0
   let currentY = 0
-  Array.from(tableElement.rows).forEach((row) => {
+  Array.from(table.rows).forEach((row) => {
     currentY = 0
     // Handle both table haders and table cells
-    Array.from((row as any).cells).forEach((column) => {
-      const { rowSpan, colSpan }: any = column
-      let content = textMode ? getElementText({ element: column }) : (column as any).innerHTML
+    Array.from(row.cells).forEach((column) => {
+      const { rowSpan, colSpan } = column
+      let content = textMode ? _getElementText(column) : column.innerHTML
       if (formatToNumber)
         content = Number.isNaN(Number.parseFloat(content.replace(/\s/g, ''))) ? content : content.replace(/\s/g, '')
       // Handle spanning cells
       for (let x = 0; x < rowSpan; x += 1) {
         for (let y = 0; y < colSpan; y += 1) {
-          if (columns[currentY + y] === undefined) {
+          if (!columns[currentY + y]) {
             columns[currentY + y] = []
           }
-          while (columns[currentY + y][currentX + x] !== undefined) {
+
+          while (columns[currentY + y][currentX + x]) {
             currentY += 1
-            if (columns[currentY + y] === undefined) {
+            if (!columns[currentY + y]) {
               columns[currentY + y] = []
             }
           }
+
           const condition = (x === 0 || dupRows) && (y === 0 || dupCols)
           columns[currentY + y][currentX + x] = condition ? content : ''
         }
@@ -56,5 +72,5 @@ export const getData = (tableElement: any, dupCols = true, dupRows = true, textM
     currentX += 1
   })
   // transpose matrix
-  return columns[0].map((_: any, i: any) => columns.map((row: any) => row[i]))
+  return columns[0].map((_, i) => columns.map((row) => row[i]))
 }
