f08f3de1f8279178b5d7f2d524903bb7ce5b47e7
Miró Sorja <ping@mirosorja.com>
2023-11-07 08:54:38 +0100

    update originalDataPointVariables

--- src/test/migrations/steps/20231103112625-step-update-odp-view-subsubvalue-logic.ts
+++ src/test/migrations/steps/20231103112625-step-update-odp-view-subsubvalue-logic.ts
@@ -1,15 +1,20 @@
+import { Promises } from 'utils/promises'
+
 import { TableNames } from 'meta/assessment'
 
 import { AreaController } from 'server/controller/area'
 import { AssessmentController } from 'server/controller/assessment'
+import { getOriginalDataPointVariables } from 'server/controller/cycleData/originalDataPoint/getOriginalDataPointVariables'
 import { BaseProtocol, Schemas } from 'server/db'
 import { getCreateSchemaCycleOriginalDataPointViewDDL } from 'server/repository/assessment/assessment/getCreateSchemaDDL'
 import { DataRedisRepository } from 'server/repository/redis/data'
 
-export default async (client: BaseProtocol) => {
-  const assessment = await AssessmentController.getOne({ assessmentName: 'fra' }, client)
+import { updateCalculatedVariable } from 'test/migrations/steps/utils/updateCalculatedVariable'
 
-  // eslint-disable-next-line no-restricted-syntax
+export default async (client: BaseProtocol) => {
+  const assessment = await AssessmentController.getOne({ assessmentName: 'fra', metaCache: true }, client)
+
+  // // eslint-disable-next-line no-restricted-syntax
   for (let i = 0; i < assessment.cycles.length; i += 1) {
     const cycle = assessment.cycles[i]
     const schemaCycle = Schemas.getNameCycle(assessment, cycle)
@@ -27,5 +32,13 @@ export default async (client: BaseProtocol) => {
     const getCountriesDataProps = { assessment, cycle, countryISOs, tables, force: true }
     // eslint-disable-next-line no-await-in-loop
     await DataRedisRepository.getCountriesData(getCountriesDataProps, client)
+
+    const originalDataPointVariables = getOriginalDataPointVariables({ cycle })
+    // eslint-disable-next-line no-await-in-loop
+    await Promises.each(originalDataPointVariables, async (variable) => {
+      // eslint-disable-next-line no-await-in-loop
+      const updateVariablesProps = { assessment, cycle, ...variable }
+      await updateCalculatedVariable(updateVariablesProps, client)
+    })
   }
 }
