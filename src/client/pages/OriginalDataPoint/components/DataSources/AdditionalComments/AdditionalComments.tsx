a3d7ad883e9b29dba3f51e1cfdc59ccd66b90b8b
Miró Sorja <ping@mirosorja.com>
2023-11-08 12:38:30 +0100

    Move OriginalDataPoint/Datasources to components structure

---
+++ src/client/pages/OriginalDataPoint/components/DataSources/AdditionalComments/AdditionalComments.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { OriginalDataPoint } from 'meta/assessment'
+import { Topics } from 'meta/messageCenter'
+
+import ReviewIndicator from 'client/components/ReviewIndicator'
+import VerticallyGrowingTextField from 'client/components/VerticallyGrowingTextField'
+
+type AdditionalCommentsProps = {
+  disabled: boolean
+  originalDataPoint: OriginalDataPoint
+  reviewIndicator: boolean
+  updateOriginalDataPoint: (originalDataPoint: OriginalDataPoint) => void
+}
+const AdditionalComments: React.FC<AdditionalCommentsProps> = (props) => {
+  const { reviewIndicator, disabled, originalDataPoint, updateOriginalDataPoint } = props
+  const { t } = useTranslation()
+
+  return (
+    <tr>
+      <th className="fra-table__header-cell-left">{t('nationalDataPoint.additionalComments')}</th>
+      <td className="fra-table__cell-left odp__data-source-input-column">
+        <VerticallyGrowingTextField
+          value={originalDataPoint.dataSourceAdditionalComments || ''}
+          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+            const caret = event.target.selectionStart
+            const element = event.target
+            window.requestAnimationFrame(() => {
+              element.selectionStart = caret
+              element.selectionEnd = caret
+            })
+            const originalDataPointUpdate = {
+              ...originalDataPoint,
+              dataSourceAdditionalComments: event.target.value,
+            }
+            updateOriginalDataPoint(originalDataPointUpdate)
+          }}
+          disabled={disabled}
+        />
+      </td>
+      {reviewIndicator && (
+        <td className="fra-table__review-cell no-print">
+          <ReviewIndicator
+            title={t('nationalDataPoint.additionalComments')}
+            subtitle={t('nationalDataPoint.dataSources')}
+            topicKey={Topics.getOdpReviewTopicKey(originalDataPoint.id, 'dataSourceAdditionalComments')}
+          />
+        </td>
+      )}
+    </tr>
+  )
+}
+
+export default AdditionalComments
---
+++ src/client/pages/OriginalDataPoint/components/DataSources/MethodsUsed/MethodsUsed.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { ODPDataSourceMethod, OriginalDataPoint } from 'meta/assessment/originalDataPoint'
+import { Topics } from 'meta/messageCenter'
+
+import MultiSelect from 'client/components/MultiSelect'
+import ReviewIndicator from 'client/components/ReviewIndicator'
+
+type MethodsUsedProps = {
+  originalDataPoint: OriginalDataPoint
+  updateOriginalDataPoint: (originalDataPoint: OriginalDataPoint) => void
+  disabled: boolean
+  reviewIndicator: boolean
+}
+
+const MethodsUsed: React.FC<MethodsUsedProps> = (props) => {
+  const { disabled, reviewIndicator, originalDataPoint, updateOriginalDataPoint } = props
+  const { t } = useTranslation()
+
+  return (
+    <tr>
+      <th className="fra-table__header-cell-left">{t('nationalDataPoint.methodsUsed')}</th>
+      <td className="fra-table__cell-left odp__data-source-input-column">
+        <MultiSelect
+          disabled={disabled}
+          values={originalDataPoint.dataSourceMethods ?? []}
+          options={Object.values(ODPDataSourceMethod).map((method) => ({
+            value: method,
+            label: t(`nationalDataPoint.dataSourceMethodsOptions.${method}`),
+          }))}
+          onChange={(values: Array<ODPDataSourceMethod>) => {
+            const originalDataPointUpdate = { ...originalDataPoint, dataSourceMethods: values }
+            updateOriginalDataPoint(originalDataPointUpdate)
+          }}
+        />
+      </td>
+      {reviewIndicator && (
+        <td className="fra-table__review-cell no-print">
+          <ReviewIndicator
+            title={t('nationalDataPoint.methodsUsed')}
+            subtitle={t('nationalDataPoint.dataSources')}
+            topicKey={Topics.getOdpReviewTopicKey(originalDataPoint.id, 'dataSourceMethods')}
+          />
+        </td>
+      )}
+    </tr>
+  )
+}
+
+export default MethodsUsed
