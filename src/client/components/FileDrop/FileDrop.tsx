b179444ff609dac3e2214755c2b82701595d0821
Miró Sorja <ping@mirosorja.com>
2023-11-13 12:51:54 +0100

    Add filedrop

---
+++ src/client/components/FileDrop/FileDrop.tsx
@@ -0,0 +1,37 @@
+import './FileDrop.scss'
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
+import { useTranslation } from 'react-i18next'
+
+import classNames from 'classnames'
+
+type Props = {
+  onDrop: (files: Array<File>) => void
+}
+
+const FileDrop: React.FC<Props> = (props: Props) => {
+  const { onDrop } = props
+  const { t } = useTranslation()
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  return (
+    <div
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...getRootProps()}
+      className={classNames('file-drop', {
+        'dropzone--isActive': isDragActive,
+      })}
+    >
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <div>{t('Drop files here')}</div>
+      ) : (
+        <div>{t('Drag and drop some files here, or click to select files')}</div>
+      )}
+    </div>
+  )
+}
+
+export default FileDrop
