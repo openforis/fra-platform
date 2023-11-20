4c478394422c4e0ee5611a735bbdcef7e5adc5ff
minotogna <mino.togna@gmail.com>
2023-11-20 13:08:41 +0100

    3123- update panEu views + linked nodes calc formulas

--- src/server/repository/assessmentCycle/data/index.ts
+++ src/server/repository/assessmentCycle/data/index.ts
@@ -1,11 +1,13 @@
 import { clearTableData } from './clearTableData'
+import { createOrReplaceTableDataView } from './createOrReplaceTableDataView'
 import { getAggregatedTableData } from './getAggregatedTableData'
 import { getOriginalDataPointData } from './getOriginalDataPointData'
 import { getTableData } from './getTableData'
 
 export const DataRepository = {
-  getTableData,
-  getOriginalDataPointData,
-  getAggregatedTableData,
   clearTableData,
+  createOrReplaceTableDataView,
+  getAggregatedTableData,
+  getOriginalDataPointData,
+  getTableData,
 }
