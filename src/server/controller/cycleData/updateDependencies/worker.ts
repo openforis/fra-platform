9a5e81298246de230c0c84fc92b771ed70f86ca6
Mino Togna <minotogna@users.noreply.github.com>
2023-11-21 11:54:45 +0100

    Heroku fix: move _scheduleExternalDependantsUpdate in workerFactory (#3248)
    * move _scheduleExternalDependantsUpdate in workerFactory
    
    * move _scheduleExternalDependantsUpdate in workerFactory

--- src/server/controller/cycleData/updateDependencies/worker.ts
+++ src/server/controller/cycleData/updateDependencies/worker.ts
@@ -1,11 +1,3 @@
-import { Promises } from 'utils/promises'
-
-import { NodeUpdates } from 'meta/data'
-import { User } from 'meta/user'
-
-import { AreaController } from 'server/controller/area'
-import { AssessmentController } from 'server/controller/assessment'
-import { scheduleUpdateDependencies } from 'server/controller/cycleData/updateDependencies/scheduleUpdateDependencies'
 import { Logger } from 'server/utils/logger'
 
 import { ContextFactory } from './context'
@@ -22,25 +14,6 @@ const _getLogKey = (job: UpdateDependenciesJob): string => {
   return `[updateDependencies-workerThread] [${[assessmentName, cycleName, countryIso].join('-')}] [job-${job.id}]`
 }
 
-const _scheduleExternalDependantsUpdate = async (props: {
-  logKey: string
-  nodeUpdates: NodeUpdates
-  user: User
-}): Promise<void> => {
-  const { logKey, nodeUpdates, user } = props
-
-  const { assessmentName, cycleName, countryIso } = nodeUpdates
-
-  const propsAssessment = { assessmentName, cycleName, metaCache: true }
-  const { assessment, cycle } = await AssessmentController.getOneWithCycle(propsAssessment)
-  const country = await AreaController.getCountry({ assessment, cycle, countryIso })
-
-  if (country) {
-    Logger.info(`${logKey} scheduling updates ${assessmentName}-${cycleName} of ${nodeUpdates.nodes.length} nodes.`)
-    await scheduleUpdateDependencies({ assessment, cycle, nodeUpdates, includeSourceNodes: true, user })
-  }
-}
-
 export default async (job: UpdateDependenciesJob) => {
   const logKey = _getLogKey(job)
   try {
@@ -52,16 +25,11 @@ export default async (job: UpdateDependenciesJob) => {
     const result = updateCalculationDependencies({ context, jobId: job.id })
     await persistResults({ result, user })
 
-    // schedule external assessment/cycle updates
-    await Promises.each(context.externalDependants, (nodeUpdates) =>
-      _scheduleExternalDependantsUpdate({ logKey, nodeUpdates, user })
-    )
-
     const resultNodeUpdates = result.nodeUpdates
     const duration = (new Date().getTime() - time) / 1000
     Logger.info(`${logKey} ended in ${duration} seconds with ${resultNodeUpdates.nodes.length} updated nodes.`)
 
-    return { nodeUpdates: resultNodeUpdates }
+    return { nodeUpdates: resultNodeUpdates, externalDependants: context.externalDependants }
   } catch (error) {
     Logger.error(`${logKey} Error.`)
     Logger.error(error)
