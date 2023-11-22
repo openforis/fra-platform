e7efb34e9ca1976e649813233f8fc1c03c2ebfda
Miró Sorja <ping@mirosorja.com>
2023-11-22 14:45:56 +0100

    Add locale to request header

--- src/client/store/middleware/axios.ts
+++ src/client/store/middleware/axios.ts
@@ -2,11 +2,20 @@ import axios, { AxiosStatic } from 'axios'
 import { Action, Dispatch, Middleware, MiddlewareAPI } from 'redux'
 import { UUIDs } from 'utils/uuids'
 
+import { Lang } from 'meta/lang'
+
 import { NotificationActions } from 'client/store/ui/notification/slice'
 
 const createAxiosMiddleware =
   (axios: AxiosStatic): Middleware =>
-  ({ dispatch }: MiddlewareAPI) => {
+  ({ dispatch, getState }: MiddlewareAPI) => {
+    axios.interceptors.request.use((config) => {
+      const userLanguage = getState().user?.props?.lang ?? localStorage.getItem('i18n/lang') ?? Lang.en
+      // eslint-disable-next-line no-param-reassign
+      config.headers.locale = userLanguage
+      return config
+    })
+
     axios.interceptors.response.use(null, (error: any) => {
       dispatch(
         NotificationActions.addMessage({
