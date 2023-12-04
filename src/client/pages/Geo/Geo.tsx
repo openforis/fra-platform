11b8db88310fe28c4f9e5e66dbb52b59129514b7
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2023-12-04 10:19:15 -0500

    Geo reviewers only access for geo and beta version message (#3277)
    * 3269 - Add reviewer and admin access only to the geo module
    
    * 3269 - Add FRA Geo - Beta version message

--- src/client/pages/Geo/Geo.tsx
+++ src/client/pages/Geo/Geo.tsx
@@ -1,11 +1,30 @@
 import './geo.scss'
 import React, { Suspense } from 'react'
+import { Navigate } from 'react-router-dom'
 
+import { Routes } from 'meta/routes'
+import { Users } from 'meta/user'
+
+import { useCycle } from 'client/store/assessment'
+import { useUser } from 'client/store/user'
+import { useCountryIso } from 'client/hooks'
 import Loading from 'client/components/Loading'
 
 const GeoMapWrapper = React.lazy(() => import('./GeoMapWrapper'))
 
 const Geo: React.FC = () => {
+  const user = useUser()
+  const countryIso = useCountryIso()
+  const cycle = useCycle()
+
+  const isReviewer = Users.isReviewer(user, countryIso, cycle)
+
+  const isAdmin = Users.isAdministrator(user)
+
+  if (!isReviewer && !isAdmin) {
+    return <Navigate to={Routes.Cycle.path.relative} replace />
+  }
+
   return (
     <div className="geo-container">
       <Suspense fallback={<Loading />}>
