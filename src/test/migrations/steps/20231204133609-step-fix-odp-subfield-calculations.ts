2a4ba1cf3bd47107fd08cd06a485183b8e1683c0
sorja <ping@mirosorja.com>
2023-12-06 11:54:10 +0200

    Update update dependencies

--- src/test/migrations/steps/20231204133609-step-fix-odp-subfield-calculations.ts
+++ src/test/migrations/steps/20231204133609-step-fix-odp-subfield-calculations.ts
@@ -1,3 +1,5 @@
+import { Promises } from 'utils/promises'
+
 import { TableNames } from 'meta/assessment'
 
 import { AreaController } from 'server/controller/area'
@@ -11,25 +13,16 @@ import { updateODPDependencies } from 'test/migrations/steps/utils/updateODPDepe
 export default async (client: BaseProtocol) => {
   const assessment = await AssessmentController.getOne({ assessmentName: 'fra', metaCache: true }, client)
 
-  // // eslint-disable-next-line no-restricted-syntax
-  for (let i = 0; i < assessment.cycles.length; i += 1) {
-    const cycle = assessment.cycles[i]
+  await Promises.each(assessment.cycles, async (cycle) => {
     const schemaCycle = Schemas.getNameCycle(assessment, cycle)
     const odpViewQuery = getCreateSchemaCycleOriginalDataPointViewDDL(schemaCycle)
-    // eslint-disable-next-line no-await-in-loop
     await client.query(odpViewQuery)
-    // eslint-disable-next-line no-await-in-loop
     const countryISOs = (await AreaController.getCountries({ assessment, cycle }, client)).map((c) => c.countryIso)
 
-    // eslint-disable-next-line no-await-in-loop
-    const tables = {
-      [TableNames.originalDataPointValue]: {},
-    }
+    const tables = { [TableNames.originalDataPointValue]: {} }
     const getCountriesDataProps = { assessment, cycle, countryISOs, tables, force: true }
-    // eslint-disable-next-line no-await-in-loop
     await DataRedisRepository.getCountriesData(getCountriesDataProps, client)
 
-    // eslint-disable-next-line no-await-in-loop
     await updateODPDependencies({ assessment, cycle }, client)
-  }
+  })
 }
--- src/test/migrations/steps/utils/updateODPDependencies.ts
+++ src/test/migrations/steps/utils/updateODPDependencies.ts
@@ -1,18 +1,14 @@
+import { Objects } from 'utils/objects'
 import { Promises } from 'utils/promises'
 
 import { CountryIso } from 'meta/area'
 import { Assessment, Cycle } from 'meta/assessment'
-import { NodeUpdate, NodeUpdates } from 'meta/data'
+import { NodeUpdate } from 'meta/data'
 
-import { AreaController } from 'server/controller/area'
-import { CycleDataController } from 'server/controller/cycleData'
 import { getOriginalDataPointVariables } from 'server/controller/cycleData/originalDataPoint/getOriginalDataPointVariables'
-import { ContextFactory } from 'server/controller/cycleData/updateDependencies/context'
-import { updateCalculationDependencies } from 'server/controller/cycleData/updateDependencies/updateCalculationDependencies'
-import { BaseProtocol, DB } from 'server/db'
-import { NodeDb, NodeRepository } from 'server/repository/assessmentCycle/node'
-import { DataRedisRepository } from 'server/repository/redis/data'
-import { Logger } from 'server/utils/logger'
+import { BaseProtocol, DB, Schemas } from 'server/db'
+
+import { updateDependencies } from './updateDependencies'
 
 type Props = {
   assessment: Assessment
@@ -21,53 +17,35 @@ type Props = {
 
 export const updateODPDependencies = async (props: Props, client: BaseProtocol = DB): Promise<void> => {
   const { assessment, cycle } = props
-  const countryISOs = (await AreaController.getCountries({ assessment, cycle }, client)).map((c) => c.countryIso)
+  const countryNodes: Record<CountryIso, Array<NodeUpdate>> = {} as Record<CountryIso, Array<NodeUpdate>>
+
   const originalDataPointVariables = getOriginalDataPointVariables({ cycle })
 
-  // eslint-disable-next-line no-await-in-loop
-  await Promises.each(countryISOs, async (countryIso) => {
-    const allNodesDb: Array<NodeDb> = []
-    const allNodes: Array<{ nodes: Record<string, NodeUpdate[]>; countryIso: CountryIso }> = []
+  const schemaCycle = Schemas.getNameCycle(assessment, cycle)
+  const originalDataPoints = await client.map<{ countryIso: CountryIso; year: number }>(
+    `
+        select country_iso, year
+        from ${schemaCycle}.original_data_point
+        where national_classes != '[]'::jsonb
+  `,
+    [],
+    (res) => Objects.camelize(res)
+  )
 
-    const getOriginalDataPointsProps = { assessment, cycle, countryIso }
-    const originalDataPoints = await CycleDataController.getOriginalDataPoints(getOriginalDataPointsProps, client)
+  await Promises.each(originalDataPoints, async ({ countryIso, year }) => {
+    countryNodes[countryIso] = []
 
-    if (originalDataPoints.length === 0) return
+    const colName = String(year)
+    const opdNodes = originalDataPointVariables.map<NodeUpdate>(({ tableName, variableName }) => {
+      return { tableName, variableName, colName, value: undefined }
+    })
+    countryNodes[countryIso].push(...opdNodes)
+  })
 
-    const nodes: Array<NodeUpdate> = originalDataPoints.reduce((acc, originalDataPoint) => {
-      const colName = String(originalDataPoint.year)
-      const opdNodes = originalDataPointVariables.map<NodeUpdate>(({ tableName, variableName }) => {
-        return { tableName, variableName, colName, value: undefined }
-      })
-      acc.push(...opdNodes)
-      return acc
-    }, [])
-
-    const assessmentName = assessment.props.name
-    const cycleName = cycle.name
-
-    const nodeUpdates: NodeUpdates = { assessmentName, cycleName, countryIso, nodes }
-    const contextProps = { assessment, cycle, isODP: true, nodeUpdates, includeSourceNodes: true }
-    const context = await ContextFactory.newInstance(contextProps, client)
-    const result = updateCalculationDependencies({ context, jobId: `migration_step-${Date.now()}` })
-
-    if (result.nodesDb.length > 0) {
-      allNodesDb.push(...result.nodesDb)
-      allNodes.push({ nodes: result.nodes, countryIso })
-    }
-
-    try {
-      if (allNodesDb.length > 0) {
-        await NodeRepository.massiveInsert({ assessment, cycle, nodes: allNodesDb }, client)
-        await Promise.all(
-          allNodes.map(async ({ countryIso, nodes }) => {
-            await DataRedisRepository.updateNodes({ assessment, cycle, countryIso, nodes })
-          })
-        )
-      }
-    } catch (e) {
-      Logger.error('Persisting nodes failed')
-      Logger.error(e)
-    }
+  await updateDependencies({
+    assessment,
+    cycle,
+    isODP: true,
+    countryNodes,
   })
 }
