32868e9838ceb9b0e3d50736ab14eb5857fc4a1c
sorja <ping@mirosorja.com>
2023-12-08 11:05:14 +0200

    Update MultiSelect value type

--- src/client/components/TableNodeExt/CellNodeExt/CellMultiselect/CellMultiselect.tsx
+++ src/client/components/TableNodeExt/CellNodeExt/CellMultiselect/CellMultiselect.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import { CellProps } from '../CellProps'
 
-const CellMultiselect: React.FC<CellProps & { value: string }> = (props: CellProps & { value: string }) => {
+const CellMultiselect: React.FC<CellProps & { value: Array<string> }> = (
+  props: CellProps & { value: Array<string> }
+) => {
   const { value } = props
-  return <div>{Array.isArray(value) ? value?.join(',') : value}</div>
+  return <div>{value.join(',')}</div>
 }
 
 export default CellMultiselect
