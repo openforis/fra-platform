5bd81c18ed1a335ac174bce5b4a140b16c83e935
sorja <ping@mirosorja.com>
2023-12-21 12:58:20 +0200

    ActivityLog: Fix tableValuesClear entries

---
+++ src/test/migrations/steps/20231221125643-step-fix-activity-log-tableValuesClear.ts
@@ -0,0 +1,11 @@
+import { BaseProtocol } from 'server/db'
+
+export default async (client: BaseProtocol) => {
+  client.query(`
+      update public.activity_log
+      set country_iso = target ->>'countryIso', target = target - 'countryIso'
+      where message = 'tableValuesClear'
+        and country_iso is null
+          returning *
+    `)
+}
