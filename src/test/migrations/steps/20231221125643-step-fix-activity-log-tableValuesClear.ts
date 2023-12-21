621715350e0d11e5859b5612c2b31667f2cf91f1
sorja <ping@mirosorja.com>
2023-12-21 13:03:10 +0200

    Reformat query

--- src/test/migrations/steps/20231221125643-step-fix-activity-log-tableValuesClear.ts
+++ src/test/migrations/steps/20231221125643-step-fix-activity-log-tableValuesClear.ts
@@ -1,11 +1,12 @@
 import { BaseProtocol } from 'server/db'
 
 export default async (client: BaseProtocol) => {
-  client.query(`
+  await client.query(`
       update public.activity_log
-      set country_iso = target ->>'countryIso', target = target - 'countryIso'
+      set
+          country_iso = target ->>'countryIso',
+          target = target - 'countryIso'
       where message = 'tableValuesClear'
         and country_iso is null
-          returning *
     `)
 }
