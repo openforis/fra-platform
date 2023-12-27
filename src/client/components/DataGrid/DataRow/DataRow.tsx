789eab520b663c32cc28d8c816881fde36c18766
minotogna <mino.togna@gmail.com>
2023-12-27 16:13:52 +0100

    3174 - data grid: add data row and update common review style

---
+++ src/client/components/DataGrid/DataRow/DataRow.tsx
@@ -0,0 +1,59 @@
+import React, { ReactElement, ReactNode } from 'react'
+
+import ButtonDelete from 'client/components/Buttons/ButtonDelete'
+import DataCell from 'client/components/DataGrid/DataCell'
+import ReviewIndicator from 'client/components/ReviewIndicator'
+
+export type DataRowActions = {
+  delete?: {
+    onDelete?: () => void
+    placeholder?: ReactNode
+  }
+  review?: {
+    placeholder?: ReactNode
+    subTitle?: string
+    title: string
+    topicKey: string
+  }
+}
+
+export type DataRowProps = {
+  actions?: DataRowActions
+  children: ReactElement | Array<ReactElement>
+}
+
+const DataRow: React.FC<DataRowProps> = (props) => {
+  const { actions, children } = props
+
+  const { delete: deleteRow, review } = actions
+
+  // TODO: Add review opened cells style
+  // const openTopics = useTopicKeys()
+  // const reviewOpened = useMemo<boolean>(() => openTopics.includes(topicKey), [openTopics, topicKey])
+
+  return (
+    <>
+      {React.Children.map(children, (child) => React.cloneElement(child))}
+
+      {deleteRow && (
+        <DataCell bordered review>
+          {deleteRow.placeholder ?? <ButtonDelete onClick={deleteRow.onDelete} />}
+        </DataCell>
+      )}
+
+      {review && (
+        <DataCell review>
+          {review.placeholder ?? (
+            <ReviewIndicator title={review.title} subtitle={review.subTitle} topicKey={review.topicKey} />
+          )}
+        </DataCell>
+      )}
+    </>
+  )
+}
+
+DataRow.defaultProps = {
+  actions: {},
+}
+
+export default DataRow
