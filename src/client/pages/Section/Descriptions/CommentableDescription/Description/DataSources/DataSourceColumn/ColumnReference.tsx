c110337b6b0cee3fc1890a2e03a516d2561d4b3a
sorja <ping@mirosorja.com>
2024-01-08 10:47:18 +0200

    Remove old textarea editor

--- src/client/pages/Section/Descriptions/CommentableDescription/Description/DataSources/DataSourceColumn/ColumnReference.tsx
+++ src/client/pages/Section/Descriptions/CommentableDescription/Description/DataSources/DataSourceColumn/ColumnReference.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import classNames from 'classnames'
@@ -7,38 +7,30 @@ import { DataSource } from 'meta/assessment'
 import { TooltipId } from 'meta/tooltip'
 
 import { DataCell } from 'client/components/DataGrid'
-import Icon from 'client/components/Icon'
-import TextArea from 'client/components/Inputs/TextArea'
 
 import { datasourceValidators } from './datasourceValidators'
 
 interface DataSourceReferenceColumnProps {
   dataSourceValue: DataSource
-  placeholder: boolean
   disabled: boolean
-  onChange: (key: string, value: Record<string, string>) => void
+  onChange: (key: string, value: string) => void
   lastRow: boolean
 }
 
 const ColumnReference: React.FC<DataSourceReferenceColumnProps> = (props: DataSourceReferenceColumnProps) => {
-  const { dataSourceValue, placeholder, disabled, onChange, lastRow } = props
+  const { dataSourceValue, disabled, onChange, lastRow } = props
   const { t } = useTranslation()
 
-  const [toggleLinkField, setToggleLinkField] = useState(false)
-
   const _onChange = useCallback(
-    (field: string, value: string) => {
-      onChange('reference', {
-        ...(dataSourceValue.reference ?? {}),
-        [field]: value,
-      })
+    (value: string) => {
+      onChange('reference', value)
     },
-    [dataSourceValue.reference, onChange]
+    [onChange]
   )
 
   const validationError = useMemo(() => {
-    return datasourceValidators.referenceText(dataSourceValue.reference?.text)
-  }, [dataSourceValue.reference?.text])
+    return datasourceValidators.referenceText(dataSourceValue.reference)
+  }, [dataSourceValue.reference])
 
   return (
     <DataCell
@@ -49,39 +41,8 @@ const ColumnReference: React.FC<DataSourceReferenceColumnProps> = (props: DataSo
         'validation-error': validationError,
       })}
     >
-      {!disabled && !toggleLinkField && (
-        <TextArea
-          disabled={disabled}
-          onChange={(event) => _onChange('text', event.target.value)}
-          value={dataSourceValue.reference?.text ?? ''}
-        />
-      )}
-
-      {!disabled && toggleLinkField && (
-        <TextArea
-          disabled={disabled}
-          onChange={(event) => _onChange('link', event.target.value)}
-          value={dataSourceValue.reference?.link ?? ''}
-        />
-      )}
-
-      {disabled && (
-        <span className="text-input__readonly-view">
-          {dataSourceValue.reference?.link ? (
-            <a href={dataSourceValue.reference?.link} target="_blank" rel="noreferrer">
-              {dataSourceValue.reference?.text}
-            </a>
-          ) : (
-            dataSourceValue.reference?.text
-          )}
-        </span>
-      )}
-
-      {!placeholder && !disabled && (
-        <button type="button" onClick={() => setToggleLinkField(!toggleLinkField)}>
-          {toggleLinkField ? <Icon name="text_fields" /> : <Icon name="insert_link" />}
-        </button>
-      )}
+      {disabled && <div>{dataSourceValue.reference}</div>}
+      {!disabled && <div>{dataSourceValue.reference}</div>}
     </DataCell>
   )
 }
