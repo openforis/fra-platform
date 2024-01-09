f513f2a6b24dd540994d0e1819f5f5e71e407500
minotogna <mino.togna@gmail.com>
2024-01-09 17:59:00 +0100

    3372 - update es/fr/zh translations

---
+++ src/i18n/resources/en/admin.js
@@ -0,0 +1,8 @@
+module.exports = {
+  admin: 'Admin',
+  filter: 'Filter by',
+  invitationPending: 'Invitation pending',
+  invitationsAcceptedCount: 'Nº of Invitations accepted',
+  invitationsSentCount: 'Nº of Invitations sent',
+  usersCount: 'Nº of Users',
+}
---
+++ src/i18n/resources/fr/admin.js
@@ -0,0 +1,8 @@
+module.exports = {
+  admin: `Administration`,
+  filter: `Filtrer par`,
+  invitationPending: `Invitation en attente`,
+  invitationsAcceptedCount: `Nombre d'invitations acceptées`,
+  invitationsSentCount: `Nombre d'invitations envoyées`,
+  usersCount: `Nombre d'utilisateurs`,
+}
