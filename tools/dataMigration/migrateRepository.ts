1930d00a3fcab78a970ba8b8303b3290848787f6
sorja <ping@mirosorja.com>
2024-01-11 17:18:22 +0200

    Revert "dataMigration: files and repository"
    This reverts commit b0cba2fbc8655410e906d06a077377602e4e00c0.

--- tools/dataMigration/migrateRepository.ts
+++ tools/dataMigration/migrateRepository.ts
@@ -1,6 +1,6 @@
 import { Assessment } from '../../src/meta/assessment/assessment'
-import { BaseProtocol, Schemas } from '../../src/server/db'
-import { Promises } from '../../src/utils/promises'
+import { BaseProtocol } from '../../src/server/db'
+import { DBNames } from './_DBNames'
 
 type Props = {
   assessment: Assessment
@@ -10,40 +10,15 @@ type Props = {
 export const migrateRepository = async (props: Props): Promise<void> => {
   const { assessment, client } = props
 
+  const schema = DBNames.getAssessmentSchema(assessment.props.name)
+
   await client.query(
     `
-            delete from public.file;
-            create table if not exists public.file
-          (
-            id         bigserial    not null,
-            uuid       uuid         not null default uuid_generate_v4(),
-            file_name  varchar(255) not null,
-            file       bytea        not null,
-            created_at timestamp    not null default now(),
-            updated_at timestamp,
-            primary key (id),
-            unique (uuid)
-          );
-                
-          insert into public.file (id, file_name, file)
-          select id, file_name, file
-          from _legacy.repository;
+        delete from ${schema}.file;
+
+        insert into ${schema}.file (country_iso, file_name, file)
+        select country_iso, file_name, file
+        from _legacy.repository;
     `
   )
-
-  await Promises.each(assessment.cycles, async (cycle) => {
-    const schemaCycle = Schemas.getNameCycle(assessment, cycle)
-
-    await client.query(`
-      delete from ${schemaCycle}.repository;
-
-      insert into ${schemaCycle}.repository (country_iso, file_uuid, name)
-      select
-          r.country_iso,
-          f.uuid,
-          r.file_name
-      from _legacy.repository r
-      inner join public.file f using (file_name)
-         `)
-  })
 }
