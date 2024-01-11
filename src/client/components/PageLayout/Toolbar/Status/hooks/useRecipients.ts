82dd7c8626372d0ff7232df3c87d52d63449bf13
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-01-11 11:50:59 -0500

    Sort collaborators list when updating status to approval

--- src/client/components/PageLayout/Toolbar/Status/hooks/useRecipients.ts
+++ src/client/components/PageLayout/Toolbar/Status/hooks/useRecipients.ts
@@ -1,12 +1,55 @@
-import { User } from 'meta/user'
+import { useMemo } from 'react'
+
+import { AssessmentStatus, CountryIso } from 'meta/area'
+import { RoleName, User, Users } from 'meta/user'
 import { UserRoles } from 'meta/user/userRoles'
 
+import { useCycle } from 'client/store/assessment'
 import { useUsers } from 'client/store/ui/userManagement'
+import { useUser } from 'client/store/user'
+import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import { StatusTransition } from 'client/components/PageLayout/Toolbar/Status/types'
 
-export const useRecipients = (props: { status: StatusTransition }): Array<User> => {
+const approvalRolesOrder: Partial<Record<RoleName, number>> = {
+  [RoleName.ADMINISTRATOR]: 0,
+  [RoleName.REVIEWER]: 1,
+  [RoleName.COLLABORATOR]: 2,
+}
+
+type Props = {
+  status: StatusTransition
+}
+
+type Returned = Array<User>
+
+export const useRecipients = (props: Props): Returned => {
+  const { status } = props
   const users = useUsers()
-  return users.filter((user) =>
-    user.roles.find((role) => UserRoles.getRecipientRoles(props.status).includes(role.role))
-  )
+  const currentUser = useUser()
+  const cycle = useCycle()
+  const { countryIso } = useCountryRouteParams<CountryIso>()
+
+  return useMemo<Returned>(() => {
+    const userList = users.filter((user) =>
+      user.roles.find((role) => currentUser.id !== user.id && UserRoles.getRecipientRoles(status).includes(role.role))
+    )
+    if (status.status !== AssessmentStatus.approval) return userList
+
+    const getApprovalRoleOrder = (user: User): number => {
+      const role = Users.getRole(user, countryIso, cycle)?.role
+      const rolesOrderLength = Object.keys(approvalRolesOrder).length
+      if (role !== undefined) return approvalRolesOrder[role as RoleName] ?? rolesOrderLength
+
+      return rolesOrderLength
+    }
+    const approvalStatusSort = (userA: User, userB: User): number => {
+      const roleOrderA = getApprovalRoleOrder(userA)
+      const roleOrderB = getApprovalRoleOrder(userB)
+      if (roleOrderA !== roleOrderB) return roleOrderA - roleOrderB
+
+      return userA.props?.name?.localeCompare(userB.props?.name) || 0
+    }
+
+    return userList.sort(approvalStatusSort)
+  }, [countryIso, currentUser, cycle, status, users])
 }
