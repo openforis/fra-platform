f3e1096308a2530b0b3cebf14cc600c2aa3f6dc7
Mino Togna <minotogna@users.noreply.github.com>
2024-01-12 13:45:52 +0100

    3249 - Tables 1a/1b chart: only one trend solid line connecting value points (#3406)

--- src/client/store/area/extraReducers/updateCountryPropReducer.ts
+++ src/client/store/area/extraReducers/updateCountryPropReducer.ts
@@ -5,7 +5,7 @@ import { updateCountryProp } from 'client/store/area/actions/updateCountryProp'
 import { AreaState } from 'client/store/area/state'
 
 export const updateCountryPropReducer = (builder: ActionReducerMapBuilder<AreaState>) => {
-  builder.addCase(updateCountryProp.pending, (state, action) => {
+  builder.addCase(updateCountryProp.fulfilled, (state, action) => {
     const { assessmentName, countryIso, countryProp, cycleName } = action.meta.arg
 
     const path = ['countries', assessmentName, cycleName, countryIso, 'props']
