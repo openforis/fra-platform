80a0328699eccb46ae495e053909a2815b6c4a2a
sorja <ping@mirosorja.com>
2024-01-29 13:24:45 +0200

    Fix issue persisting data with Jodit editor

--- src/client/components/EditorWYSIWYG/EditorWYSIWYG.tsx
+++ src/client/components/EditorWYSIWYG/EditorWYSIWYG.tsx
@@ -1,19 +1,20 @@
 import './EditorWYSIWYG.scss'
-import React, { useCallback, useMemo, useRef } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import classNames from 'classnames'
-import type { Jodit } from 'jodit/types/jodit'
-import JoditEditor from 'jodit-react'
+import JoditEditor, { IJoditEditorProps, Jodit } from 'jodit-react'
 import rehypeParse from 'rehype-parse'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import { unified } from 'unified'
 
+type JoditConfig = IJoditEditorProps['config']
+
 type Props = {
   disabled?: boolean
   onChange: (value?: string) => void
-  options?: Partial<Jodit['options']>
+  options?: Partial<JoditConfig>
   value: string
 }
 
@@ -38,25 +39,32 @@ const processor = unified().use(rehypeRaw).use(rehypeSanitize).use(rehypeParse, 
 
 const EditorWYSIWYG: React.FC<Props> = (props: Props) => {
   const { disabled, onChange, options, value } = props
+  const [joditEditor, setEditor] = useState<Jodit>(null)
   const editor = useRef(null)
 
   // all options from https://xdsoft.net/jodit/docs/
-  const config = useMemo<Partial<Jodit['options']>>(
+  // Do not change readonly or toolbar
+  const config = useMemo<Partial<JoditConfig>>(
     () => ({
       addNewLine: false,
       buttons,
       enter: 'div',
       placeholder: '',
-      readonly: disabled,
+      // We initially set readonly to true here, and then toggle it in useEffect
+      readonly: true,
       spellcheck: true,
       statusbar: false,
-      toolbar: !disabled,
       toolbarAdaptive: false,
       toolbarButtonSize: 'small',
       uploader: undefined,
+      events: {
+        afterInit: (instance: Jodit) => {
+          setEditor(instance)
+        },
+      },
       ...options,
     }),
-    [options, disabled]
+    [options]
   )
 
   // Sanitize user input on save
@@ -67,6 +75,11 @@ const EditorWYSIWYG: React.FC<Props> = (props: Props) => {
     },
     [onChange]
   )
+
+  useEffect(() => {
+    if (joditEditor) joditEditor.setReadOnly(disabled)
+    if (joditEditor?.container) joditEditor.toolbarContainer.classList.toggle('toolbar-disabled', disabled)
+  }, [disabled, joditEditor])
 
   return (
     <div className={classNames('editorWYSIWYG')}>
