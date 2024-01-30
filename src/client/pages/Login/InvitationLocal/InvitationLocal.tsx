a6dc22111eee7c705b9e0d3c24669c6d3cc637f2
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-01-30 13:11:13 -0500

    Use redux form in InvitationLocal

--- src/client/pages/Login/InvitationLocal/InvitationLocal.tsx
+++ src/client/pages/Login/InvitationLocal/InvitationLocal.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link, Navigate } from 'react-router-dom'
+
+import { Objects } from 'utils/objects'
 
 import { LoginInvitationQueryParams, Routes } from 'meta/routes'
 import { AuthProvider } from 'meta/user'
 import { UserRoles } from 'meta/user/userRoles'
 
-import { useInvitation } from 'client/store/login'
+import { useAppDispatch } from 'client/store'
+import { LoginActions, useInvitation } from 'client/store/login'
+import { useAcceptInvitationForm } from 'client/store/login/hooks'
 import { useUser } from 'client/store/user'
 import { useSearchParams } from 'client/hooks/useSearchParams'
-import AcceptInvitationButtons from 'client/pages/Login/components/AcceptInvitationButtons'
 import { useInitInvitation } from 'client/pages/Login/Invitation/hooks/useInitInvitation'
-
-import { useOnAcceptInvitationLocal } from './hooks/useOnAcceptInvitationLocal'
 
 export type InvitationLocalFormData = {
   email: string
@@ -22,6 +23,7 @@ export type InvitationLocalFormData = {
 
 const InvitationLocal: React.FC = () => {
   const { t } = useTranslation()
+  const dispatch = useAppDispatch()
   const loggedUser = useUser()
 
   // Init invitation in case the component is accessed directly through the URL
@@ -30,12 +32,8 @@ const InvitationLocal: React.FC = () => {
   const { invitationUuid, lang } = useSearchParams<LoginInvitationQueryParams>()
   const { assessment, invitedUser, userProviders, userRole } = useInvitation()
 
-  const [formData, setFormData] = useState<InvitationLocalFormData>({
-    email: '',
-    password: '',
-    password2: '',
-  })
-  const [errors, setErrors] = useState<Record<string, string>>({})
+  const formData = useAcceptInvitationForm()
+  const errors = formData?.errors ?? {}
 
   const cycle = assessment?.cycles.find((cycle) => cycle.uuid === userRole.cycleUuid)
   const assessmentName = assessment?.props.name
@@ -46,10 +44,10 @@ const InvitationLocal: React.FC = () => {
   const showForgotPassword = !userProviders || userProviders.includes(AuthProvider.local)
 
   useEffect(() => {
-    if (invitedUser?.email && formData.email.length === 0) setFormData({ ...formData, email: invitedUser.email })
-  }, [formData, invitedUser?.email])
-
-  const onAcceptInvitation = useOnAcceptInvitationLocal({ formData, invitationUuid, setErrors, showPassword2 })
+    if (invitedUser?.email && Objects.isEmpty(formData?.email)) {
+      dispatch(LoginActions.updateAcceptInvitationForm({ email: invitedUser.email }))
+    }
+  }, [dispatch, formData, invitedUser?.email])
 
   if (!invitedUser) return null
 
@@ -72,31 +70,31 @@ const InvitationLocal: React.FC = () => {
         <input
           disabled={!!invitedUser}
           name="email"
-          onFocus={() => setErrors({ ...errors, email: null })}
-          onChange={(event) => setFormData({ ...formData, email: event.target.value })}
+          onFocus={() => LoginActions.updateAcceptInvitationFormErrors({ email: null })}
+          onChange={(event) => dispatch(LoginActions.updateAcceptInvitationForm({ email: event.target.value }))}
           placeholder={t('login.email')}
           type="text"
-          value={formData.email}
+          value={formData?.email ?? ''}
         />
         {errors.email?.length > 0 && <span className="login__field-error">{t(errors.email)}</span>}
 
         <input
-          onFocus={() => setErrors({ ...errors, password: null })}
-          onChange={(event) => setFormData({ ...formData, password: event.target.value })}
+          onFocus={() => LoginActions.updateAcceptInvitationFormErrors({ password: null })}
+          onChange={(event) => dispatch(LoginActions.updateAcceptInvitationForm({ password: event.target.value }))}
           placeholder={t('login.password')}
           type="password"
-          value={formData.password}
+          value={formData?.password ?? ''}
         />
         {errors.password?.length > 0 && <span className="login__field-error">{t(errors.password)}</span>}
 
         {showPassword2 && (
           <>
             <input
-              onFocus={() => setErrors({ ...errors, password2: null })}
-              onChange={(event) => setFormData({ ...formData, password2: event.target.value })}
+              onFocus={() => LoginActions.updateAcceptInvitationFormErrors({ password2: null })}
+              onChange={(event) => dispatch(LoginActions.updateAcceptInvitationForm({ password2: event.target.value }))}
               placeholder={t('login.repeatPassword')}
               type="password"
-              value={formData.password2}
+              value={formData?.password2 ?? ''}
             />
             {errors.password2?.length > 0 && <span className="login__field-error">{t(errors.password2)}</span>}
           </>
@@ -114,8 +112,6 @@ const InvitationLocal: React.FC = () => {
             {t('login.forgotPassword')}
           </Link>
         )}
-
-        <AcceptInvitationButtons onAcceptInvitationLocalClick={onAcceptInvitation} />
       </div>
     </div>
   )
