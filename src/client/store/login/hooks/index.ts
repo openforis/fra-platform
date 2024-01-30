bf80a7b34d4d2ef91ad1252f2ca14bdc958df3e4
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-01-30 13:04:32 -0500

    Add Accept invitation form redux state

--- src/client/store/login/hooks/index.ts
+++ src/client/store/login/hooks/index.ts
@@ -1,4 +1,7 @@
 import { useAppSelector } from 'client/store'
-import { InvitationState } from 'client/store/login/stateType'
+import { AcceptInvitationFormState, InvitationState } from 'client/store/login/stateType'
 
 export const useInvitation = (): InvitationState | undefined => useAppSelector((state) => state.login?.invitation)
+
+export const useAcceptInvitationForm = (): AcceptInvitationFormState | undefined =>
+  useAppSelector((state) => state.login?.invitation?.acceptForm)
--- src/client/store/login/stateType.ts
+++ src/client/store/login/stateType.ts
@@ -1,11 +1,24 @@
 import { Assessment } from 'meta/assessment'
 import { AuthProvider, RoleName, User, UserRole } from 'meta/user'
 
+export type AcceptInvitationFormFields = {
+  email?: string
+  password?: string
+  password2?: string
+}
+
+export type AcceptInvitationErrors = Partial<Record<keyof AcceptInvitationFormFields, string | null>>
+
+export type AcceptInvitationFormState = AcceptInvitationFormFields & {
+  errors?: AcceptInvitationErrors
+}
+
 export interface InvitationState {
   assessment?: Assessment
   invitedUser?: User
   userProviders?: Array<AuthProvider>
   userRole?: UserRole<RoleName>
+  acceptForm?: AcceptInvitationFormState
 }
 
 export interface LoginState {
