0cc3b75d7783bc4314ab72f91401cd479c052560
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-01-31 08:31:33 -0500

    Remove function prop from AcceptInvitationButtons

--- src/client/pages/Login/Invitation/Invitation.tsx
+++ src/client/pages/Login/Invitation/Invitation.tsx
@@ -4,50 +4,36 @@ import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 
 import { Assessments } from 'meta/assessment'
 import { LoginInvitationQueryParams, Routes } from 'meta/routes'
-import { AuthProvider, Users } from 'meta/user'
+import { Users } from 'meta/user'
 import { UserRoles } from 'meta/user/userRoles'
 
 import { useAppDispatch } from 'client/store'
 import { LoginActions, useInvitation } from 'client/store/login'
-import { useAcceptInvitationForm } from 'client/store/login/hooks'
 import { useUser } from 'client/store/user'
-import { useIsInvitationLocalRoute } from 'client/hooks/useIsRoute'
 import { useSearchParams } from 'client/hooks/useSearchParams'
 import AcceptInvitationButtons from 'client/pages/Login/components/AcceptInvitationButtons'
 import AccessLimited from 'client/pages/Login/components/AccessLimited'
 
 import { useInitInvitation } from './hooks/useInitInvitation'
-import { useOnAcceptInvitationLocal } from './hooks/useOnAcceptInvitationLocal'
 
 const Invitation: React.FC = () => {
   const dispatch = useAppDispatch()
   const { t } = useTranslation()
   const navigate = useNavigate()
   const loggedUser = useUser()
-  const isInInvitationLocal = useIsInvitationLocalRoute()
 
   useInitInvitation()
 
-  const { invitationUuid, lang } = useSearchParams<LoginInvitationQueryParams>()
+  const { invitationUuid } = useSearchParams<LoginInvitationQueryParams>()
   const { assessment, invitedUser, userProviders, userRole } = useInvitation()
 
   const cycle = assessment?.cycles.find((cycle) => cycle.uuid === userRole.cycleUuid)
   const assessmentName = assessment?.props.name
   const cycleName = cycle?.name
 
-  const formData = useAcceptInvitationForm()
-  const showPassword2 =
-    (invitedUser && !userProviders) || (userProviders && !userProviders.includes(AuthProvider.local))
-
-  const onAcceptInvitationLocal = useOnAcceptInvitationLocal({ formData, invitationUuid, showPassword2 })
-
   const onAccept = () => {
     dispatch(LoginActions.acceptInvitation({ invitationUuid }))
     navigate(Routes.Root.generatePath())
-  }
-
-  const goToAcceptInvitationLocal = () => {
-    navigate(Routes.LoginInvitationLocal.generatePath({ assessmentName, cycleName }, { invitationUuid, lang }))
   }
 
   if (userRole?.acceptedAt) {
@@ -90,9 +76,7 @@ const Invitation: React.FC = () => {
         </button>
       ) : (
         <div className="login__form">
-          <AcceptInvitationButtons
-            onAcceptInvitationLocalClick={isInInvitationLocal ? onAcceptInvitationLocal : goToAcceptInvitationLocal}
-          />
+          <AcceptInvitationButtons />
         </div>
       )}
 
--- src/client/pages/Login/components/AcceptInvitationButtons/AcceptInvitationButtons.tsx
+++ src/client/pages/Login/components/AcceptInvitationButtons/AcceptInvitationButtons.tsx
@@ -1,32 +1,50 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
-import { LoginInvitationQueryParams } from 'meta/routes'
+import { LoginInvitationQueryParams, Routes } from 'meta/routes'
+import { AuthProvider } from 'meta/user'
 
 import { useInvitation } from 'client/store/login'
+import { useAcceptInvitationForm } from 'client/store/login/hooks'
+import { useIsInvitationLocalRoute } from 'client/hooks/useIsRoute'
 import { useSearchParams } from 'client/hooks/useSearchParams'
 import Icon from 'client/components/Icon'
 import { videoResources } from 'client/pages/Tutorials'
 
-type Props = {
-  onAcceptInvitationLocalClick: () => void
-}
+import { useOnAcceptInvitationLocal } from './hooks/useOnAcceptInvitationLocal'
 
-const AcceptInvitationButtons: React.FC<Props> = (props) => {
-  const { onAcceptInvitationLocalClick } = props
+const AcceptInvitationButtons: React.FC = () => {
   const { i18n, t } = useTranslation()
+  const navigate = useNavigate()
 
-  const { invitationUuid } = useSearchParams<LoginInvitationQueryParams>()
-  const { userRole, assessment } = useInvitation()
+  const { invitationUuid, lang } = useSearchParams<LoginInvitationQueryParams>()
+  const { assessment, invitedUser, userProviders, userRole } = useInvitation()
 
   const cycle = assessment?.cycles.find((cycle) => cycle.uuid === userRole.cycleUuid)
   const assessmentName = assessment?.props.name
   const cycleName = cycle?.name
 
+  const isInInvitationLocal = useIsInvitationLocalRoute()
+
+  const formData = useAcceptInvitationForm()
+  const showPassword2 =
+    (invitedUser && !userProviders) || (userProviders && !userProviders.includes(AuthProvider.local))
+
+  const onAcceptInvitationLocal = useOnAcceptInvitationLocal({ formData, invitationUuid, showPassword2 })
+
+  const goToAcceptInvitationLocal = () => {
+    navigate(Routes.LoginInvitationLocal.generatePath({ assessmentName, cycleName }, { invitationUuid, lang }))
+  }
+
   return (
     <>
-      <button type="button" className="btn" onClick={onAcceptInvitationLocalClick}>
+      <button
+        type="button"
+        className="btn"
+        onClick={isInInvitationLocal ? onAcceptInvitationLocal : goToAcceptInvitationLocal}
+      >
         {t('login.acceptInvitationWithFra')}
       </button>
 
