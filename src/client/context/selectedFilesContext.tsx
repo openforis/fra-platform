6dc9da75048e50657fe863d5e355f551e8fb226d
sorja <ping@mirosorja.com>
2024-02-01 10:41:24 +0200

    3214 - introduce client/context and add selectedFilesContext

--- src/client/context/selectedFilesContext.tsx
+++ src/client/context/selectedFilesContext.tsx
@@ -1,31 +1,18 @@
 import React, { createContext, Dispatch, SetStateAction, useContext, useMemo, useState } from 'react'
 
-import { AssessmentFile } from 'meta/cycleData'
-
-/**
- * @deprecated
- */
 type SelectedFilesContextType = {
-  selectedFiles: AssessmentFile[]
-  setSelectedFiles: Dispatch<SetStateAction<AssessmentFile[]>>
+  selectedFiles: FileList
+  setSelectedFiles: Dispatch<SetStateAction<FileList>>
 }
-/**
- * @deprecated
- */
+
 const SelectedFilesContext = createContext<SelectedFilesContextType | undefined>(undefined)
 
-/**
- * @deprecated
- */
 export const SelectedFilesProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [selectedFiles, setSelectedFiles] = useState<Array<AssessmentFile>>([])
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null)
 
   const value = useMemo<SelectedFilesContextType>(() => ({ selectedFiles, setSelectedFiles }), [selectedFiles])
 
   return <SelectedFilesContext.Provider value={value}>{children}</SelectedFilesContext.Provider>
 }
 
-/**
- * @deprecated
- */
 export const useSelectedFileContext = (): SelectedFilesContextType => useContext(SelectedFilesContext)
