1e54d37c7e682ebeb1c2478748906643a303ecb2
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-02-02 22:32:06 -0500

    Remove NationalClassesDownload component

--- src/client/pages/OriginalDataPoint/components/NationalClasses/components/NationalClassesTable.tsx
+++ src/client/pages/OriginalDataPoint/components/NationalClasses/components/NationalClassesTable.tsx
@@ -7,7 +7,6 @@ import { useCycle } from 'client/store/assessment'
 import { useIsPrintRoute } from 'client/hooks/useIsRoute'
 import { DataCell, DataGrid } from 'client/components/DataGrid'
 import NationalClass from 'client/pages/OriginalDataPoint/components/NationalClasses/components/NationalClass'
-import NationalClassesDownload from 'client/pages/OriginalDataPoint/components/NationalClasses/components/NationalClassesDownload'
 import { useCanEditData } from 'client/pages/OriginalDataPoint/hooks/useCanEditData'
 
 type Props = {
@@ -23,27 +22,24 @@ export const NationalClassesTable = (props: Props) => {
   const canEdit = useCanEditData(originalDataPoint)
 
   return (
-    <>
-      <NationalClassesDownload nationalClasses={nationalClasses} year={year} />
-      <DataGrid gridTemplateColumns={`${print ? `100px ` : ''}minmax(240px, 40%) 1fr`} withActions={canEdit}>
-        {print && (
-          <DataCell gridRow={`1/${nationalClasses.length + 2}`} header lastRow>
-            {year}
-          </DataCell>
-        )}
-
-        <DataCell header>
-          {t(`nationalDataPoint.${cycle.name === '2025' ? 'nationalClassifications' : 'nationalClass'}`)}
+    <DataGrid gridTemplateColumns={`${print ? `100px ` : ''}minmax(240px, 40%) 1fr`} withActions={canEdit}>
+      {print && (
+        <DataCell gridRow={`1/${nationalClasses.length + 2}`} header lastRow>
+          {year}
         </DataCell>
-        <DataCell header lastCol>
-          {t('nationalDataPoint.definition')}
-        </DataCell>
-        {canEdit && <div />}
+      )}
 
-        {nationalClasses.map((nationalClass, idx) => (
-          <NationalClass index={idx} key={nationalClass.uuid} originalDataPoint={originalDataPoint} />
-        ))}
-      </DataGrid>
-    </>
+      <DataCell header>
+        {t(`nationalDataPoint.${cycle.name === '2025' ? 'nationalClassifications' : 'nationalClass'}`)}
+      </DataCell>
+      <DataCell header lastCol>
+        {t('nationalDataPoint.definition')}
+      </DataCell>
+      {canEdit && <div />}
+
+      {nationalClasses.map((nationalClass, idx) => (
+        <NationalClass index={idx} key={nationalClass.uuid} originalDataPoint={originalDataPoint} />
+      ))}
+    </DataGrid>
   )
 }
