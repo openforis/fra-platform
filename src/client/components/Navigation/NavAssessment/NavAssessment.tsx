5a197396dae4f50acbe9718d1b468ede03214d70
minotogna <mino.togna@gmail.com>
2024-02-05 10:48:31 +0100

    3317 - Improve desktop and mobile layout

--- src/client/components/Navigation/NavAssessment/NavAssessment.tsx
+++ src/client/components/Navigation/NavAssessment/NavAssessment.tsx
@@ -1,15 +1,26 @@
 import './NavAssessment.scss'
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import MediaQuery from 'react-responsive'
+import { Link } from 'react-router-dom'
 
 import { Objects } from 'utils/objects'
 
+import { Areas, CountryIso } from 'meta/area'
+import { Routes } from 'meta/routes'
+
 import { useSections } from 'client/store/metadata'
+import { useCountryRouteParams } from 'client/hooks/useRouteParams'
+import Icon from 'client/components/Icon'
 import Header from 'client/components/Navigation/NavAssessment/Header'
 import NavigationSection from 'client/components/Navigation/NavAssessment/Section'
+import { Breakpoints } from 'client/utils'
 
 import { useMaxHeight } from './hooks/useMaxHeight'
 
 const NavAssessment: React.FC = () => {
+  const { t } = useTranslation()
+  const { assessmentName, cycleName, countryIso } = useCountryRouteParams<CountryIso>()
   const sections = useSections()
 
   const maxHeight = useMaxHeight()
@@ -24,6 +35,23 @@ const NavAssessment: React.FC = () => {
       {sections.map((section) => (
         <NavigationSection key={section.uuid} section={section} showSections={showSections} />
       ))}
+
+      {Areas.isGlobal(countryIso) && (
+        <MediaQuery minWidth={Breakpoints.laptop}>
+          <div className="nav-header__sep-container">
+            <div className="hr" />
+          </div>
+          <Link
+            className="nav-section__header nav-assessment__bulk-download"
+            to={Routes.CountryDataDownload.generatePath({ assessmentName, cycleName, countryIso })}
+          >
+            <div className="nav-section__order">
+              <Icon className="icon-sub icon-white" name="hit-down" />
+            </div>
+            <div className="nav-section__label">{t('dataDownload.dataDownload')}</div>
+          </Link>
+        </MediaQuery>
+      )}
     </div>
   )
 }
--- src/client/components/Navigation/NavAssessment/hooks/useMaxHeight.ts
+++ src/client/components/Navigation/NavAssessment/hooks/useMaxHeight.ts
@@ -1,35 +1,44 @@
 import { useEffect, useRef, useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+import { Breakpoints } from 'client/utils'
 
 // document height - toolbar
 const baseHeight = `100dvh - 50px`
 
 export const useMaxHeight = (): string => {
-  const [maxHeight, setMaxHeight] = useState<string>(`calc(${baseHeight})`)
+  const laptop = useMediaQuery({ minWidth: Breakpoints.laptop })
+  const [maxHeight, setMaxHeight] = useState<string>(laptop ? 'unset' : `calc(${baseHeight})`)
   const heightsRef = useRef<Record<string, number>>({})
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          heightsRef.current[entry.target.tagName] = entry.intersectionRect.height
-        })
+    if (laptop) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            heightsRef.current[entry.target.tagName] = entry.intersectionRect.height
+          })
 
-        const heightsCalc = Object.values(heightsRef.current)
-          .map((v) => `${v}px`)
-          .join(' - ')
+          const heightsCalc = Object.values(heightsRef.current)
+            .map((v) => `${v}px`)
+            .join(' - ')
 
-        setMaxHeight(`calc(100dvh - 50px - ${heightsCalc})`)
-      },
-      { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] }
-    )
+          setMaxHeight(`calc(100dvh - 50px - ${heightsCalc})`)
+        },
+        { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] }
+      )
 
-    observer.observe(document.getElementsByTagName('header')[0])
-    observer.observe(document.getElementsByTagName('footer')[0])
+      observer.observe(document.getElementsByTagName('header')[0])
+      observer.observe(document.getElementsByTagName('footer')[0])
 
-    return () => {
-      observer.disconnect()
+      return () => {
+        observer.disconnect()
+      }
     }
-  }, [])
+
+    setMaxHeight('unset')
+    return undefined
+  }, [laptop])
 
   return maxHeight
 }
