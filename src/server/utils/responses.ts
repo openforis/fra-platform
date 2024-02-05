76a210094cb7185c49d8e15c59b81bdbe6c536f8
sorja <ping@mirosorja.com>
2024-02-05 12:10:48 +0200

    3214 - Response utils: Send file

--- src/server/utils/responses.ts
+++ src/server/utils/responses.ts
@@ -4,6 +4,9 @@ import { AssessmentFile } from 'meta/assessment'
 
 import Requests from 'server/utils/requests'
 
+/**
+ * @deprecated
+ */
 const sendAssessmentFile = (res: Response, assessmentFile: AssessmentFile): void => {
   if (assessmentFile?.file) {
     const fileName = encodeURIComponent(assessmentFile.fileName)
@@ -14,6 +17,18 @@ const sendAssessmentFile = (res: Response, assessmentFile: AssessmentFile): void
     Requests.send404(res)
   }
 }
+
+const sendFile = (res: Response, fileName: string, file: Buffer): void => {
+  if (file) {
+    const _fileName = encodeURIComponent(fileName)
+    res.setHeader('Content-Disposition', `attachment; filename="${_fileName}"; filename*=utf-8''${_fileName}`)
+    res.end(file, 'binary')
+  } else {
+    Requests.send404(res)
+  }
+}
+
 export const Responses = {
   sendAssessmentFile,
+  sendFile,
 }
