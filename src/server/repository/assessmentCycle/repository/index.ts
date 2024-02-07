054c723bcd9600afba8e0b05d339d28c8661ee9e
sorja <ping@mirosorja.com>
2024-02-07 09:38:55 +0200

    3214 - server: update repository item

--- src/server/repository/assessmentCycle/repository/index.ts
+++ src/server/repository/assessmentCycle/repository/index.ts
@@ -2,10 +2,12 @@ import { create } from './create'
 import { getMany } from './getMany'
 import { getOne } from './getOne'
 import { remove } from './remove'
+import { update } from './update'
 
 export const RepositoryRepository = {
   create,
   remove,
-  getOne,
+  update,
   getMany,
+  getOne,
 }
