71216524fc6d2f51c20f082dfc0669329080b24e
sorja <ping@mirosorja.com>
2024-02-07 09:31:06 +0200

    3214 - add file adapter

---
+++ src/server/repository/adapter/file.ts
@@ -0,0 +1,19 @@
+import { Objects } from 'utils/objects'
+
+import { File } from 'meta/file'
+
+export type FileDB = {
+  id: number
+  uuid: string
+  file_name: string
+  file: Buffer
+  created_at: string
+}
+
+export const FileAdapter = (fileDB: FileDB): File => {
+  const { file, ...rest } = fileDB
+  return {
+    ...Objects.camelize(rest),
+    file,
+  }
+}
