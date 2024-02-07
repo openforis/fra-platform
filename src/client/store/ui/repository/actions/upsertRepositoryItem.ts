0a18292e7a00ea9117fc3b9773f9e3e5051779bc
sorja <ping@mirosorja.com>
2024-02-07 09:51:56 +0200

    3214 - redux: update repository item

--- src/client/store/ui/repository/actions/upsertRepositoryItem.ts
+++ src/client/store/ui/repository/actions/upsertRepositoryItem.ts
@@ -12,21 +12,43 @@ type Props = CycleParams & {
   repositoryItem: RepositoryItem
 }
 
+const updateRepositoryItem = async (props: Props) => {
+  const { file, repositoryItem } = props
+  const { assessmentName, cycleName, countryIso } = props
+  const formData = new FormData()
+  formData.append('repositoryItem', JSON.stringify(repositoryItem))
+
+  if (file) formData.append('file', file)
+
+  const params = { countryIso, assessmentName, cycleName }
+  const config = { params }
+
+  const { data } = await axios.patch(ApiEndPoint.CycleData.Repository.one(), formData, config)
+  return data
+}
+
+const createRepositoryItem = async (props: Props): Promise<RepositoryItem> => {
+  const { file, repositoryItem } = props
+  const { assessmentName, cycleName, countryIso } = props
+  const formData = new FormData()
+  formData.append('name', repositoryItem?.name)
+  formData.append('link', repositoryItem?.link)
+
+  if (file) formData.append('file', file)
+
+  const params = { countryIso, assessmentName, cycleName }
+  const config = { params }
+
+  const { data } = await axios.post(ApiEndPoint.CycleData.Repository.one(), formData, config)
+  return data
+}
+
 export const upsertRepositoryItem = createAsyncThunk<RepositoryItem, Props, ThunkApiConfig>(
   'repositoryItem/upsert',
   async (props) => {
-    const { file, repositoryItem } = props
-    const { assessmentName, cycleName, countryIso } = props
-    const formData = new FormData()
-    formData.append('name', repositoryItem?.name)
-    formData.append('link', repositoryItem?.link)
-
-    if (file) formData.append('file', file)
-
-    const params = { countryIso, assessmentName, cycleName }
-    const config = { params }
-    const { data } = await axios.post(ApiEndPoint.CycleData.Repository.one(), formData, config)
-
-    return data
+    const repositoryItem = props.repositoryItem.uuid
+      ? await updateRepositoryItem(props)
+      : await createRepositoryItem(props)
+    return repositoryItem
   }
 )
