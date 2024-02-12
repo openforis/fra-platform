fef02548fb6ec05174e1a0a52431906c94de8e45
minotogna <mino.togna@gmail.com>
2024-02-12 12:58:06 +0100

    3437 - rename YearForDataSource

--- src/client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/DataSourceRow.tsx
+++ src/client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/DataSourceRow.tsx
@@ -4,11 +4,11 @@ import { DataSource, SectionName } from 'meta/assessment'
 import { DataSourceDescription } from 'meta/assessment/description/nationalDataDataSourceDescription'
 
 import { DataRow } from 'client/components/DataGrid'
-import YearForDataSource from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/Columns/ColumnYearForDataSource'
 import Comments from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/Columns/Comments'
 import Reference from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/Columns/Reference'
 import TypeOfDataSource from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/Columns/TypeOfDataSource'
 import Variables from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/Columns/Variables'
+import YearForDataSource from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/Columns/YearForDataSource'
 import { useDataSourceActions } from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/hooks/useDataSourceActions'
 
 type Props = {
