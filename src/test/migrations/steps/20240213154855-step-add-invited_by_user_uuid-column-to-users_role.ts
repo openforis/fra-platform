1bfb48c884d6efb9bbd17678561be22da8809b1f
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-02-16 10:34:21 -0500

    Fix migration step: populate invited_by_user_uuid column

--- src/test/migrations/steps/20240213154855-step-add-invited_by_user_uuid-column-to-users_role.ts
+++
@@ -1,8 +0,0 @@
-import { BaseProtocol } from 'server/db'
-
-export default async (client: BaseProtocol) => {
-  await client.query(`
-    ALTER TABLE public.users_role
-    ADD COLUMN invited_by_user_uuid uuid
-  `)
-}
