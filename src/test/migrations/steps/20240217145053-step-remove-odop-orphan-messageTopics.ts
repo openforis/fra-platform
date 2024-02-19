57f54347749d4061a18b180acd74348d855ef2fc
minotogna <mino.togna@gmail.com>
2024-02-19 11:37:16 +0100

    3505 - add delete contacts orphans

--- src/test/migrations/steps/20240217145053-step-remove-odop-orphan-messageTopics.ts
+++ src/test/migrations/steps/20240217145053-step-remove-odop-orphan-messageTopics.ts
@@ -12,6 +12,7 @@ export default async (client: BaseProtocol) => {
         assessment.cycles.map(async (cycle) => {
           const schemaCycle = Schemas.getNameCycle(assessment, cycle)
 
+          // delete odp orphans
           await client.query(`
               with topics as
                   (select t.id                               as topic_id
@@ -32,6 +33,29 @@ export default async (client: BaseProtocol) => {
                     (select t.topic_id
                      from topics_odp t
                      where t.odp_id is null)
+          `)
+
+          // delete contacts oprphans
+          await client.query(`
+              with topics as
+                  (select t.id                            as topic_id
+                        , split_part(t.key, '_', 2)::uuid as contact_uuid
+                        , t.country_iso
+                   from ${schemaCycle}.message_topic t
+                   where t.key like 'contact_%')
+                 , topics_data as
+                  (select t.topic_id
+                        , t.contact_uuid
+                        , t.country_iso
+                        , n.uuid as node_uuid
+                   from topics t
+                            left outer join ${schemaCycle}.node_ext n on n.uuid = t.contact_uuid)
+              delete
+              from ${schemaCycle}.message_topic mt
+              where mt.id in
+                    (select t.topic_id
+                     from topics_data t
+                     where t.contact_uuid is null)
           `)
         })
       )
