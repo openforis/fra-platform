692c96d242a085e52a5a9765ff5e5151c9a419b3
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-02-21 09:32:51 -0500

    3516 - ar -Update Growing stock composition text

--- src/i18n/resources/ar.js
+++ src/i18n/resources/ar.js
@@ -753,7 +753,7 @@ The FRA team
   growingStockComposition: {
     native: 'أصلية',
     introduced: 'مدخلة',
-    growingStockComposition: 'تركيبة مخزون الأشجار الحية',
+    growingStockComposition: 'تكوين مخزون الأشجار الحيّة بالغابات',
     categoryHeader: 'فئات تقييم الموارد الحرجية',
     nativeTreeSpecies: 'أنواع أشجار أصلية',
     introducedTreeSpecies: 'أنواع أشجار مدخلة',
@@ -1237,7 +1237,7 @@ The FRA team
 
     // table 1.2c
     growingStockComposition: {
-      growingStockComposition: 'تركيبة مخزون الأشجار الحية',
+      growingStockComposition: 'تكوين مخزون الأشجار الحيّة بالغابات',
       speciesName: 'اسم النوع',
       growingStockInForestMillionM3OB: 'المخزون من الأشجار الحية في الغابة (مليون م3 مع اللحاء)',
       rank: 'التصنيف',
