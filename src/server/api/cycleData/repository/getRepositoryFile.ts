509366de29c7d855fef3b159004dc28b23d4fbe2
sorja <ping@mirosorja.com>
2024-02-21 10:21:36 +0200

    3214 - backend: support requesting file metadata

--- src/server/api/cycleData/repository/getRepositoryFile.ts
+++ src/server/api/cycleData/repository/getRepositoryFile.ts
@@ -9,17 +9,24 @@ import { CycleDataController } from 'server/controller/cycleData'
 import Requests from 'server/utils/requests'
 import { Responses } from 'server/utils/responses'
 
-type Request = CycleRequest<never>
+type Request = CycleRequest
 
 export const getRepositoryFile = async (req: Request, res: Response) => {
   try {
-    const { assessmentName, cycleName } = req.query
+    const { assessmentName, cycleName, metadata } = req.query
     const { uuid } = req.params
 
     const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
 
     const props = { assessment, cycle, uuid }
     const { file, repositoryItem } = await CycleDataController.Repository.getFile(props)
+
+    if (metadata) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { file: _, ...rest } = file
+      Requests.send(res, rest)
+      return
+    }
 
     // Append the original file extension to the file name
     const label = Translations.getLabel({ translation: repositoryItem.props.translation, language: Lang.en })
