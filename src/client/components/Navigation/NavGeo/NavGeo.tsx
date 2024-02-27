f3503ab6a5d08fe0c0835f9ab5bdbc0ada19361c
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-02-27 11:44:22 -0500

    3496 - Rename Background to SatelliteMosaic

--- src/client/components/Navigation/NavGeo/NavGeo.tsx
+++ src/client/components/Navigation/NavGeo/NavGeo.tsx
@@ -1,14 +1,14 @@
 import './NavGeo.scss'
 import React from 'react'
 
-import Background from 'client/components/Navigation/NavGeo/Background'
+import SatelliteMosaic from 'client/components/Navigation/NavGeo/SatelliteMosaic'
 import GeoSection from 'client/components/Navigation/NavGeo/Sections'
 
 const NavGeo: React.FC = () => {
   return (
     <div className="nav-geo">
-      <GeoSection key="background" sectionLabel="Background">
-        <Background />
+      <GeoSection key="sateliteMosaic" sectionLabel="Satellite Mosaic">
+        <SatelliteMosaic />
       </GeoSection>
     </div>
   )
