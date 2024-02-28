20afab746dc7f54f9438856f3a38d1427efd1b7a
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-02-28 17:20:51 -0500

    3496 - Add LayerToggleControl component

--- src/client/components/Navigation/NavGeo/SatelliteMosaic/SatelliteMosaic.tsx
+++ src/client/components/Navigation/NavGeo/SatelliteMosaic/SatelliteMosaic.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { useAppDispatch } from 'client/store'
+import { GeoActions, useMosaicSelected, useMosaicStatus } from 'client/store/ui/geo'
+import LayerToggleControl from 'client/components/Navigation/NavGeo/LayerToggleControl'
 
 const SatelliteMosaic: React.FC = () => {
+  const { t } = useTranslation()
+  const dispatch = useAppDispatch()
+  const selected = useMosaicSelected()
+  const status = useMosaicStatus()
+
   return (
     <div>
-      <span>Satellite Mosaic content</span>
+      <LayerToggleControl
+        label={t('geo.showSatelliteMosaic')}
+        checked={selected ?? false}
+        onCheckboxClick={() => dispatch(GeoActions.toggleMosaicLayer())}
+        status={status}
+      />
     </div>
   )
 }
--- src/i18n/resources/en/geo.js
+++ src/i18n/resources/en/geo.js
@@ -1,3 +1,4 @@
 module.exports = {
   satelliteMosaic: 'Satellite Mosaic',
+  showSatelliteMosaic: 'Show Satellite Mosaic',
 }
