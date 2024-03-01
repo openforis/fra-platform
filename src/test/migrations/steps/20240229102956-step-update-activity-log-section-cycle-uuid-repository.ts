62355a65697cc425d9f6c4cfd8792cb2be6953e8
sorja <ping@mirosorja.com>
2024-03-01 11:55:28 +0200

    3214 - fix migrations

--- src/test/migrations/steps/20240229102956-step-update-activity-log-section-cycle-uuid-repository.ts
+++ src/test/migrations/steps/20240229102956-step-update-activity-log-section-cycle-uuid-repository.ts
@@ -3,51 +3,39 @@ import { SectionNames } from 'meta/routes'
 import { BaseProtocol } from 'server/db'
 
 export default async (client: BaseProtocol) => {
-  // 1. Update public.activity_log.cycle_uuid to 2020
-  await client.query(`
-      with cycle_uuid as (select ac.uuid
-                          from public.assessment
-                                   left join public.assessment_cycle ac on assessment.id = ac.assessment_id
-                          where ac.name = '2020'
-                            and assessment.props ->> 'name' = 'fra'
-          )
-         , activity_log_entries as (
-                          select al.id
-                          from public.activity_log al
-                              left join _legacy.repository r
-                          on r.file_name = al.target ->> 'fileName'
-                          where al.message ilike 'repositoryitem%'
-                            and al.cycle_uuid is null
-                            and r.file_name is not null
-          )
-      update public.activity_log
-      set cycle_uuid = cycle_uuid.uuid from cycle_uuid, activity_log_entries
-      where public.activity_log.id = activity_log_entries.id;
-  `)
-
-  // 2. Update public.activity_log.cycle_uuid to 2025
-  await client.query(`
-      with cycle_uuid as (select ac.uuid
-                          from public.assessment
-                                   left join public.assessment_cycle ac on assessment.id = ac.assessment_id
-                          where ac.name = '2025'
-                            and assessment.props ->> 'name' = 'fra'
-          )
-      update public.activity_log
-      set cycle_uuid = cycle_uuid.uuid from cycle_uuid
-      where public.activity_log.message ilike 'repositoryitem%'
-        and public.activity_log.cycle_uuid is null
-        and (public.activity_log.target ->> 'fileName' is not null
-         or public.activity_log.target ->> 'files' is not null)
-  `)
-
-  // 3. Update all sections to repository where message contains repositoryitem
   await client.query(
     `
-      update public.activity_log
-      set section = $1
-      where public.activity_log.message ilike 'repositoryitem%' or section ilike 'fileRepository'
-  `,
+        update public.activity_log
+        set section = $1
+        where public.activity_log.message ilike 'repositoryitem%' or section ilike 'fileRepository'
+    `,
     [SectionNames.Country.Home.repository]
   )
+
+  await client.query(`
+    -- get uuid for cycle 2020
+      with cycle_uuid_2020 as (select ac.uuid
+                               from public.assessment
+                                        left join public.assessment_cycle ac on assessment.id = ac.assessment_id
+                               where ac.name = '2020'
+                                 and assessment.props ->> 'name' = 'fra'),
+    -- get uuid for cycle 2025
+          cycle_uuid_2025 as (select ac.uuid
+                              from public.assessment
+                                  left join public.assessment_cycle ac on assessment.id = ac.assessment_id
+                              where ac.name = '2025'
+                                and assessment.props ->> 'name' = 'fra')
+
+    -- update activity log : after 2023, cycle_uuid = cycle_uuid_2025.uuid,
+    -- before 2023, cycle_uuid = cycle_uuid_2020.uuid
+
+      update public.activity_log al
+      set cycle_uuid = case
+                           when al.time >= '2023-01-01' then cu2025.uuid
+                           else cu2020.uuid
+                      end
+      from cycle_uuid_2020 cu2020, cycle_uuid_2025 cu2025
+      where al.section = 'repository'
+        and al.cycle_uuid is null;
+  `)
 }
