39fcc9eb73d42f80abd6703d91eaab84557f8d43
sorja <ping@mirosorja.com>
2024-03-01 13:18:58 +0200

    add common translations

--- src/i18n/resources/en/validation.js
+++ src/i18n/resources/en/validation.js
@@ -1,7 +1,7 @@
 module.exports = {
   repositoryItem: {
     eitherFileOrLink: 'Only one between file and link must be specified',
-    fileOrLink: 'One between link and file is required',
+    fileOrLink: 'Either a file or a link is required',
     nameIsRequired: 'Name is required',
     optionsDisabled: 'Label, public visibility and delete are disabled because this item is used by:',
   },
