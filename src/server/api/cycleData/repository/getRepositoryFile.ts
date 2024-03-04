1fc9d5bb2324b6124351a2671dd2c55fdfef11d0
Miró Sorja <ping@mirosorja.com>
2024-03-04 10:24:43 +0100

    3214 - hidden files (#3590)

--- src/server/api/cycleData/repository/getRepositoryFile.ts
+++ src/server/api/cycleData/repository/getRepositoryFile.ts
@@ -13,20 +13,13 @@ type Request = CycleRequest
 
 export const getRepositoryFile = async (req: Request, res: Response) => {
   try {
-    const { assessmentName, cycleName, metadata } = req.query
+    const { assessmentName, cycleName } = req.query
     const { uuid } = req.params
 
     const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
 
     const props = { assessment, cycle, uuid }
     const { file, repositoryItem } = await CycleDataController.Repository.getFile(props)
-
-    if (metadata) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { file: _, ...rest } = file
-      Requests.send(res, rest)
-      return
-    }
 
     // Append the original file extension to the file name
     const label = Translations.getLabel({ translation: repositoryItem.props.translation, language: Lang.en })
--- src/server/repository/assessmentCycle/repository/getOne.ts
+++ src/server/repository/assessmentCycle/repository/getOne.ts
@@ -8,20 +8,21 @@ import { BaseProtocol, DB, Schemas } from 'server/db'
 type Props = {
   assessment: Assessment
   cycle: Cycle
-
-  uuid: string
-}
+} & ({ uuid: string } | { fileName: string })
 
 export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<RepositoryItem> => {
-  const { assessment, cycle, uuid } = props
+  const { assessment, cycle } = props
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
+
+  const values = 'uuid' in props ? [props.uuid] : [props.fileName]
+  const where = 'uuid' in props ? 'uuid = $1' : "props -> 'translation' ->> 'en' = $1"
 
   return client.one<RepositoryItem>(
     `
       select * from ${schemaCycle}.repository
-      where uuid = $1
+      where ${where}
     `,
-    [uuid],
+    values,
     (row) => Objects.camelize(row)
   )
 }
