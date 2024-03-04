38dcf58f935e84c6cc39a3bd964bd0fc3d2158a1
sorja <ping@mirosorja.com>
2024-03-04 17:42:17 +0200

    update es/ar

--- src/i18n/resources/es/validation.js
+++ src/i18n/resources/es/validation.js
@@ -1,7 +1,7 @@
 module.exports = {
   repositoryItem: {
     eitherFileOrLink: 'Sólo debe especificarse una entre archivo y enlace',
-    fileOrLink: 'Se requiere uno entre enlace y archivo',
+    fileOrLink: 'Se requiere un archivo o un enlace',
     nameIsRequired: 'El nombre es obligatorio',
     optionsDisabled:
       'La etiqueta, la visibilidad pública y la eliminación están desactivadas porque este elemento es utilizado por:',
