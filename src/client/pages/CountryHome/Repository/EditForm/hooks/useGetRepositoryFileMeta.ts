c21d492d34013501299a6376a9b2706572786977
minotogna <mino.togna@gmail.com>
2024-03-05 17:06:59 +0100

    3214 - Fix file meta multiple fetch + Upgrade node 20.11.1

--- src/client/pages/CountryHome/Repository/EditForm/hooks/useGetRepositoryFileMeta.ts
+++ src/client/pages/CountryHome/Repository/EditForm/hooks/useGetRepositoryFileMeta.ts
@@ -4,16 +4,22 @@ import { CountryIso } from 'meta/area'
 import { RepositoryItem } from 'meta/cycleData'
 
 import { useAppDispatch } from 'client/store'
-import { RepositoryActions, useRepositoryItem } from 'client/store/ui/repository'
+import { RepositoryActions, useRepositoryFileMeta, useRepositoryItem } from 'client/store/ui/repository'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 
 export const useGetRepositoryFileMeta = () => {
   const { assessmentName, cycleName, countryIso } = useCountryRouteParams<CountryIso>()
-  const repositoryItem = useRepositoryItem() as RepositoryItem
+
   const dispatch = useAppDispatch()
+  const repositoryItem = useRepositoryItem() as RepositoryItem
+  const fileMeta = useRepositoryFileMeta()
+
+  const { fileUuid, uuid } = repositoryItem ?? {}
+  const summaryFileUuid = fileMeta?.summary?.uuid
+
   useEffect(() => {
-    if (repositoryItem?.fileUuid && repositoryItem?.uuid) {
+    if (fileUuid && uuid && fileUuid !== summaryFileUuid) {
       dispatch(RepositoryActions.getFileMeta({ repositoryItem, assessmentName, cycleName, countryIso }))
     }
-  }, [assessmentName, countryIso, cycleName, dispatch, repositoryItem])
+  }, [assessmentName, countryIso, cycleName, dispatch, fileUuid, repositoryItem, summaryFileUuid, uuid])
 }
