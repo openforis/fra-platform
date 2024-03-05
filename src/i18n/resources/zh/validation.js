50937c5207cac5ee475ecd7d07c7b423693ecb0e
sorja <ping@mirosorja.com>
2024-03-05 13:57:58 +0200

    add zh, fr

--- src/i18n/resources/zh/validation.js
+++ src/i18n/resources/zh/validation.js
@@ -1,7 +1,7 @@
 module.exports = {
   repositoryItem: {
     eitherFileOrLink: '文件和链接之间只能指定一个',
-    fileOrLink: '链接和文件之间必须指定一个',
+    fileOrLink: '需要提供文件或链接',
     nameIsRequired: '名称是必需的',
     optionsDisabled: '标签、公开和删除已被禁用，因为此项已被使用：',
   },
