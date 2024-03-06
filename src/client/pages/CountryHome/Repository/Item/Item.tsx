696e4e5f5642b106f7b7fccedad0c524b4409f14
Mino Togna <minotogna@users.noreply.github.com>
2024-03-06 06:51:06 +0100

    3214 UI improv (#3594)
    * 3214 - Update TablePaginated Paginator style
    
    * 3214 - Update Admin sticky headers
    
    * 3214 - Update RecentActivity style
    
    * 3214 - Update Repository row height
    
    * 3214 - Update Repository Edit button
    
    * 3214 - Update Repository Edit button

--- src/client/pages/CountryHome/Repository/Item/Item.tsx
+++ src/client/pages/CountryHome/Repository/Item/Item.tsx
@@ -4,8 +4,11 @@ import { useTranslation } from 'react-i18next'
 
 import classNames from 'classnames'
 
-import { RepositoryItem } from 'meta/cycleData'
+import { RepositoryItem, RepositoryItems } from 'meta/cycleData'
 
+import { useIsCountryRepositoryEditable, useIsGlobalRepositoryEditable } from 'client/store/user'
+import Button, { ButtonSize } from 'client/components/Buttons/Button'
+import { useOpenPanel } from 'client/pages/CountryHome/Repository/hooks/useOpenPanel'
 import RepositoryLink from 'client/pages/CountryHome/Repository/RepositoryLink'
 
 type Props = {
@@ -16,13 +19,22 @@ const Item: React.FC<Props> = (props) => {
   const { repositoryItem } = props
 
   const { t } = useTranslation()
+  const isCountryRepositoryEditable = useIsCountryRepositoryEditable()
+  const isGlobalRepositoryEditable = useIsGlobalRepositoryEditable()
+  const openPanel = useOpenPanel({ repositoryItem })
 
+  const isGlobalRepositoryItem = RepositoryItems.isGlobal({ repositoryItem })
+  const withActions = (isGlobalRepositoryItem && isGlobalRepositoryEditable) || isCountryRepositoryEditable
   const level = repositoryItem.props.public ? 'public' : 'private'
 
   return (
-    <div className="repository-item">
+    <div className={classNames('repository-item', { withActions })}>
       <RepositoryLink datum={repositoryItem} />
       <div className={classNames('repository-item__badge', level)}>{t(`common.${level}`)}</div>
+
+      {withActions && (
+        <Button iconName="pencil" inverse label={t('description.edit')} onClick={openPanel} size={ButtonSize.xs} />
+      )}
     </div>
   )
 }
