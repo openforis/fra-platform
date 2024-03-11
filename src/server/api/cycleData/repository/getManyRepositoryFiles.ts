c9b54b755f03dc5938ada4081537421e30ad17da
sorja <ping@mirosorja.com>
2024-03-11 12:51:49 +0200

    3403 - fix global boolean

--- src/server/api/cycleData/repository/getManyRepositoryFiles.ts
+++ src/server/api/cycleData/repository/getManyRepositoryFiles.ts
@@ -7,13 +7,13 @@ import { CycleDataController } from 'server/controller/cycleData'
 import Requests from 'server/utils/requests'
 import { Responses } from 'server/utils/responses'
 
-type Request = CycleRequest
+type Request = CycleRequest & { global: string }
 
 export const getManyRepositoryFiles = async (req: Request, res: Response) => {
   try {
     const { assessmentName, cycleName, countryIso, global = false } = req.query
     const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
-    const props = { assessment, cycle, countryIso, global }
+    const props = { assessment, cycle, countryIso, global: JSON.parse(global) }
     const files = await CycleDataController.Repository.getManyFiles(props)
 
     await Responses.sendZip(res, files)
