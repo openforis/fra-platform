ab5f226a7d3a34262450b6542062c4dc04966de4
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-03-14 10:42:02 -0500

    3496 - Add option to export custom csv data with ButtonTableExport

--- src/client/components/ButtonTableExport/ButtonTableExport.tsx
+++ src/client/components/ButtonTableExport/ButtonTableExport.tsx
@@ -1,21 +1,25 @@
 import React, { MutableRefObject, useState } from 'react'
 import { CSVLink } from 'react-csv'
 
+import { Objects } from 'utils/objects'
+
 import { useIsDataLocked } from 'client/store/ui/dataLock'
 import { useIsPrintRoute } from 'client/hooks/useIsRoute'
 import { useButtonClassName } from 'client/components/Buttons/Button'
 import Icon from 'client/components/Icon'
 
+import { CSVData } from './types'
 import * as Utils from './utils'
 
 type Props = {
+  csvData?: CSVData
   disabled?: boolean
   filename?: string
   tableRef: MutableRefObject<HTMLTableElement>
 }
 
 const ButtonTableExport: React.FC<Props> = (props) => {
-  const { disabled, filename, tableRef } = props
+  const { csvData, disabled, filename, tableRef } = props
 
   const [data, setData] = useState<Array<object>>([])
   const { print } = useIsPrintRoute()
@@ -24,6 +28,21 @@ const ButtonTableExport: React.FC<Props> = (props) => {
   const className = useButtonClassName({ disabled: !isLocked && disabled, iconName: 'hit-down', label: 'CSV' })
 
   if (print) return null
+
+  if (!Objects.isEmpty(csvData)) {
+    return (
+      <CSVLink
+        className={className}
+        data={csvData.data}
+        filename={`${filename}.csv`}
+        headers={csvData.headers}
+        target="_blank"
+      >
+        <Icon className="icon-sub icon-white" name="hit-down" />
+        CSV
+      </CSVLink>
+    )
+  }
 
   return (
     <CSVLink
@@ -44,6 +63,7 @@ const ButtonTableExport: React.FC<Props> = (props) => {
 }
 
 ButtonTableExport.defaultProps = {
+  csvData: undefined,
   disabled: false,
   filename: 'tableData',
 }
