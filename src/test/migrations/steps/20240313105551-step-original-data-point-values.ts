d8be04b9198c15611f4f527954bd4ffc648b7ed4
sorja <ping@mirosorja.com>
2024-03-15 15:32:46 +0200

    remove tla

--- src/test/migrations/steps/20240313105551-step-original-data-point-values.ts
+++ src/test/migrations/steps/20240313105551-step-original-data-point-values.ts
@@ -1,8 +1,6 @@
-import { Objects } from 'utils/objects'
 import { Promises } from 'utils/promises'
 
-import { Country } from 'meta/area'
-import { Assessment, Cycle, ODPs, OriginalDataPoint } from 'meta/assessment'
+import { Assessment, Cycle, ODPs } from 'meta/assessment'
 
 import { AreaController } from 'server/controller/area'
 import { AssessmentController } from 'server/controller/assessment'
@@ -11,26 +9,6 @@ import { BaseProtocol, DB, Schemas } from 'server/db'
 import { OriginalDataPointRepository } from 'server/repository/assessmentCycle/originalDataPoint'
 
 const client: BaseProtocol = DB
-
-const _getTLA = async (
-  props: { originalDataPoint: OriginalDataPoint; assessment: Assessment; cycle: Cycle; country: Country },
-  tx: BaseProtocol
-) => {
-  const { assessment, cycle, originalDataPoint } = props
-  const schemaName = Schemas.getNameCycle(assessment, cycle)
-  const totalLandArea = await tx.one(
-    `
-        select
-            r.value ->> 'raw' as value
-        from ${schemaName}.node_ext r
-        where type = 'node' and r.props ->> 'variableName' = 'totalLandArea' and r.props ->> 'tableName' = 'extentOfForest'
-        and r.props ->> 'colName' = '$1' and r.country_iso = $2
-   `,
-    [originalDataPoint.year, originalDataPoint.countryIso]
-  )
-
-  return Objects.setInPath({ obj: originalDataPoint, path: ['values', 'totalLandArea'], value: totalLandArea.value })
-}
 
 const _updateDDL = async ({ assessment, cycle }: { assessment: Assessment; cycle: Cycle }) => {
   const schemaName = Schemas.getNameCycle(assessment, cycle)
@@ -50,11 +28,7 @@ export default async () => {
         const { countryIso } = country
         const getODPsProps = { assessment, cycle, countryIso }
         const originalDataPoints = await CycleDataController.getOriginalDataPoints(getODPsProps, tx)
-        const withTotalLandArea = await Promise.all(
-          originalDataPoints.map((originalDataPoint) => _getTLA({ originalDataPoint, assessment, cycle, country }, tx))
-        )
-
-        const withValues = withTotalLandArea.map(ODPs.calculateValues)
+        const withValues = originalDataPoints.map(ODPs.calculateValues)
         await Promises.each(withValues, async (odp) => {
           const updateODProps = { assessment, cycle, originalDataPoint: odp }
           await OriginalDataPointRepository.updateOriginalData(updateODProps, tx)
