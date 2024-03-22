5f099b0c88f87d467abe6fc502c9d2acbbbce2da
Miró Sorja <ping@mirosorja.com>
2024-03-22 11:17:12 +0100

    3531 - Message topics : Migrate dataSource message topic and delete orphans (#3637)
    * migration for data source message topic key
    
    * Topics.getDataSourceReviewTopicKey
    
    * remove extra check
    
    * 3531 - update remove data source key used for mt row deletion

---
+++ src/test/migrations/steps/20240321150001-step-fix-message-topic-data-source-remove-orphans.ts
@@ -0,0 +1,43 @@
+import { Promises } from 'utils/promises'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { BaseProtocol, Schemas } from 'server/db'
+
+export default async (client: BaseProtocol) => {
+  const assessments = await AssessmentController.getAll({}, client)
+
+  await Promises.each(assessments, async (assessment) => {
+    await Promises.each(assessment.cycles, async (cycle) => {
+      // drop orphan message topic rows rel. to data source
+      const schemaCycle = Schemas.getNameCycle(assessment, cycle)
+      await client.none(`
+          with activity_log_data_source_uuids as 
+              (select
+                   jsonb_array_elements(al.target -> 'description' -> 'value' -> 'dataSources') ->> 'uuid'
+              as data_source_uuid
+          from activity_log al
+          where message = 'descriptionUpdate'
+            and target ->> 'name' = 'dataSources'),
+              
+              data_sources as (select jsonb_array_elements(d.value -> 'dataSources') ->> 'uuid' as uuid
+          from ${schemaCycle}.descriptions d
+          where d.value ->> 'dataSources' is not null)
+          
+          delete from ${schemaCycle}.message_topic mt
+          -- message topic is orphan if data source uuid is found in activity log but not anymore in data sources
+          where key in (select data_source_uuid from activity_log_data_source_uuids)
+            and key not in (select uuid from data_sources);
+
+      `)
+
+      await client.none(`
+          update ${schemaCycle}.message_topic mt
+          set key = 'dataSource_' || key
+          where key ~ '^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
+          and key in (select jsonb_array_elements(d.value -> 'dataSources') ->> 'uuid' as uuid
+                      from ${schemaCycle}.descriptions d
+                      where d.value ->> 'dataSources' is not null)
+      `)
+    })
+  })
+}
