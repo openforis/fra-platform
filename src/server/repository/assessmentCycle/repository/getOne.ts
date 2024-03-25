1d80b8dc48614e3d7a47d55b1de0bb616f62327a
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-03-25 10:29:06 -0500

    3270 - Add country iso to Repository getOne props

--- src/server/repository/assessmentCycle/repository/getOne.ts
+++ src/server/repository/assessmentCycle/repository/getOne.ts
@@ -1,5 +1,6 @@
 import { Objects } from 'utils/objects'
 
+import { CountryIso } from 'meta/area'
 import { Assessment, Cycle } from 'meta/assessment'
 import { RepositoryItem } from 'meta/cycleData'
 
@@ -8,14 +9,20 @@ import { BaseProtocol, DB, Schemas } from 'server/db'
 type Props = {
   assessment: Assessment
   cycle: Cycle
+  countryIso?: CountryIso
 } & ({ uuid: string } | { fileName: string })
 
 export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<RepositoryItem> => {
-  const { assessment, cycle } = props
+  const { assessment, countryIso, cycle } = props
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
 
   const values = 'uuid' in props ? [props.uuid] : [props.fileName]
-  const where = 'uuid' in props ? 'uuid = $1' : "props -> 'translation' ->> 'en' = $1"
+  let where = 'uuid' in props ? 'uuid = $1' : "props -> 'translation' ->> 'en' = $1"
+
+  if (!Objects.isEmpty(countryIso)) {
+    values.push(countryIso)
+    where = `${where} AND country_iso = $2`
+  }
 
   return client.one<RepositoryItem>(
     `
