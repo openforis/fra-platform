4950a947c354e7b23650e57a6ebf845b1c0830ff
minotogna <mino.togna@gmail.com>
2024-03-26 17:10:30 +0100

    3496 - filter out invalid custom fnf layer on agreement layer fetch

--- src/client/store/ui/geo/actions/postLayer.ts
+++ src/client/store/ui/geo/actions/postLayer.ts
@@ -4,33 +4,31 @@ import axios from 'axios'
 import { CountryIso } from 'meta/area'
 import { LayerKey, LayerSectionKey, LayerSource, sectionsApiEndpoint } from 'meta/geo'
 
-import { RootState } from 'client/store/RootState'
+import { ThunkApiConfig } from 'client/store/types'
 
 import { _getLayerRequestBody } from '.'
 
-type Params = {
+type Props = {
   countryIso: CountryIso
   layerKey: LayerKey
   layerSource?: LayerSource
   sectionKey: LayerSectionKey
 }
 
-export const postLayer = createAsyncThunk<[LayerSectionKey, LayerKey, string], Params>(
+export const postLayer = createAsyncThunk<[LayerSectionKey, LayerKey, string], Props, ThunkApiConfig>(
   'geo/post/layer',
-  async ({ countryIso, layerKey, layerSource, sectionKey }, { getState }) => {
+  async (props, { getState }) => {
+    const { countryIso, layerKey, layerSource, sectionKey } = props
     const url = sectionsApiEndpoint[sectionKey]
 
     if (layerSource !== undefined) {
-      const body = {
-        countryIso,
-        layer: layerSource,
-      }
+      const body = { countryIso, layer: layerSource }
       const response = await axios.post(url, body)
       return [sectionKey, layerKey, response.data.mapId]
     }
 
     const state = getState()
-    const sectionState = (state as RootState).geo.sections?.[sectionKey]
+    const sectionState = state.geo.sections?.[sectionKey]
     const layerState = sectionState?.[layerKey]
     const body = _getLayerRequestBody(countryIso, layerKey, layerState, sectionState)
     const response = await axios.post(url, body)
