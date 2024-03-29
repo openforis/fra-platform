f769dc6d43ff1043e6bd5f38187b1f126d280d30
sorja <ping@mirosorja.com>
2024-03-29 10:22:14 +0200

    3654 - components/Buttons/ButtonHistory with generic props

--- src/meta/cycleData/history/histories.ts
+++ src/meta/cycleData/history/histories.ts
@@ -1,10 +1,17 @@
-import { CommentableDescriptionName } from 'meta/assessment'
 import { HistoryItemSectionKey } from 'meta/cycleData/history/history'
 
-const getHistoryItemSectionKey = (sectionName: string, name: CommentableDescriptionName): HistoryItemSectionKey => {
-  return `${sectionName}-${name}` as HistoryItemSectionKey
+const getHistoryItemSectionKey = (sectionName: string, subSection: string, name: string): HistoryItemSectionKey => {
+  return `${sectionName}-${subSection}-${name}` as HistoryItemSectionKey
+}
+
+const getHistoryItemKeyParts = (
+  key: HistoryItemSectionKey
+): { sectionName: string; subSection: string; name: string } => {
+  const [sectionName, subSection, name] = key.split('-')
+  return { sectionName, subSection, name }
 }
 
 export const Histories = {
   getHistoryItemSectionKey,
+  getHistoryItemKeyParts,
 }
