8ef0f81566e3b8996ec8a8f91e9e4be5f4e64b42
sorja <ping@mirosorja.com>
2024-04-02 13:24:05 +0300

    3654 - separate concerns for History (Hooks, Items)

---
+++ src/client/components/Navigation/History/hooks/useData.ts
@@ -0,0 +1,13 @@
+import { ApiEndPoint } from 'meta/api/endpoint'
+import { ActivityLog, ActivityLogMessage } from 'meta/assessment'
+
+import { useTablePaginatedData } from 'client/store/ui/tablePaginated'
+import { useSectionRouteParams } from 'client/hooks/useRouteParams'
+
+const path = ApiEndPoint.CycleData.activities()
+
+export const useData = () => {
+  const { sectionName } = useSectionRouteParams()
+  const data = useTablePaginatedData<ActivityLog<never>>(path)
+  return data?.filter((d) => d.message === ActivityLogMessage.descriptionUpdate && d.section === sectionName)
+}
