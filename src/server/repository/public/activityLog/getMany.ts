d417b4fb8577974dba92504c9c3aa324cb1d0ac3
sorja <ping@mirosorja.com>
2024-04-03 12:53:40 +0300

    3654 - include user

--- src/server/repository/public/activityLog/getMany.ts
+++ src/server/repository/public/activityLog/getMany.ts
@@ -17,7 +17,11 @@ type Props = {
 export const getMany = async (props: Props, client: BaseProtocol = DB): Promise<Array<ActivityLog<never>>> => {
   const { assessment, cycle, countryIso, sectionName, message, target } = props
   return client.map<ActivityLog<never>>(
-    `select * from public.activity_log where assessment_uuid = $1 and cycle_uuid = $2 and country_iso = $3 and section = $4 and message = $5 and target ->> 'name' = $6;`,
+    `select al.*,
+                  jsonb_build_object('id', u.id, 'props', u.props) as user
+          from public.activity_log al
+          join public.users u on al.user_id = u.id
+          where assessment_uuid = $1 and cycle_uuid = $2 and country_iso = $3 and section = $4 and message = $5 and target ->> 'name' = $6;`,
     [assessment.uuid, cycle.uuid, countryIso, sectionName, message, target],
     (row) => Objects.camelize(row)
   )
