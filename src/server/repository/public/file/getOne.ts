e3d1c2761322f988623ae61c9a6f50aadf23e738
Miró Sorja <ping@mirosorja.com>
2024-04-10 12:41:28 +0200

    3706 - fix legacy links in images (#3717)
    * 3706 - fix legacy links
    
    * 3706 - use promise.all
    
    * 3706 - print correct url

--- src/server/repository/public/file/getOne.ts
+++ src/server/repository/public/file/getOne.ts
@@ -4,19 +4,22 @@ import { BaseProtocol, DB } from 'server/db'
 import { FileAdapter } from 'server/repository/adapter'
 import { fieldsFile } from 'server/repository/public/file/fields'
 
-type Props = {
-  fileUuid: string
-}
+type Props =
+  | {
+      fileUuid: string
+    }
+  | { fileName: string }
 
 export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<File> => {
-  const { fileUuid } = props
+  const values = 'fileUuid' in props ? [props.fileUuid] : [props.fileName]
+  const where = 'fileUuid' in props ? 'uuid = $1' : 'name = $1'
 
   return client.one(
     `
         select ${fieldsFile.join(', ')}
         from public.file
-        where uuid = $1`,
-    [fileUuid],
+        where ${where}`,
+    values,
     FileAdapter
   )
 }
