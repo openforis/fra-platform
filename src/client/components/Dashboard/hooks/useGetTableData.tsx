4b1e87b2a7b030318d66f8d1fe80a8126d707702
sorja <ping@mirosorja.com>
2024-04-17 10:18:37 +0300

    3645 - move common hooks to Dashboard/hooks

--- src/client/components/Dashboard/hooks/useGetTableData.tsx
+++ src/client/components/Dashboard/hooks/useGetTableData.tsx
@@ -6,8 +6,7 @@ import { Table } from 'meta/assessment'
 import { useAppDispatch } from 'client/store'
 import { DataActions } from 'client/store/data'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
-
-import { useDependencies } from './useDependencies'
+import { useDependencies } from 'client/components/Dashboard/hooks/useDependencies'
 
 export const useGetTableData = (table: Table) => {
   const dispatch = useAppDispatch()
