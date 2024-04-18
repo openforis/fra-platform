7f820374b2ec6892a861738a1877182a88613420
sorja <ping@mirosorja.com>
2024-04-18 14:44:26 +0300

    3645 - forestAreaWithinProtectedAreas

---
+++ src/client/pages/CountryHome/Overview/meta/forestAreaWithinProtectedAreas/index.ts
@@ -0,0 +1,60 @@
+import { Cycle, TableNames } from 'meta/assessment'
+import { ChartColor } from 'meta/chart'
+import { DashboardItemType } from 'meta/dashboard'
+import { DashboardPieChart } from 'meta/dashboard/dashboard'
+
+import { getTable } from 'client/pages/CountryHome/Overview/meta/utils'
+import { RowsMetadata } from 'client/pages/CountryHome/Overview/meta/utils/rowsMetadata'
+
+const cols: Record<string, Array<string>> = {
+  '2020': ['2020'],
+  '2025': ['2025'],
+}
+
+const tableId = 5
+const tableName = 'forestAreaWithinProtectedAreas'
+const variableName = 'forest_area_within_protected_areas'
+
+export const forestAreaWithinProtectedAreas = (cycle: Cycle): DashboardPieChart => {
+  const rowMetadata: RowsMetadata = [
+    {
+      id: 1,
+      variableName,
+      label: `statisticalFactsheets.rowName.forestAreaWithinProtectedAreas`,
+      calculateFn: `100 * ${tableName}.${variableName} / ${TableNames.extentOfForest}.forestArea`,
+      calculationDependencies: [
+        { tableName, variableName },
+        { tableName: TableNames.extentOfForest, variableName: 'forestArea' },
+      ],
+    },
+    {
+      id: 2,
+      variableName: 'forestArea',
+      label: `statisticalFactsheets.rowName.forestArea`,
+      calculateFn: `100 - 100 * ${tableName}.${variableName} / ${TableNames.extentOfForest}.forestArea`,
+      calculationDependencies: [
+        { tableName, variableName },
+        { tableName: TableNames.extentOfForest, variableName: 'forestArea' },
+      ],
+    },
+  ]
+
+  return {
+    type: DashboardItemType.pieChart,
+    title: { key: 'statisticalFactsheets.forestAreaWithinProtectedAreas.title' },
+    table: getTable({ cycle, cols: cols[cycle.name], tableId, rowMetadata, tableName }),
+    chart: {
+      label: ({ variableName, percent }) => `${variableName} ${(percent * 100).toFixed(0)}%`,
+      cells: [
+        {
+          variableName: 'forestArea',
+          color: ChartColor.green,
+        },
+        {
+          variableName,
+          color: ChartColor.lightGreen,
+        },
+      ],
+    },
+  }
+}
