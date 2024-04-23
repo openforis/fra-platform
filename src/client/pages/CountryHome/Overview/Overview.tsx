88ebbddee1b356d1e644f3206d299a1fb2eff1b9
sorja <ping@mirosorja.com>
2024-04-23 08:57:23 +0300

    Check sections loaded before rendering overview

--- src/client/pages/CountryHome/Overview/Overview.tsx
+++ src/client/pages/CountryHome/Overview/Overview.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import { useSections } from 'client/store/metadata'
 import Dashboard from 'client/components/Dashboard'
 import { useItems } from 'client/pages/CountryHome/Overview/hooks'
 
 const Overview: React.FC = () => {
   const items = useItems()
+
+  const sections = useSections()
+  if (!sections) return null
 
   return <Dashboard items={items} />
 }
