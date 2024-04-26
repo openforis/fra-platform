104df531dfd32aa0720a9552b7ad54b7b26fc231
sorja <ping@mirosorja.com>
2024-04-26 10:43:56 +0300

    NoData: check data is not fetching

--- src/client/components/Dashboard/NoData/NoData.tsx
+++ src/client/components/Dashboard/NoData/NoData.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { useIsSomeTableDataFetching } from 'client/store/data'
+
 const NoData = () => {
   const { t } = useTranslation()
+
+  const isFetching = useIsSomeTableDataFetching()
+  if (isFetching) return null
 
   return <span> {t('statisticalFactsheets.noData')}</span>
 }
