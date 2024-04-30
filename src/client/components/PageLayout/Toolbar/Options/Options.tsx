fe1c2ab047da516bcabf3f9799805eecbaed2979
Mino Togna <minotogna@users.noreply.github.com>
2024-04-30 18:57:01 +0200

    3772 - Fix Navigating between data tables and GEO (#3780)
    * 3772 - Fix Navigating between data tables and GEO
    
    * 3772 - Add useCanViewGeo hook
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/client/components/PageLayout/Toolbar/Options/Options.tsx
+++ src/client/components/PageLayout/Toolbar/Options/Options.tsx
@@ -3,28 +3,20 @@ import React from 'react'
 
 import classNames from 'classnames'
 
-import { Users } from 'meta/user'
-
-import { useCycle } from 'client/store/assessment'
-import { useUser } from 'client/store/user'
+import { useCanViewGeo } from 'client/store/user/hooks'
 import { useIsGeoRoute } from 'client/hooks'
-import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import GeoOptions from 'client/components/PageLayout/Toolbar/Options/GeoOptions'
 import LinkPrint from 'client/components/PageLayout/Toolbar/Options/LinkPrint'
 
 const Options: React.FC = () => {
-  const { countryIso } = useCountryRouteParams()
   const geoRoute = useIsGeoRoute()
-  const user = useUser()
-  const cycle = useCycle()
-
-  const withGeo = Users.isAdministrator(user) || Users.isReviewer(user, countryIso, cycle)
+  const canViewGeo = useCanViewGeo()
 
   return (
     <div className={classNames('toolbar-options', { geoRoute })}>
       {!geoRoute && <LinkPrint />}
 
-      {withGeo && <GeoOptions />}
+      {canViewGeo && <GeoOptions />}
     </div>
   )
 }
--- src/client/pages/Geo/Geo.tsx
+++ src/client/pages/Geo/Geo.tsx
@@ -3,23 +3,16 @@ import React, { Suspense } from 'react'
 import { Navigate } from 'react-router-dom'
 
 import { Routes } from 'meta/routes'
-import { Users } from 'meta/user'
 
-import { useCycle } from 'client/store/assessment'
-import { useUser } from 'client/store/user'
-import { useCountryRouteParams } from 'client/hooks/useRouteParams'
+import { useCanViewGeo } from 'client/store/user/hooks'
 import Loading from 'client/components/Loading'
 
 const MapWrapper = React.lazy(() => import('./MapWrapper'))
 
 const Geo: React.FC = () => {
-  const { countryIso } = useCountryRouteParams()
-  const cycle = useCycle()
-  const user = useUser()
+  const canViewGeo = useCanViewGeo()
 
-  const hasRoleInCountry = Users.hasRoleInCountry({ cycle, countryIso, user })
-
-  if (!hasRoleInCountry) {
+  if (!canViewGeo) {
     return <Navigate replace to={Routes.Cycle.path.relative} />
   }
 
