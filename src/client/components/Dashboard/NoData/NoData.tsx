9bea6fe84c4774f4c0a4311531631ec61534dd85
sorja <ping@mirosorja.com>
2024-04-30 15:19:08 +0300

    Remove redundant check

--- src/client/components/Dashboard/NoData/NoData.tsx
+++ src/client/components/Dashboard/NoData/NoData.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { useIsSomeTableDataFetching } from 'client/store/data'
-
 const NoData = () => {
   const { t } = useTranslation()
-
-  const isFetching = useIsSomeTableDataFetching()
-  if (isFetching) return null
 
   return <span> {t('statisticalFactsheets.noData')}</span>
 }
