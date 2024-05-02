d4d2ec6de5f1568719cb773360ef2805a895e5a4
sorja <ping@mirosorja.com>
2024-05-02 11:27:39 +0300

    3785 - delete node.totalLandArea and union node_ext

--- src/server/repository/assessmentCycle/data/createOrReplaceTableDataView.ts
+++ src/server/repository/assessmentCycle/data/createOrReplaceTableDataView.ts
@@ -16,10 +16,9 @@ export const createOrReplaceTableDataView = async (props: Props, client: BasePro
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
   const tableName = table.props.name
 
-  return client.query(
-    `
-      create or replace view ${schemaCycle}.${tableName} as
-      (
+  return client.query(`
+    create or replace view ${schemaCycle}.${tableName} as
+    (
       select n.country_iso,
              r.props ->> 'variableName' as variable_name,
              c.props ->> 'colName'      as col_name,
@@ -30,7 +29,18 @@ export const createOrReplaceTableDataView = async (props: Props, client: BasePro
                left join ${schemaAssessment}."table" t on t.id = r.table_id
       where t.props ->> 'name' = '${tableName}'
             and r.props ->> 'type' in ('data', 'calculated')
+      union
+      select n.country_iso,
+             r.props ->> 'variableName' as variable_name,
+             c.props ->> 'colName'      as col_name,
+             n.value
+      from ${schemaCycle}.node_ext n
+               left join ${schemaAssessment}.row r on r.props ->> 'variableName' = n.props ->> 'variableName'
+               left join ${schemaAssessment}.col c on c.props ->> 'colName' = n.props ->> 'colName'
+               left join ${schemaAssessment}."table" t on t.props ->> 'name' = n.props ->> 'tableName'
+      where t.props ->> 'name' = '${tableName}'
+            and r.props ->> 'type' in ('data', 'calculated')
       order by 1, 2, 3
-      )`
-  )
+    )
+  `)
 }
