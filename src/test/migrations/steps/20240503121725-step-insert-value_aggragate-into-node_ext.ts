39e07dab4866148e074db2f64aa023c79994f930
sorja <ping@mirosorja.com>
2024-05-03 13:27:43 +0300

    insert value_aggregate into node_ext, drop value_aggregate

---
+++ src/test/migrations/steps/20240503121725-step-insert-value_aggragate-into-node_ext.ts
@@ -0,0 +1,38 @@
+import { Promises } from 'utils/promises'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { BaseProtocol, Schemas } from 'server/db'
+
+/**
+ * 1. Insert value_aggregate into node_ext
+ * 2. Drop value_aggregate
+ * Note: We don't migrate values that are not associated with a table,
+ * eg. precalculated ratios or sums or totals
+ */
+
+export default async (client: BaseProtocol) => {
+  const assessments = await AssessmentController.getAll({}, client)
+  await Promises.each(assessments, async (assessment) => {
+    await Promises.each(assessment.cycles, async (cycle) => {
+      const schemaName = Schemas.getName(assessment)
+      const schemaCycle = Schemas.getNameCycle(assessment, cycle)
+
+      const q = `
+          select va.country_iso
+               , 'node' as type
+               , jsonb_build_object(
+                  'colName', va.col_name,
+                  'tableName', t.props ->> 'name',
+                  'variableName', va.variable_name
+                 )      as props
+               , va.value
+          from ${schemaCycle}.value_aggregate va
+                   left join ${schemaName}.row on va.variable_name = row.props ->> 'variableName'
+              left join ${schemaName}.table t on row.table_id = t.id
+          where t.props ->> 'name' is not null
+      `
+      await client.query(q)
+      await client.query(`drop table ${schemaCycle}.value_aggregate`)
+    })
+  })
+}
