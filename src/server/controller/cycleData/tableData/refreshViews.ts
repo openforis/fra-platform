dc9314a3b3348144c46f6f414a34c441b73dd689
sorja <ping@mirosorja.com>
2024-05-09 12:12:40 +0300

    3785 - dont pass client or tx for refreshView

--- src/server/controller/cycleData/tableData/refreshViews.ts
+++ src/server/controller/cycleData/tableData/refreshViews.ts
@@ -1,6 +1,5 @@
 import { Assessment, Cycle } from 'meta/assessment'
 
-import { BaseProtocol, DB } from 'server/db'
 import { TableRepository } from 'server/repository/assessment/table'
 import { DataRepository } from 'server/repository/assessmentCycle/data'
 
@@ -9,11 +8,11 @@ type Props = {
   cycle: Cycle
 }
 
-export const refreshViews = async (props: Props, client: BaseProtocol = DB): Promise<void> => {
+export const refreshViews = async (props: Props): Promise<void> => {
   const { assessment, cycle } = props
   const tables = await TableRepository.getMany({ assessment, cycle })
 
   await Promise.all(
-    tables.map(async (table) => DataRepository.createOrReplaceTableDataView({ assessment, cycle, table }, client))
+    tables.map(async (table) => DataRepository.createOrReplaceTableDataView({ assessment, cycle, table }))
   )
 }
