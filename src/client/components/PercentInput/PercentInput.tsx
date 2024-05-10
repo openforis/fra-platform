8567d69af7296a431768c36ba317bde6bb95a26f
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-05-10 09:15:20 -0500

    3797 - Revert PercentInput changes & add precision prop

--- src/client/components/PercentInput/PercentInput.tsx
+++ src/client/components/PercentInput/PercentInput.tsx
@@ -1,63 +1,72 @@
 import './PercentInput.scss'
-import React, { useState } from 'react'
+import React from 'react'
 
-import * as R from 'ramda'
 import { Numbers } from 'utils/numbers'
+import * as R from 'ramda'
 
 import { Sanitizer } from 'client/utils/sanitizer'
 
+type State = any
 type Props = {
-  disabled: boolean
-  numberValue: string | number
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onPaste: (e: React.ClipboardEvent<HTMLInputElement>) => string | void
+  disabled: any
+  numberValue: any
+  onChange: any
+  onPaste: any
   precision?: number
 }
 
-const PercentInput: React.FC<Props> = (props: Props) => {
-  const { disabled, numberValue, onChange, onPaste, precision } = props
-  const [hasFocus, setHasFocus] = useState(false)
-  const [inputValue, setInputValue] = useState<string | number | null>(numberValue)
+export class PercentInput extends React.Component<Props, State> {
+  static defaultProps = {
+    precision: 3,
+  }
 
-  return (
-    <div className="percent-input__container validation-error-sensitive-field">
-      <div className="percent-input__readonly-view" style={{ visibility: hasFocus ? 'hidden' : 'visible' }}>
-        {Numbers.format(numberValue, precision)}
+  constructor(props: Props) {
+    super(props)
+    this.state = { hasFocus: false, inputValue: (props as any).numberValue }
+  }
+
+  render() {
+    const { disabled, numberValue, onChange, onPaste, precision } = this.props
+    const value = this.state.inputValue || numberValue
+    return (
+      <div className="percent-input__container validation-error-sensitive-field" ref="wrapper">
+        <div
+          className="percent-input__readonly-view"
+          style={{ visibility: this.state.hasFocus ? 'hidden' : 'visible' }}
+        >
+          {Numbers.format(numberValue, precision)}
+        </div>
+        <input
+          disabled={disabled}
+          type="text"
+          maxLength={6}
+          className="percent-input__input-field no-print"
+          ref="percentInputField"
+          value={value || ''}
+          style={{ opacity: this.state.hasFocus ? '1' : '0' }}
+          onChange={(e) => {
+            if (!Sanitizer.acceptableAsDecimal(e.target.value)) {
+              return
+            }
+            this.setState({ inputValue: e.target.value })
+            if (!R.pipe(R.path(['target', 'value']), R.defaultTo(''), R.endsWith('.'))(e)) onChange(e)
+          }}
+          onPaste={(e) => {
+            const pastedValue = onPaste(e)
+            this.setState({ inputValue: pastedValue })
+          }}
+          onFocus={() => {
+            this.setState({ hasFocus: true })
+            this.setState({ inputValue: numberValue || null })
+            ;(this.refs.percentInputField as any).select()
+          }}
+          onBlur={() => {
+            this.setState({ hasFocus: false })
+          }}
+        />
+        <div className="percent-input__sign">%</div>
       </div>
-      <input
-        className="percent-input__input-field no-print"
-        disabled={disabled}
-        maxLength={6}
-        onBlur={() => {
-          setHasFocus(false)
-        }}
-        onChange={(e) => {
-          const { value } = e.target
-          if (!Sanitizer.acceptableAsDecimal(value)) {
-            return
-          }
-          setInputValue(value)
-          if (!R.pipe(R.path(['target', 'value']), R.defaultTo(''), R.endsWith('.'))(e)) onChange(e)
-        }}
-        onFocus={() => {
-          setHasFocus(true)
-          setInputValue(numberValue || null)
-        }}
-        onPaste={(e) => {
-          const pastedValue = onPaste(e)
-          if (pastedValue) setInputValue(pastedValue)
-        }}
-        style={{ opacity: hasFocus ? '1' : '0' }}
-        type="text"
-        value={inputValue || ''}
-      />
-      <div className="percent-input__sign">%</div>
-    </div>
-  )
+    )
+  }
 }
-
-PercentInput.defaultProps = {
-  precision: 2,
-}
-
 export default PercentInput
