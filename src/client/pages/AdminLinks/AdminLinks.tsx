5c0a012e75e7a4f196aec6fea0d3060a652a58a9
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-05-30 12:14:22 -0500

    Revert "3743 - Update table with event listeners"
    This reverts commit 66806e7f60ff72218718a062bd1eb53146bfa8eb.

--- src/client/pages/AdminLinks/AdminLinks.tsx
+++ src/client/pages/AdminLinks/AdminLinks.tsx
@@ -4,13 +4,12 @@ import { useTranslation } from 'react-i18next'
 import { ApiEndPoint } from 'meta/api/endpoint'
 
 import { useAppDispatch } from 'client/store'
-import { LinksActions, useIsVerificationInProgress, useLinksChangeListener } from 'client/store/ui/links'
+import { LinksActions, useIsVerificationInProgress } from 'client/store/ui/links'
 import { useSectionRouteParams } from 'client/hooks/useRouteParams'
 import Button from 'client/components/Buttons/Button'
 import TablePaginated from 'client/components/TablePaginated'
 
 import { useColumns } from './hooks/useColumns'
-import { useListenLinksVerificationEvents } from './hooks/useListenLinksVerificationEvents'
 
 const AdminLinks: React.FC = () => {
   const columns = useColumns()
@@ -23,9 +22,6 @@ const AdminLinks: React.FC = () => {
   }
 
   const verifyLinksInProgress = useIsVerificationInProgress(assessmentName, cycleName)
-
-  useLinksChangeListener()
-  useListenLinksVerificationEvents()
 
   useEffect(() => {
     dispatch(LinksActions.getIsVerificationInProgress({ assessmentName, cycleName }))
