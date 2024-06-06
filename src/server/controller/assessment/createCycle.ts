8252465278c6ff551de2db19a5b72d4f7bf23188
sorja <ping@mirosorja.com>
2024-06-06 10:38:48 +0300

    3785 - createCycle: remove fao estimate

--- src/server/controller/assessment/createCycle.ts
+++ src/server/controller/assessment/createCycle.ts
@@ -1,12 +1,9 @@
-import { ActivityLogMessage, Assessment, Cycle, TableNames } from 'meta/assessment'
+import { ActivityLogMessage, Assessment, Cycle } from 'meta/assessment'
 import { User } from 'meta/user'
 
 import { BaseProtocol, DB } from 'server/db'
-import { TableRepository } from 'server/repository/assessment/table'
 import { CycleRepository } from 'server/repository/assessmentCycle/cycle'
-import { DataRepository } from 'server/repository/assessmentCycle/data'
 import { ActivityLogRepository } from 'server/repository/public/activityLog'
-import { Logger } from 'server/utils/logger'
 
 export const createCycle = async (
   props: { user: User; assessment: Assessment; name: string },
@@ -15,17 +12,6 @@ export const createCycle = async (
   const { user, assessment, name } = props
   return client.tx(async (t) => {
     const { assessment: updatedAssessment, cycle } = await CycleRepository.create({ assessment, name }, t)
-
-    const tables = (await TableRepository.getMany({ assessment, cycle })).filter(
-      (table) => table.props.name !== TableNames.valueAggregate
-    )
-
-    await Promise.all(
-      tables.map(async (table) => {
-        Logger.debug(`Creating materialized view for ${assessment.props.name} ${cycle.name} ${table.props.name}`)
-        return DataRepository.createMaterializedFaoEstimateView({ assessment, cycle, table })
-      })
-    )
 
     const message = ActivityLogMessage.assessmentCycleCreate
     const activityLog = { target: updatedAssessment, section: 'assessment', message, user }
