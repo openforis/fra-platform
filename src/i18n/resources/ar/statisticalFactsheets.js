eeb58b36b300a919114a5507fffa5305725db7f7
sorja <ping@mirosorja.com>
2024-06-25 14:57:25 +0300

    unit ar

--- src/i18n/resources/ar/statisticalFactsheets.js
+++ src/i18n/resources/ar/statisticalFactsheets.js
@@ -21,9 +21,9 @@ module.exports = {
   carbonAndGrowingStock: {
     title: 'مخزون وكربون الأشجار الحية في الغابات ({{startYear}} - {{endYear}})',
     rowName: '',
-    carbon_stock_biomass_total: 'مخزون الكربون في الكتلة الحيوية',
-    carbon_stock_total: 'إجمالي مخزون الكربون',
-    growing_stock_total: '$t(growingStock.growingStock)',
+    carbon_stock_biomass_total: 'مخزون الكربون في الكتلة الحيوية($t({{unit}}))',
+    carbon_stock_total: 'إجمالي مخزون الكربون($t({{unit}})) ',
+    growing_stock_total: '$t(growingStock.growingStock) ($t({{unit}}))',
   },
   forestAreaPercent: {
     title: 'النسبة المئوية لمساحة الغابة إلى إجمالي مساحة الأرض ({{year}})',
@@ -42,12 +42,12 @@ module.exports = {
   primaryDesignatedManagementObjective: {
     title: '$t(designatedManagementObjective.primaryDesignatedManagementObjective) ({{startYear}} - {{endYear}})',
     rowName: '',
-    conservation_of_biodiversity: '$t(designatedManagementObjective.biodiversityConservation)',
-    multiple_use: '$t(designatedManagementObjective.multipleUse)',
-    other: '$t(common.other)',
-    production: '$t(designatedManagementObjective.production)',
-    protection_of_soil_and_water: '$t(designatedManagementObjective.soilWaterProtection)',
-    social_services: '$t(designatedManagementObjective.socialServices)',
+    conservation_of_biodiversity: '$t(designatedManagementObjective.biodiversityConservation) ($t({{unit}}))',
+    multiple_use: '$t(designatedManagementObjective.multipleUse) ($t({{unit}}))',
+    other: '$t(common.other) ($t({{unit}}))',
+    production: '$t(designatedManagementObjective.production) ($t({{unit}}))',
+    protection_of_soil_and_water: '$t(designatedManagementObjective.soilWaterProtection) ($t({{unit}}))',
+    social_services: '$t(designatedManagementObjective.socialServices) ($t({{unit}}))',
   },
   naturallyRegeneratingForest: {
     title: 'مساحة الغابات المتجددة طبيعياً والمزروعة ({{startYear}} - {{endYear}})',
