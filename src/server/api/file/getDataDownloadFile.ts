d9603be6273c2b87b3f5108ca371dbcc280f15f9
sorja <ping@mirosorja.com>
2024-08-19 09:21:06 +0300

    use CycleRequest

--- src/server/api/file/getDataDownloadFile.ts
+++ src/server/api/file/getDataDownloadFile.ts
@@ -1,18 +1,18 @@
-import { Request, Response } from 'express'
+import { Response } from 'express'
+
+import { CycleRequest } from 'meta/api/request'
 
 import { FileRepository, fileTypes } from 'server/service/file'
 
-type Query = {
-  assessmentName: string
-  cycleName: string
+type Request = CycleRequest<{
   fileName: string
   fileType: string
   language: string
-}
+}>
 
 export const getDataDownloadFile = async (req: Request, res: Response) => {
   try {
-    const { assessmentName, cycleName, fileName, fileType, language } = <Query>req.query
+    const { assessmentName, cycleName, fileName, fileType, language } = req.query
     FileRepository.download(res, fileTypes.dataDownload(assessmentName, cycleName, fileName, fileType), language)
   } catch (err) {
     res.status(500).send('An error occurred fetching the file.')
