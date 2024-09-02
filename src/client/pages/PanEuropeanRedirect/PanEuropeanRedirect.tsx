20a9337e2a360f09b86ddd0c71093229fdbd23e9
Mino Togna <minotogna@users.noreply.github.com>
2024-09-02 19:18:42 +0200

    3899: PanEuropean redirect from old url (#3900)

---
+++ src/client/pages/PanEuropeanRedirect/PanEuropeanRedirect.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+import { AssessmentNames } from 'meta/assessment'
+import { Routes } from 'meta/routes'
+
+import { useAssessments } from 'client/store/assessment'
+
+const PanEuropeanRedirect: React.FC = () => {
+  const assessments = useAssessments()
+
+  const panEuropean = assessments.find((assessment) => assessment.props.name === AssessmentNames.panEuropean)
+  // TODO: find a better way to sort cycles (add created time ?)
+  const cycles = [...panEuropean.cycles].sort((c1, c2) => Number(c2.name) - Number(c1.name))
+  // find the last published cycle
+  const cycle = cycles.find((c) => c.published)
+
+  const assessmentName = panEuropean.props.name
+  const cycleName = cycle.name
+
+  return <Navigate to={Routes.Cycle.generatePath({ assessmentName, cycleName })} />
+}
+
+export default PanEuropeanRedirect
