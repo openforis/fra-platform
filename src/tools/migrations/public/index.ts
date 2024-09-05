d8128dbbdf56b61f78d1142c640be2c10a982455
sorja <ping@mirosorja.com>
2024-09-05 11:04:21 +0300

    2480 - Sort migrations and use Promises.each

--- src/tools/migrations/public/index.ts
+++ src/tools/migrations/public/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 
 import * as fs from 'fs'
 import * as path from 'path'
+import { Promises } from 'utils/promises'
 
 import { DB } from 'server/db'
 import { Logger } from 'server/utils/logger'
@@ -27,6 +28,7 @@ const init = async () => {
   migrationSteps = fs
     .readdirSync(path.join(__dirname, `steps`))
     .filter((file) => file !== 'template.ts' && file.endsWith('.ts') && !previousMigrations.includes(file))
+    .sort((a, b) => a.localeCompare(b))
 }
 
 const close = async () => {
@@ -37,23 +39,21 @@ const close = async () => {
 const exec = async () => {
   await init()
 
-  // eslint-disable-next-line no-restricted-syntax
-  for await (const file of migrationSteps) {
+  await Promises.each(migrationSteps, async (file) => {
     try {
       Logger.info(`Running migration ${file}`)
       // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require,import/no-dynamic-require
-      await require(`./steps/${file}`).default(client)
+      await require(`./steps/${file}`)
       executedMigrations.push(file)
     } catch (e) {
       Logger.error(e)
     }
-  }
+  })
+
   if (!process.argv.includes('--watch')) {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const file of executedMigrations) {
-      // eslint-disable-next-line no-await-in-loop
+    await Promises.each(executedMigrations, async (file) => {
       await client.query('insert into migrations (name) values ($1)', [file])
-    }
+    })
   }
 
   await close()
