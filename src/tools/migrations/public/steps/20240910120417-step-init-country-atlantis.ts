791ce89fb5a16c86e533ce3cc5e7230e0ec798ca
sorja <ping@mirosorja.com>
2024-09-11 14:27:29 +0300

    correctly number atlantis countries

--- src/tools/migrations/public/steps/20240910120417-step-init-country-atlantis.ts
+++ src/tools/migrations/public/steps/20240910120417-step-init-country-atlantis.ts
@@ -5,7 +5,8 @@ import { DB } from 'server/db'
 export default async () => {
   const pgp = pgPromise()
 
-  const countries = Array.from({ length: 20 }, (_, i) => ({ country_iso: `X${i + 1}`, config: { domain: 'tropical' } }))
+  const n = (i: number) => i.toString().padStart(2, '0')
+  const countries = Array.from({ length: 20 }, (_, i) => ({ country_iso: `X${n(i)}`, config: { domain: 'tropical' } }))
 
   const cs = new pgp.helpers.ColumnSet(['country_iso', 'config'], { table: 'country' })
   const query = `${pgp.helpers.insert(countries, cs)} ON CONFLICT (country_iso) DO NOTHING`
