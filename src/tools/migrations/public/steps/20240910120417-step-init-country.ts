b985b99fbf1bfb08e917729dc19b4967124cdeb6
sorja <ping@mirosorja.com>
2024-09-12 10:17:47 +0300

    2480  migrate all countries

--- src/tools/migrations/public/steps/20240910120417-step-init-country.ts
+++ src/tools/migrations/public/steps/20240910120417-step-init-country.ts
@@ -2,11 +2,10 @@ import * as pgPromise from 'pg-promise'
 
 import { DB } from 'server/db'
 
+import { countries } from './data/countries'
+
 export default async () => {
   const pgp = pgPromise()
-
-  const n = (i: number) => i.toString().padStart(2, '0')
-  const countries = Array.from({ length: 20 }, (_, i) => ({ country_iso: `X${n(i)}`, config: { domain: 'tropical' } }))
 
   const cs = new pgp.helpers.ColumnSet(['country_iso', 'config'], { table: 'country' })
   const query = `${pgp.helpers.insert(countries, cs)} ON CONFLICT (country_iso) DO NOTHING`
