b9cbaf8d39ab66c5dc7fcfc11b8e325378460c72
sorja <ping@mirosorja.com>
2024-09-12 10:41:01 +0300

    fix migration step typo

--- src/test/migrations/steps/20240912102424-step-fix-migrations-table-ddl.ts
+++ src/test/migrations/steps/20240912102424-step-fix-migrations-table-ddl.ts
@@ -5,6 +5,6 @@ export default async () => {
   await client.query(`
     alter table migrations
     alter column run_on set default now(),
-    alter column run_on type timestamp without time zone not null
+    alter column run_on type timestamp without time zone
   `)
 }
