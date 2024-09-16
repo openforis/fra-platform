8427d92979b85324ff44591553040d26a40499ad
sorja <ping@mirosorja.com>
2024-09-16 10:55:32 +0300

    3919 - fr translations

--- src/i18n/resources/fr/statisticalFactsheets.js
+++ src/i18n/resources/fr/statisticalFactsheets.js
@@ -1,22 +1,24 @@
 module.exports = {
   noData: 'Aucune donnée disponible',
   rowName: {
+    area: 'Superficie',
+    forest: 'Forêt',
     forestArea: 'Superficie forestière',
-    naturalForestArea: 'Forêt naturellement régénérée',
+    naturalForestArea: '$t(forestCharacteristics.naturalForestArea)',
     plantedForest: '$t(forestCharacteristics.plantedForest)',
     forestAreaWithinProtectedAreas: 'Superficie forestière à l’intérieur des aires protégées',
     landArea: 'Superficie des terres',
     primaryForest: '$t(specificForestCategories.primaryForest)',
     otherArea: 'Autre superficie',
     otherForest: 'Autre forêt',
-    private_ownership: '$t(forestOwnership.privateOwnership)',
-    public_ownership: '$t(forestOwnership.publicOwnership)',
+    private_ownership: 'Privée',
+    public_ownership: 'Publique',
     other: '$t(common.other)',
     unknown: '$t(fra.forestOwnership.unknown2025)',
     other_or_unknown: 'Autre ou inconnu',
   },
   forestArea: {
-    title: 'Superficie forestière ({{startYear}} - {{endYear}})',
+    title: 'Superficie forestière ({{startYear}} - {{endYear}}), $t({{unit}})',
   },
   carbonAndGrowingStock: {
     title: 'Volume de bois sur pied et Carbone dans les forêts ({{startYear}} - {{endYear}})',
@@ -26,13 +28,13 @@ module.exports = {
     growing_stock_total: '$t(growingStock.growingStock) ($t({{unit}}))',
   },
   forestAreaPercent: {
-    title: 'Superficie de forêt en % de la superficie des terres ({{year}})',
+    title: 'Superficie de forêt en % de la superficie totale des terres, {{year}}',
   },
   primaryForest: {
-    title: 'Forêt primaire en % de la superficie forestière ({{year}})',
+    title: 'Forêt primaire en % de la superficie forestière, {{year}}',
   },
   forestOwnership: {
-    title: '$t(forestOwnership.forestOwnership) ({{year}})',
+    title: '$t(forestOwnership.forestOwnership) %, {{year}}',
     public: 'Publique',
     private: 'Privée',
   },
@@ -40,16 +42,17 @@ module.exports = {
     title: '$t(forestAreaWithinProtectedAreas.header) ({{year}})',
   },
   primaryDesignatedManagementObjective: {
-    title: '$t(designatedManagementObjective.primaryDesignatedManagementObjective) ({{startYear}} - {{endYear}})',
+    title:
+      '$t(designatedManagementObjective.primaryDesignatedManagementObjective) ({{startYear}} - {{endYear}}), $t({{unit}})',
     rowName: '',
-    conservation_of_biodiversity: '$t(designatedManagementObjective.biodiversityConservation) ($t({{unit}}))',
-    multiple_use: '$t(designatedManagementObjective.multipleUse) ($t({{unit}}))',
-    other: '$t(common.other) ($t({{unit}}))',
-    production: '$t(designatedManagementObjective.production) ($t({{unit}}))',
-    protection_of_soil_and_water: '$t(designatedManagementObjective.soilWaterProtection) ($t({{unit}}))',
-    social_services: '$t(designatedManagementObjective.socialServices) ($t({{unit}}))',
+    conservation_of_biodiversity: '$t(designatedManagementObjective.biodiversityConservation)',
+    multiple_use: '$t(designatedManagementObjective.multipleUse)',
+    other: '$t(common.other)',
+    production: '$t(designatedManagementObjective.production)',
+    protection_of_soil_and_water: '$t(designatedManagementObjective.soilWaterProtection)',
+    social_services: '$t(designatedManagementObjective.socialServices)',
   },
   naturallyRegeneratingForest: {
-    title: 'Superficie de forêt naturellement régénérée et plantée ({{startYear}} - {{endYear}})',
+    title: 'Superficie de forêt naturellement régénérée et plantée ({{startYear}} - {{endYear}}), $t({{unit}})',
   },
 }
