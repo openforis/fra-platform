d15ff5fbcfe00f9822edcaec4a60e8d0960e53c7
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-09-20 14:58:03 -0500

    3930 - Add ar, fr and zh translations

--- src/i18n/resources/ar/admin.js
+++ src/i18n/resources/ar/admin.js
@@ -1,8 +1,13 @@
 module.exports = {
   admin: 'المدير',
+  enotfound: 'خطأ في نظام أسماء النطاقات',
   filter: 'غربلة وفق',
   invitationPending: 'تعليق الدعوة',
   invitationsAcceptedCount: 'عدد الدعوات المقبولة',
   invitationsSentCount: 'عدد الدعوات المرسلة',
+  lastStatus: 'آخر تحديث',
+  locationsCount: 'عدد المواقع',
+  urlParsingError: 'خطأ في التوصل إلى عنوان الموقع',
   usersCount: 'عدد المستخدمين',
+  verifyLinks: 'التحقق من الروابط',
 }
