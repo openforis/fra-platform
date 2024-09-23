8fc293701ee694e1873a3fbc5ea76e2a8593fa08
Mino Togna <minotogna@users.noreply.github.com>
2024-09-23 14:45:46 +0200

    3916 - Cycle clone: data (#3941)
    * 3916 - Cycle clone: areas
    
    * 3941: Clone data

--- src/server/controller/assessment/cloneCycle/cloneCycle.ts
+++ src/server/controller/assessment/cloneCycle/cloneCycle.ts
@@ -1,10 +1,15 @@
 import { Assessment, Cycle } from 'meta/assessment'
 import { User } from 'meta/user'
 
+import { cloneAreas } from 'server/controller/assessment/cloneCycle/_cloneAreas'
+import { cloneData } from 'server/controller/assessment/cloneCycle/_cloneData'
 import { cloneMetadata } from 'server/controller/assessment/cloneCycle/_cloneMetadata'
 import { generateMaterializedViews } from 'server/controller/assessment/cloneCycle/_generateMaterializedViews'
 import { CloneProps } from 'server/controller/assessment/cloneCycle/types'
 import { createCycle } from 'server/controller/assessment/createCycle'
+import { generateDataCache } from 'server/controller/assessment/generateDataCache'
+import { generateMetaCache } from 'server/controller/assessment/generateMetaCache'
+import { generateMetadataCache } from 'server/controller/assessment/generateMetadataCache'
 import { BaseProtocol, DB } from 'server/db'
 
 type Props = {
@@ -28,7 +33,14 @@ export const cloneCycle = async (props: Props, client: BaseProtocol = DB): Promi
     const cloneProps: CloneProps = { assessment, cycleSource, cycleTarget }
 
     await cloneMetadata(cloneProps, t)
+    await cloneAreas(cloneProps, t)
     await generateMaterializedViews(cloneProps, t)
+    await cloneData(cloneProps, t)
+
+    // update cache
+    await generateMetaCache(t)
+    await generateMetadataCache({ assessment }, t)
+    await generateDataCache({ assessment, cycle: cycleTarget }, t)
 
     return { assessment, cycle: cycleTarget }
   })
