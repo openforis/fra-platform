46d2527a2e381880ebc7a83fc1e9cc15a289f70c
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-09-25 08:09:54 -0500

    3949 - Add return types to table paginated hooks

--- src/client/store/ui/tablePaginated/hooks/index.ts
+++ src/client/store/ui/tablePaginated/hooks/index.ts
@@ -1,3 +1,5 @@
+import { TablePaginatedFilterValues, TablePaginatedOrderBy } from 'meta/tablePaginated'
+
 import { useAppSelector } from 'client/store/store'
 import { TablePaginatedSelectors } from 'client/store/ui/tablePaginated/selectors'
 
@@ -7,14 +9,14 @@ export const useTablePaginatedCount = (path: string): { total: number } =>
 export const useTablePaginatedData = <Datum>(path: string): Array<Datum> | undefined =>
   useAppSelector((state) => TablePaginatedSelectors.getData(state, path) as Array<Datum> | undefined)
 
-export const useTablePaginatedFilters = (path: string) =>
+export const useTablePaginatedFilters = (path: string): Record<string, TablePaginatedFilterValues> | undefined =>
   useAppSelector((state) => TablePaginatedSelectors.getFilters(state, path))
 
-export const useTablePaginatedFilterValue = (path: string, fieldName: string) =>
+export const useTablePaginatedFilterValue = (path: string, fieldName: string): TablePaginatedFilterValues | undefined =>
   useAppSelector((state) => TablePaginatedSelectors.getFilterValue(state, path, fieldName))
 
-export const useTablePaginatedOrderBy = (path: string) =>
+export const useTablePaginatedOrderBy = (path: string): TablePaginatedOrderBy | undefined =>
   useAppSelector((state) => TablePaginatedSelectors.getOrderBy(state, path))
 
-export const useTablePaginatedPage = (path: string) =>
+export const useTablePaginatedPage = (path: string): number =>
   useAppSelector((state) => TablePaginatedSelectors.getPage(state, path))
