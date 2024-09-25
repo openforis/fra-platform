e74f6078c7b8317f284a926e3af10e4726778b16
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-09-25 11:13:15 -0500

    3949 - Remove handleChangeThrottle

--- src/client/components/TablePaginated/Filters/Text/Text.tsx
+++ src/client/components/TablePaginated/Filters/Text/Text.tsx
@@ -1,10 +1,9 @@
 import './Text.scss'
-import React, { useCallback, useMemo, useState } from 'react'
-
-import { Functions } from 'utils/functions'
+import React from 'react'
 
 import { useAppDispatch } from 'client/store'
 import { TablePaginatedActions } from 'client/store/ui/tablePaginated'
+import { useTablePaginatedFilterValue } from 'client/store/ui/tablePaginated/hooks'
 import Icon from 'client/components/Icon'
 import InputText from 'client/components/Inputs/InputText'
 import { TablePaginatedFilter } from 'client/components/TablePaginated/types'
@@ -17,43 +16,26 @@ const Text = (props: Props) => {
   const { fieldName, label, path } = props
   const dispatch = useAppDispatch()
 
-  const [localInputValue, setLocalInputValue] = useState('')
+  const filterValue = useTablePaginatedFilterValue<string>(path, fieldName)
 
-  const handleChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { value } = event.target
-      dispatch(
-        TablePaginatedActions.setFilterValue({
-          fieldName,
-          path,
-          value,
-        })
-      )
-    },
-    [dispatch, fieldName, path]
-  )
-
-  const handleChangeThrottle = useMemo(
-    () =>
-      Functions.throttle((event: React.ChangeEvent<HTMLInputElement>) => handleChange(event), 250, { trailing: true }),
-    [handleChange]
-  )
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    dispatch(
+      TablePaginatedActions.setFilterValue({
+        fieldName,
+        path,
+        value,
+      })
+    )
+  }
 
   const handleClearInput = () => {
-    setLocalInputValue('')
     dispatch(TablePaginatedActions.resetFilter({ fieldName, path }))
   }
 
   return (
     <div className="table-paginated-filters__input-text-container">
-      <InputText
-        onChange={(e) => {
-          setLocalInputValue(e.target.value)
-          handleChangeThrottle(e)
-        }}
-        placeholder={label}
-        value={localInputValue}
-      />
+      <InputText onChange={handleChange} placeholder={label} value={filterValue ?? ''} />
       <button className="clear-button icon" onClick={handleClearInput} type="button">
         <Icon className="icon-sub" name="remove" />
       </button>
