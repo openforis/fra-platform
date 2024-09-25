b6ac87071cb39bef5e3e8ac9fcf86c05f73df05b
minotogna <mino.togna@gmail.com>
2024-09-25 10:28:45 +0200

    3916: Fix invitations count/list per cycle

--- src/server/repository/public/userInvitation/getCount.ts
+++ src/server/repository/public/userInvitation/getCount.ts
@@ -1,15 +1,28 @@
 import { Objects } from 'utils/objects'
 
+import { Assessment, Cycle } from 'meta/assessment'
 import { TablePaginatedCount } from 'meta/tablePaginated'
 
 import { BaseProtocol, DB } from 'server/db'
 
-export const getCount = async (client: BaseProtocol = DB): Promise<TablePaginatedCount> => {
+type Props = {
+  assessment: Assessment
+  cycle: Cycle
+}
+
+export const getCount = async (props: Props, client: BaseProtocol = DB): Promise<TablePaginatedCount> => {
+  const { assessment, cycle } = props
+
   return client.one<TablePaginatedCount>(
     `
-        select count(ur.id) as total from users_role ur
+        select count(ur.id) as total
+        from users_role ur
+                 left join public.assessment a on ur.assessment_id = a.id
+                 left join public.assessment_cycle ac on ur.cycle_uuid = ac.uuid and a.id = ac.assessment_id
+        where a.id = $1
+          and ac.id = $2
     `,
-    [],
+    [assessment.id, cycle.id],
     (res) => Objects.camelize(res)
   )
 }
--- src/server/repository/public/userInvitation/getMany.ts
+++ src/server/repository/public/userInvitation/getMany.ts
@@ -1,11 +1,14 @@
 import { Objects } from 'utils/objects'
 
+import { Assessment, Cycle } from 'meta/assessment'
 import { TablePaginatedOrderByDirection } from 'meta/tablePaginated'
 import { UserInvitationSummary } from 'meta/user/userInvitationSummary'
 
 import { BaseProtocol, DB } from 'server/db'
 
 type Props = {
+  assessment: Assessment
+  cycle: Cycle
   limit: string
   offset: string
   orderBy?: string
@@ -13,7 +16,7 @@ type Props = {
 }
 
 export const getMany = async (props: Props, client: BaseProtocol = DB): Promise<Array<UserInvitationSummary>> => {
-  const { limit, offset, orderBy, orderByDirection } = props
+  const { assessment, cycle, limit, offset, orderBy, orderByDirection } = props
 
   return client.map<UserInvitationSummary>(
     `
@@ -21,10 +24,14 @@ export const getMany = async (props: Props, client: BaseProtocol = DB): Promise<
                u.email
         from users_role ur
                  left join public.users u on ur.user_id = u.id
+                 left join public.assessment a on ur.assessment_id = a.id
+                 left join public.assessment_cycle ac on ur.cycle_uuid = ac.uuid and a.id = ac.assessment_id
+        where a.id = $1
+          and ac.id = $2
         order by ${orderBy ?? 'country_iso'} ${orderByDirection ?? TablePaginatedOrderByDirection.asc} nulls last
-        limit $1 offset $2
+        limit $3 offset $4
     `,
-    [limit, offset],
+    [assessment.id, cycle.id, limit, offset],
     (row) => Objects.camelize(row)
   )
 }
