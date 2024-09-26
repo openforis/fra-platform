aa403489f0706b351c78f321f88ef7eff5e1673c
sorja <ping@mirosorja.com>
2024-09-26 12:59:27 +0300

    3973 - Introduce regionCode when fetching region level data

--- src/client/components/Dashboard/hooks/useGetTableData.ts
+++ src/client/components/Dashboard/hooks/useGetTableData.ts
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useMemo } from 'react'
 
-import { CountryIso } from 'meta/area'
+import { Areas, CountryIso, RegionCode } from 'meta/area'
 
 import { useAppDispatch } from 'client/store'
 import { DataActions } from 'client/store/data'
+import type { Props as GetTableDataProps } from 'client/store/data/actions/getTableDataProps'
 import { useHomeCountriesFilter } from 'client/store/ui/home'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import { Props } from 'client/components/Dashboard/props'
@@ -23,8 +24,14 @@ export const useGetTableData = (props: Props): void => {
 
   const fetchTableData = useCallback(() => {
     if (dependencies.size > 0) {
-      const propsFetch = { assessmentName, cycleName, countryIso, mergeOdp: true }
-      dispatch(DataActions.getTableData({ ...propsFetch, countryISOs, tableNames: Array.from(dependencies) }))
+      const tableNames = Array.from(dependencies)
+      const propsFetch: GetTableDataProps = { assessmentName, cycleName, countryIso, tableNames, mergeOdp: true }
+
+      // When fetching data for Dashboard region level, include regionCode
+      if (!Areas.isISOCountry(countryIso)) {
+        propsFetch.regionCode = countryIso as RegionCode
+      }
+      dispatch(DataActions.getTableData({ ...propsFetch, countryISOs }))
     }
   }, [assessmentName, cycleName, countryIso, dependencies, dispatch, countryISOs])
 
