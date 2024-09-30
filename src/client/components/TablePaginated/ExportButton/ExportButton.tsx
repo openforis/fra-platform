d8e64757cd3efbd351d4a9492907828b3b6a986c
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-09-30 18:21:17 -0500

    3949 - Export filtered data

--- src/client/components/TablePaginated/ExportButton/ExportButton.tsx
+++ src/client/components/TablePaginated/ExportButton/ExportButton.tsx
@@ -2,19 +2,23 @@ import './ExportButton.scss'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { TablePaginatedFilterType } from 'meta/tablePaginated'
+
 import { useButtonClassName } from 'client/components/Buttons/Button'
 import Icon from 'client/components/Icon'
+import { TablePaginatedFilter } from 'client/components/TablePaginated/types'
 
 import { useExportUrl } from './hooks/useExportUrl'
 
 type Props = {
+  filters: Array<TablePaginatedFilter<TablePaginatedFilterType>>
   path: string
 }
 
 const ExportButton: React.FC<Props> = (props) => {
-  const { path } = props
+  const { filters, path } = props
 
-  const exportUrl = useExportUrl({ path })
+  const exportUrl = useExportUrl({ filters, path })
 
   const className = useButtonClassName({ iconName: 'hit-down' })
   return (
--- src/client/components/TablePaginated/ExportButton/hooks/useExportUrl.tsx
+++ src/client/components/TablePaginated/ExportButton/hooks/useExportUrl.tsx
@@ -1,29 +1,37 @@
 import { useMemo } from 'react'
 
+import { TablePaginatedFilterType, TablePaginateds } from 'meta/tablePaginated'
+
 import { useTablePaginatedOrderBy } from 'client/store/ui/tablePaginated'
 import { useSectionRouteParams } from 'client/hooks/useRouteParams'
+import { useFilterValues } from 'client/components/TablePaginated/hooks/useFilterValues'
+import { TablePaginatedFilter } from 'client/components/TablePaginated/types'
 
 type Props = {
+  filters: Array<TablePaginatedFilter<TablePaginatedFilterType>>
   path: string
 }
 
 export const useExportUrl = (props: Props): string => {
-  const { path } = props
+  const { filters, path } = props
 
   const { assessmentName, countryIso, cycleName, sectionName } = useSectionRouteParams()
   const orderBy = useTablePaginatedOrderBy(path)
+  const filterValues = useFilterValues({ filters, path })
 
   return useMemo<string>(() => {
+    const encodedFilters = TablePaginateds.encodeFilters(filterValues)
     const queryParams = new URLSearchParams(
       Object.entries({
         assessmentName,
         countryIso,
         cycleName,
+        filters: encodedFilters,
         orderBy: orderBy?.property,
         orderByDirection: orderBy?.direction,
         sectionName,
       }).filter(([, value]) => value !== undefined)
     )
     return `${path}/export?${queryParams.toString()}`
-  }, [assessmentName, countryIso, cycleName, orderBy, path, sectionName])
+  }, [assessmentName, countryIso, cycleName, filterValues, orderBy, path, sectionName])
 }
