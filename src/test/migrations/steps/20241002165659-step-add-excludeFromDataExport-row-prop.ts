73ec8566669416c0f11e305238dfc7c2802208d5
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-10-02 17:45:20 -0500

    3987 - Add excludeFromDataExport prop to row

---
+++ src/test/migrations/steps/20241002165659-step-add-excludeFromDataExport-row-prop.ts
@@ -0,0 +1,31 @@
+import { AssessmentNames } from 'meta/assessment'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { BaseProtocol, Schemas } from 'server/db'
+
+export default async (client: BaseProtocol) => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    { assessmentName: AssessmentNames.fra, cycleName: '2025' },
+    client
+  )
+
+  const schemaAssessment = Schemas.getName(assessment)
+
+  await client.query(`
+    update ${schemaAssessment}."row" r
+    set props = props || jsonb_build_object('excludeFromDataExport', jsonb_build_object('${cycle.uuid}', true))
+    where r.props ->> 'type' = 'data'
+    and r.props ->> 'variableName' = 'mostRecentYear'
+    and r.table_id = (
+      select t.id
+      from ${schemaAssessment}."table" t
+      left join ${schemaAssessment}.table_section ts on t.table_section_id = ts.id
+      left join ${schemaAssessment}.section s on s.id = ts.section_id
+      where s.props ->> 'name' = 'growingStockComposition'
+      and t.props ->> 'name' = 'growingStockComposition2025'
+    );
+  `)
+
+  await AssessmentController.generateMetaCache(client)
+  await AssessmentController.generateMetadataCache({ assessment }, client)
+}
