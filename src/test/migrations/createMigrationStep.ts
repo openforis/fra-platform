95fd18c857b374ab9d53ad7b07a017be610d2df5
sorja <ping@mirosorja.com>
2024-10-02 10:11:48 +0300

    migration-steps create: simplify getDate

--- src/test/migrations/createMigrationStep.ts
+++ src/test/migrations/createMigrationStep.ts
@@ -30,14 +30,11 @@ const askStepName = (): Promise<string> => {
  * @returns The current date in the format YYYYMMDDHHMMSS
  */
 const getDate = (): string => {
-  const date = new Date()
-  const year = date.getFullYear()
-  const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  const day = date.getDate().toString().padStart(2, '0')
-  const hour = date.getHours().toString().padStart(2, '0')
-  const minute = date.getMinutes().toString().padStart(2, '0')
-  const second = date.getSeconds().toString().padStart(2, '0')
-  return `${year}${month}${day}${hour}${minute}${second}`
+  const now = new Date()
+  return now
+    .toISOString()
+    .replace(/[-:T.]/g, '')
+    .slice(0, 14)
 }
 
 export const createMigrationStep = async (initialStepName: string): Promise<{ fileName: string; filePath: string }> => {
