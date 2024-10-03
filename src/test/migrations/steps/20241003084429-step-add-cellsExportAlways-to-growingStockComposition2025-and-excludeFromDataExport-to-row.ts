a534cb512f16816aec3558196e876bb9caf84181
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-10-03 08:48:04 -0500

    3987 - Merge migration steps

--- src/test/migrations/steps/20241003084429-step-add-cellsExportAlways-to-growingStockComposition2025-and-excludeFromDataExport-to-row.ts
+++ src/test/migrations/steps/20241003084429-step-add-cellsExportAlways-to-growingStockComposition2025-and-excludeFromDataExport-to-row.ts
@@ -12,6 +12,32 @@ export default async (client: BaseProtocol) => {
   const schemaAssessment = Schemas.getName(assessment)
 
   await client.query(`
+    update ${schemaAssessment}."table" t
+    set props = props
+        || jsonb_build_object(
+            'cellsExportAlways',
+            jsonb_build_object(
+                '${cycle.uuid}',
+                '[{
+                    "columnName": "mostRecentYear",
+                    "variableName": "mostRecentYear",
+                    "unit": null,
+                    "format": "integer"
+                }]'::jsonb
+            )
+      )
+    from (
+      select t2.id
+      from ${schemaAssessment}."table" t2
+      left join ${schemaAssessment}.table_section ts on t2.table_section_id = ts.id
+      left join ${schemaAssessment}.section s on s.id = ts.section_id
+      where s.props ->> 'name' = 'growingStockComposition'
+      and t2.props ->> 'name' = 'growingStockComposition2025'
+    ) as src
+    where t.id = src.id;
+  `)
+
+  await client.query(`
     update ${schemaAssessment}."row" r
     set props = props || jsonb_build_object('excludeFromDataExport', jsonb_build_object('${cycle.uuid}', true))
     where r.props ->> 'type' = 'data'
