34a0007e2a86df3eeb4f83f0383765a7580533cc
sorja <ping@mirosorja.com>
2024-10-07 11:34:58 +0300

    3989 - getUserRoles: introduce helper Users.getUserRoles

--- src/meta/user/users.ts
+++ src/meta/user/users.ts
@@ -194,9 +194,35 @@ const isPersonalInfoRequired = (user: User, role: UserRole<RoleName, any>) => {
 
 const getFullName = (user: User) => [user.props.name, user.props.surname].join(' ').trim()
 
+type UserRoles = {
+  isAdministrator: boolean
+  isAlternateNationalCorrespondent: boolean
+  isCollaborator: boolean
+  isNationalCorrespondent: boolean
+  isReviewer: boolean
+  isViewer: boolean
+}
+
+/**
+ * Get the roles of a user for a specific country and cycle
+ * @param {User} user - User
+ * @param {CountryIso} countryIso - Country ISO code
+ * @param {Cycle} cycle - Cycle
+ * @returns {UserRoles} An object containing boolean flags for each possible role
+ */
+const getUserRoles = (user: User, countryIso: CountryIso, cycle: Cycle): UserRoles => ({
+  isAdministrator: isAdministrator(user),
+  isAlternateNationalCorrespondent: isAlternateNationalCorrespondent(user, countryIso, cycle),
+  isCollaborator: isCollaborator(user, countryIso, cycle),
+  isNationalCorrespondent: isNationalCorrespondent(user, countryIso, cycle),
+  isReviewer: isReviewer(user, countryIso, cycle),
+  isViewer: isViewer(user, countryIso, cycle),
+})
+
 export const Users = {
-  getRole,
   getFullName,
+  getRole,
+  getUserRoles,
 
   isAdministrator,
   isAlternateNationalCorrespondent,
