3640af66dad845d20d629ad78bea64a079f41e92
minotogna <mino.togna@gmail.com>
2024-10-07 10:37:22 +0200

    infra - review comments

--- src/tools/db/importBackup/_pgRestore.sh
+++ src/tools/db/importBackup/_pgRestore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dropdb frap-dev --user frap
+dropdb -f frap-dev --user frap
 createdb --owner=frap --username=frap frap-dev
 pg_restore --no-owner --clean --if-exists --verbose -U frap -d frap-dev /backup
