7db067752732ed71a8983a916c682d95eba0ceb3
sorja <ping@mirosorja.com>
2024-10-10 16:51:59 +0300

    4000 - Dashboard store

--- src/client/store/metadata/selectors/index.ts
+++ src/client/store/metadata/selectors/index.ts
@@ -12,6 +12,17 @@ const getSections = createSelector(
   ],
   (metadataState, assessmentName, cycleName) => metadataState.sections?.[assessmentName]?.[cycleName]
 )
+
+const getDashboard = createSelector(
+  [
+    (state: RootState) => state.metadata,
+    (_state: RootState, assessmentName: AssessmentName) => assessmentName,
+    (_state: RootState, _assessmentName: AssessmentName, cycleName: CycleName) => cycleName,
+  ],
+  (metadataState, assessmentName, cycleName) => metadataState.dashboard?.[assessmentName]?.[cycleName]
+)
+
 export const MetadataSelectors = {
   getSections,
+  getDashboard,
 }
