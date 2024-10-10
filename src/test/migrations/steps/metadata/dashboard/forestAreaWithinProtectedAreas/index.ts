229a15207bc7ab4c43dbc4a67c35dcd80eb784f2
Miró Sorja <ping@mirosorja.com>
2024-10-10 11:04:07 +0200

    4000 - Dashboard metadata migration to node ext (#4015)
    * 4000 - Dashboard metadata migration to node ext
    
    * type camel case
    
    * update nodeExtType: dataquery -> dashboard

---
+++ src/test/migrations/steps/metadata/dashboard/forestAreaWithinProtectedAreas/index.ts
@@ -0,0 +1,68 @@
+import { Cycle, TableNames, Unit } from 'meta/assessment'
+import { ChartColor } from 'meta/chart'
+import { DashboardItemType, DashboardPieChart } from 'meta/dashboard'
+
+import { getTable, RowsMetadata } from '../utils'
+
+const cols: Record<string, Array<string>> = {
+  '2020': ['2020'],
+  '2025': ['2025'],
+}
+
+const tableId = 5
+const tableName = 'forestAreaWithinProtectedAreas'
+const variableName = 'forest_area_within_protected_areas'
+
+export const forestAreaWithinProtectedAreas = (cycle: Cycle, region: boolean): DashboardPieChart => {
+  const columnName = cols[cycle.name][0]
+  const rowMetadata: RowsMetadata = [
+    {
+      id: 1,
+      variableName,
+      label: { key: `statisticalFactsheets.rowName.protected` },
+      calculateFn: `${tableName}.${variableName} ${region ? '/ 1000' : ''}`,
+      // calculateFn: `100 * ${tableName}.${variableName} / ${TableNames.extentOfForest}.forestArea`,
+      calculationDependencies: [
+        { tableName, variableName },
+        { tableName: TableNames.extentOfForest, variableName: 'forestArea' },
+      ],
+    },
+    {
+      id: 2,
+      variableName: 'forestArea',
+      label: { key: `statisticalFactsheets.rowName.other` },
+      calculateFn: `${TableNames.extentOfForest}.forestArea ${region ? '/ 1000' : ''} - ${tableName}.${variableName} ${
+        region ? '/ 1000' : ''
+      }`,
+      // calculateFn: `100 - 100 * ${tableName}.${variableName} / ${TableNames.extentOfForest}.forestArea`,
+      calculationDependencies: [
+        { tableName, variableName },
+        { tableName: TableNames.extentOfForest, variableName: 'forestArea' },
+      ],
+    },
+  ]
+
+  return {
+    type: DashboardItemType.pieChart,
+    title: { key: 'statisticalFactsheets.forestAreaWithinProtectedAreas.title', params: { year: columnName } },
+    table: getTable({ cycle, cols: cols[cycle.name], tableId, rowMetadata, tableName }),
+    chart: {
+      cells: [
+        {
+          variableName: 'forestArea',
+          color: ChartColor.green,
+          columnName,
+          label: { key: 'statisticalFactsheets.rowName.other' },
+          unit: `unit.${Unit.haThousand}`,
+        },
+        {
+          variableName,
+          color: ChartColor.forestLight,
+          columnName,
+          label: { key: 'statisticalFactsheets.rowName.protected' },
+          unit: `unit.${Unit.haThousand}`,
+        },
+      ],
+    },
+  }
+}
---
+++ src/test/migrations/steps/metadata/dashboard/naturallyRegeneratingForestArea/index.ts
@@ -0,0 +1,58 @@
+import { Cycle, TableNames, Unit } from 'meta/assessment'
+import { ChartColor } from 'meta/chart'
+import { DashboardBarChart, DashboardItemType } from 'meta/dashboard'
+
+import type { RowsMetadata } from '../utils'
+import { getTable } from '../utils'
+
+const commonColumns = ['1990', '2000', '2010', '2020']
+
+const cols: Record<string, Array<string>> = {
+  '2020': commonColumns,
+  '2025': [...commonColumns, '2025'],
+}
+
+const tableName = TableNames.forestCharacteristics
+const tableId = 8
+export const naturallyRegeneratingForestArea = (cycle: Cycle, region: boolean): DashboardBarChart => {
+  const cells = [
+    {
+      variableName: 'naturalForestArea',
+      color: ChartColor.green,
+      unit: `unit.${Unit.haThousand}`,
+      label: { key: 'statisticalFactsheets.rowName.naturalForestArea' },
+    },
+    {
+      variableName: 'plantedForest',
+      color: ChartColor.forestPlanted,
+      unit: `unit.${Unit.haThousand}`,
+      label: { key: 'statisticalFactsheets.rowName.plantedForest' },
+    },
+  ]
+  const rowMetadata = (region: boolean): RowsMetadata => {
+    return [
+      ...cells.map(({ variableName }, i) => ({
+        id: i + 1,
+        variableName,
+        label: { key: `statisticalFactsheets.rowName.${variableName}` },
+        calculateFn: `${tableName}.${variableName} ${region ? '/ 1000' : ''}`,
+        calculationDependencies: [{ tableName, variableName }],
+      })),
+    ]
+  }
+
+  return {
+    type: DashboardItemType.barChart,
+    title: {
+      key: 'statisticalFactsheets.naturallyRegeneratingForest.title',
+      params: { startYear: cols[cycle.name].at(0), endYear: cols[cycle.name].at(-1), unit: `unit.${Unit.haThousand}` },
+    },
+    table: getTable({ cycle, cols: cols[cycle.name], tableId, rowMetadata: rowMetadata(region), tableName }),
+    chart: {
+      columns: cols[cycle.name],
+      cells,
+      xAxis: { label: { key: 'common.year' } },
+      yAxis: { label: { key: `unit.${Unit.haThousand}` } },
+    },
+  }
+}
