6bf2983b2675e4c887eacb206556265f1b5191d3
sorja <ping@mirosorja.com>
2024-10-15 08:49:03 +0300

    4000 - dashboard: refetch even when existing items (region)

--- src/client/store/metadata/hooks/useGetDashboard.ts
+++ src/client/store/metadata/hooks/useGetDashboard.ts
@@ -4,16 +4,12 @@ import { useAppDispatch } from 'client/store'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 
 import { MetadataActions } from '../slice'
-import { useDashboardItems } from './useDashboardItems'
 
 export const useGetDashboard = () => {
   const dispatch = useAppDispatch()
   const { assessmentName, cycleName, countryIso } = useCountryRouteParams()
-  const dashboardItems = useDashboardItems()
 
   useEffect(() => {
-    if (!dashboardItems) {
-      dispatch(MetadataActions.getDashboard({ assessmentName, cycleName, countryIso }))
-    }
-  }, [assessmentName, cycleName, countryIso, dispatch, dashboardItems])
+    dispatch(MetadataActions.getDashboard({ assessmentName, cycleName, countryIso }))
+  }, [assessmentName, cycleName, countryIso, dispatch])
 }
