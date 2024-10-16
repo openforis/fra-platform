45d2f2ac73fd7f88fe554a2ec1d4dd150e26c244
Mino Togna <minotogna@users.noreply.github.com>
2024-10-16 11:55:23 +0200

    4033 - NationalData-DataSources Cycle 2029: data/metadata clone (#4034)
    * 4033 - Do not import dataSources.text prop / Fix clone cycle props
    
    * 4033 - improve Objects.isEmpty + unit test
    
    * 4033 - fix DataSources grid
    
    * 4033 - Do not import dataSources.text value

--- src/meta/assessment/tables.ts
+++ src/meta/assessment/tables.ts
@@ -1,3 +1,5 @@
+import { Objects } from 'utils/objects'
+
 import { Cycle } from 'meta/assessment/cycle'
 import { Table } from 'meta/assessment/table'
 
@@ -7,18 +9,19 @@ const cloneProps = (props: { cycleSource: Cycle; cycleTarget: Cycle; table: Tabl
   const { uuid: cycleSourceUuid } = cycleSource
   const { uuid: cycleTargetUuid } = cycleTarget
 
-  const _props: Table['props'] = { ...table.props }
+  const _props: Table['props'] = Objects.cloneDeep(table.props)
   _props.cycles.push(cycleTargetUuid)
 
   if (_props.cellsExportAlways?.[cycleSourceUuid])
-    _props.cellsExportAlways[cycleTargetUuid] = _props.cellsExportAlways[cycleSourceUuid]
-  if (_props.columnNames?.[cycleSourceUuid]) _props.columnNames[cycleTargetUuid] = _props.columnNames[cycleSourceUuid]
+    _props.cellsExportAlways[cycleTargetUuid] = Objects.cloneDeep(_props.cellsExportAlways[cycleSourceUuid])
+  if (_props.columnNames?.[cycleSourceUuid])
+    _props.columnNames[cycleTargetUuid] = Objects.cloneDeep(_props.columnNames[cycleSourceUuid])
   if (_props.columnsExport?.[cycleSourceUuid])
-    _props.columnsExport[cycleTargetUuid] = _props.columnsExport[cycleSourceUuid]
+    _props.columnsExport[cycleTargetUuid] = Objects.cloneDeep(_props.columnsExport[cycleSourceUuid])
   if (_props.columnsExportAlways?.[cycleSourceUuid])
-    _props.columnsExportAlways[cycleTargetUuid] = _props.columnsExportAlways[cycleSourceUuid]
+    _props.columnsExportAlways[cycleTargetUuid] = Objects.cloneDeep(_props.columnsExportAlways[cycleSourceUuid])
   if (_props.disableErrorMessage?.[cycleSourceUuid])
-    _props.disableErrorMessage[cycleTargetUuid] = _props.disableErrorMessage[cycleSourceUuid]
+    _props.disableErrorMessage[cycleTargetUuid] = Objects.cloneDeep(_props.disableErrorMessage[cycleSourceUuid])
 
   return _props
 }
