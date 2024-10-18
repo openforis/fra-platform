4c76f2d2090ed91a20df59b9432ac42f426cd894
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-10-18 16:53:33 -0500

    4002 - Add Filter multiselect styling and tooltip

--- src/client/components/TablePaginated/Filters/MultiSelect/MultiSelect.tsx
+++ src/client/components/TablePaginated/Filters/MultiSelect/MultiSelect.tsx
@@ -1,6 +1,10 @@
+import './MultiSelect.scss'
 import React from 'react'
 
+import classNames from 'classnames'
+
 import { TablePaginatedFilterType } from 'meta/tablePaginated'
+import { TooltipId } from 'meta/tooltip'
 
 import { useAppDispatch } from 'client/store'
 import { TablePaginatedActions } from 'client/store/ui/tablePaginated'
@@ -8,16 +12,20 @@ import { useTablePaginatedFilterValue } from 'client/store/ui/tablePaginated/hoo
 import Select from 'client/components/Inputs/Select'
 import { TablePaginatedFilter } from 'client/components/TablePaginated/types'
 
+import { useTooltipContent } from './hooks/useTooltipContent'
+
 type Props = TablePaginatedFilter<TablePaginatedFilterType.MULTI_SELECT> & {
   path: string
 }
 
-const MultiSelect = (props: Props) => {
+const MultiSelect: React.FC<Props> = (props: Props) => {
   const { fieldName, label, path, options } = props
 
   const dispatch = useAppDispatch()
 
-  const filterValue = useTablePaginatedFilterValue<string>(path, fieldName)
+  const { hideTooltip, showTooltip, tooltipContent } = useTooltipContent({ fieldName, options, path })
+
+  const filterValue = useTablePaginatedFilterValue<Array<string>>(path, fieldName)
 
   const handleChange = (value: Array<string>) => {
     dispatch(
@@ -29,7 +37,29 @@ const MultiSelect = (props: Props) => {
     )
   }
 
-  return <Select isMulti onChange={handleChange} options={options} placeholder={label} value={filterValue} />
+  return (
+    <div
+      className="filter-multiselect__tooltip-trigger"
+      data-tooltip-content={tooltipContent}
+      data-tooltip-id={TooltipId.info}
+    >
+      <Select
+        classNames={{
+          container: classNames('filter-multiselect__container', {
+            active: filterValue?.length > 0,
+          }),
+        }}
+        isMulti
+        multiLabelKey="admin.role"
+        onChange={handleChange}
+        onMenuClose={showTooltip}
+        onMenuOpen={hideTooltip}
+        options={options}
+        placeholder={label}
+        value={filterValue}
+      />
+    </div>
+  )
 }
 
 export default MultiSelect
---
+++ src/client/components/TablePaginated/Filters/MultiSelect/hooks/useTooltipContent.ts
@@ -0,0 +1,58 @@
+import { useCallback, useMemo, useState } from 'react'
+
+import { Objects } from 'utils/objects'
+
+import { TablePaginatedFilterType } from 'meta/tablePaginated'
+
+import { useTablePaginatedFilterValue } from 'client/store/ui/tablePaginated/hooks'
+import { TablePaginatedFilter } from 'client/components/TablePaginated/types'
+
+type Props = {
+  fieldName: string
+  options: TablePaginatedFilter<TablePaginatedFilterType.MULTI_SELECT>['options']
+  path: string
+}
+
+type Returned = {
+  hideTooltip: () => void
+  showTooltip: () => void
+  tooltipContent: string | null
+}
+
+export const useTooltipContent = (props: Props): Returned => {
+  const { fieldName, options, path } = props
+  const filterValue = useTablePaginatedFilterValue<Array<string>>(path, fieldName)
+  const [isTooltipVisible, setTooltipVisible] = useState(true)
+
+  const valueToLabelMap = useMemo(() => {
+    return options.reduce((acc, { value, label }) => {
+      // eslint-disable-next-line no-param-reassign
+      acc[value] = label
+      return acc
+    }, {} as Record<string, string>)
+  }, [options])
+
+  const tooltipContent = useMemo(() => {
+    if (Objects.isEmpty(filterValue)) return null
+
+    const selectedLabels = filterValue.reduce<Array<string>>((acc, value) => {
+      const label = valueToLabelMap[value]
+      if (!Objects.isEmpty(label)) acc.push(label)
+      return acc
+    }, [])
+
+    if (selectedLabels.length === 0) {
+      return null
+    }
+    return selectedLabels.join(', ')
+  }, [filterValue, valueToLabelMap])
+
+  const hideTooltip = useCallback(() => setTooltipVisible(false), [])
+  const showTooltip = useCallback(() => setTooltipVisible(true), [])
+
+  return {
+    hideTooltip,
+    showTooltip,
+    tooltipContent: isTooltipVisible ? tooltipContent : null,
+  }
+}
