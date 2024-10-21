aa7d8540fdba6ad75ad062261ebf336ae88eb263
sorja <ping@mirosorja.com>
2024-10-21 14:56:54 +0300

    4032 -  migrate valid roles and populate created at

--- src/test/migrations/steps/20241016104908-step-users_role-invitation-split.ts
+++ src/test/migrations/steps/20241016104908-step-users_role-invitation-split.ts
@@ -1,8 +1,73 @@
+import * as pgPromise from 'pg-promise'
+import { Objects } from 'utils/objects'
+
+import { RoleName, UserRole } from 'meta/user'
+
 import { BaseProtocol, DB } from 'server/db'
 import { getUsersInvitationDDL, getUsersRoleDDL } from 'server/repository/public/ddl/getCreatePublicSchemaDDL'
 import { Logger } from 'server/utils/logger'
 
+type DBRole = {
+  userId: number
+  userUuid: string
+  assessmentUuid: string
+  countryIso: string
+  role: string
+  cycleUuid: string
+  permissions: string
+  props: string
+  createdAt: string
+  invitationUuid: string
+}
+
+type DBActivityLogRole = UserRole<RoleName>
+
 const client: BaseProtocol = DB
+
+const _handleRole = async (role: DBRole): Promise<DBRole> => {
+  // Role created by admin
+  if (role.invitationUuid) {
+    if (!role.createdAt) {
+      throw new Error(`Created at is null for invitation role: ${JSON.stringify(role)}`)
+    }
+    return role
+  }
+
+  // Role created through invitation
+  if (role.createdAt) {
+    return role
+  }
+
+  // Get all activity log entries for current roles user (match through userId and message)
+  const activityLogEntries = await client.manyOrNone(`
+    select * from public.activity_log where  (target->>'userId')::integer =  ${role.userId} and message = 'userRolesUpdate' order by time asc
+  `)
+
+  // It is possible we don't have any matches, e.g. Roles for FRA 2020 added by admin
+  if (activityLogEntries.length === 0) {
+    Logger.warn(
+      `No activity log entry found for role: countryIso=${role.countryIso}, role=${role.role}, userUuid=${role.userUuid}`
+    )
+    return role
+  }
+
+  // find in which activity log entry the role was first seen
+  const firstActivityLogEntry = activityLogEntries.find((entry) =>
+    entry.target.roles.some((r: DBActivityLogRole) => r.countryIso === role.countryIso)
+  )
+
+  if (!firstActivityLogEntry) {
+    Logger.warn(
+      `No activity log entry found for role ${role.countryIso} - ${JSON.stringify(role.role)} - ${role.userUuid}`
+    )
+    return role
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  role.createdAt = firstActivityLogEntry.time
+
+  return role
+}
 
 export default async () => {
   // ---- 0. Check that the migration is not already ran
@@ -12,6 +77,8 @@ export default async () => {
     Logger.info('users_invitation table already exists')
     return
   }
+
+  const pgp = pgPromise()
 
   // ---- 1. Add unique index on users (uuid) table
   await client.query(`create unique index if not exists users_uuid_key on public.users (uuid);`)
@@ -44,14 +111,14 @@ export default async () => {
     from _legacy.users_role l_ur
     where l_ur.invitation_uuid is not null;
   `)
+
   // ---- 6. Populate new users_role table from _legacy
-  await client.query(`
-    insert into public.users_role (
-      user_uuid, assessment_uuid, country_iso, role, cycle_uuid, permissions, props, created_at, invitation_uuid
-    )
-    select 
-      (select uuid from public.users where id = l_ur.user_id),
-      (select uuid from public.assessment where id = l_ur.assessment_id),
+  const _roles: Array<DBRole> = await client.map<DBRole>(
+    `
+    select
+      l_ur.user_id as user_id, -- used for activity log matching
+      u.uuid as user_uuid,
+      (select uuid from public.assessment where id = l_ur.assessment_id) as assessment_uuid,
       l_ur.country_iso,
       l_ur.role,
       l_ur.cycle_uuid,
@@ -59,8 +126,33 @@ export default async () => {
       l_ur.props,
       l_ur.accepted_at as created_at,
       l_ur.invitation_uuid
-    from _legacy.users_role l_ur;
-  `)
+    from _legacy.users_role l_ur
+    join public.users u on u.id = l_ur.user_id
+    -- To migrate a role:
+    -- either accepted at exists or invitation uuid doesn't exist
+    -- user must not be pending invitation
+    where (l_ur.accepted_at is not null or l_ur.invitation_uuid is null) and u.status != 'invitationPending'
+  `,
+    [],
+    (row) => Objects.camelize(row)
+  )
 
-  Logger.info('Migration completed successfully')
+  const roles = await Promise.all(_roles.map((role) => _handleRole(role)))
+  const columns = [
+    { name: 'user_uuid', prop: 'userUuid' },
+    { name: 'assessment_uuid', prop: 'assessmentUuid' },
+    { name: 'country_iso', prop: 'countryIso' },
+    { name: 'role', prop: 'role' },
+    { name: 'cycle_uuid', prop: 'cycleUuid' },
+    { name: 'permissions', prop: 'permissions' },
+    { name: 'props', prop: 'props' },
+    { name: 'created_at', prop: 'createdAt' },
+    { name: 'invitation_uuid', prop: 'invitationUuid' },
+  ]
+
+  const options = { table: { table: 'users_role', schema: 'public' } }
+  const cs = new pgp.helpers.ColumnSet(columns, options)
+
+  const query = pgp.helpers.insert(roles, cs)
+  await client.none(query)
 }
