a593e5620370754e84455ab6d0773c16f3ae6359
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-10-22 15:45:44 -0500

    4042 - Count unique values in MultiValueSummary

--- src/client/components/Inputs/Select/Indicators/MultiValueSummary/MultiValueSummary.tsx
+++ src/client/components/Inputs/Select/Indicators/MultiValueSummary/MultiValueSummary.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { MultiValueProps as OriginalMultiValueProps } from 'react-select'
 
-import { SelectProps } from 'client/components/Inputs/Select/types'
+import { Option, SelectProps } from 'client/components/Inputs/Select/types'
 
 type MultiValueProps = OriginalMultiValueProps & Pick<SelectProps, 'multiLabelSummaryKey'>
 
 export const MultiValueSummary: React.FC<MultiValueProps> = (props) => {
   const { getValue, index, multiLabelSummaryKey } = props
-  const count = getValue().length
   const { t } = useTranslation()
 
   const displayCountLabel = index === 0
 
   if (displayCountLabel) {
+    const count = Array.from(new Set(getValue().map((v) => (v as Option).value))).length
     return <span className="select__multiValue-label">{t(multiLabelSummaryKey, { count })}</span>
   }
 
