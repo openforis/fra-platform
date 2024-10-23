13c695183718ff0badae4011a06fc4600c07e3a7
sorja <ping@mirosorja.com>
2024-10-23 15:03:52 +0300

    4032 - userInvitation: isInvitationExpired -> isExpired

--- src/meta/user/userInvitations.ts
+++ src/meta/user/userInvitations.ts
@@ -1,8 +1,8 @@
 import { UserInvitation } from 'meta/user/userInvitation'
 
-export const isInvitationExpired = (invitation: UserInvitation, expiryPeriod?: number) =>
+export const isExpired = (invitation: UserInvitation, expiryPeriod?: number) =>
   new Date().getTime() - Date.parse(invitation.invitedAt) > (expiryPeriod || 7) * 86400000
 
 export const UserInvitations = {
-  isInvitationExpired,
+  isExpired,
 }
