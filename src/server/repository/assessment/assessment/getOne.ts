0092e405674c1e2c89a190b3c08e5fe75da022d0
sorja <ping@mirosorja.com>
2024-10-24 13:47:54 +0300

    4032 - AssessmentController: get one with UUID

--- src/server/repository/assessment/assessment/getOne.ts
+++ src/server/repository/assessment/assessment/getOne.ts
@@ -5,44 +5,21 @@ import { AssessmentAdapter } from 'server/repository/adapter'
 
 import { selectFields } from './selectFields'
 
-type Props = {
-  assessmentName?: string
-  id?: number
-  uuid?: string
-  metaCache?: boolean
-}
-
-export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<Assessment> => {
-  const { assessmentName, id, uuid, metaCache } = props
-  if (!assessmentName && !id && !uuid) {
-    throw new Error('At least one of assessmentName, id, or uuid must be provided')
-  }
-
-  let whereClause: string
-  let queryValue: number | string
-
-  if (id) {
-    whereClause = 'a.id = $1'
-    queryValue = id
-  } else if (uuid) {
-    whereClause = 'a.uuid = $1'
-    queryValue = uuid
-  } else {
-    whereClause = "a.props->>'name' = $1"
-    queryValue = assessmentName
-  }
-
+export const getOne = async (
+  props: { assessmentName: string; metaCache?: boolean } | { id: number; metaCache?: boolean },
+  client: BaseProtocol = DB
+): Promise<Assessment> => {
   return client.one(
     `
-    select ${selectFields},
-           jsonb_agg(to_jsonb(ac.*)) as cycles
-           ${metaCache ? ', meta_cache' : ''}
-    from assessment a
-    left join assessment_cycle ac on a.id = ac.assessment_id
-    where ${whereClause}
-    group by ${selectFields}
+        select ${selectFields},
+               jsonb_agg(to_jsonb(ac.*)) as cycles
+            ${props.metaCache ? `, meta_cache` : ''}
+        from assessment a
+                 left join assessment_cycle ac on a.id = ac.assessment_id
+        where ${'id' in props ? `a.id = $1` : `a.props->>'name' = $1`}
+        group by ${selectFields};
     `,
-    [queryValue],
+    ['id' in props ? props.id : props.assessmentName],
     AssessmentAdapter
   )
 }
