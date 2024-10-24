5c87c15c3e5ea50d317db74ad130eca7a3fafbd2
sorja <ping@mirosorja.com>
2024-10-24 13:58:41 +0300

    4032 - UserController: invite

--- src/server/controller/user/invite.ts
+++ src/server/controller/user/invite.ts
@@ -1,85 +1,68 @@
 import { CountryIso } from 'meta/area'
 import { ActivityLogMessage, Assessment, Cycle } from 'meta/assessment'
 import { Lang } from 'meta/lang'
-import { CollaboratorPermissions, RoleName, User, UserRole } from 'meta/user'
+import { RoleName, User, UserInvitation } from 'meta/user'
 
 import { BaseProtocol, DB } from 'server/db'
 import { ActivityLogRepository } from 'server/repository/public/activityLog'
 import { UserRepository } from 'server/repository/public/user'
-import { UserRoleRepository } from 'server/repository/public/userRole'
+import { UserInvitationRepository } from 'server/repository/public/userInvitation'
 import { MailService } from 'server/service'
 
-export const invite = async (
-  props: {
-    assessment: Assessment
-    countryIso: CountryIso
-    cycle: Cycle
-    email: string
-    lang: Lang
-    name?: string
-    permissions?: CollaboratorPermissions
-    roleName: RoleName
-    surname?: string
-    user: User
-  },
-  client: BaseProtocol = DB
-): Promise<{ userRole: UserRole<RoleName>; user: User }> => {
-  const { assessment, cycle, countryIso, email, lang, name, permissions, roleName, surname, user } = props
+type UserProps = {
+  email: string
+  name?: string
+  surname?: string
+  lang: Lang
+}
+
+type Props = {
+  assessment: Assessment
+  countryIso: CountryIso
+  cycle: Cycle
+  user: User
+  roleName: RoleName
+} & UserProps
+
+type Returned = { userInvitation: UserInvitation; user: User }
+
+const _getOrCreateUserToInvite = async (props: UserProps, client: BaseProtocol): Promise<User> => {
+  const { email, email: emailGoogle, name, surname, lang } = props
+
+  // Get user with primary email
+  let userToInvite: User = await UserRepository.getOne({ email }, client)
+  // If user with primary email not found, check if user has active google login
+  if (!userToInvite) userToInvite = await UserRepository.getOne({ emailGoogle }, client)
+  // If neither of above, create new user
+  if (!userToInvite) {
+    const user = { email, props: { name, surname, lang } }
+    userToInvite = await UserRepository.create({ user }, client)
+  }
+  return userToInvite
+}
+
+export const invite = async (props: Props, client: BaseProtocol = DB): Promise<Returned> => {
+  const { assessment, cycle, countryIso, email, lang, name, roleName, surname, user } = props
 
   return client.tx(async (t) => {
-    // Get user with primary email
-    let userToInvite = await UserRepository.getOne({ email }, t)
-    // If user with primary email not found, check if user has active google login
-    if (!userToInvite) userToInvite = await UserRepository.getOne({ emailGoogle: email }, t)
-    // If neither of above, create new user
-    if (!userToInvite)
-      userToInvite = await UserRepository.create(
-        { user: { email, props: { name: name ?? '', surname: surname ?? '', lang } } },
-        client
-      )
+    const userToInvite = await _getOrCreateUserToInvite({ email, name, surname, lang }, t)
 
-    const userRole = await UserRoleRepository.create(
-      {
-        assessment,
-        country: countryIso,
-        cycle,
-        invitedByUserUuid: user.uuid,
-        permissions,
-        props: { address: { countryIso }, primaryEmail: email },
-        role: roleName,
-        user: userToInvite,
-      },
-      t
-    )
+    const userInvitationProps = { assessment, countryIso, cycle, invitedBy: user, role: roleName, user: userToInvite }
+    const userInvitation: UserInvitation = await UserInvitationRepository.create(userInvitationProps, t)
 
-    const { userId, role } = userRole
+    const { userUuid, role } = userInvitation
 
-    await ActivityLogRepository.insertActivityLog(
-      {
-        activityLog: {
-          target: { userId, user: userToInvite.props.name, role },
-          section: 'users',
-          message: ActivityLogMessage.invitationAdd,
-          countryIso,
-          user,
-        },
-        assessment,
-        cycle,
-      },
-      t
-    )
+    const target = { userUuid, user: userToInvite.props.name, role }
+    const message = ActivityLogMessage.invitationAdd
+    const section = 'users'
+    const activityLog = { target, section, message, countryIso, user }
+    await ActivityLogRepository.insertActivityLog({ activityLog, assessment, cycle }, t)
 
-    await MailService.userInvite({
-      countryIso,
-      assessmentName: assessment.props.name,
-      cycleName: cycle.name,
-      role: userRole,
-      userToInvite,
-    })
+    const assessmentName = assessment.props.name
+    const cycleName = cycle.name
+    const mailProps = { countryIso, assessmentName, cycleName, userInvitation, userToInvite }
+    await MailService.userInvite(mailProps)
 
-    return {
-      userRole,
-      user: userToInvite,
-    }
+    return { userInvitation, user: userToInvite }
   })
 }
