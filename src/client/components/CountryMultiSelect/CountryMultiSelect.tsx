c068313e655d1b3565421716437a46a859a20e02
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-10-23 19:58:11 -0500

    4042 - Support order one regions only

--- src/client/components/CountryMultiSelect/CountryMultiSelect.tsx
+++ src/client/components/CountryMultiSelect/CountryMultiSelect.tsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Country } from 'meta/area'
-
 import Select, { SelectProps } from 'client/components/Inputs/Select'
 
 import { useCountriesByRegionOptions } from './hooks/useCountriesByRegionOptions'
 
-type Props = Omit<SelectProps, 'options'> & {
-  countries: Array<Country>
-  excludedRegions: Array<string>
-}
+type Props = Omit<SelectProps, 'options'>
 
 const CountryMultiSelect: React.FC<Props> = (props: Props) => {
-  const { countries, excludedRegions, ...selectProps } = props
-  const { multiLabelSummaryKey, placeholder } = selectProps
+  const { multiLabelSummaryKey, placeholder } = props
 
   const { t } = useTranslation()
 
-  const optionGroups = useCountriesByRegionOptions({ countries, excludedRegions })
+  const optionGroups = useCountriesByRegionOptions()
 
   return (
     <Select
       // eslint-disable-next-line react/jsx-props-no-spreading
-      {...selectProps}
+      {...props}
       multiLabelSummaryKey={multiLabelSummaryKey ?? 'admin.country'}
       options={optionGroups}
       placeholder={placeholder ?? t('common.countries')}
--- src/client/components/CountryMultiSelect/hooks/useCountriesByRegionOptions.ts
+++ src/client/components/CountryMultiSelect/hooks/useCountriesByRegionOptions.ts
@@ -1,44 +1,57 @@
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Country } from 'meta/area'
+import { CountryIso } from 'meta/area'
 
+import { useCountries, useRegionGroups } from 'client/store/area'
+import { useIsPanEuropeanRoute } from 'client/hooks'
 import { Option, OptionsGroup } from 'client/components/Inputs/Select'
 
-type Props = {
-  countries: Array<Country>
-  excludedRegions: Array<string>
-}
+type Returned = Array<Option> | Array<OptionsGroup>
 
-type Returned = Array<OptionsGroup>
+export const useCountriesByRegionOptions = (): Returned => {
+  const countries = useCountries()
+  const isPanEuropean = useIsPanEuropeanRoute()
 
-export const useCountriesByRegionOptions = (props: Props): Returned => {
-  const { countries, excludedRegions } = props
   const { t } = useTranslation()
 
+  const regionGroups = useRegionGroups()
+
   return useMemo<Returned>(() => {
+    const getCountryOption = (countryIso: CountryIso): Option => {
+      return {
+        label: t(`area.${countryIso}.listName`),
+        value: countryIso,
+      }
+    }
+
+    if (isPanEuropean) {
+      return countries.map(({ countryIso }) => getCountryOption(countryIso))
+    }
+
+    const orderOneRegionGroup = Object.values(regionGroups ?? {}).find((group) => group.order === 1)
+    if (!orderOneRegionGroup) return []
+
     const regionCountriesMap = new Map<string, Array<Option>>()
+    orderOneRegionGroup.regions.forEach(({ regionCode }) => {
+      if (!regionCountriesMap.has(regionCode)) {
+        regionCountriesMap.set(regionCode, [])
+      }
+    })
 
     countries.forEach(({ countryIso, regionCodes }) => {
       regionCodes.forEach((regionCode) => {
-        if (excludedRegions.includes(regionCode)) return
-
-        if (!regionCountriesMap.has(regionCode)) {
-          regionCountriesMap.set(regionCode, [])
-        }
-        const countryOption = {
-          label: t(`area.${countryIso}.listName`),
-          value: countryIso,
-        }
+        if (!regionCountriesMap.has(regionCode)) return
+        const countryOption = getCountryOption(countryIso)
         regionCountriesMap.get(regionCode).push(countryOption)
       })
     })
 
     return Array.from(regionCountriesMap.entries())
-      .map(([regionCode, countryOptions]) => ({
+      .map(([regionCode, options]) => ({
         label: t(`area.${regionCode}.listName`),
-        options: countryOptions.sort((a, b) => a.label.localeCompare(b.label)),
+        options,
       }))
       .sort((a, b) => a.label.localeCompare(b.label))
-  }, [countries, excludedRegions, t])
+  }, [countries, isPanEuropean, regionGroups, t])
 }
