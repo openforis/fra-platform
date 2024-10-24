17659a720be93764e7c10d52e5fc59c130e4d137
sorja <ping@mirosorja.com>
2024-10-24 13:55:00 +0300

    4032 - UserRepository: uuid on roles

--- src/server/repository/public/user/getOne.ts
+++ src/server/repository/public/user/getOne.ts
@@ -67,7 +67,7 @@ export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<U
         select ur_sub.*
         from users_role ur_sub
         left join public.assessment_cycle c on ur_sub.cycle_uuid = c.uuid
-        left join public.assessment a on a.id = ur_sub.assessment_id
+        left join public.assessment a on a.uuid = ur_sub.assessment_uuid
         where (
           (c.name is null or a.props->>'name' is null or 
            ${allowedAtlantisCycles.join(' or ')}
@@ -85,7 +85,7 @@ export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<U
         from public.users u
         left join ${
           usersRoleSubquery.length > 0 ? usersRoleSubquery : 'users_role'
-        } ur on u.id = ur.user_id and (ur.accepted_at is not null or ur.invited_at is null or ur.role = 'ADMINISTRATOR') ${join}
+        } ur on u.uuid = ur.user_uuid and (ur.created_at is not null or ur.role = 'ADMINISTRATOR') ${join}
         where ${where.join(' ')}
         group by ${selectFields}
     `,
