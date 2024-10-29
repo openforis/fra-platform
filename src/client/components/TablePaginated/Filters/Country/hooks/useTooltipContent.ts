c962edeadde718e9eea933b0d5d4afe32dc35d41
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-10-29 13:07:02 -0500

    4042 - Add tooltip improvements

--- src/client/components/TablePaginated/Filters/Country/hooks/useTooltipContent.ts
+++ src/client/components/TablePaginated/Filters/Country/hooks/useTooltipContent.ts
@@ -4,6 +4,9 @@ import { useTranslation } from 'react-i18next'
 import { Objects } from 'utils/objects'
 
 import { useTablePaginatedFilterValue } from 'client/store/ui/tablePaginated/hooks'
+import { useIsPanEuropeanRoute } from 'client/hooks'
+import { useCountriesByRegionOptions } from 'client/components/CountryMultiSelect/hooks/useCountriesByRegionOptions'
+import { OptionsGroup } from 'client/components/Inputs/Select'
 
 type Props = {
   fieldName: string
@@ -22,24 +25,52 @@ export const useTooltipContent = (props: Props): Returned => {
   const [canDisplayTooltip, setCanDisplayTooltip] = useState(true)
   const { t } = useTranslation()
 
+  const countryOptionGroups = useCountriesByRegionOptions()
+  const isPanEuropean = useIsPanEuropeanRoute()
+
   const tooltipContent = useMemo<string | null>(() => {
     if (Objects.isEmpty(filterValue)) return null
     if (!canDisplayTooltip) return null
-    const MAX_VISIBLE_COUNTRY_LABELS = 50
+    const MAX_VISIBLE_LABELS = 50
 
-    const selectedCountryLabels = filterValue
-      .map((countryIso) => {
-        return t(`area.${countryIso}.listName`)
+    let tooltipLabels: Array<string> = []
+
+    if (isPanEuropean) {
+      tooltipLabels = filterValue
+        .map((countryIso) => t(`area.${countryIso}.listName`))
+        .sort((a, b) => a.localeCompare(b))
+    } else {
+      const fullySelectedRegions: Array<string> = []
+      const partiallySelectedCountries: Array<string> = []
+
+      countryOptionGroups.forEach((group) => {
+        if (!Array.isArray((group as OptionsGroup).options)) return
+        const regionCountries = (group as OptionsGroup).options.map((option) => option.value)
+        const areAllCountriesSelected = regionCountries.every((country) => filterValue.includes(country))
+
+        if (areAllCountriesSelected) {
+          fullySelectedRegions.push(group.label)
+        } else {
+          regionCountries.forEach((country) => {
+            if (filterValue.includes(country)) {
+              partiallySelectedCountries.push(t(`area.${country}.listName`))
+            }
+          })
+        }
       })
-      .sort((a, b) => a.localeCompare(b))
-      .slice(0, MAX_VISIBLE_COUNTRY_LABELS)
-
-    if (filterValue.length > MAX_VISIBLE_COUNTRY_LABELS) {
-      selectedCountryLabels.push('...')
+      tooltipLabels = [...fullySelectedRegions, ...partiallySelectedCountries.sort((a, b) => a.localeCompare(b))]
     }
 
-    return selectedCountryLabels.join(', ')
-  }, [canDisplayTooltip, filterValue, t])
+    const allLabelsCount = tooltipLabels.length
+    const overflowCount = allLabelsCount - MAX_VISIBLE_LABELS
+
+    tooltipLabels = tooltipLabels.slice(0, MAX_VISIBLE_LABELS)
+    if (overflowCount > 0) {
+      tooltipLabels.push(t('common.plusCountMore', { count: overflowCount }))
+    }
+
+    return tooltipLabels.join(', ')
+  }, [canDisplayTooltip, countryOptionGroups, filterValue, isPanEuropean, t])
 
   const hideTooltip = useCallback(() => setCanDisplayTooltip(false), [])
   const showTooltip = useCallback(() => setCanDisplayTooltip(true), [])
