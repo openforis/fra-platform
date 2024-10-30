dfd09d022815904d93a86d6036973cc1a7959925
sorja <ping@mirosorja.com>
2024-10-30 15:26:03 +0200

    4032 - Add UserInvitations with Invite button

---
+++ src/client/pages/CountryHome/Collaborators/UserList/UserInvitations/UserInvitations.tsx
@@ -0,0 +1,14 @@
+import './UserInvitations.scss'
+import React from 'react'
+
+import Invite from './Invite'
+
+const UserInvitations: React.FC = () => {
+  return (
+    <div className="user-invitations">
+      <Invite />
+    </div>
+  )
+}
+
+export default UserInvitations
