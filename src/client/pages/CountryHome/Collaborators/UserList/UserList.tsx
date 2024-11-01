df4afe27ac97969f709de6ecbc78304af4d2a503
sorja <ping@mirosorja.com>
2024-11-01 09:20:20 +0200

    4032 - UserList: Add export and styling

--- src/client/pages/CountryHome/Collaborators/UserList/UserList.tsx
+++ src/client/pages/CountryHome/Collaborators/UserList/UserList.tsx
@@ -1,9 +1,19 @@
+import './UserList.scss'
 import React from 'react'
 
+import Export from './Export'
 import UserInvitations from './UserInvitations'
+import Users from './Users'
 
 const UserList: React.FC = () => {
-  return <UserInvitations />
+  return (
+    <div className="user-list">
+      <Export />
+
+      <Users />
+      <UserInvitations />
+    </div>
+  )
 }
 
 export default UserList
