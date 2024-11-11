4b91711209be2217ec2e9f66c695d78c5aa37084
Miró Sorja <ping@mirosorja.com>
2024-11-11 10:11:18 +0100

    4032 - CountryUserSummary view query: remove user_data CTE, rename user_uuid to uuid, include fullname

--- src/server/repository/assessment/assessment/getCreateSchemaDDL.ts
+++ src/server/repository/assessment/assessment/getCreateSchemaDDL.ts
@@ -350,42 +350,38 @@ export const getCreateOrReplaceViewCountryUserSummary = (props: { assessment: As
   const schemaName = Schemas.getNameCycle(assessment, cycle)
   return `
         create or replace view ${schemaName}.country_user_summary as
-        with user_data as (
-            select
-                u.uuid as user_uuid,
-                u.email,
-                u.props->>'name' as name,
-                u.props->>'surname' as surname,
-                u.status
-            from users u
-        )
-        select distinct on (ud.user_uuid, coalesce(ur.country_iso, ui.country_iso))
-            ud.user_uuid,
+        select distinct on (u.uuid, coalesce(ur.country_iso, ui.country_iso))
+            u.uuid,
             coalesce(ur.country_iso, ui.country_iso)  as country_iso,
-            ud.name,
-            ud.surname,
-            ud.email,
-            ud.status,
+            u.email,
+            u.props->>'name' as name,
+            u.props->>'surname' as surname,
+            concat(
+                    coalesce(u.props->>'name', ''),
+                    case when u.props->>'name' is not null and u.props->>'surname' is not null then ' ' else '' end,
+                    coalesce(u.props->>'surname', '')
+            ) as fullname,
+            u.status,
             coalesce(to_jsonb(ur.*), '{}'::jsonb)     as role,
             case
                 when
-                  to_jsonb(ur.*) is null
-                then
-                  coalesce(to_jsonb(ui.*), '{}'::jsonb)
+                    to_jsonb(ur.*) is null
+                    then
+                    coalesce(to_jsonb(ui.*), '{}'::jsonb)
                 else
-                '{}'::jsonb
-            end                                     as invitation
-        from user_data ud
-                 left join users_role ur on (ud.user_uuid = ur.user_uuid and ur.cycle_uuid = '${cycle.uuid}' and ur.assessment_uuid = '${assessment.uuid}')
-                 left join users_invitation ui on (ud.user_uuid = ui.user_uuid and ui.cycle_uuid = '${cycle.uuid}' and ui.assessment_uuid = '${assessment.uuid}')
+                    '{}'::jsonb
+                end                                     as invitation
+        from users u
+                 left join users_role ur on (u.uuid = ur.user_uuid and ur.cycle_uuid = '${cycle.uuid}' and ur.assessment_uuid = '${assessment.uuid}')
+                 left join users_invitation ui on (u.uuid = ui.user_uuid and ui.cycle_uuid = '${cycle.uuid}' and ui.assessment_uuid = '${assessment.uuid}')
         where (ur.uuid is not null or ui.uuid is not null)
         order by
-            ud.user_uuid,
+            u.uuid,
             coalesce(ur.country_iso, ui.country_iso),
             country_iso asc,
-            lower(ud.surname) asc nulls last,
-            lower(ud.name) asc nulls last,
-            ud.email asc,
+            lower(u.props->>'surname') asc nulls last,
+            lower(u.props->>'name') asc nulls last,
+            u.email asc,
             ur.uuid nulls last;
             
         comment on view ${schemaName}.country_user_summary is 'Shows users with their role or invitation by country/assessment/cycle';
