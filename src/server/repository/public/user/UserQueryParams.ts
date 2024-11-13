a5773c728271dcebf591ba3cf68988081b08f625
Miró Sorja <ping@mirosorja.com>
2024-11-13 12:10:45 +0100

    4032 - repository/user: update count

--- src/server/repository/public/user/UserQueryParams.ts
+++ src/server/repository/public/user/UserQueryParams.ts
@@ -2,6 +2,7 @@ import { RoleName, UserStatus } from 'meta/user'
 
 export interface UserQueryParams {
   fullName?: string
+  countries?: Array<string>
   countryIso?: string
   roles?: Array<RoleName>
   statuses?: Array<UserStatus>
--- src/server/repository/public/user/count.ts
+++ src/server/repository/public/user/count.ts
@@ -4,9 +4,10 @@ import { CountryIso } from 'meta/area'
 import { Assessment, Cycle } from 'meta/assessment'
 import { RoleName, UserStatus } from 'meta/user'
 
-import { BaseProtocol, DB } from 'server/db'
+import { BaseProtocol, DB, Schemas } from 'server/db'
 
 import { buildGetManyQuery } from './getMany'
+import { UserQueryParams } from './UserQueryParams'
 
 type Props = {
   administrators?: boolean
@@ -21,49 +22,54 @@ type Props = {
 type Returned = {
   total: number
 } & Record<RoleName, number>
+
 export const count = async (props: Props, client: BaseProtocol = DB): Promise<Returned> => {
-  const { assessment, countries, cycle, fullName, roles } = props
+  const { assessment, cycle, fullName, countries, roles, administrators } = props
+  const schemaName = Schemas.getNameCycle(assessment, cycle)
+
+  const queryParams: UserQueryParams = {}
+
+  const allRoles = administrators
+    ? Object.values(RoleName)
+    : Object.values(RoleName).filter((role) => role !== RoleName.ADMINISTRATOR)
+  queryParams.roles = !Objects.isEmpty(roles) ? roles : allRoles
+
+  if (fullName) queryParams.fullName = fullName.trim().toLowerCase()
 
   const conditions: Array<string> = []
-  if (Objects.isEmpty(countries)) conditions.push(`(ur.country_iso is null or ur.country_iso not like 'X%')`)
-  else conditions.push(`ur.country_iso in (${countries.map((countryIso) => `'${countryIso}'`).join(',')})`)
-
-  if (!Objects.isEmpty(roles)) conditions.push(`ur.role in (${roles.map((roleName) => `'${roleName}'`).join(',')})`)
-
-  if (!Objects.isEmpty(fullName))
-    conditions.push(`concat(u.props->'name', ' ', u.props->'surname') ilike '%${fullName}%'`)
-
-  const getQuery = (groupByRole?: boolean): string => {
-    return `select count(distinct (u.id)) as totals
-                ${groupByRole ? `, ur.role` : ''}
-            from public.users u
-                     join public.users_role ur on u.uuid = ur.user_uuid
-            where (ur.assessment_uuid is null or (ur.assessment_uuid = $1 and ur.cycle_uuid = $2))
-              -- and ((ur.accepted_at is not null and ur.invited_at is not null) or ur.invited_at is null)
-                and ${conditions.join(` 
-              and 
-              `)} ${groupByRole ? `group by ur.role` : ''}`
+  if (Objects.isEmpty(countries)) {
+    conditions.push(`(country_iso is null or country_iso not like 'X%')`)
+  } else {
+    queryParams.countries = countries
+    conditions.push(`country_iso in ($(countries:csv))`)
   }
 
-  const queryRoles = `with counts as (${getQuery(true)})
-  select jsonb_object_agg(counts.role, counts.totals) as result
-  from counts`
+  if (!Objects.isEmpty(fullName)) conditions.push(`full_name ilike '%' || $(fullName) || '%'`)
+
+  conditions.push(`(
+    (role is not null and role ->> 'role' in ($(roles:csv)))
+    or 
+    (invitation is not null and invitation ->> 'role' in ($(roles:csv)))
+  )`)
+
+  const queryRoles = `
+    with counts as (
+      select count(distinct (id)) as totals, coalesce(role ->> 'role', invitation ->> 'role') as role
+      from ${schemaName}.country_user_summary
+      where ${conditions.join(' and ')}
+      group by role ->> 'role', invitation ->> 'role'
+    )
+    select jsonb_object_agg(counts.role, counts.totals) as result
+    from counts`
 
   const { query: subQueryTotals, queryParams: queryTotalsParams } = buildGetManyQuery(props)
 
   const queryTotals = `
-  select count(*) as total
-  from (
-      ${subQueryTotals}
-  ) as users;
-  `
+    select count(*) as total
+    from (${subQueryTotals}) as users`
 
   const total = await client.one<number>(queryTotals, queryTotalsParams, ({ total }) => total)
-  const roleTotals = await client.one<Record<RoleName, number>>(
-    queryRoles,
-    [assessment.id, cycle.uuid],
-    ({ result }) => result
-  )
+  const roleTotals = await client.one<Record<RoleName, number>>(queryRoles, queryParams, ({ result }) => result)
 
   return { total, ...roleTotals }
 }
