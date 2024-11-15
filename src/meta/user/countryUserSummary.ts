e59a64fcf6d0eea081b1595dc83f1d8960d1954b
Miró Sorja <ping@mirosorja.com>
2024-11-15 09:42:25 +0100

    4032 - Include Lang column in country_user_summary

--- src/meta/user/countryUserSummary.ts
+++ src/meta/user/countryUserSummary.ts
@@ -1,7 +1,9 @@
+import { Lang } from 'meta/lang'
 import { User } from 'meta/user/user'
 import { UserInvitation } from 'meta/user/userInvitation'
 
 export type CountryUserSummary = User & {
   readonly fullName: string
+  readonly lang: Lang
   readonly invitations: Array<UserInvitation>
 }
