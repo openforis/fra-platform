7011c0239eb09c9846f6903a078b42fb3c17e6bc
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-11-15 15:57:38 -0500

    4001 - Add FRA 2025/2020 layout fixes

--- src/test/migrations/steps/20241114204645-step-fix-datatable-grid-layouts.ts
+++ src/test/migrations/steps/20241114204645-step-fix-datatable-grid-layouts.ts
@@ -3,7 +3,7 @@ import { AssessmentNames } from 'meta/assessment'
 import { AssessmentController } from 'server/controller/assessment'
 import { BaseProtocol, Schemas } from 'server/db'
 
-export default async (client: BaseProtocol) => {
+const _fixFRA2025GridLayouts = async (client: BaseProtocol) => {
   const { assessment, cycle } = await AssessmentController.getOneWithCycle(
     { assessmentName: AssessmentNames.fra, cycleName: '2025' },
     client
@@ -11,17 +11,30 @@ export default async (client: BaseProtocol) => {
 
   const schemaAssessment = Schemas.getName(assessment)
 
+  // Fix 1a Extent of forest and other wooded land -> forest area status
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+      where props->>'name' = 'extentOfForest_forestAreaStatusAndTrend'
+      `,
+    [JSON.stringify(['tier'])]
+  )
+
   // Fix 2a Growing stock status description -> tier status
   await client.query(
     `update ${schemaAssessment}.table
      set props = jsonb_set(
         props,
         '{columnNames,${cycle.uuid}}',
-        (props->'columnNames'->'${cycle.uuid}')::jsonb || $1::jsonb
-     )
+        $1::jsonb
+       )
       where props->>'name' = 'growingStock_growingStockStatus_Description'
     `,
-    [JSON.stringify(['tier'])]
+    [JSON.stringify(['status', 'tier'])]
   )
 
   // Fix 2c Biomass stock status description -> tier status
@@ -30,11 +43,11 @@ export default async (client: BaseProtocol) => {
      set props = jsonb_set(
         props,
         '{columnNames,${cycle.uuid}}',
-        (props->'columnNames'->'${cycle.uuid}')::jsonb || $1::jsonb
+        $1::jsonb
      )
       where props->>'name' = 'biomassStock_biomassStockStatus_Description'
     `,
-    [JSON.stringify(['tier'])]
+    [JSON.stringify(['status', 'tier'])]
   )
 
   // Fix 3b Forest area within protected areas
@@ -65,5 +78,114 @@ export default async (client: BaseProtocol) => {
   `
   )
 
+  // Fix 5c Degraded forest -> degradedForest2025
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+     )
+     where props->>'name' = 'degradedForest2025'
+      `,
+    [JSON.stringify(['hasNationalDefinitionOfDegradedForest', 'national_definition'])]
+  )
+  // Fix 5c Degraded forest -> degradedForestMonitoring2025
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+     )
+     where props->>'name' = 'degradedForestMonitoring2025'
+    `,
+    [JSON.stringify(['hasNationalDefinitionOfDegradedForest', 'national_definition'])]
+  )
+
+  // Fix 6b Area of permanent forest estate
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+     )
+     where props->>'name' = 'areaOfPermanentForestEstate'
+      `,
+    [JSON.stringify(['applicable', '1990', '2000', '2010', '2015', '2020', '2025'])]
+  )
+
+  // Fix 8 Sustainable Development Goal -> sustainableDevelopment15_2_1_3
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+     )
+     where props->>'name' = 'sustainableDevelopment15_2_1_3'
+    `,
+    [JSON.stringify(['2000', '2010', '2015', '2020', '2021', '2022', '2023', '2024', '2025'])]
+  )
+
+  // Fix 8 Sustainable Development Goal -> sustainableDevelopment15_2_1_5
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+     )
+     where props->>'name' = 'sustainableDevelopment15_2_1_5'
+    `,
+    [
+      JSON.stringify([
+        '2000',
+        '2005',
+        '2010',
+        '2015',
+        '2016',
+        '2017',
+        '2018',
+        '2019',
+        '2020',
+        '2021',
+        '2022',
+        '2023',
+        '2024',
+        '2025',
+      ]),
+    ]
+  )
   await AssessmentController.generateMetadataCache({ assessment }, client)
+}
+
+const _fixFRA2020GridLayouts = async (client: BaseProtocol) => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    { assessmentName: AssessmentNames.fra, cycleName: '2020' },
+    client
+  )
+
+  const schemaAssessment = Schemas.getName(assessment)
+
+  // Fix 5c Degraded forest definition
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'degradedForest'
+      `,
+    [JSON.stringify(['definition', 'answer'])]
+  )
+
+  await AssessmentController.generateMetadataCache({ assessment }, client)
+}
+
+export default async (client: BaseProtocol) => {
+  await _fixFRA2025GridLayouts(client)
+  await _fixFRA2020GridLayouts(client)
 }
