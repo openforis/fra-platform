1a02380d8cab83c853889d572e0d6ff5d42f22d8
Miró Sorja <ping@mirosorja.com>
2024-11-15 10:14:53 +0100

    4032 - Users/Actions: Update Edit [user]

--- src/client/pages/CountryHome/Collaborators/UserList/Users/Actions/Edit/Edit.tsx
+++ src/client/pages/CountryHome/Collaborators/UserList/Users/Actions/Edit/Edit.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { CountryIso } from 'meta/area'
 import { Routes } from 'meta/routes'
-import { User, Users } from 'meta/user'
+import { CountryUserSummary, Users } from 'meta/user'
+import { CountryUserSummaries } from 'meta/user/countryUserSummaries'
 
 import { useCycle } from 'client/store/assessment'
 import { useUser } from 'client/store/user'
@@ -10,15 +12,20 @@ import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import Button, { ButtonSize, ButtonType } from 'client/components/Buttons/Button'
 
 type Props = {
-  user: User
+  countryUserSummary: CountryUserSummary
 }
 
 const Edit = (props: Props) => {
-  const { user } = props
+  const { countryUserSummary } = props
+
+  const { assessmentName, cycleName, countryIso } = useCountryRouteParams<CountryIso>()
   const currentUser = useUser()
-  const { id } = user
-  const { assessmentName, cycleName, countryIso } = useCountryRouteParams()
+  const { id } = countryUserSummary
   const cycle = useCycle()
+
+  if (CountryUserSummaries.isInvitation(countryUserSummary, countryIso)) {
+    return null
+  }
 
   const currentUserIsReviewer = Users.isReviewer(currentUser, countryIso, cycle)
   // const text = t(currentUserIsReviewer ? 'common.view' : 'userManagement.edit')
