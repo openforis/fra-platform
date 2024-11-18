f793c0058c1fe8f4e0ce0944c3a8d13b0e267307
Miró Sorja <ping@mirosorja.com>
2024-11-18 15:20:57 +0100

    4032 - Fix Recipients in country status change

--- src/client/components/PageLayout/Toolbar/Status/StatusConfirm/UserList/UserList.tsx
+++ src/client/components/PageLayout/Toolbar/Status/StatusConfirm/UserList/UserList.tsx
@@ -2,32 +2,32 @@ import './UserList.scss'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { User, Users } from 'meta/user'
+import { CountryUserSummary, Users } from 'meta/user'
 
 import { useCycle } from 'client/store/assessment'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import { DataCell, DataGrid } from 'client/components/DataGrid'
 
 interface Props {
-  users: Array<User>
+  users: Array<CountryUserSummary>
 }
 
 interface CellProps {
-  user: User
+  countryUserSummary: CountryUserSummary
 }
 
-const NameCell: React.FC<CellProps> = ({ user }) => <span>{Users.getFullName(user)}</span>
+const NameCell: React.FC<CellProps> = ({ countryUserSummary }) => <span>{countryUserSummary.fullName}</span>
 
-const RoleCell: React.FC<CellProps> = ({ user }) => {
+const RoleCell: React.FC<CellProps> = ({ countryUserSummary }) => {
   const { t } = useTranslation()
   const { countryIso } = useCountryRouteParams()
   const cycle = useCycle()
-  const role = Users.getRole(user, countryIso, cycle)
+  const role = Users.getRole(countryUserSummary, countryIso, cycle)
   const key = Users.getI18nRoleLabelKey(role.role)
   return <span>{t(key)}</span>
 }
 
-const EmailCell: React.FC<CellProps> = ({ user }) => <span>{user.email}</span>
+const EmailCell: React.FC<CellProps> = ({ countryUserSummary }) => <span>{countryUserSummary.email}</span>
 
 const Components: Record<string, React.FC<CellProps>> = {
   name: NameCell,
@@ -51,12 +51,12 @@ const UserList: React.FC<Props> = (props: Props) => {
           </DataCell>
         )
       })}
-      {users.map((user, j) => {
+      {users.map((countryUserSummary, j) => {
         const lastRow = users.length - 1 === j
         return headers.map((header) => {
           return (
-            <DataCell key={`${user.email}-${header}`} lastRow={lastRow}>
-              {React.createElement(Components[header], { user })}
+            <DataCell key={`${countryUserSummary.email}-${header}`} lastRow={lastRow}>
+              {React.createElement(Components[header], { countryUserSummary })}
             </DataCell>
           )
         })
