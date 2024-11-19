d618ee2fb2926159d0e911df12b19e03e8734429
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-11-18 19:58:10 -0500

    4001 - Add panEuropean 2025/2020 layout fixes

--- src/test/migrations/steps/20241114204645-step-fix-datatable-grid-layouts.ts
+++ src/test/migrations/steps/20241114204645-step-fix-datatable-grid-layouts.ts
@@ -65,7 +65,7 @@ const _fixFRA2025GridLayouts = async (client: BaseProtocol) => {
   // Fix area header colSpan
   await client.query(
     `update ${schemaAssessment}.col c
-     set props = jsonb_set( props, '{style,${cycle.uuid}}', '{"colSpan": 6}', true)
+     set props = jsonb_set( props, '{style,${cycle.uuid},colSpan}', '6', true)
      where c.props ->> 'colType' = 'header'
         and c.props ->> 'index' = '1'
         and c.row_id in (
@@ -185,7 +185,202 @@ const _fixFRA2020GridLayouts = async (client: BaseProtocol) => {
   await AssessmentController.generateMetadataCache({ assessment }, client)
 }
 
+const _fixPanEuropean2020GridLayouts = async (client: BaseProtocol) => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    { assessmentName: AssessmentNames.panEuropean, cycleName: '2020' },
+    client
+  )
+
+  const schemaAssessment = Schemas.getName(assessment)
+
+  // Fix 2.5 Area with forest land degradation -> table_2_5 Area header colSpan
+  await client.query(
+    `update ${schemaAssessment}.col c
+     set props = jsonb_set( props, '{style,${cycle.uuid},colSpan}', '10', true)
+     where c.props ->> 'colType' = 'header'
+        and c.props ->> 'index' = '0'
+        and c.row_id in (
+          select r.id from ${schemaAssessment}.row r
+          left join ${schemaAssessment}."table" t on t.id = r.table_id
+          where r.props ->> 'type' = 'header'
+        and r.props ->> 'index' = 'header_2'
+        and t.props ->> 'name' = 'table_2_5'
+      )
+    `
+  )
+
+  await AssessmentController.generateMetadataCache({ assessment }, client)
+}
+
+const _fixPanEuropean2025GridLayouts = async (client: BaseProtocol) => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    { assessmentName: AssessmentNames.panEuropean, cycleName: '2025' },
+    client
+  )
+
+  const schemaAssessment = Schemas.getName(assessment)
+
+  // Fix 1.1 Forest area -> country_comments_1_1_1
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'country_comments_1_1_1'
+      `,
+    [JSON.stringify(['method', 'comment'])]
+  )
+
+  // Fix 1.2 Growing stock -> reasonability_check_1_2
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'reasonability_check_1_2'
+      `,
+    [JSON.stringify(['forest', 'FAWS', 'OWL', 'FOWL'])]
+  )
+
+  // Fix 1.4 Carbon stock -> table_1_4a header rowSpan
+  await client.query(
+    `update ${schemaAssessment}.col c
+     set props = jsonb_set( props, '{style,${cycle.uuid},rowSpan}', '2', true)
+     where c.props ->> 'colType' = 'header'
+        and c.props ->> 'index' = '0'
+        and c.row_id in (
+          select r.id from ${schemaAssessment}.row r
+          left join ${schemaAssessment}."table" t on t.id = r.table_id
+          where r.props ->> 'type' = 'header'
+        and r.props ->> 'index' = 'header_0'
+        and t.props ->> 'name' = 'table_1_4a'
+      )
+    `
+  )
+
+  // Fix 1.4 Carbon stock -> reasonability_check_1_4
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'reasonability_check_1_4'
+      `,
+    [JSON.stringify(['forest', 'OWL', 'FOWL'])]
+  )
+
+  // Fix 2.4 Forest area with damage -> table_2_4 header rowSpan
+  await client.query(
+    `update ${schemaAssessment}.col c
+     set props = jsonb_set( props, '{style,${cycle.uuid},rowSpan}', '3', true)
+     where c.props ->> 'colType' = 'header'
+        and c.props ->> 'index' = '0'
+        and c.row_id in (
+          select r.id from ${schemaAssessment}.row r
+          left join ${schemaAssessment}."table" t on t.id = r.table_id
+          where r.props ->> 'type' = 'header'
+        and r.props ->> 'index' = 'header_0'
+        and t.props ->> 'name' = 'table_2_4'
+      )
+      `
+  )
+
+  // Fix 2.4 Forest area with damage -> reasonability_check_2_4
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'reasonability_check_2_4'
+      `,
+    [JSON.stringify(['forest', 'OWL', 'FOWL'])]
+  )
+
+  // Fix 2.5 Area with forest land degradation -> country_comments_2_5_2
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'country_comments_2_5_2'
+    `,
+    [JSON.stringify(['total_area_of_degraded_land', 'comment', 'comment_trends'])]
+  )
+
+  // Fix 3.1 Increment and fellings -> reasonability_check_3_1
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'reasonability_check_3_1'
+      `,
+    [JSON.stringify(['FAWS'])]
+  )
+
+  // Fix 4.5 Increment and fellings -> reasonability_check_4_5
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+        props,
+        '{columnNames,${cycle.uuid}}',
+        $1::jsonb
+       )
+     where props->>'name' = 'reasonability_check_4_5'
+      `,
+    [JSON.stringify(['forest', 'OWL', 'FOWL'])]
+  )
+
+  // Fix 4.5 Increment and fellings -> country_comments_4_5_1 header colSpan
+  await client.query(
+    `update ${schemaAssessment}.col c
+     set props = jsonb_set( props, '{style,${cycle.uuid},colSpan}', '2', true)
+     where c.props ->> 'colType' = 'header'
+        and c.props ->> 'index' = '0'
+        and c.row_id in (
+          select r.id from ${schemaAssessment}.row r
+          left join ${schemaAssessment}."table" t on t.id = r.table_id
+          where r.props ->> 'type' = 'header'
+        and r.props ->> 'index' = 'header_0'
+        and t.props ->> 'name' = 'country_comments_4_5_1'
+      )
+      `
+  )
+
+  // Fix 4.8 Threatened forest species -> table_4_8 header rowSpan
+  await client.query(
+    `update ${schemaAssessment}.col c
+     set props = jsonb_set( props, '{style,${cycle.uuid},rowSpan}', '2', true)
+     where c.props ->> 'colType' = 'header'
+        and c.props ->> 'index' = '0'
+        and c.row_id in (
+          select r.id from ${schemaAssessment}.row r
+          left join ${schemaAssessment}."table" t on t.id = r.table_id
+          where r.props ->> 'type' = 'header'
+        and r.props ->> 'index' = 'header_0'
+        and t.props ->> 'name' = 'table_4_8'
+      )
+      `
+  )
+
+  await AssessmentController.generateMetadataCache({ assessment }, client)
+}
+
 export default async (client: BaseProtocol) => {
   await _fixFRA2025GridLayouts(client)
   await _fixFRA2020GridLayouts(client)
+  await _fixPanEuropean2020GridLayouts(client)
+  await _fixPanEuropean2025GridLayouts(client)
 }
