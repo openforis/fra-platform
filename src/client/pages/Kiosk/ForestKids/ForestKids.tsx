f7c534438e89ea12b8d5fab5f1c0340d5b40c8e8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2024-11-25 11:09:58 -0500

    4133 - Add kiosk bare pages

---
+++ src/client/pages/Kiosk/ForestKids/ForestKids.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ForestKids: React.FC = () => {
+  return <div>Forest Kids</div>
+}
+
+export default ForestKids
---
+++ src/client/pages/Kiosk/FraProcess/FraProcess.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FraProcess: React.FC = () => {
+  return <div>Fra process</div>
+}
+
+export default FraProcess
