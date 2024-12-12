a8401d3d5cd30c0db07654fb6603a3d3ae4c2c4a
Mino Togna <mino.togna@gmail.com>
2024-12-12 17:57:18 +0100

    4032 - Ui Collaborators grouped

--- src/client/components/TablePaginated/Body/Body.tsx
+++ src/client/components/TablePaginated/Body/Body.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import classNames from 'classnames'
 import { Objects } from 'utils/objects'
 
+import { CountryIso } from 'meta/area'
+import { CountryUserSummary } from 'meta/user'
+import { CountryUserSummaries } from 'meta/user/countryUserSummaries'
+
 import { useTablePaginatedData } from 'client/store/ui/tablePaginated'
+import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import DataColumn from 'client/components/DataGridDeprecated/DataColumn'
 import RowsSkeleton from 'client/components/TablePaginated/Body/RowsSkeleton'
 import { Props as BaseProps } from 'client/components/TablePaginated/types'
@@ -12,10 +17,16 @@ const Body = <Datum extends object>(props: BaseProps<Datum>) => {
   const { columns, compareFn, limit, path, wrapCells, skeleton } = props
 
   const data = useTablePaginatedData<Datum>(path, compareFn)
-
+  const { countryIso } = useCountryRouteParams<CountryIso>()
   if (Objects.isNil(data)) {
     return <RowsSkeleton columns={columns} limit={limit} skeleton={skeleton} />
   }
+
+  // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const d2 = Object.groupBy(data ?? [], (d) => CountryUserSummaries.getRoleName(d as CountryUserSummary, countryIso))
+
+  // console.log({ d2: Object.entries(d2) })
 
   return (
     <>
