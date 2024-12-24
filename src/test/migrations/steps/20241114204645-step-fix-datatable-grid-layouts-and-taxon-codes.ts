4c2cffa499df41509f8eadb61d8e9612422c9b88
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2024-12-24 10:15:56 -0500

    4001 - Datatable use Datagrid - Table grid template columns - panEuropean (#4189)
    * 4001 - Add panEuropean 2020 gridTemplateColumns
    
    * 4001 - Add panEuropean 2025 gridTemplateColumns
    
    * 4001 - Pass custom classname to InputText disabled div

--- src/test/migrations/steps/20241114204645-step-fix-datatable-grid-layouts-and-taxon-codes.ts
+++ src/test/migrations/steps/20241114204645-step-fix-datatable-grid-layouts-and-taxon-codes.ts
@@ -306,6 +306,79 @@ const _fixPanEuropean2020GridLayouts = async (client: BaseProtocol) => {
 
   await _removeCycleColWidthProperties({ cycleUuid, schemaAssessment }, client)
 
+  const gridTemplateColumnsToAdd: Array<{ tableName: string; gridTemplateColumns: string }> = [
+    {
+      gridTemplateColumns: '2fr 1fr',
+      tableName: 'table_1_1a',
+    },
+    {
+      gridTemplateColumns: 'auto repeat(8, 1fr)',
+      tableName: 'table_1_2c_2020',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(9, 1fr)',
+      tableName: 'table_2_4',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 150px) repeat(5, 1fr)',
+      tableName: 'table_3_2',
+    },
+    {
+      gridTemplateColumns: ' minmax(auto, 200px) repeat(6, 1fr)',
+      tableName: 'table_3_3',
+    },
+    {
+      gridTemplateColumns: ' minmax(auto, 200px) repeat(5, 1fr)',
+      tableName: 'table_3_4',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(3, 1fr)',
+      tableName: 'table_4_2a',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(5, 1fr)',
+      tableName: 'table_4_2b_2020',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(7, 1fr)',
+      tableName: 'table_4_3b_2020',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 250px) repeat(4, 1fr)',
+      tableName: 'table_4_4b_2020',
+    },
+    {
+      gridTemplateColumns: 'auto repeat(8, 1fr)',
+      tableName: 'table_6_1',
+    },
+    {
+      gridTemplateColumns: 'repeat(3, 1fr)',
+      tableName: 'table_6_2',
+    },
+    {
+      gridTemplateColumns: 'auto repeat(5, 1fr)',
+      tableName: 'table_6_5b_2020',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(26, 1fr)',
+      tableName: 'table_6_7',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(26, 1fr)',
+      tableName: 'table_6_8',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 250px) repeat(10, 1fr)',
+      tableName: 'table_6_9',
+    },
+  ]
+
+  await Promise.all(
+    gridTemplateColumnsToAdd.map(({ gridTemplateColumns, tableName }) =>
+      _addGridTemplateColumns({ cycleUuid, gridTemplateColumns, schemaAssessment, tableName }, client)
+    )
+  )
+
   // Fix 2.5 Area with forest land degradation -> table_2_5 Area header colSpan
   await client.query(
     `update ${schemaAssessment}.col c
@@ -400,6 +473,99 @@ const _fixPanEuropean2025GridLayouts = async (client: BaseProtocol) => {
   await Promise.all(
     columnsToFix.map(({ columnNames, tableName }) =>
       _fixColumnNames({ columnNames, cycleUuid, schemaAssessment, tableName }, client)
+    )
+  )
+
+  const gridTemplateColumnsToAdd: Array<{ tableName: string; gridTemplateColumns: string }> = [
+    {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      tableName: 'table_1_1a',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto,250px) repeat(9, 1fr)',
+      tableName: 'table_1_2c',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 400px) repeat(5, 1fr)',
+      tableName: 'table_1_3a2',
+    },
+    {
+      gridTemplateColumns: 'auto repeat(10, 1fr)',
+      tableName: 'table_2_4',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(7, 1fr)',
+      tableName: 'table_2_5',
+    },
+    {
+      gridTemplateColumns: '2fr 1fr',
+      tableName: 'reasonability_check_3_1',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(6, 1fr)',
+      tableName: 'table_3_3',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(2, 1fr)',
+      tableName: 'country_comments_3_3_1',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(5, 1fr)',
+      tableName: 'table_3_4',
+    },
+    {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      tableName: 'country_comments_3_4_1',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(2, 1fr)',
+      tableName: 'country_comments_3_4_2',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(3, 1fr)',
+      tableName: 'table_4_2a',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(5, 1fr)',
+      tableName: 'table_4_2b',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(7, 1fr)',
+      tableName: 'table_4_3b',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(6, 1fr)',
+      tableName: 'table_4_4b',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 200px) repeat(6, 1fr)',
+      tableName: 'table_4_4c',
+    },
+    {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      tableName: 'country_comments_4_5_1',
+    },
+    {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      tableName: 'country_comments_6_1_1',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 250px) repeat(4, 1fr)',
+      tableName: 'table_6_6',
+    },
+    {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      tableName: 'country_comments_6_6_1',
+    },
+    {
+      gridTemplateColumns: 'minmax(auto, 300px) repeat(4, 1fr)',
+      tableName: 'table_6_10d',
+    },
+  ]
+
+  await Promise.all(
+    gridTemplateColumnsToAdd.map(({ gridTemplateColumns, tableName }) =>
+      _addGridTemplateColumns({ cycleUuid, gridTemplateColumns, schemaAssessment, tableName }, client)
     )
   )
 
