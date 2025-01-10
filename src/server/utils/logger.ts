7e465e13022db5ecf1e6ee81ff81fa2d9a7a05c0
sorja <ping@mirosorja.com>
2025-01-10 12:10:48 +0200

    4200 - Logger: Fix winston import

--- src/server/utils/logger.ts
+++ src/server/utils/logger.ts
@@ -1,14 +1,14 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
 
 import { ProcessEnv } from './processEnv'
 
-const Logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'HH:mm:ss' }),
-    winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+const Logger = createLogger({
+  format: format.combine(
+    format.timestamp({ format: 'HH:mm:ss' }),
+    format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   level: ProcessEnv.logLevel,
-  transports: [new winston.transports.Console()],
+  transports: [new transports.Console()],
 })
 
 export { Logger }
