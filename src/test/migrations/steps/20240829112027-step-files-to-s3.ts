97cd363d8830bdcadcd759f34445470b19bac94a
sorja <ping@mirosorja.com>
2025-01-15 11:25:06 +0200

    3855 - Separate file uploading

--- src/test/migrations/steps/20240829112027-step-files-to-s3.ts
+++ src/test/migrations/steps/20240829112027-step-files-to-s3.ts
@@ -1,31 +1,5 @@
-import * as path from 'path'
-import { Promises } from 'utils/promises'
-
 import { BaseProtocol, DB } from 'server/db'
-import { FileStorage, FileStorageUtils } from 'server/service/fileStorage'
 import { Logger } from 'server/utils/logger'
-import { ProcessEnv } from 'server/utils/processEnv'
-
-// TODO: Restrict these
-const allowedExtensions = [
-  'zip',
-  'xlsx',
-  'xlsm',
-  'xls',
-  'tsv',
-  'pptx',
-  'png',
-  'pdf',
-  'jpg',
-  'jpeg',
-  'jfif',
-  'gif',
-  'docx',
-  'doc',
-  'csv',
-  'crdownload', // eg. half downloaded file should be removed from both cycleSchema.repository and
-  'bmp',
-]
 
 const client: BaseProtocol = DB
 
@@ -51,34 +25,6 @@ export default async () => {
     } else {
       Logger.debug('_legacy.file table already exists, skipping creation.')
     }
-
-    const files = await client.query(`
-          select * from public.file
-    `)
-
-    await Promises.each(files, async (fileRecord: any) => {
-      const { id, uuid, name, file } = fileRecord
-      const fileExtension = path.extname(name).slice(1).toLowerCase()
-
-      if (allowedExtensions.includes(fileExtension)) {
-        const s3Key = `${uuid}`
-        const fileExists = await FileStorage.fileExists({ key: s3Key })
-
-        if (!fileExists) {
-          await FileStorage.uploadFile({
-            key: s3Key,
-            body: Buffer.from(file),
-            bucket: ProcessEnv.s3BucketName,
-            contentType: FileStorageUtils.getContentType(fileExtension),
-          })
-          // Logger.debug(`File ${name} (ID: ${id}) migrated successfully.`)
-        } else {
-          Logger.debug(`File ${name} (ID: ${id}) already exists in S3, skipping upload.`)
-        }
-      } else {
-        Logger.debug(`File ${name} (ID: ${id}) skipped due to unsupported file type.`)
-      }
-    })
 
     // Remove the 'file' column from the 'public.file' table
     await client.query(`
