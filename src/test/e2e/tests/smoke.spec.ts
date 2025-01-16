a515ee98126a7d52e30e4b15682c957e7fc6cb5e
sorja <ping@mirosorja.com>
2025-01-16 14:21:49 +0200

    4222 - Initial commit for E2E Docker Environment

---
+++ src/test/e2e/tests/smoke.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/test'
+
+// test test
+
+test('basic smoke test', async ({ page }) => {
+  await page.goto('/')
+
+  expect(await page.title()).toBeTruthy()
+})
