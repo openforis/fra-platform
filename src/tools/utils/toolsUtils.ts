b5f23405da7c88c9a8ac047461b3eae8df0b2a93
Mino Togna <mino.togna@gmail.com>
2025-01-17 15:20:59 +0100

    4227: PDF Report generation

---
+++ src/tools/utils/toolsUtils.ts
@@ -0,0 +1,24 @@
+import { DB } from 'server/db'
+import { Logger } from 'server/utils/logger'
+
+const close = async () => {
+  await DB.$pool.end()
+}
+
+const exec = (fn: () => Promise<unknown>): void => {
+  const start = new Date().getTime()
+  Logger.info(`========== ******** Starting ${start}`)
+
+  fn().then(async () => {
+    await close()
+
+    const end = new Date().getTime()
+    Logger.info(`========== ******** Executed ${end} elapsed ${(end - start) / 1000}s`)
+    process.exit(0)
+  })
+}
+
+export const ToolsUtils = {
+  close,
+  exec,
+}
