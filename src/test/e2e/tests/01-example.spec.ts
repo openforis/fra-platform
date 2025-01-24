874b2b6c323d265558a674738fcdf43e6a068808
sorja <ping@mirosorja.com>
2025-01-24 10:59:18 +0200

    4243 - Remove unnecessary comments

--- src/test/e2e/tests/01-example.spec.ts
+++ src/test/e2e/tests/01-example.spec.ts
@@ -1,19 +1,9 @@
 import { expect, test } from '@playwright/test'
 
-/**
- * Example of failing test
- */
-
-test.beforeAll(async () => {
-  console.log('beforeall')
-})
-
 test('homepage has title and links to intro page', async ({ page }) => {
   await page.goto('/')
 
-  // Add more logging and increase timeout if needed
   await expect(page).toHaveTitle(/FRA platform/, { timeout: 10000 })
-  // const headerTitle = await page.getByRole('heading', { level: 1, name: 'Global Forest Resources Assessment' })
   const headerLogo = await page.getByRole('img', { name: 'FAO' })
   await expect(headerLogo).toBeVisible()
   const headerTitle = await page.getByText('Global Forest Resources Assessment').first()
