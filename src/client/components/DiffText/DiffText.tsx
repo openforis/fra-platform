4c8c7a3c3439991d95265f2801540cda3237f3f1
Miró Sorja <ping@mirosorja.com>
2025-01-29 13:45:36 +0200

    4249 - Descriptions history: common component DiffText

---
+++ src/client/components/DiffText/DiffText.tsx
@@ -0,0 +1,39 @@
+import './DiffText.scss'
+import React from 'react'
+
+import classNames from 'classnames'
+import { Change } from 'diff'
+
+interface Props {
+  changes: Array<Change>
+  className?: string
+}
+
+const DiffText: React.FC<Props> = (props) => {
+  const { changes, className } = props
+
+  return (
+    <div className={classNames('diff-text', className)}>
+      {changes?.map((change, i) => {
+        const { added, removed, value } = change
+        const key = `${value}_${String(i)}`
+
+        return (
+          <React.Fragment key={key}>
+            {value.split('\n\r').map((text, j) => (
+              <React.Fragment key={`${key}_${String(j)}`}>
+                <span className={classNames({ added, removed })}>{text}</span>
+              </React.Fragment>
+            ))}
+          </React.Fragment>
+        )
+      })}
+    </div>
+  )
+}
+
+DiffText.defaultProps = {
+  className: '',
+}
+
+export default DiffText
