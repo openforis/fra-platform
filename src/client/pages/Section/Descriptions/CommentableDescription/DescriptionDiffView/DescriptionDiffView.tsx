2c2be4dc9f2235c0647130c4b23c4f97448a86a8
Miró Sorja <ping@mirosorja.com>
2025-01-29 13:48:29 +0200

    4249 - Descriptions history: DescriptionDiffView component

---
+++ src/client/pages/Section/Descriptions/CommentableDescription/DescriptionDiffView/DescriptionDiffView.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { CommentableDescriptionName } from 'meta/assessment'
+
+import { useCommentableDescriptionValue, useLastApprovedHistoryDescriptions } from 'client/store/data'
+import DiffText from 'client/components/DiffText'
+import { useSectionContext } from 'client/pages/Section/context'
+import useChanges from 'client/pages/Section/Descriptions/CommentableDescription/hooks/useChanges'
+
+type Props = { name: CommentableDescriptionName }
+
+const DescriptionDiffView: React.FC<Props> = (props) => {
+  const { name } = props
+  const { sectionName } = useSectionContext()
+
+  const descriptions = useLastApprovedHistoryDescriptions()
+  const descriptionA = descriptions?.[name]?.text || ''
+  const descriptionB = useCommentableDescriptionValue({ name, sectionName })?.text || ''
+  const changes = useChanges({ descriptionA, descriptionB })
+
+  return <DiffText changes={changes} />
+}
+
+export default DescriptionDiffView
