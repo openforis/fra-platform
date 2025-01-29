83519a4cf9f068cfa4ef9f988df498965a4b2e89
Mino Togna <minotogna@users.noreply.github.com>
2025-01-29 07:42:05 +0100

    4279: Cycle Source reference (#4286)
    * 4279: Cycle Source reference
    
    * 4279: reorder prop
    
    * 4279: move foreign keys

--- src/server/controller/assessment/createCycle.ts
+++ src/server/controller/assessment/createCycle.ts
@@ -5,15 +5,20 @@ import { BaseProtocol, DB } from 'server/db'
 import { CycleRepository } from 'server/repository/assessmentCycle/cycle'
 import { ActivityLogRepository } from 'server/repository/public/activityLog'
 
+type Props = {
+  assessment: Assessment
+  cycleSource?: Cycle
+  name: string
+  user: User
+}
+
 type Returned = { assessment: Assessment; cycle: Cycle }
 
-export const createCycle = async (
-  props: { user: User; assessment: Assessment; name: string },
-  client: BaseProtocol = DB
-): Promise<Returned> => {
-  const { user, assessment, name } = props
+export const createCycle = async (props: Props, client: BaseProtocol = DB): Promise<Returned> => {
+  const { assessment, cycleSource, name, user } = props
+
   return client.tx(async (t) => {
-    const { assessment: updatedAssessment, cycle } = await CycleRepository.create({ assessment, name }, t)
+    const { assessment: updatedAssessment, cycle } = await CycleRepository.create({ assessment, cycleSource, name }, t)
 
     const message = ActivityLogMessage.assessmentCycleCreate
     const activityLog = { target: updatedAssessment, section: 'assessment', message, user }
