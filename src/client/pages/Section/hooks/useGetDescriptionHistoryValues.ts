4926ecce00cff4fed06e708afba5443ee724e7a1
Miró Sorja <ping@mirosorja.com>
2025-01-30 11:48:00 +0200

    4249 - useGetDescriptionValues: separate fetching history values

--- src/client/pages/Section/hooks/useGetDescriptionHistoryValues.ts
+++ src/client/pages/Section/hooks/useGetDescriptionHistoryValues.ts
@@ -3,23 +3,24 @@ import { useEffect } from 'react'
 import { CountryIso } from 'meta/area'
 
 import { useAppDispatch } from 'client/store'
-import { DataActions } from 'client/store/data'
+import { DataActions, useHistoryLastApprovedIsActive } from 'client/store/data'
 import { useIsPrintRoute } from 'client/hooks/useIsRoute'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 
 type Props = { sectionName: string }
 
-export const useGetDescriptionValues = (props: Props): void => {
+export const useGetDescriptionHistoryValues = (props: Props): void => {
   const { sectionName } = props
 
   const dispatch = useAppDispatch()
   const { print } = useIsPrintRoute()
   const { assessmentName, cycleName, countryIso: _countryIso } = useCountryRouteParams()
   const countryIso = _countryIso as CountryIso
+  const historyLastApprovedIsActive = useHistoryLastApprovedIsActive()
 
   useEffect(() => {
-    if (!print) {
-      dispatch(DataActions.getDescription({ countryIso, assessmentName, cycleName, sectionName }))
+    if (!print && historyLastApprovedIsActive) {
+      dispatch(DataActions.getDescriptionsHistory({ countryIso, assessmentName, cycleName, sectionName }))
     }
-  }, [assessmentName, countryIso, cycleName, dispatch, print, sectionName])
+  }, [assessmentName, countryIso, cycleName, dispatch, historyLastApprovedIsActive, print, sectionName])
 }
