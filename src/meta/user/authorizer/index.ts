1ff43ff365590f7b6be9313fd96f567db29be8ed
Mino Togna <mino.togna@gmail.com>
2025-01-31 13:24:15 +0100

    4313:Data History auth:if (user is admin or reviewer) and status !== notStarted

--- src/meta/user/authorizer/index.ts
+++ src/meta/user/authorizer/index.ts
@@ -217,25 +217,15 @@ const canViewHistory = (props: {
 
 /**
  * canViewHistoryLastApproved:
- *
- * Reviewer: if country status = review
- * Administrator: if status >=review
- *
- * @param props
- * @param props.country
- * @param props.cycle
- * @param props.user
- * @returns boolean
+ * returns true if (user is admin or reviewer) and status !== notStarted
  */
 const canViewHistoryLastApproved = (props: { country: Country; cycle: Cycle; user: User }): boolean => {
   const { country, cycle, user } = props
   const { status } = country?.props ?? {}
 
-  if (Users.isReviewer(user, country?.countryIso, cycle) && status === AssessmentStatus.review) return true
-
   return (
-    Users.isAdministrator(user) &&
-    [AssessmentStatus.review, AssessmentStatus.approval, AssessmentStatus.accepted].includes(status)
+    (Users.isAdministrator(user) || Users.isReviewer(user, country?.countryIso, cycle)) &&
+    status !== AssessmentStatus.notStarted
   )
 }
 
