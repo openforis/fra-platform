9647c83e5139e5c0d8cd8fc7e1a83d9952b46a50
Miró Sorja <ping@mirosorja.com>
2025-01-31 16:23:10 +0200

    4251 - Merge data

--- src/client/store/data/extraReducers/getTableDataHistory.ts
+++ src/client/store/data/extraReducers/getTableDataHistory.ts
@@ -1,11 +1,18 @@
-import { ActionReducerMapBuilder } from '@reduxjs/toolkit'
+import { ActionReducerMapBuilder, createSelector } from '@reduxjs/toolkit'
 import { Objects } from 'utils/objects'
 
 import { getTableDataHistory } from 'client/store/data/actions/getTableDataHistory'
 import { DataState } from 'client/store/data/state'
 
+const _getTableData = createSelector(
+  (state: DataState) => state.history,
+  (history) => history.lastApproved.tableData
+)
+
 export const getTableDataHistoryReducer = (builder: ActionReducerMapBuilder<DataState>) =>
-  builder.addCase(getTableDataHistory.fulfilled, (state, { payload: value }) => {
+  builder.addCase(getTableDataHistory.fulfilled, (state, { payload }) => {
     const path = ['history', 'lastApproved', 'tableData']
+    const oldValue = _getTableData(state)
+    const value = Objects.merge(oldValue, payload)
     Objects.setInPath({ obj: state, path, value })
   })
