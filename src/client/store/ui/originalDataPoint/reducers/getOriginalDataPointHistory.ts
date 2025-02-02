b792d281328812602b741c6e8e81663a236bfd31
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-02-01 23:55:28 -0500

    4253 - ODP history: update new reducer imports

--- src/client/store/ui/originalDataPoint/reducers/getOriginalDataPointHistory.ts
+++ src/client/store/ui/originalDataPoint/reducers/getOriginalDataPointHistory.ts
@@ -1,8 +1,8 @@
 import { ActionReducerMapBuilder } from '@reduxjs/toolkit'
 import { Objects } from 'utils/objects'
 
-import { getOriginalDataPointHistory } from '../actions/getOriginalDataPointHistory'
-import { OriginalDataPointState } from '../stateType'
+import { getOriginalDataPointHistory } from 'client/store/ui/originalDataPoint/actions/getOriginalDataPointHistory'
+import { OriginalDataPointState } from 'client/store/ui/originalDataPoint/stateType'
 
 export const getOriginalDataPointHistoryReducer = (builder: ActionReducerMapBuilder<OriginalDataPointState>) =>
   builder.addCase(getOriginalDataPointHistory.fulfilled, (state, { payload }) => {
