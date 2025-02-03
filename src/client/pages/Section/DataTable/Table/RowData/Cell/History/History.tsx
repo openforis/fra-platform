0d0bc8b40678eca28af80eb3def3c31a5c502302
Miró Sorja <ping@mirosorja.com>
2025-02-03 12:17:20 +0200

    4304 - fix padding for calculated cells

--- src/client/pages/Section/DataTable/Table/RowData/Cell/History/History.tsx
+++ src/client/pages/Section/DataTable/Table/RowData/Cell/History/History.tsx
@@ -1,12 +1,14 @@
+import './History.scss'
 import React, { useMemo } from 'react'
 
 import * as Diff from 'diff'
 import { Change } from 'diff'
 
 import { CountryIso } from 'meta/area'
-import { NodeValue } from 'meta/assessment'
+import { Cycles, NodeValue } from 'meta/assessment'
 import { RecordAssessmentDatas } from 'meta/data'
 
+import { useAssessment, useCycle } from 'client/store/assessment'
 import { useLastApprovedHistoryTableData } from 'client/store/data/hooks/useLastApprovedHistoryTableData'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
 import DiffText from 'client/components/DiffText'
@@ -32,18 +34,30 @@ const useChanges = (props: { nodeValueA: NodeValue; nodeValueB: NodeValue }): Re
 
 const History: React.FC<PropsCell> = (props) => {
   const { nodeValue, table, col, row } = props
-  const { assessmentName, cycleName, countryIso } = useCountryRouteParams<CountryIso>()
+  const { assessmentName, /* cycleName: cycleNameParam, */ countryIso } = useCountryRouteParams<CountryIso>()
+  const assessment = useAssessment()
+  const cycle = useCycle()
   const tableName = table.props.name
   const { colName } = col.props
   const { variableName } = row.props
   const data = useLastApprovedHistoryTableData()
+
+  // TODO: Depending on HistoryLastApprovedInfo, cycleName is either current cycleName or prevCycleName
+  // const info: HistoryLastApprovedInfo = useHistoryLastApprovedInfo()
+  const prevCycle = Cycles.getPreviousCycle({ assessment, cycle })
+  const cycleName = prevCycle?.name
+
   const nodeValueProps = { assessmentName, cycleName, countryIso, tableName, colName, variableName, data }
   const nodeValueA = RecordAssessmentDatas.getNodeValue(nodeValueProps) ?? ({} as NodeValue)
   const nodeValueB = nodeValue ?? ({} as NodeValue)
 
   const changes = useChanges({ nodeValueA, nodeValueB })
 
-  return <DiffText changes={changes} />
+  return (
+    <div className="input-text disabled table-grid__data-cell-input-number">
+      <DiffText changes={changes} />
+    </div>
+  )
 }
 
 export default History
