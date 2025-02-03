3a66a068cebda22cea19030069a120fe4f6b56d4
Miró Sorja <ping@mirosorja.com>
2025-02-03 12:28:51 +0200

    4304 - API/table/history: return previous cycleData

--- src/server/api/cycleData/table/getTableDataHistory.ts
+++ src/server/api/cycleData/table/getTableDataHistory.ts
@@ -2,7 +2,10 @@ import { Response } from 'express'
 
 import { CycleDataRequest } from 'meta/api/request'
 import { CountryIso } from 'meta/area'
+import { Cycles } from 'meta/assessment'
 
+import { AssessmentController } from 'server/controller/assessment'
+import { CycleDataController } from 'server/controller/cycleData'
 import Requests from 'server/utils/requests'
 
 type GetTableDataRequest = CycleDataRequest<{
@@ -10,9 +13,22 @@ type GetTableDataRequest = CycleDataRequest<{
   countryISOs: Array<CountryIso>
 }>
 
-export const getTableDataHistory = async (_req: GetTableDataRequest, res: Response) => {
+export const getTableDataHistory = async (req: GetTableDataRequest, res: Response) => {
   try {
-    Requests.send(res, {})
+    const { countryIso, assessmentName, cycleName, tableNames } = req.query
+    const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
+    // const info: HistoryLastApprovedInfo = await History.LastApproved.getInfo({ assessment, cycle, countryIso })
+
+    let tableData = {}
+    // if (info.prevCycle)
+    const prevCycle = Cycles.getPreviousCycle({ assessment, cycle })
+    if (prevCycle) {
+      const mergeOdp = true
+      const props = { assessment, cycle: prevCycle, countryISOs: [countryIso], tableNames, mergeOdp }
+      tableData = await CycleDataController.getTableData(props)
+    }
+
+    Requests.send(res, tableData)
   } catch (e) {
     Requests.sendErr(res, e)
   }
