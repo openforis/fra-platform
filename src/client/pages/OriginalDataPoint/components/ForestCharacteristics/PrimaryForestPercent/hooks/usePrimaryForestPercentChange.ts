f8303593d167df9b7699c1c74579e2dbc1c1d7a2
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-02-10 15:18:43 -0500

    4253 - ODP history: Add primaryForestPercentChange diffs

---
+++ src/client/pages/OriginalDataPoint/components/ForestCharacteristics/PrimaryForestPercent/hooks/usePrimaryForestPercentChange.ts
@@ -0,0 +1,32 @@
+import { useMemo } from 'react'
+
+import * as Diff from 'diff'
+import { Change } from 'diff'
+import { Numbers } from 'utils/numbers'
+
+import { useHistoryLastApprovedIsActive } from 'client/store/data'
+import { useLastApprovedOriginalDataPoint } from 'client/store/data/hooks/useLastApprovedOriginalDataPoint'
+
+type Props = {
+  primaryForestPercent?: string
+}
+
+type Returned = Array<Change>
+
+export const usePrimaryForestPercentChange = (props: Props): Returned => {
+  const { primaryForestPercent: primaryForestPercentCurrent } = props
+
+  const originalDataPointHistory = useLastApprovedOriginalDataPoint()
+  const historyLastApprovedIsActive = useHistoryLastApprovedIsActive()
+
+  return useMemo<Returned>(() => {
+    if (!historyLastApprovedIsActive) return undefined
+
+    const formattedPrimaryForestTotalPercentPrev = Numbers.toFixed(
+      originalDataPointHistory?.values?.primaryForestPercent ?? '',
+      3
+    )
+    const formattedPrimaryForestPercentCurrent = Numbers.toFixed(primaryForestPercentCurrent ?? '', 3)
+    return Diff.diffChars(formattedPrimaryForestTotalPercentPrev ?? '', formattedPrimaryForestPercentCurrent ?? '')
+  }, [historyLastApprovedIsActive, originalDataPointHistory, primaryForestPercentCurrent])
+}
