21ca6b6debfd47ef62f9b5d5573383d215e1df6e
minotogna <mino.togna@gmail.com>
2025-02-11 10:50:59 +0100

    4353 - Country status: NotStarted -> Editing

--- src/meta/area/areas.ts
+++ src/meta/area/areas.ts
@@ -1,3 +1,5 @@
+import { Objects } from 'utils/objects'
+
 import { AreaCode, Country, CountryIso, Global, RegionCode } from 'meta/area'
 import { fraRegionCodes } from 'meta/area/regionCode'
 import { AssessmentStatus } from 'meta/area/status'
@@ -16,9 +18,13 @@ const isISOGlobal = (isoCode: string): boolean => isoCode === Global.WO
 const isRegion = (isoCode: string): boolean => Object.values(RegionCode).includes(isoCode as RegionCode)
 const isFRARegion = (isoCode: string): boolean => fraRegionCodes.includes(isoCode as RegionCode)
 const getStatus = (country: Country): AssessmentStatus => {
-  if (!country?.lastEdit) return AssessmentStatus.notStarted
-  if (!country?.props?.status && country?.lastEdit) return AssessmentStatus.editing
-  return country?.props?.status
+  const hasLastEdit = !Objects.isNil(country?.lastEdit)
+  const { status } = country?.props ?? {}
+  const hasStatus = !Objects.isNil(status)
+
+  if (!hasLastEdit) return AssessmentStatus.notStarted
+  if (hasLastEdit && (!hasStatus || status === AssessmentStatus.notStarted)) return AssessmentStatus.editing
+  return status
 }
 
 export const Areas = {
