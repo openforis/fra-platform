055fd5f875ba27bdded3af07781ff766f2092c27
minotogna <mino.togna@gmail.com>
2025-02-11 10:54:34 +0100

    4353 - deepscan

--- src/meta/area/areas.ts
+++ src/meta/area/areas.ts
@@ -18,12 +18,10 @@ const isISOGlobal = (isoCode: string): boolean => isoCode === Global.WO
 const isRegion = (isoCode: string): boolean => Object.values(RegionCode).includes(isoCode as RegionCode)
 const isFRARegion = (isoCode: string): boolean => fraRegionCodes.includes(isoCode as RegionCode)
 const getStatus = (country: Country): AssessmentStatus => {
-  const hasLastEdit = !Objects.isNil(country?.lastEdit)
   const { status } = country?.props ?? {}
-  const hasStatus = !Objects.isNil(status)
 
-  if (!hasLastEdit) return AssessmentStatus.notStarted
-  if (hasLastEdit && (!hasStatus || status === AssessmentStatus.notStarted)) return AssessmentStatus.editing
+  if (Objects.isNil(country?.lastEdit)) return AssessmentStatus.notStarted
+  if (status === AssessmentStatus.notStarted || Objects.isNil(status)) return AssessmentStatus.editing
   return status
 }
 
