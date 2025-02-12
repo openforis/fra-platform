0c85326d10b78bd977718b61314a408727a49f0e
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-02-12 09:24:59 -0500

    4325 - Add sheet url env var

--- src/server/api/kiosk/getLatestActivities.ts
+++ src/server/api/kiosk/getLatestActivities.ts
@@ -2,15 +2,13 @@ import axios from 'axios'
 import { Request, Response } from 'express'
 
 import { KioskController } from 'server/controller/kiosk'
+import { ProcessEnv } from 'server/utils'
 import Requests from 'server/utils/requests'
 
 export const getLatestActivities = async (_req: Request, res: Response) => {
   try {
-    const spreadsheetId = '15HPJG9c8VQIiznk4eCkM5WV-1YaLDCwEH7jXprhfpH8'
-    const gid = '0'
-    const googleSheetsUrl = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/export?format=csv&gid=${gid}`
-
-    const response = await axios.get(googleSheetsUrl, { responseType: 'text' })
+    const sheetUrl = ProcessEnv.kioskActivitiesSheetUrl
+    const response = await axios.get(sheetUrl, { responseType: 'text' })
     const csvData = response.data
 
     const latestActivities = await KioskController.getLatestActivitiesFromCsv({ csvData })
