40ce8c4564e02cf36d3e1a8e9bbfbfed230e8913
minotogna <mino.togna@gmail.com>
2025-02-12 14:42:10 +0100

    code clen up

--- src/server/repository/assessmentCycle/originalDataPoint/getLastAccepted.ts
+++ src/server/repository/assessmentCycle/originalDataPoint/getLastAccepted.ts
@@ -14,13 +14,10 @@ type Props = {
 export const getLastAccepted = async (props: Props, client: BaseProtocol = DB): Promise<OriginalDataPoint | null> => {
   const { assessment, cycle, countryIso, year } = props
 
+  const _props = { assessment, cycle, countryISOs: [countryIso], year }
+
   return client.oneOrNone<OriginalDataPoint>(
-    `with activities as (${OriginalDataPointQueries.getLastAcceptedActivity({
-      assessment,
-      cycle,
-      countryISOs: [countryIso],
-      year,
-    })})
+    `with activities as (${OriginalDataPointQueries.getLastAcceptedActivity(_props)})
      select a.target as odp
      from activities a
      where a.row_number = 1
