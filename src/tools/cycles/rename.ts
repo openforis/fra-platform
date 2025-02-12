6fa6d1c683793bda2a186ae37f464aaeccea4b57
Mino Togna <minotogna@users.noreply.github.com>
2025-02-12 09:52:29 +0100

    Clone/Rename/Remove Cycle scripts (#4365)

---
+++ src/tools/cycles/rename.ts
@@ -0,0 +1,27 @@
+import 'tsconfig-paths/register'
+import 'dotenv/config'
+
+import { ToolsUtils } from 'tools/utils/toolsUtils'
+
+import { AssessmentNames } from 'meta/assessment'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { UserController } from 'server/controller/user'
+import { DB } from 'server/db'
+
+const client = DB
+const assessmentName = AssessmentNames.fra
+// const cycleName = '2025'
+const cycleNameClone = 'latest'
+const cycleNameCloneRenamed = '2026'
+
+export const renameCycle = async (): Promise<void> => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    { assessmentName, cycleName: cycleNameClone },
+    client
+  )
+  const user = await UserController.getOne({ email: 'fra@fao.org' }, client)
+  await AssessmentController.renameCycle({ assessment, cycle, name: cycleNameCloneRenamed, user }, client)
+}
+
+ToolsUtils.exec(renameCycle)
