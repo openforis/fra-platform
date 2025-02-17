37cd16dbe7baa874fc440147c0888b8bfc847886
Miró Sorja <ping@mirosorja.com>
2025-02-17 12:11:59 +0200

    4372 - Update redirect logic

--- src/client/components/PageLayout/Header/CycleSwitcher/hooks/useNavigateTo.tsx
+++ src/client/components/PageLayout/Header/CycleSwitcher/hooks/useNavigateTo.tsx
@@ -1,7 +1,6 @@
 import { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { Areas, Global } from 'meta/area'
 import { Assessment, Cycle, Cycles } from 'meta/assessment'
 import { Routes } from 'meta/routes'
 import { User, Users } from 'meta/user'
@@ -13,7 +12,7 @@ export const useNavigateTo = () => {
   const navigate = useNavigate()
   // const { countryIso } = useCountryRouteParams()
   const isAdminPage = useIsAdminRoute()
-  const { countryIso } = useCountryRouteParams()
+  const { countryIso, assessmentName: prevAssessmentName } = useCountryRouteParams()
 
   return useCallback(
     (props: { assessment: Assessment; cycle: Cycle; user: User }) => {
@@ -21,16 +20,15 @@ export const useNavigateTo = () => {
       const assessmentName = assessment.props.name
       const cycleName = cycle.name
       const hasRoleInCountry = Users.hasRoleInCountry({ user, cycle, countryIso })
+      const isSameAssessment = prevAssessmentName === assessmentName
 
       let link = Routes.Cycle.generatePath({ assessmentName, cycleName })
-      if (Areas.isISOCountry(countryIso) && (Cycles.isPublished(cycle) || hasRoleInCountry))
+      if (isSameAssessment && countryIso && (Cycles.isPublished(cycle) || hasRoleInCountry)) {
         link = Routes.CountryHome.generatePath({ countryIso, assessmentName, cycleName })
-      if (!Areas.isISOCountry(countryIso) && (Cycles.isPublished(cycle) || hasRoleInCountry))
-        link = Routes.CountryHome.generatePath({ countryIso: Global.WO, assessmentName, cycleName })
-      else if (isAdminPage) link = Routes.Admin.generatePath({ assessmentName, cycleName })
+      } else if (isAdminPage) link = Routes.Admin.generatePath({ assessmentName, cycleName })
 
       navigate(link)
     },
-    [countryIso, isAdminPage, navigate]
+    [countryIso, isAdminPage, navigate, prevAssessmentName]
   )
 }
