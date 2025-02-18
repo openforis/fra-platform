0435bacfa13ed93b529fcce42242a3616113853b
Miró Sorja <ping@mirosorja.com>
2025-02-18 15:09:34 +0200

    4372 - admin/country: use common country selector

--- src/client/pages/CountryHome/CountryHeader/CountrySelector/CountrySelector.tsx
+++ src/client/pages/CountryHome/CountryHeader/CountrySelector/CountrySelector.tsx
@@ -2,7 +2,6 @@ import './CountrySelector.scss'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import classNames from 'classnames'
 import { Objects } from 'utils/objects'
 
 import { Areas, CountryIso } from 'meta/area'
@@ -13,10 +12,9 @@ import { useDashboardItems } from 'client/store/metadata'
 import { useHomeCountriesFilter } from 'client/store/ui/home'
 import { HomeActions } from 'client/store/ui/home/slice'
 import { useCountryRouteParams } from 'client/hooks/useRouteParams'
-import CountryMultiSelect from 'client/components/CountryMultiSelect/CountryMultiSelect'
+import CountryMultiSelect from 'client/components/CountryMultiSelect'
+import Hr from 'client/components/Hr'
 import Icon from 'client/components/Icon'
-
-import { useTooltipContent } from './hooks/useTooltipContent'
 
 const __MIN_COUNTRIES__ = 9
 
@@ -32,55 +30,25 @@ const CountrySelector: React.FC = () => {
   const dashboardItems = useDashboardItems()
 
   const error = selection.length > 0 && selection.length < __MIN_COUNTRIES__
-  const errorMessage = t('statisticalFactsheets.validation.selectAtLeastNCountries', { count: __MIN_COUNTRIES__ })
-
-  const { hideTooltip, showTooltip, tooltipContent, dataTooltipId } = useTooltipContent({
-    selection,
-    error,
-    errorMessage,
-  })
 
   const onMenuClose = () => {
     if (!error) dispatch(HomeActions.updateCountriesFilter(selection))
-    showTooltip()
-  }
-
-  const onChange = (selection: Array<CountryIso>) => {
-    setSelection(selection)
   }
 
   if (Objects.isEmpty(countries)) return null
   if (!Areas.isISOGlobal(countryIso)) return null
   if (!dashboardItems) return null
 
-  const placeholder = t('common.filterCountries')
-
   return (
-    <div
-      className="country-selector"
-      data-tooltip-class-name="filter-country__tooltip"
-      data-tooltip-delay-hide={100}
-      data-tooltip-html={tooltipContent}
-      data-tooltip-id={dataTooltipId}
-      data-tooltip-place="bottom"
-    >
+    <div className="country-selector">
       <Icon name="filter" />
-
+      <Hr vertical />
       <CountryMultiSelect
-        classNames={{
-          container: classNames('filter-multiselect__container', {
-            active: selection?.length > 0,
-            error: true,
-          }),
-        }}
-        collapsibleGroups
-        isMulti
-        onChange={onChange}
+        error={error}
+        minCountries={__MIN_COUNTRIES__}
+        onChange={(value) => setSelection(value as Array<CountryIso>)}
         onMenuClose={onMenuClose}
-        onMenuOpen={hideTooltip}
-        placeholder={placeholder}
-        selectableGroups
-        toggleAll
+        placeholder={t('common.filterCountries')}
         value={selection}
       />
     </div>
