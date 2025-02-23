f4b8791d845f3b758c2cc9c8c9ba3d402d8d21b7
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-02-23 21:42:27 +0530

    4391 - Add panEu 3.2 reasonableness new structure

--- src/test/migrations/steps/20250222015809-step-panEuropean-2025-reasonability-check-3-2-fix.ts
+++ src/test/migrations/steps/20250222015809-step-panEuropean-2025-reasonability-check-3-2-fix.ts
@@ -8,9 +8,9 @@ import { TableRepository } from 'server/repository/assessment/table'
 
 const years = [2020, 2015, 2010, 2005, 2000, 1990]
 
-const getGrowingStockSupplyYearNonHeaderCols = (year: number, cycleUuid: string): Array<ColProps> => {
+const getRoundwoodSupplyYearsCols = (year: number, cycleUuid: string): Array<ColProps> => {
   let index = years.indexOf(year)
-  if (year === 2020) index += 1
+  if (year === 2020) index += 2 // Offset var name and unit '%'
   return [
     {
       // year col
@@ -18,7 +18,7 @@ const getGrowingStockSupplyYearNonHeaderCols = (year: number, cycleUuid: string)
       index,
       labels: {
         [cycleUuid]: {
-          key: year.toString(), // TODO: replace with translation key
+          label: year.toString(),
         },
       },
     },
@@ -29,30 +29,30 @@ const getGrowingStockSupplyYearNonHeaderCols = (year: number, cycleUuid: string)
     },
     {
       // FAWS col
-      colType: ColType.placeholder,
-      index,
-      labels: {
-        [cycleUuid]: {
-          key: 'N/A', // TODO: replace with translation key
-        },
+      colName: 'FAWS',
+      colType: ColType.calculated,
+      calculateFn: {
+        [cycleUuid]: `table_3_2._of_which_forest_available_for_wood_supply_${year}.net_annual_increment / (table_1_2a._of_which_available_for_wood_supply_${year}.total * 1000)`,
       },
+      index: index + 2,
     },
+
     {
       // OWL col
       colType: ColType.placeholder,
-      index: index + 2,
+      index: index + 3,
     },
     {
       // FOWL col
       colType: ColType.placeholder,
-      index: index + 3,
+      index: index + 4,
     },
   ]
 }
 
-const getGrowingStockTotalYearNonHeaderCols = (year: number, cycleUuid: string): Array<ColProps> => {
+const getRoundwoodTotalYearsCols = (year: number, cycleUuid: string): Array<ColProps> => {
   let index = years.indexOf(year)
-  if (year === 2020) index += 1
+  if (year === 2020) index += 2 // Offset var name and unit '%'
   return [
     {
       // year col
@@ -60,7 +60,7 @@ const getGrowingStockTotalYearNonHeaderCols = (year: number, cycleUuid: string):
       index,
       labels: {
         [cycleUuid]: {
-          key: year.toString(), // TODO: replace with translation key
+          label: year.toString(),
         },
       },
     },
@@ -72,22 +72,22 @@ const getGrowingStockTotalYearNonHeaderCols = (year: number, cycleUuid: string):
     {
       // FAWS col
       colType: ColType.placeholder,
-      index: index + 3,
+      index: index + 2,
     },
     {
       // OWL col
       colType: ColType.placeholder,
-      index: index + 2,
+      index: index + 3,
     },
     {
       // FOWL col
-      colType: ColType.placeholder,
-      index,
-      labels: {
-        [cycleUuid]: {
-          key: 'N/A', // TODO: replace with translation key
-        },
-      },
+      colType: ColType.calculated,
+      // TODO: add calculateFn
+      // colName: '',
+      // calculateFn: {
+      //   [cycleUuid]: '',
+      // },
+      index: index + 4,
     },
   ]
 }
@@ -101,11 +101,12 @@ export default async (client: BaseProtocol) => {
   const cycleUuid = cycle.uuid
   const schemaAssessment = Schemas.getName(assessment)
   const tableName = 'reasonability_check_3_2'
+  const table = await TableRepository.getOne({ assessment, cycle, tableName })
 
   // 1. Add new grid template columns
   const gridTemplateColumns = 'minmax(150px, auto) repeat(6, minmax(min-content, 1fr))'
 
-  client.query(
+  await client.query(
     `update ${schemaAssessment}.table
      set props = jsonb_set(
       jsonb_set(
@@ -122,11 +123,111 @@ export default async (client: BaseProtocol) => {
     `,
     [JSON.stringify({ gridTemplateColumns })]
   )
-  // 2. TODO: Delete current cols
-  // 3. TODO: Delete current rows
 
-  // 4. Insert new rows and cols
-  const roundwoodRemovalAsPctOfGrowingStockSupplyRows: Array<RowProps> = years.map((year, index) => {
+  // 2. Delete current cols
+  await client.query(
+    `delete from ${schemaAssessment}.col
+     where id in (
+        select c.id
+        from ${schemaAssessment}.col c
+        join ${schemaAssessment}.row r on c.row_id = r.id
+        join ${schemaAssessment}.table t on r.table_id = t.id
+        where t.props->>'name' = '${tableName}'
+     );`,
+    []
+  )
+  // 3. Delete current rows
+  await client.query(
+    `delete from ${schemaAssessment}.row
+     where id in (
+        select r.id
+        from ${schemaAssessment}.row r
+        join ${schemaAssessment}.table t on r.table_id = t.id
+        where t.props->>'name' = '${tableName}'
+     );`,
+    []
+  )
+
+  // 4. Insert new rows and cols for Roundwood Supply and Roundwood Total
+  const headerRowProps: RowProps = {
+    index: 'header_0',
+    type: RowType.header,
+  }
+
+  const headerCols: Array<ColProps> = [
+    {
+      colType: ColType.header,
+      index: 0,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.variable',
+        },
+      },
+    },
+    {
+      colType: ColType.header,
+      index: 1,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.reasonabilityChecks.unit',
+        },
+      },
+    },
+    {
+      colType: ColType.header,
+      index: 2,
+      labels: {
+        [cycle.uuid]: {
+          key: 'common.year',
+        },
+      },
+    },
+    {
+      colType: ColType.header,
+      index: 3,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.reasonabilityChecks.forest',
+        },
+      },
+    },
+
+    {
+      colType: ColType.header,
+      index: 4,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.reasonabilityChecks.FAWS',
+        },
+      },
+    },
+
+    {
+      colType: ColType.header,
+      index: 5,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.reasonabilityChecks.OWL',
+        },
+      },
+    },
+    {
+      colType: ColType.header,
+      index: 6,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.reasonabilityChecks.FOWL',
+        },
+      },
+    },
+  ]
+
+  const headerRow = await RowRepository.create({ assessment, cycles: [cycle], rowProps: headerRowProps, table }, client)
+  headerCols.forEach(async (colProps) => {
+    await ColRepository.create({ assessment, cycles: [cycle], row: headerRow, colProps }, client)
+  })
+
+  const roundwoodSupplyRows: Array<RowProps> = years.map((year, index) => {
     return {
       index,
       label: {
@@ -137,43 +238,10 @@ export default async (client: BaseProtocol) => {
     }
   })
 
-  const roundwoodRemovalAsPctOfGrowingStockTotalHeaderCols: Array<ColProps> = [
+  const roundwoodSupplyVarAndUnitCols: Array<ColProps> = [
     {
       colType: ColType.header,
-      index: 'header_0',
-      labels: {
-        [cycle.uuid]: {
-          key: 'panEuropean.reasonabilityChecks.roundwoodRemovalAsPctOfGrowingStockTotal',
-        },
-      },
-      style: {
-        [cycle.uuid]: {
-          colSpan: 1,
-          rowSpan: 3,
-        },
-      },
-    },
-    {
-      colType: ColType.header,
-      index: 1,
-      labels: {
-        [cycle.uuid]: {
-          key: '%', // TODO: replace with translation key
-        },
-      },
-      style: {
-        [cycle.uuid]: {
-          colSpan: 1,
-          rowSpan: 3,
-        },
-      },
-    },
-  ]
-
-  const roundwoodRemovalAsPctOfGrowingStockSupplyHeaderCols: Array<ColProps> = [
-    {
-      colType: ColType.header,
-      index: 'header_0',
+      index: 0,
       labels: {
         [cycle.uuid]: {
           key: 'panEuropean.reasonabilityChecks.roundwoodRemovalAsPctOfGrowingStockSupply',
@@ -182,28 +250,28 @@ export default async (client: BaseProtocol) => {
       style: {
         [cycle.uuid]: {
           colSpan: 1,
-          rowSpan: 3,
+          rowSpan: 6,
         },
       },
     },
     {
-      colType: ColType.header,
+      colType: ColType.placeholder,
       index: 1,
       labels: {
         [cycle.uuid]: {
-          key: '%', // TODO: replace with translation key
+          label: '%',
         },
       },
       style: {
         [cycle.uuid]: {
           colSpan: 1,
-          rowSpan: 3,
+          rowSpan: 6,
         },
       },
     },
   ]
 
-  const roundwoodRemovalAsPctOfGrowingStockTotalRows: Array<RowProps> = years.map((year, index) => {
+  const roundwoodTotalRows: Array<RowProps> = years.map((year, index) => {
     return {
       index,
       label: {
@@ -214,35 +282,67 @@ export default async (client: BaseProtocol) => {
     }
   })
 
-  const table = await TableRepository.getOne({ assessment, cycle, tableName })
+  const roundwoodTotalVarAndUnitCols: Array<ColProps> = [
+    {
+      colType: ColType.header,
+      index: 0,
+      labels: {
+        [cycle.uuid]: {
+          key: 'panEuropean.reasonabilityChecks.roundwoodRemovalAsPctOfGrowingStockTotal',
+        },
+      },
+      style: {
+        [cycle.uuid]: {
+          colSpan: 1,
+          rowSpan: 6,
+        },
+      },
+    },
+    {
+      colType: ColType.placeholder,
+      index: 1,
+      labels: {
+        [cycle.uuid]: {
+          label: '%',
+        },
+      },
+      style: {
+        [cycle.uuid]: {
+          colSpan: 1,
+          rowSpan: 6,
+        },
+      },
+    },
+  ]
 
-  roundwoodRemovalAsPctOfGrowingStockSupplyRows.forEach(async (rowProps) => {
+  roundwoodSupplyRows.forEach(async (rowProps) => {
     const row = await RowRepository.create({ assessment, cycles: [cycle], rowProps, table }, client)
     if (rowProps.index === 0) {
-      roundwoodRemovalAsPctOfGrowingStockSupplyHeaderCols.forEach(async (colProps) => {
+      roundwoodSupplyVarAndUnitCols.forEach(async (colProps) => {
         await ColRepository.create({ assessment, cycles: [cycle], row, colProps }, client)
       })
     }
     const year = years[Number(rowProps.index)]
-    const yearCols = getGrowingStockSupplyYearNonHeaderCols(year, cycle.uuid)
+    const yearCols = getRoundwoodSupplyYearsCols(year, cycle.uuid)
     yearCols.forEach(async (colProps) => {
       await ColRepository.create({ assessment, cycles: [cycle], row, colProps }, client)
     })
   })
 
-  roundwoodRemovalAsPctOfGrowingStockTotalRows.forEach(async (rowProps) => {
+  roundwoodTotalRows.forEach(async (rowProps) => {
     const row = await RowRepository.create({ assessment, cycles: [cycle], rowProps, table }, client)
     if (rowProps.index === 0) {
-      roundwoodRemovalAsPctOfGrowingStockTotalHeaderCols.forEach(async (colProps) => {
+      roundwoodTotalVarAndUnitCols.forEach(async (colProps) => {
         await ColRepository.create({ assessment, cycles: [cycle], row, colProps }, client)
       })
     }
     const year = years[Number(rowProps.index)]
-    const yearCols = getGrowingStockTotalYearNonHeaderCols(year, cycle.uuid)
+    const yearCols = getRoundwoodTotalYearsCols(year, cycle.uuid)
     yearCols.forEach(async (colProps) => {
       await ColRepository.create({ assessment, cycles: [cycle], row, colProps }, client)
     })
   })
 
-  // 5. TODO: Generate cache
+  // 5. Generate cache
+  await AssessmentController.generateMetadataCache({ assessment }, client)
 }
