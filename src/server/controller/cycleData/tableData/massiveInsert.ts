89a2fca67a1f9885f738d3ac8f1854898a7666b2
Mino Togna <mino.togna@gmail.com>
2025-02-25 20:58:57 +0100

    pass client to worker data

--- src/server/controller/cycleData/tableData/massiveInsert.ts
+++ src/server/controller/cycleData/tableData/massiveInsert.ts
@@ -1,3 +1,5 @@
+import { Promises } from 'utils/promises'
+
 import { CountryIso } from 'meta/area'
 import { ActivityLogMessage, Assessment, Cycle, Node, RowCache, RowCaches, TableName } from 'meta/assessment'
 import { NodeUpdate } from 'meta/data'
@@ -25,53 +27,51 @@ export const massiveInsert = async (props: Props): Promise<void> => {
   const rows = await RowRedisRepository.getRows({ assessment })
 
   await DB.tx(async (client) => {
-    await Promise.all(
-      // For each country
-      Object.entries(countryNodes).map(async ([_countryIso, nodes]) => {
-        const countryIso = _countryIso as CountryIso
-        const tableNames = new Set<TableName>()
-        const rowsByRowUuid: Record<string, RowCache> = {}
+    // For each country
+    await Promises.each(Object.entries(countryNodes), async ([_countryIso, nodes]) => {
+      const countryIso = _countryIso as CountryIso
+      const tableNames = new Set<TableName>()
+      const rowsByRowUuid: Record<string, RowCache> = {}
 
-        // 1. create nodes db
-        const nodesDb = nodes.map<NodeDb>((node) => {
-          const { tableName, variableName, colName } = node
-          const rowKey = RowCaches.getKey({ tableName, variableName })
-          const row = rows[rowKey]
-          const col = row.cols.find((c) => c.props.colName === colName)
-          tableNames.add(tableName)
-          rowsByRowUuid[row.uuid] = row
-          return { country_iso: countryIso, col_uuid: col.uuid, row_uuid: row.uuid, value: node.value }
-        })
-
-        // 2. Insert nodes into DB
-        const nodesInsert = await NodeRepository.massiveInsert({ assessment, cycle, nodes: nodesDb }, client)
-
-        // 3. Insert activity logs into DB
-        const activityLogs = nodesInsert.map<ActivityLogDb<Node>>((target: Node) => {
-          const section = rowsByRowUuid[target.rowUuid]?.sectionName
-          const message = ActivityLogMessage.nodeValueImport
-          return {
-            assessment_uuid: assessmentUuid,
-            cycle_uuid: cycleUuid,
-            country_iso: countryIso,
-            section,
-            message,
-            target,
-            user_id: user.id,
-          }
-        })
-        await ActivityLogRepository.massiveInsert({ activityLogs }, client)
-
-        // 4. Update Redis cache for each affected table
-        await Promise.all(
-          Array.from(tableNames).map((tableName) =>
-            DataRedisRepository.cacheCountryTable({ assessment, cycle, countryIso, tableName, force: true }, client)
-          )
-        )
-
-        // 5. Update dependencies
-        await updateTableDataDependencies({ assessment, cycle, countryIso, nodes, user })
+      // 1. create nodes db
+      const nodesDb = nodes.map<NodeDb>((node) => {
+        const { tableName, variableName, colName } = node
+        const rowKey = RowCaches.getKey({ tableName, variableName })
+        const row = rows[rowKey]
+        const col = row.cols.find((c) => c.props.colName === colName)
+        tableNames.add(tableName)
+        rowsByRowUuid[row.uuid] = row
+        return { country_iso: countryIso, col_uuid: col.uuid, row_uuid: row.uuid, value: node.value }
       })
-    )
+
+      // 2. Insert nodes into DB
+      const nodesInsert = await NodeRepository.massiveInsert({ assessment, cycle, nodes: nodesDb }, client)
+
+      // 3. Insert activity logs into DB
+      const activityLogs = nodesInsert.map<ActivityLogDb<Node>>((target: Node) => {
+        const section = rowsByRowUuid[target.rowUuid]?.sectionName
+        const message = ActivityLogMessage.nodeValueImport
+        return {
+          assessment_uuid: assessmentUuid,
+          cycle_uuid: cycleUuid,
+          country_iso: countryIso,
+          section,
+          message,
+          target,
+          user_id: user.id,
+        }
+      })
+      await ActivityLogRepository.massiveInsert({ activityLogs }, client)
+
+      // 4. Update Redis cache for each affected table
+      await Promise.all(
+        Array.from(tableNames).map((tableName) =>
+          DataRedisRepository.cacheCountryTable({ assessment, cycle, countryIso, tableName, force: true }, client)
+        )
+      )
+
+      // 5. Update dependencies
+      await updateTableDataDependencies({ assessment, cycle, countryIso, nodes, user }, client)
+    })
   })
 }
--- src/server/controller/cycleData/tableData/updateTableDataDependencies.ts
+++ src/server/controller/cycleData/tableData/updateTableDataDependencies.ts
@@ -5,6 +5,7 @@ import { User } from 'meta/user'
 
 import { UpdateDependenciesJob } from 'server/controller/cycleData/updateDependencies/props'
 import worker from 'server/controller/cycleData/updateDependencies/worker'
+import { BaseProtocol, DB } from 'server/db'
 
 type Props = {
   assessment: Assessment
@@ -16,13 +17,21 @@ type Props = {
   user: User
 }
 
-export const updateTableDataDependencies = async (props: Props): Promise<void> => {
+export const updateTableDataDependencies = async (props: Props, client: BaseProtocol = DB): Promise<void> => {
   const { assessment, cycle, countryIso, nodes, includeSourceNodes, isODP, user } = props
   const { name: assessmentName } = assessment.props
   const { name: cycleName } = cycle
 
   const nodeUpdates = { assessmentName, cycleName, countryIso, nodes }
-  const data: UpdateDependenciesJob['data'] = { assessment, cycle, nodeUpdates, user, isODP, includeSourceNodes }
+  const data: UpdateDependenciesJob['data'] = {
+    assessment,
+    cycle,
+    nodeUpdates,
+    user,
+    isODP,
+    includeSourceNodes,
+    client,
+  }
   const job = { queue: undefined, id: 'job-id', name: 'job-name', data } as unknown as UpdateDependenciesJob
 
   await worker(job)
