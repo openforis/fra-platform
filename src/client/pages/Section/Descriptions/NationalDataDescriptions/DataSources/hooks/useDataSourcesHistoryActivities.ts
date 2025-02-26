287b51c88a15df074153ffc73b2b00ef66d00807
Mino Togna <mino.togna@gmail.com>
2025-02-26 19:39:36 +0100

    4206 - Data History: Fix and Simplify Data Sources differences

--- src/client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/hooks/useDataSourcesHistoryActivities.ts
+++ src/client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/hooks/useDataSourcesHistoryActivities.ts
@@ -3,13 +3,16 @@ import { useMemo } from 'react'
 import { CommentableDescription, CommentableDescriptionName, DataSource } from 'meta/assessment'
 
 import { useHistoryActivitiesCompareItem } from 'client/store/data'
-import { DataSourceHistoryCompare } from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/types'
+
+import { DataSourceHistoryCompare } from '../types'
+import { getDataSourceHistoryCompares } from './_getDataSourceHistoryCompares'
 
 type ActivityLogTarget = { description: CommentableDescription }
 
 type Props = {
   dataSources: Array<DataSource>
 }
+
 type Returned = Array<DataSourceHistoryCompare> | undefined
 
 export const useDataSourcesHistoryActivities = (props: Props): Returned => {
@@ -20,40 +23,8 @@ export const useDataSourcesHistoryActivities = (props: Props): Returned => {
   return useMemo<Returned>(() => {
     if (!compareItem) return undefined
 
-    const items: Returned = []
     const dataSourcesHistory = compareItem.target?.description?.value?.dataSources ?? []
 
-    const dataLength = dataSources.length
-    const historyLength = dataSourcesHistory.length
-
-    let dataIndex = 0
-    let historyIndex = 0
-    for (let i = 0; i < Math.max(dataLength, historyLength); i += 1) {
-      let dataItem: DataSource
-      let historyItem: DataSource
-
-      const data = dataSources[dataIndex]
-      const history = dataSourcesHistory[historyIndex]
-      if (data?.uuid === history?.uuid) {
-        dataItem = data
-        dataIndex += 1
-        historyItem = history
-        historyIndex += 1
-      } else {
-        const onlyData = Boolean(data?.uuid) && !dataSourcesHistory.some((d) => d.uuid === data.uuid)
-
-        if (onlyData) {
-          dataItem = data
-          dataIndex += 1
-        } else {
-          historyItem = history
-          historyIndex += 1
-        }
-      }
-
-      items.push({ dataItem, historyItem })
-    }
-
-    return items
+    return getDataSourceHistoryCompares({ dataSources, dataSourcesHistory })
   }, [compareItem, dataSources])
 }
--- src/client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/hooks/useDataSourcesHistoryLastApproved.ts
+++ src/client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/hooks/useDataSourcesHistoryLastApproved.ts
@@ -3,7 +3,9 @@ import { useMemo } from 'react'
 import { CommentableDescriptionName, DataSource } from 'meta/assessment'
 
 import { useHistoryLastApprovedIsActive, useLastApprovedHistoryDescriptions } from 'client/store/data'
-import { DataSourceHistoryCompare } from 'client/pages/Section/Descriptions/NationalDataDescriptions/DataSources/types'
+
+import { DataSourceHistoryCompare } from '../types'
+import { getDataSourceHistoryCompares } from './_getDataSourceHistoryCompares'
 
 type Props = {
   dataSources: Array<DataSource>
@@ -23,23 +25,6 @@ export const useDataSourcesHistoryLastApproved = (props: Props): Returned => {
     const dataSourcesHistory =
       lastApprovedHistoryDescriptions?.[CommentableDescriptionName.dataSources]?.dataSources ?? []
 
-    const items: Returned = []
-
-    const dataLength = dataSources.length
-    const historyLength = dataSourcesHistory.length
-
-    let dataIndex = 0
-    let historyIndex = 0
-    for (let i = 0; i < Math.max(dataLength, historyLength); i += 1) {
-      const dataItem = dataSources[dataIndex]
-      // TODO: Handle non-empty history items by matching dataSources
-      const historyItem = dataSourcesHistory[historyIndex]
-
-      dataIndex += 1
-      historyIndex += 1
-      items.push({ dataItem, historyItem })
-    }
-
-    return items
+    return getDataSourceHistoryCompares({ dataSources, dataSourcesHistory })
   }, [dataSources, historyLastApprovedIsActive, lastApprovedHistoryDescriptions])
 }
