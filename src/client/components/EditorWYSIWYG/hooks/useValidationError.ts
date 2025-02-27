f20066146b7e37ed46d2a378e8b28bce12ce4b86
Miró Sorja <ping@mirosorja.com>
2025-02-27 10:11:10 +0200

    4399 - Hide validation in print view (#4406)

--- src/client/components/EditorWYSIWYG/hooks/useValidationError.ts
+++ src/client/components/EditorWYSIWYG/hooks/useValidationError.ts
@@ -2,6 +2,7 @@ import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { useCanEditCycleData } from 'client/store/user'
+import { useIsPrintRoute } from 'client/hooks/useIsRoute'
 import { EditorValidators } from 'client/components/EditorWYSIWYG'
 
 type Returned = string
@@ -15,11 +16,13 @@ export const useValidationError = (props: Props): Returned => {
   const { t } = useTranslation()
   const editor = useCanEditCycleData()
 
+  const { print } = useIsPrintRoute()
+
   const validationError = useMemo<Returned>(() => {
-    if (editor && !EditorValidators.links(value)) return t('generalValidation.invalidLink')
+    if (!print && editor && !EditorValidators.links(value)) return t('generalValidation.invalidLink')
 
     return ''
-  }, [editor, t, value])
+  }, [editor, print, t, value])
 
   return validationError
 }
