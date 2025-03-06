e7867b9e631191389f9d3528248a76435be62963
Miró Sorja <ping@mirosorja.com>
2025-03-06 12:05:09 +0200

    4263 - EditorWYSIWYGReadOnly

--- src/client/components/EditorWYSIWYG/EditorWYSIWYGSwitch.tsx
+++ src/client/components/EditorWYSIWYG/EditorWYSIWYGSwitch.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { useIsPrintRoute } from 'client/hooks/useIsRoute'
+import EditorWYSIWYGReadOnly from 'client/components/EditorWYSIWYG/EditorWYSIWYGReadOnly'
 
 import { EditorWYSIWYGProps } from './EditorWYSIWYG'
 import EditorWYSIWYGWithRepositoryContext from './EditorWYSIWYGWithRepositoryContext'
@@ -10,9 +11,7 @@ const EditorWYSIWYGSwitch: React.FC<EditorWYSIWYGProps> = (props: EditorWYSIWYGP
   const { print } = useIsPrintRoute()
 
   if (print) {
-    return (
-      <div className="editorWYSIWYG jodit-wysiwyg textarea-print" dangerouslySetInnerHTML={{ __html: value || '' }} />
-    )
+    return <EditorWYSIWYGReadOnly value={value} />
   }
 
   // eslint-disable-next-line react/jsx-props-no-spreading
