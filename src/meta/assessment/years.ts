8758b9bd2aff37b2267efa7eb3b9d85c02ee9c4a
Miró Sorja <ping@mirosorja.com>
2025-03-11 15:30:23 +0200

    4440 - Add todo and fix years for 2020, 2025 and latest etc

--- src/meta/assessment/years.ts
+++ src/meta/assessment/years.ts
@@ -1,27 +1,27 @@
 import { Cycle } from './cycle'
 
+// TODO: Refactor / dynamic / cycle.props
 const fraYears = (cycle: Cycle): Array<string> => {
-  const years = ['1990', '2000', '2010', '2015', '2020']
-  if (cycle.name === '2025') {
-    years.push('2025')
+  if (cycle.name === '2020') {
+    return ['1990', '2000', '2010', '2015', '2020']
   }
-  return years
+  return ['1990', '2000', '2010', '2015', '2020', '2025']
 }
 
+// TODO: Refactor / dynamic / cycle.props
 const intervals = (cycle: Cycle): Array<string> => {
-  const years = ['1990-2000', '2000-2010', '2010-2015', '2015-2020']
-  if (cycle.name === '2025') {
-    years.push('2020-2025')
+  if (cycle.name === '2020') {
+    return ['1990-2000', '2000-2010', '2010-2015', '2015-2020']
   }
-  return years
+  return ['1990-2000', '2000-2010', '2010-2015', '2015-2020', '2020-2025']
 }
 
+// TODO: Refactor / dynamic / cycle.props
 const annual = (cycle: Cycle): Array<string> => {
-  const years = Array.from({ length: 18 }, (_, i) => String(2000 + i))
-  if (cycle.name === '2025') {
-    years.push(...Array.from({ length: 6 }, (_, i) => String(2018 + i)))
+  if (cycle.name === '2020') {
+    return Array.from({ length: 18 }, (_, i) => String(2000 + i))
   }
-  return years
+  return Array.from({ length: 24 }, (_, i) => String(2000 + i))
 }
 
 export const Years = {
