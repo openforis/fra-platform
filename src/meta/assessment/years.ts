e6e74f9fe2f6da6148d6ac650b606906ccc22859
Miró Sorja <ping@mirosorja.com>
2025-03-12 08:09:07 +0200

    4440 - years: static by cycle name

--- src/meta/assessment/years.ts
+++ src/meta/assessment/years.ts
@@ -1,27 +1,36 @@
 import { Cycle } from './cycle'
 
+const _fraYears: Record<string, Array<string>> = {
+  '2020': ['1990', '2000', '2010', '2015', '2020'],
+  '2025': ['1990', '2000', '2010', '2015', '2020', '2025'],
+  latest: ['1990', '2000', '2010', '2015', '2020', '2025'],
+}
+
 // TODO: Refactor / dynamic / cycle.props
 const fraYears = (cycle: Cycle): Array<string> => {
-  if (cycle.name === '2020') {
-    return ['1990', '2000', '2010', '2015', '2020']
-  }
-  return ['1990', '2000', '2010', '2015', '2020', '2025']
+  return _fraYears[cycle.name]
+}
+
+const _intervals: Record<string, Array<string>> = {
+  '2020': ['1990-2000', '2000-2010', '2010-2015', '2015-2020'],
+  '2025': ['1990-2000', '2000-2010', '2010-2015', '2015-2020', '2020-2025'],
+  latest: ['1990-2000', '2000-2010', '2010-2015', '2015-2020', '2020-2025'],
 }
 
 // TODO: Refactor / dynamic / cycle.props
 const intervals = (cycle: Cycle): Array<string> => {
-  if (cycle.name === '2020') {
-    return ['1990-2000', '2000-2010', '2010-2015', '2015-2020']
-  }
-  return ['1990-2000', '2000-2010', '2010-2015', '2015-2020', '2020-2025']
+  return _intervals[cycle.name]
+}
+
+const _annual: Record<string, Array<string>> = {
+  '2020': Array.from({ length: 18 }, (_, i) => String(2000 + i)),
+  '2025': Array.from({ length: 24 }, (_, i) => String(2000 + i)),
+  latest: Array.from({ length: 24 }, (_, i) => String(2000 + i)),
 }
 
 // TODO: Refactor / dynamic / cycle.props
 const annual = (cycle: Cycle): Array<string> => {
-  if (cycle.name === '2020') {
-    return Array.from({ length: 18 }, (_, i) => String(2000 + i))
-  }
-  return Array.from({ length: 24 }, (_, i) => String(2000 + i))
+  return _annual[cycle.name]
 }
 
 export const Years = {
