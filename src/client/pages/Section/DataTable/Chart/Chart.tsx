c4b7ba605fb182c1d59b25b19e6fe554404a9f3e
Mino Togna <mino.togna@gmail.com>
2025-03-18 09:28:29 +0100

    4458: Chart Legend+ODPTicks refactor

--- src/client/pages/Section/DataTable/Chart/Chart.tsx
+++ src/client/pages/Section/DataTable/Chart/Chart.tsx
@@ -2,18 +2,17 @@ import './style.scss'
 import React from 'react'
 
 import { useIsPrintRoute } from 'client/hooks/useIsRoute'
+import Legend from 'client/pages/Section/DataTable/Chart/Legend'
 import NoDataPlaceholder from 'client/pages/Section/DataTable/Chart/NoDataPlaceholder'
+import OdpTicks from 'client/pages/Section/DataTable/Chart/OdpTicks'
 import YAxis from 'client/pages/Section/DataTable/Chart/YAxis'
 
 import DataTrend from './components/dataTrend'
-import Legend from './components/legend'
-import OdpTicks from './components/odpTicks'
 import XAxis from './components/xAxis'
 import { useChartData } from './hooks/useChartData'
 import { useTrends } from './hooks/useTrends'
 import { useXScale } from './hooks/useXScale'
 import { useYScale } from './hooks/useYScale'
-import * as charts from './charts'
 import { Charts } from './charts'
 import { ChartProps } from './types'
 
@@ -29,19 +28,18 @@ const Chart = (props: ChartProps) => {
   return (
     <div>
       <svg height={Charts.styles.height} width={width}>
-        <Legend data={trendsData} trends={trends} wrapperWidth={width} />
+        <Legend trends={trends} trendsData={trendsData} width={width} />
         <YAxis trendsData={trendsData} width={width} yScale={yScale} />
         <XAxis bottom={Charts.styles.bottom} data={trendsData} height={Charts.styles.height} xScale={xScale} />
+
         {/* odp ticks must be positioned behind all data points */}
-        {trends.map((t) => (
-          <OdpTicks
-            key={`odp-ticks-${t.name}`}
-            className={`chart__odp-ticks-${t.name}`}
-            data={charts.getTrendOdps(trendsData?.[t.name])}
-            xScale={xScale}
-            yScale={yScale}
-          />
-        ))}
+        {trends.map((trend) => {
+          const { name: trendName } = trend
+          const trendData = trendsData[trendName]
+          const key = `odp-ticks-${trendName}`
+          return <OdpTicks key={key} trendData={trendData} xScale={xScale} yScale={yScale} />
+        })}
+
         {trends.map((t) => (
           <DataTrend
             key={`data-trend-${t.name}`}
---
+++ src/client/pages/Section/DataTable/Chart/Legend/hooks/useLegendTransition.ts
@@ -0,0 +1,35 @@
+import { RefObject, useLayoutEffect } from 'react'
+
+import * as d3 from 'd3'
+
+import { Charts } from 'client/pages/Section/DataTable/Chart/charts'
+import { RecordTrendData, Trends } from 'client/pages/Section/DataTable/Chart/types'
+
+import { _getLegendTrendClassName } from '../_getLegendTrendClassName'
+
+type Props = {
+  containerRef: RefObject<HTMLDivElement>
+  trends: Trends
+  trendsData: RecordTrendData
+}
+
+export const useLegendTransition = (props: Props): void => {
+  const { containerRef, trends, trendsData } = props
+
+  useLayoutEffect(() => {
+    trends.forEach((trend) => {
+      const { name: trendName } = trend
+      const trendData = trendsData[trendName]
+      const hasData = Charts.hasData({ trendsData: { [trendName]: trendData } })
+      const className = _getLegendTrendClassName({ trendName })
+
+      d3.select(containerRef.current)
+        .select(`.${className}`)
+        .transition()
+        .ease(d3.easePolyOut)
+        .duration(Charts.transitionDuration)
+        .style('width', hasData ? 'auto' : '0')
+        .style('opacity', hasData ? '1' : '0')
+    })
+  }, [containerRef, trends, trendsData])
+}
