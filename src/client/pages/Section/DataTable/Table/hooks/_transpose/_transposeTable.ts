116471b8111f554e56ea074292254193127e35ad
Mino Togna <mino.togna@gmail.com>
2025-03-19 10:47:49 +0100

    4468: PDF: Transposed table headers adjustment

--- src/client/pages/Section/DataTable/Table/hooks/_transpose/_transposeTable.ts
+++ src/client/pages/Section/DataTable/Table/hooks/_transpose/_transposeTable.ts
@@ -1,12 +1,19 @@
 import { Objects } from 'utils/objects'
 
+import { TableName, TableNames } from 'meta/assessment'
+
 import { _transposeData } from './_transposeData'
 import { _transposeHeaders } from './_transposeHeaders'
 import { PropsTranspose, ReturnedTranspose } from './_types'
 
+const customGridTemplateColumns: Record<TableName, string> = {
+  [TableNames.sustainableDevelopment15_2_1_5]: `0.3fr max-content`,
+}
+
 export const transposeTable = (props: PropsTranspose): ReturnedTranspose => {
   const { cycle, table: _table } = props
   const { uuid: cycleUUID } = cycle
+  const { name: tableName } = _table.props
 
   const { headers, rowsHeader } = _transposeHeaders(props)
   const { rowsData } = _transposeData(props)
@@ -14,8 +21,9 @@ export const transposeTable = (props: PropsTranspose): ReturnedTranspose => {
   // update gridTemplateColumns
   const table = Objects.cloneDeep(_table)
   const path = ['props', 'style', cycleUUID, 'gridTemplateColumns']
-  const colsSize = headers.length > 1 ? `minmax(min-content, 1fr)` : `max-content`
-  const gridTemplateColumns = `minmax(100px, max-content) repeat(${headers.length}, ${colsSize})`
+  const gridTemplateColumns =
+    customGridTemplateColumns[tableName] ??
+    `minmax(100px, max-content) repeat(${headers.length}, minmax(min-content, 1fr))`
   Objects.setInPath({ obj: table, path, value: gridTemplateColumns })
 
   return { headers, table, rowsData, rowsHeader }
