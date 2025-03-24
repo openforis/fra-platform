c7577c222ff4c642f3d7a8a3835616fca1b59628
Miró Sorja <ping@mirosorja.com>
2025-03-24 10:00:54 +0200

    4450 - API middleware: Populate base params (#4478)
    * 4450 - API middleware: Populate base params
    
    * 4450 - express.d.ts: extend express request with prop context
    
    * 4450 - middleware/populateBaseParams: route api/*
    
    * 4450 - rename RequestPopulateMiddleware.populateBaseParams to  ApiContextMiddleware.initContext
    
    * 4450 - Move types under server
    
    * 4450 - comments
    
    * typo
    
    * 4450 - only set assessment
    
    * 4450 - apiContext: populate only country from redis
    
    * 4450 - express.d.ts: update types

---
+++ src/server/middleware/apiContext.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from 'express'
+import { Objects } from 'utils/objects'
+
+import { CountryIso } from 'meta/area'
+import { Assessment, AssessmentName, Cycle, CycleName } from 'meta/assessment'
+
+import { CountryRepository } from 'server/repository/assessmentCycle/country'
+
+type BaseType = {
+  assessmentName?: AssessmentName
+  cycleName?: CycleName
+  countryIso?: CountryIso
+}
+
+const initContext = async (req: Request, _: Response, next: NextFunction): Promise<void> => {
+  try {
+    const params = { ...req.params, ...req.query, ...req.body } as BaseType
+    const { assessmentName, cycleName, countryIso } = params
+
+    Objects.setInPath({ obj: req, path: ['context'], value: {} })
+
+    if (assessmentName && cycleName) {
+      // TODO: Fetch and populate from redis
+      // const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
+      // Objects.setInPath({ obj: req, path: ['context', 'assessment'], value: assessment })
+
+      const assessment = { props: { name: assessmentName } } as unknown as Assessment
+      const cycle = { name: cycleName } as Cycle
+
+      if (countryIso) {
+        const country = await CountryRepository.getOne({ assessment, cycle, countryIso })
+        Objects.setInPath({ obj: req, path: ['context', 'country'], value: country })
+      }
+    }
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const ApiContextMiddleware = {
+  initContext,
+}
