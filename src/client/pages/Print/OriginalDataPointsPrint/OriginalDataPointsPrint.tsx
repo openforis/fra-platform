2c96f371827737241dafad03a8b7a24dc34055a8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-04-03 20:05:09 +0400

    4320 - PDF 1b remove Data Sources and National Classifications

--- src/client/pages/Print/OriginalDataPointsPrint/OriginalDataPointsPrint.tsx
+++ src/client/pages/Print/OriginalDataPointsPrint/OriginalDataPointsPrint.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next'
 
 import { Objects } from 'utils/objects'
 
+import { SectionNames } from 'meta/assessment/section'
+
 import { DataCell, DataGrid } from 'client/components/DataGrid'
 import EditorWYSIWYG from 'client/components/EditorWYSIWYG'
 import DataSources from 'client/pages/OriginalDataPoint/components/DataSources'
@@ -25,29 +27,38 @@ const OriginalDataPointsPrint: React.FC<Props> = (props) => {
   if (loading || Objects.isEmpty(originalDataPoints)) return null
 
   const hasDescriptions = originalDataPoints.some((odp) => !Objects.isEmpty(odp.description))
+  const isExtendOfForest = sectionName === SectionNames.extentOfForest
 
   return (
     <div className="print-break-after">
-      <h2 className="headline">{i18n.t('nationalDataPoint.nationalData')}</h2>
+      <h2 className="headline">
+        {isExtendOfForest
+          ? i18n.t('nationalDataPoint.nationalData')
+          : i18n.t('nationalDataPoint.nationalDataReferToSection1a')}
+      </h2>
 
-      <div className="odp__section-print-mode">
-        <h3 className="subhead">{i18n.t('nationalDataPoint.dataSources')}</h3>
-        {originalDataPoints.map((originalDataPoint) => (
-          <DataSources key={originalDataPoint.id} originalDataPoint={originalDataPoint} />
-        ))}
-      </div>
+      {isExtendOfForest && (
+        <>
+          <div className="odp__section-print-mode">
+            <h3 className="subhead">{i18n.t('nationalDataPoint.dataSources')}</h3>
+            {originalDataPoints.map((originalDataPoint) => (
+              <DataSources key={originalDataPoint.id} originalDataPoint={originalDataPoint} />
+            ))}
+          </div>
 
-      <div className="odp__section-print-mode">
-        <h3 className="subhead">{i18n.t('nationalDataPoint.nationalClassifications')}</h3>
-        {originalDataPoints.map((originalDataPoint) => (
-          <NationalClasses key={originalDataPoint.id} canEditData={false} originalDataPoint={originalDataPoint} />
-        ))}
-      </div>
+          <div className="odp__section-print-mode">
+            <h3 className="subhead">{i18n.t('nationalDataPoint.nationalClassifications')}</h3>
+            {originalDataPoints.map((originalDataPoint) => (
+              <NationalClasses key={originalDataPoint.id} canEditData={false} originalDataPoint={originalDataPoint} />
+            ))}
+          </div>
+        </>
+      )}
 
       <div className="odp__section-print-mode print-break-inside-avoid">
         <h3 className="subhead">{i18n.t('nationalDataPoint.reclassificationLabel')}</h3>
         {originalDataPoints.map((originalDataPoint) => {
-          const Component = sectionName === 'extentOfForest' ? ExtentOfForest : ForestCharacteristics
+          const Component = isExtendOfForest ? ExtentOfForest : ForestCharacteristics
           return React.createElement(Component, {
             key: originalDataPoint.id,
             originalDataPoint,
