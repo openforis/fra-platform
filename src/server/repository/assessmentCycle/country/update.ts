cd148b1f06e70f025e2d1b86b30d2b2a4bffac32
Miró Sorja <ping@mirosorja.com>
2025-04-03 15:13:07 +0300

    4525 - repository/Country: update many fields: lastUpdate, lastEdit, lastEditOdp, lastInStatus

--- src/server/repository/assessmentCycle/country/update.ts
+++ src/server/repository/assessmentCycle/country/update.ts
@@ -1,3 +1,5 @@
+import * as pgPromise from 'pg-promise'
+
 import { Country } from 'meta/area'
 import { Assessment } from 'meta/assessment/assessment'
 import { Cycle } from 'meta/assessment/cycle'
@@ -10,25 +12,41 @@ type Props = {
   assessment: Assessment
   cycle: Cycle
   country: Country
+  lastUpdate?: boolean
+  lastEdit?: boolean
+  lastEditOdp?: boolean
+  lastInStatus?: boolean
 }
 
 export const update = async (props: Props, client: BaseProtocol = DB): Promise<Country> => {
-  const { country, assessment, cycle } = props
+  const { country, assessment, cycle, lastUpdate, lastEdit, lastEditOdp, lastInStatus } = props
   const { countryIso } = country
-
-  const assessmentCycleName = Schemas.getNameCycle(assessment, cycle)
-
   const { status, ...countryProps } = country.props
 
-  await client.none(
-    `
-        update ${assessmentCycleName}.country
-        set props = $2,
-            status = $3
-        where country_iso = $1
-    `,
-    [countryIso, countryProps, status]
-  )
+  const pgp = pgPromise()
+  const updateData = {
+    props: countryProps,
+    status,
+  }
 
+  const timestampFields = [
+    { name: `last_in_${status}`, enabled: lastInStatus },
+    { name: 'last_update', enabled: lastUpdate },
+    { name: 'last_edit', enabled: lastEdit },
+    { name: 'last_edit_odp', enabled: lastEditOdp },
+  ]
+
+  const columns = [
+    'props',
+    'status',
+    ...timestampFields.filter(({ enabled }) => enabled).map(({ name }) => ({ name, init: () => 'now()' })),
+  ]
+
+  const cs = new pgp.helpers.ColumnSet(columns, {
+    table: { table: 'country', schema: Schemas.getNameCycle(assessment, cycle) },
+  })
+
+  const query = `${pgp.helpers.update(updateData, cs)} WHERE country_iso = $1`
+  await client.none(query, [countryIso])
   return getOne({ assessment, cycle, countryIso }, client)
 }
