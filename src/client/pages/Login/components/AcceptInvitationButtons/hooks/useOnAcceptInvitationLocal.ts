046b7c723f209d28bb7af58b972ab90f2385b03f
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-04-04 13:10:52 +0400

    4514 - Fix redirect for accept invitation local

--- src/client/pages/Login/components/AcceptInvitationButtons/hooks/useOnAcceptInvitationLocal.ts
+++ src/client/pages/Login/components/AcceptInvitationButtons/hooks/useOnAcceptInvitationLocal.ts
@@ -4,8 +4,9 @@ import { useNavigate } from 'react-router-dom'
 import { Routes } from 'meta/routes'
 
 import { useAppDispatch } from 'client/store'
-import { LoginActions } from 'client/store/login'
+import { LoginActions, useInvitation } from 'client/store/login'
 import { AcceptInvitationFormState } from 'client/store/login/stateType'
+import { useCycleRouteParams } from 'client/hooks/useRouteParams'
 import { isError, LoginValidator } from 'client/pages/Login/utils/LoginValidator'
 
 type Props = {
@@ -23,6 +24,11 @@ export const useOnAcceptInvitationLocal = (props: Props): Returned => {
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
 
+  const { userInvitation } = useInvitation()
+  const { countryIso } = userInvitation
+  const { assessmentName, cycleName } = useCycleRouteParams()
+  const redirectUrl = Routes.Country.generatePath({ assessmentName, countryIso, cycleName })
+
   return useCallback<Returned>(() => {
     const fieldErrors = showPassword2
       ? LoginValidator.invitationValidate(email, password, password2)
@@ -38,8 +44,8 @@ export const useOnAcceptInvitationLocal = (props: Props): Returned => {
         })
       ).then(() => {
         dispatch(LoginActions.resetAcceptInvitationForm())
-        navigate(Routes.Root.path.absolute)
+        navigate(redirectUrl)
       })
     }
-  }, [dispatch, email, invitationUuid, navigate, password, password2, showPassword2])
+  }, [dispatch, email, invitationUuid, navigate, password, password2, redirectUrl, showPassword2])
 }
