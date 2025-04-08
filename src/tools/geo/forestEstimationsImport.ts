58bc1f1f83c2752a2ff59d95d8869c5cdf893e1d
andyarnell <andyarnell@hotmail.com>
2025-04-08 14:58:31 +0200

    adding in new JRC 2000 v2 GEE layer and associated stats table updates

--- src/tools/geo/forestEstimationsImport.ts
+++ src/tools/geo/forestEstimationsImport.ts
@@ -26,6 +26,7 @@ type Response = {
       fa_agreement_hansen10_gte_6: number
       fa_agreement_hansen10_gte_7: number
       fa_agreement_hansen10_gte_8: number
+      fa_agreement_hansen10_gte_9: number
       fa_agreement_hansen20_gte_1: number
       fa_agreement_hansen20_gte_2: number
       fa_agreement_hansen20_gte_3: number
@@ -34,6 +35,7 @@ type Response = {
       fa_agreement_hansen20_gte_6: number
       fa_agreement_hansen20_gte_7: number
       fa_agreement_hansen20_gte_8: number
+      fa_agreement_hansen20_gte_9: number
       fa_agreement_hansen30_gte_1: number
       fa_agreement_hansen30_gte_2: number
       fa_agreement_hansen30_gte_3: number
@@ -42,6 +44,7 @@ type Response = {
       fa_agreement_hansen30_gte_6: number
       fa_agreement_hansen30_gte_7: number
       fa_agreement_hansen30_gte_8: number
+      fa_agreement_hansen30_gte_9: number
       fa_copernicus: number
       fa_esa_2009: number
       fa_esa_2020: number
@@ -52,6 +55,7 @@ type Response = {
       fa_hansen30: number
       fa_jaxa: number
       fa_tandemx: number
+      fa_jrc_2020: number
       fa_copernicus_protected: number
       fa_esa_2009_protected: number
       fa_esa_2020_protected: number
@@ -62,6 +66,7 @@ type Response = {
       fa_hansen30_protected: number
       fa_jaxa_protected: number
       fa_tandemx_protected: number
+      fa_jrc_2020_protected: number
       fra_1a_forestArea: number
       fra_1a_landArea: number
       fra_3b_protected: number
