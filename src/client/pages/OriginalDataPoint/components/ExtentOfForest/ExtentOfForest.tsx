53518447f0efa31a3596cc3503232d5d41a4822f
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2025-04-22 07:54:41 -0500

    4555 - Prevent section title/content splitting (#4580)

--- src/client/pages/OriginalDataPoint/components/ExtentOfForest/ExtentOfForest.tsx
+++ src/client/pages/OriginalDataPoint/components/ExtentOfForest/ExtentOfForest.tsx
@@ -73,7 +73,7 @@ const ExtentOfForest: React.FC<Props> = (props) => {
           />
         </div>
       )}
-      <div className="fra-table__container print-break-inside-avoid">
+      <div className="fra-table__container">
         <div className="fra-table__scroll-wrapper">
           <table ref={tableRef} className="fra-table ">
             <tbody>
