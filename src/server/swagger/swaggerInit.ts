87b1726f5672d2d35bf70b1e22f87cdc50505133
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-04-22 12:46:14 +0200

    4060 - Add country iso dropdown

--- src/server/swagger/swaggerInit.ts
+++ src/server/swagger/swaggerInit.ts
@@ -1,11 +1,38 @@
 import { Express } from 'express'
+import { OpenAPIV3 } from 'openapi-types'
 import * as swaggerJSDoc from 'swagger-jsdoc'
 import * as swaggerUi from 'swagger-ui-express'
 
+import { AssessmentNames } from 'meta/assessment/assessment'
+
 import { swaggerOptions } from 'docs/api/swagger.config'
 
-const swaggerSpec = swaggerJSDoc(swaggerOptions)
+import { AssessmentController } from 'server/controller/assessment'
+import { CountryRepository } from 'server/repository/assessmentCycle/country'
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions) as OpenAPIV3.Document
 
 export const swaggerInit = (app: Express): void => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  app.get('/api-docs/swagger.json', async (_req, res) => {
+    const { assessment, cycle } = await AssessmentController.getOneWithCycle({
+      assessmentName: AssessmentNames.fra,
+      cycleName: '2020',
+    })
+    const countryIsos = await CountryRepository.getCountryIsos({ assessment, cycle })
+
+    const cycleParams = swaggerSpec.components?.parameters
+    if (cycleParams?.countryIso && 'schema' in cycleParams.countryIso) {
+      cycleParams.countryIso.schema = { enum: countryIsos, example: countryIsos[0], type: 'string' }
+    }
+
+    res.json(swaggerSpec)
+  })
+
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerUrl: '/api-docs/swagger.json',
+    })
+  )
 }
