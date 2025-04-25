fe9a4894c4dd964cf8b38f85295779d7a9d77307
Miró Sorja <ping@mirosorja.com>
2025-04-25 10:42:25 +0300

    4541 -  Cycle: Remove redirect logic
    - Redirect handled in Country level

--- src/client/pages/Cycle/Cycle.tsx
+++ src/client/pages/Cycle/Cycle.tsx
@@ -1,31 +1,13 @@
 import './Cycle.scss'
-import React, { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-
-import { useCycle } from 'client/store/assessment'
-import { useUserCycles } from 'client/store/user'
-import { useIsLoginRoute } from 'client/hooks/useIsRoute'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 import { useCountryStatusListener } from './hooks/useCountryStatusListener'
 import { useInitAreas } from './hooks/useInitAreas'
 
 const Cycle: React.FC = () => {
-  const cycle = useCycle()
-  const isLogin = useIsLoginRoute()
-
-  const navigate = useNavigate()
-  const userCycles = useUserCycles()
   useInitAreas()
   useCountryStatusListener()
-
-  // TODO: think later
-  useEffect(() => {
-    // user has no permission to view cycle
-    // If user is accessing login page, do not redirect
-    if (!isLogin && !userCycles?.find((userCycle) => userCycle.id === cycle.id)) {
-      navigate('/')
-    }
-  }, [userCycles, cycle, navigate, isLogin])
 
   return <Outlet />
 }
