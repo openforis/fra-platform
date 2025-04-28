23a77c6a0a2c975a0fd3d24156fb7d7a6494d310
Miró Sorja <ping@mirosorja.com>
2025-04-28 16:43:13 +0300

    4541 - fetch metadata with countryIso

--- src/client/store/metadata/actions/getSections.ts
+++ src/client/store/metadata/actions/getSections.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
-import { Global } from 'meta/area'
+import { AreaCode } from 'meta/area'
 import { AssessmentName } from 'meta/assessment/assessment'
 import { CycleName } from 'meta/assessment/cycle'
 import { Section } from 'meta/assessment/section'
@@ -10,12 +10,12 @@ import { Section } from 'meta/assessment/section'
 type Props = {
   assessmentName: AssessmentName
   cycleName: CycleName
+  countryIso: AreaCode
 }
 
 type Returned = Array<Section>
 
-export const getSections = createAsyncThunk<Returned, Props>('metadata/sections/get', async (props) => {
-  const params = { ...props, countryIso: Global.WO }
+export const getSections = createAsyncThunk<Returned, Props>('metadata/sections/get', async (params) => {
   const { data } = await axios.get(ApiEndPoint.MetaData.sections(), { params })
   return data
 })
