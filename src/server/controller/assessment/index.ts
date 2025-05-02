5a4eae2ee02f21eb3c6b533e25e9d7792c857cdc
Miró Sorja <ping@mirosorja.com>
2025-05-02 09:30:37 +0300

    4541 - Publish integration test cycle

--- src/server/controller/assessment/index.ts
+++ src/server/controller/assessment/index.ts
@@ -1,4 +1,5 @@
 import { AssessmentRepository } from 'server/repository/assessment/assessment'
+import { CycleRepository } from 'server/repository/assessmentCycle/cycle'
 
 import { cloneCycle } from './cloneCycle'
 import { create } from './create'
@@ -26,6 +27,7 @@ export const AssessmentController = {
   removeCycle,
   renameCycle,
   publishCycle,
+  updateCycle: CycleRepository.update,
 
   // data cache
   generateDataCache,
