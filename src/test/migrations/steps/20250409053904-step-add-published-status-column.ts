834b86d7de592f4bddccab5b8054d77a11815256
Miró Sorja <ping@mirosorja.com>
2025-05-02 13:38:36 +0300

    4541 - Use AssessmentController.publishCycle

--- src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
+++ src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
@@ -1,13 +1,14 @@
 import { Promises } from 'utils/promises'
 
-import { CountryStatus } from 'meta/area'
 import { CycleStatus } from 'meta/assessment/cycle'
 
 import { AssessmentController } from 'server/controller/assessment'
 import { CacheController } from 'server/controller/cache'
+import { UserController } from 'server/controller/user'
 import { BaseProtocol, DB, Schemas } from 'server/db'
 
 export default async (client: BaseProtocol) => {
+  const user = await UserController.getOne({ email: 'fra@fao.org' })
   const assessments = await AssessmentController.getAll({}, client)
 
   await Promises.each(assessments, async (assessment) => {
@@ -15,7 +16,7 @@ export default async (client: BaseProtocol) => {
       const schemaName = Schemas.getNameCycle(assessment, cycle)
 
       if (cycle.props.status === CycleStatus.published) {
-        await client.query(`update ${schemaName}.country set status = $1`, [CountryStatus.published])
+        await AssessmentController.publishCycle({ assessment, cycle, allCountries: true, user })
       }
 
       await DB.query(`alter table ${schemaName}.country add column last_in_published timestamptz;`)
