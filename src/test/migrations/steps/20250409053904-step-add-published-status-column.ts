57a153098e865b8ed3b1eef02bf6d51b4c91c803
Miró Sorja <ping@mirosorja.com>
2025-05-02 12:35:40 +0300

    4541 - Publish countries for published cycles

--- src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
+++ src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
@@ -1,5 +1,8 @@
 import { Promises } from 'utils/promises'
 
+import { CountryStatus } from 'meta/area'
+import { CycleStatus } from 'meta/assessment/cycle'
+
 import { AssessmentController } from 'server/controller/assessment'
 import { CacheController } from 'server/controller/cache'
 import { BaseProtocol, DB, Schemas } from 'server/db'
@@ -10,6 +13,15 @@ export default async (client: BaseProtocol) => {
   await Promises.each(assessments, async (assessment) => {
     return Promises.each(assessment.cycles, async (cycle) => {
       const schemaName = Schemas.getNameCycle(assessment, cycle)
+
+      if (cycle.props.status === CycleStatus.published) {
+        await client.query(
+          `
+            update ${schemaName}.country set status = $1 where status = $2
+        `,
+          [CountryStatus.published, CountryStatus.accepted]
+        )
+      }
 
       await DB.query(`alter table ${schemaName}.country add column last_in_published timestamptz;`)
       await CacheController.generateArea({ assessment, cycle }, client)
