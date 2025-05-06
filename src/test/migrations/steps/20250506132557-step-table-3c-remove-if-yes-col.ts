e5c17df3e4e61a627c94f991bc58cce1b770af20
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2025-05-06 14:32:47 -0500

    4619 - PDF - Remove table 3c 'if yes' col (#4649)
    * 4619 - Remove table 3c 'if yes' col
    
    * 4619 - Remove col for fra latest
    
    * 4619 - Simplify jsonb set & delete col
    
    * 4619 - Update comment

---
+++ src/test/migrations/steps/20250506132557-step-table-3c-remove-if-yes-col.ts
@@ -0,0 +1,78 @@
+import { AssessmentNames } from 'meta/assessment/assessment'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { BaseProtocol, Schemas } from 'server/db'
+
+const tableName = 'forestRestoration'
+
+const removeFraForestRestorationIfYesColumn = async (props: { cycleName: string }, client: BaseProtocol) => {
+  const { cycleName } = props
+  const assessmentName = AssessmentNames.fra
+
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    {
+      assessmentName,
+      cycleName,
+    },
+    client
+  )
+
+  const cycleUuid = cycle.uuid
+  const schemaAssessment = Schemas.getName(assessment)
+
+  // 1. Update gridTemplateColumns
+  const gridTemplateColumns = 'minmax(min-content, 1fr) minmax(min-content, 2fr)'
+  await client.query(
+    `update ${schemaAssessment}.table
+     set props = jsonb_set(
+       props,
+       '{style,${cycleUuid}}',
+       $1::jsonb,
+       true
+     )
+     where props->>'name' = '${tableName}'
+    `,
+    [JSON.stringify({ gridTemplateColumns })]
+  )
+
+  // 2. Set header colSpan to 1
+  await client.query(
+    `update ${schemaAssessment}.col c
+     set props = jsonb_set(props, '{style,${cycleUuid},colSpan}', '1', true)
+     where c.props ->> 'colType' = 'header'
+       and c.props ->> 'index' = 'header_0'
+       and c.row_id in (
+         select r.id from ${schemaAssessment}.row r
+         left join ${schemaAssessment}."table" t on t.id = r.table_id
+         where r.props ->> 'type' = 'data'
+       and r.props ->> 'index' = '0'
+       and t.props ->> 'name' = '${tableName}'
+     )
+    `
+  )
+}
+
+export default async (client: BaseProtocol) => {
+  const cycles = ['2025', 'latest']
+  await Promise.all(cycles.map((cycleName) => removeFraForestRestorationIfYesColumn({ cycleName }, client)))
+  const assessment = await AssessmentController.getOne({ assessmentName: AssessmentNames.fra }, client)
+
+  // 3. Delete the "if yes" header column for the assessment
+  const schemaAssessment = Schemas.getName(assessment)
+  await client.query(
+    `delete from ${schemaAssessment}.col c
+     where c.props->>'colType' = 'header'
+       and c.props->>'index' = 'header_0'
+       and c.row_id in (
+         select r.id
+         from ${schemaAssessment}.row r
+         left join ${schemaAssessment}."table" t on t.id = r.table_id
+         where r.props->>'type' = 'data'
+           and r.props->>'index' = '1'
+           and t.props->>'name' = '${tableName}'
+       )
+    `
+  )
+
+  await AssessmentController.generateMetadataCache({ assessment }, client)
+}
