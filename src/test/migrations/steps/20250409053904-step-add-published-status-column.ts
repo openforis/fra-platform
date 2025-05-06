5556cd9821719af9fff1452b2f6e1659710426d8
Miró Sorja <ping@mirosorja.com>
2025-05-06 11:05:57 +0300

    4541 - Fix migration

--- src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
+++ src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
@@ -5,15 +5,24 @@ import { CycleStatus } from 'meta/assessment/cycle'
 
 import { AssessmentController } from 'server/controller/assessment'
 import { CacheController } from 'server/controller/cache'
-import { BaseProtocol, DB, Schemas } from 'server/db'
+import { BaseProtocol, Schemas } from 'server/db'
 
 export default async (client: BaseProtocol) => {
   const assessments = await AssessmentController.getAll({}, client)
 
+  // 1. Add the column to all schemas
   await Promises.each(assessments, async (assessment) => {
     return Promises.each(assessment.cycles, async (cycle) => {
       const schemaName = Schemas.getNameCycle(assessment, cycle)
-      await DB.query(`alter table ${schemaName}.country add column last_in_published timestamptz;`)
+      await client.query(`alter table ${schemaName}.country add column last_in_published timestamptz;`)
+    })
+  })
+
+  // 2. Update data and generate cache for each cycle
+  // N.B. When populating data, we expect _all assessment schemas_ to have above column
+  await Promises.each(assessments, async (assessment) => {
+    return Promises.each(assessment.cycles, async (cycle) => {
+      const schemaName = Schemas.getNameCycle(assessment, cycle)
 
       if (cycle.props.status === CycleStatus.published) {
         await client.query(
