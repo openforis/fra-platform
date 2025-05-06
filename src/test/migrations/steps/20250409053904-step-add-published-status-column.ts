9deff00f5b3fb436f903c730cf7021898cc73fdb
Miró Sorja <ping@mirosorja.com>
2025-05-06 11:02:50 +0200

    4541 - Fix migration (#4647)
    * 4541 - Fix migration
    
    * 4541 - Further separation of concerns
    
    * 4541 - use DB

--- src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
+++ src/test/migrations/steps/20250409053904-step-add-published-status-column.ts
@@ -10,12 +10,19 @@ import { BaseProtocol, DB, Schemas } from 'server/db'
 export default async (client: BaseProtocol) => {
   const assessments = await AssessmentController.getAll({}, client)
 
+  // 1. Add the column to all schemas
   await Promises.each(assessments, async (assessment) => {
     return Promises.each(assessment.cycles, async (cycle) => {
       const schemaName = Schemas.getNameCycle(assessment, cycle)
       await DB.query(`alter table ${schemaName}.country add column last_in_published timestamptz;`)
+    })
+  })
 
+  // 2. Update data
+  await Promises.each(assessments, async (assessment) => {
+    return Promises.each(assessment.cycles, async (cycle) => {
       if (cycle.props.status === CycleStatus.published) {
+        const schemaName = Schemas.getNameCycle(assessment, cycle)
         await client.query(
           `
           update ${schemaName}.country
@@ -25,7 +32,13 @@ export default async (client: BaseProtocol) => {
           [cycle.props.datePublished, CountryStatus.published]
         )
       }
+    })
+  })
 
+  // 3. Generate cache for each cycle
+  // N.B. When populating cache, we expect _all assessment cycle schemas_ to have above column
+  await Promises.each(assessments, async (assessment) => {
+    return Promises.each(assessment.cycles, async (cycle) => {
       await CacheController.generateArea({ assessment, cycle }, client)
     })
   })
