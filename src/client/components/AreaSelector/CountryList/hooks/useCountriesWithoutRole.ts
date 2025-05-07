b5b73b917cf8d2e3bb4b1baa40de4456a01de8b9
Miró Sorja <ping@mirosorja.com>
2025-05-07 13:03:50 +0300

    4541 - Fix user countries

--- src/client/components/AreaSelector/CountryList/hooks/useCountriesWithoutRole.ts
+++ src/client/components/AreaSelector/CountryList/hooks/useCountriesWithoutRole.ts
@@ -1,23 +1,31 @@
 import { useMemo } from 'react'
 
 import { Areas, CountryIso } from 'meta/area'
+import { Cycles } from 'meta/assessment/cycles'
 
 import { useCountries } from 'client/store/area'
+import { useCycle } from 'client/store/assessment'
 import { useUser, useUserCountries } from 'client/store/user'
 
 export const useCountriesWithoutRole = (userCountries: boolean): Array<CountryIso> => {
+  const cycle = useCycle()
   const user = useUser()
 
   const _userCountries = useUserCountries()
   const allCountries = useCountries()
+
   return useMemo(() => {
-    if (userCountries && user) {
+    const isCyclePublished = Cycles.isPublished(cycle)
+    const allCountryISOs = allCountries.map(({ countryIso }) => countryIso)
+
+    if (userCountries && user && !isCyclePublished) {
       return _userCountries
     }
-    if (userCountries && !user) {
-      return allCountries.map(({ countryIso }) => countryIso).filter((countryIso) => !Areas.isAtlantis(countryIso))
+
+    if (userCountries) {
+      return allCountryISOs.filter((countryIso) => !Areas.isAtlantis(countryIso))
     }
 
-    return allCountries.map(({ countryIso }) => countryIso)
-  }, [_userCountries, allCountries, user, userCountries])
+    return allCountryISOs
+  }, [_userCountries, allCountries, cycle, user, userCountries])
 }
