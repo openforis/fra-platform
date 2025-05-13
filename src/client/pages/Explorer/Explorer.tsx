aae5be189d505f646ead6a86749b3cd795d97d16
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-12 19:23:17 -0500

    4565 - Add empty Explorer component

---
+++ src/client/pages/Explorer/Explorer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Explorer: React.FC = () => {
+  return (
+    <div className="app-view__content">
+      <h1>Explorer</h1>
+    </div>
+  )
+}
+
+export default Explorer
