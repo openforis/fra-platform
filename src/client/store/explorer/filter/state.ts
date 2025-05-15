d9282ae74d6f5abd7742de7a40b2e14e9e777e7f
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-15 13:03:18 -0500

    4205 - Add section to filter state

--- src/client/store/explorer/filter/state.ts
+++ src/client/store/explorer/filter/state.ts
@@ -1,5 +1,6 @@
 import { AssessmentName } from 'meta/assessment/assessment'
 import { CycleName } from 'meta/assessment/cycle'
+import { SectionName } from 'meta/assessment/section'
 import { ExplorerFilter } from 'meta/explorer/filter'
 
-export type ExplorerFilterState = Record<AssessmentName, Record<CycleName, ExplorerFilter>>
+export type ExplorerFilterState = Record<AssessmentName, Record<CycleName, Record<SectionName, ExplorerFilter>>>
