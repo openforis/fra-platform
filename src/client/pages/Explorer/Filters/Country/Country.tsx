639fc3380954f1e5e334bd862133ffb7e4d2a6a8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-19 11:27:47 -0500

    4205 - Filter explorer countries based on home countries

--- src/client/pages/Explorer/Filters/Country/Country.tsx
+++ src/client/pages/Explorer/Filters/Country/Country.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { CountryIso } from 'meta/area'
 
 import { useAppDispatch } from 'client/store'
 import { ExplorerFilterActions } from 'client/store/explorer/filter/actions'
 import { useExplorerCountries } from 'client/store/explorer/filter/hooks/useExplorerCountries'
+import { useHomeCountriesFilter } from 'client/store/ui/home'
 import { useCycleRouteParams } from 'client/hooks/useRouteParams'
 import CountryMultiSelect from 'client/components/CountryMultiSelect'
 
 const Country: React.FC = () => {
   const { assessmentName, cycleName } = useCycleRouteParams()
   const dispatch = useAppDispatch()
+  const homeCountriesFilter = useHomeCountriesFilter()
+  const allowedCountries = homeCountriesFilter?.length > 0 ? homeCountriesFilter : undefined
 
   const explorerCountries = useExplorerCountries(assessmentName, cycleName)
+
+  useEffect(() => {
+    dispatch(ExplorerFilterActions.setCountries({ assessmentName, countries: [], cycleName }))
+  }, [assessmentName, cycleName, dispatch, homeCountriesFilter])
 
   const handleChange = (value: Array<CountryIso>) => {
     dispatch(ExplorerFilterActions.setCountries({ assessmentName, countries: value, cycleName }))
   }
 
-  return <CountryMultiSelect onChange={handleChange} value={explorerCountries} />
+  return <CountryMultiSelect allowedCountries={allowedCountries} onChange={handleChange} value={explorerCountries} />
 }
 
 export default Country
