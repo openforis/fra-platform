bff0d971475d5c3f36af8d104de22cb8d03e9e8a
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2025-05-19 10:20:34 -0500

    4205 - Data Explorer UI - Database setup (#4693)
    * 4205 - Add database setup
    
    * 4205 - Remove schema name from index
    
    * 4205 - Insert systems of measurement using seed file
    
    * 4205 - Rename public migration
    
    * 4205 - Add name indexes

---
+++ src/tools/migrations/public/steps/20250515205949-step-init-measurement-schema.ts
@@ -0,0 +1,66 @@
+import { Objects } from 'utils/objects'
+
+import { UUID } from 'meta/uuid'
+
+import { BaseProtocol, DB } from 'server/db'
+import { SystemOfMeasurementRepository } from 'server/repository/measurement/systemOfMeasurement'
+import { UnitRepository } from 'server/repository/measurement/unit'
+import { DDL } from 'server/repository/public/ddl'
+import { Logger } from 'server/utils/logger'
+
+import { systemsOfMeasurement } from './data/systemsOfMeasurement'
+
+const client: BaseProtocol = DB
+
+export default async () => {
+  // 0. Avoid re-running
+  const { exists } = await client.one<{ exists: boolean }>(
+    `select exists(select 1 from information_schema.schemata where schema_name = 'measurement') as exists;`
+  )
+  if (exists) {
+    Logger.info('Measurement schema already initialized')
+    return
+  }
+
+  // 1. Create measurement schema and tables
+  await client.query(DDL.getCreateMeasurementSchemaDDL())
+
+  // 2. Insert units and system of measurements
+  const insertTasks = Object.entries(systemsOfMeasurement).map(async ([systemOfMeasurementName, system]) => {
+    Logger.info(`Inserting ${system.units.length} units for system "${systemOfMeasurementName}"…`)
+
+    const dbUnits = await UnitRepository.massiveInsert({ units: system.units }, client)
+
+    const baseUnit = system.units.find((u) => u.conversionFactor === 1)
+
+    if (Objects.isEmpty(baseUnit)) {
+      return
+    }
+
+    const { name: baseUnitName } = baseUnit
+    const dbBaseUnit = dbUnits.find((u) => u.name === baseUnitName)
+
+    const conversionFactors = dbUnits.reduce<Record<UUID, number>>((acc, dbUnit) => {
+      const unitDef = system.units.find((u) => u.name === dbUnit.name)
+      const conversionFactor = unitDef?.conversionFactor
+      if (!Objects.isEmpty(conversionFactor)) {
+        acc[dbUnit.uuid] = unitDef.conversionFactor
+      }
+      return acc
+    }, {})
+
+    Logger.info(`Inserting system of measurement "${systemOfMeasurementName}"…`)
+    await SystemOfMeasurementRepository.create(
+      {
+        baseUnitUUID: dbBaseUnit.uuid,
+        conversionFactors,
+        name: systemOfMeasurementName,
+      },
+      client
+    )
+  })
+
+  await Promise.all(insertTasks)
+
+  Logger.info('Measurement schema initialized')
+}
