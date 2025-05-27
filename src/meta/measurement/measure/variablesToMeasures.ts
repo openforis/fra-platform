21cb9c1b7428fe614bd22a10c96f83edfece0d02
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-26 22:26:36 -0500

    4205 - Unique measure names

---
+++ src/meta/measurement/measure/variablesToMeasures.ts
@@ -0,0 +1,78 @@
+import { TableName } from 'meta/assessment/table'
+import { VariableName } from 'meta/assessment/variable'
+import { MeasureName } from 'meta/measurement/measure'
+
+// Variables omitted from this map will default to using their own names as measure names
+export const variableToMeasures: Record<TableName, Record<VariableName, MeasureName>> = {
+  areaAffectedByFire: {
+    of_which_on_forest: 'ofWhichOnForest',
+    total_land_area_affected_by_fire: 'totalLandAreaAffectedByFire',
+  },
+
+  areaOfPermanentForestEstate: {
+    area_of_permanent_forest_estate: 'areaOfPermanentForestEstate',
+  },
+
+  disturbances: {
+    other: 'disturbancesOtherArea',
+    severe_weather_events: 'severeWeatherEvents',
+    total: 'disturbancesTotalArea',
+  },
+
+  forestAreaWithinProtectedAreas: {
+    forest_area_with_long_term_management_plan: 'forestAreaWithLongTermManagementPlan',
+    forest_area_within_protected_areas: 'forestAreaWithinProtectedAreas',
+    of_which_in_protected_areas: 'ofWhichInProtectedAreas',
+  },
+
+  forestOwnership: {
+    of_which_by_communities: 'ofWhichByCommunities',
+    of_which_by_individuals: 'ofWhichByIndividuals',
+    of_which_by_private_businesses: 'ofWhichByPrivateBusinesses',
+    other_or_unknown: 'otherOrUnknown',
+    other: 'forestOwnershipOtherArea',
+    private_ownership: 'privateOwnership',
+    public_ownership: 'publicOwnership',
+    total: 'forestOwnershipTotalArea',
+    unknown: 'forestOwnershipUnknownArea',
+  },
+
+  holderOfManagementRights: {
+    other: 'holderOfManagementRightsOtherArea',
+    private_businesses: 'privateBusinesses',
+    public_administration: 'publicAdministration',
+    unknown: 'holderOfManagementRightsUnknownArea',
+  },
+
+  otherLandWithTreeCover: {
+    other: 'otherLandWithTreeCoverOtherArea',
+    tree_orchards: 'treeOrchards',
+    trees_in_urban_settings: 'treesInUrbanSettings',
+  },
+
+  primaryDesignatedManagementObjective: {
+    conservation_of_biodiversity: 'primaryDesignatedManagementObjectiveConservationOfBiodiversity',
+    multiple_use: 'multipleUse',
+    no_designation: 'noDesignation',
+    no_unknown: 'noUnknown',
+    other: 'primaryDesignatedManagementObjectiveOtherArea',
+    production: 'primaryDesignatedManagementObjectiveProductionArea',
+    protection_of_soil_and_water: 'primaryDesignatedManagementObjectiveProtectionOfSoilAndWater',
+    social_services: 'primaryDesignatedManagementObjectiveSocialServices',
+    unknown: 'primaryDesignatedManagementObjectiveUnknown',
+  },
+
+  specificForestCategories: {
+    primary_forest: 'primaryForest',
+    rubber_wood: 'rubberWood',
+    temporarily_unstocked: 'temporarilyUnstocked',
+  },
+
+  totalAreaWithDesignatedManagementObjective: {
+    conservation_of_biodiversity: 'totalAreaWithDesignatedManagementObjectiveConservationOfBiodiversity',
+    other: 'totalAreaWithDesignatedManagementObjectiveOtherArea',
+    production: 'totalAreaWithDesignatedManagementObjectiveProductionArea',
+    protection_of_soil_and_water: 'totalAreaWithDesignatedManagementObjectiveProtectionOfSoilAndWater',
+    social_services: 'totalAreaWithDesignatedManagementObjectiveSocialServices',
+  },
+}
--- src/tools/createMeasuresDimensions/index.ts
+++ src/tools/createMeasuresDimensions/index.ts
@@ -1,13 +1,20 @@
 import 'tsconfig-paths/register'
 import 'dotenv/config'
 
+import * as pgPromise from 'pg-promise'
 import { ToolsUtils } from 'tools/utils/toolsUtils'
+import { Objects } from 'utils/objects'
 
 import { AssessmentNames } from 'meta/assessment/assessment'
+import { TableName } from 'meta/assessment/table'
+import { VariableName } from 'meta/assessment/variable'
+import { Measures } from 'meta/measurement/measures'
+import { SystemOfMeasurementName } from 'meta/measurement/systemOfMeasurement'
 
 import { AssessmentController } from 'server/controller/assessment'
 import { CacheController } from 'server/controller/cache'
 import { BaseProtocol, DB, Schemas } from 'server/db'
+import { SystemOfMeasurementRepository } from 'server/repository/measurement/systemOfMeasurement'
 
 const areaBasedTables = [
   'areaAffectedByFire',
@@ -34,31 +41,43 @@ const createMeasuresDimensions = async () => {
   )
 
   const schemaAssessment = Schemas.getName(assessment)
-  const areaBasedTablesString = areaBasedTables.map((n) => `'${n}'`).join(', ')
+
+  const { uuid: areaSystemOfMeasurementUuid } = await SystemOfMeasurementRepository.getOne(
+    { systemOfMeasurementName: SystemOfMeasurementName.area },
+    client
+  )
 
   // 1. Migrate area based measures
-  await client.query(
+  const variableNames = await client.map<{ tableName: TableName; variableName: VariableName }>(
     `
-    insert into measurement.measure (name, system_uuid)
-    select v.name, som.uuid
-    from (
-      select distinct
-        r.props ->> 'variableName' as name
-      from ${schemaAssessment}.row r
-      join ${schemaAssessment}."table" t
-        on t.id = r.table_id
-      where t.props ->> 'name' in (${areaBasedTablesString})
-        and r.props ->> 'variableName' is not null
-        and r.props ->> 'variableName' <> ''
-    ) as v
-    cross join (
-      select som.uuid
-      from measurement.system_of_measurement som
-      where som.name = 'area'
-    ) as som
-    on conflict (name) do nothing;
-    `
+    select distinct t.props ->> 'name' as table_name, r.props ->> 'variableName' AS variable_name
+    from ${schemaAssessment}.row r
+    join ${schemaAssessment}."table" t
+      on t.id = r.table_id
+    where t.props ->> 'name' in ($1:csv)
+      and r.props ->> 'variableName' is not null
+      and r.props ->> 'variableName' <> ''
+    `,
+    [areaBasedTables],
+    (res) => Objects.camelize(res)
   )
+
+  const measures = variableNames.map(({ tableName, variableName }) => {
+    const name = Measures.variableNameToMeasureName(tableName, variableName)
+    return { name, system_uuid: areaSystemOfMeasurementUuid }
+  })
+
+  const pgp = pgPromise()
+  const columns = [
+    { name: 'name', prop: 'name' },
+    { name: 'system_uuid', prop: 'system_uuid' },
+  ]
+  const table = { table: 'measure', schema: 'measurement' }
+  const cs = new pgp.helpers.ColumnSet(columns, { table })
+
+  const insertMeasures = `${pgp.helpers.insert(measures, cs)} on conflict (name) do nothing;`
+
+  await client.query(insertMeasures)
 
   // 2. Migrate area based dimensions
   await client.query(
@@ -71,10 +90,11 @@ const createMeasuresDimensions = async () => {
       jsonb_each(t.props -> 'columnNames') as cn(cycle_uuid, names)
     cross join lateral
       jsonb_array_elements_text(cn.names) as dim(name)
-    where t.props ->> 'name' in (${areaBasedTablesString})
+    where t.props ->> 'name' in ($1:csv)
       and t.props -> 'columnNames' is not null
     on conflict (name) do nothing;
-    `
+    `,
+    [areaBasedTables]
   )
 
   await CacheController.generateExplorerMetadata({ assessment, cycle }, client)
