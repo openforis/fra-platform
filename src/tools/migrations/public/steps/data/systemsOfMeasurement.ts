137ec61a9f0cb10668a946df20713710da842c5b
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-27 14:49:53 -0500

    4205 - Add new systems of measurement to seed

--- src/tools/migrations/public/steps/data/systemsOfMeasurement.ts
+++ src/tools/migrations/public/steps/data/systemsOfMeasurement.ts
@@ -1,17 +1,19 @@
+import { SystemOfMeasurementName } from 'meta/measurement/systemOfMeasurement'
 import { Unit } from 'meta/measurement/unit'
 
 type UnitWithFactor = Omit<Unit, 'uuid'> & {
   conversionFactor?: number
 }
 
-type SystemOfMeasurementSeed = {
-  [systemOfMeasurementName: string]: {
+type SystemOfMeasurementSeed = Record<
+  SystemOfMeasurementName,
+  {
     units: Array<UnitWithFactor>
   }
-}
+>
 
 export const systemsOfMeasurement: SystemOfMeasurementSeed = {
-  area: {
+  [SystemOfMeasurementName.area]: {
     units: [
       { name: 'haThousand', symbol: '1000 ha', conversionFactor: 1 },
       { name: 'ha', symbol: 'ha', conversionFactor: 1000 },
@@ -21,5 +23,14 @@ export const systemsOfMeasurement: SystemOfMeasurementSeed = {
       { name: 'acre', symbol: 'ac', conversionFactor: 2471.05 },
       { name: 'haMillion', symbol: '1000000 ha', conversionFactor: 0.001 },
     ],
+  },
+  [SystemOfMeasurementName.areaPerYear]: {
+    units: [{ name: 'haThousandPerYear', symbol: '1000ha/year', conversionFactor: 1 }],
+  },
+  [SystemOfMeasurementName.volume]: {
+    units: [{ name: 'millionCubicMeter', symbol: '1000000m³', conversionFactor: 1 }],
+  },
+  [SystemOfMeasurementName.volumePerArea]: {
+    units: [{ name: 'cubicMeterPerHa', symbol: 'm³/ha', conversionFactor: 1 }],
   },
 }
