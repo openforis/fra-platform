3f5e21f8668357c3dd88aab7031515bf861bd996
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-29 17:40:15 -0500

    4205 - Map column names to dimensions init/cache

--- src/server/repository/measurement/dimension/getTableDimensions.ts
+++ src/server/repository/measurement/dimension/getTableDimensions.ts
@@ -2,6 +2,7 @@ import { Objects } from 'utils/objects'
 
 import { Assessment } from 'meta/assessment/assessment'
 import { Dimension } from 'meta/measurement/dimension'
+import { Dimensions } from 'meta/measurement/dimensions'
 
 import { BaseProtocol, DB, Schemas } from 'server/db'
 
@@ -14,9 +15,10 @@ export const getTableDimensions = async (props: Props, client: BaseProtocol = DB
   const { assessment, tableName } = props
   const schemaName = Schemas.getName(assessment)
 
-  return client.map<Dimension>(
+  const columnNames = await client.map<string>(
     `
-    with extracted_dimension_names as (
+    select distinct name
+    from (
       select distinct jsonb_array_elements_text(cn.names) as name
       from ${schemaName}."table" t,
            jsonb_each(t.props -> 'columnNames') as cn(cycle_uuid, names)
@@ -29,13 +31,23 @@ export const getTableDimensions = async (props: Props, client: BaseProtocol = DB
            jsonb_array_elements(cea.arr) as elem
       where t.props ->> 'name' = $1
         and t.props -> 'cellsExportAlways' is not null
-    )
-
-    select distinct d.*
-    from extracted_dimension_names edn
-    join measurement.dimension d on d.name = edn.name;
+    ) raw;
     `,
     [tableName],
+    (row) => row.name
+  )
+
+  if (columnNames.length === 0) return []
+
+  const dimensionNames = columnNames.map((cn) => Dimensions.columnNameToDimensionName(tableName, cn))
+
+  return client.map<Dimension>(
+    `
+      select distinct *
+      from measurement.dimension
+      where name in ($1:csv);
+    `,
+    [dimensionNames],
     (dimension) => Objects.camelize(dimension)
   )
 }
