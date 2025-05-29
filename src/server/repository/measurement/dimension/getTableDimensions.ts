548cefc91f68ee2cee44c67e5ba7cf1efe2622c5
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-05-29 18:06:37 -0500

    4205 - Remove outer Select in getTableDimensions

--- src/server/repository/measurement/dimension/getTableDimensions.ts
+++ src/server/repository/measurement/dimension/getTableDimensions.ts
@@ -17,24 +17,21 @@ export const getTableDimensions = async (props: Props, client: BaseProtocol = DB
 
   const columnNames = await client.map<string>(
     `
-    select distinct name
-    from (
-      select distinct jsonb_array_elements_text(cn.names) as name
-      from ${schemaName}."table" t,
-           jsonb_each(t.props -> 'columnNames') as cn(cycle_uuid, names)
-      where t.props ->> 'name' = $1
-        and t.props -> 'columnNames' is not null
-      union
-      select distinct elem ->> 'columnName' as name
-      from ${schemaName}."table" t,
-           jsonb_each(t.props -> 'cellsExportAlways') as cea(cycle_uuid, arr),
-           jsonb_array_elements(cea.arr) as elem
-      where t.props ->> 'name' = $1
-        and t.props -> 'cellsExportAlways' is not null
-    ) raw;
-    `,
+    select jsonb_array_elements_text(cn.names) as column_name
+    from ${schemaName}."table" t,
+         jsonb_each(t.props -> 'columnNames') as cn(cycle_uuid, names)
+    where t.props ->> 'name' = $1
+      and t.props -> 'columnNames' is not null
+    union
+    select elem ->> 'columnName' as column_name
+    from ${schemaName}."table" t,
+         jsonb_each(t.props -> 'cellsExportAlways') as cea(cycle_uuid, arr),
+         jsonb_array_elements(cea.arr) as elem
+    where t.props ->> 'name' = $1
+      and t.props -> 'cellsExportAlways' is not null
+  `,
     [tableName],
-    (row) => row.name
+    (row) => row.column_name
   )
 
   if (columnNames.length === 0) return []
