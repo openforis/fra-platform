3bf76320d597955ce5d96f187a3fe43a52233330
Miró Sorja <ping@mirosorja.com>
2025-06-03 09:57:40 +0300

    4771 - Login reducer lazy loaded

--- src/client/pages/Login/Login.tsx
+++ src/client/pages/Login/Login.tsx
@@ -1,10 +1,19 @@
 import './Login.scss'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import { loginReducer } from 'client/store/login'
+import { injectReducer, removeReducer } from 'client/store/store'
 import Partners from 'client/pages/CycleHome/Partners'
 
 const Login: React.FC = () => {
+  useEffect(() => {
+    injectReducer('login', loginReducer)
+    return () => {
+      removeReducer('login')
+    }
+  }, [])
+
   return (
     <div className="login-view">
       <div className="app-view__content">
--- src/client/store/store.ts
+++ src/client/store/store.ts
@@ -1,23 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import createDebounce from 'redux-debounced'
 
 import axiosMiddleware from './middleware/axios'
 import { listenerMiddleware } from './middleware/listener'
 import rootReducer from './rootReducer'
 
+const asyncReducers: Record<string, any> = {}
+
+const createReducer = () =>
+  combineReducers({
+    ...rootReducer,
+    ...asyncReducers,
+  })
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: createReducer(),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().prepend(listenerMiddleware.middleware).concat(createDebounce(), axiosMiddleware),
 })
 
-// injectReducers has TypeScript issues. uncomment code below if injectReducers will be ever needed
-// const asyncReducers: any = {}
+export const injectReducer = (key: string, asyncReducer: any) => {
+  if (!asyncReducers[key]) {
+    asyncReducers[key] = asyncReducer
+    store.replaceReducer(createReducer())
+  }
+}
 
-// export const injectReducers = (name: string, asyncReducer: any) => {
-//   asyncReducers[name] = asyncReducer
-//
-//   store.replaceReducer(combineReducers({ ...rootReducer, ...asyncReducers }))
-// }
+export const removeReducer = (key: string) => {
+  if (asyncReducers[key]) {
+    delete asyncReducers[key]
+    store.replaceReducer(createReducer())
+  }
+}
 
 export default store
