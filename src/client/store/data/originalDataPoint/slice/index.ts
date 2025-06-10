dd6011a4aadb8124922aedd116b181a52ae53fa1
Miró Sorja <ping@mirosorja.com>
2025-06-10 10:25:02 +0300

    4789 - Store Refactor: Data->originalDataPoint

---
+++ src/client/store/data/originalDataPoint/slice/index.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { resetReducer } from 'client/store/data/originalDataPoint/slice/extraReducers/resetReducer'
+import { setOriginalDataPointReducer } from 'client/store/data/originalDataPoint/slice/extraReducers/setOriginalDataPointReducer'
+import { setReservedYearsReducer } from 'client/store/data/originalDataPoint/slice/extraReducers/setReservedYearsReducer'
+import { setUpdatingReducer } from 'client/store/data/originalDataPoint/slice/extraReducers/setUpdatingReducer'
+import { initialState } from 'client/store/data/originalDataPoint/state'
+
+export const OriginalDataPointSlice = createSlice({
+  name: 'originalDataPoint',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    resetReducer(builder)
+    setReservedYearsReducer(builder)
+    setOriginalDataPointReducer(builder)
+    setUpdatingReducer(builder)
+  },
+})
