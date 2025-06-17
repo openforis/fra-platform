5afb78c912733fb80033748ae2243f945230e307
Miró Sorja <ping@mirosorja.com>
2025-06-17 15:37:37 +0300

    4857 - Generic Form component
    Co-authored-by: Mino Togna <mino.togna@gmail.com>

--- src/client/components/InviteUserForm/InviteUserForm.tsx
+++ src/client/components/InviteUserForm/InviteUserForm.tsx
@@ -1,11 +1,11 @@
 import './InviteUserForm.scss'
 import React, { useCallback } from 'react'
-import { useForm } from 'react-hook-form'
+import { FieldErrors, SubmitHandler, useForm, UseFormRegister, UseFormSetValue, UseFormWatch } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
+import { z, ZodOptional, ZodString } from 'zod'
 
 import { CountryIso } from 'meta/area'
 import { LanguageCodes } from 'meta/lang'
@@ -20,6 +20,115 @@ import Select from 'client/components/Inputs/Select'
 import { useInitialState } from 'client/components/InviteUserForm/hooks/initialState'
 import { useOnSubmit } from 'client/components/InviteUserForm/hooks/useOnSubmit'
 import InviteCollaboratorPermissions from 'client/components/InviteUserForm/InviteCollaboratorPermissions'
+
+enum FormType {
+  'text' = 'text',
+}
+
+type FieldProps<T extends Record<string, unknown> = Record<string, unknown>> = {
+  errors: FieldErrors<T>
+  register: UseFormRegister<T>
+  setValue: UseFormSetValue<T>
+  watch: UseFormWatch<T>
+
+  name: string
+  label: string
+  required: boolean
+}
+
+const TextField = (props: FieldProps) => {
+  const { errors, label, name, register, required } = props
+  const { t } = useTranslation()
+
+  return (
+    <DataRow>
+      <DataCell className="form-cell-label" noBorder>
+        <label htmlFor={name}>
+          {t(label)}
+          {required ? '*' : ''}
+        </label>
+      </DataCell>
+      <DataCell editable lastCol lastRow>
+        <InputText id={name} name={name} {...register(name)} />
+        {errors[name] && <div className="form-cell-error">{errors[name].message}</div>}
+      </DataCell>
+    </DataRow>
+  )
+}
+
+const FormFields: Record<FormType, React.FC<FieldProps>> = {
+  [FormType.text]: TextField,
+}
+
+type Props = {
+  onSubmit: SubmitHandler<unknown>
+  onCancel: () => void
+  formDefinition: Array<{ name: string; label: string; validation: ZodString; type: FormType }>
+  defaultValues: object
+}
+
+const Form: React.FC<Props> = ({ defaultValues, formDefinition, onCancel, onSubmit }) => {
+  const { t } = useTranslation()
+
+  const formSchemaObject = formDefinition.reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr.name]: curr.validation,
+    }
+  }, {})
+
+  const formSchema = z.object(formSchemaObject)
+
+  type FormValues = z.infer<typeof formSchema>
+
+  const {
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    register,
+    setValue,
+    watch,
+  } = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  })
+
+  const gridTemplateColumns = '0.3fr 1fr'
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <DataGrid className="form-grid" gridTemplateColumns={gridTemplateColumns}>
+        {formDefinition.map((formField) => {
+          const Component = FormFields[formField.type]
+
+          const schema = formSchema.shape[formField.name as keyof typeof formSchema.shape] as z.ZodTypeAny
+          const isOptional = schema instanceof ZodOptional
+
+          return (
+            <Component
+              errors={errors}
+              label={formField.label}
+              name={formField.name}
+              register={register}
+              required={!isOptional}
+              setValue={setValue}
+              watch={watch}
+            />
+          )
+        })}
+
+        <div className="edit-user__form-item button-container">
+          <button className="btn btn-secondary" onClick={onCancel} type="button">
+            {t('common.cancel')}
+          </button>
+
+          <button className="btn btn-primary" disabled={isSubmitting} type="submit">
+            {isSubmitting ? t('common.submitting') : t('common.submit')}
+          </button>
+        </div>
+      </DataGrid>
+    </form>
+  )
+}
 
 const formSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters.'),
@@ -47,6 +156,8 @@ const InviteUserForm: React.FC = () => {
     navigate(-1)
   }, [navigate])
 
+  const onCancel = handleCancel
+
   const {
     formState: { errors, isSubmitting },
     handleSubmit,
@@ -70,8 +181,29 @@ const InviteUserForm: React.FC = () => {
 
   const gridTemplateColumns = '0.3fr 1fr'
 
+  const permissionsSchema = formSchema.shape.permissions
+  const isOptional = permissionsSchema instanceof ZodOptional
+
+  console.log(isOptional)
+
   return (
     <div className="app-view__content">
+      <Form
+        defaultValues={defaultValues}
+        formDefinition={[
+          {
+            name: 'name',
+            type: FormType.text,
+            validation: z.string().min(2, 'Name must be at least 2 characters.'),
+            label: 'common.name',
+          },
+        ]}
+        onCancel={onCancel}
+        onSubmit={onSubmit}
+      />
+
+      <hr />
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <DataGrid className="form-grid" gridTemplateColumns={gridTemplateColumns}>
           <DataRow>
