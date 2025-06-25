33a541c534faa5d7c02c3970c395a3db8757320b
Mino Togna <mino.togna@gmail.com>
2025-06-25 11:00:25 +0200

    4866: Fix user role adapter

--- src/server/repository/adapter/userRole.ts
+++ src/server/repository/adapter/userRole.ts
@@ -1,11 +1,3 @@
 import { Objects } from 'utils/objects'
 
-import { CollaboratorPermissions, RoleName, UserRole } from 'meta/user'
-
-export const UserRoleAdapter = <N extends RoleName>({ permissions, ...role }: any): UserRole<N> => ({
-  ...Objects.camelize(role),
-  permissions: {
-    ...Objects.camelize(permissions),
-    sections: (permissions as CollaboratorPermissions).sections,
-  },
-})
+export const UserRoleAdapter = Objects.camelize
