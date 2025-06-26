0b9e4bb9b260da10e47bb7c8e5e235d04c54e537
Mino Togna <mino.togna@gmail.com>
2025-06-26 15:46:55 +0200

    Merge: use useGlobalCountries
    Co-authored-by: Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>

--- src/client/pages/Explorer/Filters/Country/Country.tsx
+++ src/client/pages/Explorer/Filters/Country/Country.tsx
@@ -5,14 +5,14 @@ import { CountryIso } from 'meta/area'
 import { ExplorerSelectionActions } from 'client/store/explorer/selection/actions'
 import { useExplorerCountries } from 'client/store/explorer/selection/hooks/countries'
 import { useAppDispatch } from 'client/store/hooks'
-import { useHomeCountriesFilter } from 'client/store/ui/home'
+import { useGlobalCountries } from 'client/store/ui/countryReport/hooks/globalCountries'
 import { useCycleRouteParams } from 'client/hooks/useRouteParams'
 import CountryMultiSelect from 'client/components/CountryMultiSelect'
 
 const Country: React.FC = () => {
   const { assessmentName, cycleName } = useCycleRouteParams()
   const dispatch = useAppDispatch()
-  const homeCountriesFilter = useHomeCountriesFilter()
+  const homeCountriesFilter = useGlobalCountries()
   const allowedCountries = homeCountriesFilter?.length > 0 ? homeCountriesFilter : undefined
 
   const explorerCountries = useExplorerCountries()
