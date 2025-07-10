3b503e0e4f673b389633dbbdb257589d9122a031
Miró Sorja <ping@mirosorja.com>
2025-07-10 12:20:01 +0300

    4872 - RoleRepository: remove getOne

--- src/server/repository/public/userRole/index.ts
+++ src/server/repository/public/userRole/index.ts
@@ -1,12 +1,10 @@
 import { create } from './create'
-import { getOne } from './getOne'
 import { update } from './update'
 import { updateProps } from './updateProps'
 import { updateSectionAuth } from './updateSectionAuth'
 
 export const UserRoleRepository = {
   create,
-  getOne,
   update,
   updateProps,
   updateSectionAuth,
