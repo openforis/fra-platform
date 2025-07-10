43510cfaab189f637b441098f31382c0da8e3129
Miró Sorja <ping@mirosorja.com>
2025-07-10 10:44:27 +0300

    4872 - RoleRepository: getOne

--- src/server/repository/public/userRole/index.ts
+++ src/server/repository/public/userRole/index.ts
@@ -1,10 +1,12 @@
 import { create } from './create'
+import { getOne } from './getOne'
 import { update } from './update'
 import { updateProps } from './updateProps'
 import { updateSectionAuth } from './updateSectionAuth'
 
 export const UserRoleRepository = {
   create,
+  getOne,
   update,
   updateProps,
   updateSectionAuth,
