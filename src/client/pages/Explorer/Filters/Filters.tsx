c4d3120678cdb278d484587db4c5e3f1ede8b35a
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2025-07-11 08:15:07 -0500

    4895 - Data Explorer UI - Side panel (#4905)
    * 4895 - Remove axis selectors from filters
    
    * 4895 - Add new axis selection component
    
    * 4895 - Add options button
    
    * 4895 - Make options button an icon button
    
    * 4895 - Add useCallbacks
    
    * 4895 - Add common.json empty line back

--- src/client/pages/Explorer/Filters/Filters.tsx
+++ src/client/pages/Explorer/Filters/Filters.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 import Hr from 'client/components/Hr/Hr'
 import ExportButton from 'client/pages/Explorer/ExportButton/ExportButton'
-import AxisSelection from 'client/pages/Explorer/Filters/AxisSelection/AxisSelection'
 import Country from 'client/pages/Explorer/Filters/Country/Country'
 import Dimension from 'client/pages/Explorer/Filters/Dimension/Dimension'
 import Measure from 'client/pages/Explorer/Filters/Measure/Measure'
+import Options from 'client/pages/Explorer/Filters/Options/Options'
 import { ExplorerGridProps } from 'client/pages/Explorer/types'
 
 const Filters: React.FC<ExplorerGridProps> = (props: ExplorerGridProps) => {
@@ -19,7 +19,8 @@ const Filters: React.FC<ExplorerGridProps> = (props: ExplorerGridProps) => {
       <Country />
       <Measure />
       <Dimension />
-      <AxisSelection />
+      <Hr vertical />
+      <Options />
     </div>
   )
 }
---
+++ src/client/pages/Explorer/Filters/Options/Options.tsx
@@ -0,0 +1,55 @@
+import './Options.scss'
+import React, { useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import Button, { ButtonSize, ButtonType } from 'client/components/Buttons/Button'
+import Hr from 'client/components/Hr'
+import SlidingPanel from 'client/components/SlidingPanel'
+import AxisSelection from 'client/pages/Explorer/Filters/Options/AxisSelection/AxisSelection'
+
+import { useToggleAxis } from './hooks/useToggleAxis'
+
+const Options: React.FC = () => {
+  const { t } = useTranslation()
+  const [opened, setOpened] = useState(false)
+
+  const { applyAxisSelection, axisSelection, resetAxisSelection, toggleAxis } = useToggleAxis()
+
+  const openPanel = useCallback(() => setOpened(true), [setOpened])
+
+  const closePanel = useCallback(() => setOpened(false), [setOpened])
+
+  const handleCancel = useCallback(() => {
+    resetAxisSelection()
+    closePanel()
+  }, [closePanel, resetAxisSelection])
+
+  const handleApply = useCallback(() => {
+    applyAxisSelection()
+    closePanel()
+  }, [applyAxisSelection, closePanel])
+
+  return (
+    <div>
+      <Button iconName="hit-up" onClick={openPanel} />
+      <SlidingPanel closePanel={closePanel} opened={opened} size={45}>
+        <AxisSelection axisSelection={axisSelection} toggleAxis={toggleAxis} />
+
+        <Hr className="options-hr" />
+
+        <div className="options-actions">
+          <Button iconName="undo" inverse label={t('common.cancel')} onClick={handleCancel} size={ButtonSize.m} />
+          <Button
+            iconName="hit-up"
+            label={t('common.apply')}
+            onClick={handleApply}
+            size={ButtonSize.m}
+            type={ButtonType.primary}
+          />
+        </div>
+      </SlidingPanel>
+    </div>
+  )
+}
+
+export default Options
