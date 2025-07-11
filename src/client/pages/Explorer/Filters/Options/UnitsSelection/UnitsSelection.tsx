9e0e659e14fbab9f0f49b5abd0a413169610b585
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-07-11 18:47:44 -0500

    4895 - Add key prop

--- src/client/pages/Explorer/Filters/Options/UnitsSelection/UnitsSelection.tsx
+++ src/client/pages/Explorer/Filters/Options/UnitsSelection/UnitsSelection.tsx
@@ -27,10 +27,10 @@ const UnitsSelection: React.FC<Props> = (props: Props) => {
       <h2 className="options-title">{t('common.unit')}</h2>
       <div className="units-selection">
         {unitSelectors.map(({ measureName, onChange, options, selectedUnit }) => (
-          <>
+          <React.Fragment key={measureName}>
             <span>{t(Measures.getTName(measureName))}:</span>
             <SelectSecondary isClearable={false} onChange={onChange} options={options} value={selectedUnit} />
-          </>
+          </React.Fragment>
         ))}
       </div>
       <Hr className="options-hr" />
