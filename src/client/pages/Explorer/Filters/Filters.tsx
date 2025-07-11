466429edc64aeebb59fd018e938722571cbf74c5
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-07-10 19:38:19 -0500

    4895 - Remove axis selectors from filters

--- src/client/pages/Explorer/Filters/Filters.tsx
+++ src/client/pages/Explorer/Filters/Filters.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 import Hr from 'client/components/Hr/Hr'
 import ExportButton from 'client/pages/Explorer/ExportButton/ExportButton'
-import AxisSelection from 'client/pages/Explorer/Filters/AxisSelection/AxisSelection'
 import Country from 'client/pages/Explorer/Filters/Country/Country'
 import Dimension from 'client/pages/Explorer/Filters/Dimension/Dimension'
 import Measure from 'client/pages/Explorer/Filters/Measure/Measure'
@@ -19,7 +18,6 @@ const Filters: React.FC<ExplorerGridProps> = (props: ExplorerGridProps) => {
       <Country />
       <Measure />
       <Dimension />
-      <AxisSelection />
     </div>
   )
 }
