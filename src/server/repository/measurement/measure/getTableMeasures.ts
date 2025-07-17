c7df68d7d6891449b592a7e4c62f01a82a62abbd
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-07-17 12:19:25 -0500

    4896 - Preserve measures order

--- src/server/repository/measurement/measure/getTableMeasures.ts
+++ src/server/repository/measurement/measure/getTableMeasures.ts
@@ -19,14 +19,17 @@ export const getTableMeasures = async (props: Props, client: BaseProtocol = DB):
 
   const variableNames = await client.map<string>(
     `
-      select distinct r.props ->> 'variableName' as variable_name
+      select distinct 
+        r.props ->> 'variableName' as variable_name,
+        (r.props ->> 'index')::int as idx
       from ${schemaName}.row r
       join ${schemaName}."table" t
         on t.id = r.table_id
       where t.props ->> 'name' = $1
         and (r.props -> 'cycles') ? $2
         and r.props ->> 'variableName' is not null
-        and r.props ->> 'variableName' <> '';
+        and r.props ->> 'variableName' <> ''
+      order by idx nulls last;
     `,
     [tableName, cycle.uuid],
     (row) => row.variable_name
@@ -38,14 +41,20 @@ export const getTableMeasures = async (props: Props, client: BaseProtocol = DB):
 
   return client.map<Measure>(
     `
-      select distinct
+      with measures as (
+        select *
+        from unnest($1::text[]) with ordinality as measures(name, ord)
+      )
+      select
         m.name,
-        s.name AS "system_name"
-      from measurement.measure m
+        s.name as system_name
+      from measures
+      join measurement.measure m
+        on m.name = measures.name
       left join measurement.system_of_measurement s
         on m.system_uuid = s.uuid
-      where m.name in ($1:csv);
-  `,
+      order by measures.ord
+    `,
     [measureNames],
     (measure) => Objects.camelize(measure)
   )
