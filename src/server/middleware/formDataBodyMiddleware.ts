eb260f9c42df44cb4b5ccc0b33f62a8ac3c6da88
Miró Sorja <ping@mirosorja.com>
2025-08-11 10:25:21 +0300

    4943 - formDataBodyMiddleware: Parse booleans correctly

--- src/server/middleware/formDataBodyMiddleware.ts
+++ src/server/middleware/formDataBodyMiddleware.ts
@@ -5,7 +5,10 @@ export const parseBody: RequestHandler = (req, _res, next) => {
   if (!Objects.isEmpty(req.body)) {
     const body = {}
     Object.entries(req.body).forEach(([key, value]) => {
-      Objects.set(body, key, value)
+      let parsedValue = value
+      if (value === 'true') parsedValue = true
+      if (value === 'false') parsedValue = false
+      Objects.set(body, key, parsedValue)
     })
     // eslint-disable-next-line no-param-reassign
     req.body = body
