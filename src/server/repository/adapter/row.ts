637f61a6fbde77cd2d3ff40864eb0217df57a3ad
Miró Sorja <ping@mirosorja.com>
2025-08-15 14:17:55 +0300

    3701 - Adapter: col/row: sortable, sortableBy

--- src/server/repository/adapter/row.ts
+++ src/server/repository/adapter/row.ts
@@ -15,7 +15,17 @@ export interface RowDB {
 
 export const RowAdapter = (rowDB: RowDB): Row => {
   const {
-    props: { calculateFn, calculateIf, chart, excludeFromDataExport, linkToSection, validateFns, withReview, ...rest },
+    props: {
+      calculateFn,
+      calculateIf,
+      chart,
+      excludeFromDataExport,
+      linkToSection,
+      sortableBy,
+      validateFns,
+      withReview,
+      ...rest
+    },
     ...row
   } = rowDB
 
@@ -28,6 +38,7 @@ export const RowAdapter = (rowDB: RowDB): Row => {
       chart,
       excludeFromDataExport,
       linkToSection,
+      sortableBy,
       validateFns,
       withReview,
     },
