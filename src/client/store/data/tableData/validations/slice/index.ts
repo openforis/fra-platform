0e7039336ef5ce14cd7c3f9f272ec418813a7c90
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-08-25 19:35:10 -0500

    4937 - Add slice names

--- src/client/store/data/tableData/validations/slice/index.ts
+++ src/client/store/data/tableData/validations/slice/index.ts
@@ -3,8 +3,10 @@ import { createSlice } from '@reduxjs/toolkit'
 import { setValidationsReducer } from 'client/store/data/tableData/validations/slice/extraReducers/setValidationsReducer'
 import { initialState } from 'client/store/data/tableData/validations/state'
 
+import { ValidationsReducerName } from './name'
+
 export const ValidationsReducer = createSlice({
-  name: 'validations',
+  name: ValidationsReducerName,
   initialState,
   reducers: {},
   extraReducers: (builder) => {
