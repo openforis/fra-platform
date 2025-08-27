365605db345dfa916c80cbee0014f38adcfbb484
Miró Sorja <ping@mirosorja.com>
2025-08-27 11:00:46 +0300

    4932 - Remove atlantis from calling code selector

--- src/client/components/Form/FormFields/TelephoneField/CallingCode/hooks/useCountryOptions.tsx
+++ src/client/components/Form/FormFields/TelephoneField/CallingCode/hooks/useCountryOptions.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Areas } from 'meta/area'
+
 import { useCountries } from 'client/store/area/hooks/countries'
 import Option from 'client/components/Form/FormFields/TelephoneField/CallingCode/Option'
 import { Option as OptionType } from 'client/components/Inputs/Select'
@@ -7,13 +9,15 @@ import { Option as OptionType } from 'client/components/Inputs/Select'
 export const useCountryOptions = (): Array<OptionType> => {
   const countries = useCountries()
 
-  return countries.map((country) => {
-    const { callingCode, countryIso } = country
+  return countries
+    .filter((c) => !Areas.isAtlantis(c.countryIso))
+    .map((country) => {
+      const { callingCode, countryIso } = country
 
-    // Returns e.g. { label: Option, value: '358:FIN' }
-    return {
-      label: <Option country={country} />,
-      value: `${callingCode}:${countryIso}`,
-    }
-  })
+      // Returns e.g. { label: Option, value: '358:FIN' }
+      return {
+        label: <Option country={country} />,
+        value: `${callingCode}:${countryIso}`,
+      }
+    })
 }
