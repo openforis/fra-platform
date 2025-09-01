28db63a2e9ffd4b9aa8ed8689e0ff9ac407aac0c
Miró Sorja <ping@mirosorja.com>
2025-09-01 13:41:24 +0300

    4534 - AssessmentBase type

--- src/server/controller/assessment/create.ts
+++ src/server/controller/assessment/create.ts
@@ -1,33 +1,23 @@
 import { ActivityLogMessage } from 'meta/assessment/activityLog'
-import { Assessment } from 'meta/assessment/assessment'
+import { Assessment, AssessmentBase } from 'meta/assessment/assessment'
 import { User } from 'meta/user'
 
 import { BaseProtocol, DB } from 'server/db'
 import { AssessmentRepository } from 'server/repository/assessment/assessment'
 import { ActivityLogRepository } from 'server/repository/public/activityLog'
 
-export const create = async (
-  props: { user: User; assessment: Pick<Assessment, 'props'> },
-  client: BaseProtocol = DB
-): Promise<Assessment> => {
+type Props = { user: User; assessment: Pick<Assessment, 'props'> }
+
+export const create = async (props: Props, client: BaseProtocol = DB): Promise<AssessmentBase> => {
   const { assessment, user } = props
   await AssessmentRepository.createAssessmentSchema({ assessment })
 
   return client.tx(async (t) => {
     const createdAssessment = await AssessmentRepository.createAssessment({ assessment }, t)
 
-    await ActivityLogRepository.insertActivityLog(
-      {
-        activityLog: {
-          target: createdAssessment,
-          section: 'assessment',
-          message: ActivityLogMessage.assessmentCreate,
-          user,
-        },
-        assessment: createdAssessment,
-      },
-      t
-    )
+    const message = ActivityLogMessage.assessmentCreate
+    const activityLog = { target: createdAssessment, section: 'assessment', message, user }
+    await ActivityLogRepository.insertActivityLog({ activityLog, assessment: createdAssessment }, t)
     return createdAssessment
   })
 }
