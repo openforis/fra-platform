d6faf0f749426505b3e3c25a7c4848070d75491c
Miró Sorja <ping@mirosorja.com>
2025-09-03 15:47:06 +0300

    4534 - Migration step

--- src/server/repository/adapter/assessment.ts
+++ src/server/repository/adapter/assessment.ts
@@ -1,15 +1,13 @@
 import { Objects } from 'utils/objects'
 
 import { Assessment, AssessmentProps } from 'meta/assessment/assessment'
-import { AssessmentMetaCache } from 'meta/assessment/metaCache'
 
 interface AssessmentDB {
   id: number
   uuid: string
   props: AssessmentProps
-  meta_cache: AssessmentMetaCache
 }
 
 export const AssessmentAdapter = (assessment: AssessmentDB): Assessment => {
-  return { ...Objects.camelize(assessment), metaCache: assessment.meta_cache }
+  return Objects.camelize(assessment)
 }
