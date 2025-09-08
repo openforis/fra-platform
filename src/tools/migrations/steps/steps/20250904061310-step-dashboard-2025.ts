fc837d02b8170feae4f88ceff4f8870daebb8cc0
Miró Sorja <ping@mirosorja.com>
2025-09-08 14:23:18 +0300

    4979 - Migration step: Update region and country dashboard items

---
+++ src/tools/migrations/steps/steps/20250904061310-step-dashboard-2025.ts
@@ -0,0 +1,48 @@
+import { forestArea } from 'tools/migrations/steps/steps/metadata/dashboard/forestArea'
+import { forestAreaPercentOfLandArea } from 'tools/migrations/steps/steps/metadata/dashboard/forestAreaPercentOfLandArea'
+import { forestAreaWithinProtectedAreas } from 'tools/migrations/steps/steps/metadata/dashboard/forestAreaWithinProtectedAreas'
+import { forestGrowingStockAndCarbonDashboard } from 'tools/migrations/steps/steps/metadata/dashboard/forestGrowingStockAndCarbon'
+import { forestOwnership } from 'tools/migrations/steps/steps/metadata/dashboard/forestOwnership'
+import { naturallyRegeneratingForestArea } from 'tools/migrations/steps/steps/metadata/dashboard/naturallyRegeneratingForestArea'
+import { primaryDesignatedManagementObjectiveDashboard } from 'tools/migrations/steps/steps/metadata/dashboard/primaryDesignatedManagementObjective'
+import { primaryForestPercentOfForestArea } from 'tools/migrations/steps/steps/metadata/dashboard/primaryForestPercentOfForestArea'
+import { Objects } from 'utils/objects'
+
+import { AssessmentNames } from 'meta/assessment/assessment'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { BaseProtocol, Schemas } from 'server/db'
+
+const assessmentName = AssessmentNames.fra
+const cycleName = '2025'
+
+export default async (client: BaseProtocol): Promise<void> => {
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName }, client)
+
+  const fns = [
+    forestArea,
+    forestGrowingStockAndCarbonDashboard,
+    forestAreaPercentOfLandArea,
+    primaryForestPercentOfForestArea,
+    forestAreaWithinProtectedAreas,
+    forestOwnership,
+    primaryDesignatedManagementObjectiveDashboard,
+    naturallyRegeneratingForestArea,
+  ]
+
+  const countryDashboardItems = fns.map((fn) => fn(cycle, false))
+  const regionDashboardItems = fns.map((fn) => fn(cycle, true))
+  const regionDashboardItemsPartial = countryDashboardItems.map((countryDashboardItem, i) => {
+    return Objects.getDiff(countryDashboardItem, regionDashboardItems[i])
+  })
+
+  const schemaName = Schemas.getNameCycle(assessment, cycle)
+  await client.query(
+    `update ${schemaName}.node_ext set value = $1::jsonb where type ='dashboard' and props ->> 'region' is null `,
+    [JSON.stringify(countryDashboardItems)]
+  )
+  await client.query(
+    `update ${schemaName}.node_ext set value = $1::jsonb where type ='dashboard' and props ->> 'region' = 'true' `,
+    [JSON.stringify(regionDashboardItemsPartial)]
+  )
+}
