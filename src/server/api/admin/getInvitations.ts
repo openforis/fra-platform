561958f6f1c513782863cf19d7f7b92026a5944a
Miró Sorja <ping@mirosorja.com>
2025-09-10 09:14:16 +0200

    4534 - API/Admin: use context (#4993)

--- src/server/api/admin/getInvitations.ts
+++ src/server/api/admin/getInvitations.ts
@@ -2,15 +2,14 @@ import { Response } from 'express'
 
 import { TablePaginatedDataRequest } from 'meta/api/request/tablePaginated'
 
-import { AssessmentController } from 'server/controller/assessment'
 import { UserController } from 'server/controller/user'
 import Requests from 'server/utils/requests'
 
-export const getInvitations = async (req: TablePaginatedDataRequest, res: Response) => {
+export const getInvitations = async (req: TablePaginatedDataRequest, res: Response): Promise<void> => {
   try {
-    const { assessmentName, cycleName, limit, offset, orderBy, orderByDirection } = req.query
+    const { limit, offset, orderBy, orderByDirection } = req.query
 
-    const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
+    const { assessment, cycle } = req.context
 
     const props = { assessment, cycle, limit, offset, orderBy, orderByDirection }
     const invitations = await UserController.getManyInvitations(props)
