e1b0f8631e9f5e8d936d42268f901bfc891e0cd6
Miró Sorja <ping@mirosorja.com>
2025-09-10 09:57:20 +0300

    4534 - API/area: use context

--- src/server/api/area/getAreas.ts
+++ src/server/api/area/getAreas.ts
@@ -3,14 +3,11 @@ import { Response } from 'express'
 import { InitRequest } from 'meta/api/request'
 
 import { AreaController } from 'server/controller/area'
-import { AssessmentController } from 'server/controller/assessment'
 import Requests from 'server/utils/requests'
 
-export const getAreas = async (req: InitRequest, res: Response) => {
+export const getAreas = async (req: InitRequest, res: Response): Promise<void> => {
   try {
-    const { assessmentName, cycleName } = req.query
-
-    const { assessment, cycle } = await AssessmentController.getOneWithCycle({ assessmentName, cycleName })
+    const { assessment, cycle } = req.context
 
     const [countries, regionGroups] = await Promise.all([
       AreaController.getCountries({ assessment, cycle }),
