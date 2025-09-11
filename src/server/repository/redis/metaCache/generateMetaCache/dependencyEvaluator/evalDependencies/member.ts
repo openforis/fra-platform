e48f917fb2e3e134a1926107346bd2b94bbdd1cf
Mino Togna <minotogna@users.noreply.github.com>
2025-09-11 14:25:36 +0200

    4534: Cache assessments: Fix Validation with prev cycle data (#5008)

--- src/server/repository/redis/metaCache/generateMetaCache/dependencyEvaluator/evalDependencies/member.ts
+++ src/server/repository/redis/metaCache/generateMetaCache/dependencyEvaluator/evalDependencies/member.ts
@@ -87,7 +87,7 @@ export class MemberEvaluator extends ExpressionNodeEvaluator<Context, MemberExpr
 
     if (this.#variableExists(variable)) {
       const assessment = assessments[assessmentName]
-      const cycle = Assessments.getCycle({ assessment, cycleName: variable.cycleName ?? cycleName })
+      const cycle = Assessments.getCycle({ assessment, cycleName })
       const metaCache = AssessmentMetaCaches.getMetaCache({ assessment, cycle })
 
       const propsDependency = { assessment, cycle, tableName, variableName }
