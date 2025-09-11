807adc30852c830340edba1dd04b9de3cf922e6e
Miró Sorja <ping@mirosorja.com>
2025-09-11 14:48:58 +0300

    4997 - useRolesValidationSchema: Fix checking admin role

--- src/client/pages/User/hooks/useRolesValidationSchema.ts
+++ src/client/pages/User/hooks/useRolesValidationSchema.ts
@@ -5,6 +5,7 @@ import { z, ZodAny } from 'zod'
 import { ParsePayload } from 'zod/v4/core'
 
 import { UserEditFormRoles } from 'meta/form/userEdit/form'
+import { RoleName } from 'meta/user'
 
 export const useRolesValidationSchema = (): ZodAny => {
   const { t } = useTranslation()
@@ -12,11 +13,16 @@ export const useRolesValidationSchema = (): ZodAny => {
   return useMemo<ZodAny>(() => {
     return z.any().check((ctx: ParsePayload) => {
       const values = ctx.value as UserEditFormRoles
+
       const emptyRoles = Object.values(values).every((value) => {
         if (Array.isArray(value)) return value.length <= 0
         return value !== true
       })
-      if (emptyRoles) {
+
+      // Note: Admin role is special case!
+      const isAdmin = values[RoleName.ADMINISTRATOR]
+
+      if (emptyRoles && !isAdmin) {
         const message = t('form.errors.atLeastOneRoleRequired')
         ctx.issues.push({ code: 'custom', message, input: ctx.value })
       }
