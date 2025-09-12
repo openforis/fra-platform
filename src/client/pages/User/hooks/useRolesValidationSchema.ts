dd0723ad08416109656b33fc8de1cfb5aefc01a4
Miró Sorja <ping@mirosorja.com>
2025-09-12 09:34:10 +0300

    4997 - useRolesValidationSchema: Use proper schema validation

--- src/client/pages/User/hooks/useRolesValidationSchema.ts
+++ src/client/pages/User/hooks/useRolesValidationSchema.ts
@@ -1,31 +1,42 @@
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { z, ZodAny } from 'zod'
-import { ParsePayload } from 'zod/v4/core'
+import { Objects } from 'utils/objects'
+import { z } from 'zod'
 
-import { UserEditFormRoles } from 'meta/form/userEdit/form'
 import { RoleName } from 'meta/user'
 
-export const useRolesValidationSchema = (): ZodAny => {
+// Roles with one or more countries
+const countryRoles = [
+  RoleName.REVIEWER,
+  RoleName.NATIONAL_CORRESPONDENT,
+  RoleName.ALTERNATE_NATIONAL_CORRESPONDENT,
+  RoleName.COLLABORATOR,
+  RoleName.VIEWER,
+]
+
+const roleSchema = z.object({
+  [RoleName.ADMINISTRATOR]: z.boolean().optional(),
+  ...Object.fromEntries(countryRoles.map((roleName) => [roleName, z.array(z.string()).optional()])),
+})
+
+type Returned = ReturnType<typeof roleSchema.refine>
+
+export const useRolesValidationSchema = (): Returned => {
   const { t } = useTranslation()
 
-  return useMemo<ZodAny>(() => {
-    return z.any().check((ctx: ParsePayload) => {
-      const values = ctx.value as UserEditFormRoles
-
-      const emptyRoles = Object.values(values).every((value) => {
-        if (Array.isArray(value)) return value.length <= 0
-        return value !== true
-      })
-
-      // Note: Admin role is special case!
-      const isAdmin = values[RoleName.ADMINISTRATOR]
-
-      if (emptyRoles && !isAdmin) {
-        const message = t('form.errors.atLeastOneRoleRequired')
-        ctx.issues.push({ code: 'custom', message, input: ctx.value })
+  return useMemo(() => {
+    return roleSchema.refine(
+      (values) => {
+        // Check that at least one country role is not empty or is an administrator
+        return Object.entries(values).some(([key, value]) => {
+          if (key === RoleName.ADMINISTRATOR) return Boolean(value)
+          return !Objects.isEmpty(value)
+        })
+      },
+      {
+        message: t('form.errors.atLeastOneRoleRequired'),
       }
-    })
+    )
   }, [t])
 }
