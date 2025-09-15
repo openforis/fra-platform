7dddeb0e3337b89e1b6cb3f807cf4a92e3d1f50e
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-09-15 09:49:51 -0500

    3061 - Remove invitation props method

--- src/server/api/admin/getInvitations.ts
+++ src/server/api/admin/getInvitations.ts
@@ -1,15 +1,22 @@
 import { Response } from 'express'
 
 import { TablePaginatedDataRequest } from 'meta/api/request/tablePaginated'
+import { TablePaginateds } from 'meta/tablePaginated'
+import { InvitationFilters } from 'meta/tablePaginated/invitations'
 
-import { getInvitationsGetManyPropsFromRequest } from 'server/api/admin/_getInvitationsGetManyPropsFromRequest'
 import { UserController } from 'server/controller/user'
 import Requests from 'server/utils/requests'
 
 export const getInvitations = async (req: TablePaginatedDataRequest, res: Response): Promise<void> => {
   try {
-    const props = await getInvitationsGetManyPropsFromRequest(req)
+    const { filters: filtersReq, limit: limitReq, offset: offsetReq, orderBy, orderByDirection } = req.query
 
+    const filters = TablePaginateds.decodeFilters<InvitationFilters>(filtersReq)
+    const limit = limitReq && Number(limitReq)
+    const offset = offsetReq && Number(offsetReq)
+
+    const { assessment, cycle } = req.context
+    const props = { assessment, cycle, filters, limit, offset, orderBy, orderByDirection }
     const invitations = await UserController.getManyInvitations(props)
 
     Requests.sendOk(res, invitations)
