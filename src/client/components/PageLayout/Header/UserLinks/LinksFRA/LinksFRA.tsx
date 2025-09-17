9d4990c7162bd4ffd9ab7e53470c037996cb7db0
Miró Sorja <ping@mirosorja.com>
2025-09-17 10:04:30 +0300

    5004 - User Logout: Redirect to last published Cycle home page (#5023)
    * 5004 - LogOut: Redirect to latest published cycle
    
    * 5004 - LinksFRA: Minor clean up
    
    * 5004 - LinksFRA: move hook to ./hook

--- src/client/components/PageLayout/Header/UserLinks/LinksFRA/LinksFRA.tsx
+++ src/client/components/PageLayout/Header/UserLinks/LinksFRA/LinksFRA.tsx
@@ -2,74 +2,25 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
-import { TFunction } from 'i18next'
-
-import { CountryIso, Global } from 'meta/area'
-import { Assessment } from 'meta/assessment/assessment'
-import { Cycle } from 'meta/assessment/cycle'
 import { Routes } from 'meta/routes'
-import { User, Users } from 'meta/user'
-import { UserRoles } from 'meta/user/userRoles'
+import { Users } from 'meta/user'
 
-import { useAppDispatch } from 'client/store/hooks'
 import { useAssessment } from 'client/store/meta/hooks/assessments'
 import { useCycle } from 'client/store/meta/hooks/cycles'
-import { AppDispatch } from 'client/store/types'
-import { UserActions } from 'client/store/user/actions'
 import { useUser } from 'client/store/user/hooks/user'
-import { useCountryIso, useIsLoginRoute } from 'client/hooks'
-import { ToasterHook, useToaster } from 'client/hooks/useToaster'
+import { useIsLoginRoute } from 'client/hooks'
 import Icon from 'client/components/Icon'
-import PopoverControl, { PopoverItem } from 'client/components/PopoverControl'
+import PopoverControl from 'client/components/PopoverControl'
 
-const getLinks = (
-  t: TFunction,
-  assessment: Assessment,
-  countryIso: CountryIso,
-  cycle: Cycle,
-  user: User,
-  dispatch: AppDispatch,
-  toaster: ToasterHook
-) => {
-  const assessmentName = assessment.props.name
-  const cycleName = cycle.name
-  const userCountryIso = countryIso ?? UserRoles.getLastRole({ assessment, user })?.countryIso ?? Global.WO
-  const items: Array<PopoverItem> = [
-    {
-      content: t<string>('header.editProfile'),
-      link: Routes.CountryUser.generatePath({ assessmentName, cycleName, countryIso: userCountryIso, id: user.id }),
-    },
-  ]
-  if (Users.isAdministrator(user)) {
-    items.push({
-      content: t<string>('admin.admin'),
-      link: Routes.Admin.generatePath({ assessmentName, cycleName }),
-    })
-  }
-  items.push(
-    {
-      divider: true,
-    },
-    {
-      content: t<string>('header.logout'),
-      onClick: () => {
-        dispatch(UserActions.logout()).then(() => toaster.toaster.info(t('login.logoutSuccessful')))
-      },
-    }
-  )
-  return items
-}
+import { useUserLinks } from './hooks/useUserLinks'
 
 const LinksFRA: React.FC = () => {
-  const assessment = useAssessment()
-  const countryIso = useCountryIso()
-  const cycle = useCycle()
-  const dispatch = useAppDispatch()
-  const user = useUser()
-  const toaster = useToaster()
-
   const { t } = useTranslation()
+  const assessment = useAssessment()
+  const cycle = useCycle()
+  const user = useUser()
   const isLogin = useIsLoginRoute()
+  const userLinks = useUserLinks()
 
   const assessmentName = assessment.props.name
   const cycleName = cycle.name
@@ -77,7 +28,7 @@ const LinksFRA: React.FC = () => {
   return (
     <>
       {user && (
-        <PopoverControl items={getLinks(t, assessment, countryIso, cycle, user, dispatch, toaster)}>
+        <PopoverControl items={userLinks}>
           <div className="app-header__menu-item">
             {Users.getFullName(user)}
             <Icon className="icon-middle" name="small-down" />
@@ -97,4 +48,5 @@ const LinksFRA: React.FC = () => {
     </>
   )
 }
+
 export default LinksFRA
