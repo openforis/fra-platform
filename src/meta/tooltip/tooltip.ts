6d5e20822fdd4a14decbb98486432482d49fad51
Miró Sorja <ping@mirosorja.com>
2025-09-19 08:51:06 +0300

    4404 - Tooltip: Add new black, success, white

--- src/meta/tooltip/tooltip.ts
+++ src/meta/tooltip/tooltip.ts
@@ -1,5 +1,8 @@
 export enum TooltipId {
+  black = 'black-tooltip',
   error = 'error-tooltip',
   info = 'info-tooltip',
   infoClickable = 'info-clickable-tooltip',
+  success = 'success-tooltip',
+  white = 'white-tooltip',
 }
