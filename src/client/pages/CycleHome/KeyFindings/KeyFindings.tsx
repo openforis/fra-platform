1fcd06d5a0197f04206f1a354ea63dc2db69a0b7
Miró Sorja <ping@mirosorja.com>
2025-09-25 14:29:09 +0300

    5057 - Update landing page for 2025 and latest

--- src/client/pages/CycleHome/KeyFindings/KeyFindings.tsx
+++ src/client/pages/CycleHome/KeyFindings/KeyFindings.tsx
@@ -2,26 +2,37 @@ import './keyFindings.scss'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-const climaticDomains = {
-  tropical: 45,
-  boreal: 27,
-  temperate: 16,
-  subtropical: 11,
+import { ApiEndPoint } from 'meta/api/endpoint'
+import { Global } from 'meta/area'
+import { CycleName } from 'meta/assessment/cycle'
+
+import { useCycleRouteParams } from 'client/hooks/useRouteParams'
+
+const climaticDomains: Record<CycleName, Record<string, number>> = {
+  '2020': { boreal: 27, subtropical: 11, temperate: 16, tropical: 45 },
+  '2025': { boreal: 28, subtropical: 11, temperate: 17, tropical: 45 },
+  latest: { boreal: 28, subtropical: 11, temperate: 17, tropical: 45 },
 }
 
 const KeyFindings: React.FC = () => {
   const { t } = useTranslation()
+  const { assessmentName, cycleName } = useCycleRouteParams()
+  const searchParams = new URLSearchParams({ assessmentName, cycleName, countryIso: Global.WO })
 
   return (
     <div className="home-key-findings">
       <div className="home-key-findings__map">
-        <img alt="" className="map" src="/img/map.png" />
+        <img
+          alt={t(`home.mapAltText.${cycleName}`)}
+          className="map"
+          src={ApiEndPoint.Static.file(`fra/${cycleName}/landing/map.png?${searchParams.toString()}`)}
+        />
       </div>
 
-      <div>{t('home.keyFindings')}</div>
+      <div>{t(`home.keyFindings.${cycleName}`)}</div>
 
       <div className="home-key-findings__map-legend">
-        {Object.entries(climaticDomains).map(([key, value]) => (
+        {Object.entries(climaticDomains[cycleName]).map(([key, value]) => (
           <div key={key} className="legend">
             <img alt="" className="legend-icon" src={`/img/mapLegend_${key}.svg`} />
             <div className="legend-key">{t(`climaticDomain.${key}`)}</div>
