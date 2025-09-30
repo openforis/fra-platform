e0c836f90aed98deb942470e4f917293b13d1c8f
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-09-30 18:16:19 -0500

    5050 - Add mobile support for explorer

--- src/client/pages/Explorer/Filters/Filters.tsx
+++ src/client/pages/Explorer/Filters/Filters.tsx
@@ -1,5 +1,6 @@
 import './Filters.scss'
 import React from 'react'
+import MediaQuery from 'react-responsive'
 
 import Hr from 'client/components/Hr/Hr'
 import ExportButton from 'client/pages/Explorer/ExportButton/ExportButton'
@@ -8,6 +9,7 @@ import Dimension from 'client/pages/Explorer/Filters/Dimension/Dimension'
 import Measure from 'client/pages/Explorer/Filters/Measure/Measure'
 import Options from 'client/pages/Explorer/Filters/Options/Options'
 import { ExplorerGridProps } from 'client/pages/Explorer/types'
+import { Breakpoints } from 'client/utils'
 
 const Filters: React.FC<ExplorerGridProps> = (props: ExplorerGridProps) => {
   const { gridRef } = props
@@ -17,10 +19,14 @@ const Filters: React.FC<ExplorerGridProps> = (props: ExplorerGridProps) => {
       <Country />
       <Measure />
       <Dimension />
-      <Hr vertical />
-      <Options />
-      <Hr vertical />
-      <ExportButton gridRef={gridRef} />
+      <MediaQuery minWidth={Breakpoints.laptop}>
+        <Hr vertical />
+      </MediaQuery>
+      <div className="explorer-filters__actions">
+        <Options />
+        <Hr vertical />
+        <ExportButton gridRef={gridRef} />
+      </div>
     </div>
   )
 }
