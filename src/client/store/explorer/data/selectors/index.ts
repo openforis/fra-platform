7f0d9a02bf6e217ff61862dce53256ed07f1cc15
Miró Sorja <ping@mirosorja.com>
2025-09-30 10:33:35 +0200

    5048, 5046 - Explorer store: Null check lazy loaded

--- src/client/store/explorer/data/selectors/index.ts
+++ src/client/store/explorer/data/selectors/index.ts
@@ -3,12 +3,13 @@ import { createSelector } from '@reduxjs/toolkit'
 import { SectionName } from 'meta/assessment/section'
 
 import { ExplorerDataSlice } from 'client/store/explorer/data/slice'
+import { ExplorerDataState } from 'client/store/explorer/data/state'
 import { RootState } from 'client/store/types'
 
-const _getState = (state: RootState) => state.explorer[ExplorerDataSlice.name]
+const _getState = (state: RootState): ExplorerDataState => state.explorer?.[ExplorerDataSlice.name]
 
 export const getSectionData = createSelector(
-  [_getState, (_state: RootState, sectionName: SectionName) => sectionName],
+  [_getState, (_state: RootState, sectionName: SectionName): string => sectionName],
   (data, sectionName) => data?.[sectionName]
 )
 
