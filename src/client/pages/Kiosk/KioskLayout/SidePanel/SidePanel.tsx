e8054a00332577a7f63437b4cf4185866ca43a68
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2025-10-08 23:15:52 -0500

    5072 - Kiosk Update - 1: Edit main colors and edit side panel partially (#5078)
    * 5072 - Edit main colors and side panel
    
    * 5072 - Add remaining partner logos
    
    * 5072 - Remove kiosk svg from icons

--- src/client/pages/Kiosk/KioskLayout/SidePanel/SidePanel.tsx
+++ src/client/pages/Kiosk/KioskLayout/SidePanel/SidePanel.tsx
@@ -15,45 +15,58 @@ const SidePanel: React.FC = () => {
 
   const isKioskHomeRoute = useIsRoute({ path: Routes.Kiosk.path.absolute, exact: true })
 
-  const goBack = useCallback(() => {
+  const goBack = useCallback((): void => {
     navigate(-1)
   }, [navigate])
 
   return (
     <div className="kiosk-side-panel">
       <div className="kiosk-side-panel__header">
-        <img alt="FAO" src="/img/fao/FAOen_3_lines.svg" />
-        <button
-          className="kiosk-side-panel__home-button"
-          onClick={() => startTransition(() => navigate(Routes.Kiosk.path.absolute))}
-          type="button"
-        >
-          <Icon name="home-circle" />
-        </button>
-      </div>
-      <div className="kiosk-side-panel__quiz">
-        {/* <h1>Get to know FRA and test your knowledge!</h1> */}
-        <div className="kiosk-side-panel__quiz-img-container">
-          {/* <div className="kiosk-side-panel__quiz-title">FRA quiz</div> */}
-          <img alt="quiz" className="kiosk-side-panel__quiz-img" src="/img/kiosk/quiz-bubbles.png" />
-        </div>
-      </div>
-      <div className="kiosk-side-panel__footer">
-        <p>
-          <i>Created with the financial support of:</i>
-        </p>
-        <div className="kiosk-side-panel__partners">
-          <img alt="EU" className="kiosk-side-panel__partners-eu" src="/img/partners/EU.jpg" />
-          <img alt="NICFI" className="kiosk-side-panel__partners-nicfi" src="/img/partners/NICFI.png" />
-
+        <img alt="FAO" className="kiosk-side-panel__header-logo" src="/img/fao/FAOen_2_lines_blue.png" />
+        <div className="kiosk-side-panel__buttons">
+          <button
+            className="kiosk-side-panel__home-button"
+            onClick={(): void => startTransition(() => navigate(Routes.Kiosk.path.absolute))}
+            type="button"
+          >
+            <img alt="Go to kiosk home" className="kiosk-side-panel__home-icon" src="/img/kiosk/home-button.svg" />
+          </button>
           {/* No conditional rendering here to avoid layout shifting in certain screen sizes */}
           <button
             className={classNames('kiosk-side-panel__back-button', { visible: !isKioskHomeRoute })}
-            onClick={() => startTransition(() => goBack())}
+            onClick={(): void => startTransition(() => goBack())}
             type="button"
           >
             <Icon name="arrow-back" />
           </button>
+        </div>
+      </div>
+      <div className="kiosk-side-panel__content">
+        <h1 className="kiosk-side-panel__title">Global Forest Resources Assessment</h1>
+        <div className="kiosk-side-panel__language-buttons">
+          <button className="kiosk-side-panel__language-button" type="button">
+            English
+          </button>
+          <button className="kiosk-side-panel__language-button" type="button">
+            Français
+          </button>
+          <button className="kiosk-side-panel__language-button" type="button">
+            Español
+          </button>
+        </div>
+      </div>
+      <div className="kiosk-side-panel__footer">
+        <p>
+          <strong>
+            <i>With the assistance of:</i>
+          </strong>
+        </p>
+        <div className="kiosk-side-panel__partners">
+          <img alt="EU" src="/img/partners/EU.jpg" />
+          <img alt="Ministry for Foreign Affairs of Finland" src="/img/partners/mfafi_black.png" />
+          <img alt="NICFI" src="/img/partners/NICFI.png" />
+          <img alt="CHE" src="/img/partners/CHE.png" />
+          <img alt="GEF" src="/img/partners/GEF_fullname.png" />
         </div>
       </div>
     </div>
