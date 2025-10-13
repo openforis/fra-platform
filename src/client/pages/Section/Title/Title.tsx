a39b9b4edb16a72da94e651f4a81862bb38dc069
Miró Sorja <ping@mirosorja.com>
2025-10-13 11:35:57 +0300

    2804 - CSV All button (#5085)
    * 2804 - CSV All button
    
    * 2804 - remove double label
    
    * 2804 - General improvements

--- src/client/pages/Section/Title/Title.tsx
+++ src/client/pages/Section/Title/Title.tsx
@@ -6,6 +6,7 @@ import { SectionName } from 'meta/assessment/section'
 import { useIsPrintRoute } from 'client/hooks/useIsRoute'
 import { useCycleRouteParams } from 'client/hooks/useRouteParams'
 import { Components, TitleDefault } from 'client/pages/Section/Title/Components'
+import CSVAllTables from 'client/pages/Section/Title/CSVAllTables'
 import Hints, { HintsSustainableDevelopment } from 'client/pages/Section/Title/Hints'
 
 import { Props } from './props'
@@ -27,8 +28,10 @@ const Title: React.FC<Props> = (props) => {
 
   return (
     <div className="section__title">
-      {React.createElement(Component, { subSection })}
-
+      <div className="section__title__header">
+        {React.createElement(Component, { subSection })}
+        {!print && <CSVAllTables />}
+      </div>
       {!print && <HintsComponent subSection={subSection} />}
     </div>
   )
