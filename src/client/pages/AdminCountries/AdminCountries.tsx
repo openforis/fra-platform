d6e0955ff27db52e68bf27276d90d4df2ea07c9c
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-10-16 08:56:30 -0500

    4005 - Update useLanguage hook import

--- src/client/pages/AdminCountries/AdminCountries.tsx
+++ src/client/pages/AdminCountries/AdminCountries.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
 
-import { useLanguage } from 'client/hooks/useLanguage'
+import { useLanguage } from 'client/hooks/language'
 import TablePaginated from 'client/components/TablePaginated'
 import { useColumns } from 'client/pages/AdminCountries/hooks/useColumns'
 import { useFilters } from 'client/pages/AdminCountries/hooks/useFilters'
