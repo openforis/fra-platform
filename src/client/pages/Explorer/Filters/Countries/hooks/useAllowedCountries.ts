1b65d6c3b1a8d07cb7170e638a94a61701fd2026
Mino Togna <minotogna@users.noreply.github.com>
2025-10-22 10:22:49 +0200

    5130: Data explorer Regions: country selectors show only region countries (#5131)
    * 5130: Data explorer Regions: country selectors show only region countries
    
    * 5130: restore useCycleRouteParams in Countries
    
    * 5130: use reduce in useAllowedCountries

---
+++ src/client/pages/Explorer/Filters/Countries/hooks/useAllowedCountries.ts
@@ -0,0 +1,26 @@
+import { Areas, CountryIso, RegionCode } from 'meta/area'
+
+import { useCountries } from 'client/store/area/hooks/countries'
+import { useGlobalCountries } from 'client/store/ui/countryReport/hooks/globalCountries'
+import { useCountryRouteParams } from 'client/hooks/routeParams'
+
+export const useAllowedCountries = (): Array<CountryIso> | undefined => {
+  const { countryIso } = useCountryRouteParams()
+  const countries = useCountries()
+  const homeCountriesFilter = useGlobalCountries()
+
+  if (Areas.isGlobal(countryIso) && homeCountriesFilter?.length > 0) {
+    return homeCountriesFilter
+  }
+
+  if (Areas.isRegion(countryIso)) {
+    return countries.reduce<Array<CountryIso>>((acc, country) => {
+      if (country.regionCodes.includes(countryIso as RegionCode)) {
+        acc.push(country.countryIso)
+      }
+      return acc
+    }, [])
+  }
+
+  return undefined
+}
