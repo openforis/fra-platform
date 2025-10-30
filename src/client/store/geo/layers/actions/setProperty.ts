643a5fce3ad41cf622f30d77a1b85304f399becf
Miró Sorja <ping@mirosorja.com>
2025-10-30 10:22:03 +0200

    5217 - add global.d.ts with KeyedValue (#5222)

--- src/client/store/geo/layers/actions/setProperty.ts
+++ src/client/store/geo/layers/actions/setProperty.ts
@@ -1,7 +1,6 @@
 import { createAction } from '@reduxjs/toolkit'
 
 import { LayerKey } from 'meta/geo'
-import { KeyedValue } from 'meta/utils/generics'
 
 import { LayerState } from 'client/store/geo/layers/state'
 
---
+++ src/meta/global/global.d.ts
@@ -0,0 +1,19 @@
+declare global {
+  /**
+   * Creates a union of objects, where each object contains a `key` from the original type T,
+   * and a `value` with the corresponding type of that key.
+   *
+   * For example:
+   * type Car = { type: string; year: number; }
+   * type CarProperty = KeyedValue<Car>
+   * The result is: { key: "type"; value: string; } | { key: "year"; value: number; }
+   */
+  type KeyedValue<T> = {
+    [K in keyof T]: {
+      key: K
+      value: T[K]
+    }
+  }[keyof T]
+}
+
+export {}
