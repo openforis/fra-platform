54d1347f443a27a79bf5013ceace4caf0dcfada9
Mino Togna <mino.togna@gmail.com>
2025-11-13 07:57:45 +0100

    5276: Barrel files refactor: meta/cycleData/links

--- src/server/db/repository/assessmentCycle/links/LinksQueryParams.ts
+++ src/server/db/repository/assessmentCycle/links/LinksQueryParams.ts
@@ -1,5 +1,5 @@
 import { CountryIso } from 'meta/area/countryIso'
-import { LinkValidationStatusCode } from 'meta/cycleData'
+import { LinkValidationStatusCode } from 'meta/cycleData/links/link'
 import { TablePaginatedOrderByDirection } from 'meta/tablePaginated'
 
 export interface LinksQueryParams {
