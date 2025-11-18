d3a8d32738371120e4446c75822b2c08568f9c6a
Miró Sorja <ping@mirosorja.com>
2025-11-18 14:22:48 +0200

    5166 - tools/import: generate cycle schemas

--- src/tools/data/import.ts
+++ src/tools/data/import.ts
@@ -3,12 +3,15 @@ import '../scriptInit'
 import * as path from 'path'
 import { ToolsUtils } from 'tools/utils/toolsUtils'
 
-import { AssessmentNames } from 'meta/assessment/assessment'
-
 import { DB } from 'server/db/db'
 import { AssessmentRepository } from 'server/db/repository/assessment/assessment'
+import { Schemas } from 'server/db/schemas'
 import { DatabaseService } from 'server/service/databaseService'
-import { EXPORT_TABLES } from 'server/service/databaseService/EXPORT_TABLES'
+import {
+  EXPORT_ASSESSMENTS,
+  EXPORT_ASSESSMENTS_CYCLES,
+  EXPORT_TABLES,
+} from 'server/service/databaseService/EXPORT_TABLES'
 
 import { _readTableFromFile } from './_readTableFromFile'
 
@@ -17,9 +20,20 @@ const INPUT_DIR = path.join(__dirname, 'fixtures')
 const exec = async (): Promise<void> => {
   // Create assessment schemas
   await Promise.all(
-    [AssessmentNames.fra, AssessmentNames.panEuropean].map((assessmentName) => {
+    EXPORT_ASSESSMENTS.map((assessmentName) => {
       return AssessmentRepository.createAssessmentSchema({ assessmentName })
     })
+  )
+
+  // Create assessment cycle schemas
+  await Promise.all(
+    EXPORT_ASSESSMENTS.flatMap((assessmentName) =>
+      EXPORT_ASSESSMENTS_CYCLES[assessmentName].map(async (cycleName) => {
+        const schemaAssessment = Schemas.getSchemaAssessment(assessmentName)
+        const schemaCycle = Schemas.getSchemaAssessmentCycle(assessmentName, cycleName)
+        await DB.query(AssessmentRepository.getCreateSchemaCycleDDL(schemaAssessment, schemaCycle))
+      })
+    )
   )
 
   await DB.tx(async (t) => {
