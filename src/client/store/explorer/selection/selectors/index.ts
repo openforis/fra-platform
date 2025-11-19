c4bd28907845f3bf3dd51f66c158fb1a2b122830
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-11-19 15:26:49 -0500

    5346 - Fix TSC Issue:explorer/selection/selectors/index.ts

--- src/client/store/explorer/selection/selectors/index.ts
+++ src/client/store/explorer/selection/selectors/index.ts
@@ -5,10 +5,13 @@ import { CycleName } from 'meta/assessment/cycle'
 import { SectionName } from 'meta/assessment/section'
 
 import { ExplorerSelectionSlice } from 'client/store/explorer/selection/slice'
+import type { ExplorerSelectionState } from 'client/store/explorer/selection/state'
 import { defaultAxisSelection } from 'client/store/explorer/selection/state'
 import { RootState } from 'client/store/types'
 
-const _getState = (state: RootState) => state.explorer?.[ExplorerSelectionSlice.name]
+const _getState = (state: RootState): ExplorerSelectionState | undefined => {
+  return state.explorer?.[ExplorerSelectionSlice.name]
+}
 
 const getCountries = createSelector(
   [
@@ -16,7 +19,7 @@ const getCountries = createSelector(
     (_state: RootState, assessmentName: AssessmentName) => assessmentName,
     (_state: RootState, _assessmentName: AssessmentName, cycleName: CycleName) => cycleName,
   ],
-  (filters, assessmentName, cycleName) => {
+  (filters: ExplorerSelectionState | undefined, assessmentName: AssessmentName, cycleName: CycleName) => {
     return filters?.[assessmentName]?.[cycleName]?.countries
   }
 )
@@ -27,21 +30,54 @@ const _baseSelectors = [
   (_state: RootState, _assessmentName: AssessmentName, cycleName: CycleName) => cycleName,
   (_state: RootState, _assessmentName: AssessmentName, _cycleName: CycleName, sectionName: SectionName) => sectionName,
 ]
-const getAxisSelection = createSelector(_baseSelectors, (filters, assessmentName, cycleName, sectionName) => {
-  return filters?.[assessmentName]?.[cycleName]?.axis?.[sectionName] ?? defaultAxisSelection
-})
 
-const getDimensions = createSelector(_baseSelectors, (filters, assessmentName, cycleName, sectionName) => {
-  return filters?.[assessmentName]?.[cycleName]?.dimensions?.[sectionName] ?? []
-})
+const getAxisSelection = createSelector(
+  _baseSelectors,
+  (
+    filters: ExplorerSelectionState | undefined,
+    assessmentName: AssessmentName,
+    cycleName: CycleName,
+    sectionName: SectionName
+  ) => {
+    return filters?.[assessmentName]?.[cycleName]?.axis?.[sectionName] ?? defaultAxisSelection
+  }
+)
 
-const getMeasures = createSelector(_baseSelectors, (filters, assessmentName, cycleName, sectionName) => {
-  return filters?.[assessmentName]?.[cycleName]?.measures?.[sectionName] ?? []
-})
+const getDimensions = createSelector(
+  _baseSelectors,
+  (
+    filters: ExplorerSelectionState | undefined,
+    assessmentName: AssessmentName,
+    cycleName: CycleName,
+    sectionName: SectionName
+  ) => {
+    return filters?.[assessmentName]?.[cycleName]?.dimensions?.[sectionName] ?? []
+  }
+)
 
-const getUnits = createSelector(_baseSelectors, (filters, assessmentName, cycleName, sectionName) => {
-  return filters?.[assessmentName]?.[cycleName]?.units?.[sectionName]
-})
+const getMeasures = createSelector(
+  _baseSelectors,
+  (
+    filters: ExplorerSelectionState | undefined,
+    assessmentName: AssessmentName,
+    cycleName: CycleName,
+    sectionName: SectionName
+  ) => {
+    return filters?.[assessmentName]?.[cycleName]?.measures?.[sectionName] ?? []
+  }
+)
+
+const getUnits = createSelector(
+  _baseSelectors,
+  (
+    filters: ExplorerSelectionState | undefined,
+    assessmentName: AssessmentName,
+    cycleName: CycleName,
+    sectionName: SectionName
+  ) => {
+    return filters?.[assessmentName]?.[cycleName]?.units?.[sectionName]
+  }
+)
 
 export const ExplorerSelectionSelectors = {
   getAxisSelection,
