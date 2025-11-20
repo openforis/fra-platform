3380a6e30bd3dc3d65bf75149527a4f68fc6dad8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-11-20 17:12:00 -0500

    5367 - Migrate sliding panel

--- src/client/components/SlidingPanel/SlidingPanel.tsx
+++ src/client/components/SlidingPanel/SlidingPanel.tsx
@@ -1,7 +1,7 @@
 import './SlidingPanel.scss'
-import 'react-sliding-side-panel/lib/index.css'
-import React from 'react'
-import SlidingPanelComponent from 'react-sliding-side-panel'
+import React, { useMemo } from 'react'
+
+import classNames from 'classnames'
 
 import ButtonClose from 'client/components/Buttons/ButtonClose'
 
@@ -14,22 +14,36 @@ type Props = {
 
 const SlidingPanel: React.FC<React.PropsWithChildren<Props>> = (props) => {
   const { children, closePanel, noBackdrop = false, opened, size = 30 } = props
-  const panelType = 'right'
+
+  const panelStyle = useMemo<Record<string, string>>(
+    () => ({
+      '--sliding-panel-width': `${size}vw`,
+    }),
+    [size]
+  )
 
   return (
-    <SlidingPanelComponent
-      backdropClicked={closePanel}
-      isOpen={opened}
-      noBackdrop={noBackdrop}
-      panelClassName="sliding-panel"
-      size={size}
-      type={panelType}
+    <div
+      aria-hidden={!opened}
+      className={classNames('sliding-panel-container', {
+        active: opened,
+        open: opened,
+        'click-through': noBackdrop,
+      })}
+      style={panelStyle}
     >
-      <>
-        <ButtonClose className="sliding-panel__close" onClick={closePanel} />
-        <div className="panel-container">{children}</div>
-      </>
-    </SlidingPanelComponent>
+      {!noBackdrop && (
+        /* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
+        <div className="sliding-panel-backdrop" onClick={closePanel} />
+      )}
+
+      <div className="sliding-panel-track">
+        <div className="sliding-panel">
+          <ButtonClose className="sliding-panel__close" onClick={closePanel} />
+          <div className="panel-container">{children}</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
