e32e19640d0d41a5a768170702cc50deaab2fc97
Miró Sorja <ping@mirosorja.com>
2025-11-20 11:26:54 +0200

    5159 - Add static fixtures
    - Add static fixtures functionality
    - Add static fixtures for test user
    - Update .gitignore
    - Update README.md

--- src/tools/data/export.ts
+++ src/tools/data/export.ts
@@ -6,6 +6,7 @@ import { ToolsUtils } from 'tools/utils/toolsUtils'
 import { DB } from 'server/db/db'
 import { DatabaseService } from 'server/service/databaseService'
 
+import { _copyStaticFixtures } from './_copyStaticFixtures'
 import { _writeTableToFile } from './_writeTableToFile'
 
 const OUTPUT_DIR = path.join(__dirname, 'fixtures')
@@ -18,6 +19,8 @@ const exec = async (): Promise<void> => {
       _writeTableToFile(tableData, OUTPUT_DIR)
     })
   })
+
+  _copyStaticFixtures(OUTPUT_DIR)
 }
 
 ToolsUtils.exec(exec)
