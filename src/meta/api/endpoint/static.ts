6b36f8218f7aeef9102eb58cb5c125fce7aedb42
Mino Togna <minotogna@users.noreply.github.com>
2025-11-21 18:51:35 +0100

    5381: Dependencies upgrade: express & related (#5382)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/meta/api/endpoint/static.ts
+++ src/meta/api/endpoint/static.ts
@@ -1,6 +1,6 @@
 import { apiPath } from 'meta/api/endpoint/_utils'
 
 export const Static = {
-  file: (s3path = ':s3path(*)'): string => apiPath('static', 'file', s3path),
+  file: (s3path = '*s3path'): string => apiPath('static', 'file', s3path),
   files: (): string => apiPath('static', 'files'),
 }
