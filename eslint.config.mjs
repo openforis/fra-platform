277b8c0505c7571356eb5c097264e691c931aef4
Mino Togna <mino.togna@gmail.com>
2025-11-25 19:25:51 +0100

    5413: Client/styles cleanup: buttons pages [part 2 Measures]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -148,7 +148,7 @@ export default defineConfig([
               '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
               '^@?\\w',
             ],
-            ['^meta(\\/\\w)*'],
+            ['^meta(\\/\\w)*', '^utils(\\/\\w)*'],
             [
               '^client\\/store(\\/\\w)*',
               '^client\\/hooks(\\/\\w)*',
