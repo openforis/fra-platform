6c7e8d4677af724642dda9712490018094fdd46e
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-11-27 11:02:37 -0500

    5291 - Update DataCell props type definition

--- src/client/components/DataGrid/DataCell/types.ts
+++ src/client/components/DataGrid/DataCell/types.ts
@@ -1,21 +1,28 @@
 import { HTMLAttributes, PropsWithChildren } from 'react'
 
+type DivElementProps = Pick<HTMLAttributes<HTMLDivElement>, 'className' | 'id' | 'style'>
+
+type DivTooltipProps = Pick<
+  HTMLAttributes<HTMLDivElement>,
+  'data-tooltip-content' | 'data-tooltip-html' | 'data-tooltip-id'
+>
+
+type GridPlacementProps = Pick<HTMLAttributes<HTMLDivElement>['style'], 'gridColumn' | 'gridRow'>
+
+type DataCellStateProps = {
+  actions?: boolean
+  editable?: boolean
+  error?: boolean
+  firstCol?: boolean
+  firstHighlightCol?: boolean
+  header?: boolean
+  highlighted?: boolean
+  lastCol?: boolean
+  lastHighlightCol?: boolean
+  lastRow?: boolean
+  noBorder?: boolean
+}
+
 export type DataCellProps = PropsWithChildren<
-  Pick<
-    HTMLAttributes<HTMLDivElement>,
-    'className' | 'data-tooltip-content' | 'data-tooltip-html' | 'data-tooltip-id' | 'id' | 'style'
-  >
-> &
-  Pick<HTMLAttributes<HTMLDivElement>['style'], 'gridColumn' | 'gridRow'> & {
-    actions?: boolean
-    editable?: boolean
-    error?: boolean
-    firstCol?: boolean
-    firstHighlightCol?: boolean
-    header?: boolean
-    highlighted?: boolean
-    lastCol?: boolean
-    lastHighlightCol?: boolean
-    lastRow?: boolean
-    noBorder?: boolean
-  }
+  DivElementProps & DivTooltipProps & GridPlacementProps & DataCellStateProps
+>
