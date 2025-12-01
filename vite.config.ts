cec514d902be43ff26d84d3d6993243477ab02ed
Mino Togna <mino.togna@gmail.com>
2025-12-01 10:01:01 +0100

    4200: add @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 
 import path from 'path'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { version } from './package.json'
+
 const port = 9000
 
 export default defineConfig(({ mode }) => {
