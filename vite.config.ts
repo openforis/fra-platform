59a35743a01f056bdef8df1317bb7c4ca014771b
Mino Togna <minotogna@users.noreply.github.com>
2025-12-01 10:03:34 +0100

    4200 - Vite: initial configuration  (#5434)
    * 4200: Rename web-resources to public
    
    * 4200: add vite + config + index.html
    
    * 4200: remove import * as from utils
    
    * 4200: update server app.use
    
    * 4200: update DataPoints.scss @import partials path
    
    * 4200: add @vitejs/plugin-react-swc

---
+++ vite.config.ts
@@ -0,0 +1,92 @@
+import 'dotenv/config'
+
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { version } from './package.json'
+
+const port = 9000
+
+export default defineConfig(({ mode }) => {
+  const buildProd = mode === 'production'
+
+  return {
+    // Build Configuration
+    build: {
+      cssCodeSplit: false,
+      emptyOutDir: true,
+      rollupOptions: {
+        input: path.resolve(__dirname, 'index.html'),
+        output: {
+          dir: 'dist/client',
+        },
+      },
+      sourcemap: !buildProd,
+    },
+
+    // Environment Variables Definition
+    define: {
+      __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+      __GOOGLE_API__: JSON.stringify(process.env.FRA_GOOGLE_API),
+      __GOOGLE_MAPS_API_KEY__: JSON.stringify(process.env.FRA_GOOGLE_MAPS_API_KEY),
+      __APPLICATION_VERSION__: JSON.stringify(version),
+      __BUILD_DATE__: JSON.stringify(new Date().toISOString().split('T')[0]),
+    },
+
+    // Plugins
+    plugins: [react(), tsconfigPaths()],
+
+    // CSS Configuration
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '',
+          sassOptions: {
+            includePaths: [path.resolve(__dirname)],
+          },
+        },
+      },
+    },
+
+    // Preview Server Configuration
+    preview: {
+      port,
+    },
+
+    // Development Server Configuration
+    server: {
+      allowedHosts: ['localhost', 'fra-data.local'],
+      port,
+      watch: {
+        usePolling: true,
+      },
+      proxy: {
+        '/api': {
+          target: process.env.APP_URI ?? 'http://localhost:9001',
+        },
+        '/auth': {
+          target: process.env.APP_URI ?? 'http://localhost:9001',
+        },
+        '/css': {
+          target: process.env.APP_URI ?? 'http://localhost:9001',
+        },
+        '/definitions': {
+          target: process.env.APP_URI ?? 'http://localhost:9001',
+        },
+        '/socket.io': {
+          target: process.env.APP_URI ?? 'http://localhost:9001',
+          ws: true,
+        },
+      },
+    },
+
+    // Path Resolution
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, './src'),
+      },
+    },
+  }
+})
