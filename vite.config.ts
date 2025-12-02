b78843aba2edc0d6c4e15dc64b8bc6131f3c2233
Mino Togna <mino.togna@gmail.com>
2025-12-02 13:48:04 +0100

    4200: Vite: Fix heroku build (app version)

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ const port = 9000
 
 export default defineConfig(({ mode }) => {
   const buildProd = mode === 'production'
+  const appVersion = process.env.APP_VERSION ?? version
 
   return {
     // Build Configuration
@@ -31,7 +32,7 @@ export default defineConfig(({ mode }) => {
       __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
       __GOOGLE_API__: JSON.stringify(process.env.FRA_GOOGLE_API),
       __GOOGLE_MAPS_API_KEY__: JSON.stringify(process.env.FRA_GOOGLE_MAPS_API_KEY),
-      __APPLICATION_VERSION__: process.env.APP_VERSION ?? JSON.stringify(version),
+      __APPLICATION_VERSION__: JSON.stringify(appVersion),
       __BUILD_DATE__: JSON.stringify(new Date().toISOString().split('T')[0]),
     },
 
