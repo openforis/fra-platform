207c3eca045c3e728e6a6d5271a3bf499db26508
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2025-12-03 12:39:04 -0500

    5427 - Add panEuropean api docs (#5442)
    * 5427 - Add panEu docs initial setup
    
    * 5427 - Build table names/columns/variables dynamically
    
    * 5427 - Support multiple table selection
    
    * 5427 - Add Descriptions
    
    * 5427 - Use existing RecordAssessmentData

---
+++ src/docs/api/swaggerPanEuropean.config.ts
@@ -0,0 +1,24 @@
+import { Options } from 'swagger-jsdoc'
+
+export const swaggerPanEuropeanOptions: Options = {
+  apis: [
+    './src/server/api/**/*.panEuropean.apidoc.yml',
+    './src/docs/api/schemas/panEuropean/*.yml',
+    './src/docs/api/schemas/DescriptionCountryValues.yml',
+    './src/docs/api/schemas/RecordAssessmentData.yml',
+  ],
+  definition: {
+    info: {
+      description: '',
+      title: 'panEuropean API',
+      version: '1.0.0',
+    },
+    openapi: '3.0.0',
+    servers: [
+      {
+        url: '/api',
+      },
+    ],
+    tags: [{ name: 'panEuropean data' }, { name: 'Cycle data' }],
+  },
+}
