7107e9d2bc2ee13cd439767cb228946b12c05145
Miró Sorja <ping@mirosorja.com>
2025-12-03 12:34:51 +0200

    5363 - Clean up ResetPassword.tsx

--- src/client/pages/Login/ResetPassword/ResetPassword.tsx
+++ src/client/pages/Login/ResetPassword/ResetPassword.tsx
@@ -1,141 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router'
 
-import { ApiEndPoint } from 'meta/api/endpoint'
-
 import { useAppDispatch } from 'client/store/hooks'
 import { LoginActions } from 'client/store/login/actions'
-import { useAssessment } from 'client/store/meta/hooks/assessments'
-import { useCycle } from 'client/store/meta/hooks/cycles'
-import { useGetRequest } from 'client/hooks/getRequest'
+import { useCycleRouteParams } from 'client/hooks/routeParams'
 import Button, { ButtonSize } from 'client/components/Buttons/Button'
 import Flex from 'client/components/Layout/Flex'
-import { isError, LoginValidator } from 'client/pages/Login/utils/LoginValidator'
-import { Urls } from 'client/utils/urls'
+
+type FormData = {
+  email: string
+}
+
+// TODO: Use Form component
 
 const ResetPassword: React.FC = () => {
   const dispatch = useAppDispatch()
-  const assessment = useAssessment()
-  const cycle = useCycle()
-
-  const { t } = useTranslation()
+  const { assessmentName, cycleName } = useCycleRouteParams()
   const navigate = useNavigate()
+  const { t } = useTranslation()
 
-  const resetPasswordUuid = Urls.getRequestParam('resetPasswordUuid')
+  const {
+    clearErrors,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormData>({ defaultValues: { email: '' } })
 
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [password2, setPassword2] = useState<string>('')
-  const [errors, setErrors] = useState<Record<string, string>>({})
-
-  const { data, dispatch: fetchData } = useGetRequest(ApiEndPoint.User.resetPassword(), {
-    params: { resetPasswordUuid },
-  })
-
-  useEffect(() => {
-    if (resetPasswordUuid) fetchData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [resetPasswordUuid])
-
-  useEffect(() => {
-    if (data?.user?.email) setEmail(data.user.email)
-  }, [data])
-
-  const onResetPassword = async (): Promise<void> => {
-    const fieldErrors = LoginValidator.resetPasswordValidate(email)
-    setErrors(fieldErrors)
-
-    if (!isError(fieldErrors)) {
-      dispatch(
-        LoginActions.createResetPassword({
-          assessmentName: assessment.props.name,
-          cycleName: cycle.name,
-          email,
-          navigate,
-        })
-      )
-    }
+  const onSubmit = async (formData: FormData): Promise<void> => {
+    if (!formData.email) return
+    dispatch(LoginActions.createResetPassword({ assessmentName, cycleName, email: formData.email, navigate }))
   }
 
-  const onChangePassword = async (): Promise<void> => {
-    const fieldErrors = LoginValidator.invitationValidate(email, password, password2)
-    setErrors(fieldErrors)
-
-    if (!isError(fieldErrors)) {
-      await dispatch(LoginActions.changePassword({ email, password, resetPasswordUuid, navigate })).unwrap()
-    }
+  const onCancel = (): void => {
+    navigate(-1)
   }
-
-  if (!!resetPasswordUuid && !data?.user?.email)
-    return (
-      <div className="login__form">
-        <h3>{t('login.expired')}</h3>
-      </div>
-    )
 
   return (
     <div className="login__form">
-      {!resetPasswordUuid && <h3>{t('login.forgotPasswordTitle')}</h3>}
-
+      <h3>{t('login.forgotPasswordTitle')}</h3>
       <input
-        disabled={!!resetPasswordUuid}
-        name="email"
-        onChange={(event): void => setEmail(event.target.value)}
-        onFocus={(): void => setErrors({ ...errors, email: null })}
+        onFocus={(): void => clearErrors('email')}
         placeholder={t('login.email')}
         type="text"
-        value={email}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...register('email', { required: true })}
       />
-      {errors.email && <span className="login__field-error">{t(errors.email)}</span>}
+      {errors.email && <span className="login__field-error">{t('login.emailRequired')}</span>}
 
-      {resetPasswordUuid && (
-        <>
-          <input
-            onChange={(event): void => setPassword(event.target.value)}
-            onFocus={(): void => setErrors({ ...errors, password: null })}
-            placeholder={t('login.password')}
-            type="password"
-            value={password}
-          />
-          {errors.password && <span className="login__field-error">{t(errors.password)}</span>}
-
-          <input
-            onChange={(event): void => setPassword2(event.target.value)}
-            onFocus={(): void => setErrors({ ...errors, password2: null })}
-            placeholder={t('login.repeatPassword')}
-            type="password"
-            value={password2}
-          />
-          {errors.password2 && <span className="login__field-error">{t(errors.password2)}</span>}
-
-          <Flex gap={'16'} justifyContent="center">
-            <Button
-              label={t('login.cancel')}
-              onClick={(): void => {
-                navigate(-1)
-              }}
-              size={ButtonSize.l}
-            />
-
-            <Button label={t('login.changePassword')} onClick={onChangePassword} size={ButtonSize.l} />
-          </Flex>
-        </>
-      )}
-
-      {!resetPasswordUuid && (
-        <Flex gap={'16'} justifyContent="center">
-          <Button
-            label={t('login.cancel')}
-            onClick={(): void => {
-              navigate(-1)
-            }}
-            size={ButtonSize.l}
-          />
-
-          <Button label={t('login.resetPassword')} onClick={onResetPassword} size={ButtonSize.l} />
-        </Flex>
-      )}
+      <Flex gap={'16'} justifyContent="center">
+        <Button label={t('login.cancel')} onClick={onCancel} size={ButtonSize.l} />
+        <Button label={t('login.resetPassword')} onClick={handleSubmit(onSubmit)} size={ButtonSize.l} />
+      </Flex>
     </div>
   )
 }
