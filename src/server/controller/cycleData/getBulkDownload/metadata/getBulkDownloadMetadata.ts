af736b377b245066be98dba5e54678f2d6ddceb6
Mino Togna <minotogna@users.noreply.github.com>
2025-12-04 09:31:50 +0100

    5449 - Bulk Download: Annual + Interval years (#5465)
    * 5449 - Bulk Download: Annual years
    
    * 5449 - Bulk Download: Intervals years

--- src/server/controller/cycleData/getBulkDownload/metadata/getBulkDownloadMetadata.ts
+++ src/server/controller/cycleData/getBulkDownload/metadata/getBulkDownloadMetadata.ts
@@ -1,8 +1,10 @@
+import { getAnnualYears } from 'server/controller/cycleData/getBulkDownload/metadata/_getAnnualYears'
 import { getFraYears } from 'server/controller/cycleData/getBulkDownload/metadata/_getFraYears'
+import { getIntervalYears } from 'server/controller/cycleData/getBulkDownload/metadata/_getIntervalYears'
 import { BulkDownloadMetadata, PropsBulkDownload } from 'server/controller/cycleData/getBulkDownload/types'
 
 export const getBulkDownloadMetadata = (props: PropsBulkDownload): BulkDownloadMetadata => {
-  const years: BulkDownloadMetadata['years'] = [getFraYears(props)]
+  const years: BulkDownloadMetadata['years'] = [getFraYears(props), getAnnualYears(props), getIntervalYears(props)]
 
   return { years }
 }
