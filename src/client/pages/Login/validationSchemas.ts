95759c8d854301103c6bd409cade327666846664
Miró Sorja <ping@mirosorja.com>
2025-12-04 16:35:13 +0200

    5363 - simplify validation

--- src/client/pages/Login/validationSchemas.ts
+++ src/client/pages/Login/validationSchemas.ts
@@ -1,18 +1,13 @@
-import { z, ZodString } from 'zod'
+import { TFunction } from 'i18next'
+import { z, ZodEmail, ZodString } from 'zod'
 
-export const emailValidationSchema = (t: (key: string) => string): z.ZodString => {
-  return z
-    .string()
-    .min(1, { message: t('login.emptyEmail') })
-    .email({ message: t('login.invalidEmail') })
+export const emailValidationSchema = (t: TFunction): ZodEmail => {
+  return z.email({ message: t('login.invalidEmail') })
 }
 
-export const passwordValidationSchema = (t: (key: string) => string): ZodString => {
+export const passwordValidationSchema = (t: TFunction, minLength = 8): ZodString => {
   return z
     .string()
     .min(1, { message: t('login.noEmptyPassword') })
-    .min(6, { message: t('login.passwordMinLength') })
-    .refine((password) => /[a-z]/.test(password), { message: t('login.passwordLowercase') })
-    .refine((password) => /[A-Z]/.test(password), { message: t('login.passwordUppercase') })
-    .refine((password) => /[0-9]/.test(password), { message: t('login.passwordNumber') })
+    .min(minLength, { message: t('login.passwordMinLength', { count: minLength }) })
 }
