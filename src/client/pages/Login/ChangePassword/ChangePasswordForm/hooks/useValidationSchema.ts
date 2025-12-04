bf7692cf3fce8f6cb713816122495df717929f22
Miró Sorja <ping@mirosorja.com>
2025-12-04 14:55:53 +0200

    5363 - Add password validation and i18n

--- src/client/pages/Login/ChangePassword/ChangePasswordForm/hooks/useValidationSchema.ts
+++ src/client/pages/Login/ChangePassword/ChangePasswordForm/hooks/useValidationSchema.ts
@@ -8,10 +8,23 @@ export const useValidationSchema = (): FormValidationSchema => {
   const { t } = useTranslation()
 
   return useMemo<FormValidationSchema>(() => {
-    return z.object({
-      email: z.string().min(1, { message: t('login.emptyEmail') }),
-      password: z.string().min(1, { message: t('login.noEmptyPassword') }),
-      password2: z.string().min(1, { message: t('login.noEmptyPassword') }),
-    })
+    const passwordSchema = z
+      .string()
+      .min(1, { message: t('login.noEmptyPassword') })
+      .min(6, { message: t('login.passwordMinLength') })
+      .refine((password) => /[a-z]/.test(password), { message: t('login.passwordLowercase') })
+      .refine((password) => /[A-Z]/.test(password), { message: t('login.passwordUppercase') })
+      .refine((password) => /[0-9]/.test(password), { message: t('login.passwordNumber') })
+
+    return z
+      .object({
+        email: z.string().min(1, { message: t('login.emptyEmail') }),
+        password: passwordSchema,
+        password2: z.string().min(1, { message: t('login.noEmptyPassword') }),
+      })
+      .refine((data) => data.password === data.password2, {
+        message: t('login.noMatchPasswords'),
+        path: ['password2'],
+      })
   }, [t])
 }
