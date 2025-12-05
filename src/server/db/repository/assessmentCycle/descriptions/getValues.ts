f94c36b6a91662c3d9c9e75ee396f2444e1a5178
Mino Togna <minotogna@users.noreply.github.com>
2025-12-05 10:18:35 +0100

    5482: DescriptionRepository: getValues from multiple sections (#5483)

--- src/server/db/repository/assessmentCycle/descriptions/getValues.ts
+++ src/server/db/repository/assessmentCycle/descriptions/getValues.ts
@@ -2,6 +2,7 @@ import { CountryIso } from 'meta/area/countryIso'
 import { Assessment } from 'meta/assessment/assessment'
 import { Cycle } from 'meta/assessment/cycle'
 import { CommentableDescriptionName, DescriptionCountryValues } from 'meta/assessment/descriptionValue'
+import { SectionName } from 'meta/assessment/section'
 
 import { BaseProtocol, DB } from 'server/db/db'
 import { Schemas } from 'server/db/schemas'
@@ -10,12 +11,12 @@ type Props = {
   assessment: Assessment
   cycle: Cycle
   countryIso: CountryIso
-  sectionName?: string
+  sectionNames?: Array<SectionName>
   name?: CommentableDescriptionName
 }
 
 export const getValues = async (props: Props, client: BaseProtocol = DB): Promise<DescriptionCountryValues> => {
-  const { assessment, countryIso, cycle, name, sectionName } = props
+  const { assessment, countryIso, cycle, name, sectionNames } = props
 
   const schemaCycle = Schemas.getNameCycle(assessment, cycle)
 
@@ -26,7 +27,7 @@ export const getValues = async (props: Props, client: BaseProtocol = DB): Promis
                       jsonb_object_agg(d.name, d.value) as descriptions
                from ${schemaCycle}.descriptions d
                where d.country_iso = $1
-                 ${sectionName ? `and d.section_name = $2` : ``}
+                 ${sectionNames ? `and d.section_name in ($2:csv)` : ``}
                  ${name ? `and d.name = $3` : ``}
                group by 1, 2),
           agg2 as
@@ -36,7 +37,7 @@ export const getValues = async (props: Props, client: BaseProtocol = DB): Promis
                group by 1)
      select coalesce(jsonb_object_agg(a.country_iso, a.descriptions), '{}'::jsonb) as descriptions
      from agg2 a`,
-    [countryIso, sectionName, name],
+    [countryIso, sectionNames, name],
     ({ descriptions }) => descriptions
   )
 }
