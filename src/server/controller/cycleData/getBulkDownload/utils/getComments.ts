ec2b43c2c5efdb18dcd18be414d486565249912f
Mino Togna <mino.togna@gmail.com>
2025-12-05 07:38:58 +0100

    5482: DescriptionRepository: getValues from multiple sections

--- src/server/controller/cycleData/getBulkDownload/utils/getComments.ts
+++ src/server/controller/cycleData/getBulkDownload/utils/getComments.ts
@@ -52,8 +52,9 @@ const _extractTextAndLinks = (html: string): string => {
 }
 
 export const getComments = async (props: Props): Promise<string> => {
-  const { countryIso, name, sectionName } = props
-  const values = await CycleDataController.Description.getDescriptionValues(props)
+  const { assessment, countryIso, cycle, name, sectionName } = props
+  const propsValue = { assessment, countryIso, cycle, name, sectionNames: [sectionName] }
+  const values = await CycleDataController.Description.getDescriptionValues(propsValue)
   const comments = values?.[countryIso]?.[sectionName]?.[name]?.text
 
   return comments ? _extractTextAndLinks(comments) : ''
