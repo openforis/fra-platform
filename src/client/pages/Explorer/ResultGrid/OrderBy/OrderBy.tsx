11c8761310feb871743976e6c9f30c01a96f17e9
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-12-05 17:57:09 -0500

    5476 - Add order by button

---
+++ src/client/pages/Explorer/ResultGrid/OrderBy/OrderBy.tsx
@@ -0,0 +1,62 @@
+import React, { ReactElement, useCallback } from 'react'
+
+import { ExplorerOrderBy, ExplorerOrderByDirection } from 'meta/explorer/selection'
+import { DimensionName } from 'meta/measurement/dimension'
+import { MeasureName } from 'meta/measurement/measure'
+
+import { ExplorerSelectionActions } from 'client/store/explorer/selection/actions'
+import { useExplorerOrderBy } from 'client/store/explorer/selection/hooks/orderBy'
+import { useAppDispatch } from 'client/store/hooks'
+import { useSectionRouteParams } from 'client/hooks/routeParams'
+import Button, { ButtonSize, ButtonType } from 'client/components/Buttons/Button'
+
+type Props = {
+  dimensionName: DimensionName
+  measureName: MeasureName
+}
+
+const OrderBy: React.FC<Props> = (props: Props): ReactElement => {
+  const { dimensionName, measureName } = props
+
+  const dispatch = useAppDispatch()
+  const orderBy = useExplorerOrderBy()
+  const { assessmentName, cycleName, sectionName } = useSectionRouteParams()
+
+  const active = orderBy?.dimensionName === dimensionName && orderBy?.measureName === measureName
+  const activeAsc = active && orderBy?.order === ExplorerOrderByDirection.asc
+  const activeDesc = active && orderBy?.order === ExplorerOrderByDirection.desc
+  const iconName = activeDesc ? 'sort-amount-desc' : 'sort-amount-asc'
+
+  const onClick = useCallback<() => void>(() => {
+    let orderByUpdate: ExplorerOrderBy | null = { dimensionName, measureName, order: ExplorerOrderByDirection.asc }
+
+    if (activeAsc) {
+      orderByUpdate = { dimensionName, measureName, order: ExplorerOrderByDirection.desc }
+    } else if (activeDesc) {
+      orderByUpdate = null
+    }
+
+    dispatch(
+      ExplorerSelectionActions.setOrderBy({
+        assessmentName,
+        cycleName,
+        orderBy: orderByUpdate,
+        sectionName,
+      })
+    )
+  }, [activeAsc, activeDesc, assessmentName, cycleName, dimensionName, dispatch, measureName, sectionName])
+
+  return (
+    <Button
+      bgTransparent
+      iconName={iconName}
+      inverse
+      noBorder
+      onClick={onClick}
+      size={ButtonSize.m}
+      type={active ? ButtonType.primary : ButtonType.anonymous}
+    />
+  )
+}
+
+export default OrderBy
--- src/client/pages/Explorer/ResultGrid/ResultGrid.tsx
+++ src/client/pages/Explorer/ResultGrid/ResultGrid.tsx
@@ -1,11 +1,12 @@
 import './ResultGrid.scss'
 import React from 'react'
-
 import classNames from 'classnames'
-import { Objects } from 'utils/objects'
 
 import { CountryIso } from 'meta/area/countryIso'
 import { Axis, AxisType } from 'meta/explorer/selection'
+import { DimensionName } from 'meta/measurement/dimension'
+import { MeasureName } from 'meta/measurement/measure'
+import { Objects } from 'utils/objects'
 
 import { useGetExplorerSectionData } from 'client/store/explorer/data/hooks/data'
 import { useExplorerSectionMetadata } from 'client/store/explorer/metadata/hooks/metadata'
@@ -13,6 +14,7 @@ import { DataCell, DataGrid } from 'client/components/DataGrid'
 import { useHideGrid } from 'client/pages/Explorer/hooks/useHideGrid'
 import MeasureTitle from 'client/pages/Explorer/ResultGrid/MeasureTitle/MeasureTitle'
 import Observation from 'client/pages/Explorer/ResultGrid/Observation/Observation'
+import OrderBy from 'client/pages/Explorer/ResultGrid/OrderBy'
 import { CountryEntry } from 'client/pages/Explorer/ResultGrid/types'
 import { ExplorerGridProps } from 'client/pages/Explorer/types'
 
@@ -101,6 +103,12 @@ export const ResultGrid: React.FC<ExplorerGridProps> = (props: ExplorerGridProps
       {xCombinations.map((combination, idx) => {
         const isPrimaryVariable = xAxisVariableCount === 1
         const axisType = xAxisSelection[isPrimaryVariable ? 0 : 1]
+        const measureIdx = xAxisSelection.indexOf(AxisType.measures)
+        const dimensionIdx = xAxisSelection.indexOf(AxisType.dimensions)
+        const measureName = measureIdx === -1 ? null : _getCombinationStringValue<MeasureName>(combination[measureIdx])
+        const dimensionName =
+          dimensionIdx === -1 ? null : _getCombinationStringValue<DimensionName>(combination[dimensionIdx])
+
         return (
           <DataCell
             key={`${combination.map(_getCombinationStringValue).join('-')}-x-header`}
@@ -112,11 +120,15 @@ export const ResultGrid: React.FC<ExplorerGridProps> = (props: ExplorerGridProps
             header
             lastCol={idx === xCombinations.length - 1}
           >
-            {renderLabel({
-              // If there are two X variables selected, render only the secondary variable header
-              axisType,
-              value: combination[xAxisVariableCount - 1],
-            })}
+            <div className="result-grid__header-content">
+              {renderLabel({
+                // If there are two X variables selected, render only the secondary variable header
+                axisType,
+                value: combination[xAxisVariableCount - 1],
+              })}
+
+              {measureName && dimensionName && <OrderBy dimensionName={dimensionName} measureName={measureName} />}
+            </div>
           </DataCell>
         )
       })}
