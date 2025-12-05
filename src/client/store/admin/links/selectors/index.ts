1eb70bd0ca5b47f0c0d672e169e17c5c7f7ceec2
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-12-04 21:14:01 -0500

    5366 - Fix undefined error when refreshing links page

--- src/client/store/admin/links/selectors/index.ts
+++ src/client/store/admin/links/selectors/index.ts
@@ -5,8 +5,8 @@ import { LinksState } from 'client/store/admin/links/state'
 import { AdminSliceName } from 'client/store/admin/name'
 import { RootState } from 'client/store/types'
 
-const _getState = (state: RootState): LinksState => state[AdminSliceName][LinksSlice.name]
-const isVerificationInProgress = createSelector(_getState, (links) => links.isVerificationInProgress)
+const _getState = (state: RootState): LinksState | undefined => state[AdminSliceName]?.[LinksSlice.name]
+const isVerificationInProgress = createSelector(_getState, (links) => links?.isVerificationInProgress)
 
 export const LinksSelectors = {
   isVerificationInProgress,
