0f69469a413ea5d724e9845811c16aeb66468db0
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-12-08 10:10:09 -0500

    5476 - Use new Flex component

--- src/client/pages/Explorer/ResultGrid/ResultGrid.tsx
+++ src/client/pages/Explorer/ResultGrid/ResultGrid.tsx
@@ -11,6 +11,7 @@ import { Objects } from 'utils/objects'
 import { useGetExplorerSectionData } from 'client/store/explorer/data/hooks/data'
 import { useExplorerSectionMetadata } from 'client/store/explorer/metadata/hooks/metadata'
 import { DataCell, DataGrid } from 'client/components/DataGrid'
+import Flex from 'client/components/Layout/Flex'
 import { useHideGrid } from 'client/pages/Explorer/hooks/useHideGrid'
 import MeasureTitle from 'client/pages/Explorer/ResultGrid/MeasureTitle/MeasureTitle'
 import Observation from 'client/pages/Explorer/ResultGrid/Observation/Observation'
@@ -105,9 +106,10 @@ export const ResultGrid: React.FC<ExplorerGridProps> = (props: ExplorerGridProps
         const axisType = xAxisSelection[isPrimaryVariable ? 0 : 1]
         const measureIdx = xAxisSelection.indexOf(AxisType.measures)
         const dimensionIdx = xAxisSelection.indexOf(AxisType.dimensions)
-        const measureName = measureIdx === -1 ? null : _getCombinationStringValue<MeasureName>(combination[measureIdx])
+        const measureName =
+          measureIdx === -1 ? undefined : _getCombinationStringValue<MeasureName>(combination[measureIdx])
         const dimensionName =
-          dimensionIdx === -1 ? null : _getCombinationStringValue<DimensionName>(combination[dimensionIdx])
+          dimensionIdx === -1 ? undefined : _getCombinationStringValue<DimensionName>(combination[dimensionIdx])
 
         return (
           <DataCell
@@ -120,15 +122,17 @@ export const ResultGrid: React.FC<ExplorerGridProps> = (props: ExplorerGridProps
             header
             lastCol={idx === xCombinations.length - 1}
           >
-            <div className="result-grid__header-content">
-              {renderLabel({
-                // If there are two X variables selected, render only the secondary variable header
-                axisType,
-                value: combination[xAxisVariableCount - 1],
-              })}
+            <Flex alignItems="center" justifyContent="space-between">
+              <span className="explorer-result-grid__header-label">
+                {renderLabel({
+                  // If there are two X variables selected, render only the secondary variable header
+                  axisType,
+                  value: combination[xAxisVariableCount - 1],
+                })}
+              </span>
 
               {measureName && dimensionName && <OrderBy dimensionName={dimensionName} measureName={measureName} />}
-            </div>
+            </Flex>
           </DataCell>
         )
       })}
