b23169a327f676fc23a6758422a52d6d1ab5ea8a
Miró Sorja <ping@mirosorja.com>
2025-12-09 13:30:10 +0200

    5360 - Add activity log

--- src/server/controller/user/changePassword.ts
+++ src/server/controller/user/changePassword.ts
@@ -1,6 +1,8 @@
+import { ActivityLogMessage } from 'meta/assessment/activityLog'
 import { UserResetPassword } from 'meta/user/resetPassword'
 
 import { BaseProtocol, DB } from 'server/db/db'
+import { ActivityLogRepository } from 'server/db/repository/public/activityLog'
 import { UserRepository } from 'server/db/repository/public/user'
 import { UserProviderRepository } from 'server/db/repository/public/userProvider'
 import { UserResetPasswordRepository } from 'server/db/repository/public/userResetPassword'
@@ -26,6 +28,12 @@ export const changePassword = async (props: Props, client: BaseProtocol = DB): P
     const userAuthProvider = await UserProviderRepository.update({ userUuid: user.uuid, password }, t)
     if (!userAuthProvider) return null
 
-    return UserResetPasswordRepository.update({ uuid: userResetPassword.uuid }, t)
+    const updatedResetPassword = await UserResetPasswordRepository.update({ uuid: userResetPassword.uuid }, t)
+
+    const target = { email, usersResetPasswordUuid: updatedResetPassword.uuid }
+    const activityLog = { target, section: 'users', message: ActivityLogMessage.userPasswordChange, user }
+    await ActivityLogRepository.insertActivityLog({ activityLog }, t)
+
+    return updatedResetPassword
   })
 }
