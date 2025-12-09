ecfd3e90632b330a84f9422b466ca5feb3ab76da
Miró Sorja <ping@mirosorja.com>
2025-12-09 13:37:00 +0200

    5360 - Reset password refactor + Revisit UserResetPassword logic (#5508)
    * 5360 - controller/changePassword: add tx
    
    * 5360 - Add activity log

--- src/server/controller/user/changePassword.ts
+++ src/server/controller/user/changePassword.ts
@@ -1,30 +1,39 @@
+import { ActivityLogMessage } from 'meta/assessment/activityLog'
 import { UserResetPassword } from 'meta/user/resetPassword'
 
 import { BaseProtocol, DB } from 'server/db/db'
+import { ActivityLogRepository } from 'server/db/repository/public/activityLog'
 import { UserRepository } from 'server/db/repository/public/user'
 import { UserProviderRepository } from 'server/db/repository/public/userProvider'
 import { UserResetPasswordRepository } from 'server/db/repository/public/userResetPassword'
 
-export const changePassword = async (
-  props: {
-    email: string
-    password: string
-    resetPasswordUuid: string
-  },
-  client: BaseProtocol = DB
-): Promise<UserResetPassword | null> => {
+type Props = {
+  email: string
+  password: string
+  resetPasswordUuid: string
+}
+
+type Returned = UserResetPassword | null
+
+export const changePassword = async (props: Props, client: BaseProtocol = DB): Promise<Returned> => {
   const { email, password, resetPasswordUuid } = props
 
   return client.tx(async (t) => {
-    const userResetPassword = await UserResetPasswordRepository.read({ uuid: resetPasswordUuid })
+    const userResetPassword = await UserResetPasswordRepository.read({ uuid: resetPasswordUuid }, t)
     if (!userResetPassword) return null
 
-    const user = await UserRepository.getOne({ id: userResetPassword.userId })
+    const user = await UserRepository.getOne({ id: userResetPassword.userId }, t)
     if (user.email !== email) return null
 
-    const userAuthProvider = await UserProviderRepository.update({ userUuid: user.uuid, password })
+    const userAuthProvider = await UserProviderRepository.update({ userUuid: user.uuid, password }, t)
     if (!userAuthProvider) return null
 
-    return UserResetPasswordRepository.update({ uuid: userResetPassword.uuid }, t)
+    const updatedResetPassword = await UserResetPasswordRepository.update({ uuid: userResetPassword.uuid }, t)
+
+    const target = { email, usersResetPasswordUuid: updatedResetPassword.uuid }
+    const activityLog = { target, section: 'users', message: ActivityLogMessage.userPasswordChange, user }
+    await ActivityLogRepository.insertActivityLog({ activityLog }, t)
+
+    return updatedResetPassword
   })
 }
