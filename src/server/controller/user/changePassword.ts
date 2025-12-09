35122e39babe0b27fba14d2ff81699dc8765a420
Miró Sorja <ping@mirosorja.com>
2025-12-09 13:18:52 +0200

    5360 - controller/changePassword: add tx

--- src/server/controller/user/changePassword.ts
+++ src/server/controller/user/changePassword.ts
@@ -5,24 +5,25 @@ import { UserRepository } from 'server/db/repository/public/user'
 import { UserProviderRepository } from 'server/db/repository/public/userProvider'
 import { UserResetPasswordRepository } from 'server/db/repository/public/userResetPassword'
 
-export const changePassword = async (
-  props: {
-    email: string
-    password: string
-    resetPasswordUuid: string
-  },
-  client: BaseProtocol = DB
-): Promise<UserResetPassword | null> => {
+type Props = {
+  email: string
+  password: string
+  resetPasswordUuid: string
+}
+
+type Returned = UserResetPassword | null
+
+export const changePassword = async (props: Props, client: BaseProtocol = DB): Promise<Returned> => {
   const { email, password, resetPasswordUuid } = props
 
   return client.tx(async (t) => {
-    const userResetPassword = await UserResetPasswordRepository.read({ uuid: resetPasswordUuid })
+    const userResetPassword = await UserResetPasswordRepository.read({ uuid: resetPasswordUuid }, t)
     if (!userResetPassword) return null
 
-    const user = await UserRepository.getOne({ id: userResetPassword.userId })
+    const user = await UserRepository.getOne({ id: userResetPassword.userId }, t)
     if (user.email !== email) return null
 
-    const userAuthProvider = await UserProviderRepository.update({ userUuid: user.uuid, password })
+    const userAuthProvider = await UserProviderRepository.update({ userUuid: user.uuid, password }, t)
     if (!userAuthProvider) return null
 
     return UserResetPasswordRepository.update({ uuid: userResetPassword.uuid }, t)
