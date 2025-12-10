beea3858ca52dd11f8f627994dca53969d028b44
Mino Togna <minotogna@users.noreply.github.com>
2025-12-10 19:18:56 +0100

    5521 - Dependencies upgrade: express & co (#5538)
    * 5521 - Dependencies upgrade: express & co
    
    * 5521 - Dependencies upgrade: fix json body parser

--- src/server/serverInit.ts
+++ src/server/serverInit.ts
@@ -1,10 +1,11 @@
 import path from 'path'
-import bodyParser from 'body-parser'
 import compression from 'compression'
 import wwwhisper from 'connect-wwwhisper'
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import morgan from 'morgan'
+
+import { Objects } from 'utils/objects'
 
 import { Api } from 'server/api'
 import { Proxy } from 'server/proxy/proxy'
@@ -18,21 +19,31 @@ import * as resourceCacheControl from './resourceCacheControl'
 
 export const serverInit = (): void => {
   const app = express()
+
   Proxy.init(app)
 
   app.use(wwwhisper(false))
   app.use(cookieParser())
   app.set('query parser', 'extended')
+  // TODO: pass content-type from our client requests - now it's empty and therefore solution below
+  // app.use(express.json({ limit: '5000kb', type: ['application/json', 'application/*+json'] }))
+  app.use(
+    express.json({
+      limit: '5000kb',
+      type: (req) => {
+        const contentType = req.headers['content-type']
+        return (
+          Objects.isEmpty(contentType) || contentType.startsWith('application/json') || contentType.includes('+json')
+        )
+      },
+    })
+  )
+  resourceCacheControl.init(app)
+  app.use(compression({ threshold: 512 }))
 
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
   }
-
-  app.use(bodyParser.json({ limit: '5000kb' }))
-
-  resourceCacheControl.init(app)
-
-  app.use(compression({ threshold: 512 }))
 
   app.use('/assets', express.static(`${__dirname}/../client/assets`))
   app.use('/css', express.static(`${__dirname}/../client/css`))
