00e7358d821e74cc77b08f0a2edf44faa41cdd3a
Miró Sorja <ping@mirosorja.com>
2025-12-18 16:11:54 +0200

    5546 - E2E: Fix test

--- src/test/e2e/tests/03-login.spec.ts
+++ src/test/e2e/tests/03-login.spec.ts
@@ -4,12 +4,10 @@ test.describe('Login', () => {
   test('should login with test user credentials', async ({ page }) => {
     await page.goto('/assessments/fra/2025/login')
 
-    await page.click('button:has-text("Sign in with FRA")')
-
     await page.fill('input[name="email"]', 'test@test.com')
     await page.fill('input[type="password"]', 'password123')
 
-    await page.click('button.button:has-text("Login")')
+    await page.click('button.button:has-text("Sign in with FRA")')
 
     await expect(page).toHaveURL('/assessments/fra/2025')
 
@@ -19,12 +17,10 @@ test.describe('Login', () => {
   test('should show error with invalid credentials', async ({ page }) => {
     await page.goto('/assessments/fra/2025/login')
 
-    await page.click('button:has-text("Sign in with FRA")')
-
     await page.fill('input[name="email"]', 'wrong@email.com')
     await page.fill('input[type="password"]', 'wrongpassword')
 
-    await page.click('button.button:has-text("Login")')
+    await page.click('button.button:has-text("Sign in with FRA")')
 
     await expect(page.getByText("We couldn't find any user matching these credentials.")).toBeVisible()
     await expect(
