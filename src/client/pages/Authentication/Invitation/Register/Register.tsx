f4e95696b12f5e5bf3918dad0cc0a4f42118c6f1
Miró Sorja <ping@mirosorja.com>
2025-12-18 15:30:09 +0200

    5546 - Invitation: Add Register

--- src/client/pages/Authentication/Invitation/Register/Register.tsx
+++ src/client/pages/Authentication/Invitation/Register/Register.tsx
@@ -1,15 +1,79 @@
 import React from 'react'
-import { useParams } from 'react-router'
+import { useTranslation } from 'react-i18next'
 
-// Todo - new user accepting invitation
+import { ApiEndPoint } from 'meta/api/endpoint'
+import { Assessments } from 'meta/assessment/assessments'
+import { AuthProvider } from 'meta/user/auth'
+import { Users } from 'meta/user/users'
+
+import Icon from 'client/components/Icon'
+import ButtonGoogle from 'client/pages/Authentication/ButtonGoogle'
+import Divider from 'client/pages/Authentication/Divider'
+import FormLogin from 'client/pages/Authentication/FormLogin'
+import { useData } from 'client/pages/Authentication/Invitation/hooks/useData'
+import { videoResources } from 'client/pages/Tutorials'
+
+import { useOnSuccess } from './hooks/useOnSuccess'
+
 const Register: React.FC = () => {
-  const { invitationUuid } = useParams<{ invitationUuid: string }>()
+  const { i18n, t } = useTranslation()
+  const data = useData()
+  const onSuccess = useOnSuccess({ data })
+
+  if (!data) return null
+
+  const { assessmentName, cycleName, user, userInvitation, userProviders } = data
+  const { countryIso, uuid: invitationUuid } = userInvitation
+
+  const invitationMessageParams = {
+    assessment: t(Assessments.getShortLabel(assessmentName)),
+    country: t(`area.${countryIso}.listName`),
+    cycle: cycleName,
+    role: t(Users.getI18nRoleLabelKey(userInvitation.role)),
+  }
+
+  const showPassword2 = !userProviders?.includes(AuthProvider.local)
+  const authProviderNames = userProviders?.join(', ') ?? ''
 
   return (
     <div className="login-form">
-      <p>{invitationUuid}</p>
-      <button>With FRA</button>
-      <button>With Google</button>
+      <h3>{t('login.invitationMessage', invitationMessageParams)}</h3>
+
+      {userProviders && userProviders.length > 0 && (
+        <h3>{t('login.invitationProvidersRegistered', { authProviderNames })}</h3>
+      )}
+
+      <FormLogin
+        action={ApiEndPoint.Auth.login()}
+        disableEmail
+        email={user?.email}
+        invitationUuid={invitationUuid}
+        labels={{ submit: t('login.acceptInvitationWithFra') }}
+        onSuccess={onSuccess}
+        password2={showPassword2}
+      />
+
+      <a
+        className="btn-help"
+        href={videoResources[0].url[i18n.resolvedLanguage] ?? videoResources[0].url.en}
+        rel="noreferrer"
+        target="_blank"
+      >
+        <Icon name="video" /> {t(videoResources[0].labelKeyShort)}
+      </a>
+
+      <Divider />
+
+      <ButtonGoogle data={data} label="login.acceptInvitationWithGoogle" />
+
+      <a
+        className="btn-help"
+        href={videoResources[1].url[i18n.resolvedLanguage] ?? videoResources[1].url.en}
+        rel="noreferrer"
+        target="_blank"
+      >
+        <Icon name="video" /> {t(videoResources[1].labelKeyShort)}
+      </a>
     </div>
   )
 }
