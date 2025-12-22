47b3a5586b7d82331e873296c17b38d03e1d8ef1
Mino Togna <minotogna@users.noreply.github.com>
2025-12-22 15:49:52 +0100

    5507 - [2] Change password: Login user after submit (#5599)
    * 55071 - [2] Change password: Login user after submit
    
    * 55071 - update generatePasswordHash.ts script
    
    * 55071 - [2] Change password: Login user after submit -> add confirmation message
    
    * 55071 - Remove tablePaginated selector console warning

--- src/server/api/auth/strategy/local.ts
+++ src/server/api/auth/strategy/local.ts
@@ -1,21 +1,15 @@
-import { Request } from 'express'
 import { PassportStatic } from 'passport'
-import { VerifiedCallback } from 'passport-jwt'
-import passportLocal from 'passport-local'
+import passportLocal, { VerifyFunctionWithRequest } from 'passport-local'
 
 import { Objects } from 'utils/objects'
 import { RegExps } from 'utils/regExps'
 
 import { localAcceptInvitation } from 'server/api/auth/strategy/_local/acceptInvitation'
+import { localChangePassword } from 'server/api/auth/strategy/_local/changePassword'
 import { localLogin } from 'server/api/auth/strategy/_local/login'
 
-const localStrategyVerifyCallback = async (
-  req: Request,
-  email: string,
-  password: string,
-  done: VerifiedCallback
-): Promise<void> => {
-  const { invitationUuid } = req.body
+const localStrategyVerifyCallback: VerifyFunctionWithRequest = (req, email, password, done): void | Promise<void> => {
+  const { invitationUuid, resetPasswordUuid } = req.body
   const sendErr = (message: string): void => done(null, false, { message })
 
   try {
@@ -30,6 +24,11 @@ const localStrategyVerifyCallback = async (
     // accept invitation
     if (invitationUuid) {
       return localAcceptInvitation({ done, req, sendErr })
+    }
+
+    // change password
+    if (resetPasswordUuid) {
+      return localChangePassword({ done, req, sendErr })
     }
 
     // login
