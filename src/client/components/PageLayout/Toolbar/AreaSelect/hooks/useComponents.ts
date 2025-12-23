f819e9dc0cdd5f0386186b826505442dc3d89eb7
Mino Togna <mino.togna@gmail.com>
2025-12-23 08:54:40 +0100

    5602 - [2] Refactor AreaSelector with ReactSelect: Single Value

--- src/client/components/PageLayout/Toolbar/AreaSelect/hooks/useComponents.ts
+++ src/client/components/PageLayout/Toolbar/AreaSelect/hooks/useComponents.ts
@@ -2,11 +2,13 @@ import { useMemo } from 'react'
 
 import { SelectProps } from 'client/components/Inputs/Select'
 import GroupHeading from 'client/components/PageLayout/Toolbar/AreaSelect/GroupHeading'
+import SingleValue from 'client/components/PageLayout/Toolbar/AreaSelect/SingleValue'
 
 export const useComponents = (): SelectProps['components'] => {
   return useMemo<SelectProps['components']>(() => {
     return {
       GroupHeading,
+      SingleValue,
     }
   }, [])
 }
