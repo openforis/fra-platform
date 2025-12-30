8c0badbea9c4874231e53b088907371c8dd0d293
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-12-30 17:21:22 -0500

    5491 - Init local worker on startup

--- src/server/start.ts
+++ src/server/start.ts
@@ -1,6 +1,16 @@
-import 'tsconfig-paths/register'
 import 'dotenv/config'
+import 'tsconfig-paths/register'
 
+import { Logger } from './utils/logger'
+import { NodeEnv, ProcessEnv } from './utils/processEnv'
 import { serverInit } from './serverInit'
 
 serverInit()
+
+if (ProcessEnv.nodeEnv === NodeEnv.development) {
+  void import('./api/admin/links/triggerVerifyLinksWorker')
+    .then(({ triggerVerifyLinksWorker }) => triggerVerifyLinksWorker())
+    .catch((error) => {
+      Logger.error(`[verifyLinks] failed to start local worker: ${JSON.stringify(error)}`)
+    })
+}
