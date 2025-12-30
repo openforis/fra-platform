e22458dcfbb5a1510da70cebf9f53d4cc0604203
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2025-12-30 18:31:01 -0500

    5491 - Add triggerVerifyLinksWorker

--- src/server/start.ts
+++ src/server/start.ts
@@ -8,7 +8,7 @@ import { serverInit } from './serverInit'
 serverInit()
 
 if (ProcessEnv.nodeEnv === NodeEnv.development) {
-  void import('./api/admin/links/triggerVerifyLinksWorker')
+  void import('./worker/tasks/verifyLinks/triggerVerifyLinksWorker')
     .then(({ triggerVerifyLinksWorker }) => triggerVerifyLinksWorker())
     .catch((error) => {
       Logger.error(`[verifyLinks] failed to start local worker: ${JSON.stringify(error)}`)
