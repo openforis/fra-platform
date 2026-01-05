b0a34ea58e05a04c81a84ca96502fb909626fa0d
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-01-05 11:01:03 -0500

    5624 - Add Access JRC GWIS hyperlink

--- src/client/pages/Geo/StatisticsSidePanel/BurnedAreaPanel/BurnedAreaPanel.tsx
+++ src/client/pages/Geo/StatisticsSidePanel/BurnedAreaPanel/BurnedAreaPanel.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { CountryIso } from 'meta/area/countryIso'
 
 import { useCountryRouteParams } from 'client/hooks/routeParams'
+import Link from 'client/components/Links/Link'
 import StatisticsTable from 'client/pages/Geo/StatisticsSidePanel/StatisticsTable'
 
 import { useBurnedAreaData } from './hooks/useBurnedAreaData'
@@ -20,22 +21,36 @@ const BurnedAreaPanel: React.FC<Props> = (props: Props) => {
 
   const { columns, errorKey, loading, tableData, units } = useBurnedAreaData()
 
-  if (!loading && tableData.length === 0 && !errorKey) return <p>{t('geo.error.statistics.foundNoData')}</p>
+  const gwisUrl = `https://gwis.jrc.ec.europa.eu/apps/gwis.statistics/estimates/${countryIso}2012`
 
-  if (!loading && errorKey?.length > 0) {
-    return <p>{t('geo.error.statistics.failedToFetch', { error: t(errorKey) })}</p>
+  const renderContent = (): React.ReactNode => {
+    if (loading) return <p>{t('common.loading')}</p>
+
+    if (errorKey) return <p>{t('geo.error.statistics.failedToFetch', { error: t(errorKey) })}</p>
+
+    if (tableData.length === 0) return <p>{t('geo.error.statistics.foundNoData')}</p>
+
+    return (
+      <StatisticsTable
+        columns={columns}
+        fileName={`burned-area-estimations-${countryIso}-${year}`}
+        loaded={!loading}
+        tableData={tableData}
+        units={units}
+      />
+    )
   }
 
-  if (loading) return <p>{t('common.loading')}</p>
-
   return (
-    <StatisticsTable
-      columns={columns}
-      fileName={`burned-area-estimations-${countryIso}-${year}`}
-      loaded={!loading}
-      tableData={tableData}
-      units={units}
-    />
+    <>
+      <div className="geo-statistics-side-panel__jrc-gwis-link">
+        <Link rel="noreferrer" target="_blank" to={gwisUrl}>
+          {t('geo.accessJrcGwis')}
+        </Link>
+      </div>
+
+      {renderContent()}
+    </>
   )
 }
 
