9c20f4071db5c9eb78b135f960a1b1f80e682106
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-01-06 09:42:52 -0500

    5491 - Admin links job on separate Heroku dyno - Add worker trigger  (#5618)
    * 5491 - Improve visitCycleLinks logs
    
    * 5491 - Add task-verifyLinks to procfile
    
    * 5491 - Init local worker on startup
    
    * 5491 - Add triggerVerifyLinksWorker
    
    * 5491 - Move link related files
    
    * 5491 - Fix lingering instances in dev

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: yarn start
 worker: node dist/server/worker
+task-verifyLinks: node dist/server/worker/tasks/verifyLinks.js
---
+++ src/server/worker/tasks/verifyLinks/visitCycleLinks/utils/mergeLinks.ts
@@ -0,0 +1,25 @@
+import { LinkToVisit } from 'meta/cycleData/links/link'
+
+type Props = {
+  linksToVisit: Array<LinkToVisit>
+}
+
+export const mergeLinks = (props: Props): Array<LinkToVisit> => {
+  const { linksToVisit } = props
+
+  const mergedLinksMap = linksToVisit.reduce(
+    (acc, curr) => {
+      const key = `${curr.countryIso}_${curr.link ?? ''}`
+
+      if (!acc[key]) {
+        acc[key] = { ...curr, locations: [...curr.locations] }
+      } else {
+        acc[key].locations.push(...curr.locations)
+      }
+      return acc
+    },
+    {} as { [key: string]: LinkToVisit }
+  )
+
+  return Object.values(mergedLinksMap)
+}
