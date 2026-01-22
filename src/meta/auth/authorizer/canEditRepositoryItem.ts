0b2d99f50e1ff5a65b304475a4e572ac4624c663
Miró Sorja <ping@mirosorja.com>
2026-01-22 15:53:18 +0200

    5647 - Fix auth for repository items

--- src/meta/auth/authorizer/canEditRepositoryItem.ts
+++ src/meta/auth/authorizer/canEditRepositoryItem.ts
@@ -1,8 +1,17 @@
 import { Country } from 'meta/area/country'
 import { Cycle } from 'meta/assessment/cycle'
 import { canEditCountryProps } from 'meta/auth/authorizer/canEditCountryProps'
+import { Authorizer } from 'meta/auth/authorizer/index'
+import { CollaboratorEditPropertyType } from 'meta/user/role/collaborator'
 import { User } from 'meta/user/user'
+import { Users } from 'meta/user/users'
 
 export const canEditRepositoryItem = (props: { cycle: Cycle; country: Country; user: User }): boolean => {
+  const { country, cycle, user } = props
+  if (Users.isReviewer(user, country.countryIso, cycle)) {
+    const permission = CollaboratorEditPropertyType.descriptions
+    return Authorizer.canEditSectionData({ country, cycle, permission, section: undefined, user })
+  }
+
   return canEditCountryProps({ ...props, allowCollaborator: true })
 }
