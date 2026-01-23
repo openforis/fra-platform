f1ba82e86eba8928ff510fa3d0b8787bce745101
Miró Sorja <ping@mirosorja.com>
2026-01-23 08:52:09 +0200

    5647 - Fix auth for repository items for RFC

--- src/meta/auth/authorizer/canEditRepositoryItem.ts
+++ src/meta/auth/authorizer/canEditRepositoryItem.ts
@@ -8,7 +8,11 @@ import { Users } from 'meta/user/users'
 
 export const canEditRepositoryItem = (props: { cycle: Cycle; country: Country; user: User }): boolean => {
   const { country, cycle, user } = props
-  if (Users.isReviewer(user, country.countryIso, cycle)) {
+
+  const isReviewer = Users.isReviewer(user, country.countryIso, cycle)
+  const isRegionalFocalPoint = Users.isRegionalFocalPoint(user, country.countryIso, cycle)
+
+  if (isReviewer || isRegionalFocalPoint) {
     const permission = CollaboratorEditPropertyType.descriptions
     return Authorizer.canEditSectionData({ country, cycle, permission, section: undefined, user })
   }
