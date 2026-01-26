6fcbaeb04e5f829dbc91fbe573a58486ef2e84be
Miró Sorja <ping@mirosorja.com>
2026-01-26 16:16:00 +0200

    5645 - Conditional hero component

--- src/client/pages/CycleHome/CycleHome.tsx
+++ src/client/pages/CycleHome/CycleHome.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
 
 import { AssessmentName, AssessmentNames } from 'meta/assessment/assessment'
+import { CycleName } from 'meta/assessment/cycle'
 
+import { useCycleRouteParams } from 'client/hooks/routeParams'
 import AssessmentSwitch from 'client/components/AssessmentSwitch'
 import PanEuropeanOverview from 'client/pages/CountryHome/PanEuropeanOverview'
+import KeyFindings from 'client/pages/CycleHome/KeyFindings'
 
 import { useCycleRedirect } from './hooks/useCycleRedirect'
 import Introduction from './Introduction'
-import KeyFindings from './KeyFindings'
 import Partners from './Partners'
+import Report from './Report'
+
+const HeroComponents: Record<CycleName, React.FC> = {
+  '2020': KeyFindings,
+  '2025': Report,
+  latest: Report,
+}
 
 const Components: { [key: AssessmentName]: React.FC } = {
-  [AssessmentNames.fra]: () => (
-    <>
-      <Introduction />
-      <KeyFindings />
-      <Partners />
-    </>
-  ),
+  [AssessmentNames.fra]: () => {
+    const { cycleName } = useCycleRouteParams()
+
+    const HeroComponent = HeroComponents[cycleName]
+
+    return (
+      <>
+        <Introduction />
+        <HeroComponent />
+        <Partners />
+      </>
+    )
+  },
   [AssessmentNames.panEuropean]: PanEuropeanOverview,
 }
 
