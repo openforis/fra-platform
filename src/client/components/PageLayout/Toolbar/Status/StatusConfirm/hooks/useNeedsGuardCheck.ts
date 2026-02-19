12705dc5a76cf8c2cac825c42d393e58b358d9f4
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-02-19 10:14:21 -0500

    5710 - Links block status change for all roles (#5712)

--- src/client/components/PageLayout/Toolbar/Status/StatusConfirm/hooks/useNeedsGuardCheck.ts
+++ src/client/components/PageLayout/Toolbar/Status/StatusConfirm/hooks/useNeedsGuardCheck.ts
@@ -1,15 +1,15 @@
 import { useMemo } from 'react'
 
-import { CountryIso } from 'meta/area/countryIso'
+import { Country } from 'meta/area/country'
 import { CountryStatus } from 'meta/area/countryStatus'
+import { CountryStatuses } from 'meta/area/countryStatuses'
 import { Cycle } from 'meta/assessment/cycle'
 import { User } from 'meta/user/user'
-import { Users } from 'meta/user/users'
 
 import { StatusTransition } from 'client/components/PageLayout/Toolbar/Status/types'
 
 type Props = {
-  countryIso: CountryIso
+  country: Country
   cycle: Cycle
   hasRouteParams: boolean
   status: StatusTransition
@@ -19,14 +19,13 @@ type Props = {
 type Returned = boolean
 
 export const useNeedsGuardCheck = (props: Props): Returned => {
-  const { countryIso, cycle, hasRouteParams, status, user } = props
+  const { country, cycle, hasRouteParams, status, user } = props
 
   return useMemo<Returned>(() => {
     const isTransitionToApproval = status.status === CountryStatus.approval && status.direction === 'next'
-    const isAdmin = Users.isAdministrator(user)
     const canSendToApproval =
-      Users.isRegionalFocalPoint(user, countryIso, cycle) || Users.isReviewer(user, countryIso, cycle)
+      CountryStatuses.getAllowedTransition({ country, cycle, user }).next === CountryStatus.approval
 
-    return hasRouteParams && canSendToApproval && isTransitionToApproval && !isAdmin
-  }, [countryIso, cycle, hasRouteParams, status.direction, status.status, user])
+    return hasRouteParams && canSendToApproval && isTransitionToApproval
+  }, [country, cycle, hasRouteParams, status.direction, status.status, user])
 }
