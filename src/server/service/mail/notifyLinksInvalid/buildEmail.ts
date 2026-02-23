a62713847b0b37b93fab66aac88c5f5690df36ba
Miró Sorja <ping@mirosorja.com>
2026-02-23 11:47:51 +0200

    5640 - Scheduler and email (#5713)
    * 5057 - Mail flow
    
    * 5057 - Mail flow
    
    * 5640 - Scheduler-NotifyLinksInvalid: create or use existing job
    
    * 5640 - Clean up, refactor and i18n

---
+++ src/server/service/mail/notifyLinksInvalid/buildEmail.ts
@@ -0,0 +1,104 @@
+import { createI18nPromise } from 'i18n/i18nFactory'
+
+import { CountryIso } from 'meta/area/countryIso'
+import { Assessment, AssessmentName } from 'meta/assessment/assessment'
+import { Cycle, CycleName } from 'meta/assessment/cycle'
+import { Link } from 'meta/cycleData/links/link'
+import { Lang } from 'meta/lang'
+import { Routes } from 'meta/routes/routes'
+import { SectionNames } from 'meta/routes/sectionNames'
+import { User } from 'meta/user/user'
+import { Users } from 'meta/user/users'
+
+import { MailServiceEmail } from 'server/service/mail/mail'
+import { ProcessEnv } from 'server/utils'
+
+type Props = {
+  assessment: Assessment
+  cycle: Cycle
+  linksByCountry: Record<CountryIso, Array<Link>>
+  threshold: number
+  user: User
+}
+
+type CountryEntry = {
+  countryIso: CountryIso
+  countryName: string
+  countryLinksUrl: string
+  links: Array<Link>
+}
+
+type GetCountryEntriesProps = {
+  assessmentName: AssessmentName
+  cycleName: CycleName
+  linksByCountry: Record<CountryIso, Array<Link>>
+  t: (key: string) => string
+}
+
+// Return links to the linkStatus page per assessment x cycle x country
+const getCountryLinksUrl = (params: {
+  assessmentName: AssessmentName
+  cycleName: CycleName
+  countryIso: CountryIso
+}): string =>
+  `${ProcessEnv.appUri}${Routes.CountryHomeSection.generatePath({ ...params, sectionName: SectionNames.Country.Home.linksStatus })}`
+
+// Returns the text version of the body of the email
+const _getTextLines = (countryEntries: Array<CountryEntry>): string =>
+  countryEntries
+    .map(({ countryIso, countryLinksUrl, countryName, links }) => {
+      const urlLines = links.map(({ link }) => `    - ${link}`).join('\n')
+      return `  ${countryName} (${countryIso}): ${links.length} invalid link(s)\n  ${countryLinksUrl}\n${urlLines}`
+    })
+    .join('\n\n')
+
+// Returns the HTML body of the email
+const _getHtmlItems = (countryEntries: Array<CountryEntry>): string =>
+  countryEntries
+    .map(({ countryIso, countryLinksUrl, countryName, links }) => {
+      const urlItems = links.map(({ link }) => `<li><a href="${link}">${link}</a></li>`).join('')
+      return `<li><a href="${countryLinksUrl}"><strong>${countryName} (${countryIso})</strong></a>: ${links.length} invalid link(s)<ul>${urlItems}</ul></li>`
+    })
+    .join('<br>')
+
+const _getCountryEntries = (props: GetCountryEntriesProps): Array<CountryEntry> => {
+  const { assessmentName, cycleName, linksByCountry, t } = props
+
+  const entries = Object.entries(linksByCountry) as Array<[CountryIso, Array<Link>]>
+
+  return entries.map(([countryIso, links]) => ({
+    countryIso,
+    countryName: t(`area.${countryIso}.listName`),
+    countryLinksUrl: getCountryLinksUrl({ assessmentName, cycleName, countryIso }),
+    links,
+  }))
+}
+
+export const buildEmail = async (props: Props): Promise<MailServiceEmail> => {
+  const { assessment, cycle, linksByCountry, threshold, user } = props
+  const { t } = await createI18nPromise(Lang.en)
+  const to = user.email
+
+  const subject = t('email.invalidLinks.subject')
+  const messageHeader = t('email.invalidLinks.messageHeader', { recipientName: Users.getFullName(user) })
+  const messageBodyIntro = t('email.invalidLinks.messageBodyIntro', { threshold })
+  const messageFooter = t('email.invalidLinks.messageFooter')
+
+  const { name: assessmentName } = assessment.props
+  const { name: cycleName } = cycle
+
+  const countryEntries = _getCountryEntries({ assessmentName, cycleName, linksByCountry, t })
+  const textLines = _getTextLines(countryEntries)
+  const htmlItems = _getHtmlItems(countryEntries)
+
+  const text = [messageHeader, messageBodyIntro, textLines, messageFooter].join('\n\n')
+
+  const html = [
+    `<p>${messageHeader}</p>`,
+    `<p>${messageBodyIntro}</p>`,
+    `<ul>${htmlItems}</ul>`,
+    `<p style="white-space: pre-line">${messageFooter}</p>`,
+  ].join('')
+
+  return { to, subject, text, html }
+}
---
+++ src/server/worker/cronJobs/notifyLinksInvalid.ts
@@ -0,0 +1,49 @@
+import { QueueEvents } from 'bullmq'
+
+import { AssessmentNames } from 'meta/assessment/assessment'
+import { CycleNames } from 'meta/assessment/cycle/names'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { CycleDataController } from 'server/controller/cycleData'
+import { UserController } from 'server/controller/user'
+import { MailService } from 'server/service'
+import { ProcessEnv } from 'server/utils'
+import { Job } from 'server/worker/job/job'
+import { triggerVerifyLinksWorker } from 'server/worker/tasks/verifyLinks/triggerVerifyLinksWorker'
+import { VisitCycleLinksQueueFactory } from 'server/worker/tasks/verifyLinks/visitCycleLinks/queueFactory'
+
+const name = 'Scheduler-NotifyLinksInvalid'
+// 20 min
+const VERIFY_TIMEOUT_MS = 20 * 60 * 1000
+const cycleName = CycleNames.latest
+const assessmentName = AssessmentNames.fra
+
+export class NotifyLinksInvalid extends Job {
+  constructor() {
+    super(name)
+  }
+
+  protected async execute(): Promise<void> {
+    const { assessment, cycle } = await AssessmentController.getOneWithCycle({
+      assessmentName,
+      cycleName,
+    })
+
+    const user = await UserController.getUserRobot()
+
+    // get existing job or create new job and process it
+    const existingJob = await VisitCycleLinksQueueFactory.getQueuedOrActiveJob({ assessment, cycle })
+    const job = existingJob ?? (await CycleDataController.Links.verify({ assessment, cycle, user }))
+    await triggerVerifyLinksWorker()
+
+    const connection = { url: ProcessEnv.redisQueueUrl }
+    const queueEvents = new QueueEvents(VisitCycleLinksQueueFactory.queueName, { connection })
+
+    try {
+      await job.waitUntilFinished(queueEvents, VERIFY_TIMEOUT_MS)
+    } finally {
+      await queueEvents.close()
+      await MailService.notifyLinksInvalid({ assessment, cycle })
+    }
+  }
+}
