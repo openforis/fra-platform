bd6d6d107e1b5d26eda89e02d2c8050e230f3b98
Miró Sorja <ping@mirosorja.com>
2026-02-23 10:45:25 +0200

    5640 - Clean up, refactor and i18n

--- src/server/service/mail/notifyLinksInvalid/buildEmail.ts
+++ src/server/service/mail/notifyLinksInvalid/buildEmail.ts
@@ -1,37 +1,104 @@
 import { createI18nPromise } from 'i18n/i18nFactory'
 
 import { CountryIso } from 'meta/area/countryIso'
+import { Assessment, AssessmentName } from 'meta/assessment/assessment'
+import { Cycle, CycleName } from 'meta/assessment/cycle'
 import { Link } from 'meta/cycleData/links/link'
 import { Lang } from 'meta/lang'
+import { Routes } from 'meta/routes/routes'
+import { SectionNames } from 'meta/routes/sectionNames'
+import { User } from 'meta/user/user'
+import { Users } from 'meta/user/users'
 
 import { MailServiceEmail } from 'server/service/mail/mail'
+import { ProcessEnv } from 'server/utils'
 
 type Props = {
-  to: string
+  assessment: Assessment
+  cycle: Cycle
   linksByCountry: Record<CountryIso, Array<Link>>
   threshold: number
+  user: User
 }
 
-export const buildEmail = async (props: Props): Promise<MailServiceEmail> => {
-  const { linksByCountry, threshold, to } = props
-  const { t } = await createI18nPromise(Lang.en)
+type CountryEntry = {
+  countryIso: CountryIso
+  countryName: string
+  countryLinksUrl: string
+  links: Array<Link>
+}
 
-  const subject = t('email.invalidLinks.subject')
-  const messageBodyIntro = t('email.invalidLinks.messageBodyIntro', { threshold })
-  const messageFooter = t('email.invalidLinks.messageFooter')
+type GetCountryEntriesProps = {
+  assessmentName: AssessmentName
+  cycleName: CycleName
+  linksByCountry: Record<CountryIso, Array<Link>>
+  t: (key: string) => string
+}
+
+// Return links to the linkStatus page per assessment x cycle x country
+const getCountryLinksUrl = (params: {
+  assessmentName: AssessmentName
+  cycleName: CycleName
+  countryIso: CountryIso
+}): string =>
+  `${ProcessEnv.appUri}${Routes.CountryHomeSection.generatePath({ ...params, sectionName: SectionNames.Country.Home.linksStatus })}`
+
+// Returns the text version of the body of the email
+const _getTextLines = (countryEntries: Array<CountryEntry>): string =>
+  countryEntries
+    .map(({ countryIso, countryLinksUrl, countryName, links }) => {
+      const urlLines = links.map(({ link }) => `    - ${link}`).join('\n')
+      return `  ${countryName} (${countryIso}): ${links.length} invalid link(s)\n  ${countryLinksUrl}\n${urlLines}`
+    })
+    .join('\n\n')
+
+// Returns the HTML body of the email
+const _getHtmlItems = (countryEntries: Array<CountryEntry>): string =>
+  countryEntries
+    .map(({ countryIso, countryLinksUrl, countryName, links }) => {
+      const urlItems = links.map(({ link }) => `<li><a href="${link}">${link}</a></li>`).join('')
+      return `<li><a href="${countryLinksUrl}"><strong>${countryName} (${countryIso})</strong></a>: ${links.length} invalid link(s)<ul>${urlItems}</ul></li>`
+    })
+    .join('<br>')
+
+const _getCountryEntries = (props: GetCountryEntriesProps): Array<CountryEntry> => {
+  const { assessmentName, cycleName, linksByCountry, t } = props
 
   const entries = Object.entries(linksByCountry) as Array<[CountryIso, Array<Link>]>
 
-  // TODO : Add links
-  const textLines = entries
-    .map(([countryIso, links]) => `  - ${countryIso}: ${links.length} invalid link(s)`)
-    .join('\n')
-  const htmlItems = entries
-    .map(([countryIso, links]) => `<li>${countryIso}: ${links.length} invalid link(s)</li>`)
-    .join('')
+  return entries.map(([countryIso, links]) => ({
+    countryIso,
+    countryName: t(`area.${countryIso}.listName`),
+    countryLinksUrl: getCountryLinksUrl({ assessmentName, cycleName, countryIso }),
+    links,
+  }))
+}
 
-  const text = `${messageBodyIntro}\n\n${textLines}\n\n${messageFooter}`
-  const html = `<p>${messageBodyIntro}</p><ul>${htmlItems}</ul><p style="white-space: pre-line">${messageFooter}</p>`
+export const buildEmail = async (props: Props): Promise<MailServiceEmail> => {
+  const { assessment, cycle, linksByCountry, threshold, user } = props
+  const { t } = await createI18nPromise(Lang.en)
+  const to = user.email
+
+  const subject = t('email.invalidLinks.subject')
+  const messageHeader = t('email.invalidLinks.messageHeader', { recipientName: Users.getFullName(user) })
+  const messageBodyIntro = t('email.invalidLinks.messageBodyIntro', { threshold })
+  const messageFooter = t('email.invalidLinks.messageFooter')
+
+  const { name: assessmentName } = assessment.props
+  const { name: cycleName } = cycle
+
+  const countryEntries = _getCountryEntries({ assessmentName, cycleName, linksByCountry, t })
+  const textLines = _getTextLines(countryEntries)
+  const htmlItems = _getHtmlItems(countryEntries)
+
+  const text = [messageHeader, messageBodyIntro, textLines, messageFooter].join('\n\n')
+
+  const html = [
+    `<p>${messageHeader}</p>`,
+    `<p>${messageBodyIntro}</p>`,
+    `<ul>${htmlItems}</ul>`,
+    `<p style="white-space: pre-line">${messageFooter}</p>`,
+  ].join('')
 
   return { to, subject, text, html }
 }
--- src/server/service/mail/notifyLinksInvalid/index.ts
+++ src/server/service/mail/notifyLinksInvalid/index.ts
@@ -1,17 +1,34 @@
-import { CountryIso } from 'meta/area/countryIso'
-import { Link } from 'meta/cycleData/links/link'
-import { UserEmails } from 'meta/user/emails'
+import { Assessment } from 'meta/assessment/assessment'
+import { Cycle } from 'meta/assessment/cycle'
 
+import { UserRepository } from 'server/db/repository/public/user'
 import { sendMail } from 'server/service/mail/mail'
 
 import { buildEmail } from './buildEmail'
+import { groupLinksByCountry } from './groupLinksByCountry'
+
+type Props = {
+  assessment: Assessment
+  cycle: Cycle
+}
 
 const INVALID_LINK_THRESHOLD = 3
 
-// TODO
-export const notifyLinksInvalid = async (): Promise<void> => {
-  const linksByCountry = { X01: [] } as Record<CountryIso, Array<Link>>
-  const emailProps = { to: UserEmails.robot, linksByCountry, threshold: INVALID_LINK_THRESHOLD }
-  const email = await buildEmail(emailProps)
-  await sendMail(email)
+export const notifyLinksInvalid = async (props: Props): Promise<void> => {
+  const { assessment, cycle } = props
+
+  const linksByCountry = await groupLinksByCountry({ assessment, cycle, threshold: INVALID_LINK_THRESHOLD })
+  // Return if no matches
+  if (Object.keys(linksByCountry).length === 0) return
+
+  // TODO: Instead, send to RFC (pt2)
+  const admins = await UserRepository.getAdmins()
+
+  await Promise.all(
+    admins.map(async (user) => {
+      const emailParams = { user, assessment, cycle, linksByCountry, threshold: INVALID_LINK_THRESHOLD }
+      const email = await buildEmail(emailParams)
+      return sendMail(email)
+    })
+  )
 }
