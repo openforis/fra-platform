1dbbdceb469d108daa6e1311cdbde5a32019f795
Miró Sorja <ping@mirosorja.com>
2026-02-24 14:22:49 +0200

    5640 - notifyLinksInvalid: send email to RFC

--- src/server/service/mail/notifyLinksInvalid/index.ts
+++ src/server/service/mail/notifyLinksInvalid/index.ts
@@ -1,8 +1,13 @@
+import { CountryIso } from 'meta/area/countryIso'
 import { Assessment } from 'meta/assessment/assessment'
 import { Cycle } from 'meta/assessment/cycle'
+import { RoleName } from 'meta/user/role/name'
+import { User } from 'meta/user/user'
+import { Objects } from 'utils/objects'
 
 import { UserRepository } from 'server/db/repository/public/user'
 import { sendMail } from 'server/service/mail/mail'
+import { LinksByCountry } from 'server/service/mail/notifyLinksInvalid/types'
 
 import { buildEmail } from './buildEmail'
 import { groupLinksByCountry } from './groupLinksByCountry'
@@ -13,22 +18,35 @@ type Props = {
 }
 
 const INVALID_LINK_THRESHOLD = 3
+const roles = [RoleName.REGIONAL_FOCAL_POINT]
 
+// match linksByCountry to user countries
+export const _getUserLinksByCountry = (props: { user: User; linksByCountry: LinksByCountry }): LinksByCountry => {
+  const { linksByCountry, user } = props
+  return Object.fromEntries(user.roles.map((r) => [r.countryIso, linksByCountry[r.countryIso as CountryIso]]))
+}
+
+// Notify RFC's of broken links via email
 export const notifyLinksInvalid = async (props: Props): Promise<void> => {
   const { assessment, cycle } = props
 
   const linksByCountry = await groupLinksByCountry({ assessment, cycle, threshold: INVALID_LINK_THRESHOLD })
-  // Return if no matches
-  if (Object.keys(linksByCountry).length === 0) return
+  if (Objects.isEmpty(linksByCountry)) return
 
-  // TODO: Instead, send to RFC (pt2)
-  const admins = await UserRepository.getAdmins()
+  const countryISOs = Object.keys(linksByCountry) as Array<CountryIso>
+  const users = await UserRepository.readCountryUsersByRole({ cycle, countryISOs, roles })
 
-  await Promise.all(
-    admins.map(async (user) => {
-      const emailParams = { user, assessment, cycle, linksByCountry, threshold: INVALID_LINK_THRESHOLD }
-      const email = await buildEmail(emailParams)
-      return sendMail(email)
+  const emailPromises = users.map(async (user) => {
+    // Note: user.roles only contains REGIONAL_FOCAL_POINT roles for the affected countries
+    const email = await buildEmail({
+      user,
+      assessment,
+      cycle,
+      linksByCountry: _getUserLinksByCountry({ user, linksByCountry }),
+      threshold: INVALID_LINK_THRESHOLD,
     })
-  )
+    return sendMail(email)
+  })
+
+  await Promise.all(emailPromises)
 }
