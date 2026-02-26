04798d25a3a59442d08afb724e5a46eab17e5724
Miró Sorja <ping@mirosorja.com>
2026-02-26 15:54:40 +0200

    5648 - migration util: tableExists

---
+++ src/tools/migrations/steps/steps/utils/tableExists.ts
@@ -0,0 +1,18 @@
+import { BaseProtocol } from 'server/db/db'
+
+type Props = {
+  schema: string
+  tableName: string
+}
+
+export const exists = (props: Props, client: BaseProtocol): Promise<{ exists: boolean }> => {
+  const { schema, tableName } = props
+  return client.oneOrNone<{ exists: boolean }>(
+    `
+      select exists (
+        select 1 from information_schema.tables
+        where table_schema = '${schema}' and table_name = '${tableName}'
+      ) as exists
+    `
+  )
+}
