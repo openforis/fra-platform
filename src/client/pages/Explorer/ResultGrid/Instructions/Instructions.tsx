3ca90ea2f138900da1441d82f5da564bc47b96e2
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-02-27 09:14:42 -0500

    5720 - Add section name to instructions

--- src/client/pages/Explorer/ResultGrid/Instructions/Instructions.tsx
+++ src/client/pages/Explorer/ResultGrid/Instructions/Instructions.tsx
@@ -2,14 +2,23 @@ import './Instructions.scss'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { Labels } from 'meta/assessment/labels'
+
+import { useCycle } from 'client/store/meta/hooks/cycles'
+import { useSection } from 'client/store/meta/hooks/sections'
+import { useSectionRouteParams } from 'client/hooks/routeParams'
 import Flex from 'client/components/Layout/Flex'
 
 const Instructions: React.FC = () => {
   const { t } = useTranslation()
+  const cycle = useCycle()
+  const { sectionName } = useSectionRouteParams()
+  const subSection = useSection(sectionName)
+  const sectionLabel = Labels.getCycleLabel({ cycle, labels: subSection?.props.labels ?? {}, t })
 
   return (
     <Flex alignItems="start" className="explorer-result-grid__instructions" flexDirection="column" gap="4">
-      <p>{t('explorer.selectDataInstruction')}</p>
+      <p>{t('explorer.selectDataInstruction', { sectionLabel })}</p>
       <p>{t('explorer.downloadWholeDatasetInstruction')}</p>
     </Flex>
   )
