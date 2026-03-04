988ae2eb4c4aeb593c9a5458bfb81382ef2925a6
Miró Sorja <ping@mirosorja.com>
2026-03-04 10:24:13 +0200

    5648 - repository/getOne: Strict conditions when fetching item by name

--- src/server/db/repository/assessmentCycle/repository/getOne.ts
+++ src/server/db/repository/assessmentCycle/repository/getOne.ts
@@ -20,7 +20,8 @@ export const getOne = async (props: Props, client: BaseProtocol = DB): Promise<R
     where = 'uuid = $(value)'
   } else if ('fileName' in props) {
     value = props.fileName
-    where = "props -> 'translation' ->> 'en' = $(value)"
+    where =
+      "props -> 'translation' ->> 'en' = $(value) and (props ->> 'hidden')::boolean = true and country_iso is null"
   } else {
     value = props.fileUuid
     where = 'file_uuid = $(value)'
