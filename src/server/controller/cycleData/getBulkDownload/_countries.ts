f25af4a5c10659b7d5921c4aee1a7523de76bd56
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-03-12 09:26:58 -0500

    5741 - Subregions (#5746)
    * 5741 - Subregions - Add definition (#5742)
    
    * 5741 - Add Subregions definition
    
    * 5741 - Keep SubRegion type in its own file
    
    * 5741 - Use region properties for subregion type
    
    * 5741 - Add delete cascade
    
    * 5741 - Add region codes local variable
    
    * 5741 - Remove parent code index
    
    * 5741 - Insert subregions (#5745)
    
    * 5741 - Subregions - Bulk download / API (#5749)
    
    * 5741 - Add subregions to countries
    
    * 5741 - Add subregions to bulkdownload
    
    * 5741 - Initialize empty vars consistently
    
    * 5741 - Remove fra2020 check
    
    * 5741 - Remove the logic of allowed sub regions
    
    * 5741 - Subregions - Var names (#5753)
    
    * 5741 - Fix file casing
    
    * 5741 - Correct subregion spelling
    
    * 5741 - Test db update (#5759)

--- src/server/controller/cycleData/getBulkDownload/_countries.ts
+++ src/server/controller/cycleData/getBulkDownload/_countries.ts
@@ -1,5 +1,6 @@
 import { Areas } from 'meta/area/areas'
 import { Country } from 'meta/area/country'
+import { RegionCode } from 'meta/area/regionCode'
 import { RegionGroupName } from 'meta/area/regionGroup'
 import { Lang } from 'meta/lang'
 
@@ -14,14 +15,17 @@ export const getCountries = async (props: PropsBulkDownload): Promise<Array<Coun
     AreaRedisRepository.getManyRegionGroups({ assessment, cycle }),
   ])
   const fraRegionGroup = regionGroups.find((rg) => rg.name === RegionGroupName.fra2020)
-  const allowedRegionCodes = fraRegionGroup.regions.map((r) => r.regionCode)
+  const allowedRegionCodes = new Set<RegionCode>(fraRegionGroup.regions.map((region) => region.regionCode))
 
   return countries
     .reduce<Array<Country>>((acc, country) => {
       if (!Areas.isAtlantis(country.countryIso)) {
-        const regionCodes = country.regionCodes.filter((r) => allowedRegionCodes.includes(r))
-        acc.push({ ...country, regionCodes })
+        const regionCodes = country.regionCodes.filter((regionCode) => allowedRegionCodes.has(regionCode))
+        const subregionCodes = country.subregionCodes ?? []
+
+        acc.push({ ...country, regionCodes, subregionCodes })
       }
+
       return acc
     }, [])
     .sort((c1, c2) => Areas.getCompareListName(c1, c2, Lang.en))
