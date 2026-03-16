2c8e81f50b540bf312a1f9c6df7e731eb3fd8c95
Miró Sorja <ping@mirosorja.com>
2026-03-16 08:07:37 +0200

    5755 - server: i18n util

---
+++ src/server/utils/i18n.ts
@@ -0,0 +1,15 @@
+import { createI18nPromise } from 'i18n/i18nFactory'
+import { TFunction } from 'i18next'
+
+import { Lang } from 'meta/lang'
+import { User } from 'meta/user/user'
+
+type Props = {
+  user?: User
+  lang?: Lang
+}
+
+const get = ({ lang, user }: Props): Promise<{ language: Lang; t: TFunction }> =>
+  createI18nPromise(user?.props.lang ?? lang ?? Lang.en)
+
+export const I18nUtils = { get }
--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -1,2 +1,3 @@
+export { I18nUtils } from './i18n'
 export { ProcessEnv } from './processEnv'
 export { Requests } from './requests'
