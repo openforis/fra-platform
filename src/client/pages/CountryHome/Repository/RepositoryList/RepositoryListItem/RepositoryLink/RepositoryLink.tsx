cf0d893043f5e933e5060d9e45af18c82ee1d847
Miró Sorja <ping@mirosorja.com>
2026-03-27 13:35:30 +0200

    5767 - Add link and file icon

--- src/client/pages/CountryHome/Repository/RepositoryList/RepositoryListItem/RepositoryLink/RepositoryLink.tsx
+++ src/client/pages/CountryHome/Repository/RepositoryList/RepositoryListItem/RepositoryLink/RepositoryLink.tsx
@@ -6,6 +6,7 @@ import { Translations } from 'meta/translation/translations'
 
 import { useLanguage } from 'client/hooks/language'
 import { useCountryRouteParams } from 'client/hooks/routeParams'
+import Icon from 'client/components/Icon'
 import Link from 'client/components/Links/Link'
 
 type Props = {
@@ -21,8 +22,11 @@ const RepositoryLink: React.FC<Props> = (props) => {
   const label = Translations.getLabel({ translation: datum.props.translation, language })
   const url = RepositoryItems.getURL({ repositoryItem: datum, assessmentName, cycleName, countryIso })
 
+  const iconName = datum.fileUuid ? 'icon-files' : 'external-link'
+
   return (
     <Link rel="noreferrer" target="_blank" to={url}>
+      <Icon name={iconName} />
       {label}
     </Link>
   )
