421e8a4e72a2c02517f47d356aa93bff3002aa85
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-04-04 11:33:06 -0500

    5758 - Iterate over sections

--- src/server/controller/cycleData/validations/summary/getSummary.ts
+++ src/server/controller/cycleData/validations/summary/getSummary.ts
@@ -21,28 +21,31 @@ export const getValidationSummary = async (props: Props): Promise<ValidationSumm
     ValidationRedisRepository.getTableValidations({ assessment, countryIso, cycle }),
   ])
   const summary: ValidationSummary = { sections: {}, subsections: {}, tables: {} }
-  const subSections = sections.flatMap((section) => section.subSections ?? [])
 
-  Object.entries(sectionsMetadata).forEach(([subSectionName, tableSections]) => {
-    const subSection = subSections.find((_subSection) => _subSection.props.name === subSectionName)
-    const sectionUuid = subSection.parentUuid
-    const subsectionUuid = subSection.uuid
-    const tableNames = tableSections.flatMap((tableSection) => tableSection.tables.map((table) => table.props.name))
-    let subsectionValid = true
+  sections.forEach((section) => {
+    section.subSections?.forEach((subSection) => {
+      const tableSections = sectionsMetadata[subSection.props.name]
+      if (!tableSections) return
 
-    tableNames.forEach((tableName) => {
-      const valid = Objects.isEmpty(tableValidations[tableName] ?? {})
-      summary.tables[tableName] = { valid }
-      subsectionValid = subsectionValid && valid
+      const sectionUuid = section.uuid
+      const subsectionUuid = subSection.uuid
+      const tableNames = tableSections.flatMap((tableSection) => tableSection.tables.map((table) => table.props.name))
+      let subsectionValid = true
+
+      tableNames.forEach((tableName) => {
+        const valid = Objects.isEmpty(tableValidations[tableName] ?? {})
+        summary.tables[tableName] = { valid }
+        subsectionValid = subsectionValid && valid
+      })
+
+      const subsection = { tableNames, valid: subsectionValid }
+      summary.sections[sectionUuid] ??= { subsections: {}, valid: true }
+      const summarySection = summary.sections[sectionUuid]
+
+      summary.subsections[subsectionUuid] = subsection
+      summarySection.subsections[subsectionUuid] = subsection
+      summarySection.valid = summarySection.valid && subsectionValid
     })
-
-    const subsection = { tableNames, valid: subsectionValid }
-    summary.sections[sectionUuid] ??= { subsections: {}, valid: true }
-    const section = summary.sections[sectionUuid]
-
-    summary.subsections[subsectionUuid] = subsection
-    section.subsections[subsectionUuid] = subsection
-    section.valid = section.valid && subsectionValid
   })
 
   return summary
