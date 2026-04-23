86e00c18d2fbe90db110eb4d2913c2e3e08bd399
Miró Sorja <ping@mirosorja.com>
2026-04-23 16:40:55 +0300

    5767 - Disable removing folders containing items (#5864)
    * 5767 - Repository/delete: Disable and show notification if folder contains items
    
    * 5767 - Repository/delete: Remove generic class
    
    * 5767 - Repository/Tooltip: Adjust tooltip and add reasoning

--- src/client/components/RepositoryList/EditForm/RepositoryItemForm/RepositoryItemForm.tsx
+++ src/client/components/RepositoryList/EditForm/RepositoryItemForm/RepositoryItemForm.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Tooltip } from 'react-tooltip'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
 import { CountryIso } from 'meta/area/countryIso'
-import { RepositoryItem } from 'meta/cycleData/repository/item'
+import { RepositoryItem, RepositoryItemTree } from 'meta/cycleData/repository/item'
+import { RepositoryItems } from 'meta/cycleData/repository/items'
+import { TooltipId } from 'meta/tooltip/id'
 import { Objects } from 'utils/objects'
 
 import { useCountryRouteParams } from 'client/hooks/routeParams'
@@ -34,6 +37,8 @@ const RepositoryItemForm: React.FC<Props> = (props) => {
 
   const isEditing = !Objects.isEmpty(repositoryItem?.uuid)
   const isUsed = !Objects.isEmpty(fileMeta?.usages)
+  const isNonEmptyFolder =
+    RepositoryItems.isFolder(repositoryItem) && Boolean((repositoryItem as RepositoryItemTree).children?.length)
   const method = isEditing ? 'put' : 'post'
   const action = Urls.withSearchParams(ApiEndPoint.CycleData.Repository.one(), {
     assessmentName,
@@ -53,13 +58,28 @@ const RepositoryItemForm: React.FC<Props> = (props) => {
       />
       {isEditing && (
         <div className="repository-item-form__delete">
-          <Button
-            disabled={isLoading || isUsed}
-            iconName="trash-simple"
-            label={t('common.delete')}
-            onClick={onDelete}
-            type={ButtonType.danger}
-          />
+          <div data-tooltip-id="repository-item-form__delete-btn">
+            <Button
+              disabled={isLoading || isUsed || isNonEmptyFolder}
+              iconName="trash-simple"
+              label={t('common.delete')}
+              onClick={onDelete}
+              type={ButtonType.danger}
+            />
+            {isNonEmptyFolder && (
+              // Workaround to get around z-index issue with panel
+              <div className="tooltip-container">
+                <Tooltip
+                  className={TooltipId.error}
+                  classNameArrow={`${TooltipId.error}-arrow`}
+                  id="repository-item-form__delete-btn"
+                  place="top"
+                >
+                  {t('landing.links.folderNotEmpty')}
+                </Tooltip>
+              </div>
+            )}
+          </div>
         </div>
       )}
       <FileUsages fileMeta={fileMeta} />
