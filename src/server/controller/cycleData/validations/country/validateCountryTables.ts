9997fa88374b2eb4fdab8afbb254817121da2179
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-04-23 12:33:47 -0500

    5758 - Navigation: Data errors - validateAll tool - 1 (#5859)
    * 5758 - use shouldSkipValidationFormula in evaluateTableValidations
    
    * 5758 - Add computeTableValidations
    
    * 5758 - Add initial backfillTableValidations
    
    * 5758 - Remove evaluateTableValidations
    
    * 5758 - Remove TODO
    
    * 5758 - Remove computeValidations
    
    * 5758 - Replace backfillTableValidations
    
    * 5758 - Fix deepscan
    
    * 5758 - Centralize context building and cache setting
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/server/controller/cycleData/validations/country/validateCountryTables.ts
@@ -0,0 +1,39 @@
+import { CountryIso } from 'meta/area/countryIso'
+import { AssessmentName } from 'meta/assessment/assessment'
+import { CycleName } from 'meta/assessment/cycle'
+
+import { TableRedisRepository } from 'server/cache/repository/table'
+import { AreaController } from 'server/controller/area'
+import { AssessmentController } from 'server/controller/assessment'
+import { updateValidations } from 'server/controller/cycleData/validations/updateValidations'
+import { BaseProtocol, DB } from 'server/db/db'
+
+import { buildTablesNodeUpdates } from './buildTablesNodeUpdates'
+
+type Props = {
+  assessmentName: AssessmentName
+  countryIso: CountryIso
+  cycleName: CycleName
+}
+
+export const validateCountryTables = async (props: Props, client: BaseProtocol = DB): Promise<void> => {
+  const { assessmentName, countryIso, cycleName } = props
+
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
+    { assessmentName, cycleName, metaCache: true },
+    client
+  )
+  const [country, tables] = await Promise.all([
+    AreaController.getCountry({ assessment, countryIso, cycle }, client),
+    TableRedisRepository.getManyRecord({ assessment, cycle }),
+  ])
+  const nodeUpdates = buildTablesNodeUpdates({ assessment, country, cycle, tables })
+
+  await updateValidations({
+    assessment,
+    country,
+    cycle,
+    nodeUpdates,
+    notifyClients: false,
+  })
+}
