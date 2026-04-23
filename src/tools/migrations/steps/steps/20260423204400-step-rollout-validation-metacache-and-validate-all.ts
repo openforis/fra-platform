b1e791d93b1a7178fe0d69c23b2352cb7af9b7d0
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-04-23 16:26:58 -0500

    5758 - Only use generateMetaCache

--- src/tools/migrations/steps/steps/20260423204400-step-rollout-validation-metacache-and-validate-all.ts
+++ src/tools/migrations/steps/steps/20260423204400-step-rollout-validation-metacache-and-validate-all.ts
@@ -4,7 +4,6 @@ import { CacheController } from 'server/cache/controller'
 import { BaseProtocol } from 'server/db/db'
 
 export default async (client: BaseProtocol): Promise<void> => {
-  await CacheController.generateAssessments(client)
   await CacheController.generateMetaCache({}, client)
   await validateAll(client)
 }
