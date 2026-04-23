95f980995183364fd9ee3fb8e3c4ab67b3cf9991
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-04-23 15:50:13 -0500

    5758 - Add migration step

---
+++ src/tools/migrations/steps/steps/20260423204400-step-rollout-validation-metacache-and-validate-all.ts
@@ -0,0 +1,10 @@
+import { validateAll } from 'tools/validations/validateAllRunner'
+
+import { CacheController } from 'server/cache/controller'
+import { BaseProtocol } from 'server/db/db'
+
+export default async (client: BaseProtocol): Promise<void> => {
+  await CacheController.generateAssessments(client)
+  await CacheController.generateMetaCache({}, client)
+  await validateAll(client)
+}
