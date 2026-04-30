1d7b30714ab06e4843137a0f9b186e00be3264a6
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-04-30 09:03:40 -0500

    5758 - Get country from cache

--- src/server/controller/cycleData/validations/country/validateCountryTables.ts
+++ src/server/controller/cycleData/validations/country/validateCountryTables.ts
@@ -3,9 +3,9 @@ import { AssessmentName } from 'meta/assessment/assessment'
 import { Assessments } from 'meta/assessment/assessments'
 import { CycleName } from 'meta/assessment/cycle'
 
+import { AreaRedisRepository } from 'server/cache/repository/area'
 import { TableRedisRepository } from 'server/cache/repository/table'
 import { ValidationRedisRepository } from 'server/cache/repository/validation'
-import { AreaController } from 'server/controller/area'
 import { AssessmentController } from 'server/controller/assessment'
 import { updateValidations } from 'server/controller/cycleData/validations/updateValidations'
 import { BaseProtocol, DB } from 'server/db/db'
@@ -25,7 +25,7 @@ export const validateCountryTables = async (props: Props, client: BaseProtocol =
   const cycle = Assessments.getCycle({ assessment, cycleName })
 
   const [country, tables] = await Promise.all([
-    AreaController.getCountry({ assessment, countryIso, cycle }, client),
+    AreaRedisRepository.getOneCountry({ assessment, countryIso, cycle }, client),
     TableRedisRepository.getManyRecord({ assessment, cycle }),
   ])
   const nodeUpdates = buildTablesNodeUpdates({ assessment, country, cycle, tables })
