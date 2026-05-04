15538d713d9049dbef1104b550ba8a30e3bf63aa
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-05-04 01:30:28 -0500

    5758 - Navigation: Data errors - Prevent stale data (#5884)
    * 5758 - Load all cycles in assessment
    
    * 5758 - Prevent stale data
    
    * 5758 - Get country from cache

--- src/server/controller/cycleData/validations/country/validateCountryTables.ts
+++ src/server/controller/cycleData/validations/country/validateCountryTables.ts
@@ -1,9 +1,11 @@
 import { CountryIso } from 'meta/area/countryIso'
 import { AssessmentName } from 'meta/assessment/assessment'
+import { Assessments } from 'meta/assessment/assessments'
 import { CycleName } from 'meta/assessment/cycle'
 
+import { AreaRedisRepository } from 'server/cache/repository/area'
 import { TableRedisRepository } from 'server/cache/repository/table'
-import { AreaController } from 'server/controller/area'
+import { ValidationRedisRepository } from 'server/cache/repository/validation'
 import { AssessmentController } from 'server/controller/assessment'
 import { updateValidations } from 'server/controller/cycleData/validations/updateValidations'
 import { BaseProtocol, DB } from 'server/db/db'
@@ -19,15 +21,16 @@ type Props = {
 export const validateCountryTables = async (props: Props, client: BaseProtocol = DB): Promise<void> => {
   const { assessmentName, countryIso, cycleName } = props
 
-  const { assessment, cycle } = await AssessmentController.getOneWithCycle(
-    { assessmentName, cycleName, metaCache: true },
-    client
-  )
+  const assessment = await AssessmentController.getOne({ assessmentName, metaCache: true }, client)
+  const cycle = Assessments.getCycle({ assessment, cycleName })
+
   const [country, tables] = await Promise.all([
-    AreaController.getCountry({ assessment, countryIso, cycle }, client),
+    AreaRedisRepository.getOneCountry({ assessment, countryIso, cycle }, client),
     TableRedisRepository.getManyRecord({ assessment, cycle }),
   ])
   const nodeUpdates = buildTablesNodeUpdates({ assessment, country, cycle, tables })
+
+  await ValidationRedisRepository.clearCountryValidations({ assessment, countryIso, cycle })
 
   await updateValidations({
     assessment,
