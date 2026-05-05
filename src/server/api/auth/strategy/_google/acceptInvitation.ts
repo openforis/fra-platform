afcaa9416a1e4627910b6c806ff342ffe0c926fe
Miró Sorja <ping@mirosorja.com>
2026-05-05 10:30:21 +0300

    5844 - Invitation flow/google login: refactor (#5891)

---
+++ src/server/api/auth/strategy/_google/acceptInvitation.ts
@@ -0,0 +1,58 @@
+import { VerifyCallback } from 'passport-google-oauth20'
+
+import { AuthProvider, AuthProviderGoogleProps } from 'meta/user/auth'
+
+import { AssessmentController } from 'server/controller/assessment'
+import { UserController } from 'server/controller/user'
+import { UserProviderController } from 'server/controller/userProvider'
+
+type Props = {
+  done: VerifyCallback
+  email: string
+  invitationUuid: string
+}
+
+export const googleAcceptInvitation = async (props: Props): Promise<void> => {
+  const { done, email, invitationUuid } = props
+
+  const { user: invitedUser, userInvitation } = await UserController.findByInvitation({ invitationUuid })
+
+  let userProvider = await UserProviderController.read<AuthProviderGoogleProps>({
+    user: invitedUser,
+    provider: AuthProvider.google,
+  })
+
+  if (!userProvider) {
+    const googleUser = await UserController.getOne({ emailGoogle: email })
+
+    if (!googleUser) {
+      userProvider = await UserProviderController.create<AuthProviderGoogleProps>({
+        user: invitedUser,
+        provider: { provider: AuthProvider.google, props: { email } },
+      })
+    } else if (invitedUser.id !== googleUser.id) {
+      done(null, false, { message: 'login.alreadyLinked' })
+      return
+    }
+  }
+
+  if (!userProvider || userProvider.props.email !== email) {
+    done(null, false, { message: 'login.notAuthorized' })
+    return
+  }
+
+  const { assessment, cycle } = await AssessmentController.getOneWithCycle({
+    uuid: userInvitation.assessmentUuid,
+    cycleUuid: userInvitation.cycleUuid,
+  })
+
+  const user = await UserController.acceptInvitation({ assessment, cycle, user: invitedUser, userInvitation })
+
+  done(null, user, {
+    message: JSON.stringify({
+      assessmentName: assessment.props.name,
+      countryIso: userInvitation.countryIso,
+      cycleName: cycle.name,
+    }),
+  })
+}
