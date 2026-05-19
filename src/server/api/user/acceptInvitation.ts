3707a9fbd16685f14db60ea23d2b0a585e4f45eb
Miró Sorja <ping@mirosorja.com>
2026-05-19 09:36:25 +0300

    5844 - Backend: update user and role props on accept

--- src/server/api/user/acceptInvitation.ts
+++ src/server/api/user/acceptInvitation.ts
@@ -1,17 +1,26 @@
 import { Response } from 'express'
 
 import { CountryRequest } from 'meta/api/request/country'
+import { UserRoleBaseProps, UserRoleExtendedProps } from 'meta/user/role/props'
+import { UserProps } from 'meta/user/user'
 
 import { AssessmentController } from 'server/controller/assessment'
 import { UserController } from 'server/controller/user'
 import { Requests } from 'server/utils'
 
+type Body = {
+  user?: { props?: Partial<UserProps> }
+  role?: { props?: UserRoleBaseProps | UserRoleExtendedProps }
+}
+
 export const acceptInvitation = async (
-  req: CountryRequest<{ invitationUuid: string }>,
+  req: CountryRequest<{ invitationUuid: string }, Body>,
   res: Response
 ): Promise<void> => {
   try {
     const { invitationUuid } = req.query
+    const userProps = req.body?.user?.props
+    const roleProps = req.body?.role?.props
 
     const { user, userInvitation } = await UserController.findByInvitation({ invitationUuid })
 
@@ -19,7 +28,14 @@ export const acceptInvitation = async (
     const { cycleUuid } = userInvitation
     const { assessment, cycle } = await AssessmentController.getOneWithCycle({ uuid, cycleUuid })
 
-    const acceptedUser = await UserController.acceptInvitation({ assessment, cycle, user, userInvitation })
+    const acceptedUser = await UserController.acceptInvitation({
+      assessment,
+      cycle,
+      user,
+      userInvitation,
+      userProps,
+      roleProps,
+    })
 
     Requests.sendOk(res, {
       user: acceptedUser,
