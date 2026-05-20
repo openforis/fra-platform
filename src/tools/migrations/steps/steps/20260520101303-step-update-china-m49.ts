acc7ebbe11c61ad14e5b922df9d11754b1defc19
Miró Sorja <ping@mirosorja.com>
2026-05-20 13:17:03 +0300

    5926 - China M49 code 159

---
+++ src/tools/migrations/steps/steps/20260520101303-step-update-china-m49.ts
@@ -0,0 +1,20 @@
+import { Promises } from 'utils/promises'
+
+import { CacheController } from 'server/cache/controller'
+import { AssessmentController } from 'server/controller/assessment'
+import { BaseProtocol } from 'server/db/db'
+
+export default async (client: BaseProtocol): Promise<void> => {
+  const assessments = await AssessmentController.getAll({})
+  const allCycles = assessments.flatMap((assessment) => assessment.cycles.map((cycle) => ({ assessment, cycle })))
+
+  // update public
+  await client.none(`
+      update public.country c
+       set m49 = 159
+     where country_iso = 'CHN'`)
+
+  await Promises.each(allCycles, async ({ assessment, cycle }) => {
+    await CacheController.generateArea({ assessment, cycle })
+  })
+}
