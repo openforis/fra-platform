d251a6976bf67ba23d65e3aea78e28e600e23377
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-05-21 09:58:36 -0500

    5931 - Read errors from props

---
+++ src/client/components/EditorWYSIWYG/hooks/useValidationTooltip.tsx
@@ -0,0 +1,41 @@
+import React, { HTMLAttributes, useMemo } from 'react'
+import ReactDOMServer from 'react-dom/server'
+
+import { TooltipId } from 'meta/tooltip/id'
+import { Objects } from 'utils/objects'
+
+type Props = {
+  validationErrors: Array<string>
+}
+
+type Returned = {
+  dataTooltipHtml?: HTMLAttributes<HTMLDivElement>['data-tooltip-html']
+  dataTooltipId?: HTMLAttributes<HTMLDivElement>['data-tooltip-id']
+  hasValidationErrors: boolean
+}
+
+export const useValidationTooltip = (props: Props): Returned => {
+  const { validationErrors } = props
+
+  return useMemo<Returned>(() => {
+    const hasValidationErrors = !Objects.isEmpty(validationErrors)
+
+    if (!hasValidationErrors) {
+      return {
+        dataTooltipHtml: undefined,
+        dataTooltipId: undefined,
+        hasValidationErrors,
+      }
+    }
+
+    const dataTooltipHtml = ReactDOMServer.renderToStaticMarkup(
+      <ul>
+        {validationErrors.map((validationError, index) => (
+          <li key={`${validationError}-${index}`}>{validationError}</li>
+        ))}
+      </ul>
+    )
+
+    return { dataTooltipHtml, dataTooltipId: TooltipId.error, hasValidationErrors }
+  }, [validationErrors])
+}
