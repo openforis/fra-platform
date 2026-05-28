fb29adaa7d171a63f18dc4319a7bd6de57f3aa78
Miró Sorja <ping@mirosorja.com>
2026-05-28 16:00:02 +0300

    5844 - revert lost file (#5952)

---
+++ src/test/e2e/playwright.config.local.ts
@@ -0,0 +1,21 @@
+import { PlaywrightTestConfig } from '@playwright/test'
+
+import baseConfig from './playwright.config'
+
+const config: PlaywrightTestConfig = {
+  ...baseConfig,
+  fullyParallel: false,
+  workers: 1,
+  retries: 0,
+  use: {
+    ...baseConfig.use,
+    headless: false,
+    baseURL: 'http://localhost:9000',
+    launchOptions: {
+      slowMo: 100,
+    },
+    trace: 'on-first-retry',
+  },
+}
+
+export default config
