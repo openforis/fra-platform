ca900f7e878c4d48dc3f6e9bc02777c7c8d9c4db
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-06-01 11:13:31 -0500

    5758 - Add description validation redux (#5969)

---
+++ src/client/store/data/tableData/validations/selectors/descriptions.ts
@@ -0,0 +1,34 @@
+import { createSelector } from '@reduxjs/toolkit'
+
+import { CountryIso } from 'meta/area/countryIso'
+import { AssessmentName } from 'meta/assessment/assessment'
+import { CycleName } from 'meta/assessment/cycle'
+import { CommentableDescriptionName } from 'meta/assessment/descriptionValue'
+import { SectionName } from 'meta/assessment/section'
+
+import { RootState } from 'client/store/types'
+
+import { getCountryValidations } from './base'
+
+export const getDescriptionValidation = createSelector(
+  [
+    getCountryValidations,
+    (
+      _state: RootState,
+      _assessmentName: AssessmentName,
+      _cycleName: CycleName,
+      _countryIso: CountryIso,
+      sectionName: SectionName
+    ) => sectionName,
+    (
+      _state: RootState,
+      _assessmentName: AssessmentName,
+      _cycleName: CycleName,
+      _countryIso: CountryIso,
+      _sectionName: SectionName,
+      descriptionName: CommentableDescriptionName
+    ) => descriptionName,
+  ],
+  (countryValidations, sectionName, descriptionName) =>
+    countryValidations.descriptions?.[sectionName]?.descriptions?.[descriptionName] ?? { valid: true }
+)
