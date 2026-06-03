10bb14fd75f6ef9e4b00daff3c010f61249ce915
Mino Togna <mino.togna@gmail.com>
2026-06-03 09:22:17 +0200

    5987: Assessment status change: Regional focal points receive email to all status change

--- src/meta/user/roles/roles.ts
+++ src/meta/user/roles/roles.ts
@@ -1,5 +1,4 @@
 import i18n from 'i18next'
-import { Dates } from 'utils/dates'
 
 import { Areas } from 'meta/area/areas'
 import { CountryStatus } from 'meta/area/countryStatus'
@@ -8,6 +7,7 @@ import { CollaboratorEditPropertyType, CollaboratorPermissions } from 'meta/user
 import { RoleName } from 'meta/user/role/name'
 import { UserRole } from 'meta/user/role/role'
 import { User } from 'meta/user/user'
+import { Dates } from 'utils/dates'
 
 const noRole = { role: 'NONE', labelKey: 'user.roles.noRole' }
 
@@ -19,18 +19,24 @@ const getRecipientRoles = (props: { status: CountryStatus }): Array<RoleName> =>
     case CountryStatus.editing:
       return [
         RoleName.COLLABORATOR,
-        RoleName.REVIEWER,
         RoleName.ALTERNATE_NATIONAL_CORRESPONDENT,
         RoleName.NATIONAL_CORRESPONDENT,
+        RoleName.REGIONAL_FOCAL_POINT,
+        RoleName.REVIEWER,
       ]
     case CountryStatus.review:
-      return [RoleName.REVIEWER]
+      return [RoleName.REGIONAL_FOCAL_POINT, RoleName.REVIEWER]
     case CountryStatus.approval:
-      return [RoleName.ADMINISTRATOR, RoleName.REVIEWER]
+      return [RoleName.ADMINISTRATOR, RoleName.REGIONAL_FOCAL_POINT, RoleName.REVIEWER]
     case CountryStatus.accepted:
-      return [RoleName.REVIEWER, RoleName.NATIONAL_CORRESPONDENT]
+      return [RoleName.REGIONAL_FOCAL_POINT, RoleName.REVIEWER, RoleName.NATIONAL_CORRESPONDENT]
     case CountryStatus.published:
-      return [RoleName.ADMINISTRATOR, RoleName.NATIONAL_CORRESPONDENT, RoleName.ALTERNATE_NATIONAL_CORRESPONDENT]
+      return [
+        RoleName.ADMINISTRATOR,
+        RoleName.NATIONAL_CORRESPONDENT,
+        RoleName.ALTERNATE_NATIONAL_CORRESPONDENT,
+        RoleName.REGIONAL_FOCAL_POINT,
+      ]
     default:
       return []
   }
