bcbbd73f4bc1f8edf58ed45748abf52f9b93e612
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-06-05 11:47:18 -0500

    5758 - Add getKey util

--- src/meta/cycleData/links/links.ts
+++ src/meta/cycleData/links/links.ts
@@ -1,7 +1,9 @@
 import { getI18nValidationStatusLabelKey } from 'meta/cycleData/links/getI18nValidationStatusLabelKey'
+import { getKey } from 'meta/cycleData/links/getKey'
 import { getLocationLabel } from 'meta/cycleData/links/getLocationLabel'
 
 export const Links = {
   getI18nValidationStatusLabelKey,
+  getKey,
   getLocationLabel,
 }
