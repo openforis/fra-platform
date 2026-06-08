99e8bcd5d68adbb3dbee7872488e1ec697e45e75
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-06-07 23:00:41 -0500

    5758 - Navigation: Data errors - Reference links - Validate in worker (#6006)
    * 5758 - Get reference links to visit
    
    * 5758 - Add location utils
    
    * 5758 - Split buildDescriptionLinkValidations & update reference link validation
    
    * 5758 - Use Objects.isEqual

--- src/meta/cycleData/links/links.ts
+++ src/meta/cycleData/links/links.ts
@@ -1,9 +1,11 @@
 import { getI18nValidationStatusLabelKey } from 'meta/cycleData/links/getI18nValidationStatusLabelKey'
 import { getKey } from 'meta/cycleData/links/getKey'
 import { getLocationLabel } from 'meta/cycleData/links/getLocationLabel'
+import { isDescriptionLocation } from 'meta/cycleData/links/isDescriptionLocation'
 
 export const Links = {
   getI18nValidationStatusLabelKey,
   getKey,
   getLocationLabel,
+  isDescriptionLocation,
 }
