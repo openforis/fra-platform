be3f85631386efb6b6c024d324bf648afa32961a
Mino Togna <mino.togna@gmail.com>
2026-06-10 15:13:03 +0200

    3596: NDP data sources: Create NDP
    Co-authored-by: sorja <ping@mirosorja.com>

--- src/server/db/repository/assessmentCycle/originalDataPoint/create.ts
+++ src/server/db/repository/assessmentCycle/originalDataPoint/create.ts
@@ -1,65 +1,62 @@
 import { Assessment } from 'meta/assessment/assessment'
 import { Cycle } from 'meta/assessment/cycle'
+import { CommentableDescriptionName } from 'meta/assessment/descriptionValue'
 import { OriginalDataPoint } from 'meta/assessment/originalDataPoint'
+import { SectionNames } from 'meta/assessment/section'
 import { TableNames } from 'meta/assessment/table'
 
 import { BaseProtocol, DB } from 'server/db/db'
-import { OriginalDataPointAdapterDeprecated } from 'server/db/repository/adapter/originalDataPoint'
 import { ODPCommentColumns } from 'server/db/repository/assessmentCycle/originalDataPoint/commentColumns'
+import { getOne } from 'server/db/repository/assessmentCycle/originalDataPoint/getOne'
 import { Schemas } from 'server/db/schemas'
 
-export const create = async (
-  params: {
-    assessment: Assessment
-    cycle: Cycle
-    originalDataPoint: OriginalDataPoint
-  },
-  client: BaseProtocol = DB
-): Promise<OriginalDataPoint> => {
-  const {
-    assessment,
-    cycle,
-    originalDataPoint: {
-      comments,
-      countryIso,
-      dataSourceAdditionalComments,
-      dataSourceMethods,
-      dataSourceReferences,
-      nationalClasses,
-      values,
-      year,
-    },
-  } = params
+type Props = {
+  assessment: Assessment
+  cycle: Cycle
+  originalDataPoint: OriginalDataPoint
+}
+
+export const create = async (props: Props, client: BaseProtocol = DB): Promise<OriginalDataPoint> => {
+  const { assessment, cycle, originalDataPoint } = props
+  const { comments, countryIso, nationalClasses, values, year } = originalDataPoint
 
   const schemaName = Schemas.getNameCycle(assessment, cycle)
-
   const commentColumnExtent = ODPCommentColumns[TableNames.extentOfForest]
   const commentColumnForestCharacteristics = ODPCommentColumns[TableNames.forestCharacteristics]
 
-  return client.one<OriginalDataPoint>(
+  const uuid = await client.one<string>(
     `
         insert into ${schemaName}.original_data_point (
           country_iso,
           year,
-          data_source_additional_comments,
-          data_source_methods,
-          data_source_references,
           ${commentColumnExtent},
           ${commentColumnForestCharacteristics},
           national_classes,
           values
-        ) values ($1, $2, $3, $4::jsonb, $5, $6, $7, $8::jsonb, $9::jsonb) returning *;`,
-    [
+        ) values ($(countryIso), $(year), $(commentsExtentOfForest), $(commentsForestCharacteristics), $(nationalClasses)::jsonb, $(values)::jsonb) returning uuid;`,
+    {
       countryIso,
       year,
-      dataSourceAdditionalComments || '',
-      dataSourceMethods ? JSON.stringify(dataSourceMethods) : '[]',
-      dataSourceReferences || '',
-      comments?.[TableNames.extentOfForest] ?? '',
-      comments?.[TableNames.forestCharacteristics] ?? '',
-      nationalClasses ? JSON.stringify(nationalClasses) : '[]',
-      values ? JSON.stringify(values) : '{}',
-    ],
-    OriginalDataPointAdapterDeprecated
+      commentsExtentOfForest: comments?.[TableNames.extentOfForest],
+      commentsForestCharacteristics: comments?.[TableNames.forestCharacteristics],
+      nationalClasses: nationalClasses ? JSON.stringify(nationalClasses) : '[]',
+      values: values ? JSON.stringify(values) : '{}',
+    },
+    (row) => row.uuid
   )
+  await client.query(
+    `
+  insert into ${schemaName}.descriptions (country_iso, section_name, section_uuid, name, value)
+  values ($(countryIso), $(sectionName), $(sectionUuid), $(name), $(value)::jsonb)
+  `,
+    {
+      countryIso,
+      sectionName: SectionNames.nationalDataPoint,
+      sectionUuid: uuid,
+      name: CommentableDescriptionName.dataSources,
+      value: JSON.stringify([]),
+    }
+  )
+
+  return getOne({ assessment, cycle, countryIso, year: String(year) }, client)
 }
