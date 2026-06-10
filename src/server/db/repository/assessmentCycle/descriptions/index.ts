4035e5551fdccb7536aad025ecf032d38f4f4bd8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-06-10 09:50:25 -0500

    5758 - Add names array to getValues

--- src/server/db/repository/assessmentCycle/descriptions/index.ts
+++ src/server/db/repository/assessmentCycle/descriptions/index.ts
@@ -1,5 +1,4 @@
 import { getDataSources } from 'server/db/repository/assessmentCycle/descriptions/getDataSources'
-import { getMany } from 'server/db/repository/assessmentCycle/descriptions/getMany'
 import { getManyWithDataSourcesLinks } from 'server/db/repository/assessmentCycle/descriptions/getManyWithDataSourcesLinks'
 import { getManyWithTextLinks } from 'server/db/repository/assessmentCycle/descriptions/getManyWithTextLinks'
 import { getValues } from 'server/db/repository/assessmentCycle/descriptions/getValues'
@@ -8,7 +7,6 @@ import { upsert } from 'server/db/repository/assessmentCycle/descriptions/upsert
 
 export const DescriptionRepository = {
   getDataSources,
-  getMany,
   getManyWithDataSourcesLinks,
   getManyWithTextLinks,
   getValues,
