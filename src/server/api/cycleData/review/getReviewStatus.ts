0d9ffb0e02cce5ba6857b7fc6b1eaecbf3da71f7
Mino Togna <mino.togna@gmail.com>
2026-06-16 13:57:15 +0200

    6050: Split ReviewController

--- src/server/api/cycleData/review/getReviewStatus.ts
+++ src/server/api/cycleData/review/getReviewStatus.ts
@@ -2,7 +2,7 @@ import { Response } from 'express'
 
 import { CycleDataRequest } from 'meta/api/request/cycleData/cycleData'
 
-import { CycleDataController } from 'server/controller/cycleData'
+import { ReviewController } from 'server/controller/cycleData/review'
 import Requests from 'server/utils/requests'
 
 export const getReviewStatus = async (req: CycleDataRequest<{ odpId: string }>, res: Response): Promise<void> => {
@@ -11,7 +11,7 @@ export const getReviewStatus = async (req: CycleDataRequest<{ odpId: string }>, 
     const { countryIso, odpId, sectionName } = req.query
 
     const user = Requests.getUser(req)
-    const reviewStatus = await CycleDataController.getReviewStatus({
+    const reviewStatus = await ReviewController.getReviewStatus({
       assessment,
       cycle,
       countryIso,
--- src/server/controller/cycleData/index.ts
+++ src/server/controller/cycleData/index.ts
@@ -2,15 +2,8 @@ import { Links } from 'server/controller/cycleData/links'
 import { Report } from 'server/controller/cycleData/report'
 import { Repository } from 'server/controller/cycleData/repository'
 import { CountryActivityLogRepository } from 'server/db/repository/assessmentCycle/countryActivityLog'
-import { MessageTopicUserRepository } from 'server/db/repository/assessmentCycle/messageTopicUser'
-
-import { getReviewStatus } from './getReviewStatus'
 
 export const CycleDataController = {
-  // ===== review
-  getReviewStatus,
-  getReviewSummary: MessageTopicUserRepository.getReviewSummary,
-
   // ==== activities
   getActivities: CountryActivityLogRepository.getMany,
   getActivitiesCount: CountryActivityLogRepository.getCount,
