4c231bb4c0662a5f757ae6fe1802532e23a84c80
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-06-23 12:24:31 -0500

    5758 - Add NDP redis updateValidations

--- src/meta/assessment/validation/nationalDataPointValidations/index.ts
+++ src/meta/assessment/validation/nationalDataPointValidations/index.ts
@@ -1,5 +1,7 @@
 import { calculateSummary } from 'meta/assessment/validation/nationalDataPointValidations/calculateSummary'
+import { mergeValidations } from 'meta/assessment/validation/nationalDataPointValidations/mergeValidations'
 
 export const NationalDataPointValidations = {
   calculateSummary,
+  mergeValidations,
 }
--- src/server/cache/repository/validation/nationalDataPoint/index.ts
+++ src/server/cache/repository/validation/nationalDataPoint/index.ts
@@ -1,7 +1,9 @@
 import { getValidations } from 'server/cache/repository/validation/nationalDataPoint/getValidations'
 import { setValidations } from 'server/cache/repository/validation/nationalDataPoint/setValidations'
+import { updateValidations } from 'server/cache/repository/validation/nationalDataPoint/updateValidations'
 
 export const NationalDataPointValidationRedisRepository = {
   getValidations,
   setValidations,
+  updateValidations,
 }
