ba1b192c0ebbe88920ad6ed75011de785ca49795
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-06-22 20:41:18 -0500

    5758 - Add redis getNationalDataPointValidations

--- src/server/cache/repository/validation/nationalDataPoint/index.ts
+++ src/server/cache/repository/validation/nationalDataPoint/index.ts
@@ -1,5 +1,7 @@
+import { getNationalDataPointValidations } from 'server/cache/repository/validation/nationalDataPoint/getNationalDataPointValidations'
 import { setNationalDataPointValidations } from 'server/cache/repository/validation/nationalDataPoint/setNationalDataPointValidations'
 
 export const NationalDataPointValidationRedisRepository = {
+  getNationalDataPointValidations,
   setNationalDataPointValidations,
 }
