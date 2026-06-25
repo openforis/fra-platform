e71a28c4e16b65d0ebab3fdf570a34cbf6b54e18
Miró Sorja <ping@mirosorja.com>
2026-06-25 14:23:26 +0300

    6073 - E2E: tooltip: take into account multiple tooltips

--- src/test/e2e/utils/tooltip/index.ts
+++ src/test/e2e/utils/tooltip/index.ts
@@ -2,7 +2,11 @@ import { expect, Locator, Page } from '@playwright/test'
 
 const expectValidationTooltip = async (page: Page, locator: Locator, text: string): Promise<void> => {
   await locator.hover()
-  await expect(page.getByRole('tooltip').filter({ hasText: text })).toBeVisible({ timeout: 20000 })
+
+  // When one or more tooltips are visible at the same time, choose the one that's not closing
+  // __closing = we are not hovering it anymore
+  const tooltip = page.locator('[role="tooltip"]:not(.react-tooltip__closing)').filter({ hasText: text })
+  await expect(tooltip).toBeVisible({ timeout: 20000 })
 }
 
 export const TooltipUtils = {
