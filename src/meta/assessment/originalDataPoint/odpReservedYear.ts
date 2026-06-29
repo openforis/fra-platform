8cbebe4983e3a6671aa33777b17bacd72551f914
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-06-29 12:50:30 -0500

    5758 - Return uudi in getReservedYears

--- src/meta/assessment/originalDataPoint/odpReservedYear.ts
+++ src/meta/assessment/originalDataPoint/odpReservedYear.ts
@@ -1,5 +1,8 @@
+import { UUID } from 'meta/uuid/uuid'
+
 export interface ODPReservedYear {
   id: number
-  year: number
   nationalClasses: number
+  uuid: UUID
+  year: number
 }
--- src/server/db/repository/assessmentCycle/originalDataPoint/getReservedYears.ts
+++ src/server/db/repository/assessmentCycle/originalDataPoint/getReservedYears.ts
@@ -1,9 +1,8 @@
-import { Objects } from 'utils/objects'
-
 import { CountryIso } from 'meta/area/countryIso'
 import { Assessment } from 'meta/assessment/assessment'
 import { Cycle } from 'meta/assessment/cycle'
 import { ODPReservedYear } from 'meta/assessment/originalDataPoint'
+import { Objects } from 'utils/objects'
 
 import { BaseProtocol, DB } from 'server/db/db'
 import { Schemas } from 'server/db/schemas'
@@ -22,7 +21,7 @@ export const getReservedYears = async (
 
   return client.map<ODPReservedYear>(
     `
-      select id, year, jsonb_array_length(national_classes) AS national_classes
+      select id, uuid, year, jsonb_array_length(national_classes) AS national_classes
       from ${schemaName}.original_data_point
       where country_iso = $1
     `,
