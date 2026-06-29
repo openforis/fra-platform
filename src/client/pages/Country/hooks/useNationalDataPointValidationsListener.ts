652ca33bc6c8be4b9720f3ade0dfe5d1decfdf4e
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-06-29 14:58:13 -0500

    5758 - Emit event on NDP edits

---
+++ src/client/pages/Country/hooks/useNationalDataPointValidationsListener.ts
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+
+import { CountryIso } from 'meta/area/countryIso'
+import { RecordNDPValidations } from 'meta/assessment/validation/nationalDataPoint'
+import { Sockets } from 'meta/socket/sockets'
+
+import { ValidationsActions } from 'client/store/data/tableData/validations/actions'
+import { useAppDispatch } from 'client/store/hooks'
+import { useCanEditCycleData } from 'client/store/user/hooks/auth'
+import { useCountryRouteParams } from 'client/hooks/routeParams'
+import { SocketClient } from 'client/service/socket/client'
+
+type NationalDataPointValidationsListenerArgs = [{ validations: RecordNDPValidations }]
+
+export const useNationalDataPointValidationsListener = (): void => {
+  const { assessmentName, countryIso, cycleName } = useCountryRouteParams<CountryIso>()
+  const canEditData = useCanEditCycleData()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (!canEditData) return
+
+    const eventName = Sockets.getNationalDataPointValidationsUpdateEvent({ countryIso, assessmentName, cycleName })
+
+    const listener = (args: NationalDataPointValidationsListenerArgs): void => {
+      const [{ validations }] = args
+      dispatch(
+        ValidationsActions.updateNationalDataPointValidations({ assessmentName, cycleName, countryIso, validations })
+      )
+    }
+
+    SocketClient.on(eventName, listener)
+
+    return (): void => {
+      SocketClient.off(eventName, listener)
+    }
+  }, [assessmentName, canEditData, countryIso, cycleName, dispatch])
+}
