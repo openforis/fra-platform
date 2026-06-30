60e9670e7f08798b3b16a8fb5e867bfe12ea2c71
Miró Sorja <ping@mirosorja.com>
2026-06-30 12:40:59 +0300

    6106 - Update latest and latest2 header labels
    Co-authored-by: Mino Togna <mino.togna@gmail.com>

--- src/client/components/PageLayout/Header/CycleSwitcher/CycleSwitcher.tsx
+++ src/client/components/PageLayout/Header/CycleSwitcher/CycleSwitcher.tsx
@@ -2,10 +2,13 @@ import './CycleSwitcher.scss'
 import React from 'react'
 
 import { Assessments } from 'meta/assessment/assessments'
+import { CycleNames } from 'meta/assessment/cycle/names'
 import { Cycles } from 'meta/assessment/cycles'
+import { Users } from 'meta/user/users'
 
 import { useAssessment } from 'client/store/meta/hooks/assessments'
 import { useCycle } from 'client/store/meta/hooks/cycles'
+import { useUser } from 'client/store/user/hooks/user'
 import { useCycleRouteParams } from 'client/hooks/routeParams'
 import Icon from 'client/components/Icon'
 import PopoverControl from 'client/components/PopoverControl'
@@ -17,6 +20,7 @@ const CycleSwitcher: React.FC = () => {
   const cycle = useCycle()
   const { cycleName } = useCycleRouteParams()
   const popoverItems = usePopoverItems()
+  const user = useUser()
 
   if (popoverItems.length < 1) {
     const cycleLabel = Cycles.isPublished(cycle) ? cycleName : Assessments.getLastPublishedCycle(assessment).name
@@ -24,7 +28,9 @@ const CycleSwitcher: React.FC = () => {
   }
 
   const latestCycle = Assessments.getLastCreatedCycle(assessment)
-  const displayName = latestCycle.name === cycleName ? '' : cycleName
+  const isCycleNameEmpty =
+    (latestCycle.name === cycleName || cycleName?.includes(CycleNames.latest)) && !Users.isAdministrator(user)
+  const displayName = isCycleNameEmpty ? '' : cycleName
 
   return (
     <div className="cycle-switcher">
