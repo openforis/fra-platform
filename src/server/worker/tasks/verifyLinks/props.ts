b8f00221bbcf1c7a6fb4bb308036ab7ea7651bc8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-07-03 13:05:16 -0500

    5758 - Add shell NDP links worker

--- src/server/worker/tasks/verifyLinks/props.ts
+++ src/server/worker/tasks/verifyLinks/props.ts
@@ -2,7 +2,11 @@ import { Job } from 'bullmq'
 
 import { VerifyAllLinksJobProps } from 'server/worker/tasks/verifyLinks/visitCycleLinks/props'
 import { VerifyDescriptionLinksJobProps } from 'server/worker/tasks/verifyLinks/visitDescriptionLinks/props'
+import { VerifyNationalDataPointLinksJobProps } from 'server/worker/tasks/verifyLinks/visitNationalDataPointLinks/props'
 
-export type VerifyLinksQueueProps = VerifyAllLinksJobProps | VerifyDescriptionLinksJobProps
+export type VerifyLinksQueueProps =
+  | VerifyAllLinksJobProps
+  | VerifyDescriptionLinksJobProps
+  | VerifyNationalDataPointLinksJobProps
 
 export type VerifyLinksQueueJob = Job<VerifyLinksQueueProps>
---
+++ src/server/worker/tasks/verifyLinks/visitNationalDataPointLinks/props.ts
@@ -0,0 +1,21 @@
+import { Job } from 'bullmq'
+
+import { CountryIso } from 'meta/area/countryIso'
+import { Assessment } from 'meta/assessment/assessment'
+import { Cycle } from 'meta/assessment/cycle'
+
+import { VerifyLinksJobName } from 'server/worker/tasks/verifyLinks/jobNames'
+
+export type VerifyNationalDataPointLinksJobProps = {
+  assessment: Assessment
+  countryIso: CountryIso
+  cycle: Cycle
+  notifyClients?: boolean
+  // TODO: targets: Array<NDPLinkTarget>
+}
+
+export type VerifyNationalDataPointLinksJob = Job<
+  VerifyNationalDataPointLinksJobProps,
+  void,
+  typeof VerifyLinksJobName.verifyNationalDataPointLinks
+>
