dc540adb26c43fba312269b882ecb7061e62fa60
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-07-02 23:52:31 -0500

    5758 - Navigation: Data errors - NDP - Delete validations on NDP deletion (#6112)
    * 5758 - Delete NDP validations on NDP deletion
    
    * 5758 - Add separe event & action
    
    * 5758 - Format lines
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/server/cache/repository/validation/nationalDataPoint/index.ts
+++ src/server/cache/repository/validation/nationalDataPoint/index.ts
@@ -1,8 +1,10 @@
+import { deleteValidation } from 'server/cache/repository/validation/nationalDataPoint/deleteValidation'
 import { getValidation } from 'server/cache/repository/validation/nationalDataPoint/getValidation'
 import { getValidations } from 'server/cache/repository/validation/nationalDataPoint/getValidations'
 import { setValidations } from 'server/cache/repository/validation/nationalDataPoint/setValidations'
 
 export const NationalDataPointValidationRedisRepository = {
+  deleteValidation,
   getValidation,
   getValidations,
   setValidations,
