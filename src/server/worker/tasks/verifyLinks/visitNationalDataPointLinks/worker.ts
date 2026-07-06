d13ff0d720ff99b44db2c18d173ae64e95dd8377
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-07-06 09:16:46 -0500

    5758 - Navigation: Data errors - NDP - Add buildNationalDataPointLinkValidations  (#6121)
    * 5758 - Add Links.isOriginalDataPointLocation
    
    * 5758 - Extract common getLinkValidationMessage & buildVisitedLinksByKey
    
    * 5758 - Add buildNationalDataPointLinkValidations
    
    * 5758 - Add ndp prefix in updateNationalDataPointLocationValidation
    
    * 5758 - use national data point name

--- src/server/worker/tasks/verifyLinks/visitNationalDataPointLinks/worker.ts
+++ src/server/worker/tasks/verifyLinks/visitNationalDataPointLinks/worker.ts
@@ -1,5 +1,6 @@
 import { Logger } from 'server/utils/logger'
 
+import { buildNationalDataPointLinkValidations } from './utils/buildNationalDataPointLinkValidations'
 import { getNationalDataPointLinks } from './utils/getNationalDataPointLinks'
 import { syncNationalDataPointLinks } from './utils/syncNationalDataPointLinks'
 import { VerifyNationalDataPointLinksJob } from './props'
@@ -21,7 +22,17 @@ export default async (job: VerifyNationalDataPointLinksJob): Promise<void> => {
 
     const linksToVisit = await getNationalDataPointLinks({ assessment, countryIso, cycle, targets })
 
-    const { linkVisits } = await syncNationalDataPointLinks({ assessment, countryIso, cycle, linksToVisit, targets })
+    const { approvedLinks, linkVisits } = await syncNationalDataPointLinks({
+      assessment,
+      countryIso,
+      cycle,
+      linksToVisit,
+      targets,
+    })
+
+    const linkValidations = buildNationalDataPointLinkValidations({ approvedLinks, linkVisits, linksToVisit })
+    Logger.debug(`${logKey} built link validations for ${Object.keys(linkValidations).length} national data points.`)
+    // TODO: persist the link validations and notify clients
 
     const duration = (new Date().getTime() - time) / 1000
     Logger.info(`${logKey} ended in ${duration} seconds with ${linkVisits.length} links visited.`)
