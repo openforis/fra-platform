27506d0f1f31c421d80dd9c778ee2719e6ffe5b7
Miró Sorja <ping@mirosorja.com>
2026-07-07 15:06:15 +0300

    5758 - Worker: visitDescriptionLinks: export functionality

--- src/server/worker/tasks/verifyLinks/visitDescriptionLinks/worker.ts
+++ src/server/worker/tasks/verifyLinks/visitDescriptionLinks/worker.ts
@@ -8,72 +8,69 @@ import { Logger } from 'server/utils/logger'
 import { buildDescriptionLinkValidations } from './utils/buildDescriptionLinkValidations'
 import { getDescriptionLinks } from './utils/getDescriptionLinks'
 import { syncDescriptionLinks } from './utils/syncDescriptionLinks'
-import { VerifyDescriptionLinksJob } from './props'
+import { VerifyDescriptionLinksJob, VerifyDescriptionLinksJobProps } from './props'
 
-const _getLogKey = (job: VerifyDescriptionLinksJob): string => {
-  const { assessment, countryIso, cycle } = job.data
+export const runDescriptionLinkValidation = async (props: VerifyDescriptionLinksJobProps): Promise<void> => {
+  const { assessment, countryIso, cycle, descriptionIdentifiers, notifyClients = true } = props
+  const logKey = `[visitDescriptionLinks] [${assessment.props.name}-${cycle.name}-${countryIso}]`
+  const time = new Date().getTime()
 
-  return `[visitDescriptionLinks-workerThread] [${assessment.props.name}-${cycle.name}-${countryIso}] [job-${job.id}]`
+  Logger.info(`${logKey} started.`)
+
+  const { descriptions, linksToVisit } = await getDescriptionLinks({
+    assessment,
+    countryIso,
+    cycle,
+    descriptionIdentifiers,
+  })
+
+  if (Objects.isEmpty(descriptions)) {
+    Logger.info(`${logKey} ended with no descriptions to validate.`)
+    return
+  }
+
+  const { approvedLinks, linkVisits } = await syncDescriptionLinks({
+    assessment,
+    countryIso,
+    cycle,
+    descriptions,
+    linksToVisit,
+  })
+
+  const descriptionValidations = buildDescriptionLinkValidations({
+    approvedLinks,
+    initialDescriptions: descriptions,
+    linkVisits,
+    linksToVisit,
+  })
+
+  const sectionNames = Array.from(new Set(descriptions.map(({ sectionName }) => sectionName)))
+
+  await DescriptionValidationRedisRepository.setValidations({
+    assessment,
+    countryIso,
+    cycle,
+    descriptionValidations,
+  })
+
+  if (notifyClients) {
+    const eventName = Sockets.getDescriptionValidationsUpdateEvent({
+      assessmentName: assessment.props.name,
+      countryIso,
+      cycleName: cycle.name,
+    })
+    SocketServer.emit(eventName, { descriptionValidations, sectionNames })
+  }
+
+  const duration = (new Date().getTime() - time) / 1000
+  Logger.info(`${logKey} ended in ${duration} seconds with ${linkVisits.length} links visited.`)
 }
 
 export default async (job: VerifyDescriptionLinksJob): Promise<void> => {
-  const logKey = _getLogKey(job)
-
   try {
-    const { assessment, countryIso, cycle, descriptionIdentifiers, notifyClients = true } = job.data
-    const time = new Date().getTime()
-
-    Logger.info(`${logKey} started.`)
-
-    const { descriptions, linksToVisit } = await getDescriptionLinks({
-      assessment,
-      countryIso,
-      cycle,
-      descriptionIdentifiers,
-    })
-
-    if (Objects.isEmpty(descriptions)) {
-      Logger.info(`${logKey} ended with no descriptions to validate.`)
-      return
-    }
-
-    const { approvedLinks, linkVisits } = await syncDescriptionLinks({
-      assessment,
-      countryIso,
-      cycle,
-      descriptions,
-      linksToVisit,
-    })
-
-    const descriptionValidations = buildDescriptionLinkValidations({
-      approvedLinks,
-      initialDescriptions: descriptions,
-      linkVisits,
-      linksToVisit,
-    })
-
-    const sectionNames = Array.from(new Set(descriptions.map(({ sectionName }) => sectionName)))
-
-    await DescriptionValidationRedisRepository.setValidations({
-      assessment,
-      countryIso,
-      cycle,
-      descriptionValidations,
-    })
-
-    if (notifyClients) {
-      const eventName = Sockets.getDescriptionValidationsUpdateEvent({
-        assessmentName: assessment.props.name,
-        countryIso,
-        cycleName: cycle.name,
-      })
-      SocketServer.emit(eventName, { descriptionValidations, sectionNames })
-    }
-
-    const duration = (new Date().getTime() - time) / 1000
-    Logger.info(`${logKey} ended in ${duration} seconds with ${linkVisits.length} links visited.`)
+    await runDescriptionLinkValidation(job.data)
   } catch (error) {
-    Logger.error(`${logKey} Error.`)
+    Logger.error(`[visitDescriptionLinks-workerThread] [job-${job.id}] Error.`)
     Logger.error(error)
     throw error
   }
