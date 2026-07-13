47a58cc550dee869e4162cf837f44079c09154c6
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-07-13 14:53:51 -0500

    6097 - Upgrade react router

--- src/server/start.ts
+++ src/server/start.ts
@@ -1,16 +1,26 @@
 import 'dotenv/config'
 import 'tsconfig-paths/register'
 
+import { createRequire } from 'node:module'
+
 import { Logger } from './utils/logger'
 import { NodeEnv, ProcessEnv } from './utils/processEnv'
 import { serverInit } from './serverInit'
 
+type VerifyLinksWorkerModule = {
+  triggerVerifyLinksWorker: () => Promise<void>
+}
+
+const loadModule = createRequire(__filename)
+
 serverInit()
 
 if (ProcessEnv.nodeEnv === NodeEnv.development) {
-  void import('./worker/tasks/verifyLinks/triggerVerifyLinksWorker')
-    .then(({ triggerVerifyLinksWorker }) => triggerVerifyLinksWorker())
-    .catch((error) => {
-      Logger.error(`[verifyLinks] failed to start local worker: ${JSON.stringify(error)}`)
-    })
+  const { triggerVerifyLinksWorker } = loadModule(
+    './worker/tasks/verifyLinks/triggerVerifyLinksWorker'
+  ) as VerifyLinksWorkerModule
+
+  void triggerVerifyLinksWorker().catch((error) => {
+    Logger.error(`[verifyLinks] failed to start local worker: ${JSON.stringify(error)}`)
+  })
 }
