8df095c6ace3d800172cee20449ded7298c9fcfd
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-07-24 13:29:48 -0500

    5758 - Add LinksService.enqueueAllLinksValidation

--- src/server/service/links/index.ts
+++ src/server/service/links/index.ts
@@ -1,8 +1,10 @@
 import { enqueueDescriptionLinksValidation } from 'server/service/links/enqueueDescriptionLinksValidation'
 import { enqueueNationalDataPointLinksValidation } from 'server/service/links/enqueueNationalDataPointLinksValidation'
 import { verifyLinks } from 'server/service/links/verifyLinks'
+import { scheduleVerifyAllLinks } from 'server/worker/tasks/verifyLinks/visitCycleLinks/scheduleVerifyAllLinks'
 
 export const LinksService = {
+  enqueueAllLinksValidation: scheduleVerifyAllLinks,
   enqueueDescriptionLinksValidation,
   enqueueNationalDataPointLinksValidation,
   verifyLinks,
