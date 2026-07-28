2235565e10c1abdd15c239ee7116d8058ae361a8
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-07-28 18:05:23 -0500

    5758 - Add DataValidationService.notifyDescriptionValidationUpdate

--- src/server/service/dataValidation/index.ts
+++ src/server/service/dataValidation/index.ts
@@ -1,8 +1,10 @@
+import { notifyDescriptionValidationUpdate } from './descriptions/notifyDescriptionValidationUpdate'
 import { validateDataSources } from './validateDataSources'
 import { validateDescriptions } from './validateDescriptions'
 import { validateNodes } from './validateNodes'
 
 export const DataValidationService = {
+  notifyDescriptionValidationUpdate,
   validateDataSources,
   validateDescriptions,
   validateNodes,
