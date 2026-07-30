3c5a786a9963211737580fdb0084ce339d467699
Miró Sorja <ping@mirosorja.com>
2026-07-30 09:06:56 +0300

    6111 - update logic

--- src/client/components/PageLayout/Header/CycleSwitcher/hooks/useDisplayName.ts
+++ src/client/components/PageLayout/Header/CycleSwitcher/hooks/useDisplayName.ts
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next'
 
-import { AssessmentNames } from 'meta/assessment/assessment'
 import { Assessments } from 'meta/assessment/assessments'
 import { CycleNames } from 'meta/assessment/cycle/names'
 
@@ -13,22 +12,10 @@ export const useDisplayName = (): string => {
 
   const { t } = useTranslation()
 
-  const { name: assessmentName } = assessment.props
-
-  // Hide header cycle name for panEuropean last created cycle
-  const isPanEuropean = assessmentName === AssessmentNames.panEuropean
   const isLastCreatedCycle = Assessments.getLastCreatedCycle(assessment).name === cycleName
-  if (isPanEuropean && isLastCreatedCycle) {
-    return ''
-  }
 
-  // TODO: NOTE: This logic is subject to change when we merge latest2 to latest
-  // Hide header cycle name for fra cycle named 'latest', show 'in Progress' for 'latest2'
-  const isFra = assessmentName === AssessmentNames.fra
-  if (isFra) {
-    if (cycleName === CycleNames.latest) return ''
-    if (cycleName === CycleNames.latest2) return t('common.inProgress')
-  }
+  if (cycleName === CycleNames.latest2) return t('common.inProgress')
+  if (cycleName === CycleNames.latest || isLastCreatedCycle) return ''
 
   return cycleName
 }
