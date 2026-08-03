3a4c3bd16c21b081b381e99513a3e8a6294cf0fb
Miró Sorja <ping@mirosorja.com>
2026-08-03 13:16:59 +0300

    6175 - Tools cycle : merge (#6185)
    * 6175 - Tools cycle : merge
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle : merge ndp
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle : merge descriptions
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle : merge repository
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle : merge message topic
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle : merge activity log
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle : deprecate cycle from
    
    Co-authored-by: sorja <ping@mirosorja.com>
    
    * 6175 - Tools cycle merge: updateCache (#6189)
    
    * 6175 - Merge countries and node_ext (#6196)
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    
    * 6175 - Fix cycle create props.status (#6197)
    
    * 6175 - Run merge cycle in transaction (#6198)
    
    ---------
    
    Co-authored-by: Mino Togna <mino.togna@gmail.com>
    Co-authored-by: Mino Togna <minotogna@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/tools/cycles/merge/mergeData.ts
@@ -0,0 +1,96 @@
+import { NodeExtType } from 'meta/nodeExt/nodeExt'
+import { PropsMerge } from 'tools/cycles/merge/_types'
+import { getSchemas } from 'tools/cycles/merge/_utils'
+
+import { BaseProtocol } from 'server/db/db'
+
+export const mergeData = async (props: PropsMerge, client: BaseProtocol): Promise<void> => {
+  const { countryISOs } = props
+
+  const { schemaCycleFrom, schemaCycleTo } = getSchemas(props)
+  const params = { countryISOs }
+
+  await client.query(
+    `
+      delete
+      from ${schemaCycleTo}.node
+      where country_iso in ($(countryISOs:list));
+
+      insert into ${schemaCycleTo}.node
+        (uuid, country_iso, row_uuid, col_uuid, value)
+      select f.uuid, f.country_iso, f.row_uuid, f.col_uuid, f.value
+      from ${schemaCycleFrom}.node f
+      where f.country_iso in ($(countryISOs:list));
+
+      -- node_ext : only contacts
+      delete
+      from ${schemaCycleTo}.node_ext
+      where country_iso in ($(countryISOs:list))
+        and type = '${NodeExtType.contact}';
+
+      insert into ${schemaCycleTo}.node_ext
+        (uuid, country_iso, parent_uuid, props, type, value)
+      select f.uuid, f.country_iso, f.parent_uuid, f.props, f.type, f.value
+      from ${schemaCycleFrom}.node_ext f
+      where f.country_iso in ($(countryISOs:list))
+        and f.type = '${NodeExtType.contact}';
+
+      delete
+      from ${schemaCycleTo}.node_values_estimation
+      where country_iso in ($(countryISOs:list));
+
+      insert into ${schemaCycleTo}.node_values_estimation
+        (uuid, country_iso, table_uuid, created_at, method, variables)
+      select f.uuid, f.country_iso, f.table_uuid, f.created_at, f.method, f.variables
+      from ${schemaCycleFrom}.node_values_estimation f
+      where f.country_iso in ($(countryISOs:list));
+
+      delete
+      from ${schemaCycleTo}.original_data_point
+      where country_iso in ($(countryISOs:list));
+
+      insert into ${schemaCycleTo}.original_data_point
+      (country_iso, "year", comments_extentofforest, national_classes, "values", id_legacy,
+       comments_forestcharacteristics, uuid)
+      select f.country_iso,
+             f."year",
+             f.comments_extentofforest,
+             f.national_classes,
+             f."values",
+             f.id_legacy,
+             f.comments_forestcharacteristics,
+             f.uuid
+      from ${schemaCycleFrom}.original_data_point f
+      where f.country_iso in ($(countryISOs:list));
+
+      delete
+      from ${schemaCycleTo}.descriptions
+      where country_iso in ($(countryISOs:list));
+
+      insert into ${schemaCycleTo}.descriptions
+        (country_iso, section_name, name, value, section_uuid)
+      select f.country_iso, f.section_name, f.name, f.value, f.section_uuid
+      from ${schemaCycleFrom}.descriptions f
+      where f.country_iso in ($(countryISOs:list));
+
+      delete
+      from ${schemaCycleTo}.repository
+      where country_iso in ($(countryISOs:list));
+
+      insert into ${schemaCycleTo}.repository (uuid, country_iso, file_uuid, link, props, folder_name, parent_uuid, description,
+                              created_at)
+      select f.uuid,
+             f.country_iso,
+             f.file_uuid,
+             f.link,
+             f.props,
+             f.folder_name,
+             f.parent_uuid,
+             f.description,
+             f.created_at
+      from ${schemaCycleFrom}.repository f
+      where f.country_iso in ($(countryISOs:list));
+    `,
+    params
+  )
+}
