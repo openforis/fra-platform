40b4c583fad8fad7a93765399637004190359e36
Mino Togna <minotogna@users.noreply.github.com>
2026-08-03 07:41:57 +0200

    6175 - Tools cycle merge: updateCache (#6189)

--- src/tools/cycles/merge/updateCache.ts
+++ src/tools/cycles/merge/updateCache.ts
@@ -2,9 +2,15 @@ import { PropsMerge } from 'tools/cycles/merge/_types'
 
 import { CacheController } from 'server/cache/controller'
 import { BaseProtocol } from 'server/db/db'
+import { RefreshMaterializedViews } from 'server/worker/cronJobs/refreshMaterializedViews'
 
 export const updateCache = async (props: PropsMerge, client: BaseProtocol): Promise<void> => {
-  const { assessment, cycleTo: cycle } = props
+  const { cycleTo: cycle } = props
 
+  const assessment = await CacheController.generateAssessment({ assessmentName: props.assessment.props.name }, client)
+  await CacheController.generateMetaCache({ assessments: [assessment] }, client)
+  await CacheController.generateMetadata({ assessment }, client)
   await CacheController.generateData({ assessment, cycle }, client)
+
+  await new RefreshMaterializedViews().run()
 }
