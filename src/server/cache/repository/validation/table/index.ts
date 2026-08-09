d4941a46e929e753bbf9dd6c649e7d8a9b05dd60
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-08-09 18:26:40 -0500

    5758 - Add renameValidationsCache to renameCycle

--- src/server/cache/repository/validation/table/index.ts
+++ src/server/cache/repository/validation/table/index.ts
@@ -1,9 +1,11 @@
 import { getValidations } from 'server/cache/repository/validation/table/getValidations'
 import { removeValidations } from 'server/cache/repository/validation/table/removeValidations'
+import { renameValidations } from 'server/cache/repository/validation/table/renameValidations'
 import { setValidations } from 'server/cache/repository/validation/table/setValidations'
 
 export const TableValidationRedisRepository = {
   getValidations,
   removeValidations,
+  renameValidations,
   setValidations,
 }
