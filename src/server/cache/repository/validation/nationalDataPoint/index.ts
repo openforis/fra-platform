67d54b9383aef015e3b875e906a3e2f62eb81b75
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-08-12 13:31:48 -0500

    5758 - Navigation: Data errors - Cycles - Clone & Merge (#6227)
    * 5758 - Add copyValidations
    
    * 5758 - Add merge and clone validations
    
    * 5758 - Fix removeMetadata
    
    * 5758 - Remove mergeValidations
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/server/cache/repository/validation/nationalDataPoint/index.ts
+++ src/server/cache/repository/validation/nationalDataPoint/index.ts
@@ -1,3 +1,4 @@
+import { copyValidations } from 'server/cache/repository/validation/nationalDataPoint/copyValidations'
 import { deleteValidations } from 'server/cache/repository/validation/nationalDataPoint/deleteValidations'
 import { getValidation } from 'server/cache/repository/validation/nationalDataPoint/getValidation'
 import { getValidations } from 'server/cache/repository/validation/nationalDataPoint/getValidations'
@@ -6,6 +7,7 @@ import { renameValidations } from 'server/cache/repository/validation/nationalDa
 import { setValidations } from 'server/cache/repository/validation/nationalDataPoint/setValidations'
 
 export const NationalDataPointValidationRedisRepository = {
+  copyValidations,
   deleteValidations,
   getValidation,
   getValidations,
