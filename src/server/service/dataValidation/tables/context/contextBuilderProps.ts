0771192f2a65132e717f21a94585bde2e88048be
Miró Sorja <ping@mirosorja.com>
2026-08-17 13:08:26 +0300

    6233 - Data entry: Database lock (#6238)
    * 6233 - Country level lock
    
    * 6233 - Pass down client to calculations and validations
    
    * 6233 - Serialize writes per assessment/cycle/country
    
    * Revert "6233 - Serialize writes per assessment/cycle/country"
    
    This reverts commit 41ab1b716af3d6d49e1888025cd9f3b83a4789ff.
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/server/service/dataValidation/tables/context/contextBuilderProps.ts
+++ src/server/service/dataValidation/tables/context/contextBuilderProps.ts
@@ -3,8 +3,11 @@ import { Assessment } from 'meta/assessment/assessment'
 import { Cycle } from 'meta/assessment/cycle'
 import { NodeUpdates } from 'meta/data/nodeUpdates'
 
+import { BaseProtocol } from 'server/db/db'
+
 export type ContextBuilderProps = {
   assessment: Assessment
+  client: BaseProtocol
   country: Country
   cycle: Cycle
   nodeUpdates: NodeUpdates
