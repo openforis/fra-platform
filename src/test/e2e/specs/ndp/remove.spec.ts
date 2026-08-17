424e5d75cdf29ff80867709f675c9edddd37ec80
Miró Sorja <ping@mirosorja.com>
2026-08-17 18:29:37 +0300

    5758 - E2E: ODP Full test: Refactor 1 (#6226)
    * 5758 - Refactor NDP e2e test
    
    * 5758 - Clean up comment
    
    * 5758 - Clean up const
    
    * 5758 - Refactor NDP e2e test

---
+++ src/test/e2e/specs/ndp/remove.spec.ts
@@ -0,0 +1,33 @@
+import { SectionNames } from 'meta/assessment/section'
+
+import { expect, test } from 'test/e2e/fixtures/ndp'
+import { DOMUtils } from 'test/e2e/utils/dom'
+import { SectionUtils } from 'test/e2e/utils/section'
+import { TableDomUtils } from 'test/e2e/utils/table'
+
+const countryIso = 'X09'
+const extentOfForestPath = SectionUtils.path({ countryIso, sectionName: SectionNames.extentOfForest })
+
+const seededYear = 2015
+const seededUrlRegex = new RegExp(`/originalDataPoints/${seededYear}/extentOfForest$`)
+
+test.describe('National data point: remove', () => {
+  test.use({ ndpSeeds: [{ countryIso, nationalClasses: [], year: seededYear }] })
+
+  test('NC deletes the national data point', async ({ authenticatedPage, ndp }) => {
+    const page = authenticatedPage
+    expect(ndp.id).toBeTruthy()
+
+    await page.goto(extentOfForestPath)
+    await DOMUtils.ensureEditingUnlocked(page)
+    await TableDomUtils.clickOdpLink(page, String(seededYear), seededUrlRegex)
+
+    page.once('dialog', (dialog) => dialog.accept())
+    await page.getByRole('button', { name: 'Delete' }).first().click()
+
+    await expect(page).toHaveURL(/\/sections\/extentOfForest$/)
+    await expect(page.locator('.table-grid__odp-link', { hasText: String(seededYear) })).toHaveCount(0, {
+      timeout: 10000,
+    })
+  })
+})
