0ece7df40ea86372ddfb7c2048824944be396a05
Miró Sorja <ping@mirosorja.com>
2026-08-18 15:05:57 +0300

    5758 - NDP e2e test: extent of forest (#6245)

--- src/test/e2e/utils/navigation/index.ts
+++ src/test/e2e/utils/navigation/index.ts
@@ -18,12 +18,17 @@ type ExpectNavigationErrorProps = {
   sectionItemPath: string
 }
 
+const subSectionHasError = async (page: Page, path: string, hasError: boolean): Promise<void> => {
+  await _expectErrorIndicator(getNavigationSubSectionItem(page, path), hasError)
+}
+
+// Check for the error on the subsection and on the top level section header
 const expectNavigationError = async (page: Page, props: ExpectNavigationErrorProps): Promise<void> => {
   const { hasError, sectionHeader, sectionItemPath } = props
-  const subSectionItem = getNavigationSubSectionItem(page, sectionItemPath)
-  const header = page.locator('.nav-section__header', { hasText: sectionHeader })
 
-  await _expectErrorIndicator(subSectionItem, hasError)
+  await subSectionHasError(page, sectionItemPath, hasError)
+
+  const header = page.locator('.nav-section__header', { hasText: sectionHeader })
   await header.click()
   await _expectErrorIndicator(header, hasError)
 }
@@ -31,4 +36,5 @@ const expectNavigationError = async (page: Page, props: ExpectNavigationErrorPro
 export const NavigationUtils = {
   expectNavigationError,
   getNavigationSubSectionItem,
+  subSectionHasError,
 }
