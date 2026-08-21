3415dc4f655a90353e4bc9f83385b203add5d088
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-08-21 11:56:46 -0500

    6263 - pass client to context repository reads

--- src/server/controller/cycleData/tableData/updateDependencies/context/dataContextBuilder.ts
+++ src/server/controller/cycleData/tableData/updateDependencies/context/dataContextBuilder.ts
@@ -34,13 +34,14 @@ export class DataContextBuilder extends BaseContextBuilder {
 
   async #addDependency(variable: VariableCache): Promise<void> {
     const { tableName } = variable
+    const { client } = this.props
 
     const assessmentName = variable.assessmentName ?? this.props.assessment.props.name
     const cycleName = variable.cycleName ?? this.props.cycle.name
 
     // External dependents update might reference an assessment different to the context one
     if (!this.#assessments[assessmentName]) {
-      this.#assessments[assessmentName] = await AssessmentController.getOne({ assessmentName, metaCache: true })
+      this.#assessments[assessmentName] = await AssessmentController.getOne({ assessmentName, metaCache: true }, client)
     }
 
     if (!this.#tables[assessmentName]?.[cycleName]) {
