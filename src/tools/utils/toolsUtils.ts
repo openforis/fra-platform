d27a8fd2ffa2988e08e806ac15537b266b7fd33d
Miró Sorja <ping@mirosorja.com>
2026-09-01 12:41:37 +0300

    6259 - ToolsUtils.confirmVarsAndContinue

--- src/tools/utils/toolsUtils.ts
+++ src/tools/utils/toolsUtils.ts
@@ -1,8 +1,35 @@
+import readline from 'readline'
+
 import { DB } from 'server/db/db'
 import { Logger } from 'server/utils/logger'
 
 const close = async (): Promise<void> => {
   await DB.$pool.end()
+}
+
+/**
+ * Useful when running scripts in different environments
+ *
+ * @example
+ * // generateCache:
+ * const ENV_VARS = ['DATABASE_URL','REDIS_QUEUE_URL','REDIS_DATA_URL','PGSSL','REDIS_TLS_REJECT_UNAUTHORIZED','NODE_ENV']
+ * await ToolsUtils.confirmVarsAndContinue(ENV_VARS)
+ * // PGSSL: true
+ * // REDIS_TLS_REJECT_UNAUTHORIZED: false
+ * // ...
+ * // [key]: [value]
+ * // Press Enter to continue, or Ctrl+C to abort...
+ */
+const confirmVarsAndContinue = async (varNames: Array<string>): Promise<void> => {
+  varNames.forEach((varName) => Logger.info(`${varName}: ${process.env[varName]}`))
+
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+  await new Promise<void>((resolve) => {
+    rl.question('\nPress Enter to continue, or Ctrl+C to abort...', () => {
+      rl.close()
+      resolve()
+    })
+  })
 }
 
 const exec = (fn: () => Promise<unknown>): void => {
@@ -20,5 +47,6 @@ const exec = (fn: () => Promise<unknown>): void => {
 
 export const ToolsUtils = {
   close,
+  confirmVarsAndContinue,
   exec,
 }
