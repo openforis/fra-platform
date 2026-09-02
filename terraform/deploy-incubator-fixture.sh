5ff560217eab0052db91300d8162ccf58889fde4
Miró Sorja <ping@mirosorja.com>
2026-09-02 10:43:34 +0300

    6259 - Terraform: Edit scripts to print web url, add user

--- terraform/deploy-incubator-fixture.sh
+++ terraform/deploy-incubator-fixture.sh
@@ -3,7 +3,13 @@ source "$(dirname "$0")/_common.sh"
 
 # This is a "fast" script
 
-# init schemas, import fixtures and generate cache
+# init schemas, import fixtures, seed test admin (test@test.com / password123) and generate cache
 yarn ts-node src/tools/db/initSchemas.ts
 yarn ts-node src/tools/db/import.ts
 yarn ts-node src/tools/generateCache
+
+# add test user
+yarn ts-node src/tools/user/createAdmin.ts
+
+# print web url
+heroku apps:info -a fra-platform-incubator -s | grep web_url | cut -d= -f2-
--- terraform/deploy-incubator-review.sh
+++ terraform/deploy-incubator-review.sh
@@ -6,3 +6,6 @@ source "$(dirname "$0")/_common.sh"
 # copy review db and generate cache
 heroku pg:copy dev-fra-platform::DATABASE_URL DATABASE_URL -a fra-platform-incubator --confirm DATABASE --verbose
 yarn ts-node src/tools/generateCache
+
+# print web url
+heroku apps:info -a fra-platform-incubator -s | grep web_url | cut -d= -f2-
