85f5e0ee8e5f56e4aa81b65ac12dd29b253ea7b5
Miró Sorja <ping@mirosorja.com>
2026-09-03 16:55:28 +0300

    6259 - Terraform: add test user in review

--- terraform/deploy-incubator-review.sh
+++ terraform/deploy-incubator-review.sh
@@ -7,5 +7,8 @@ source "$(dirname "$0")/_common.sh"
 heroku pg:copy dev-fra-platform::DATABASE_URL DATABASE_URL -a fra-platform-incubator --confirm DATABASE --verbose
 yarn ts-node src/tools/generateCache
 
+# add test user
+yarn ts-node src/tools/user/createAdmin.ts
+
 # print web url
 heroku apps:info -a fra-platform-incubator -s | grep web_url | cut -d= -f2-
