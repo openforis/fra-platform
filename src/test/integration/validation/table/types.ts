86f061867a61e61093525fca3c3ac1d265af1aef
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-09-08 11:30:18 -0500

    6214 - Get data

--- src/test/integration/validation/table/types.ts
+++ src/test/integration/validation/table/types.ts
@@ -1,21 +1,6 @@
-import { ColName } from 'meta/assessment/col'
 import { VariableCache } from 'meta/assessment/metaCache'
 import { NodeValueValidation } from 'meta/assessment/nodeValueValidation'
-import { TableName } from 'meta/assessment/table'
-import { VariableName } from 'meta/assessment/variable'
 import { NodeUpdate } from 'meta/data/nodeUpdates'
-
-type ColMetadata = {
-  colName: ColName
-  validateFns?: Array<string>
-}
-
-type RowMetadata = {
-  cols: Array<ColMetadata>
-  tableName: TableName
-  validateFns?: Array<string>
-  variableName: VariableName
-}
 
 type Cell = Required<Pick<VariableCache, 'colName' | 'tableName' | 'variableName'>>
 
@@ -26,5 +11,4 @@ export type TableValidationTestCase = {
   // Valid cells have no stored validation entry, so valid cases state expected: undefined
   expected: NodeValueValidation | undefined
   name: string
-  rows: Array<RowMetadata>
 }
