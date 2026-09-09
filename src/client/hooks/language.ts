4a0ee6cfe77dd8e681119fd06eb9daa782f1e27b
Miró Sorja <ping@mirosorja.com>
2026-09-09 11:35:43 +0300

    6252 - Validate Params: remove countryIso from language change (#6301)

--- src/client/hooks/language.ts
+++ src/client/hooks/language.ts
@@ -3,16 +3,15 @@ import { useTranslation } from 'react-i18next'
 import axios from 'axios'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
-import { Global } from 'meta/area/global'
 import { Lang } from 'meta/lang'
 
 import { useUser } from 'client/store/user/hooks/user'
-import { useCountryRouteParams } from 'client/hooks/routeParams'
+import { useCycleRouteParams } from 'client/hooks/routeParams'
 import i18nGlobal from 'client/i18n'
 
 type UpdateLanguage = (props: { lang: Lang; persist?: boolean }) => Promise<void>
 export const useUpdateLanguage = (): UpdateLanguage => {
-  const { assessmentName, countryIso, cycleName } = useCountryRouteParams()
+  const { assessmentName, cycleName } = useCycleRouteParams()
   const user = useUser()
 
   return useCallback<UpdateLanguage>(
@@ -33,11 +32,11 @@ export const useUpdateLanguage = (): UpdateLanguage => {
         formData.append('user.id', String(user.id))
         formData.append('user.props', JSON.stringify({ lang }))
 
-        const params = { assessmentName, cycleName, countryIso: countryIso ?? Global.WO }
+        const params = { assessmentName, cycleName }
         await axios.put(ApiEndPoint.User.one(), formData, { params })
       }
     },
-    [assessmentName, countryIso, cycleName, user]
+    [assessmentName, cycleName, user]
   )
 }
 
