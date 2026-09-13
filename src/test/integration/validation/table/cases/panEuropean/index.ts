e24fcef94d0b857f98f7a8d1b35e7a145de76115
Yhoan Alejandro Guzmán García <41337901+yaguzmang@users.noreply.github.com>
2026-09-13 08:44:25 -0500

    6214 - Validation Integration Tests - Pan-European table_1_4a cases (#6342)
    * 6214 - Add Pan-European cases for table_1_4a
    
    * 6214 - Complete the Pan-European table_1_4a soil carbon cases
    
    * 6214 - Remove fixture
    
    * 6214 - Update fixture

--- src/test/integration/validation/table/cases/panEuropean/index.ts
+++ src/test/integration/validation/table/cases/panEuropean/index.ts
@@ -1,5 +1,22 @@
 import { TableValidationTestCase } from '../../types'
 import { table11b } from './table_1_1b'
+import { table12a } from './table_1_2a'
+import { table12b } from './table_1_2b'
+import { table12c } from './table_1_2c'
+import { table13a1 } from './table_1_3a1'
+import { table13a2 } from './table_1_3a2'
+import { table13b } from './table_1_3b'
+import { table14a } from './table_1_4a'
 import { table31 } from './table_3_1'
 
-export const cases: Array<TableValidationTestCase> = [...table11b, ...table31]
+export const cases: Array<TableValidationTestCase> = [
+  ...table11b,
+  ...table12a,
+  ...table12b,
+  ...table12c,
+  ...table13a1,
+  ...table13a2,
+  ...table13b,
+  ...table14a,
+  ...table31,
+]
