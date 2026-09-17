bce2544ae052902d9a0f83dff733ad5f46b30b29
Miró Sorja <ping@mirosorja.com>
2026-09-17 13:51:25 +0300

    6252 - Validate Params: fix getTableDataHistory prop type declaration

--- src/client/store/data/history/actions/getTableDataHistory.ts
+++ src/client/store/data/history/actions/getTableDataHistory.ts
@@ -2,20 +2,19 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 import { ApiEndPoint } from 'meta/api/endpoint'
-import { CountryIso } from 'meta/area/countryIso'
-import { SectionName } from 'meta/assessment/section'
+import { CycleDataParams } from 'meta/api/request/cycleData/cycleData'
 import { RecordAssessmentData } from 'meta/data/recordData'
 
-import { Props } from 'client/store/data/tableData/nodeValues/actions/getTableDataProps'
+type Props = CycleDataParams & { tableNames: Array<string> }
 
-export const getTableDataHistory = createAsyncThunk<
-  RecordAssessmentData,
-  Props & { countryIso: CountryIso; sectionName: SectionName }
->('data/history/tableData/get', async (props) => {
-  const { assessmentName, countryIso, cycleName, sectionName, tableNames } = props
+export const getTableDataHistory = createAsyncThunk<RecordAssessmentData, Props>(
+  'data/history/tableData/get',
+  async (props) => {
+    const { assessmentName, countryIso, cycleName, sectionName, tableNames } = props
 
-  const params = { assessmentName, countryIso, cycleName, tableNames, sectionName }
-  const { data } = await axios.get(ApiEndPoint.CycleData.Table.tableDataHistory(), { params })
+    const params = { assessmentName, countryIso, cycleName, tableNames, sectionName }
+    const { data } = await axios.get(ApiEndPoint.CycleData.Table.tableDataHistory(), { params })
 
-  return data
-})
+    return data
+  }
+)
