43ec2e77111d1b0332a916fd55b150f306e84b54
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-09-17 16:35:59 -0500

    6214 - Add Pan-European cases for table_4_4a

---
+++ src/test/integration/validation/table/cases/panEuropean/table_4_4a/index.ts
@@ -0,0 +1,4 @@
+import { TableValidationTestCase } from '../../../types'
+import { totalForestAndOtherWoodedLand1990 } from './total_forest_and_other_wooded_land_1990'
+
+export const table44a: Array<TableValidationTestCase> = [...totalForestAndOtherWoodedLand1990]
