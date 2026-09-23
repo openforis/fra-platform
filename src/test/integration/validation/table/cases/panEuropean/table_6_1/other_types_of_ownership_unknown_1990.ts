24bb2a5910b7902e5d3f56950eac7f29cfee184d
Alejandro Guzman <alejandroguzman.dev@gmail.com>
2026-09-23 12:55:53 -0500

    6404 - Pass label keys to the table 6.1 size class validations

--- src/test/integration/validation/table/cases/panEuropean/table_6_1/other_types_of_ownership_unknown_1990.ts
+++ src/test/integration/validation/table/cases/panEuropean/table_6_1/other_types_of_ownership_unknown_1990.ts
@@ -1,3 +1,6 @@
 import { buildCases } from './_buildCases'
 
-export const otherTypesOfOwnershipUnknown1990 = buildCases({ variableName: 'other_types_of_ownership_unknown_1990' })
+export const otherTypesOfOwnershipUnknown1990 = buildCases({
+  ownership: 'other',
+  variableName: 'other_types_of_ownership_unknown_1990',
+})
